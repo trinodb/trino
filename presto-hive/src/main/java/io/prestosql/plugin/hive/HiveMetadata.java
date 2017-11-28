c6034745dffde9dbac4a98ce7dd06dbe1ad55a2a
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-11-28 14:51:24 +0100

    Consider enabled roles for permissions
    Extracted-From: https://github.com/prestodb/presto/pull/10904

--- presto-hive/src/main/java/io/prestosql/plugin/hive/HiveMetadata.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/HiveMetadata.java
@@ -183,7 +183,7 @@ import static io.prestosql.plugin.hive.metastore.MetastoreUtil.getProtectMode;
 import static io.prestosql.plugin.hive.metastore.MetastoreUtil.verifyOnline;
 import static io.prestosql.plugin.hive.metastore.StorageFormat.VIEW_STORAGE_FORMAT;
 import static io.prestosql.plugin.hive.metastore.StorageFormat.fromHiveStorageFormat;
-import static io.prestosql.plugin.hive.metastore.thrift.ThriftMetastoreUtil.listApplicableTablePrivileges;
+import static io.prestosql.plugin.hive.metastore.thrift.ThriftMetastoreUtil.listEnabledTablePrivileges;
 import static io.prestosql.plugin.hive.util.ConfigurationUtils.toJobConf;
 import static io.prestosql.plugin.hive.util.Statistics.ReduceOperator.ADD;
 import static io.prestosql.plugin.hive.util.Statistics.createComputedStatisticsToPartitionMap;
@@ -1845,7 +1845,7 @@ public class HiveMetadata
         ImmutableList.Builder<GrantInfo> grantInfos = ImmutableList.builder();
         for (SchemaTableName tableName : listTables(session, schemaTablePrefix)) {
             Set<PrivilegeInfo> privileges =
-                    listApplicableTablePrivileges(metastore, tableName.getSchemaName(), tableName.getTableName(), new PrestoPrincipal(USER, session.getUser())).stream()
+                    listEnabledTablePrivileges(metastore, tableName.getSchemaName(), tableName.getTableName(), session.getIdentity()).stream()
                             .map(HivePrivilegeInfo::toPrivilegeInfo)
                             .flatMap(Set::stream)
                             .collect(toImmutableSet());
