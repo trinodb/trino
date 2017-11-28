c88bdfd29619b8b024acfebd19789e6e6b4c18c4
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-11-28 14:51:21 +0100

    Simplify checkTablePermission
    Extracted-From: https://github.com/prestodb/presto/pull/10904

--- presto-hive/src/main/java/io/prestosql/plugin/hive/security/SqlStandardAccessControl.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/security/SqlStandardAccessControl.java
@@ -13,7 +13,6 @@
  */
 package io.prestosql.plugin.hive.security;
 
-import com.google.common.collect.ImmutableSet;
 import io.prestosql.plugin.hive.HiveConnectorId;
 import io.prestosql.plugin.hive.HiveTransactionHandle;
 import io.prestosql.plugin.hive.metastore.Database;
@@ -381,7 +380,7 @@ public class SqlStandardAccessControl
         return checkTablePermission(transaction, identity, tableName, OWNERSHIP);
     }
 
-    private boolean checkTablePermission(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName, HivePrivilege... requiredPrivileges)
+    private boolean checkTablePermission(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName, HivePrivilege requiredPrivilege)
     {
         if (isAdmin(transaction, identity)) {
             return true;
@@ -396,15 +395,9 @@ public class SqlStandardAccessControl
         }
 
         SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));
-        Set<HivePrivilege> privilegeSet = listApplicableTablePrivileges(
-                metastore,
-                tableName.getSchemaName(),
-                tableName.getTableName(),
-                new PrestoPrincipal(USER, identity.getUser()))
+        return listApplicableTablePrivileges(metastore, tableName.getSchemaName(), tableName.getTableName(), new PrestoPrincipal(USER, identity.getUser()))
                 .stream()
-                .map(HivePrivilegeInfo::getHivePrivilege)
-                .collect(toSet());
-        return privilegeSet.containsAll(ImmutableSet.copyOf(requiredPrivileges));
+                .anyMatch(privilegeInfo -> privilegeInfo.getHivePrivilege().equals(requiredPrivilege));
     }
 
     private boolean hasGrantOptionForPrivilege(ConnectorTransactionHandle transaction, ConnectorIdentity identity, Privilege privilege, SchemaTableName tableName)
