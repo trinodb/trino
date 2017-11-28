dd359c5630d701b5b97b6f5addeeb064ddd36b08
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-11-28 14:50:36 +0100

    Introduce <catalog>.information_schema.roles table
    Extracted-From: https://github.com/prestodb/presto/pull/10904

--- presto-main/src/main/java/io/prestosql/connector/informationSchema/InformationSchemaPageSourceProvider.java
+++ presto-main/src/main/java/io/prestosql/connector/informationSchema/InformationSchemaPageSourceProvider.java
@@ -43,6 +43,7 @@ import java.util.Set;
 
 import static com.google.common.collect.Sets.union;
 import static io.prestosql.connector.informationSchema.InformationSchemaMetadata.TABLE_COLUMNS;
+import static io.prestosql.connector.informationSchema.InformationSchemaMetadata.TABLE_ROLES;
 import static io.prestosql.connector.informationSchema.InformationSchemaMetadata.TABLE_SCHEMATA;
 import static io.prestosql.connector.informationSchema.InformationSchemaMetadata.TABLE_TABLES;
 import static io.prestosql.connector.informationSchema.InformationSchemaMetadata.TABLE_TABLE_PRIVILEGES;
@@ -120,6 +121,9 @@ public class InformationSchemaPageSourceProvider
         }
         if (table.equals(TABLE_TABLE_PRIVILEGES)) {
             return buildTablePrivileges(session, prefixes);
+        }
+        if (table.equals(TABLE_ROLES)) {
+            return buildRoles(session, catalog);
         }
 
         throw new IllegalArgumentException(format("table does not exist: %s", table));
@@ -216,6 +220,15 @@ public class InformationSchemaPageSourceProvider
         InternalTable.Builder table = InternalTable.builder(informationSchemaTableColumns(TABLE_SCHEMATA));
         for (String schema : listSchemas(session, metadata, accessControl, catalogName)) {
             table.add(catalogName, schema);
+        }
+        return table.build();
+    }
+
+    private InternalTable buildRoles(Session session, String catalog)
+    {
+        InternalTable.Builder table = InternalTable.builder(informationSchemaTableColumns(TABLE_ROLES));
+        for (String role : metadata.listRoles(session, catalog)) {
+            table.add(role);
         }
         return table.build();
     }
