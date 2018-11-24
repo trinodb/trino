81332d5fc68053e660a26c0b2a76ea7f402b896c
kasiafi <30203062+kasiafi@users.noreply.github.com>
2018-11-24 17:22:06 +0100

    Add PushLimitThroughOuterJoin rule

--- presto-main/src/main/java/io/prestosql/sql/planner/plan/Patterns.java
+++ presto-main/src/main/java/io/prestosql/sql/planner/plan/Patterns.java
@@ -213,6 +213,14 @@ public class Patterns
         }
     }
 
+    public static class Join
+    {
+        public static Property<JoinNode, Lookup, JoinNode.Type> type()
+        {
+            return property("type", JoinNode::getType);
+        }
+    }
+
     public static class LateralJoin
     {
         public static Property<LateralJoinNode, Lookup, List<Symbol>> correlation()
