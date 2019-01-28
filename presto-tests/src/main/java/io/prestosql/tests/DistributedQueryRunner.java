15feecda9236d0cdd22fb6478e5044a7fb6964f7
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2019-01-28 08:36:30 +0100

    Configure defaults for coordinator in DistributedQueryRunner in one place

--- presto-tests/src/main/java/io/prestosql/tests/DistributedQueryRunner.java
+++ presto-tests/src/main/java/io/prestosql/tests/DistributedQueryRunner.java
@@ -133,7 +133,6 @@ public class DistributedQueryRunner
             }
 
             Map<String, String> extraCoordinatorProperties = new HashMap<>();
-            extraCoordinatorProperties.put("experimental.iterative-optimizer-enabled", "true");
             extraCoordinatorProperties.putAll(extraProperties);
             extraCoordinatorProperties.putAll(coordinatorProperties);
             coordinator = closer.register(createTestingPrestoServer(discoveryServer.getBaseUrl(), true, extraCoordinatorProperties, parserOptions, environment));
@@ -191,6 +190,7 @@ public class DistributedQueryRunner
         if (coordinator) {
             propertiesBuilder.put("node-scheduler.include-coordinator", "true");
             propertiesBuilder.put("join-distribution-type", "PARTITIONED");
+            propertiesBuilder.put("experimental.iterative-optimizer-enabled", "true");
         }
         HashMap<String, String> properties = new HashMap<>(propertiesBuilder.build());
         properties.putAll(extraProperties);
