1492ca499d8bd52cd4697ce5d07722519b6f9f17
Hao Luo <hluo@twitter.com>
2019-01-30 16:36:06 -0800

    Add header for extra credential

--- presto-main/src/main/java/io/prestosql/server/TaskResource.java
+++ presto-main/src/main/java/io/prestosql/server/TaskResource.java
@@ -125,7 +125,7 @@ public class TaskResource
     {
         requireNonNull(taskUpdateRequest, "taskUpdateRequest is null");
 
-        Session session = taskUpdateRequest.getSession().toSession(sessionPropertyManager);
+        Session session = taskUpdateRequest.getSession().toSession(sessionPropertyManager, taskUpdateRequest.getExtraCredentials());
         TaskInfo taskInfo = taskManager.updateTask(session,
                 taskId,
                 taskUpdateRequest.getFragment(),
