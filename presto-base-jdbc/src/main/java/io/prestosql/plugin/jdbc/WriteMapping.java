prestodb/presto

Add WriteMapping
WriteMapping captures how data should be written in a JDBC connector:

- what data type should be used in the remote database
- how the data should be bound to `PreparedStatement`

presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/WriteMapping.java
@@ -1,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.jdbc;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public final class WriteMapping
+{
+    public static WriteMapping booleanWriteMapping(String dataType, BooleanWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping longWriteMapping(String dataType, LongWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping doubleWriteMapping(String dataType, DoubleWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping sliceWriteMapping(String dataType, SliceWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    private final String dataType;
+    private final WriteFunction writeFunction;
+
+    private WriteMapping(String dataType, WriteFunction writeFunction)
+    {
+        this.dataType = requireNonNull(dataType, "dataType is null");
+        this.writeFunction = requireNonNull(writeFunction, "writeFunction is null");
+    }
+
+    public String getDataType()
+    {
+        return dataType;
+    }
+
+    public WriteFunction getWriteFunction()
+    {
+        return writeFunction;
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("dataType", dataType)
+                .toString();
+    }
+}

presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlClient.java
@@ -18,6 +18,7 @@ import com.facebook.presto.plugin.jdbc.BaseJdbcConfig;
 import com.facebook.presto.plugin.jdbc.ConnectionFactory;
 import com.facebook.presto.plugin.jdbc.DriverConnectionFactory;
 import com.facebook.presto.plugin.jdbc.JdbcConnectorId;
+import com.facebook.presto.plugin.jdbc.WriteMapping;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.spi.type.Type;
@@ -37,6 +38,12 @@ import java.util.Properties;
 import java.util.Set;
 
 import static com.facebook.presto.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
+import static com.facebook.presto.plugin.jdbc.StandardColumnMappings.realWriteFunction;
+import static com.facebook.presto.plugin.jdbc.StandardColumnMappings.timestampWriteFunction;
+import static com.facebook.presto.plugin.jdbc.StandardColumnMappings.varbinaryWriteFunction;
+import static com.facebook.presto.plugin.jdbc.StandardColumnMappings.varcharWriteFunction;
+import static com.facebook.presto.plugin.jdbc.WriteMapping.longWriteMapping;
+import static com.facebook.presto.plugin.jdbc.WriteMapping.sliceWriteMapping;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.facebook.presto.spi.type.RealType.REAL;
 import static com.facebook.presto.spi.type.TimeWithTimeZoneType.TIME_WITH_TIME_ZONE;
@@ -143,37 +150,41 @@ public class MySqlClient
     }
 
     @Override
-    protected String toSqlType(Type type)
+    protected WriteMapping toWriteMapping(Type type)
     {
         if (REAL.equals(type)) {
-            return "float";
+            return longWriteMapping("float", realWriteFunction());
         }
         if (TIME_WITH_TIME_ZONE.equals(type) || TIMESTAMP_WITH_TIME_ZONE.equals(type)) {
             throw new PrestoException(NOT_SUPPORTED, "Unsupported column type: " + type.getDisplayName());
         }
         if (TIMESTAMP.equals(type)) {
-            return "datetime";
+            return longWriteMapping("datetime", timestampWriteFunction());
         }
         if (VARBINARY.equals(type)) {
-            return "mediumblob";
+            return sliceWriteMapping("mediumblob", varbinaryWriteFunction());
         }
         if (isVarcharType(type)) {
             VarcharType varcharType = (VarcharType) type;
+            String dataType;
             if (varcharType.isUnbounded()) {
-                return "longtext";
+                dataType = "longtext";
             }
-            if (varcharType.getLengthSafe() <= 255) {
-                return "tinytext";
+            else if (varcharType.getLengthSafe() <= 255) {
+                dataType = "tinytext";
             }
-            if (varcharType.getLengthSafe() <= 65535) {
-                return "text";
+            else if (varcharType.getLengthSafe() <= 65535) {
+                dataType = "text";
             }
-            if (varcharType.getLengthSafe() <= 16777215) {
-                return "mediumtext";
+            else if (varcharType.getLengthSafe() <= 16777215) {
+                dataType = "mediumtext";
             }
-            return "longtext";
+            else {
+                dataType = "longtext";
+            }
+            return sliceWriteMapping(dataType, varcharWriteFunction());
         }
 
-        return super.toSqlType(type);
+        return super.toWriteMapping(type);
     }
 }

trinodb/trino

presto-base-jdbc/src/main/java/io/prestosql/plugin/jdbc/WriteMapping.java
@@ -1,0 +1,70 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.jdbc;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public final class WriteMapping
+{
+    public static WriteMapping booleanMapping(String dataType, BooleanWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping longMapping(String dataType, LongWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping doubleMapping(String dataType, DoubleWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    public static WriteMapping sliceMapping(String dataType, SliceWriteFunction writeFunction)
+    {
+        return new WriteMapping(dataType, writeFunction);
+    }
+
+    private final String dataType;
+    private final WriteFunction writeFunction;
+
+    private WriteMapping(String dataType, WriteFunction writeFunction)
+    {
+        this.dataType = requireNonNull(dataType, "dataType is null");
+        this.writeFunction = requireNonNull(writeFunction, "writeFunction is null");
+    }
+
+    /**
+     * Data type that should be used in the remote database when defining a column.
+     */
+    public String getDataType()
+    {
+        return dataType;
+    }
+
+    public WriteFunction getWriteFunction()
+    {
+        return writeFunction;
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("dataType", dataType)
+                .toString();
+    }
+}

presto-mysql/src/main/java/io/prestosql/plugin/mysql/MySqlClient.java
@@ -21,6 +21,7 @@ import io.prestosql.plugin.jdbc.BaseJdbcConfig;
 import io.prestosql.plugin.jdbc.ConnectionFactory;
 import io.prestosql.plugin.jdbc.DriverConnectionFactory;
 import io.prestosql.plugin.jdbc.JdbcConnectorId;
+import io.prestosql.plugin.jdbc.WriteMapping;
 import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.connector.SchemaTableName;
 import io.prestosql.spi.type.Type;
@@ -38,6 +39,10 @@ import java.util.Set;
 
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static io.prestosql.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
+import static io.prestosql.plugin.jdbc.StandardColumnMappings.realWriteFunction;
+import static io.prestosql.plugin.jdbc.StandardColumnMappings.timestampWriteFunction;
+import static io.prestosql.plugin.jdbc.StandardColumnMappings.varbinaryWriteFunction;
+import static io.prestosql.plugin.jdbc.StandardColumnMappings.varcharWriteFunction;
 import static io.prestosql.spi.StandardErrorCode.NOT_SUPPORTED;
 import static io.prestosql.spi.type.RealType.REAL;
 import static io.prestosql.spi.type.TimeWithTimeZoneType.TIME_WITH_TIME_ZONE;
@@ -143,37 +148,41 @@ public class MySqlClient
     }
 
     @Override
-    protected String toSqlType(Type type)
+    protected WriteMapping toWriteMapping(Type type)
     {
         if (REAL.equals(type)) {
-            return "float";
+            return WriteMapping.longMapping("float", realWriteFunction());
         }
         if (TIME_WITH_TIME_ZONE.equals(type) || TIMESTAMP_WITH_TIME_ZONE.equals(type)) {
             throw new PrestoException(NOT_SUPPORTED, "Unsupported column type: " + type.getDisplayName());
         }
         if (TIMESTAMP.equals(type)) {
-            return "datetime";
+            return WriteMapping.longMapping("datetime", timestampWriteFunction());
         }
         if (VARBINARY.equals(type)) {
-            return "mediumblob";
+            return WriteMapping.sliceMapping("mediumblob", varbinaryWriteFunction());
         }
         if (isVarcharType(type)) {
             VarcharType varcharType = (VarcharType) type;
+            String dataType;
             if (varcharType.isUnbounded()) {
-                return "longtext";
+                dataType = "longtext";
             }
-            if (varcharType.getBoundedLength() <= 255) {
-                return "tinytext";
+            else if (varcharType.getBoundedLength() <= 255) {
+                dataType = "tinytext";
             }
-            if (varcharType.getBoundedLength() <= 65535) {
-                return "text";
+            else if (varcharType.getBoundedLength() <= 65535) {
+                dataType = "text";
             }
-            if (varcharType.getBoundedLength() <= 16777215) {
-                return "mediumtext";
+            else if (varcharType.getBoundedLength() <= 16777215) {
+                dataType = "mediumtext";
             }
-            return "longtext";
+            else {
+                dataType = "longtext";
+            }
+            return WriteMapping.sliceMapping(dataType, varcharWriteFunction());
         }
 
-        return super.toSqlType(type);
+        return super.toWriteMapping(type);
     }
 }