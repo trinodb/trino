d32e598a3598a4c7c123b7ce9f5bf479341a5f00
Zhengren Pan <zhengrenpan@gmail.com>
2019-02-16 16:28:21 -0800

    Allow PrestoS3FileSystem to support skipping Glacier files

--- presto-hive/src/main/java/io/prestosql/plugin/hive/s3/PrestoS3FileSystem.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/s3/PrestoS3FileSystem.java
@@ -94,6 +94,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static com.amazonaws.regions.Regions.US_EAST_1;
 import static com.amazonaws.services.s3.Headers.SERVER_SIDE_ENCRYPTION;
 import static com.amazonaws.services.s3.Headers.UNENCRYPTED_CONTENT_LENGTH;
+import static com.amazonaws.services.s3.model.StorageClass.Glacier;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkPositionIndexes;
 import static com.google.common.base.Strings.isNullOrEmpty;
@@ -122,6 +123,7 @@ import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_PATH_STYLE_A
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_PIN_CLIENT_TO_CURRENT_REGION;
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SECRET_KEY;
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SIGNER_TYPE;
+import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SKIP_GLACIER_OBJECTS;
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SOCKET_TIMEOUT;
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SSE_ENABLED;
 import static io.prestosql.plugin.hive.s3.S3ConfigurationUpdater.S3_SSE_KMS_KEY_ID;
@@ -177,6 +179,7 @@ public class PrestoS3FileSystem
     private long multiPartUploadMinFileSize;
     private long multiPartUploadMinPartSize;
     private PrestoS3AclType s3AclType;
+    private boolean skipGlacierObjects;
 
     @Override
     public void initialize(URI uri, Configuration conf)
@@ -212,6 +215,7 @@ public class PrestoS3FileSystem
         this.sseKmsKeyId = conf.get(S3_SSE_KMS_KEY_ID, defaults.getS3SseKmsKeyId());
         this.s3AclType = PrestoS3AclType.valueOf(conf.get(S3_ACL_TYPE, defaults.getS3AclType().name()));
         String userAgentPrefix = conf.get(S3_USER_AGENT_PREFIX, defaults.getS3UserAgentPrefix());
+        this.skipGlacierObjects = conf.getBoolean(S3_SKIP_GLACIER_OBJECTS, defaults.isSkipGlacierObjects());
 
         ClientConfiguration configuration = new ClientConfiguration()
                 .withMaxErrorRetry(maxErrorRetries)
@@ -527,6 +531,7 @@ public class PrestoS3FileSystem
         // user metadata, and in this case it doesn't matter.
         return objects.stream()
                 .filter(object -> !object.getKey().endsWith(PATH_SEPARATOR))
+                .filter(object -> !skipGlacierObjects || !isGlacierObject(object))
                 .map(object -> new FileStatus(
                         object.getSize(),
                         false,
@@ -536,6 +541,11 @@ public class PrestoS3FileSystem
                         qualifiedPath(new Path(PATH_SEPARATOR + object.getKey()))))
                 .map(this::createLocatedFileStatus)
                 .iterator();
+    }
+
+    private boolean isGlacierObject(S3ObjectSummary object)
+    {
+        return Glacier.toString().equals(object.getStorageClass());
     }
 
     /**
