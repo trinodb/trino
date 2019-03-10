f8094f754d00432c504d42f6ff6a5e4f72c3fcaa
Dain Sundstrom <dain@iq80.com>
2019-03-09 20:25:35 -0800

    Rename SqlQueryManagerStats to QueryManagerStats

--- presto-main/src/main/java/io/prestosql/execution/SqlQueryManager.java
+++ presto-main/src/main/java/io/prestosql/execution/SqlQueryManager.java
@@ -78,7 +78,7 @@ public class SqlQueryManager
     private final ScheduledExecutorService queryManagementExecutor;
     private final ThreadPoolExecutorMBean queryManagementExecutorMBean;
 
-    private final SqlQueryManagerStats stats = new SqlQueryManagerStats();
+    private final QueryManagerStats stats = new QueryManagerStats();
 
     @Inject
     public SqlQueryManager(ClusterMemoryManager memoryManager, QueryMonitor queryMonitor, EmbedVersion embedVersion, QueryManagerConfig queryManagerConfig)
@@ -273,7 +273,7 @@ public class SqlQueryManager
     @Override
     @Managed
     @Flatten
-    public SqlQueryManagerStats getStats()
+    public QueryManagerStats getStats()
     {
         return stats;
     }
