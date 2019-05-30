656eb15dcc917df02a6057d798bc716393acdcbd
David Phillips <david@acz.org>
2019-05-29 21:40:18 -0700

    Fix decoding of UUID type in CLI and JDBC driver

--- presto-client/src/main/java/io/prestosql/client/ClientStandardTypes.java
+++ presto-client/src/main/java/io/prestosql/client/ClientStandardTypes.java
@@ -41,6 +41,7 @@ public final class ClientStandardTypes
     public static final String MAP = "map";
     public static final String JSON = "json";
     public static final String IPADDRESS = "ipaddress";
+    public static final String UUID = "uuid";
     public static final String GEOMETRY = "Geometry";
     public static final String BING_TILE = "BingTile";
 
