26a5cc97b7771142e730b4f481e6b4d27cd6feeb
praveenkrishna <praveenkrishna@tutanota.com>
2019-06-04 18:42:04 +0530

    Simplify ClientTypeSignature#toString

--- presto-client/src/test/java/io/prestosql/client/TestClientTypeSignature.java
+++ presto-client/src/test/java/io/prestosql/client/TestClientTypeSignature.java
@@ -60,6 +60,23 @@ public class TestClientTypeSignature
         assertEquals(legacy, signature);
     }
 
+    @Test
+    public void testStringSerialization()
+    {
+        ClientTypeSignature bigint = new ClientTypeSignature(StandardTypes.BIGINT);
+        assertEquals(bigint.toString(), "bigint");
+        ClientTypeSignature varchar = new ClientTypeSignature(StandardTypes.VARCHAR, ImmutableList.of(ClientTypeSignatureParameter.ofLong(50)));
+        assertEquals(varchar.toString(), "varchar(50)");
+        ClientTypeSignature array = new ClientTypeSignature(StandardTypes.ARRAY, ImmutableList.of(ClientTypeSignatureParameter.ofType(new ClientTypeSignature(StandardTypes.BIGINT))));
+        assertEquals(array.toString(), "array(bigint)");
+        ClientTypeSignature row = new ClientTypeSignature(
+                StandardTypes.ROW,
+                ImmutableList.of(
+                        ClientTypeSignatureParameter.ofNamedType(new NamedClientTypeSignature(Optional.of(new RowFieldName("foo", false)), bigint)),
+                        ClientTypeSignatureParameter.ofNamedType(new NamedClientTypeSignature(Optional.of(new RowFieldName("bar", false)), bigint))));
+        assertEquals(row.toString(), "row(foo bigint,bar bigint)");
+    }
+
     private static void assertJsonRoundTrip(ClientTypeSignature signature)
     {
         String json = CLIENT_TYPE_SIGNATURE_CODEC.toJson(signature);
