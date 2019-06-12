794ddeadb8c74e2ec9885920851285a8bcea4ab5
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-06-12 22:21:14 +0200

    Define product test suites
    `PRODUCT_TESTS_BASIC_ENVIRONMENT` became `product-tests-suite-1.sh`
    `PRODUCT_TESTS_BASIC_ENVIRONMENT` became `product-tests-suite-2.sh`
    `PRODUCT_TESTS_SPECIFIC_ENVIRONMENT_2` became `product-tests-suite-3.sh`

---
+++ presto-product-tests/bin/product-tests-suite-1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+presto-product-tests/bin/run_on_docker.sh \
+    multinode \
+    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql_connector,postgresql_connector,mysql,kafka,avro
