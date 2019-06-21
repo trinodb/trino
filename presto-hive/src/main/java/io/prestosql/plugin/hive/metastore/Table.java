d10adc629a857851879759adce356f32cc146abf
David Phillips <david@acz.org>
2019-06-21 12:43:48 -0700

    Add setParameter to Table.Builder

--- presto-hive/src/main/java/io/prestosql/plugin/hive/metastore/Table.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/metastore/Table.java
@@ -297,6 +297,12 @@ public class Table
             return this;
         }
 
+        public Builder setParameter(String key, String value)
+        {
+            this.parameters.put(key, value);
+            return this;
+        }
+
         public Builder setViewOriginalText(Optional<String> viewOriginalText)
         {
             this.viewOriginalText = viewOriginalText;
