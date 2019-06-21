b8e1c4768231eca79a2b74753f96ffd80e59657c
Anoop Johnson <anoopj@amazon.com>
2019-06-20 21:13:15 -0700

    Get PrestoS3FileSystem, S3Select and Glue Metastore to work with AWS Default Credentials Provider
    DefaultAWSCredentialsProviderChain is frequently used by AWS customers
    and it provides access from a documented list of sources. This
    especially makes it easier to run Presto on non-EC2 hosts where you
    don't have the instance profile. (e.g. Macs, during development). This
    change also makes PrestoS3FileSystem to be consistent with the Glue
    connector in Presto.
    
    See:
    https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/DefaultAWSCredentialsProviderChain.html

--- presto-hive/src/test/java/io/prestosql/plugin/hive/s3/TestPrestoS3FileSystem.java
+++ presto-hive/src/test/java/io/prestosql/plugin/hive/s3/TestPrestoS3FileSystem.java
@@ -18,6 +18,7 @@ import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -155,8 +156,8 @@ public class TestPrestoS3FileSystem
         }
     }
 
-    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "S3 credentials not configured")
-    public void testInstanceCredentialsDisabled()
+    @Test
+    public void testDefaultCredentials()
             throws Exception
     {
         Configuration config = new Configuration();
@@ -164,6 +165,7 @@ public class TestPrestoS3FileSystem
 
         try (PrestoS3FileSystem fs = new PrestoS3FileSystem()) {
             fs.initialize(new URI("s3n://test-bucket/"), config);
+            assertInstanceOf(getAwsCredentialsProvider(fs), DefaultAWSCredentialsProviderChain.class);
         }
     }
 
