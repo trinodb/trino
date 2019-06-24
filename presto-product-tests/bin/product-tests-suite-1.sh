a79d6ad5ddf8fae735db58d770dcf3c0739ea2c0
Anu Sudarsan <anuatinfy@gmail.com>
2019-06-24 14:44:15 -0400

    Exclude Enterprise JDBC tests from travis pipeline

--- presto-product-tests/bin/product-tests-suite-1.sh
+++ presto-product-tests/bin/product-tests-suite-1.sh
@@ -4,4 +4,4 @@ set -xeuo pipefail
 
 presto-product-tests/bin/run_on_docker.sh \
     multinode \
-    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql_connector,postgresql_connector,mysql,kafka,avro
+    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql_connector,postgresql_connector,mysql,kafka,avro,simba_jdbc
