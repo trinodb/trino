3acbabee7a5943b76fac725c01bd6097d592e6af
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-07-25 16:05:02 +0200

    Remove obsolete override

--- presto-base-jdbc/src/test/java/io/prestosql/plugin/jdbc/TestJdbcDistributedQueries.java
+++ presto-base-jdbc/src/test/java/io/prestosql/plugin/jdbc/TestJdbcDistributedQueries.java
@@ -25,9 +25,4 @@ public class TestJdbcDistributedQueries
     {
         super(() -> createJdbcQueryRunner(TpchTable.getTables()));
     }
-
-    @Override
-    public void testLargeIn()
-    {
-    }
 }
