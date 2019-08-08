b65a0fd5838b75a2d7e56f020a1a65e52486e35b
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-08-08 21:38:54 +0200

    Support writing PostgreSQL timestamp when gap in JVM zone

--- presto-postgresql/src/test/java/io/prestosql/plugin/postgresql/TestPostgreSqlTypeMapping.java
+++ presto-postgresql/src/test/java/io/prestosql/plugin/postgresql/TestPostgreSqlTypeMapping.java
@@ -544,17 +544,9 @@ public class TestPostgreSqlTypeMapping
                     .addRoundTrip(timestampDataType(), timeDoubledInJvmZone)
                     .addRoundTrip(timestampDataType(), timeDoubledInVilnius);
 
-            if (insertWithPresto) {
-                // When inserting with Presto, we effectively go through PostgreSQL driver's PreparedStatement.
-                // The driver converts LocalDateTime to string representing date-time in JVM zone
-                // TODO https://github.com/pgjdbc/pgjdbc/issues/1390 or find a different way to write timestamp values
-            }
-            else {
-                addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, epoch); // epoch also is a gap in JVM zone
-                addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInJvmZone1);
-                addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInJvmZone2);
-            }
-
+            addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, epoch); // epoch also is a gap in JVM zone
+            addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInJvmZone1);
+            addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInJvmZone2);
             addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInVilnius);
             addTimestampTestIfSupported(tests, legacyTimestamp, sessionZone, timeGapInKathmandu);
 
