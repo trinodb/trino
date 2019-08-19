45f30a278c30b27c8e673a0f873a7d28fc206a84
wujianchao <wujianchao@oppo.com>
2019-08-19 20:26:58 +0800

    to trigger build

--- presto-hive/src/main/java/io/prestosql/plugin/hive/rcfile/RcFilePageSourceFactory.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/rcfile/RcFilePageSourceFactory.java
@@ -188,7 +188,7 @@ public class RcFilePageSourceFactory
 
         // the first three separators are set by old-old properties
         separators[0] = getByte(schema.getProperty(FIELD_DELIM, schema.getProperty(SERIALIZATION_FORMAT)), DEFAULT_SEPARATORS[0]);
-        // for map field collection deliminator, hive 1.x use "collection.delim" but hive 3.x use "colelction.delim"
+        // for map field collection deliminator, hive 1.x use "collection.delim" but hive 3.x use "colelction.delim" 
         separators[1] = getByte(schema.getProperty(COLLECTION_DELIM, schema.getProperty("colelction.delim")), DEFAULT_SEPARATORS[1]);
         separators[2] = getByte(schema.getProperty(MAPKEY_DELIM), DEFAULT_SEPARATORS[2]);
 
