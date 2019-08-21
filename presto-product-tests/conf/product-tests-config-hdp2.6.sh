86cc18a6e3c0052cc22a144a31bb1a7d7a7dfbe5
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-08-21 14:59:04 +0200

    Run product tests on HDP 3.1 and 2.6
    HDP 3.1 gives better test coverage in the future.
    
    Tests on HDP 2.6 will be run in "branch builds".

---
+++ presto-product-tests/conf/product-tests-config-hdp2.6.sh
@@ -0,0 +1,2 @@
+export HADOOP_BASE_IMAGE="prestodev/hdp2.6-hive"
+export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/volumes/conf/tempto/tempto-configuration-for-hadoop2.yaml"
--- presto-product-tests/conf/product-tests-config-hdp3.sh
+++
@@ -1,2 +0,0 @@
-export HADOOP_BASE_IMAGE="prestodev/hdp3.1-hive"
-export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/volumes/conf/tempto/tempto-configuration-for-hdp3.yaml"
