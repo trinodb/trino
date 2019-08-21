45b0cf97ff56ade4a8038b641cf8e7cbb21adda1
JackyWoo <wuchienchao@qq.com>
2019-08-21 10:07:15 +0800

    fix spelling error
    Co-Authored-By: Piotr Findeisen <piotr.findeisen@gmail.com>

--- presto-hive/src/main/java/io/prestosql/plugin/hive/rcfile/RcFilePageSourceFactory.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/rcfile/RcFilePageSourceFactory.java
@@ -188,7 +188,7 @@ public class RcFilePageSourceFactory
 
         // the first three separators are set by old-old properties
         separators[0] = getByte(schema.getProperty(FIELD_DELIM, schema.getProperty(SERIALIZATION_FORMAT)), DEFAULT_SEPARATORS[0]);
-        // for map field collection deliminator, hive 1.x use "colelction.delim" but hive 3.x use "collection.delim"
+        // for map field collection delimiter, Hive 1.x uses "colelction.delim" but Hive 3.x uses "collection.delim"
         // https://github.com/prestosql/presto/pull/1321
         separators[1] = getByte(schema.getProperty(COLLECTION_DELIM, schema.getProperty("colelction.delim")), DEFAULT_SEPARATORS[1]);
         separators[2] = getByte(schema.getProperty(MAPKEY_DELIM), DEFAULT_SEPARATORS[2]);
