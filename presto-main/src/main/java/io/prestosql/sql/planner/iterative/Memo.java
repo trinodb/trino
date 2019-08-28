9fcb7288ca57b79083fcc06975e61f18d97038cc
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-08-28 21:50:23 +0200

    Reduce getGroup() calls

--- presto-main/src/main/java/io/prestosql/sql/planner/iterative/Memo.java
+++ presto-main/src/main/java/io/prestosql/sql/planner/iterative/Memo.java
@@ -110,9 +110,10 @@ public class Memo
         return resolveGroupReferences(node, Lookup.from(planNode -> Stream.of(this.resolve(planNode))));
     }
 
-    public PlanNode replace(int group, PlanNode node, String reason)
+    public PlanNode replace(int groupId, PlanNode node, String reason)
     {
-        PlanNode old = getGroup(group).membership;
+        Group group = getGroup(groupId);
+        PlanNode old = group.membership;
 
         checkArgument(new HashSet<>(old.getOutputSymbols()).equals(new HashSet<>(node.getOutputSymbols())),
                 "%s: transformed expression doesn't produce same outputs: %s vs %s",
@@ -127,21 +128,21 @@ public class Memo
             node = insertChildrenAndRewrite(node);
         }
 
-        incrementReferenceCounts(node, group);
-        getGroup(group).membership = node;
-        decrementReferenceCounts(old, group);
+        incrementReferenceCounts(node, groupId);
+        group.membership = node;
+        decrementReferenceCounts(old, groupId);
         evictStatisticsAndCost(group);
 
         return node;
     }
 
-    private void evictStatisticsAndCost(int group)
+    private void evictStatisticsAndCost(Group group)
     {
-        getGroup(group).stats = null;
-        getGroup(group).cost = null;
-        for (int parentGroup : getGroup(group).incomingReferences.elementSet()) {
+        group.stats = null;
+        group.cost = null;
+        for (int parentGroup : group.incomingReferences.elementSet()) {
             if (parentGroup != ROOT_GROUP_REF) {
-                evictStatisticsAndCost(parentGroup);
+                evictStatisticsAndCost(getGroup(parentGroup));
             }
         }
     }
@@ -155,7 +156,7 @@ public class Memo
     {
         Group group = getGroup(groupId);
         if (group.stats != null) {
-            evictStatisticsAndCost(groupId); // cost is derived from stats, also needs eviction
+            evictStatisticsAndCost(group); // cost is derived from stats, also needs eviction
         }
         group.stats = requireNonNull(stats, "stats is null");
     }
