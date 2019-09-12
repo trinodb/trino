c5f1c093810cc2c3adf6439f9833c6df4e04311a
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-09-12 10:57:00 +0200

    Add convenience debugging configuration for CLI in product tests

--- presto-product-tests/conf/docker/files/presto-cli.sh
+++ presto-product-tests/conf/docker/files/presto-cli.sh
@@ -2,4 +2,7 @@
 
 set -euxo pipefail
 
-java -jar /docker/volumes/presto-cli/presto-cli-executable.jar ${CLI_ARGUMENTS} "$@"
+java \
+    `#-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5008` \
+    -jar /docker/volumes/presto-cli/presto-cli-executable.jar \
+    ${CLI_ARGUMENTS} "$@"
