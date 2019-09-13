742c022e92610b6ba737b142d9c18ae338dd01f0
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-09-13 09:41:06 +0200

    Continue product tests when a test run fails

--- presto-product-tests/bin/product-tests-suite-4.sh
+++ presto-product-tests/bin/product-tests-suite-4.sh
@@ -2,23 +2,32 @@
 
 set -xeuo pipefail
 
+exit_code=0
+
 presto-product-tests/bin/run_on_docker.sh \
     multinode-tls \
-    -g smoke,cli,group-by,join,tls
+    -g smoke,cli,group-by,join,tls \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     multinode-tls-kerberos \
-    -g cli,group-by,join,tls
+    -g cli,group-by,join,tls \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-ldap \
     -g ldap \
-    -x simba_jdbc
+    -x simba_jdbc \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-sqlserver \
-    -g sqlserver
+    -g sqlserver \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-kafka \
-    -g kafka
+    -g kafka \
+    || exit_code=1
+
+exit "${exit_code}"
