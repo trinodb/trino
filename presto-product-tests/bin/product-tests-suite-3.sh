5c1fa38e8563def29ee454f9cc4e7300cd8c8348
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-09-13 09:41:06 +0200

    Continue product tests when a test run fails

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -2,28 +2,38 @@
 
 set -xeuo pipefail
 
+exit_code=0
+
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-mysql \
-    -g mysql_connector,mysql
+    -g mysql_connector,mysql \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-postgresql \
-    -g postgresql_connector
+    -g postgresql_connector \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-cassandra \
-    -g cassandra
+    -g cassandra \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-kerberos-hdfs-impersonation-with-wire-encryption \
-    -g storage_formats,cli,hdfs_impersonation,authorization
+    -g storage_formats,cli,hdfs_impersonation,authorization \
+    || exit_code=1
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-kerberos-kms-hdfs-no-impersonation \
-    -g storage_formats
+    -g storage_formats \
+    || exit_code=1
 
 # TODO enable avro_schema_url when adding Metastore impersonation
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-kerberos-kms-hdfs-impersonation \
     -g storage_formats \
-    -x avro_schema_url
+    -x avro_schema_url \
+    || exit_code=1
+
+exit "${exit_code}"
