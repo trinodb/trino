e1d4bcca050be2e7995fd08160ca3620e5358d52
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-09-30 09:44:02 +0200

    Fix running product tests on HDP 3.1
    Previously the `PRODUCT_TESTS_CONFIG=config-hdp3` configurations would
    still run on HDP 2.6.

--- presto-product-tests/conf/product-tests-config-hdp3.sh
+++ presto-product-tests/conf/product-tests-config-hdp3.sh
@@ -1,2 +1,2 @@
-HADOOP_BASE_IMAGE="prestodev/hdp3.1-hive"
-TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/volumes/conf/tempto/tempto-configuration-for-hdp3.yaml"
+export HADOOP_BASE_IMAGE="prestodev/hdp3.1-hive"
+export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/volumes/conf/tempto/tempto-configuration-for-hdp3.yaml"
