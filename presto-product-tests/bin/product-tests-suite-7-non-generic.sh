9c1b9d8e29861c0b47cff45543c8fcf65191d90e
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-10-01 16:12:38 +0200

    Extract suite with non-generic product tests

---
+++ presto-product-tests/bin/product-tests-suite-7-non-generic.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# Suite of product tests which are *not* run dependent on ${HADOOP_BASE_IMAGE} and therefore
+# there is no point in running them several times, for different Hadoop distributions.
+
+if test -v HADOOP_BASE_IMAGE; then
+    echo "The suite should be run with default HADOOP_BASE_IMAGE. Leave HADOOP_BASE_IMAGE unset." >&2
+    exit 1
+fi
+
+exit_code=0
+
+# Does not use hadoop
+presto-product-tests/bin/run_on_docker.sh \
+    singlenode-mysql \
+    -g mysql \
+    || exit_code=1
+
+# Does not use hadoop
+presto-product-tests/bin/run_on_docker.sh \
+    singlenode-postgresql \
+    -g postgresql \
+    || exit_code=1
+
+# Does not use hadoop
+presto-product-tests/bin/run_on_docker.sh \
+    singlenode-sqlserver \
+    -g sqlserver \
+    || exit_code=1
+
+exit "${exit_code}"
