007078acdeaaafcb4c558344b88940eb9bbefe6d
David Phillips <david@acz.org>
2019-10-04 11:17:50 -0700

    Remove legacy serialization for ClientTypeSignature

--- presto-client/src/main/java/io/prestosql/client/ClientTypeSignature.java
+++ presto-client/src/main/java/io/prestosql/client/ClientTypeSignature.java
@@ -16,6 +16,7 @@ package io.prestosql.client;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
+import io.prestosql.client.ClientTypeSignatureParameter.ParameterKind;
 
 import javax.annotation.concurrent.Immutable;
 
@@ -23,7 +24,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -47,48 +47,16 @@ public class ClientTypeSignature
         this(rawType, ImmutableList.of());
     }
 
-    public ClientTypeSignature(String rawType, List<ClientTypeSignatureParameter> arguments)
-    {
-        this(rawType, ImmutableList.of(), ImmutableList.of(), arguments);
-    }
-
     @JsonCreator
     public ClientTypeSignature(
             @JsonProperty("rawType") String rawType,
-            @JsonProperty("typeArguments") List<ClientTypeSignature> typeArguments,
-            @JsonProperty("literalArguments") List<Object> literalArguments,
             @JsonProperty("arguments") List<ClientTypeSignatureParameter> arguments)
     {
         requireNonNull(rawType, "rawType is null");
         this.rawType = rawType;
         checkArgument(!rawType.isEmpty(), "rawType is empty");
         checkArgument(!PATTERN.matcher(rawType).matches(), "Bad characters in rawType type: %s", rawType);
-        if (arguments != null) {
-            this.arguments = unmodifiableList(new ArrayList<>(arguments));
-        }
-        else {
-            requireNonNull(typeArguments, "typeArguments is null");
-            requireNonNull(literalArguments, "literalArguments is null");
-            ImmutableList.Builder<ClientTypeSignatureParameter> convertedArguments = ImmutableList.builder();
-            // Talking to a legacy server (< 0.133)
-            if (rawType.equals(ROW)) {
-                checkArgument(typeArguments.size() == literalArguments.size());
-                for (int i = 0; i < typeArguments.size(); i++) {
-                    Object value = literalArguments.get(i);
-                    checkArgument(value instanceof String, "Expected literalArgument %s in %s to be a string", i, literalArguments);
-                    convertedArguments.add(ClientTypeSignatureParameter.ofNamedType(new NamedClientTypeSignature(
-                            Optional.of(new RowFieldName((String) value, false)),
-                            typeArguments.get(i))));
-                }
-            }
-            else {
-                checkArgument(literalArguments.isEmpty(), "Unexpected literal arguments from legacy server");
-                for (ClientTypeSignature typeArgument : typeArguments) {
-                    convertedArguments.add(new ClientTypeSignatureParameter(ParameterKind.TYPE, typeArgument));
-                }
-            }
-            this.arguments = convertedArguments.build();
-        }
+        this.arguments = unmodifiableList(new ArrayList<>(requireNonNull(arguments, "arguments is null")));
     }
 
     @JsonProperty
@@ -110,49 +78,6 @@ public class ClientTypeSignature
                         "Expected all parameters to be TypeSignatures but [%s] was found", parameter))
                 .map(ClientTypeSignatureParameter::getTypeSignature)
                 .collect(toImmutableList());
-    }
-
-    /**
-     * This field is deprecated and clients should switch to {@link #getArguments()}
-     */
-    @Deprecated
-    @JsonProperty
-    public List<ClientTypeSignature> getTypeArguments()
-    {
-        List<ClientTypeSignature> result = new ArrayList<>();
-        for (ClientTypeSignatureParameter argument : arguments) {
-            switch (argument.getKind()) {
-                case TYPE:
-                    result.add(argument.getTypeSignature());
-                    break;
-                case NAMED_TYPE:
-                    result.add(argument.getNamedTypeSignature().getTypeSignature());
-                    break;
-                default:
-                    return new ArrayList<>();
-            }
-        }
-        return result;
-    }
-
-    /**
-     * This field is deprecated and clients should switch to {@link #getArguments()}
-     */
-    @Deprecated
-    @JsonProperty
-    public List<Object> getLiteralArguments()
-    {
-        List<Object> result = new ArrayList<>();
-        for (ClientTypeSignatureParameter argument : arguments) {
-            switch (argument.getKind()) {
-                case NAMED_TYPE:
-                    result.add(argument.getNamedTypeSignature().getName());
-                    break;
-                default:
-                    return new ArrayList<>();
-            }
-        }
-        return result;
     }
 
     @Override
