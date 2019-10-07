f2b432d76c2beff67ea38757420e58764cace1b6
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-10-07 10:15:42 +0200

    Merge product test suite 4 into 3

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -5,6 +5,16 @@ set -xeuo pipefail
 exit_code=0
 
 presto-product-tests/bin/run_on_docker.sh \
+    multinode-tls \
+    -g smoke,cli,group-by,join,tls \
+    || exit_code=1
+
+presto-product-tests/bin/run_on_docker.sh \
+    multinode-tls-kerberos \
+    -g cli,group-by,join,tls \
+    || exit_code=1
+
+presto-product-tests/bin/run_on_docker.sh \
     singlenode-kerberos-hdfs-impersonation-with-wire-encryption \
     -g storage_formats,cli,hdfs_impersonation,authorization \
     || exit_code=1
