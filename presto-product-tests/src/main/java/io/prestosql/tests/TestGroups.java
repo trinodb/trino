f3fc9ac8c9bf482f27db25d0a9e02ee38da3d4d3
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2019-10-21 12:01:54 +0200

    Add tests for two different Hive catalogs

--- presto-product-tests/src/main/java/io/prestosql/tests/TestGroups.java
+++ presto-product-tests/src/main/java/io/prestosql/tests/TestGroups.java
@@ -64,6 +64,7 @@ public final class TestGroups
     public static final String BIG_QUERY = "big_query";
     public static final String HIVE_TABLE_STATISTICS = "hive_table_statistics";
     public static final String KAFKA = "kafka";
+    public static final String TWO_HIVES = "two_hives";
 
     private TestGroups() {}
 }
