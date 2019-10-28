05ed0f86372a0022ffc946706ddd7bedac803b39
ptkool <michael.styles@shopify.com>
2019-10-28 14:21:39 -0400

    Fix checkstyle violation

--- presto-main/src/main/java/io/prestosql/sql/analyzer/StatementAnalyzer.java
+++ presto-main/src/main/java/io/prestosql/sql/analyzer/StatementAnalyzer.java
@@ -146,7 +146,6 @@ import io.prestosql.type.TypeCoercion;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
