016caf07c8e48b2619276a84b7e5b0c9ea7ead83
David Phillips <david@acz.org>
2019-10-28 17:03:05 -0700

    Cleanup TestDbSessionPropertyManagerIntegration

--- presto-session-property-managers/src/test/java/io/prestosql/plugin/session/db/TestDbSessionPropertyManagerIntegration.java
+++ presto-session-property-managers/src/test/java/io/prestosql/plugin/session/db/TestDbSessionPropertyManagerIntegration.java
@@ -48,15 +48,15 @@ import org.testng.annotations.Test;
 import java.util.Map;
 import java.util.Optional;
 
-import static com.google.common.base.Throwables.throwIfUnchecked;
 import static com.google.common.collect.MoreCollectors.onlyElement;
 import static io.airlift.configuration.ConfigBinder.configBinder;
+import static io.airlift.testing.Closeables.closeQuietly;
 import static io.prestosql.testing.TestingSession.DEFAULT_TIME_ZONE_KEY;
 import static java.util.Collections.emptyMap;
 import static java.util.Locale.ENGLISH;
 import static java.util.concurrent.TimeUnit.DAYS;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
 import static org.weakref.jmx.guice.ExportBinder.newExporter;
 
 public class TestDbSessionPropertyManagerIntegration
@@ -67,7 +67,7 @@ public class TestDbSessionPropertyManagerIntegration
     private static final Duration EXAMPLE_VALUE_DEFAULT = new QueryManagerConfig().getQueryMaxCpuTime();
     private static final Duration EXAMPLE_VALUE_CONFIGURED = new Duration(50000, DAYS);
 
-    TestingMySqlServer testingMySqlServer;
+    private TestingMySqlServer testingMySqlServer;
     private SessionPropertiesDao dao;
     private static final String MYSQL_TEST_USER = "testuser";
     private static final String MYSQL_TEST_PASSWORD = "testpassword";
@@ -80,10 +80,11 @@ public class TestDbSessionPropertyManagerIntegration
         assertEquals(session.getSystemProperties(), emptyMap());
 
         Duration sessionValue = session.getSystemProperty(EXAMPLE_PROPERTY, Duration.class);
-        assertEquals(EXAMPLE_VALUE_DEFAULT, sessionValue, "sessionValue is not equal to EXAMPLE_VALUE_DEFAULT");
+        assertEquals(sessionValue, EXAMPLE_VALUE_DEFAULT);
+        assertNotEquals(EXAMPLE_VALUE_DEFAULT, EXAMPLE_VALUE_CONFIGURED);
 
         DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(session).build();
-        queryRunner.installPlugin(new TestSessionPropertyConfigurationManagerPlugin());
+        queryRunner.installPlugin(new TestingSessionPropertyConfigurationManagerPlugin());
         return queryRunner;
     }
 
@@ -92,15 +93,14 @@ public class TestDbSessionPropertyManagerIntegration
             throws Exception
     {
         testingMySqlServer = new TestingMySqlServer(MYSQL_TEST_USER, MYSQL_TEST_PASSWORD, MYSQL_TEST_DATABASE);
-        assertFalse(EXAMPLE_VALUE_CONFIGURED.equals(EXAMPLE_VALUE_DEFAULT));
         queryRunner = createQueryRunner();
     }
 
     @AfterClass(alwaysRun = true)
     public void destroy()
     {
-        testingMySqlServer.close();
-        queryRunner.close();
+        closeQuietly(testingMySqlServer);
+        closeQuietly(queryRunner);
     }
 
     @BeforeMethod
@@ -179,17 +179,17 @@ public class TestDbSessionPropertyManagerIntegration
                 .setUserAgent("agent");
     }
 
-    static class TestSessionPropertyConfigurationManagerPlugin
+    private static class TestingSessionPropertyConfigurationManagerPlugin
             implements Plugin
     {
         @Override
         public Iterable<SessionPropertyConfigurationManagerFactory> getSessionPropertyConfigurationManagerFactories()
         {
-            return ImmutableList.of(new TestDbSessionPropertyManagerFactory());
+            return ImmutableList.of(new TestingDbSessionPropertyManagerFactory());
         }
     }
 
-    static class TestDbSessionPropertyManagerModule
+    private static class TestingDbSessionPropertyManagerModule
             implements Module
     {
         @Override
@@ -203,7 +203,7 @@ public class TestDbSessionPropertyManagerIntegration
         }
     }
 
-    static class TestDbSessionPropertyManagerFactory
+    private static class TestingDbSessionPropertyManagerFactory
             implements SessionPropertyConfigurationManagerFactory
     {
         @Override
@@ -215,23 +215,17 @@ public class TestDbSessionPropertyManagerIntegration
         @Override
         public SessionPropertyConfigurationManager create(Map<String, String> config, SessionPropertyConfigurationManagerContext context)
         {
-            try {
-                Bootstrap app = new Bootstrap(
-                        new JsonModule(),
-                        new TestDbSessionPropertyManagerModule());
+            Bootstrap app = new Bootstrap(
+                    new JsonModule(),
+                    new TestingDbSessionPropertyManagerModule());
 
-                Injector injector = app
-                        .strictConfig()
-                        .doNotInitializeLogging()
-                        .setRequiredConfigurationProperties(config)
-                        .quiet()
-                        .initialize();
-                return injector.getInstance(DbSessionPropertyManager.class);
-            }
-            catch (Exception e) {
-                throwIfUnchecked(e);
-                throw new RuntimeException(e);
-            }
+            Injector injector = app
+                    .strictConfig()
+                    .doNotInitializeLogging()
+                    .setRequiredConfigurationProperties(config)
+                    .quiet()
+                    .initialize();
+            return injector.getInstance(DbSessionPropertyManager.class);
         }
     }
 }
