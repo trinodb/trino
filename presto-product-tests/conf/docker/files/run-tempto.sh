eeaaf275b4c179878d04038c14a9b70b01efa19f
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-11-14 16:11:36 +0100

    Shorten the paths to JAR files

--- presto-product-tests/conf/docker/files/run-tempto.sh
+++ presto-product-tests/conf/docker/files/run-tempto.sh
@@ -22,7 +22,7 @@ java \
   `#-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5007` \
   "-Djava.util.logging.config.file=${DOCKER_TEMPTO_CONF_DIR}/logging.properties" \
   -Duser.timezone=Asia/Kathmandu \
-  -cp "/docker/volumes/jdbc/driver.jar:/docker/volumes/presto-product-tests/presto-product-tests-executable.jar" \
+  -cp "/docker/presto-jdbc.jar:/docker/presto-product-tests-executable.jar" \
   io.prestosql.tests.TemptoProductTestRunner \
   --report-dir "/docker/volumes/test-reports" \
   --config "${TEMPTO_CONFIG_FILES}" \
