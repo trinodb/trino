4356045910a7ef42e54eadc939d2866cefe99489
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-11-18 11:11:59 +0100

    Run product tests on CDH

--- presto-product-tests/bin/product-tests-suite-1.sh
+++ presto-product-tests/bin/product-tests-suite-1.sh
@@ -6,7 +6,7 @@ exit_code=0
 
 presto-product-tests/bin/run_on_docker.sh \
     multinode \
-    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql,postgresql,kafka,simba_jdbc,hive_compression \
+    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql,postgresql,kafka,simba_jdbc,hive_compression,"${DISTRO_SKIP_GROUP}" \
     || exit_code=1
 
 exit "${exit_code}"
---
+++ presto-product-tests/conf/product-tests-config-cdh5.sh
@@ -0,0 +1,3 @@
+export TESTS_HIVE_VERSION_MAJOR=1
+export HADOOP_BASE_IMAGE="prestodev/cdh5.15-hive"
+export DISTRO_SKIP_GROUP=skip_on_cdh
