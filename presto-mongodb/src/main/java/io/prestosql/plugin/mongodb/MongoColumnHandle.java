e0d4adca64e6fb90308c598dbfe7c182fc6cf2c7
Yuya Ebihara <ebyhry@gmail.com>
2019-11-19 23:38:32 +0900

    Remove unsed code in MongoDB connector

--- presto-mongodb/src/main/java/io/prestosql/plugin/mongodb/MongoColumnHandle.java
+++ presto-mongodb/src/main/java/io/prestosql/plugin/mongodb/MongoColumnHandle.java
@@ -28,8 +28,6 @@ import static java.util.Objects.requireNonNull;
 public class MongoColumnHandle
         implements ColumnHandle
 {
-    public static final String SAMPLE_WEIGHT_COLUMN_NAME = "presto_sample_weight";
-
     private final String name;
     private final Type type;
     private final boolean hidden;
