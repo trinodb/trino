7e29926297b375fe295933292a9de8a6009038fb
Karol Sobczak <karol.sobczak@karolsobczak.com>
2019-11-20 17:22:18 +0100

    Simplify approx_distinct tests

--- presto-main/src/test/java/io/prestosql/operator/aggregation/TestApproximateCountDistinctLong.java
+++ presto-main/src/test/java/io/prestosql/operator/aggregation/TestApproximateCountDistinctLong.java
@@ -14,23 +14,14 @@
 package io.prestosql.operator.aggregation;
 
 import io.prestosql.spi.type.Type;
-import io.prestosql.sql.tree.QualifiedName;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 import static io.prestosql.spi.type.BigintType.BIGINT;
-import static io.prestosql.spi.type.DoubleType.DOUBLE;
-import static io.prestosql.sql.analyzer.TypeSignatureProvider.fromTypes;
 
 public class TestApproximateCountDistinctLong
         extends AbstractTestApproximateCountDistinct
 {
-    @Override
-    protected InternalAggregationFunction getAggregationFunction()
-    {
-        return metadata.getAggregateFunctionImplementation(metadata.resolveFunction(QualifiedName.of("approx_distinct"), fromTypes(BIGINT, DOUBLE)));
-    }
-
     @Override
     protected Type getValueType()
     {
