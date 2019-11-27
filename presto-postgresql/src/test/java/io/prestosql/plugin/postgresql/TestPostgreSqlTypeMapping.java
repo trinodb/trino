4ffe90f806d7f4e46a5f83a965e9a0c0433fdb8f
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2019-11-27 14:10:53 +0100

    Test predicate pushdown for column that has forced varchar mapping

--- presto-postgresql/src/test/java/io/prestosql/plugin/postgresql/TestPostgreSqlTypeMapping.java
+++ presto-postgresql/src/test/java/io/prestosql/plugin/postgresql/TestPostgreSqlTypeMapping.java
@@ -321,6 +321,14 @@ public class TestPostgreSqlTypeMapping
                     "SELECT * FROM tpch.test_forced_varchar_mapping",
                     "VALUES ('[\"2010-01-01 14:30:00\",\"2010-01-01 15:30:00\")','172.0.0.1',ARRAY['[\"2010-01-01 14:30:00\",\"2010-01-01 15:30:00\")'])");
 
+            // test predicate pushdown to column that has forced varchar mapping
+            assertQuery(
+                    "SELECT 1 FROM tpch.test_forced_varchar_mapping WHERE tsrange_col = '[\"2010-01-01 14:30:00\",\"2010-01-01 15:30:00\")'",
+                    "VALUES 1");
+            assertQuery(
+                    "SELECT 1 FROM tpch.test_forced_varchar_mapping WHERE tsrange_col = 'some value'",
+                    "SELECT 1 WHERE false");
+
             // test insert into column that has forced varchar mapping
             assertQueryFails(
                     "INSERT INTO tpch.test_forced_varchar_mapping (tsrange_col) VALUES ('some value')",
