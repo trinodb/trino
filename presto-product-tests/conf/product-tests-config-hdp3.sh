d924e950aeba18f2ab584e2d8875733cd860afd9
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-11-29 14:07:16 +0100

    Expose whether Hive is 1.1 in product tests

--- presto-product-tests/conf/product-tests-config-hdp3.sh
+++ presto-product-tests/conf/product-tests-config-hdp3.sh
@@ -1,4 +1,5 @@
-export TESTS_HIVE_VERSION_MAJOR=3
 export HADOOP_BASE_IMAGE="prestodev/hdp3.1-hive"
+export TESTS_HIVE_VERSION_MAJOR="3"
+export TESTS_HIVE_VERSION_MINOR="1"
 export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/presto-product-tests/conf/tempto/tempto-configuration-for-hdp3.yaml"
 export DISTRO_SKIP_GROUP=
