be1328cdb936ed635a1463f0178c214ff9b0c081
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-11-29 15:31:57 +0100

    Fix singlenode-hdp3 configuration
    Tests on HDP3 incorrectly assumed Hive major version=1.

--- presto-product-tests/bin/product-tests-suite-7-non-generic.sh
+++ presto-product-tests/bin/product-tests-suite-7-non-generic.sh
@@ -46,7 +46,7 @@ presto-product-tests/bin/run_on_docker.sh \
     -g two_hives \
     || exit_code=1
 
-presto-product-tests/bin/run_on_docker.sh \
+env TESTS_HIVE_VERSION_MAJOR="3" presto-product-tests/bin/run_on_docker.sh \
     singlenode-hdp3 \
     -g hdp3_only,storage_formats,hive_transactional \
     || exit_code=1
