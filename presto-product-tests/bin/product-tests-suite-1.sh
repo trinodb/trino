aa2f03c6d1b34765cd95108dd11ad9b018f0cbd5
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2019-12-31 10:51:14 +0100

    Use suite_exit_code for tests exit code
    Thanks to that it is much easier to grep the logs.

--- presto-product-tests/bin/product-tests-suite-1.sh
+++ presto-product-tests/bin/product-tests-suite-1.sh
@@ -2,12 +2,12 @@
 
 set -xeuo pipefail
 
-exit_code=0
+suite_exit_code=0
 
 presto-product-tests/bin/run_on_docker.sh \
     multinode \
     -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql,postgresql,kafka,simba_jdbc,hive_compression,"${DISTRO_SKIP_GROUP}" \
-    || exit_code=1
+    || suite_exit_code=1
 
-echo "$0: exiting with ${exit_code}"
-exit "${exit_code}"
+echo "$0: exiting with ${suite_exit_code}"
+exit "${suite_exit_code}"
