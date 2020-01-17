99d4f8f39c3925d2355160758c337f43db6cd64e
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-01-18 00:06:20 +0100

    Convert singlenode-hdfs-impersonation to Launcher

--- presto-product-tests-launcher/src/main/java/io/prestosql/tests/product/launcher/env/EnvironmentModule.java
+++ presto-product-tests-launcher/src/main/java/io/prestosql/tests/product/launcher/env/EnvironmentModule.java
@@ -21,6 +21,7 @@ import io.prestosql.tests.product.launcher.env.common.Kerberos;
 import io.prestosql.tests.product.launcher.env.common.Standard;
 import io.prestosql.tests.product.launcher.env.environment.Multinode;
 import io.prestosql.tests.product.launcher.env.environment.Singlenode;
+import io.prestosql.tests.product.launcher.env.environment.SinglenodeHdfsImpersonation;
 import io.prestosql.tests.product.launcher.env.environment.SinglenodeKerberosHdfsNoImpersonation;
 
 import static com.google.inject.multibindings.MapBinder.newMapBinder;
@@ -47,6 +48,7 @@ public final class EnvironmentModule
 
         MapBinder<String, EnvironmentProvider> environments = newMapBinder(binder, String.class, EnvironmentProvider.class);
         environments.addBinding("singlenode").to(Singlenode.class);
+        environments.addBinding("singlenode-hdfs-impersonation").to(SinglenodeHdfsImpersonation.class);
         environments.addBinding("singlenode-kerberos-hdfs-no-impersonation").to(SinglenodeKerberosHdfsNoImpersonation.class);
         environments.addBinding("multinode").to(Multinode.class);
         binder.install(additionalEnvironments);
