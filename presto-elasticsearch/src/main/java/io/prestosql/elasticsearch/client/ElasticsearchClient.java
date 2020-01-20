5250db4c96e6406dfc04fb49e78fb54feba94826
Martin Traverso <mtraverso@gmail.com>
2020-01-20 10:56:54 -0800

    Iterate over ES documents in index order
    This can improve performance of scroll queries.

--- presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/client/ElasticsearchClient.java
+++ presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/client/ElasticsearchClient.java
@@ -513,11 +513,13 @@ public class ElasticsearchClient
         return jsonNode.get(name);
     }
 
-    public SearchResponse beginSearch(String index, int shard, QueryBuilder query, Optional<List<String>> fields, List<String> documentFields)
+    public SearchResponse beginSearch(String index, int shard, QueryBuilder query, Optional<List<String>> fields, List<String> documentFields, Optional<String> sort)
     {
         SearchSourceBuilder sourceBuilder = SearchSourceBuilder.searchSource()
                 .query(query)
                 .size(scrollSize);
+
+        sort.ifPresent(sourceBuilder::sort);
 
         fields.ifPresent(values -> {
             if (values.isEmpty()) {
