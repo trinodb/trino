36ff467ca543cf4e566317b0193c7bc4a05662c5
David Phillips <david@acz.org>
2020-01-22 12:52:52 -0800

    Remove unused class SchemaMetadataBuilder

--- presto-main/src/main/java/io/prestosql/metadata/MetadataUtil.java
+++ presto-main/src/main/java/io/prestosql/metadata/MetadataUtil.java
@@ -191,27 +191,6 @@ public final class MetadataUtil
         return metadata.getTableHandle(session, name).isPresent();
     }
 
-    public static class SchemaMetadataBuilder
-    {
-        public static SchemaMetadataBuilder schemaMetadataBuilder()
-        {
-            return new SchemaMetadataBuilder();
-        }
-
-        private final ImmutableMap.Builder<SchemaTableName, ConnectorTableMetadata> tables = ImmutableMap.builder();
-
-        public SchemaMetadataBuilder table(ConnectorTableMetadata tableMetadata)
-        {
-            tables.put(tableMetadata.getTable(), tableMetadata);
-            return this;
-        }
-
-        public ImmutableMap<SchemaTableName, ConnectorTableMetadata> build()
-        {
-            return tables.build();
-        }
-    }
-
     public static class TableMetadataBuilder
     {
         public static TableMetadataBuilder tableMetadataBuilder(String schemaName, String tableName)
