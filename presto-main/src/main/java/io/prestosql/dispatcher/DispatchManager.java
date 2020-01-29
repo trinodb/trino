jtao15/presto

Tracer introduction

presto-main/src/main/java/io/prestosql/dispatcher/DispatchManager.java
@@ -35,6 +35,8 @@ import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.QueryId;
 import io.prestosql.spi.resourcegroups.SelectionContext;
 import io.prestosql.spi.resourcegroups.SelectionCriteria;
+import io.prestosql.spi.tracer.Tracer;
+import io.prestosql.tracer.TracerFactory;
 import io.prestosql.transaction.TransactionManager;
 import org.weakref.jmx.Flatten;
 import org.weakref.jmx.Managed;
@@ -51,6 +53,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
 import static io.prestosql.spi.StandardErrorCode.QUERY_TEXT_TOO_LARGE;
+import static io.prestosql.spi.tracer.TracerEventType.PLAN_QUERY_END;
+import static io.prestosql.spi.tracer.TracerEventType.PLAN_QUERY_START;
 import static io.prestosql.util.StatementUtils.getQueryType;
 import static io.prestosql.util.StatementUtils.isTransactionControlStatement;
 import static java.lang.String.format;
@@ -76,6 +80,8 @@ public class DispatchManager
 
     private final QueryManagerStats stats = new QueryManagerStats();
 
+    private final TracerFactory tracerFactory;
+
     @Inject
     public DispatchManager(
             QueryIdGenerator queryIdGenerator,
@@ -88,7 +94,8 @@ public class DispatchManager
             SessionSupplier sessionSupplier,
             SessionPropertyDefaults sessionPropertyDefaults,
             QueryManagerConfig queryManagerConfig,
-            DispatchExecutor dispatchExecutor)
+            DispatchExecutor dispatchExecutor,
+            TracerFactory tracerFactory)
     {
         this.queryIdGenerator = requireNonNull(queryIdGenerator, "queryIdGenerator is null");
         this.queryPreparer = requireNonNull(queryPreparer, "queryPreparer is null");
@@ -106,6 +113,7 @@ public class DispatchManager
         this.queryExecutor = requireNonNull(dispatchExecutor, "dispatchExecutor is null").getExecutor();
 
         this.queryTracker = new QueryTracker<>(queryManagerConfig, dispatchExecutor.getScheduledExecutor());
+        this.tracerFactory = requireNonNull(tracerFactory, "tracerFactory is null");
     }
 
     @PostConstruct
@@ -169,9 +177,12 @@ public class DispatchManager
 
             // decode session
             session = sessionSupplier.createSession(queryId, sessionContext);
+            Tracer queryTracer = tracerFactory.createBasicTracer(queryId.getId(), session);
 
             // prepare query
+            queryTracer.emitEvent(PLAN_QUERY_START, null);
             preparedQuery = queryPreparer.prepareQuery(session, query);
+            queryTracer.emitEvent(PLAN_QUERY_END, null);
 
             // select resource group
             Optional<String> queryType = getQueryType(preparedQuery.getStatement().getClass()).map(Enum::name);

presto-main/src/main/java/io/prestosql/tracer/TracerFactory.java
@@ -11,23 +11,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.spi.eventlistener;
+package io.prestosql.tracer;
 
-public interface EventListener
+import io.prestosql.Session;
+import io.prestosql.spi.tracer.Tracer;
+
+public interface TracerFactory
 {
-    default void queryCreated(QueryCreatedEvent queryCreatedEvent)
-    {
-    }
-
-    default void queryCompleted(QueryCompletedEvent queryCompletedEvent)
-    {
-    }
-
-    default void splitCompleted(SplitCompletedEvent splitCompletedEvent)
-    {
-    }
-
-    default void tracerEventOccurred(TracerEvent tracerEvent)
-    {
-    }
+    Tracer createBasicTracer(String queryId, Session session);
 }

presto-spi/src/main/java/io/prestosql/spi/tracer/NoOpTracer.java
@@ -1,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.spi.tracer;
+
+public class NoOpTracer
+        implements Tracer
+{
+    @Override
+    public Tracer newTracerWithStageId(String stageId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer newTracerWithTaskId(String taskId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer newTracerWithPipelineId(String pipelineId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer newTracerWithSplitId(String splitId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer newTracerWithOperatorId(String operatorId)
+    {
+        return this;
+    }
+
+    @Override
+    public void emitEvent(TracerEventTypeSupplier tracerEventTypeSupplier, PayloadBuilder payloadBuilder)
+    { }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return false;
+    }
+}

trinodb/trino

presto-main/src/main/java/io/prestosql/dispatcher/DispatchManager.java
@@ -35,6 +35,8 @@ import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.QueryId;
 import io.prestosql.spi.resourcegroups.SelectionContext;
 import io.prestosql.spi.resourcegroups.SelectionCriteria;
+import io.prestosql.spi.tracer.Tracer;
+import io.prestosql.tracer.TracerFactory;
 import io.prestosql.transaction.TransactionManager;
 import org.weakref.jmx.Flatten;
 import org.weakref.jmx.Managed;
@@ -51,6 +53,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
 import static io.prestosql.spi.StandardErrorCode.QUERY_TEXT_TOO_LARGE;
+import static io.prestosql.spi.tracer.TracerEventType.PLAN_QUERY_END;
+import static io.prestosql.spi.tracer.TracerEventType.PLAN_QUERY_START;
 import static io.prestosql.util.StatementUtils.getQueryType;
 import static io.prestosql.util.StatementUtils.isTransactionControlStatement;
 import static java.lang.String.format;
@@ -76,6 +80,8 @@ public class DispatchManager
 
     private final QueryManagerStats stats = new QueryManagerStats();
 
+    private final TracerFactory tracerFactory;
+
     @Inject
     public DispatchManager(
             QueryIdGenerator queryIdGenerator,
@@ -88,7 +94,8 @@ public class DispatchManager
             SessionSupplier sessionSupplier,
             SessionPropertyDefaults sessionPropertyDefaults,
             QueryManagerConfig queryManagerConfig,
-            DispatchExecutor dispatchExecutor)
+            DispatchExecutor dispatchExecutor,
+            TracerFactory tracerFactory)
     {
         this.queryIdGenerator = requireNonNull(queryIdGenerator, "queryIdGenerator is null");
         this.queryPreparer = requireNonNull(queryPreparer, "queryPreparer is null");
@@ -106,6 +113,7 @@ public class DispatchManager
         this.queryExecutor = requireNonNull(dispatchExecutor, "dispatchExecutor is null").getExecutor();
 
         this.queryTracker = new QueryTracker<>(queryManagerConfig, dispatchExecutor.getScheduledExecutor());
+        this.tracerFactory = requireNonNull(tracerFactory, "tracerFactory is null");
     }
 
     @PostConstruct
@@ -169,9 +177,12 @@ public class DispatchManager
 
             // decode session
             session = sessionSupplier.createSession(queryId, sessionContext);
+            Tracer queryTracer = tracerFactory.createTracer(queryId.getId(), session);
 
             // prepare query
+            queryTracer.emitEvent(PLAN_QUERY_START, null);
             preparedQuery = queryPreparer.prepareQuery(session, query);
+            queryTracer.emitEvent(PLAN_QUERY_END, null);
 
             // select resource group
             Optional<String> queryType = getQueryType(preparedQuery.getStatement().getClass()).map(Enum::name);

presto-main/src/main/java/io/prestosql/tracer/TracerFactory.java
@@ -11,23 +11,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.spi.eventlistener;
+package io.prestosql.tracer;
 
-public interface EventListener
+import io.prestosql.Session;
+import io.prestosql.spi.tracer.Tracer;
+
+public interface TracerFactory
 {
-    default void queryCreated(QueryCreatedEvent queryCreatedEvent)
-    {
-    }
-
-    default void queryCompleted(QueryCompletedEvent queryCompletedEvent)
-    {
-    }
-
-    default void splitCompleted(SplitCompletedEvent splitCompletedEvent)
-    {
-    }
-
-    default void tracerEventOccurred(TracerEvent tracerEvent)
-    {
-    }
+    Tracer createTracer(String queryId, Session session);
 }

presto-spi/src/main/java/io/prestosql/spi/tracer/NoOpTracer.java
@@ -1,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.spi.tracer;
+
+public class NoOpTracer
+        implements Tracer
+{
+    @Override
+    public Tracer withStageId(String stageId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer withTaskId(String taskId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer withPipelineId(String pipelineId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer withSplitId(String splitId)
+    {
+        return this;
+    }
+
+    @Override
+    public Tracer withOperatorId(String operatorId)
+    {
+        return this;
+    }
+
+    @Override
+    public void emitEvent(TracerEventTypeSupplier tracerEventTypeSupplier, PayloadBuilder payloadBuilder)
+    { }
+}