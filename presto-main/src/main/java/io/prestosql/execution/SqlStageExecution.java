jtao15/presto

Tracer propagation

presto-main/src/main/java/io/prestosql/execution/SqlStageExecution.java
@@ -28,6 +28,7 @@ import io.prestosql.failuredetector.FailureDetector;
 import io.prestosql.metadata.InternalNode;
 import io.prestosql.metadata.Split;
 import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.split.RemoteSplit;
 import io.prestosql.sql.planner.PlanFragment;
 import io.prestosql.sql.planner.plan.PlanFragmentId;
@@ -103,6 +104,8 @@ public final class SqlStageExecution
 
     private final ListenerManager<Set<Lifespan>> completedLifespansChangeListeners = new ListenerManager<>();
 
+    private final Tracer tracer;
+
     public static SqlStageExecution createSqlStageExecution(
             StageId stageId,
             PlanFragment fragment,
@@ -113,7 +116,8 @@ public final class SqlStageExecution
             NodeTaskMap nodeTaskMap,
             ExecutorService executor,
             FailureDetector failureDetector,
-            SplitSchedulerStats schedulerStats)
+            SplitSchedulerStats schedulerStats,
+            Tracer tracer)
     {
         requireNonNull(stageId, "stageId is null");
         requireNonNull(fragment, "fragment is null");
@@ -131,12 +135,13 @@ public final class SqlStageExecution
                 nodeTaskMap,
                 summarizeTaskInfo,
                 executor,
-                failureDetector);
+                failureDetector,
+                tracer);
         sqlStageExecution.initialize();
         return sqlStageExecution;
     }
 
-    private SqlStageExecution(StageStateMachine stateMachine, RemoteTaskFactory remoteTaskFactory, NodeTaskMap nodeTaskMap, boolean summarizeTaskInfo, Executor executor, FailureDetector failureDetector)
+    private SqlStageExecution(StageStateMachine stateMachine, RemoteTaskFactory remoteTaskFactory, NodeTaskMap nodeTaskMap, boolean summarizeTaskInfo, Executor executor, FailureDetector failureDetector, Tracer tracer)
     {
         this.stateMachine = stateMachine;
         this.remoteTaskFactory = requireNonNull(remoteTaskFactory, "remoteTaskFactory is null");
@@ -152,6 +157,7 @@ public final class SqlStageExecution
             }
         }
         this.exchangeSources = fragmentToExchangeSource.build();
+        this.tracer = requireNonNull(tracer, "tracer is null");
     }
 
     // this is a separate method to ensure that the `this` reference is not leaked during construction
@@ -393,6 +399,7 @@ public final class SqlStageExecution
         }
         else {
             task = tasks.iterator().next();
+            Tracer taskTracer = tracer.newTracerWithTaskId(String.valueOf(task.getTaskId().getId()));
             task.addSplits(splits);
         }
         if (noMoreSplitsNotification.size() > 1) {
@@ -410,6 +417,7 @@ public final class SqlStageExecution
     private synchronized RemoteTask scheduleTask(InternalNode node, TaskId taskId, Multimap<PlanNodeId, Split> sourceSplits, OptionalInt totalPartitions)
     {
         checkArgument(!allTasks.contains(taskId), "A task with id %s already exists", taskId);
+        Tracer taskTracer = tracer.newTracerWithTaskId(String.valueOf(taskId.getId()));
 
         ImmutableMultimap.Builder<PlanNodeId, Split> initialSplits = ImmutableMultimap.builder();
         initialSplits.putAll(sourceSplits);
@@ -433,7 +441,8 @@ public final class SqlStageExecution
                 totalPartitions,
                 outputBuffers,
                 nodeTaskMap.createPartitionedSplitCountTracker(node, taskId),
-                summarizeTaskInfo);
+                summarizeTaskInfo,
+                tracer);
 
         completeSources.forEach(task::noMoreSplits);
 

presto-main/src/main/java/io/prestosql/execution/SqlTaskExecution.java
@@ -37,6 +37,7 @@ import io.prestosql.operator.PipelineContext;
 import io.prestosql.operator.PipelineExecutionStrategy;
 import io.prestosql.operator.StageExecutionDescriptor;
 import io.prestosql.operator.TaskContext;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.sql.planner.LocalExecutionPlanner.LocalExecutionPlan;
 import io.prestosql.sql.planner.plan.PlanNodeId;
 
@@ -141,6 +142,8 @@ public class SqlTaskExecution
 
     private final Status status;
 
+    private final Tracer tracer;
+
     static SqlTaskExecution createSqlTaskExecution(
             TaskStateMachine taskStateMachine,
             TaskContext taskContext,
@@ -149,7 +152,8 @@ public class SqlTaskExecution
             LocalExecutionPlan localExecutionPlan,
             TaskExecutor taskExecutor,
             Executor notificationExecutor,
-            SplitMonitor queryMonitor)
+            SplitMonitor queryMonitor,
+            Tracer tracer)
     {
         SqlTaskExecution task = new SqlTaskExecution(
                 taskStateMachine,
@@ -158,7 +162,8 @@ public class SqlTaskExecution
                 localExecutionPlan,
                 taskExecutor,
                 queryMonitor,
-                notificationExecutor);
+                notificationExecutor,
+                tracer);
         try (SetThreadName ignored = new SetThreadName("Task-%s", task.getTaskId())) {
             // The scheduleDriversForTaskLifeCycle method calls enqueueDriverSplitRunner, which registers a callback with access to this object.
             // The call back is accessed from another thread, so this code can not be placed in the constructor.
@@ -175,7 +180,8 @@ public class SqlTaskExecution
             LocalExecutionPlan localExecutionPlan,
             TaskExecutor taskExecutor,
             SplitMonitor splitMonitor,
-            Executor notificationExecutor)
+            Executor notificationExecutor,
+            Tracer tracer)
     {
         this.taskStateMachine = requireNonNull(taskStateMachine, "taskStateMachine is null");
         this.taskId = taskStateMachine.getTaskId();
@@ -186,6 +192,7 @@ public class SqlTaskExecution
         this.notificationExecutor = requireNonNull(notificationExecutor, "notificationExecutor is null");
 
         this.splitMonitor = requireNonNull(splitMonitor, "splitMonitor is null");
+        this.tracer = requireNonNull(tracer, "tracer is null");
 
         try (SetThreadName ignored = new SetThreadName("Task-%s", taskId)) {
             // index driver factories
@@ -196,15 +203,15 @@ public class SqlTaskExecution
             for (DriverFactory driverFactory : localExecutionPlan.getDriverFactories()) {
                 Optional<PlanNodeId> sourceId = driverFactory.getSourceId();
                 if (sourceId.isPresent() && partitionedSources.contains(sourceId.get())) {
-                    driverRunnerFactoriesWithSplitLifeCycle.put(sourceId.get(), new DriverSplitRunnerFactory(driverFactory, true));
+                    driverRunnerFactoriesWithSplitLifeCycle.put(sourceId.get(), new DriverSplitRunnerFactory(driverFactory, true, tracer));
                 }
                 else {
                     switch (driverFactory.getPipelineExecutionStrategy()) {
                         case GROUPED_EXECUTION:
-                            driverRunnerFactoriesWithDriverGroupLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false));
+                            driverRunnerFactoriesWithDriverGroupLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false, tracer));
                             break;
                         case UNGROUPED_EXECUTION:
-                            driverRunnerFactoriesWithTaskLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false));
+                            driverRunnerFactoriesWithTaskLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false, tracer));
                             break;
                         default:
                             throw new UnsupportedOperationException();
@@ -916,12 +923,15 @@ public class SqlTaskExecution
     {
         private final DriverFactory driverFactory;
         private final PipelineContext pipelineContext;
+        private final Tracer pipelineTracer;
         private boolean closed;
 
-        private DriverSplitRunnerFactory(DriverFactory driverFactory, boolean partitioned)
+        private DriverSplitRunnerFactory(DriverFactory driverFactory, boolean partitioned, Tracer taskTracer)
         {
             this.driverFactory = driverFactory;
-            this.pipelineContext = taskContext.addPipelineContext(driverFactory.getPipelineId(), driverFactory.isInputDriver(), driverFactory.isOutputDriver(), partitioned);
+            int pipelineId = driverFactory.getPipelineId();
+            this.pipelineTracer = taskTracer.newTracerWithPipelineId(String.valueOf(pipelineId));
+            this.pipelineContext = taskContext.addPipelineContext(pipelineId, driverFactory.isInputDriver(), driverFactory.isOutputDriver(), partitioned);
         }
 
         // TODO: split this method into two: createPartitionedDriverRunner and createUnpartitionedDriverRunner.
@@ -933,12 +943,12 @@ public class SqlTaskExecution
             // create driver context immediately so the driver existence is recorded in the stats
             // the number of drivers is used to balance work across nodes
             DriverContext driverContext = pipelineContext.addDriverContext(lifespan);
-            return new DriverSplitRunner(this, driverContext, partitionedSplit, lifespan);
+            return new DriverSplitRunner(this, driverContext, partitionedSplit, lifespan, pipelineTracer);
         }
 
         public Driver createDriver(DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit)
         {
-            Driver driver = driverFactory.createDriver(driverContext);
+            Driver driver = driverFactory.createDriver(driverContext, pipelineTracer);
 
             // record driver so other threads add unpartitioned sources can see the driver
             // NOTE: this MUST be done before reading unpartitionedSources, so we see a consistent view of the unpartitioned sources
@@ -1024,12 +1034,15 @@ public class SqlTaskExecution
         @GuardedBy("this")
         private Driver driver;
 
-        private DriverSplitRunner(DriverSplitRunnerFactory driverSplitRunnerFactory, DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit, Lifespan lifespan)
+        private final Tracer pipelineTracer;
+
+        private DriverSplitRunner(DriverSplitRunnerFactory driverSplitRunnerFactory, DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit, Lifespan lifespan, Tracer tracer)
         {
             this.driverSplitRunnerFactory = requireNonNull(driverSplitRunnerFactory, "driverFactory is null");
             this.driverContext = requireNonNull(driverContext, "driverContext is null");
             this.partitionedSplit = partitionedSplit;
             this.lifespan = requireNonNull(lifespan, "lifespan is null");
+            this.pipelineTracer = requireNonNull(tracer, "tracer is null");
         }
 
         public synchronized DriverContext getDriverContext()
@@ -1043,6 +1056,12 @@ public class SqlTaskExecution
         public Lifespan getLifespan()
         {
             return lifespan;
+        }
+
+        @Override
+        public Tracer getTracer()
+        {
+            return pipelineTracer;
         }
 
         @Override

presto-main/src/main/java/io/prestosql/operator/TableScanOperator.java
@@ -26,6 +26,7 @@ import io.prestosql.spi.connector.ColumnHandle;
 import io.prestosql.spi.connector.ConnectorPageSource;
 import io.prestosql.spi.connector.UpdatablePageSource;
 import io.prestosql.spi.predicate.TupleDomain;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.split.EmptySplit;
 import io.prestosql.split.EmptySplitPageSource;
 import io.prestosql.split.PageSourceProvider;
@@ -95,16 +96,18 @@ public class TableScanOperator
         }
 
         @Override
-        public SourceOperator createOperator(DriverContext driverContext)
+        public SourceOperator createOperator(DriverContext driverContext, Tracer pipelineTracer)
         {
             checkState(!closed, "Factory is already closed");
             OperatorContext operatorContext = driverContext.addOperatorContext(operatorId, sourceId, getOperatorType());
+            Tracer tracer = pipelineTracer.newTracerWithOperatorId(String.valueOf(operatorId));
             return new TableScanOperator(
                     operatorContext,
                     sourceId,
                     pageSourceProvider,
                     table,
-                    columns);
+                    columns,
+                    tracer);
         }
 
         @Override
@@ -137,6 +140,7 @@ public class TableScanOperator
     private final List<ColumnHandle> columns;
     private final LocalMemoryContext systemMemoryContext;
     private final SettableFuture<?> blocked = SettableFuture.create();
+    private final Tracer tracer;
 
     private Split split;
     private ConnectorPageSource source;
@@ -151,7 +155,8 @@ public class TableScanOperator
             PlanNodeId planNodeId,
             PageSourceProvider pageSourceProvider,
             TableHandle table,
-            Iterable<ColumnHandle> columns)
+            Iterable<ColumnHandle> columns,
+            Tracer tracer)
     {
         this.operatorContext = requireNonNull(operatorContext, "operatorContext is null");
         this.planNodeId = requireNonNull(planNodeId, "planNodeId is null");
@@ -159,6 +164,7 @@ public class TableScanOperator
         this.table = requireNonNull(table, "table is null");
         this.columns = ImmutableList.copyOf(requireNonNull(columns, "columns is null"));
         this.systemMemoryContext = operatorContext.newLocalSystemMemoryContext(TableScanOperator.class.getSimpleName());
+        this.tracer = requireNonNull(tracer, "tracer is null");
     }
 
     @Override

trinodb/trino

presto-main/src/main/java/io/prestosql/execution/SqlStageExecution.java
@@ -28,6 +28,7 @@ import io.prestosql.failuredetector.FailureDetector;
 import io.prestosql.metadata.InternalNode;
 import io.prestosql.metadata.Split;
 import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.split.RemoteSplit;
 import io.prestosql.sql.planner.PlanFragment;
 import io.prestosql.sql.planner.plan.PlanFragmentId;
@@ -103,6 +104,8 @@ public final class SqlStageExecution
 
     private final ListenerManager<Set<Lifespan>> completedLifespansChangeListeners = new ListenerManager<>();
 
+    private final Tracer tracer;
+
     public static SqlStageExecution createSqlStageExecution(
             StageId stageId,
             PlanFragment fragment,
@@ -113,7 +116,8 @@ public final class SqlStageExecution
             NodeTaskMap nodeTaskMap,
             ExecutorService executor,
             FailureDetector failureDetector,
-            SplitSchedulerStats schedulerStats)
+            SplitSchedulerStats schedulerStats,
+            Tracer tracer)
     {
         requireNonNull(stageId, "stageId is null");
         requireNonNull(fragment, "fragment is null");
@@ -131,12 +135,13 @@ public final class SqlStageExecution
                 nodeTaskMap,
                 summarizeTaskInfo,
                 executor,
-                failureDetector);
+                failureDetector,
+                tracer);
         sqlStageExecution.initialize();
         return sqlStageExecution;
     }
 
-    private SqlStageExecution(StageStateMachine stateMachine, RemoteTaskFactory remoteTaskFactory, NodeTaskMap nodeTaskMap, boolean summarizeTaskInfo, Executor executor, FailureDetector failureDetector)
+    private SqlStageExecution(StageStateMachine stateMachine, RemoteTaskFactory remoteTaskFactory, NodeTaskMap nodeTaskMap, boolean summarizeTaskInfo, Executor executor, FailureDetector failureDetector, Tracer tracer)
     {
         this.stateMachine = stateMachine;
         this.remoteTaskFactory = requireNonNull(remoteTaskFactory, "remoteTaskFactory is null");
@@ -152,6 +157,7 @@ public final class SqlStageExecution
             }
         }
         this.exchangeSources = fragmentToExchangeSource.build();
+        this.tracer = requireNonNull(tracer, "tracer is null");
     }
 
     // this is a separate method to ensure that the `this` reference is not leaked during construction
@@ -393,6 +399,7 @@ public final class SqlStageExecution
         }
         else {
             task = tasks.iterator().next();
+            Tracer taskTracer = tracer.withTaskId(String.valueOf(task.getTaskId().getId()));
             task.addSplits(splits);
         }
         if (noMoreSplitsNotification.size() > 1) {
@@ -410,6 +417,7 @@ public final class SqlStageExecution
     private synchronized RemoteTask scheduleTask(InternalNode node, TaskId taskId, Multimap<PlanNodeId, Split> sourceSplits, OptionalInt totalPartitions)
     {
         checkArgument(!allTasks.contains(taskId), "A task with id %s already exists", taskId);
+        Tracer taskTracer = tracer.withTaskId(String.valueOf(taskId.getId()));
 
         ImmutableMultimap.Builder<PlanNodeId, Split> initialSplits = ImmutableMultimap.builder();
         initialSplits.putAll(sourceSplits);
@@ -433,7 +441,8 @@ public final class SqlStageExecution
                 totalPartitions,
                 outputBuffers,
                 nodeTaskMap.createPartitionedSplitCountTracker(node, taskId),
-                summarizeTaskInfo);
+                summarizeTaskInfo,
+                tracer);
 
         completeSources.forEach(task::noMoreSplits);
 

presto-main/src/main/java/io/prestosql/execution/SqlTaskExecution.java
@@ -37,6 +37,7 @@ import io.prestosql.operator.PipelineContext;
 import io.prestosql.operator.PipelineExecutionStrategy;
 import io.prestosql.operator.StageExecutionDescriptor;
 import io.prestosql.operator.TaskContext;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.sql.planner.LocalExecutionPlanner.LocalExecutionPlan;
 import io.prestosql.sql.planner.plan.PlanNodeId;
 
@@ -141,6 +142,8 @@ public class SqlTaskExecution
 
     private final Status status;
 
+    private final Tracer tracer;
+
     static SqlTaskExecution createSqlTaskExecution(
             TaskStateMachine taskStateMachine,
             TaskContext taskContext,
@@ -149,7 +152,8 @@ public class SqlTaskExecution
             LocalExecutionPlan localExecutionPlan,
             TaskExecutor taskExecutor,
             Executor notificationExecutor,
-            SplitMonitor queryMonitor)
+            SplitMonitor queryMonitor,
+            Tracer tracer)
     {
         SqlTaskExecution task = new SqlTaskExecution(
                 taskStateMachine,
@@ -158,7 +162,8 @@ public class SqlTaskExecution
                 localExecutionPlan,
                 taskExecutor,
                 queryMonitor,
-                notificationExecutor);
+                notificationExecutor,
+                tracer);
         try (SetThreadName ignored = new SetThreadName("Task-%s", task.getTaskId())) {
             // The scheduleDriversForTaskLifeCycle method calls enqueueDriverSplitRunner, which registers a callback with access to this object.
             // The call back is accessed from another thread, so this code cannot be placed in the constructor.
@@ -175,7 +180,8 @@ public class SqlTaskExecution
             LocalExecutionPlan localExecutionPlan,
             TaskExecutor taskExecutor,
             SplitMonitor splitMonitor,
-            Executor notificationExecutor)
+            Executor notificationExecutor,
+            Tracer tracer)
     {
         this.taskStateMachine = requireNonNull(taskStateMachine, "taskStateMachine is null");
         this.taskId = taskStateMachine.getTaskId();
@@ -186,6 +192,7 @@ public class SqlTaskExecution
         this.notificationExecutor = requireNonNull(notificationExecutor, "notificationExecutor is null");
 
         this.splitMonitor = requireNonNull(splitMonitor, "splitMonitor is null");
+        this.tracer = requireNonNull(tracer, "tracer is null");
 
         try (SetThreadName ignored = new SetThreadName("Task-%s", taskId)) {
             // index driver factories
@@ -196,15 +203,15 @@ public class SqlTaskExecution
             for (DriverFactory driverFactory : localExecutionPlan.getDriverFactories()) {
                 Optional<PlanNodeId> sourceId = driverFactory.getSourceId();
                 if (sourceId.isPresent() && partitionedSources.contains(sourceId.get())) {
-                    driverRunnerFactoriesWithSplitLifeCycle.put(sourceId.get(), new DriverSplitRunnerFactory(driverFactory, true));
+                    driverRunnerFactoriesWithSplitLifeCycle.put(sourceId.get(), new DriverSplitRunnerFactory(driverFactory, true, tracer));
                 }
                 else {
                     switch (driverFactory.getPipelineExecutionStrategy()) {
                         case GROUPED_EXECUTION:
-                            driverRunnerFactoriesWithDriverGroupLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false));
+                            driverRunnerFactoriesWithDriverGroupLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false, tracer));
                             break;
                         case UNGROUPED_EXECUTION:
-                            driverRunnerFactoriesWithTaskLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false));
+                            driverRunnerFactoriesWithTaskLifeCycle.add(new DriverSplitRunnerFactory(driverFactory, false, tracer));
                             break;
                         default:
                             throw new UnsupportedOperationException();
@@ -916,12 +923,15 @@ public class SqlTaskExecution
     {
         private final DriverFactory driverFactory;
         private final PipelineContext pipelineContext;
+        private final Tracer pipelineTracer;
         private boolean closed;
 
-        private DriverSplitRunnerFactory(DriverFactory driverFactory, boolean partitioned)
+        private DriverSplitRunnerFactory(DriverFactory driverFactory, boolean partitioned, Tracer taskTracer)
         {
             this.driverFactory = driverFactory;
-            this.pipelineContext = taskContext.addPipelineContext(driverFactory.getPipelineId(), driverFactory.isInputDriver(), driverFactory.isOutputDriver(), partitioned);
+            int pipelineId = driverFactory.getPipelineId();
+            this.pipelineTracer = taskTracer.withPipelineId(String.valueOf(pipelineId));
+            this.pipelineContext = taskContext.addPipelineContext(pipelineId, driverFactory.isInputDriver(), driverFactory.isOutputDriver(), partitioned);
         }
 
         // TODO: split this method into two: createPartitionedDriverRunner and createUnpartitionedDriverRunner.
@@ -933,12 +943,12 @@ public class SqlTaskExecution
             // create driver context immediately so the driver existence is recorded in the stats
             // the number of drivers is used to balance work across nodes
             DriverContext driverContext = pipelineContext.addDriverContext(lifespan);
-            return new DriverSplitRunner(this, driverContext, partitionedSplit, lifespan);
+            return new DriverSplitRunner(this, driverContext, partitionedSplit, lifespan, pipelineTracer);
         }
 
         public Driver createDriver(DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit)
         {
-            Driver driver = driverFactory.createDriver(driverContext);
+            Driver driver = driverFactory.createDriver(driverContext, pipelineTracer);
 
             // record driver so other threads add unpartitioned sources can see the driver
             // NOTE: this MUST be done before reading unpartitionedSources, so we see a consistent view of the unpartitioned sources
@@ -1024,12 +1034,15 @@ public class SqlTaskExecution
         @GuardedBy("this")
         private Driver driver;
 
-        private DriverSplitRunner(DriverSplitRunnerFactory driverSplitRunnerFactory, DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit, Lifespan lifespan)
+        private final Tracer pipelineTracer;
+
+        private DriverSplitRunner(DriverSplitRunnerFactory driverSplitRunnerFactory, DriverContext driverContext, @Nullable ScheduledSplit partitionedSplit, Lifespan lifespan, Tracer tracer)
         {
             this.driverSplitRunnerFactory = requireNonNull(driverSplitRunnerFactory, "driverFactory is null");
             this.driverContext = requireNonNull(driverContext, "driverContext is null");
             this.partitionedSplit = partitionedSplit;
             this.lifespan = requireNonNull(lifespan, "lifespan is null");
+            this.pipelineTracer = requireNonNull(tracer, "tracer is null");
         }
 
         public synchronized DriverContext getDriverContext()
@@ -1043,6 +1056,12 @@ public class SqlTaskExecution
         public Lifespan getLifespan()
         {
             return lifespan;
+        }
+
+        @Override
+        public Tracer getTracer()
+        {
+            return pipelineTracer;
         }
 
         @Override

presto-main/src/main/java/io/prestosql/operator/TableScanOperator.java
@@ -26,6 +26,7 @@ import io.prestosql.spi.connector.ColumnHandle;
 import io.prestosql.spi.connector.ConnectorPageSource;
 import io.prestosql.spi.connector.UpdatablePageSource;
 import io.prestosql.spi.predicate.TupleDomain;
+import io.prestosql.spi.tracer.Tracer;
 import io.prestosql.split.EmptySplit;
 import io.prestosql.split.EmptySplitPageSource;
 import io.prestosql.split.PageSourceProvider;
@@ -95,16 +96,18 @@ public class TableScanOperator
         }
 
         @Override
-        public SourceOperator createOperator(DriverContext driverContext)
+        public SourceOperator createOperator(DriverContext driverContext, Tracer pipelineTracer)
         {
             checkState(!closed, "Factory is already closed");
             OperatorContext operatorContext = driverContext.addOperatorContext(operatorId, sourceId, getOperatorType());
+            Tracer tracer = pipelineTracer.withOperatorId(String.valueOf(operatorId));
             return new TableScanOperator(
                     operatorContext,
                     sourceId,
                     pageSourceProvider,
                     table,
-                    columns);
+                    columns,
+                    tracer);
         }
 
         @Override
@@ -137,6 +140,7 @@ public class TableScanOperator
     private final List<ColumnHandle> columns;
     private final LocalMemoryContext systemMemoryContext;
     private final SettableFuture<?> blocked = SettableFuture.create();
+    private final Tracer tracer;
 
     private Split split;
     private ConnectorPageSource source;
@@ -151,7 +155,8 @@ public class TableScanOperator
             PlanNodeId planNodeId,
             PageSourceProvider pageSourceProvider,
             TableHandle table,
-            Iterable<ColumnHandle> columns)
+            Iterable<ColumnHandle> columns,
+            Tracer tracer)
     {
         this.operatorContext = requireNonNull(operatorContext, "operatorContext is null");
         this.planNodeId = requireNonNull(planNodeId, "planNodeId is null");
@@ -159,6 +164,7 @@ public class TableScanOperator
         this.table = requireNonNull(table, "table is null");
         this.columns = ImmutableList.copyOf(requireNonNull(columns, "columns is null"));
         this.systemMemoryContext = operatorContext.newLocalSystemMemoryContext(TableScanOperator.class.getSimpleName());
+        this.tracer = requireNonNull(tracer, "tracer is null");
     }
 
     @Override