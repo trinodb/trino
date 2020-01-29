693c14880db2eb9847f1f221dcb4c884250c31e8
Harmandeep Singh <harmandeeps@qubole.com>
2020-01-29 19:34:33 +0530

    restrict S3 configuration provider to S3 schemes

--- presto-hive/src/main/java/io/prestosql/plugin/hive/s3/S3ConfigurationProvider.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/s3/S3ConfigurationProvider.java
@@ -13,12 +13,14 @@
  */
 package io.prestosql.plugin.hive.s3;
 
+import com.google.common.collect.ImmutableSet;
 import io.prestosql.plugin.hive.DynamicConfigurationProvider;
 import io.prestosql.plugin.hive.HiveSessionProperties;
 import org.apache.hadoop.conf.Configuration;
 
 import java.net.URI;
 import java.util.Optional;
+import java.util.Set;
 
 import static io.prestosql.plugin.hive.DynamicConfigurationProvider.setCacheKey;
 import static io.prestosql.plugin.hive.HdfsEnvironment.HdfsContext;
@@ -27,9 +29,15 @@ import static io.prestosql.plugin.hive.s3.PrestoS3FileSystem.S3_IAM_ROLE;
 public class S3ConfigurationProvider
         implements DynamicConfigurationProvider
 {
+    private static final Set<String> SUPPORTED_S3_SCHEMES = ImmutableSet.of("s3", "s3a", "s3n");
+
     @Override
     public void updateConfiguration(Configuration configuration, HdfsContext context, URI uri)
     {
+        if (!SUPPORTED_S3_SCHEMES.contains(uri.getScheme())) {
+            return;
+        }
+
         Optional<String> iamRole = context.getSession().map(session -> HiveSessionProperties.getS3IamRole(session)).filter(role -> role != null && !role.isEmpty());
         if (iamRole.isPresent()) {
             configuration.set(S3_IAM_ROLE, iamRole.get());
