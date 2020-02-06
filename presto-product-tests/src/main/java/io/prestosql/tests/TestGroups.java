cd1d4813bfca39e0715164a0cbb17da5b6a56435
Vlad Rozov <vrozov@apache.org>
2020-02-05 19:00:10 -0800

    Add iceberg to product tests suite

--- presto-product-tests/src/main/java/io/prestosql/tests/TestGroups.java
+++ presto-product-tests/src/main/java/io/prestosql/tests/TestGroups.java
@@ -66,6 +66,7 @@ public final class TestGroups
     public static final String BIG_QUERY = "big_query";
     public static final String KAFKA = "kafka";
     public static final String TWO_HIVES = "two_hives";
+    public static final String ICEBERG = "iceberg";
 
     private TestGroups() {}
 }
