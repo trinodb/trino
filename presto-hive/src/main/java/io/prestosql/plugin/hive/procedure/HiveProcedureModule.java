ce485376f586b052ef782313e7c7881be74094a3
David Phillips <david@acz.org>
2020-02-11 11:19:25 -0800

    Move Hive procedures to separate package

--- presto-hive/src/main/java/io/prestosql/plugin/hive/procedure/HiveProcedureModule.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/procedure/HiveProcedureModule.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.plugin.hive;
+package io.prestosql.plugin.hive.procedure;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
