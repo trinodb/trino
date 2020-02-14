7545687bde9f0120b89458a074b2b9398d6fd221
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-02-14 18:38:37 +0100

    Use apply-site-xml-override from image

--- presto-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/singlenode-hdp3/hadoop-entrypoint.sh
+++ presto-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/singlenode-hdp3/hadoop-entrypoint.sh
@@ -11,7 +11,7 @@ DIR="${BASH_SOURCE%/*}"
 
 echo "[$(date)] $0: configuring hadoop services"
 
-${DIR}/apply-site-xml-override.sh /etc/hive/conf/hive-site.xml "${DIR}/hive-site-overrides.xml" || fail "Could not apply hive-site-overrides.xml"
+apply-site-xml-override /etc/hive/conf/hive-site.xml "${DIR}/hive-site-overrides.xml" || fail "Could not apply hive-site-overrides.xml"
 
 echo "[$(date)] $0: starting hadoop services"
 set -x
