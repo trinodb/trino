38f03477ea8ab38569e6c7738c8bd6f2f2b4d9dc
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-02-15 23:21:27 +0100

    Retain decimal attributes

--- presto-parquet/src/main/java/io/prestosql/parquet/reader/MetadataReader.java
+++ presto-parquet/src/main/java/io/prestosql/parquet/reader/MetadataReader.java
@@ -20,7 +20,6 @@ import org.apache.parquet.CorruptStatistics;
 import org.apache.parquet.column.statistics.BinaryStatistics;
 import org.apache.parquet.format.ColumnChunk;
 import org.apache.parquet.format.ColumnMetaData;
-import org.apache.parquet.format.ConvertedType;
 import org.apache.parquet.format.Encoding;
 import org.apache.parquet.format.FileMetaData;
 import org.apache.parquet.format.KeyValue;
@@ -36,7 +35,6 @@ import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.hadoop.metadata.ParquetMetadata;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.OriginalType;
 import org.apache.parquet.schema.PrimitiveType;
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName;
 import org.apache.parquet.schema.Type.Repetition;
@@ -63,6 +61,7 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.apache.parquet.format.Util.readFileMetaData;
+import static org.apache.parquet.format.converter.ParquetMetadataConverterUtil.getLogicalTypeAnnotation;
 
 public final class MetadataReader
 {
@@ -197,7 +196,7 @@ public final class MetadataReader
             }
 
             if (element.isSetConverted_type()) {
-                typeBuilder.as(getOriginalType(element.converted_type));
+                typeBuilder.as(getLogicalTypeAnnotation(new ParquetMetadataConverter(), element.converted_type, element));
             }
             if (element.isSetField_id()) {
                 typeBuilder.id(element.field_id);
@@ -329,58 +328,6 @@ public final class MetadataReader
                 return PrimitiveTypeName.FIXED_LEN_BYTE_ARRAY;
             default:
                 throw new IllegalArgumentException("Unknown type " + type);
-        }
-    }
-
-    private static OriginalType getOriginalType(ConvertedType type)
-    {
-        switch (type) {
-            case UTF8:
-                return OriginalType.UTF8;
-            case MAP:
-                return OriginalType.MAP;
-            case MAP_KEY_VALUE:
-                return OriginalType.MAP_KEY_VALUE;
-            case LIST:
-                return OriginalType.LIST;
-            case ENUM:
-                return OriginalType.ENUM;
-            case DECIMAL:
-                return OriginalType.DECIMAL;
-            case DATE:
-                return OriginalType.DATE;
-            case TIME_MILLIS:
-                return OriginalType.TIME_MILLIS;
-            case TIMESTAMP_MILLIS:
-                return OriginalType.TIMESTAMP_MILLIS;
-            case INTERVAL:
-                return OriginalType.INTERVAL;
-            case INT_8:
-                return OriginalType.INT_8;
-            case INT_16:
-                return OriginalType.INT_16;
-            case INT_32:
-                return OriginalType.INT_32;
-            case INT_64:
-                return OriginalType.INT_64;
-            case UINT_8:
-                return OriginalType.UINT_8;
-            case UINT_16:
-                return OriginalType.UINT_16;
-            case UINT_32:
-                return OriginalType.UINT_32;
-            case UINT_64:
-                return OriginalType.UINT_64;
-            case JSON:
-                return OriginalType.JSON;
-            case BSON:
-                return OriginalType.BSON;
-            case TIMESTAMP_MICROS:
-                return OriginalType.TIMESTAMP_MICROS;
-            case TIME_MICROS:
-                return OriginalType.TIME_MICROS;
-            default:
-                throw new IllegalArgumentException("Unknown converted type " + type);
         }
     }
 
