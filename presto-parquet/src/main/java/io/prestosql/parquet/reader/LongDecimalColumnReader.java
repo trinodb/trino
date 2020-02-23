34678faf9863615d620515b4db962e09d42df204
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2020-02-23 20:20:17 +0100

    Allow rescaling parquet decimals

--- presto-parquet/src/main/java/io/prestosql/parquet/reader/LongDecimalColumnReader.java
+++ presto-parquet/src/main/java/io/prestosql/parquet/reader/LongDecimalColumnReader.java
@@ -13,6 +13,7 @@
  */
 package io.prestosql.parquet.reader;
 
+import io.airlift.slice.Slice;
 import io.prestosql.parquet.RichColumnDescriptor;
 import io.prestosql.spi.block.BlockBuilder;
 import io.prestosql.spi.type.DecimalType;
@@ -23,7 +24,8 @@ import org.apache.parquet.io.api.Binary;
 
 import java.math.BigInteger;
 
-import static io.prestosql.spi.type.Decimals.isLongDecimal;
+import static io.prestosql.spi.type.DecimalConversions.longToLongCast;
+import static io.prestosql.spi.type.DecimalConversions.longToShortCast;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
@@ -41,20 +43,32 @@ public class LongDecimalColumnReader
     @Override
     protected void readValue(BlockBuilder blockBuilder, Type prestoType)
     {
-        if (!isLongDecimal(prestoType)) {
+        if (!(prestoType instanceof DecimalType)) {
             throw new ParquetDecodingException(format("Unsupported Presto column type (%s) for Parquet column (%s)", prestoType, columnDescriptor));
         }
+
         DecimalType prestoDecimalType = (DecimalType) prestoType;
-        if (prestoDecimalType.getScale() != parquetDecimalType.getScale()) {
-            throw new ParquetDecodingException(format(
-                    "Presto decimal column type has different scale (%s) than Parquet decimal column (%s)",
-                    prestoDecimalType.getScale(),
-                    parquetDecimalType.getScale()));
-        }
 
         if (definitionLevel == columnDescriptor.getMaxDefinitionLevel()) {
-            Binary value = valuesReader.readBytes();
-            prestoType.writeSlice(blockBuilder, Decimals.encodeUnscaledValue(new BigInteger(value.getBytes())));
+            Binary binary = valuesReader.readBytes();
+            Slice value = Decimals.encodeUnscaledValue(new BigInteger(binary.getBytes()));
+
+            if (prestoDecimalType.isShort()) {
+                prestoType.writeLong(blockBuilder, longToShortCast(
+                        value,
+                        parquetDecimalType.getPrecision(),
+                        parquetDecimalType.getScale(),
+                        prestoDecimalType.getPrecision(),
+                        prestoDecimalType.getScale()));
+            }
+            else {
+                prestoType.writeSlice(blockBuilder, longToLongCast(
+                        value,
+                        parquetDecimalType.getPrecision(),
+                        parquetDecimalType.getScale(),
+                        prestoDecimalType.getPrecision(),
+                        prestoDecimalType.getScale()));
+            }
         }
         else if (isValueNull()) {
             blockBuilder.appendNull();
