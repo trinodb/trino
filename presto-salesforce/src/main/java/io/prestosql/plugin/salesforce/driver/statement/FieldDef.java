b7d593824078cc298c11a25b92a1a7d50f8fd2ac
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-02-28 17:15:58 -0500

    Make FieldDef hash and equals methods take advantage of Objects functions

--- presto-salesforce/src/main/java/io/prestosql/plugin/salesforce/driver/statement/FieldDef.java
+++ presto-salesforce/src/main/java/io/prestosql/plugin/salesforce/driver/statement/FieldDef.java
@@ -13,6 +13,10 @@
  */
 package io.prestosql.plugin.salesforce.driver.statement;
 
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 public class FieldDef
 {
     private final String name;
@@ -40,39 +44,17 @@ public class FieldDef
     @Override
     public int hashCode()
     {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
+        return Objects.hash(name, type);
     }
 
     @Override
     public boolean equals(Object obj)
     {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
+        if (obj.getClass() != FieldDef.class) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
+
         FieldDef other = (FieldDef) obj;
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        }
-        else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (type == null) {
-            return other.type == null;
-        }
-        else {
-            return type.equals(other.type);
-        }
+        return Objects.equals(name, other.name) && Objects.equals(type, other.type);
     }
 }
