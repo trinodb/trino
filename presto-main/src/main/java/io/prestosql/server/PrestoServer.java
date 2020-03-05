b93a8db77f91a128c1fa37d42d8219b091afe953
Arturo Viveros <gugalnikov@gmail.com>
2020-03-05 15:04:06 +0100

    Add SignalHandler to Presto

--- presto-main/src/main/java/io/prestosql/server/PrestoServer.java
+++ presto-main/src/main/java/io/prestosql/server/PrestoServer.java
@@ -147,6 +147,8 @@ public class PrestoServer
 
             injector.getInstance(ServerInfoResource.class).startupComplete();
 
+            SignalHandler.listenTo(injector.getInstance(ServerConfig.class), injector.getInstance(GracefulShutdownHandler.class));
+
             log.info("======== SERVER STARTED ========");
         }
         catch (Throwable e) {
