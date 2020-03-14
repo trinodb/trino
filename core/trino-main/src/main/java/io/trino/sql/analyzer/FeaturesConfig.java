8cfa0c45e78e2fd72675734eb096e560f2d0a044
Karol Sobczak <karol.sobczak@karolsobczak.com>
2020-03-15 00:06:41 +0100

    Enable late materialization by default

--- core/trino-main/src/main/java/io/trino/sql/analyzer/FeaturesConfig.java
+++ core/trino-main/src/main/java/io/trino/sql/analyzer/FeaturesConfig.java
@@ -124,7 +124,7 @@ public class FeaturesConfig
     private boolean useMarkDistinct = true;
     private boolean preferPartialAggregation = true;
     private boolean optimizeTopNRanking = true;
-    private boolean lateMaterializationEnabled;
+    private boolean lateMaterializationEnabled = true;
     private boolean skipRedundantSort = true;
     private boolean predicatePushdownUseTableProperties = true;
     private boolean ignoreDownstreamPreferences;
@@ -971,8 +971,8 @@ public class FeaturesConfig
         return lateMaterializationEnabled;
     }
 
-    @Config("experimental.late-materialization.enabled")
-    @LegacyConfig("experimental.work-processor-pipelines")
+    @Config("late-materialization.enabled")
+    @LegacyConfig({"experimental.work-processor-pipelines", "experimental.late-materialization.enabled"})
     public FeaturesConfig setLateMaterializationEnabled(boolean lateMaterializationEnabled)
     {
         this.lateMaterializationEnabled = lateMaterializationEnabled;
