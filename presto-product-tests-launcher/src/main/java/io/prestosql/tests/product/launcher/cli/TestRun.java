e41fadf168317d0d2565f81327c8d43790570e96
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-03-22 20:45:02 +0100

    Remove unused TEMPTO_EXTRA_CONFIG_FILE

--- presto-product-tests-launcher/src/main/java/io/prestosql/tests/product/launcher/cli/TestRun.java
+++ presto-product-tests-launcher/src/main/java/io/prestosql/tests/product/launcher/cli/TestRun.java
@@ -183,7 +183,6 @@ public final class TestRun
                                                 .add(CONTAINER_TEMPTO_PROFILE_CONFIG)
                                                 .add(System.getenv().getOrDefault("TEMPTO_ENVIRONMENT_CONFIG_FILE", "/dev/null"))
                                                 .add(container.getEnvMap().getOrDefault("TEMPTO_CONFIG_FILES", "/dev/null"))
-                                                .add(System.getenv().getOrDefault("TEMPTO_EXTRA_CONFIG_FILE", "/dev/null"))
                                                 .build()))
                                 .addAll(testArguments)
                                 .build().toArray(new String[0]));
