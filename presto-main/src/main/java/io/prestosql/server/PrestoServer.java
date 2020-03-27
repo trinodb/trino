15bad291e57f23ad30c430dada3c8151f67a5ca7
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-03-27 17:31:42 +0100

    Log Java version during startup

--- presto-main/src/main/java/io/prestosql/server/PrestoServer.java
+++ presto-main/src/main/java/io/prestosql/server/PrestoServer.java
@@ -14,6 +14,7 @@
 package io.prestosql.server;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.StandardSystemProperty;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -78,6 +79,7 @@ public class PrestoServer
         verifySystemTimeIsReasonable();
 
         Logger log = Logger.get(PrestoServer.class);
+        log.info("Java version: %s", StandardSystemProperty.JAVA_VERSION.value());
 
         ImmutableList.Builder<Module> modules = ImmutableList.builder();
         modules.add(
