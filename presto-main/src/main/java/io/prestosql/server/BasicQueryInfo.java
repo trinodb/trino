72abec95585813a69e5167cfb9f3803a05e19ae4
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-04-02 11:20:08 +0200

    Add updateType to query event
    Thanks to that event listener will know if query was SELECT, DELETE
    or any other DDL statement.

--- presto-main/src/main/java/io/prestosql/server/BasicQueryInfo.java
+++ presto-main/src/main/java/io/prestosql/server/BasicQueryInfo.java
@@ -48,6 +48,7 @@ public class BasicQueryInfo
     private final boolean scheduled;
     private final URI self;
     private final String query;
+    private final Optional<String> updateType;
     private final Optional<String> preparedQuery;
     private final BasicQueryStats queryStats;
     private final ErrorType errorType;
@@ -63,6 +64,7 @@ public class BasicQueryInfo
             @JsonProperty("scheduled") boolean scheduled,
             @JsonProperty("self") URI self,
             @JsonProperty("query") String query,
+            @JsonProperty("updateType") Optional<String> updateType,
             @JsonProperty("preparedQuery") Optional<String> preparedQuery,
             @JsonProperty("queryStats") BasicQueryStats queryStats,
             @JsonProperty("errorType") ErrorType errorType,
@@ -78,6 +80,7 @@ public class BasicQueryInfo
         this.scheduled = scheduled;
         this.self = requireNonNull(self, "self is null");
         this.query = requireNonNull(query, "query is null");
+        this.updateType = requireNonNull(updateType, "updateType is null");
         this.preparedQuery = requireNonNull(preparedQuery, "preparedQuery is null");
         this.queryStats = requireNonNull(queryStats, "queryStats is null");
     }
@@ -92,6 +95,7 @@ public class BasicQueryInfo
                 queryInfo.isScheduled(),
                 queryInfo.getSelf(),
                 queryInfo.getQuery(),
+                Optional.ofNullable(queryInfo.getUpdateType()),
                 queryInfo.getPreparedQuery(),
                 new BasicQueryStats(queryInfo.getQueryStats()),
                 queryInfo.getErrorType(),
@@ -144,6 +148,12 @@ public class BasicQueryInfo
     public String getQuery()
     {
         return query;
+    }
+
+    @JsonProperty
+    public Optional<String> getUpdateType()
+    {
+        return updateType;
     }
 
     @JsonProperty
