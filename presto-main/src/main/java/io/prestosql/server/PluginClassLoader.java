e50178168a52ecb70f4bda62b4293c198dc99af3
Dain Sundstrom <dain@iq80.com>
2020-04-04 16:39:09 -0700

    Use platform classloader in Java 8 and 11 for plugins

--- presto-main/src/main/java/io/prestosql/server/PluginClassLoader.java
+++ presto-main/src/main/java/io/prestosql/server/PluginClassLoader.java
@@ -16,8 +16,6 @@ package io.prestosql.server;
 import com.google.common.collect.ImmutableList;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Enumeration;
@@ -29,8 +27,6 @@ import static java.util.Objects.requireNonNull;
 class PluginClassLoader
         extends URLClassLoader
 {
-    private static final ClassLoader PLATFORM_CLASS_LOADER = findPlatformClassLoader();
-
     private final ClassLoader spiClassLoader;
     private final List<String> spiPackages;
     private final List<String> spiResources;
@@ -55,7 +51,7 @@ class PluginClassLoader
             Iterable<String> spiResources)
     {
         // plugins should not have access to the system (application) class loader
-        super(urls.toArray(new URL[0]), PLATFORM_CLASS_LOADER);
+        super(urls.toArray(new URL[0]), getSystemClassLoader().getParent());
         this.spiClassLoader = requireNonNull(spiClassLoader, "spiClassLoader is null");
         this.spiPackages = ImmutableList.copyOf(spiPackages);
         this.spiResources = ImmutableList.copyOf(spiResources);
@@ -142,22 +138,5 @@ class PluginClassLoader
     private static String classNameToResource(String className)
     {
         return className.replace('.', '/');
-    }
-
-    @SuppressWarnings("JavaReflectionMemberAccess")
-    private static ClassLoader findPlatformClassLoader()
-    {
-        try {
-            // use platform class loader on Java 9
-            Method method = ClassLoader.class.getMethod("getPlatformClassLoader");
-            return (ClassLoader) method.invoke(null);
-        }
-        catch (NoSuchMethodException ignored) {
-            // use null class loader on Java 8
-            return null;
-        }
-        catch (IllegalAccessException | InvocationTargetException e) {
-            throw new AssertionError(e);
-        }
     }
 }
