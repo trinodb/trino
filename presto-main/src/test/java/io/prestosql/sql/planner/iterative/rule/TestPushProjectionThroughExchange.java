1cbb33f41daee9da423f0ba82b9abfba4601192d
kasiafi <30203062+kasiafi@users.noreply.github.com>
2020-04-05 23:30:56 +0200

    Fix duplicate symbols in Exchange outputs
    In PushProjectionThroughExchange rule, when the pushed-down
    Projection has an identity assignment on a symbol which is used
    for partitioning, ordering or as a hash symbol by the Exchange,
    the symbol is added to Exchange's outputs twice:
    - once as a symbol required by Exchange
    - once as required by the parent Projection
    This is fixed by skipping the other occurrence

--- presto-main/src/test/java/io/prestosql/sql/planner/iterative/rule/TestPushProjectionThroughExchange.java
+++ presto-main/src/test/java/io/prestosql/sql/planner/iterative/rule/TestPushProjectionThroughExchange.java
@@ -139,6 +139,116 @@ public class TestPushProjectionThroughExchange
     }
 
     @Test
+    public void testSkipIdentityProjectionIfOutputPresent()
+    {
+        // In the following example, the Projection over Exchange has got an identity assignment (a -> a).
+        // The Projection is pushed down to Exchange's source, and the identity assignment is translated into
+        // a0 -> a. The assignment is added to the pushed-down Projection because the input symbol 'a' is
+        // required by the Exchange as a partitioning symbol.
+        // When all the assignments from the parent Projection are added to the pushed-down Projection,
+        // this assignment is omitted. Otherwise the doubled assignment would cause an error.
+        tester().assertThat(new PushProjectionThroughExchange())
+                .on(p -> {
+                    Symbol a = p.symbol("a");
+                    Symbol aTimes5 = p.symbol("a_times_5");
+                    return p.project(
+                            Assignments.of(
+                                    aTimes5, new ArithmeticBinaryExpression(ArithmeticBinaryExpression.Operator.MULTIPLY, new SymbolReference("a"), new LongLiteral("5")),
+                                    a, a.toSymbolReference()),
+                            p.exchange(e -> e
+                                    .addSource(p.values(a))
+                                    .addInputsSet(a)
+                                    .fixedHashDistributionParitioningScheme(ImmutableList.of(a), ImmutableList.of(a))));
+                })
+                .matches(
+                        exchange(
+                                strictProject(
+                                        ImmutableMap.of("a_0", expression("a"), "a_times_5", expression("a * 5")),
+                                        values(ImmutableList.of("a")))));
+
+        // In the following example, the Projection over Exchange has got an identity assignment (b -> b).
+        // The Projection is pushed down to Exchange's source, and the identity assignment is translated into
+        // a0 -> a. The assignment is added to the pushed-down Projection because the input symbol 'a' is
+        // required by the Exchange as a partitioning symbol.
+        // When all the assignments from the parent Projection are added to the pushed-down Projection,
+        // this assignment is omitted. Otherwise the doubled assignment would cause an error.
+        tester().assertThat(new PushProjectionThroughExchange())
+                .on(p -> {
+                    Symbol a = p.symbol("a");
+                    Symbol bTimes5 = p.symbol("b_times_5");
+                    Symbol b = p.symbol("b");
+                    return p.project(
+                            Assignments.of(
+                                    bTimes5, new ArithmeticBinaryExpression(ArithmeticBinaryExpression.Operator.MULTIPLY, new SymbolReference("b"), new LongLiteral("5")),
+                                    b, b.toSymbolReference()),
+                            p.exchange(e -> e
+                                    .addSource(p.values(a))
+                                    .addInputsSet(a)
+                                    .fixedHashDistributionParitioningScheme(ImmutableList.of(b), ImmutableList.of(b))));
+                })
+                .matches(
+                        exchange(
+                                strictProject(
+                                        ImmutableMap.of("a_0", expression("a"), "a_times_5", expression("a * 5")),
+                                        values(ImmutableList.of("a")))));
+    }
+
+    @Test
+    public void testDoNotSkipIdentityProjectionIfOutputAbsent()
+    {
+        // In the following example, the Projection over Exchange has got an identity assignment (a -> a).
+        // The Projection is pushed down to Exchange's source, and the identity assignment is translated into
+        // a0 -> a. Input symbol 'a' is not used in the Exchange for partitioning, ordering or as a hash symbol.
+        // It is just passed to output.
+        // When all the assignments from the parent Projection are added to the pushed-down Projection,
+        // the translated assignment is added too, so that the input symbol 'a' can be passed to the Exchange's output.
+        tester().assertThat(new PushProjectionThroughExchange())
+                .on(p -> {
+                    Symbol a = p.symbol("a");
+                    Symbol aTimes5 = p.symbol("a_times_5");
+                    return p.project(
+                            Assignments.of(
+                                    aTimes5, new ArithmeticBinaryExpression(ArithmeticBinaryExpression.Operator.MULTIPLY, new SymbolReference("a"), new LongLiteral("5")),
+                                    a, a.toSymbolReference()),
+                            p.exchange(e -> e
+                                    .addSource(p.values(a))
+                                    .addInputsSet(a)
+                                    .singleDistributionPartitioningScheme(a)));
+                })
+                .matches(
+                        exchange(
+                                strictProject(
+                                        ImmutableMap.of("a_0", expression("a"), "a_times_5", expression("a * 5")),
+                                        values(ImmutableList.of("a")))));
+
+        // In the following example, the Projection over Exchange has got an identity assignment (b -> b).
+        // The Projection is pushed down to Exchange's source, and the identity assignment is translated into
+        // a0 -> a. Input symbol 'a' is not used in the Exchange for partitioning, ordering or as a hash symbol.
+        // It is just passed to output.
+        // When all the assignments from the parent Projection are added to the pushed-down Projection,
+        // the translated assignment is added too, so that the input symbol 'a' can be passed to the Exchange's output.
+        tester().assertThat(new PushProjectionThroughExchange())
+                .on(p -> {
+                    Symbol a = p.symbol("a");
+                    Symbol bTimes5 = p.symbol("b_times_5");
+                    Symbol b = p.symbol("b");
+                    return p.project(
+                            Assignments.of(
+                                    bTimes5, new ArithmeticBinaryExpression(ArithmeticBinaryExpression.Operator.MULTIPLY, new SymbolReference("b"), new LongLiteral("5")),
+                                    b, b.toSymbolReference()),
+                            p.exchange(e -> e
+                                    .addSource(p.values(a))
+                                    .addInputsSet(a)
+                                    .singleDistributionPartitioningScheme(b)));
+                })
+                .matches(
+                        exchange(
+                                strictProject(
+                                        ImmutableMap.of("a_0", expression("a"), "a_times_5", expression("a * 5")),
+                                        values(ImmutableList.of("a")))));
+    }
+
+    @Test
     public void testPartitioningColumnAndHashWithoutIdentityMappingInProjection()
     {
         tester().assertThat(new PushProjectionThroughExchange())
