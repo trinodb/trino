fe7e78ec2b9dc4e6eae61a50837c28ee33e891cf
kasiafi <30203062+kasiafi@users.noreply.github.com>
2020-04-07 21:26:59 +0200

    Add column-pruning rule for ExchangeNode

--- presto-main/src/main/java/io/prestosql/sql/planner/PlanOptimizers.java
+++ presto-main/src/main/java/io/prestosql/sql/planner/PlanOptimizers.java
@@ -69,6 +69,7 @@ import io.prestosql.sql.planner.iterative.rule.PruneDeleteSourceColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneDistinctLimitSourceColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneEnforceSingleRowColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneExceptSourceColumns;
+import io.prestosql.sql.planner.iterative.rule.PruneExchangeSourceColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneFilterColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneIndexSourceColumns;
 import io.prestosql.sql.planner.iterative.rule.PruneIntersectSourceColumns;
@@ -250,6 +251,7 @@ public class PlanOptimizers
                 new PruneDistinctLimitSourceColumns(),
                 new PruneEnforceSingleRowColumns(),
                 new PruneExceptSourceColumns(),
+                new PruneExchangeSourceColumns(),
                 new PruneFilterColumns(),
                 new PruneIndexSourceColumns(),
                 new PruneIntersectSourceColumns(),
