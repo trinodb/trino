aa8919acbc97fa82dc40e6321b764da45f1192d7
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2020-04-15 12:40:07 +0200

    Add Oracle smoke tests with subpartitioned tables
    (cherry-picked from commit: 373b454084e48debbb278b5c1afea69585194e6c)

---
+++ plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/TestOracleParallelSubpartitionsIntegrationSmokeTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.oracle;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.prestosql.testing.QueryRunner;
+import io.prestosql.tpch.TpchTable;
+
+import static com.starburstdata.presto.plugin.oracle.OracleQueryRunner.createSession;
+import static com.starburstdata.presto.plugin.oracle.OracleTestUsers.KERBERIZED_USER;
+import static com.starburstdata.presto.plugin.oracle.OracleTestUsers.createStandardUsers;
+import static com.starburstdata.presto.plugin.oracle.OracleTestUsers.createUser;
+import static com.starburstdata.presto.plugin.oracle.TestingOracleServer.executeInOracle;
+import static java.lang.String.format;
+
+public class TestOracleParallelSubpartitionsIntegrationSmokeTest
+        extends BaseOracleIntegrationSmokeTest
+{
+    private static final String SUBPARTITIONED_USER = "subpartitioned";
+
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        return OracleQueryRunner.builder()
+                .withConnectorProperties(ImmutableMap.<String, String>builder()
+                        .putAll(TestingOracleServer.connectionProperties())
+                        .put("allow-drop-table", "true")
+                        .put("oracle.parallelism-type", "PARTITIONS")
+                        .put("oracle.concurrent.max-splits-per-scan", "17")
+                        .build())
+                .withSessionModifier(session -> createSession(SUBPARTITIONED_USER, SUBPARTITIONED_USER))
+                .withTables(ImmutableList.of(TpchTable.ORDERS, TpchTable.NATION))
+                .withCreateUsers(TestOracleParallelSubpartitionsIntegrationSmokeTest::createUsers)
+                .withProvisionTables(TestOracleParallelSubpartitionsIntegrationSmokeTest::partitionTables)
+                .build();
+    }
+
+    private static void createUsers()
+    {
+        createStandardUsers();
+        createUser(SUBPARTITIONED_USER, KERBERIZED_USER);
+        executeInOracle(format("GRANT SELECT ON user_context to %s", SUBPARTITIONED_USER));
+    }
+
+    private static void partitionTables()
+    {
+        executeInOracle(format("ALTER TABLE %s.orders MODIFY PARTITION BY RANGE (orderdate) INTERVAL (NUMTODSINTERVAL(1,'DAY'))\n" +
+                "SUBPARTITION BY LIST (orderstatus)\n" +
+                "SUBPARTITION TEMPLATE (\n" +
+                "    SUBPARTITION opened VALUES ('O'),\n" +
+                "    SUBPARTITION fulfilled VALUES ('F'),\n" +
+                "    SUBPARTITION postponed VALUES ('P')\n" +
+                ") (PARTITION before_1900 VALUES LESS THAN (TO_DATE('01-JAN-1900','dd-MON-yyyy')))", SUBPARTITIONED_USER));
+
+        executeInOracle(format("ALTER TABLE %s.nation MODIFY PARTITION BY HASH(name) PARTITIONS 3", SUBPARTITIONED_USER));
+    }
+}
