cd8a4744216d2009c126058ccae8ca3731d538bc
Pratham Desai <prathamd94@gmail.com>
2020-04-17 16:18:20 -0700

    Add a test for predicate pushdown in hive connector

--- presto-hive/src/test/java/io/prestosql/plugin/hive/optimizer/TestConnectorPushdownRulesWithHive.java
+++ presto-hive/src/test/java/io/prestosql/plugin/hive/optimizer/TestConnectorPushdownRulesWithHive.java
@@ -36,12 +36,15 @@ import io.prestosql.plugin.hive.metastore.Database;
 import io.prestosql.plugin.hive.metastore.HiveMetastore;
 import io.prestosql.plugin.hive.metastore.file.FileHiveMetastore;
 import io.prestosql.plugin.hive.testing.TestingHiveConnectorFactory;
+import io.prestosql.spi.predicate.Domain;
 import io.prestosql.spi.predicate.TupleDomain;
 import io.prestosql.spi.security.PrincipalType;
 import io.prestosql.spi.type.RowType;
 import io.prestosql.spi.type.Type;
+import io.prestosql.sql.planner.iterative.rule.PushPredicateIntoTableScan;
 import io.prestosql.sql.planner.iterative.rule.PushProjectionIntoTableScan;
 import io.prestosql.sql.planner.iterative.rule.test.BaseRuleTest;
+import io.prestosql.sql.planner.iterative.rule.test.PlanBuilder;
 import io.prestosql.sql.planner.plan.Assignments;
 import io.prestosql.sql.tree.DereferenceExpression;
 import io.prestosql.sql.tree.Identifier;
@@ -57,26 +60,32 @@ import java.util.Optional;
 import static com.google.common.base.Predicates.equalTo;
 import static com.google.common.io.MoreFiles.deleteRecursively;
 import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
+import static io.prestosql.plugin.hive.HiveColumnHandle.ColumnType.REGULAR;
+import static io.prestosql.plugin.hive.HiveColumnHandle.createBaseColumn;
+import static io.prestosql.plugin.hive.HiveType.HIVE_INT;
 import static io.prestosql.plugin.hive.HiveType.toHiveType;
 import static io.prestosql.spi.type.BigintType.BIGINT;
+import static io.prestosql.spi.type.IntegerType.INTEGER;
 import static io.prestosql.spi.type.RowType.field;
 import static io.prestosql.sql.planner.assertions.PlanMatchPattern.expression;
+import static io.prestosql.sql.planner.assertions.PlanMatchPattern.filter;
 import static io.prestosql.sql.planner.assertions.PlanMatchPattern.project;
 import static io.prestosql.sql.planner.assertions.PlanMatchPattern.tableScan;
 import static io.prestosql.testing.TestingConnectorSession.SESSION;
 import static io.prestosql.testing.TestingSession.testSessionBuilder;
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 
-public class TestPushProjectionRuleWithHive
+public class TestConnectorPushdownRulesWithHive
         extends BaseRuleTest
 {
     private static final String HIVE_CATALOG_NAME = "hive";
     private static final String SCHEMA_NAME = "test_schema";
-    private static final String TABLE_NAME = "test_table";
 
     private static final Type ROW_TYPE = RowType.from(asList(field("a", BIGINT), field("b", BIGINT)));
 
     private File baseDir;
+    private HiveMetastore metastore;
 
     private static final Session HIVE_SESSION = testSessionBuilder()
             .setCatalog(HIVE_CATALOG_NAME)
@@ -91,7 +100,7 @@ public class TestPushProjectionRuleWithHive
         HdfsConfiguration configuration = new HiveHdfsConfiguration(new HdfsConfigurationInitializer(config), ImmutableSet.of());
         HdfsEnvironment environment = new HdfsEnvironment(configuration, config, new NoHdfsAuthentication());
 
-        HiveMetastore metastore = new FileHiveMetastore(environment, baseDir.toURI().toString(), "test");
+        metastore = new FileHiveMetastore(environment, baseDir.toURI().toString(), "test");
         Database database = Database.builder()
                 .setDatabaseName(SCHEMA_NAME)
                 .setOwnerName("public")
@@ -109,10 +118,13 @@ public class TestPushProjectionRuleWithHive
     @Test
     public void testProjectionPushdown()
     {
+        String tableName = "projection_test";
         PushProjectionIntoTableScan pushProjectionIntoTableScan = new PushProjectionIntoTableScan(tester().getMetadata(), tester().getTypeAnalyzer());
 
-        tester().getQueryRunner().execute("CREATE TABLE " + TABLE_NAME + "(struct_of_int) AS " +
-                "SELECT cast(row(5, 6) as row(a bigint, b bigint)) as struct_of_int where false");
+        tester().getQueryRunner().execute(format(
+                "CREATE TABLE  %s (struct_of_int) AS " +
+                        "SELECT cast(row(5, 6) as row(a bigint, b bigint)) as struct_of_int where false",
+                tableName));
 
         Type baseType = ROW_TYPE;
 
@@ -126,10 +138,10 @@ public class TestPushProjectionRuleWithHive
                     ImmutableList.of("a"),
                     toHiveType(new HiveTypeTranslator(), BIGINT),
                     BIGINT)),
-                HiveColumnHandle.ColumnType.REGULAR,
+                REGULAR,
                 Optional.empty());
 
-        HiveTableHandle hiveTable = new HiveTableHandle(SCHEMA_NAME, TABLE_NAME, ImmutableMap.of(), ImmutableList.of(), Optional.empty());
+        HiveTableHandle hiveTable = new HiveTableHandle(SCHEMA_NAME, tableName, ImmutableMap.of(), ImmutableList.of(), Optional.empty());
         TableHandle table = new TableHandle(new CatalogName(HIVE_CATALOG_NAME), hiveTable, new HiveTransactionHandle(), Optional.empty());
 
         HiveColumnHandle fullColumn = partialColumn.getBaseColumn();
@@ -163,6 +175,40 @@ public class TestPushProjectionRuleWithHive
                                 equalTo(table.getConnectorHandle()),
                                 TupleDomain.all(),
                                 ImmutableMap.of("struct_of_int#a", equalTo(partialColumn)))));
+
+        metastore.dropTable(new HiveIdentity(SESSION), SCHEMA_NAME, tableName, true);
+    }
+
+    @Test
+    public void testPredicatePushdown()
+    {
+        String tableName = "predicate_test";
+        tester().getQueryRunner().execute(format("CREATE TABLE %s (a, b) AS SELECT 5, 6", tableName));
+
+        PushPredicateIntoTableScan pushPredicateIntoTableScan = new PushPredicateIntoTableScan(tester().getMetadata(), tester().getTypeAnalyzer());
+
+        HiveTableHandle hiveTable = new HiveTableHandle(SCHEMA_NAME, tableName, ImmutableMap.of(), ImmutableList.of(), Optional.empty());
+        TableHandle table = new TableHandle(new CatalogName(HIVE_CATALOG_NAME), hiveTable, new HiveTransactionHandle(), Optional.empty());
+
+        HiveColumnHandle column = createBaseColumn("a", 0, HIVE_INT, INTEGER, REGULAR, Optional.empty());
+
+        tester().assertThat(pushPredicateIntoTableScan)
+                .on(p ->
+                        p.filter(
+                                PlanBuilder.expression("a = 5"),
+                                p.tableScan(
+                                        table,
+                                        ImmutableList.of(p.symbol("a", INTEGER)),
+                                        ImmutableMap.of(p.symbol("a", INTEGER), column))))
+                .matches(filter(
+                        "a = 5",
+                        tableScan(
+                                tableHandle -> ((HiveTableHandle) tableHandle).getCompactEffectivePredicate().getDomains().get()
+                                        .equals(ImmutableMap.of(column, Domain.singleValue(INTEGER, 5L))),
+                                TupleDomain.all(),
+                                ImmutableMap.of("a", equalTo(column)))));
+
+        metastore.dropTable(new HiveIdentity(SESSION), SCHEMA_NAME, tableName, true);
     }
 
     @AfterClass(alwaysRun = true)
