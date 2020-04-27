e1dc91cecb511333138d6d15e5c92a22b948dc2f
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-04-27 23:19:52 +0200

    Simplify Field#toString
    The `Field`'s `toString` is complex, as if it was playing some
    conversion role, but it does not. Simplify it and make more robust
    (avoid throwing exceptions), at the cost of not producing human-readable
    string representation of collections.

--- presto-accumulo/src/test/java/io/prestosql/plugin/accumulo/model/TestField.java
+++ presto-accumulo/src/test/java/io/prestosql/plugin/accumulo/model/TestField.java
@@ -61,7 +61,6 @@ public class TestField
         assertEquals(f1.getArray(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "ARRAY ['a','b','c']");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -75,13 +74,11 @@ public class TestField
         assertEquals(f1.getBoolean().booleanValue(), true);
         assertEquals(f1.getObject(), true);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "true");
 
         f1 = new Field(false, type);
         assertEquals(f1.getBoolean().booleanValue(), false);
         assertEquals(f1.getObject(), false);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "false");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -96,7 +93,6 @@ public class TestField
         assertEquals(f1.getDate(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "DATE '1999-01-01'");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -111,7 +107,6 @@ public class TestField
         assertEquals(f1.getDouble(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "123.45678");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -126,7 +121,6 @@ public class TestField
         assertEquals(f1.getFloat(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "123.45678");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -141,7 +135,6 @@ public class TestField
         assertEquals(f1.getInt(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "12345678");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -156,7 +149,6 @@ public class TestField
         assertEquals(f1.getLong(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "12345678");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -173,7 +165,6 @@ public class TestField
         assertEquals(f1.getMap(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "MAP(ARRAY ['a','b','c'], ARRAY [1,2,3])");
     }
 
     @Test
@@ -185,7 +176,6 @@ public class TestField
         assertEquals(f1.getShort(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "12345");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -200,7 +190,6 @@ public class TestField
         assertEquals(f1.getTime(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "TIME '12:30:00'");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -215,7 +204,6 @@ public class TestField
         assertEquals(f1.getTimestamp(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "TIMESTAMP '1999-01-01 12:30:00.0'");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -230,7 +218,6 @@ public class TestField
         assertEquals(f1.getByte(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "123");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -245,7 +232,6 @@ public class TestField
         assertEquals(f1.getVarbinary(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "CAST('O''Leary' AS VARBINARY)");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
@@ -260,7 +246,6 @@ public class TestField
         assertEquals(f1.getVarchar(), expected);
         assertEquals(f1.getObject(), expected);
         assertEquals(f1.getType(), type);
-        assertEquals(f1.toString(), "'O''Leary'");
 
         Field f2 = new Field(f1);
         assertEquals(f2, f1);
