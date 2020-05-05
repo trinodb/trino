0ec2d4b52c73d2d5b2220a0d8e3ac64bb29727ae
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-05-05 16:22:21 +0200

    Allow Rubix caching for all Azure filesystems

--- presto-hive/src/main/java/io/prestosql/plugin/hive/rubix/RubixConfigurationInitializer.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/rubix/RubixConfigurationInitializer.java
@@ -13,9 +13,13 @@
  */
 package io.prestosql.plugin.hive.rubix;
 
+import com.qubole.rubix.hadoop2.CachingNativeAzureFileSystem;
+import com.qubole.rubix.prestosql.CachingPrestoAzureBlobFileSystem;
 import com.qubole.rubix.prestosql.CachingPrestoGoogleHadoopFileSystem;
 import com.qubole.rubix.prestosql.CachingPrestoNativeAzureFileSystem;
 import com.qubole.rubix.prestosql.CachingPrestoS3FileSystem;
+import com.qubole.rubix.prestosql.CachingPrestoSecureAzureBlobFileSystem;
+import com.qubole.rubix.prestosql.CachingPrestoSecureNativeAzureFileSystem;
 import com.qubole.rubix.prestosql.PrestoClusterManager;
 import io.prestosql.plugin.hive.ConfigurationInitializer;
 import io.prestosql.spi.HostAddress;
@@ -40,7 +44,12 @@ public class RubixConfigurationInitializer
         implements ConfigurationInitializer
 {
     private static final String RUBIX_S3_FS_CLASS_NAME = CachingPrestoS3FileSystem.class.getName();
-    private static final String RUBIX_AZURE_FS_CLASS_NAME = CachingPrestoNativeAzureFileSystem.class.getName();
+
+    private static final String RUBIX_NATIVE_AZURE_FS_CLASS_NAME = CachingPrestoNativeAzureFileSystem.class.getName();
+    private static final String RUBIX_SECURE_NATIVE_AZURE_FS_CLASS_NAME = CachingPrestoSecureNativeAzureFileSystem.class.getName();
+    private static final String RUBIX_AZURE_BLOB_FS_CLASS_NAME = CachingPrestoAzureBlobFileSystem.class.getName();
+    private static final String RUBIX_SECURE_AZURE_BLOB_FS_CLASS_NAME = CachingPrestoSecureAzureBlobFileSystem.class.getName();
+
     private static final String RUBIX_GS_FS_CLASS_NAME = CachingPrestoGoogleHadoopFileSystem.class.getName();
 
     private final boolean parallelWarmupEnabled;
@@ -95,7 +104,12 @@ public class RubixConfigurationInitializer
         config.set("fs.s3.impl", RUBIX_S3_FS_CLASS_NAME);
         config.set("fs.s3a.impl", RUBIX_S3_FS_CLASS_NAME);
         config.set("fs.s3n.impl", RUBIX_S3_FS_CLASS_NAME);
-        config.set("fs.wasb.impl", RUBIX_AZURE_FS_CLASS_NAME);
+
+        config.set("fs.wasb.impl", RUBIX_NATIVE_AZURE_FS_CLASS_NAME);
+        config.set("fs.wasbs.impl", RUBIX_SECURE_NATIVE_AZURE_FS_CLASS_NAME);
+        config.set("fs.abfs.impl", RUBIX_AZURE_BLOB_FS_CLASS_NAME);
+        config.set("fs.abfss.impl", RUBIX_SECURE_AZURE_BLOB_FS_CLASS_NAME);
+
         config.set("fs.gs.impl", RUBIX_GS_FS_CLASS_NAME);
         return config;
     }
