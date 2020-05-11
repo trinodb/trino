995e3dda9ad82c21ee697f2720dd151aec60c99b
Arvin Zheng <zmw890713@gmail.com>
2020-05-11 14:40:27 -0700

    Fix matching Hive bucket filenames without leading zeros

--- presto-hive/src/main/java/io/prestosql/plugin/hive/BackgroundHiveSplitLoader.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/BackgroundHiveSplitLoader.java
@@ -108,12 +108,12 @@ public class BackgroundHiveSplitLoader
         implements HiveSplitLoader
 {
     private static final Iterable<Pattern> BUCKET_PATTERNS = ImmutableList.of(
-            // Hive naming pattern per `org.apache.hadoop.hive.ql.exec.Utilities#getBucketIdFromFile()`
-            Pattern.compile("(0\\d+)_\\d+.*"),
-            // Hive ACID
-            Pattern.compile("bucket_(\\d+)"),
             // legacy Presto naming pattern (current version matches Hive)
-            Pattern.compile("\\d{8}_\\d{6}_\\d{5}_[a-z0-9]{5}_bucket-(\\d+)(?:[-_.].*)?"));
+            Pattern.compile("\\d{8}_\\d{6}_\\d{5}_[a-z0-9]{5}_bucket-(\\d+)(?:[-_.].*)?"),
+            // Hive naming pattern per `org.apache.hadoop.hive.ql.exec.Utilities#getBucketIdFromFile()`
+            Pattern.compile("(\\d+)_\\d+.*"),
+            // Hive ACID
+            Pattern.compile("bucket_(\\d+)"));
 
     private static final ListenableFuture<?> COMPLETED_FUTURE = immediateFuture(null);
 
