9d02a3891d9d206c08158a985ff7640478c21530
Karol Sobczak <napewnotrafi@gmail.com>
2020-05-15 17:30:16 +0200

    Add large file test for Rubix

--- presto-hive/src/test/java/io/prestosql/plugin/hive/rubix/TestRubixCaching.java
+++ presto-hive/src/test/java/io/prestosql/plugin/hive/rubix/TestRubixCaching.java
@@ -16,12 +16,14 @@ package io.prestosql.plugin.hive.rubix;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.io.ByteStreams;
 import com.google.common.io.CharStreams;
 import com.google.common.io.Closer;
 import com.qubole.rubix.bookkeeper.LocalDataTransferServer;
 import com.qubole.rubix.core.CachingFileSystem;
 import com.qubole.rubix.core.CachingFileSystemStats;
 import com.qubole.rubix.core.utils.DummyClusterManager;
+import io.airlift.units.DataSize;
 import io.prestosql.metadata.InternalNode;
 import io.prestosql.plugin.base.CatalogName;
 import io.prestosql.plugin.hive.HdfsConfig;
@@ -47,7 +49,12 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -55,17 +62,23 @@ import static com.google.common.io.MoreFiles.deleteRecursively;
 import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
 import static com.qubole.rubix.spi.CacheConfig.setPrestoClusterManager;
 import static io.airlift.testing.Assertions.assertGreaterThan;
+import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static io.prestosql.client.NodeVersion.UNKNOWN;
 import static java.lang.String.format;
 import static java.lang.Thread.sleep;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.createTempDirectory;
+import static java.util.Collections.nCopies;
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @Test(singleThreaded = true)
 public class TestRubixCaching
 {
+    private static final DataSize LARGE_FILE_SIZE = DataSize.of(100, MEGABYTE);
+
     private java.nio.file.Path tempDirectory;
     private FileSystem nonCachingFileSystem;
     private FileSystem cachingFileSystem;
@@ -196,11 +209,82 @@ public class TestRubixCaching
         assertEquals(getCacheStats().getRemoteReads(), intermittentRemoteReads);
     }
 
+    @Test
+    public void testLargeFile()
+            throws Exception
+    {
+        byte[] randomData = new byte[(int) LARGE_FILE_SIZE.toBytes()];
+        new Random().nextBytes(randomData);
+
+        Path file = getStoragePath("large_file");
+        try (FSDataOutputStream outputStream = nonCachingFileSystem.create(file)) {
+            outputStream.write(randomData);
+        }
+
+        long beforeRemoteReads = getCacheStats().getRemoteReads();
+        long beforeCachedReads = getCacheStats().getCachedReads();
+
+        assertTrue(Arrays.equals(randomData, readFileBytes(cachingFileSystem.open(file))));
+
+        // stats are propagated asynchronously, wait for them
+        sleep(1000L);
+        long firstRemoteReads = getCacheStats().getRemoteReads();
+        // data should be fetched from remote source
+        assertGreaterThan(firstRemoteReads, beforeRemoteReads);
+
+        assertTrue(Arrays.equals(randomData, readFileBytes(cachingFileSystem.open(file))));
+
+        // stats are propagated asynchronously, wait for them
+        sleep(1000L);
+        long secondCachedReads = getCacheStats().getCachedReads();
+        long secondRemoteReads = getCacheStats().getRemoteReads();
+        // data should be read from cache only
+        assertGreaterThan(secondCachedReads, beforeCachedReads);
+        assertEquals(secondRemoteReads, firstRemoteReads);
+
+        // make sure parallel reading of large file works
+        ExecutorService executorService = newFixedThreadPool(3);
+        try {
+            List<Callable<?>> reads = nCopies(
+                    3,
+                    () -> {
+                        assertTrue(Arrays.equals(randomData, readFileBytes(cachingFileSystem.open(file))));
+                        return null;
+                    });
+            List<Future<?>> futures = reads.stream()
+                    .map(executorService::submit)
+                    .collect(toImmutableList());
+            for (Future<?> future : futures) {
+                future.get();
+            }
+        }
+        finally {
+            executorService.shutdownNow();
+        }
+
+        // stats are propagated asynchronously, wait for them
+        sleep(1000L);
+        // data should be read from cache only
+        assertGreaterThan(getCacheStats().getCachedReads(), secondCachedReads);
+        assertEquals(getCacheStats().getRemoteReads(), secondRemoteReads);
+    }
+
     private String readFile(FSDataInputStream inputStream)
             throws IOException
     {
         try {
             return CharStreams.toString(new InputStreamReader(inputStream, UTF_8));
+        }
+        finally {
+            inputStream.close();
+        }
+    }
+
+    private byte[] readFileBytes(FSDataInputStream inputStream)
+            throws IOException
+    {
+        try {
+            return ByteStreams.toByteArray(inputStream);
         }
         finally {
             inputStream.close();
