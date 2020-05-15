66c360963e690a13b5a526060d7faac2110279ef
Alex <jo.alex2144@gmail.com>
2020-05-15 12:34:47 -0400

    Fix support for creating Hive external tables using CTAS

--- presto-hive/src/main/java/io/prestosql/plugin/hive/LocationService.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/LocationService.java
@@ -25,7 +25,7 @@ import static java.util.Objects.requireNonNull;
 
 public interface LocationService
 {
-    LocationHandle forNewTable(SemiTransactionalHiveMetastore metastore, ConnectorSession session, String schemaName, String tableName);
+    LocationHandle forNewTable(SemiTransactionalHiveMetastore metastore, ConnectorSession session, String schemaName, String tableName, Optional<Path> externalLocation);
 
     LocationHandle forExistingTable(SemiTransactionalHiveMetastore metastore, ConnectorSession session, Table table);
 
