90d08775bd081a8d601a0b547a85b98d2628605d
Karol Sobczak <napewnotrafi@gmail.com>
2020-05-20 14:05:33 +0200

    Add AVRO product test group

--- presto-product-tests/src/main/java/io/prestosql/tests/hive/TestAvroSchemaEvolution.java
+++ presto-product-tests/src/main/java/io/prestosql/tests/hive/TestAvroSchemaEvolution.java
@@ -23,6 +23,7 @@ import static io.prestosql.tempto.assertions.QueryAssert.Row.row;
 import static io.prestosql.tempto.assertions.QueryAssert.assertThat;
 import static io.prestosql.tempto.context.ThreadLocalTestContextHolder.testContext;
 import static io.prestosql.tempto.query.QueryExecutor.query;
+import static io.prestosql.tests.TestGroups.AVRO;
 import static java.lang.String.format;
 
 public class TestAvroSchemaEvolution
@@ -60,14 +61,14 @@ public class TestAvroSchemaEvolution
         query(format("DROP TABLE IF EXISTS %s", TABLE_NAME));
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testSelectTable()
     {
         assertThat(query(format("SELECT string_col FROM %s", TABLE_NAME)))
                 .containsExactly(row("string0"));
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testInsertAfterSchemaEvolution()
     {
         assertThat(query(SELECT_STAR))
@@ -81,7 +82,7 @@ public class TestAvroSchemaEvolution
                         row("string1", 1, 101));
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testSchemaEvolutionWithIncompatibleType()
     {
         assertThat(query(COLUMNS_IN_TABLE))
@@ -96,7 +97,7 @@ public class TestAvroSchemaEvolution
                 .failsWithMessage("Found int, expecting string");
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testSchemaEvolution()
     {
         assertThat(query(COLUMNS_IN_TABLE))
@@ -138,7 +139,7 @@ public class TestAvroSchemaEvolution
                 .containsExactly(row("string0", null));
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testSchemaWhenUrlIsUnset()
     {
         assertThat(query(COLUMNS_IN_TABLE))
@@ -154,7 +155,7 @@ public class TestAvroSchemaEvolution
                         row("dummy_col", "varchar", "", ""));
     }
 
-    @Test
+    @Test(groups = AVRO)
     public void testCreateTableLike()
     {
         String createTableLikeName = "test_avro_like";
