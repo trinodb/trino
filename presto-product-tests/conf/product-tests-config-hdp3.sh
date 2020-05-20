ab7ef8ad04a1156e09d7a299cded822837830964
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-05-20 11:52:02 +0200

    Rename tempto config file

--- presto-product-tests/conf/product-tests-config-hdp3.sh
+++ presto-product-tests/conf/product-tests-config-hdp3.sh
@@ -1,5 +1,5 @@
 export HADOOP_BASE_IMAGE="prestodev/hdp3.1-hive"
 export TESTS_HIVE_VERSION_MAJOR="3"
 export TESTS_HIVE_VERSION_MINOR="1"
-export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/presto-product-tests/conf/tempto/tempto-configuration-for-hdp3.yaml"
+export TEMPTO_ENVIRONMENT_CONFIG_FILE="/docker/presto-product-tests/conf/tempto/tempto-configuration-for-hive3.yaml"
 export DISTRO_SKIP_GROUP=
