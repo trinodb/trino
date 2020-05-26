ea580b0ab04a72b60c1effc90d61ecbe65ec4185
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-05-26 09:31:34 +0200

    Fix current_time in legacy timestamp semantics
    Fix `current_time` in legacy timestamp semantics when session zone has
    different zone offset on current date compared to offset on 1970-01-01.

--- presto-main/src/test/java/io/prestosql/operator/scalar/TestDateTimeFunctionsLegacy.java
+++ presto-main/src/test/java/io/prestosql/operator/scalar/TestDateTimeFunctionsLegacy.java
@@ -21,7 +21,6 @@ import org.testng.annotations.Test;
 
 import java.time.Instant;
 
-import static io.prestosql.spi.type.TimeWithTimeZoneType.TIME_WITH_TIME_ZONE;
 import static io.prestosql.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
 import static io.prestosql.spi.type.VarcharType.VARCHAR;
 import static io.prestosql.spi.type.VarcharType.createVarcharType;
@@ -44,20 +43,6 @@ public class TestDateTimeFunctionsLegacy
     public void testFormatDateCanImplicitlyAddTimeZoneToTimestampLiteral()
     {
         assertFunction("format_datetime(" + TIMESTAMP_LITERAL + ", 'YYYY/MM/dd HH:mm ZZZZ')", VARCHAR, "2001/08/22 03:04 " + DATE_TIME_ZONE.getID());
-    }
-
-    @Test
-    public void testCurrentTime()
-    {
-        Session localSession = Session.builder(session)
-                // we use Asia/Kathmandu here to test the difference in semantic change of current_time
-                // between legacy and non-legacy timestamp
-                .setTimeZoneKey(KATHMANDU_ZONE_KEY)
-                .setStart(Instant.ofEpochMilli(new DateTime(2017, 3, 1, 15, 45, 0, 0, KATHMANDU_ZONE).getMillis()))
-                .build();
-        try (FunctionAssertions localAssertion = new FunctionAssertions(localSession)) {
-            localAssertion.assertFunctionString("CURRENT_TIME", TIME_WITH_TIME_ZONE, "15:30:00.000 Asia/Kathmandu");
-        }
     }
 
     @Test
