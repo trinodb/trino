8d684e0fef94f84cfddd390fff6f149fd9dde8b1
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-06-16 11:58:49 +0200

    Allow skipping indivudual tests

--- presto-product-tests/bin/product-tests-suite-5.sh
+++ presto-product-tests/bin/product-tests-suite-5.sh
@@ -2,22 +2,25 @@
 
 set -xeuo pipefail
 
+DISTRO_SKIP_GROUP="${DISTRO_SKIP_GROUP:-}"
+DISTRO_SKIP_TEST="${DISTRO_SKIP_TEST:-}"
+
 suite_exit_code=0
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment singlenode-hive-impersonation \
-    -- -g storage_formats,hdfs_impersonation \
+    -- -g storage_formats,hdfs_impersonation -x "${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment singlenode-kerberos-hive-impersonation \
-    -- -g storage_formats,hdfs_impersonation,authorization -x iceberg \
+    -- -g storage_formats,hdfs_impersonation,authorization -x iceberg,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 # TODO: enable avro tests once https://github.com/prestosql/presto/issues/3794 is fixed
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment multinode-hive-caching \
-    -- -g hive_caching,storage_formats -x iceberg,avro \
+    -- -g hive_caching,storage_formats -x iceberg,avro,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 echo "$0: exiting with ${suite_exit_code}"
