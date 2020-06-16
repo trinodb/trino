6f7e32060d6fa10a566bc01faab5c2879b3062f7
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-06-16 11:58:49 +0200

    Allow skipping indivudual tests

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -2,21 +2,24 @@
 
 set -xeuo pipefail
 
+DISTRO_SKIP_GROUP="${DISTRO_SKIP_GROUP:-}"
+DISTRO_SKIP_TEST="${DISTRO_SKIP_TEST:-}"
+
 suite_exit_code=0
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment multinode-tls \
-    -- -g smoke,cli,group-by,join,tls \
+    -- -g smoke,cli,group-by,join,tls -x "${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment multinode-tls-kerberos \
-    -- -g cli,group-by,join,tls \
+    -- -g cli,group-by,join,tls -x "${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment singlenode-kerberos-hdfs-impersonation-with-wire-encryption \
-    -- -g storage_formats,cli,hdfs_impersonation,authorization -x iceberg \
+    -- -g storage_formats,cli,hdfs_impersonation,authorization -x iceberg,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 echo "$0: exiting with ${suite_exit_code}"
