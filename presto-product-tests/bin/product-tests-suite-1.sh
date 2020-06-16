1b4275037b6f834dcb230d9ec9e6f028564216b5
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-06-16 11:58:49 +0200

    Allow skipping indivudual tests

--- presto-product-tests/bin/product-tests-suite-1.sh
+++ presto-product-tests/bin/product-tests-suite-1.sh
@@ -3,12 +3,15 @@
 set -xeuo pipefail
 
 DISTRO_SKIP_GROUP="${DISTRO_SKIP_GROUP:-}"
+DISTRO_SKIP_TEST="${DISTRO_SKIP_TEST:-}"
 
 suite_exit_code=0
 
 presto-product-tests-launcher/bin/run-launcher test run \
     --environment multinode \
-    -- -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql,postgresql,kafka,hive_compression,"${DISTRO_SKIP_GROUP}" \
+    -- \
+    -x quarantine,big_query,storage_formats,profile_specific_tests,tpcds,cassandra,mysql,postgresql,kafka,hive_compression,"${DISTRO_SKIP_GROUP}" \
+    -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
 echo "$0: exiting with ${suite_exit_code}"
