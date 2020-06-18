0099250342f6550fe25ad9ba9b29376a0574e1c4
David Stryker <david.stryker@starburstdata.com>
2020-06-18 16:37:51 -0700

    Fix Iceberg ORC timestamps
    This commit enables Timestamp columns in Iceberg ORC data files,
    and fixes two bugs in handling of Iceberg ORC timestamps,
    both a consequence of performing conversion from the
    Presto representation to/from the ORC representation twice.
    
    Timestamps in Parquet are still handled incorrectly - -
    the Presto Parquet writer is attempting to write the timestamp
    as a string, but it should be a long.  The proper fix is to
    switch to use the Parquet writer.
    
    This uncomments the timestamp test in TestIcebergSmoke.  However,
    it leaves timestamps in testCreatePartitionedTable commented out
    until writing Parquet timestamps is fixed.

--- presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergMetadata.java
+++ presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergMetadata.java
@@ -112,6 +112,7 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 import static org.apache.iceberg.BaseMetastoreTableOperations.ICEBERG_TABLE_TYPE_VALUE;
 import static org.apache.iceberg.BaseMetastoreTableOperations.TABLE_TYPE_PROP;
+import static org.apache.iceberg.FileFormat.ORC;
 import static org.apache.iceberg.TableMetadata.newTableMetadata;
 import static org.apache.iceberg.TableProperties.DEFAULT_FILE_FORMAT;
 import static org.apache.iceberg.Transactions.createTableTransaction;
@@ -381,14 +382,15 @@ public class IcebergMetadata
     {
         IcebergTableHandle table = (IcebergTableHandle) tableHandle;
         org.apache.iceberg.Table icebergTable = getIcebergTable(metastore, hdfsEnvironment, session, table.getSchemaTableName());
+        boolean orcFormat = ORC == getFileFormat(icebergTable);
 
         for (NestedField column : icebergTable.schema().columns()) {
             io.prestosql.spi.type.Type type = toPrestoType(column.type(), typeManager);
             if (type instanceof DecimalType) {
                 throw new PrestoException(NOT_SUPPORTED, "Writing to columns of type decimal not yet supported");
             }
-            if (type instanceof TimestampType) {
-                throw new PrestoException(NOT_SUPPORTED, "Writing to columns of type timestamp not yet supported");
+            if (type instanceof TimestampType && !orcFormat) {
+                throw new PrestoException(NOT_SUPPORTED, "Writing to columns of type timestamp not yet supported for PARQUET format");
             }
         }
 
