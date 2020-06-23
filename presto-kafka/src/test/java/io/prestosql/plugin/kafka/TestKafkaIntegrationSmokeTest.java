6c1fcd2504bcab45b559961453e751b19d49b2fb
Charles Morgan <cjmorgan2000@gmail.com>
2020-06-23 16:07:11 -0400

    Fix error when getting value from ByteBuffer more than once

--- presto-kafka/src/test/java/io/prestosql/plugin/kafka/TestKafkaIntegrationSmokeTest.java
+++ presto-kafka/src/test/java/io/prestosql/plugin/kafka/TestKafkaIntegrationSmokeTest.java
@@ -13,25 +13,107 @@
  */
 package io.prestosql.plugin.kafka;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import io.prestosql.plugin.kafka.util.TestingKafka;
+import io.prestosql.spi.connector.SchemaTableName;
+import io.prestosql.spi.type.BigintType;
+import io.prestosql.spi.type.Type;
 import io.prestosql.testing.AbstractTestIntegrationSmokeTest;
 import io.prestosql.testing.QueryRunner;
 import io.prestosql.tpch.TpchTable;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class TestKafkaIntegrationSmokeTest
         extends AbstractTestIntegrationSmokeTest
 {
     private TestingKafka testingKafka;
+    private String topicName;
 
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
         testingKafka = new TestingKafka();
-        return KafkaQueryRunner.builder(testingKafka)
-                .setTables(TpchTable.getTables())
+        topicName = "test_raw_" + UUID.randomUUID().toString().replaceAll("-", "_");
+
+        Map<SchemaTableName, KafkaTopicDescription> extraTopicDescriptions = ImmutableMap.<SchemaTableName, KafkaTopicDescription>builder()
+                .put(new SchemaTableName("default", topicName),
+                        createDescription(topicName, "default", topicName,
+                                createFieldGroup("raw", ImmutableList.of(
+                                        createOneFieldDescription("id", BigintType.BIGINT, "0", "LONG")))))
                 .build();
+
+        QueryRunner queryRunner = KafkaQueryRunner.builder(testingKafka)
+                .setTables(TpchTable.getTables())
+                .setExtraTopicDescription(ImmutableMap.<SchemaTableName, KafkaTopicDescription>builder()
+                        .putAll(extraTopicDescriptions)
+                        .build())
+                .build();
+
+        testingKafka.createTopics(topicName);
+        return queryRunner;
+    }
+
+    @Test
+    public void testColumnReferencedTwice()
+    {
+        ByteBuffer buf = ByteBuffer.allocate(8);
+        buf.putLong(0, 1);
+
+        insertData(buf.array());
+
+        assertQuery("SELECT id FROM default." + topicName + " WHERE id = 1", "VALUES (1)");
+        assertQuery("SELECT id FROM default." + topicName + " WHERE id < 2", "VALUES (1)");
+    }
+
+    private void insertData(byte[] data)
+    {
+        try (KafkaProducer<byte[], byte[]> producer = createProducer()) {
+            producer.send(new ProducerRecord<>(topicName, data));
+        }
+    }
+
+    private KafkaProducer<byte[], byte[]> createProducer()
+    {
+        Properties properties = new Properties();
+        properties.put(BOOTSTRAP_SERVERS_CONFIG, testingKafka.getConnectString());
+        properties.put(ACKS_CONFIG, "all");
+        properties.put(KEY_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+        properties.put(VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+
+        return new KafkaProducer<>(properties);
+    }
+
+    private KafkaTopicDescription createDescription(String name, String schema, String topic, Optional<KafkaTopicFieldGroup> message)
+    {
+        return new KafkaTopicDescription(name, Optional.of(schema), topic, Optional.empty(), message);
+    }
+
+    private Optional<KafkaTopicFieldGroup> createFieldGroup(String dataFormat, List<KafkaTopicFieldDescription> fields)
+    {
+        return Optional.of(new KafkaTopicFieldGroup(dataFormat, Optional.empty(), fields));
+    }
+
+    private KafkaTopicFieldDescription createOneFieldDescription(String name, Type type, String mapping, String dataFormat)
+    {
+        return new KafkaTopicFieldDescription(name, type, mapping, null, dataFormat, null, false);
     }
 
     @AfterClass(alwaysRun = true)
