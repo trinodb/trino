ebe8afa0f65329c11078b2fbfda2945faca12dd8
Michał Ślizak <michal.slizak+github@gmail.com>
2020-06-25 15:02:21 +0200

    Run fewer tests in HDP3 "by the book" environment
    `storage_formats` are covered also by suites-1,2,3,5 when run with
    `config-hdp3`. Although this is different environment configuration, it
    is still not needed to run them again.

--- presto-product-tests/bin/product-tests-suite-8-non-generic.sh
+++ presto-product-tests/bin/product-tests-suite-8-non-generic.sh
@@ -14,7 +14,7 @@ suite_exit_code=0
 
 presto-product-tests-launcher/bin/run-launcher test run \
    --environment singlenode-hdp3 \
-    -- -g hdp3_only,storage_formats,hive_transactional \
+    -- -g hdp3_only,hive_transactional \
     || suite_exit_code=1
 
 echo "$0: exiting with ${suite_exit_code}"
