8f9f7aa70fbbd23c8ce7256ccca676085dcb1c78
kasiafi <30203062+kasiafi@users.noreply.github.com>
2020-06-30 16:17:05 +0200

    Refactor UnaliasSymbolReferences and SymbolMapper
    In UnaliasSymbolReferences:
    Introduce context, including inherited mapping for correlation symbols.
    Introduce return type for visit() methods, including:
        - rewritten plan,
        - symbol mappings (both inherited and derived).
    
    Previously, there was global symbol mapping that changed each time
    when new symbol mapping was defined. Hence, once a mapping for a symbol
    was defined, it was applied to all occurrences of the symbol that
    were encountered later.
    This approach caused errors in case of reused (duplicate) symbols
    in different branches of the plan.
    
    Symbol mapping is now performed only by SymbolMapper.
    Previously, some mapping methods were defined in
    UnaliasSymbolReferences and some code was copied between
    Unalias and SymbolMapper.

--- presto-main/src/main/java/io/prestosql/sql/planner/optimizations/SymbolMapper.java
+++ presto-main/src/main/java/io/prestosql/sql/planner/optimizations/SymbolMapper.java
@@ -21,20 +21,25 @@ import io.prestosql.sql.planner.PartitioningScheme;
 import io.prestosql.sql.planner.Symbol;
 import io.prestosql.sql.planner.plan.AggregationNode;
 import io.prestosql.sql.planner.plan.AggregationNode.Aggregation;
+import io.prestosql.sql.planner.plan.DistinctLimitNode;
+import io.prestosql.sql.planner.plan.GroupIdNode;
 import io.prestosql.sql.planner.plan.LimitNode;
 import io.prestosql.sql.planner.plan.PlanNode;
 import io.prestosql.sql.planner.plan.PlanNodeId;
+import io.prestosql.sql.planner.plan.RowNumberNode;
 import io.prestosql.sql.planner.plan.StatisticAggregations;
-import io.prestosql.sql.planner.plan.StatisticAggregationsDescriptor;
 import io.prestosql.sql.planner.plan.StatisticsWriterNode;
 import io.prestosql.sql.planner.plan.TableFinishNode;
 import io.prestosql.sql.planner.plan.TableWriterNode;
 import io.prestosql.sql.planner.plan.TopNNode;
+import io.prestosql.sql.planner.plan.TopNRowNumberNode;
+import io.prestosql.sql.planner.plan.WindowNode;
 import io.prestosql.sql.tree.Expression;
 import io.prestosql.sql.tree.ExpressionRewriter;
 import io.prestosql.sql.tree.ExpressionTreeRewriter;
 import io.prestosql.sql.tree.SymbolReference;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +48,7 @@ import java.util.Set;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.prestosql.sql.planner.plan.AggregationNode.groupingSets;
 import static java.util.Objects.requireNonNull;
 
@@ -55,12 +61,33 @@ public class SymbolMapper
         this.mapping = ImmutableMap.copyOf(requireNonNull(mapping, "mapping is null"));
     }
 
+    public Map<Symbol, Symbol> getMapping()
+    {
+        return mapping;
+    }
+
+    // Return the canonical mapping for the symbol.
     public Symbol map(Symbol symbol)
     {
         while (mapping.containsKey(symbol) && !mapping.get(symbol).equals(symbol)) {
             symbol = mapping.get(symbol);
         }
         return symbol;
+    }
+
+    public List<Symbol> map(List<Symbol> symbols)
+    {
+        return symbols.stream()
+                .map(this::map)
+                .collect(toImmutableList());
+    }
+
+    public List<Symbol> mapAndDistinct(List<Symbol> symbols)
+    {
+        return symbols.stream()
+                .map(this::map)
+                .distinct()
+                .collect(toImmutableList());
     }
 
     public Expression map(Expression expression)
@@ -115,14 +142,73 @@ public class SymbolMapper
                 aggregation.getMask().map(this::map));
     }
 
-    public TopNNode map(TopNNode node, PlanNode source, PlanNodeId newNodeId)
+    public GroupIdNode map(GroupIdNode node, PlanNode source)
     {
-        return new TopNNode(
-                newNodeId,
+        Map<Symbol, Symbol> newGroupingMappings = new HashMap<>();
+        ImmutableList.Builder<List<Symbol>> newGroupingSets = ImmutableList.builder();
+
+        for (List<Symbol> groupingSet : node.getGroupingSets()) {
+            ImmutableList.Builder<Symbol> newGroupingSet = ImmutableList.builder();
+            for (Symbol output : groupingSet) {
+                Symbol newOutput = map(output);
+                newGroupingMappings.putIfAbsent(
+                        newOutput,
+                        map(node.getGroupingColumns().get(output)));
+                newGroupingSet.add(newOutput);
+            }
+            newGroupingSets.add(newGroupingSet.build());
+        }
+
+        return new GroupIdNode(
+                node.getId(),
                 source,
-                node.getCount(),
-                map(node.getOrderingScheme()),
-                node.getStep());
+                newGroupingSets.build(),
+                newGroupingMappings,
+                mapAndDistinct(node.getAggregationArguments()),
+                map(node.getGroupIdSymbol()));
+    }
+
+    public WindowNode map(WindowNode node, PlanNode source)
+    {
+        ImmutableMap.Builder<Symbol, WindowNode.Function> newFunctions = ImmutableMap.builder();
+        node.getWindowFunctions().forEach((symbol, function) -> {
+            List<Expression> newArguments = function.getArguments().stream()
+                    .map(this::map)
+                    .collect(toImmutableList());
+            WindowNode.Frame newFrame = map(function.getFrame());
+
+            newFunctions.put(map(symbol), new WindowNode.Function(function.getResolvedFunction(), newArguments, newFrame, function.isIgnoreNulls()));
+        });
+
+        return new WindowNode(
+                node.getId(),
+                source,
+                mapAndDistinct(node.getSpecification()),
+                newFunctions.build(),
+                node.getHashSymbol().map(this::map),
+                node.getPrePartitionedInputs().stream()
+                        .map(this::map)
+                        .collect(toImmutableSet()),
+                node.getPreSortedOrderPrefix());
+    }
+
+    private WindowNode.Frame map(WindowNode.Frame frame)
+    {
+        return new WindowNode.Frame(
+                frame.getType(),
+                frame.getStartType(),
+                frame.getStartValue().map(this::map),
+                frame.getEndType(),
+                frame.getEndValue().map(this::map),
+                frame.getOriginalStartValue(),
+                frame.getOriginalEndValue());
+    }
+
+    private WindowNode.Specification mapAndDistinct(WindowNode.Specification specification)
+    {
+        return new WindowNode.Specification(
+                mapAndDistinct(specification.getPartitionBy()),
+                specification.getOrderingScheme().map(this::map));
     }
 
     public LimitNode map(LimitNode node, PlanNode source)
@@ -135,30 +221,30 @@ public class SymbolMapper
                 node.isPartial());
     }
 
-    public TableWriterNode map(TableWriterNode node, PlanNode source)
+    public OrderingScheme map(OrderingScheme orderingScheme)
     {
-        return map(node, source, node.getId());
+        ImmutableList.Builder<Symbol> newSymbols = ImmutableList.builder();
+        ImmutableMap.Builder<Symbol, SortOrder> newOrderings = ImmutableMap.builder();
+        Set<Symbol> added = new HashSet<>(orderingScheme.getOrderBy().size());
+        for (Symbol symbol : orderingScheme.getOrderBy()) {
+            Symbol canonical = map(symbol);
+            if (added.add(canonical)) {
+                newSymbols.add(canonical);
+                newOrderings.put(canonical, orderingScheme.getOrdering(symbol));
+            }
+        }
+        return new OrderingScheme(newSymbols.build(), newOrderings.build());
     }
 
-    public TableWriterNode map(TableWriterNode node, PlanNode source, PlanNodeId newNodeId)
+    public DistinctLimitNode map(DistinctLimitNode node, PlanNode source)
     {
-        // Intentionally does not use canonicalizeAndDistinct as that would remove columns
-        ImmutableList<Symbol> columns = node.getColumns().stream()
-                .map(this::map)
-                .collect(toImmutableList());
-
-        return new TableWriterNode(
-                newNodeId,
+        return new DistinctLimitNode(
+                node.getId(),
                 source,
-                node.getTarget(),
-                map(node.getRowCountSymbol()),
-                map(node.getFragmentSymbol()),
-                columns,
-                node.getColumnNames(),
-                node.getNotNullColumnSymbols(),
-                node.getPartitioningScheme().map(partitioningScheme -> canonicalize(partitioningScheme, source)),
-                node.getStatisticsAggregation().map(this::map),
-                node.getStatisticsAggregationDescriptor().map(this::map));
+                node.getLimit(),
+                node.isPartial(),
+                mapAndDistinct(node.getDistinctSymbols()),
+                node.getHashSymbol().map(this::map));
     }
 
     public StatisticsWriterNode map(StatisticsWriterNode node, PlanNode source)
@@ -167,9 +253,41 @@ public class SymbolMapper
                 node.getId(),
                 source,
                 node.getTarget(),
-                node.getRowCountSymbol(),
+                map(node.getRowCountSymbol()),
                 node.isRowCountEnabled(),
                 node.getDescriptor().map(this::map));
+    }
+
+    public TableWriterNode map(TableWriterNode node, PlanNode source)
+    {
+        return map(node, source, node.getId());
+    }
+
+    public TableWriterNode map(TableWriterNode node, PlanNode source, PlanNodeId newId)
+    {
+        // Intentionally does not use mapAndDistinct on columns as that would remove columns
+        return new TableWriterNode(
+                newId,
+                source,
+                node.getTarget(),
+                map(node.getRowCountSymbol()),
+                map(node.getFragmentSymbol()),
+                map(node.getColumns()),
+                node.getColumnNames(),
+                node.getNotNullColumnSymbols(),
+                node.getPartitioningScheme().map(partitioningScheme -> map(partitioningScheme, source.getOutputSymbols())),
+                node.getStatisticsAggregation().map(this::map),
+                node.getStatisticsAggregationDescriptor().map(descriptor -> descriptor.map(this::map)));
+    }
+
+    public PartitioningScheme map(PartitioningScheme scheme, List<Symbol> sourceLayout)
+    {
+        return new PartitioningScheme(
+                scheme.getPartitioning().translate(this::map),
+                mapAndDistinct(sourceLayout),
+                scheme.getHashColumn().map(this::map),
+                scheme.isReplicateNullsAndAny(),
+                scheme.getBucketToPartition());
     }
 
     public TableFinishNode map(TableFinishNode node, PlanNode source)
@@ -183,16 +301,6 @@ public class SymbolMapper
                 node.getStatisticsAggregationDescriptor().map(descriptor -> descriptor.map(this::map)));
     }
 
-    private PartitioningScheme canonicalize(PartitioningScheme scheme, PlanNode source)
-    {
-        return new PartitioningScheme(
-                scheme.getPartitioning().translate(this::map),
-                mapAndDistinct(source.getOutputSymbols()),
-                scheme.getHashColumn().map(this::map),
-                scheme.isReplicateNullsAndAny(),
-                scheme.getBucketToPartition());
-    }
-
     private StatisticAggregations map(StatisticAggregations statisticAggregations)
     {
         Map<Symbol, Aggregation> aggregations = statisticAggregations.getAggregations().entrySet().stream()
@@ -200,47 +308,46 @@ public class SymbolMapper
         return new StatisticAggregations(aggregations, mapAndDistinct(statisticAggregations.getGroupingSymbols()));
     }
 
-    private StatisticAggregationsDescriptor<Symbol> map(StatisticAggregationsDescriptor<Symbol> descriptor)
+    public RowNumberNode map(RowNumberNode node, PlanNode source)
     {
-        return descriptor.map(this::map);
+        return new RowNumberNode(
+                node.getId(),
+                source,
+                mapAndDistinct(node.getPartitionBy()),
+                node.isOrderSensitive(),
+                map(node.getRowNumberSymbol()),
+                node.getMaxRowCountPerPartition(),
+                node.getHashSymbol().map(this::map));
     }
 
-    private List<Symbol> map(List<Symbol> outputs)
+    public TopNRowNumberNode map(TopNRowNumberNode node, PlanNode source)
     {
-        return outputs.stream()
-                .map(this::map)
-                .collect(toImmutableList());
+        return new TopNRowNumberNode(
+                node.getId(),
+                source,
+                mapAndDistinct(node.getSpecification()),
+                map(node.getRowNumberSymbol()),
+                node.getMaxRowCountPerPartition(),
+                node.isPartial(),
+                node.getHashSymbol().map(this::map));
     }
 
-    private List<Symbol> mapAndDistinct(List<Symbol> outputs)
+    public TopNNode map(TopNNode node, PlanNode source)
     {
-        Set<Symbol> added = new HashSet<>();
-        ImmutableList.Builder<Symbol> builder = ImmutableList.builder();
-        for (Symbol symbol : outputs) {
-            Symbol canonical = map(symbol);
-            if (added.add(canonical)) {
-                builder.add(canonical);
-            }
-        }
-        return builder.build();
+        return map(node, source, node.getId());
     }
 
-    private OrderingScheme map(OrderingScheme orderingScheme)
+    public TopNNode map(TopNNode node, PlanNode source, PlanNodeId nodeId)
     {
-        ImmutableList.Builder<Symbol> symbols = ImmutableList.builder();
-        ImmutableMap.Builder<Symbol, SortOrder> orderings = ImmutableMap.builder();
-        Set<Symbol> seenCanonicals = new HashSet<>(orderingScheme.getOrderBy().size());
-        for (Symbol symbol : orderingScheme.getOrderBy()) {
-            Symbol canonical = map(symbol);
-            if (seenCanonicals.add(canonical)) {
-                symbols.add(canonical);
-                orderings.put(canonical, orderingScheme.getOrdering(symbol));
-            }
-        }
-        return new OrderingScheme(symbols.build(), orderings.build());
+        return new TopNNode(
+                nodeId,
+                source,
+                node.getCount(),
+                map(node.getOrderingScheme()),
+                node.getStep());
     }
 
-    public static SymbolMapper.Builder builder()
+    public static Builder builder()
     {
         return new Builder();
     }
