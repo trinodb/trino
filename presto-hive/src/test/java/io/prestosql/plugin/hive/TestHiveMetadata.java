5cdbb7cfd85bf50e3a79b71251ae145451cfe81d
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-06-30 14:14:40 +0200

    Fix NaN handling in Hive partition column

--- presto-hive/src/test/java/io/prestosql/plugin/hive/TestHiveMetadata.java
+++ presto-hive/src/test/java/io/prestosql/plugin/hive/TestHiveMetadata.java
@@ -16,6 +16,7 @@ package io.prestosql.plugin.hive;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.prestosql.spi.connector.SchemaTableName;
+import io.prestosql.spi.predicate.Domain;
 import io.prestosql.spi.predicate.NullableValue;
 import io.prestosql.spi.predicate.ValueSet;
 import org.testng.annotations.Test;
@@ -27,7 +28,10 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.prestosql.plugin.hive.HiveColumnHandle.createBaseColumn;
 import static io.prestosql.plugin.hive.HiveMetadata.createPredicate;
+import static io.prestosql.spi.type.DoubleType.DOUBLE;
 import static io.prestosql.spi.type.VarcharType.VARCHAR;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 public class TestHiveMetadata
 {
@@ -36,6 +40,14 @@ public class TestHiveMetadata
             0,
             HiveType.HIVE_STRING,
             VARCHAR,
+            HiveColumnHandle.ColumnType.PARTITION_KEY,
+            Optional.empty());
+
+    private static final HiveColumnHandle DOUBLE_COLUMN_HANDLE = createBaseColumn(
+            "test",
+            0,
+            HiveType.HIVE_DOUBLE,
+            DOUBLE,
             HiveColumnHandle.ColumnType.PARTITION_KEY,
             Optional.empty());
 
@@ -76,6 +88,53 @@ public class TestHiveMetadata
         Domain domain = createPredicate(ImmutableList.of(TEST_COLUMN_HANDLE), partitions.build())
                 .getDomains().orElseThrow().get(TEST_COLUMN_HANDLE);
         assertEquals(domain, Domain.onlyNull(VARCHAR));
+    }
+
+    @Test
+    public void testCreatePredicateWithNaN()
+    {
+        HiveColumnHandle columnHandle = DOUBLE_COLUMN_HANDLE;
+        ImmutableList.Builder<HivePartition> partitions = ImmutableList.builder();
+
+        partitions.add(new HivePartition(
+                new SchemaTableName("test", "test"),
+                "p1",
+                ImmutableMap.of(columnHandle, NullableValue.of(DOUBLE, Double.NaN))));
+
+        partitions.add(new HivePartition(
+                new SchemaTableName("test", "test"),
+                "p2",
+                ImmutableMap.of(columnHandle, NullableValue.of(DOUBLE, 4.2))));
+
+        Domain domain = createPredicate(ImmutableList.of(columnHandle), partitions.build())
+                .getDomains().orElseThrow().get(columnHandle);
+        assertEquals(domain, Domain.notNull(DOUBLE));
+    }
+
+    @Test
+    public void testCreatePredicateWithNaNAndNull()
+    {
+        HiveColumnHandle columnHandle = DOUBLE_COLUMN_HANDLE;
+        ImmutableList.Builder<HivePartition> partitions = ImmutableList.builder();
+
+        partitions.add(new HivePartition(
+                new SchemaTableName("test", "test"),
+                "p1",
+                ImmutableMap.of(columnHandle, NullableValue.of(DOUBLE, Double.NaN))));
+
+        partitions.add(new HivePartition(
+                new SchemaTableName("test", "test"),
+                "p2",
+                ImmutableMap.of(columnHandle, NullableValue.of(DOUBLE, 4.2))));
+
+        partitions.add(new HivePartition(
+                new SchemaTableName("test", "test"),
+                "p3",
+                ImmutableMap.of(columnHandle, NullableValue.asNull(DOUBLE))));
+
+        Domain domain = createPredicate(ImmutableList.of(columnHandle), partitions.build())
+                .getDomains().orElseThrow().get(columnHandle);
+        assertNull(domain);
     }
 
     @Test
