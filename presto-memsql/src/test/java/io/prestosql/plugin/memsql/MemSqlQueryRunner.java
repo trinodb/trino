d0606095f811effef3c4e8b0c39c4da7fccba16e
Karol Sobczak <napewnotrafi@gmail.com>
2020-07-09 11:33:17 +0200

    Add memsql integration smoke tests

---
+++ presto-memsql/src/test/java/io/prestosql/plugin/memsql/MemSqlQueryRunner.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.memsql;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.prestosql.Session;
+import io.prestosql.plugin.tpch.TpchPlugin;
+import io.prestosql.testing.DistributedQueryRunner;
+import io.prestosql.testing.QueryRunner;
+import io.prestosql.tpch.TpchTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.airlift.testing.Closeables.closeAllSuppress;
+import static io.prestosql.plugin.tpch.TpchMetadata.TINY_SCHEMA_NAME;
+import static io.prestosql.testing.QueryAssertions.copyTpchTables;
+import static io.prestosql.testing.TestingSession.testSessionBuilder;
+
+public class MemSqlQueryRunner
+{
+    private MemSqlQueryRunner() {}
+
+    private static final String TPCH_SCHEMA = "tpch";
+
+    public static QueryRunner createMemSqlQueryRunner(TestingMemSqlServer server, TpchTable<?>... tables)
+            throws Exception
+    {
+        return createMemSqlQueryRunner(server, ImmutableMap.of(), ImmutableList.copyOf(tables));
+    }
+
+    public static QueryRunner createMemSqlQueryRunner(TestingMemSqlServer server, Map<String, String> connectorProperties, Iterable<TpchTable<?>> tables)
+            throws Exception
+    {
+        DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(createSession()).build();
+        try {
+            queryRunner.installPlugin(new TpchPlugin());
+            queryRunner.createCatalog("tpch", "tpch");
+
+            connectorProperties = new HashMap<>(ImmutableMap.copyOf(connectorProperties));
+            connectorProperties.putIfAbsent("connection-url", server.getJdbcUrl());
+            connectorProperties.putIfAbsent("connection-user", server.getUsername());
+            connectorProperties.putIfAbsent("connection-password", server.getPassword());
+            connectorProperties.putIfAbsent("allow-drop-table", "true");
+
+            server.execute("CREATE SCHEMA tpch");
+
+            queryRunner.installPlugin(new MemSqlPlugin());
+            queryRunner.createCatalog("memsql", "memsql", connectorProperties);
+
+            copyTpchTables(queryRunner, "tpch", TINY_SCHEMA_NAME, createSession(), tables);
+
+            return queryRunner;
+        }
+        catch (Throwable e) {
+            closeAllSuppress(e, queryRunner);
+            throw e;
+        }
+    }
+
+    private static Session createSession()
+    {
+        return testSessionBuilder()
+                .setCatalog("memsql")
+                .setSchema(TPCH_SCHEMA)
+                .build();
+    }
+}
