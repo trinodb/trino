3328585691d90ed090b90714a361f44fb8c55c3e
Karol Sobczak <napewnotrafi@gmail.com>
2020-07-09 11:33:17 +0200

    Add memsql integration smoke tests

---
+++ presto-memsql/src/test/java/io/prestosql/plugin/memsql/TestMemSqlIntegrationSmokeTest.java
@@ -0,0 +1,200 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.memsql;
+
+import io.prestosql.testing.AbstractTestIntegrationSmokeTest;
+import io.prestosql.testing.MaterializedResult;
+import io.prestosql.testing.MaterializedRow;
+import io.prestosql.testing.QueryRunner;
+import org.intellij.lang.annotations.Language;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static io.prestosql.plugin.memsql.MemSqlQueryRunner.createMemSqlQueryRunner;
+import static io.prestosql.spi.type.VarcharType.VARCHAR;
+import static io.prestosql.testing.MaterializedResult.resultBuilder;
+import static io.prestosql.testing.assertions.Assert.assertEquals;
+import static io.prestosql.tpch.TpchTable.CUSTOMER;
+import static io.prestosql.tpch.TpchTable.NATION;
+import static io.prestosql.tpch.TpchTable.ORDERS;
+import static io.prestosql.tpch.TpchTable.REGION;
+import static java.lang.String.format;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class TestMemSqlIntegrationSmokeTest
+        extends AbstractTestIntegrationSmokeTest
+{
+    protected TestingMemSqlServer memSqlServer;
+
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        memSqlServer = new TestingMemSqlServer();
+        return createMemSqlQueryRunner(memSqlServer, CUSTOMER, NATION, ORDERS, REGION);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public final void destroy()
+    {
+        memSqlServer.close();
+    }
+
+    @Test
+    public void testDropTable()
+    {
+        assertUpdate("CREATE TABLE test_drop AS SELECT 123 x", 1);
+        assertTrue(getQueryRunner().tableExists(getSession(), "test_drop"));
+
+        assertUpdate("DROP TABLE test_drop");
+        assertFalse(getQueryRunner().tableExists(getSession(), "test_drop"));
+    }
+
+    @Test
+    public void testViews()
+    {
+        execute("CREATE VIEW tpch.test_view AS SELECT * FROM tpch.orders");
+        assertQuery("SELECT orderkey FROM test_view", "SELECT orderkey FROM orders");
+        execute("DROP VIEW IF EXISTS tpch.test_view");
+    }
+
+    @Test
+    public void testInsert()
+    {
+        execute("CREATE TABLE tpch.test_insert (x bigint, y varchar(100))");
+        assertUpdate("INSERT INTO test_insert VALUES (123, 'test')", 1);
+        assertQuery("SELECT * FROM test_insert", "SELECT 123 x, 'test' y");
+        assertUpdate("DROP TABLE test_insert");
+    }
+
+    @Test
+    public void testInsertInPresenceOfNotSupportedColumn()
+    {
+        execute("CREATE TABLE tpch.test_insert_not_supported_column_present(x bigint, y decimal(50,0), z varchar(10))");
+        // Check that column y is not supported.
+        assertQuery("SELECT column_name FROM information_schema.columns WHERE table_name = 'test_insert_not_supported_column_present'", "VALUES 'x', 'z'");
+        assertUpdate("INSERT INTO test_insert_not_supported_column_present (x, z) VALUES (123, 'test')", 1);
+        assertQuery("SELECT x, z FROM test_insert_not_supported_column_present", "SELECT 123, 'test'");
+        assertUpdate("DROP TABLE test_insert_not_supported_column_present");
+    }
+
+    @Test
+    public void testNameEscaping()
+    {
+        assertFalse(getQueryRunner().tableExists(getSession(), "test_table"));
+
+        assertUpdate(getSession(), "CREATE TABLE test_table AS SELECT 123 x", 1);
+        assertTrue(getQueryRunner().tableExists(getSession(), "test_table"));
+
+        assertQuery(getSession(), "SELECT * FROM test_table", "SELECT 123");
+
+        assertUpdate(getSession(), "DROP TABLE test_table");
+        assertFalse(getQueryRunner().tableExists(getSession(), "test_table"));
+    }
+
+    @Test
+    public void testMySqlTinyint1()
+    {
+        execute("CREATE TABLE tpch.mysql_test_tinyint1 (c_tinyint tinyint(1))");
+
+        MaterializedResult actual = computeActual("SHOW COLUMNS FROM mysql_test_tinyint1");
+        MaterializedResult expected = resultBuilder(getSession(), VARCHAR, VARCHAR, VARCHAR, VARCHAR)
+                .row("c_tinyint", "tinyint", "", "")
+                .build();
+
+        assertEquals(actual, expected);
+
+        execute("INSERT INTO tpch.mysql_test_tinyint1 VALUES (127), (-128)");
+        MaterializedResult materializedRows = computeActual("SELECT * FROM tpch.mysql_test_tinyint1 WHERE c_tinyint = 127");
+        assertEquals(materializedRows.getRowCount(), 1);
+        MaterializedRow row = getOnlyElement(materializedRows);
+
+        assertEquals(row.getFields().size(), 1);
+        assertEquals(row.getField(0), (byte) 127);
+
+        assertUpdate("DROP TABLE mysql_test_tinyint1");
+    }
+
+    @Test
+    public void testCharTrailingSpace()
+    {
+        execute("CREATE TABLE tpch.char_trailing_space (x char(10))");
+        assertUpdate("INSERT INTO char_trailing_space VALUES ('test')", 1);
+
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test'", "VALUES 'test'");
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test  '", "VALUES 'test'");
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test        '", "VALUES 'test'");
+
+        assertEquals(getQueryRunner().execute("SELECT * FROM char_trailing_space WHERE x = char ' test'").getRowCount(), 0);
+
+        assertUpdate("DROP TABLE char_trailing_space");
+    }
+
+    @Test
+    public void testInsertIntoNotNullColumn()
+    {
+        @Language("SQL") String createTableSql = format("" +
+                        "CREATE TABLE %s.tpch.test_insert_not_null (\n" +
+                        "   column_a date,\n" +
+                        "   column_b date NOT NULL\n" +
+                        ")",
+                getSession().getCatalog().get());
+        assertUpdate(createTableSql);
+        assertEquals(computeScalar("SHOW CREATE TABLE test_insert_not_null"), createTableSql);
+
+        assertQueryFails("INSERT INTO test_insert_not_null (column_a) VALUES (date '2012-12-31')", ".* Field 'column_b' doesn't have a default value");
+        assertQueryFails("INSERT INTO test_insert_not_null (column_a, column_b) VALUES (date '2012-12-31', null)", "NULL value not allowed for NOT NULL column: column_b");
+
+        assertUpdate("ALTER TABLE test_insert_not_null ADD COLUMN column_c BIGINT NOT NULL");
+
+        createTableSql = format("" +
+                        "CREATE TABLE %s.tpch.test_insert_not_null (\n" +
+                        "   column_a date,\n" +
+                        "   column_b date NOT NULL,\n" +
+                        "   column_c bigint NOT NULL\n" +
+                        ")",
+                getSession().getCatalog().get());
+        assertEquals(computeScalar("SHOW CREATE TABLE test_insert_not_null"), createTableSql);
+
+        assertQueryFails("INSERT INTO test_insert_not_null (column_b) VALUES (date '2012-12-31')", ".* Field 'column_c' doesn't have a default value");
+        assertQueryFails("INSERT INTO test_insert_not_null (column_b, column_c) VALUES (date '2012-12-31', null)", "NULL value not allowed for NOT NULL column: column_c");
+
+        assertUpdate("INSERT INTO test_insert_not_null (column_b, column_c) VALUES (date '2012-12-31', 1)", 1);
+        assertUpdate("INSERT INTO test_insert_not_null (column_a, column_b, column_c) VALUES (date '2013-01-01', date '2013-01-02', 2)", 1);
+        assertQuery(
+                "SELECT * FROM test_insert_not_null",
+                "VALUES (NULL, CAST('2012-12-31' AS DATE), 1), (CAST('2013-01-01' AS DATE), CAST('2013-01-02' AS DATE), 2)");
+
+        assertUpdate("DROP TABLE test_insert_not_null");
+    }
+
+    @Test
+    public void testColumnComment()
+    {
+        execute("CREATE TABLE tpch.test_column_comment (col1 bigint COMMENT 'test comment', col2 bigint COMMENT '', col3 bigint)");
+
+        assertQuery(
+                "SELECT column_name, comment FROM information_schema.columns WHERE table_schema = 'tpch' AND table_name = 'test_column_comment'",
+                "VALUES ('col1', 'test comment'), ('col2', null), ('col3', null)");
+
+        assertUpdate("DROP TABLE test_column_comment");
+    }
+
+    private void execute(String sql)
+    {
+        memSqlServer.execute(sql);
+    }
+}
