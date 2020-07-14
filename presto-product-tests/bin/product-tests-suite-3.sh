d7ef95858d1373dea65ced6fbd5e6ca0b011d108
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-07-14 13:26:05 +0200

    Run subset of product tests against env with data protection

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -22,5 +22,11 @@ presto-product-tests-launcher/bin/run-launcher test run \
     -- -g storage_formats,cli,hdfs_impersonation,authorization -x iceberg,"${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
     || suite_exit_code=1
 
+# Smoke run af a few tests in environment with dfs.data.transfer.protection=true. Arbitrary tests which access HDFS data were chosen.
+presto-product-tests-launcher/bin/run-launcher test run \
+    --environment singlenode-kerberos-hdfs-impersonation-with-data-protection \
+    -- -t TestHiveStorageFormats.testOrcTableCreatedInPresto,TestHiveCreateTable.testCreateTable  -x "${DISTRO_SKIP_GROUP}" -e "${DISTRO_SKIP_TEST}" \
+    || suite_exit_code=1
+
 echo "$0: exiting with ${suite_exit_code}"
 exit "${suite_exit_code}"
