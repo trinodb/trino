ee1c88f625fb182b07c94d43e6c04441b8d0d993
Harmandeep Singh <harmandeeps@qubole.com>
2020-07-23 01:15:10 +0530

    fixing product test and unit test

--- presto-hive/src/main/java/io/prestosql/plugin/hive/orc/OrcDeletedRows.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/orc/OrcDeletedRows.java
@@ -50,7 +50,8 @@ import static org.apache.hadoop.hive.ql.io.AcidUtils.deleteDeltaSubdir;
 public class OrcDeletedRows
 {
     private static final int ORIGINAL_TRANSACTION_INDEX = 0;
-    private static final int ROW_ID_INDEX = 2;
+    private static final int ROW_ID_INDEX_DELETE_DELTA = 1; // delete delta page has columns: <originalId,rowId>
+    private static final int ROW_ID_INDEX = 2; // base/delta page has columns: <originalId,bucketId,rowId>
 
     private final String sourceFileName;
     private final OrcDeleteDeltaPageSourceFactory pageSourceFactory;
@@ -209,7 +210,7 @@ public class OrcDeletedRows
                         if (page != null) {
                             for (int i = 0; i < page.getPositionCount(); i++) {
                                 long originalTransaction = BIGINT.getLong(page.getBlock(ORIGINAL_TRANSACTION_INDEX), i);
-                                long row = BIGINT.getLong(page.getBlock(ROW_ID_INDEX), i);
+                                long row = BIGINT.getLong(page.getBlock(ROW_ID_INDEX_DELETE_DELTA), i);
                                 deletedRowsBuilder.add(new RowId(originalTransaction, row));
                             }
                         }
