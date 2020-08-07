5f86d562544c61fd93aafebb3319a92c97914c9f
David Phillips <david@acz.org>
2020-08-07 15:05:43 -0700

    Fix config for HTTP/2 and disable by default

--- presto-main/src/test/java/io/prestosql/server/TestInternalCommunicationConfig.java
+++ presto-main/src/test/java/io/prestosql/server/TestInternalCommunicationConfig.java
@@ -32,7 +32,7 @@ public class TestInternalCommunicationConfig
     {
         assertRecordedDefaults(recordDefaults(InternalCommunicationConfig.class)
                 .setSharedSecret(null)
-                .setHttp2Enabled(true)
+                .setHttp2Enabled(false)
                 .setHttpsRequired(false)
                 .setKeyStorePath(null)
                 .setKeyStorePassword(null)
@@ -49,7 +49,7 @@ public class TestInternalCommunicationConfig
 
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
                 .put("internal-communication.shared-secret", "secret")
-                .put("internal-communication.http2.enabled", "false")
+                .put("internal-communication.http2.enabled", "true")
                 .put("internal-communication.https.required", "true")
                 .put("internal-communication.https.keystore.path", keystoreFile.toString())
                 .put("internal-communication.https.keystore.key", "key-key")
@@ -59,7 +59,7 @@ public class TestInternalCommunicationConfig
 
         InternalCommunicationConfig expected = new InternalCommunicationConfig()
                 .setSharedSecret("secret")
-                .setHttp2Enabled(false)
+                .setHttp2Enabled(true)
                 .setHttpsRequired(true)
                 .setKeyStorePath(keystoreFile.toString())
                 .setKeyStorePassword("key-key")
