3ff22037284faf854460b40e1976434b10decca1
Dain Sundstrom <dain@iq80.com>
2020-08-10 15:19:46 -0700

    Return TIMESTAMP WITH TIME ZONE from Cassandra connector

--- presto-product-tests/src/main/java/io/prestosql/tests/cassandra/DataTypesTableDefinition.java
+++ presto-product-tests/src/main/java/io/prestosql/tests/cassandra/DataTypesTableDefinition.java
@@ -26,7 +26,8 @@ import java.math.BigInteger;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -52,24 +53,52 @@ public final class DataTypesTableDefinition
         RelationalDataSource dataSource = () -> {
             try {
                 return ImmutableList.of(
-                        ImmutableList.of("\0", Long.MIN_VALUE, Bytes.fromHexString("0x00"), false,
-                                BigDecimal.ZERO, Double.MIN_VALUE, LocalDate.fromYearMonthDay(1970, 1, 2), Float.MIN_VALUE, ImmutableSet.of(0),
-                                Inet4Address.getByName("0.0.0.0"), Integer.MIN_VALUE, ImmutableList.of(0),
-                                ImmutableMap.of("a", 0, "\0", Integer.MIN_VALUE), ImmutableSet.of(0), Short.MIN_VALUE,
-                                "\0", Byte.MIN_VALUE, Timestamp.valueOf(LocalDateTime.of(1970, 1, 1, 0, 0)),
+                        ImmutableList.of(
+                                "\0",
+                                Long.MIN_VALUE,
+                                Bytes.fromHexString("0x00"),
+                                false,
+                                BigDecimal.ZERO,
+                                Double.MIN_VALUE,
+                                LocalDate.fromYearMonthDay(1970, 1, 2),
+                                Float.MIN_VALUE,
+                                ImmutableSet.of(0),
+                                Inet4Address.getByName("0.0.0.0"),
+                                Integer.MIN_VALUE,
+                                ImmutableList.of(0),
+                                ImmutableMap.of("a", 0, "\0", Integer.MIN_VALUE),
+                                ImmutableSet.of(0),
+                                Short.MIN_VALUE,
+                                "\0",
+                                Byte.MIN_VALUE,
+                                Timestamp.from(OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant()),
                                 UUID.fromString("d2177dd0-eaa2-11de-a572-001b779c76e3"),
                                 UUID.fromString("01234567-0123-0123-0123-0123456789ab"),
-                                "\0", BigInteger.valueOf(Long.MIN_VALUE)),
-                        ImmutableList.of("the quick brown fox jumped over the lazy dog", Long.MAX_VALUE,
-                                Bytes.fromHexString("0x3031323334"), true,
+                                "\0",
+                                BigInteger.valueOf(Long.MIN_VALUE)),
+                        ImmutableList.of(
+                                "the quick brown fox jumped over the lazy dog",
+                                Long.MAX_VALUE,
+                                Bytes.fromHexString("0x3031323334"),
+                                true,
                                 BigDecimal.valueOf(parseDouble("99999999999999999999999999999999999999")),
-                                Double.MAX_VALUE, LocalDate.fromYearMonthDay(9999, 12, 31), Float.MAX_VALUE, ImmutableSet.of(4, 5, 6, 7),
-                                Inet4Address.getByName("255.255.255.255"), Integer.MAX_VALUE,
-                                ImmutableList.of(4, 5, 6), ImmutableMap.of("a", 1, "b", 2), ImmutableSet.of(4, 5, 6), Short.MAX_VALUE,
-                                "this is a text value", Byte.MAX_VALUE, Timestamp.valueOf(LocalDateTime.of(9999, 12, 31, 23, 59, 59)),
+                                Double.MAX_VALUE,
+                                LocalDate.fromYearMonthDay(9999, 12, 31),
+                                Float.MAX_VALUE,
+                                ImmutableSet.of(4, 5, 6, 7),
+                                Inet4Address.getByName("255.255.255.255"),
+                                Integer.MAX_VALUE,
+                                ImmutableList.of(4, 5, 6),
+                                ImmutableMap.of("a", 1, "b", 2),
+                                ImmutableSet.of(4, 5, 6),
+                                Short.MAX_VALUE,
+                                "this is a text value",
+                                Byte.MAX_VALUE,
+                                Timestamp.from(OffsetDateTime.of(9999, 12, 31, 23, 59, 59, 0, ZoneOffset.UTC).toInstant()),
                                 UUID.fromString("d2177dd0-eaa2-11de-a572-001b779c76e3"),
                                 UUID.fromString("01234567-0123-0123-0123-0123456789ab"),
-                                "abc", BigInteger.valueOf(Long.MAX_VALUE)),
+                                "abc",
+                                BigInteger.valueOf(Long.MAX_VALUE)),
                         Arrays.asList(new Object[] {"def", null, null, null, null, null, null, null, null, null, null,
                                 null, null, null, null, null, null, null, null, null, null, null})
                 ).iterator();
