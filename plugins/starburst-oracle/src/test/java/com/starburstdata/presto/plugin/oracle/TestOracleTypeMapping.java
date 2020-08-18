c23246ddfd69d896ed0eedb071a86c7a99b0a70a
Jacob I. Komissar <jacob.komissar@starburstdata.com>
2020-08-18 10:15:45 -0400

    Clean up some tests and a comment missed in recent datetime changes
    (cherry-picked from commit: 748da7850d78ea9bfddc9cdd9bfbfddf4726fbbb)

--- plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/TestOracleTypeMapping.java
+++ plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/TestOracleTypeMapping.java
@@ -631,13 +631,12 @@ public class TestOracleTypeMapping
                 .addRoundTrip(timestampDataType(), beforeEpoch)
                 .addRoundTrip(timestampDataType(), afterEpoch)
                 .addRoundTrip(timestampDataType(), timeDoubledInJvmZone)
-                .addRoundTrip(timestampDataType(), timeDoubledInVilnius);
-
-        addTimestampTestIfSupported(tests, epoch); // epoch also is a gap in JVM zone
-        addTimestampTestIfSupported(tests, timeGapInJvmZone1);
-        addTimestampTestIfSupported(tests, timeGapInJvmZone2);
-        addTimestampTestIfSupported(tests, timeGapInVilnius);
-        addTimestampTestIfSupported(tests, timeGapInKathmandu);
+                .addRoundTrip(timestampDataType(), timeDoubledInVilnius)
+                .addRoundTrip(timestampDataType(), epoch) // epoch also is a gap in JVM zone
+                .addRoundTrip(timestampDataType(), timeGapInJvmZone1)
+                .addRoundTrip(timestampDataType(), timeGapInJvmZone2)
+                .addRoundTrip(timestampDataType(), timeGapInVilnius)
+                .addRoundTrip(timestampDataType(), timeGapInKathmandu);
 
         Session session = Session.builder(getQueryRunner().getDefaultSession())
                 .setTimeZoneKey(TimeZoneKey.getTimeZoneKey(sessionZone.getId()))
@@ -649,11 +648,6 @@ public class TestOracleTypeMapping
         else {
             tests.execute(getQueryRunner(), session, oracleCreateAndInsert("test_timestamp"));
         }
-    }
-
-    private void addTimestampTestIfSupported(DataTypeTest tests, LocalDateTime dateTime)
-    {
-        tests.addRoundTrip(timestampDataType(), dateTime);
     }
 
     @DataProvider
