a0900ee57de510fb9c79fdb8d8ea28591f13e05d
Charles Morgan <cjmorgan2000@gmail.com>
2020-08-19 15:45:56 -0400

    Add tests for Kafka JSON date time types

---
+++ presto-kafka/src/test/java/io/prestosql/plugin/kafka/encoder/json/TestISO8601JsonDateTimeFormatter.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.kafka.encoder.json;
+
+import io.prestosql.plugin.kafka.encoder.json.format.JsonDateTimeFormatter;
+import io.prestosql.spi.type.SqlDate;
+import io.prestosql.spi.type.SqlTime;
+import io.prestosql.spi.type.SqlTimeWithTimeZone;
+import io.prestosql.spi.type.SqlTimestamp;
+import io.prestosql.spi.type.SqlTimestampWithTimeZone;
+import io.prestosql.spi.type.TimeZoneKey;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static io.prestosql.plugin.kafka.encoder.json.format.DateTimeFormat.ISO8601;
+import static io.prestosql.spi.type.TimeZoneKey.UTC_KEY;
+import static io.prestosql.testing.DateTimeTestingUtils.sqlDateOf;
+import static io.prestosql.testing.DateTimeTestingUtils.sqlTimeOf;
+import static io.prestosql.testing.DateTimeTestingUtils.sqlTimeWithTimeZoneOf;
+import static io.prestosql.testing.DateTimeTestingUtils.sqlTimestampOf;
+import static io.prestosql.testing.DateTimeTestingUtils.sqlTimestampWithTimeZoneOf;
+import static io.prestosql.testing.assertions.Assert.assertEquals;
+
+public class TestISO8601JsonDateTimeFormatter
+{
+    private JsonDateTimeFormatter getFormatter()
+    {
+        return ISO8601.getFormatter(Optional.empty());
+    }
+
+    private void testDate(SqlDate value, String expectedLiteral)
+    {
+        String actualLiteral = getFormatter().formatDate(value);
+        assertEquals(actualLiteral, expectedLiteral);
+    }
+
+    private void testTime(SqlTime value, int precision, String expectedLiteral)
+    {
+        String actualLiteral = getFormatter().formatTime(value, precision);
+        assertEquals(actualLiteral, expectedLiteral);
+    }
+
+    private void testTimeWithTZ(SqlTimeWithTimeZone value, String expectedLiteral)
+    {
+        String actualLiteral = getFormatter().formatTimeWithZone(value);
+        assertEquals(actualLiteral, expectedLiteral);
+    }
+
+    private void testTimestamp(SqlTimestamp value, String expectedLiteral)
+    {
+        String actualLiteral = getFormatter().formatTimestamp(value);
+        assertEquals(actualLiteral, expectedLiteral);
+    }
+
+    private void testTimestampWithTZ(SqlTimestampWithTimeZone value, String expectedLiteral)
+    {
+        String actualLiteral = getFormatter().formatTimestampWithZone(value);
+        assertEquals(actualLiteral, expectedLiteral);
+    }
+
+    @Test
+    public void testISO8601DateTimeFunctions()
+    {
+        testDate(sqlDateOf(2020, 8, 14), "2020-08-14");
+        testDate(sqlDateOf(1970, 1, 1), "1970-01-01");
+        testDate(sqlDateOf(1900, 1, 1), "1900-01-01");
+        testDate(sqlDateOf(3001, 1, 1), "3001-01-01");
+
+        testTime(sqlTimeOf(3, 15, 36, 25, 123000000), 3, "15:36:25.123");
+        testTime(sqlTimeOf(3, 15, 36, 25, 0), 3, "15:36:25");
+        testTime(sqlTimeOf(3, 8, 12, 45, 987000000), 3, "08:12:45.987");
+        testTime(sqlTimeOf(3, 0, 0, 0, 0), 3, "00:00");
+        testTime(sqlTimeOf(3, 23, 59, 59, 999000000), 3, "23:59:59.999");
+
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 10, 23, 35, 123000000, 0, 0), "10:23:35.123Z");
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 0, 0, 0, 0, 0, 0), "00:00:00Z");
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 10, 23, 35, 123000000, 2, 0), "10:23:35.123+02:00");
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 10, 23, 35, 123000000, 10, 0), "10:23:35.123+10:00");
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 10, 23, 35, 123000000, -10, 0), "10:23:35.123-10:00");
+        testTimeWithTZ(sqlTimeWithTimeZoneOf(3, 23, 59, 59, 999000000, 0, 0), "23:59:59.999Z");
+
+        testTimestamp(sqlTimestampOf(3, 2020, 8, 18, 12, 38, 29, 123), "2020-08-18T12:38:29.123");
+        testTimestamp(sqlTimestampOf(3, 1970, 1, 1, 0, 0, 0, 0), "1970-01-01T00:00");
+        testTimestamp(sqlTimestampOf(3, 1800, 8, 18, 12, 38, 29, 123), "1800-08-18T12:38:29.123");
+
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 2020, 8, 19, 12, 23, 41, 123000000, UTC_KEY), "2020-08-19T12:23:41.123Z");
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 2020, 8, 19, 12, 23, 41, 123000000, TimeZoneKey.getTimeZoneKey("America/New_York")), "2020-08-19T12:23:41.123-04:00");
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 1800, 8, 19, 12, 23, 41, 123000000, UTC_KEY), "1800-08-19T12:23:41.123Z");
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 2020, 8, 19, 12, 23, 41, 123000000, TimeZoneKey.getTimeZoneKey("Asia/Hong_Kong")), "2020-08-19T12:23:41.123+08:00");
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 2020, 8, 19, 12, 23, 41, 123000000, TimeZoneKey.getTimeZoneKey("Africa/Mogadishu")), "2020-08-19T12:23:41.123+03:00");
+        testTimestampWithTZ(sqlTimestampWithTimeZoneOf(3, 1970, 1, 1, 0, 0, 0, 0, UTC_KEY), "1970-01-01T00:00:00Z");
+    }
+}
