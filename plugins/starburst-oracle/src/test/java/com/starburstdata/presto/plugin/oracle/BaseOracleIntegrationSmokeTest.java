2c5455aad4114d791d3f1d367854a438e9959724
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-08-24 23:56:57 +0200

    Add Oracle aggregation + filter pushdown smoke tests
    (cherry-picked from commit: 5b9ab12895bc407813fd2b11d0f7554183ade43d)

--- plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/BaseOracleIntegrationSmokeTest.java
+++ plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/BaseOracleIntegrationSmokeTest.java
@@ -11,7 +11,9 @@ package com.starburstdata.presto.plugin.oracle;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import io.prestosql.sql.planner.plan.AggregationNode;
 import io.prestosql.sql.planner.plan.FilterNode;
+import io.prestosql.sql.planner.plan.ProjectNode;
 import io.prestosql.testing.AbstractTestIntegrationSmokeTest;
 import io.prestosql.testing.MaterializedResult;
 import io.prestosql.testing.sql.SqlExecutor;
@@ -225,14 +227,40 @@ public abstract class BaseOracleIntegrationSmokeTest
         assertThat(query("SELECT regionkey, avg(nationkey) FROM nation GROUP BY regionkey")).isCorrectlyPushedDown();
 
         try (TestTable testTable = new TestTable(inOracle(), getSession().getSchema().orElseThrow() + ".test_aggregation_pushdown",
-                "(short_decimal decimal(9, 3), long_decimal decimal(30, 10))")) {
-            executeInOracle("INSERT INTO " + testTable.getName() + " VALUES (100.000, 100000000.000000000)");
-            executeInOracle("INSERT INTO " + testTable.getName() + " VALUES (123.321, 123456789.987654321)");
+                "(short_decimal decimal(9, 3), long_decimal decimal(30, 10), varchar_column varchar(10))")) {
+            executeInOracle("INSERT INTO " + testTable.getName() + " VALUES (100.000, 100000000.000000000, 'ala')");
+            executeInOracle("INSERT INTO " + testTable.getName() + " VALUES (123.321, 123456789.987654321, 'kot')");
 
             assertThat(query("SELECT min(short_decimal), min(long_decimal) FROM " + testTable.getName())).isCorrectlyPushedDown();
             assertThat(query("SELECT max(short_decimal), max(long_decimal) FROM " + testTable.getName())).isCorrectlyPushedDown();
             assertThat(query("SELECT sum(short_decimal), sum(long_decimal) FROM " + testTable.getName())).isCorrectlyPushedDown();
             assertThat(query("SELECT avg(short_decimal), avg(long_decimal) FROM " + testTable.getName())).isCorrectlyPushedDown();
+
+            // smoke testing of more complex cases
+            // WHERE on aggregation column
+            assertThat(query("SELECT min(short_decimal), min(long_decimal) FROM " + testTable.getName() + " WHERE short_decimal < 110 AND long_decimal < 124")).isCorrectlyPushedDown();
+            // WHERE on non-aggregation column
+            assertThat(query("SELECT min(long_decimal) FROM " + testTable.getName() + " WHERE short_decimal < 110")).isCorrectlyPushedDown();
+            // GROUP BY
+            assertThat(query("SELECT short_decimal, min(long_decimal) FROM " + testTable.getName() + " GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // GROUP BY with WHERE on both grouping and aggregation column
+            assertThat(query("SELECT short_decimal, min(long_decimal) FROM " + testTable.getName() + " WHERE short_decimal < 110 AND long_decimal < 124" + " GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // GROUP BY with WHERE on grouping column
+            assertThat(query("SELECT short_decimal, min(long_decimal) FROM " + testTable.getName() + " WHERE short_decimal < 110 GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // GROUP BY with WHERE on aggregation column
+            assertThat(query("SELECT short_decimal, min(long_decimal) FROM " + testTable.getName() + " WHERE long_decimal < 124 GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // GROUP BY with WHERE on neither grouping nor aggregation column
+            assertThat(query("SELECT short_decimal, min(long_decimal) FROM " + testTable.getName() + " WHERE varchar_column = 'ala' GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // aggregation on varchar column
+            assertThat(query("SELECT min(varchar_column) FROM " + testTable.getName())).isCorrectlyPushedDown();
+            // aggregation on varchar column with GROUPING
+            assertThat(query("SELECT short_decimal, min(varchar_column) FROM " + testTable.getName() + " GROUP BY short_decimal")).isCorrectlyPushedDown();
+            // aggregation on varchar column with WHERE
+            assertThat(query("SELECT min(varchar_column) FROM " + testTable.getName() + " WHERE varchar_column ='ala'")).isCorrectlyPushedDown();
+
+            // not supported yet
+            assertThat(query("SELECT min(DISTINCT short_decimal) FROM " + testTable.getName())).isNotFullyPushedDown(AggregationNode.class);
+            assertThat(query("SELECT DISTINCT short_decimal, min(long_decimal) FROM " + testTable.getName() + " GROUP BY short_decimal")).isNotFullyPushedDown(ProjectNode.class);
         }
     }
 
