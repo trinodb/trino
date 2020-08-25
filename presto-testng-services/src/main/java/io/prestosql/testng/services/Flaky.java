416c0dd13906fe44f9081d5a215ce404b0d2bde9
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-08-25 07:46:58 +0200

    Require identifying flakiness's exception
    When marking a `@Flaky` test for retry, require identification of a
    flakiness by providing matching exception pattern. This prevents further
    regressions and helps as a code documentation.

--- presto-testng-services/src/main/java/io/prestosql/testng/services/Flaky.java
+++ presto-testng-services/src/main/java/io/prestosql/testng/services/Flaky.java
@@ -15,6 +15,7 @@ package io.prestosql.testng.services;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.util.regex.Matcher;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -28,4 +29,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface Flaky
 {
     String issue();
+
+    /**
+     * A test is retried when it fails with a stacktrace, which string representation matches given regular expression.
+     * The pattern is searched for, as if {@link Matcher#find()} was used.
+     */
+    String match();
 }
