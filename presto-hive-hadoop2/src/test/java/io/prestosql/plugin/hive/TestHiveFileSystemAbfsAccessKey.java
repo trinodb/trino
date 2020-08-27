baf442855b324212544bece9e46cce164db7bdc6
Jacob I. Komissar <jacob.komissar@starburstdata.com>
2020-08-27 18:05:41 -0400

    Add superclass for tests using different ABFS authentication methods

--- presto-hive-hadoop2/src/test/java/io/prestosql/plugin/hive/TestHiveFileSystemAbfsAccessKey.java
+++ presto-hive-hadoop2/src/test/java/io/prestosql/plugin/hive/TestHiveFileSystemAbfsAccessKey.java
@@ -13,39 +13,13 @@
  */
 package io.prestosql.plugin.hive;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import io.prestosql.plugin.hive.AbstractTestHive.Transaction;
 import io.prestosql.plugin.hive.azure.HiveAzureConfig;
-import io.prestosql.plugin.hive.azure.PrestoAzureConfigurationInitializer;
-import io.prestosql.spi.connector.ColumnMetadata;
-import io.prestosql.spi.connector.ConnectorTableMetadata;
-import io.prestosql.spi.connector.SchemaTableName;
-import org.apache.hadoop.fs.Path;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
-import java.util.Map;
-import java.util.Optional;
-
-import static com.google.common.base.Preconditions.checkArgument;
-import static io.prestosql.plugin.hive.HiveTableProperties.BUCKETED_BY_PROPERTY;
-import static io.prestosql.plugin.hive.HiveTableProperties.BUCKET_COUNT_PROPERTY;
-import static io.prestosql.plugin.hive.HiveTableProperties.EXTERNAL_LOCATION_PROPERTY;
-import static io.prestosql.plugin.hive.HiveTableProperties.SKIP_FOOTER_LINE_COUNT;
-import static io.prestosql.plugin.hive.HiveTableProperties.SKIP_HEADER_LINE_COUNT;
-import static io.prestosql.plugin.hive.HiveTableProperties.SORTED_BY_PROPERTY;
-import static io.prestosql.plugin.hive.HiveTableProperties.STORAGE_FORMAT_PROPERTY;
-import static io.prestosql.spi.type.BigintType.BIGINT;
-import static java.lang.String.format;
-import static org.testng.util.Strings.isNullOrEmpty;
-
 public class TestHiveFileSystemAbfsAccessKey
-        extends AbstractTestHiveFileSystem
+        extends AbstractTestHiveFileSystemAbfs
 {
-    private String container;
-    private String account;
     private String accessKey;
     private String testDirectory;
 
@@ -61,62 +35,15 @@ public class TestHiveFileSystemAbfsAccessKey
     @BeforeClass
     public void setup(String host, int port, String databaseName, String container, String account, String accessKey, String testDirectory)
     {
-        checkArgument(!isNullOrEmpty(host), "expected non empty host");
-        checkArgument(!isNullOrEmpty(databaseName), "Expected non empty databaseName");
-        checkArgument(!isNullOrEmpty(container), "expected non empty container");
-        checkArgument(!isNullOrEmpty(account), "expected non empty account");
-        checkArgument(!isNullOrEmpty(accessKey), "expected non empty accessKey");
-        checkArgument(!isNullOrEmpty(testDirectory), "expected non empty testDirectory");
-
-        this.container = container;
-        this.account = account;
-        this.accessKey = accessKey;
-        this.testDirectory = testDirectory;
-
-        super.setup(host, port, databaseName, false, createHdfsConfiguration());
+        this.accessKey = checkParameter(accessKey, "access key");
+        super.setup(host, port, databaseName, container, account, testDirectory);
     }
 
     @Override
-    protected void onSetupComplete()
+    protected HiveAzureConfig getConfig()
     {
-        ensureTableExists(table, "presto_test_external_fs", ImmutableMap.of());
-
-        ensureTableExists(tableWithHeader, "presto_test_external_fs_with_header", ImmutableMap.of(SKIP_HEADER_LINE_COUNT, 1));
-        ensureTableExists(tableWithHeaderAndFooter, "presto_test_external_fs_with_header_and_footer", ImmutableMap.of(SKIP_HEADER_LINE_COUNT, 2, SKIP_FOOTER_LINE_COUNT, 2));
-    }
-
-    private void ensureTableExists(SchemaTableName table, String tableDirectoryName, Map<String, Object> tableProperties)
-    {
-        try (Transaction transaction = newTransaction()) {
-            ConnectorTableMetadata tableMetadata = new ConnectorTableMetadata(
-                    table,
-                    ImmutableList.of(new ColumnMetadata("t_bigint", BIGINT)),
-                    ImmutableMap.<String, Object>builder()
-                            .putAll(tableProperties)
-                            .put(STORAGE_FORMAT_PROPERTY, HiveStorageFormat.TEXTFILE)
-                            .put(EXTERNAL_LOCATION_PROPERTY, getBasePath().toString() + "/" + tableDirectoryName)
-                            .put(BUCKET_COUNT_PROPERTY, 0)
-                            .put(BUCKETED_BY_PROPERTY, ImmutableList.of())
-                            .put(SORTED_BY_PROPERTY, ImmutableList.of())
-                            .build());
-            if (!transaction.getMetadata().listTables(newSession(), Optional.of(table.getSchemaName())).contains(table)) {
-                transaction.getMetadata().createTable(newSession(), tableMetadata, false);
-            }
-            transaction.commit();
-        }
-    }
-
-    private HdfsConfiguration createHdfsConfiguration()
-    {
-        ConfigurationInitializer azureConfig = new PrestoAzureConfigurationInitializer(new HiveAzureConfig()
+        return new HiveAzureConfig()
                 .setAbfsAccessKey(accessKey)
-                .setAbfsStorageAccount(account));
-        return new HiveHdfsConfiguration(new HdfsConfigurationInitializer(new HdfsConfig(), ImmutableSet.of(azureConfig)), ImmutableSet.of());
-    }
-
-    @Override
-    protected Path getBasePath()
-    {
-        return new Path(format("abfs://%s@%s.dfs.core.windows.net/%s/", container, account, testDirectory));
+                .setAbfsStorageAccount(account);
     }
 }
