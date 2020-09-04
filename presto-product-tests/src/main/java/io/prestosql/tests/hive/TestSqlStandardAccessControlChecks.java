85205d0953dcbcb1c5c059846e39f971be77489e
zaz968m <myminitrue@gmail.com>
2020-09-04 10:18:32 +0200

    Improve Access Denied message: insufficient view owner privileges
    If a user A creates a view and user B wants to access the view and
    then user A loses access to the original data
    then B should not be able to access data in the view.
    If B gets and access denied,
    they are told whether it's because they don't have access to the view
    or if the view creator doesn't have access to the underlying data.

--- presto-product-tests/src/main/java/io/prestosql/tests/hive/TestSqlStandardAccessControlChecks.java
+++ presto-product-tests/src/main/java/io/prestosql/tests/hive/TestSqlStandardAccessControlChecks.java
@@ -162,7 +162,7 @@ public class TestSqlStandardAccessControlChecks
 
         // Charlie still cannot access view because Bob does not have SELECT WITH GRANT OPTION
         assertThat(() -> charlieExecutor.executeQuery(format("SELECT * FROM %s", viewName)))
-                .failsWithMessage(format("Access Denied: View owner 'bob' cannot create view that selects from default.%s", tableName));
+                .failsWithMessage(format("Access Denied: View owner does not have sufficient privileges: View owner 'bob' cannot create view that selects from default.%s", tableName));
 
         // Give Bob SELECT WITH GRANT OPTION on the underlying table
         aliceExecutor.executeQuery(format("REVOKE SELECT ON %s FROM bob", tableName));
