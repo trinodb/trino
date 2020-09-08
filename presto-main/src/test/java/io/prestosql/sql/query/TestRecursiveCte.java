2bcbe9b96faa923f1a8d26257c63008613c1b76e
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-09-08 14:06:57 +0200

    Fix class name to match code style

--- presto-main/src/test/java/io/prestosql/sql/query/TestRecursiveCte.java
+++ presto-main/src/test/java/io/prestosql/sql/query/TestRecursiveCte.java
@@ -21,7 +21,7 @@ import static io.prestosql.SystemSessionProperties.getMaxRecursionDepth;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class TestRecursiveCTE
+public class TestRecursiveCte
 {
     private QueryAssertions assertions;
 
