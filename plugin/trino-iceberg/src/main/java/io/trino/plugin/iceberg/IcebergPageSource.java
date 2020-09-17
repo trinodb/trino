501c630136e5194d9e4d7a6f9d7ea338cbaa560a
Pratham Desai <prathamd94@gmail.com>
2020-09-16 18:22:24 -0700

    Move some helper methods to IcebergUtil

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSource.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSource.java
@@ -13,51 +13,23 @@
  */
 package io.trino.plugin.iceberg;
 
-import io.airlift.slice.Slice;
-import io.airlift.slice.SliceUtf8;
 import io.trino.spi.Page;
 import io.trino.spi.TrinoException;
 import io.trino.spi.block.Block;
 import io.trino.spi.block.RunLengthEncodedBlock;
 import io.trino.spi.connector.ConnectorPageSource;
 import io.trino.spi.predicate.Utils;
-import io.trino.spi.type.DecimalType;
-import io.trino.spi.type.Decimals;
 import io.trino.spi.type.TimeZoneKey;
 import io.trino.spi.type.Type;
-import io.trino.spi.type.VarbinaryType;
-import io.trino.spi.type.VarcharType;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Throwables.throwIfInstanceOf;
-import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.plugin.iceberg.IcebergErrorCode.ICEBERG_BAD_DATA;
-import static io.trino.plugin.iceberg.IcebergErrorCode.ICEBERG_INVALID_PARTITION_VALUE;
-import static io.trino.plugin.iceberg.util.Timestamps.timestampTzFromMicros;
-import static io.trino.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
-import static io.trino.spi.type.BigintType.BIGINT;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.DateType.DATE;
-import static io.trino.spi.type.Decimals.isLongDecimal;
-import static io.trino.spi.type.Decimals.isShortDecimal;
-import static io.trino.spi.type.DoubleType.DOUBLE;
-import static io.trino.spi.type.IntegerType.INTEGER;
-import static io.trino.spi.type.RealType.REAL;
-import static io.trino.spi.type.TimeType.TIME_MICROS;
-import static io.trino.spi.type.TimestampType.TIMESTAMP_MICROS;
-import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
-import static io.trino.spi.type.Timestamps.PICOSECONDS_PER_MICROSECOND;
-import static java.lang.Double.parseDouble;
-import static java.lang.Float.floatToRawIntBits;
-import static java.lang.Float.parseFloat;
-import static java.lang.Long.parseLong;
-import static java.lang.String.format;
+import static io.trino.plugin.iceberg.IcebergUtil.deserializePartitionValue;
 import static java.util.Objects.requireNonNull;
 
 public class IcebergPageSource
@@ -178,78 +150,5 @@ public class IcebergPageSource
                 throwable.addSuppressed(e);
             }
         }
-    }
-
-    private static Object deserializePartitionValue(Type type, String valueString, String name, TimeZoneKey timeZoneKey)
-    {
-        if (valueString == null) {
-            return null;
-        }
-
-        try {
-            if (type.equals(BOOLEAN)) {
-                if (valueString.equalsIgnoreCase("true")) {
-                    return true;
-                }
-                if (valueString.equalsIgnoreCase("false")) {
-                    return false;
-                }
-                throw new IllegalArgumentException();
-            }
-            if (type.equals(INTEGER)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(BIGINT)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(REAL)) {
-                return (long) floatToRawIntBits(parseFloat(valueString));
-            }
-            if (type.equals(DOUBLE)) {
-                return parseDouble(valueString);
-            }
-            if (type.equals(DATE)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIME_MICROS)) {
-                return parseLong(valueString) * PICOSECONDS_PER_MICROSECOND;
-            }
-            if (type.equals(TIMESTAMP_MICROS)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIMESTAMP_TZ_MICROS)) {
-                return timestampTzFromMicros(parseLong(valueString), timeZoneKey);
-            }
-            if (type instanceof VarcharType) {
-                Slice value = utf8Slice(valueString);
-                VarcharType varcharType = (VarcharType) type;
-                if (!varcharType.isUnbounded() && SliceUtf8.countCodePoints(value) > varcharType.getBoundedLength()) {
-                    throw new IllegalArgumentException();
-                }
-                return value;
-            }
-            if (type.equals(VarbinaryType.VARBINARY)) {
-                return utf8Slice(valueString);
-            }
-            if (isShortDecimal(type) || isLongDecimal(type)) {
-                DecimalType decimalType = (DecimalType) type;
-                BigDecimal decimal = new BigDecimal(valueString);
-                decimal = decimal.setScale(decimalType.getScale(), BigDecimal.ROUND_UNNECESSARY);
-                if (decimal.precision() > decimalType.getPrecision()) {
-                    throw new IllegalArgumentException();
-                }
-                BigInteger unscaledValue = decimal.unscaledValue();
-                return isShortDecimal(type) ? unscaledValue.longValue() : Decimals.encodeUnscaledValue(unscaledValue);
-            }
-        }
-        catch (IllegalArgumentException e) {
-            throw new TrinoException(ICEBERG_INVALID_PARTITION_VALUE, format(
-                    "Invalid partition value '%s' for %s partition key: %s",
-                    valueString,
-                    type.getDisplayName(),
-                    name));
-        }
-        // Iceberg tables don't partition by non-primitive-type columns.
-        throw new TrinoException(GENERIC_INTERNAL_ERROR, "Invalid partition type " + type.toString());
     }
 }
