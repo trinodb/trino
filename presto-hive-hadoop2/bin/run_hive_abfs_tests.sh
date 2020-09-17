aokolnychyi/presto

Share more setup code between hive test scripts

presto-hive-hadoop2/bin/run_hive_abfs_tests.sh
@@ -12,43 +12,16 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="abfs://${ABFS_CONTAINER}@${ABFS_ACCOUNT}.dfs.core.windows.net/${test_directory}"
 
 # insert Azure credentials
 # TODO replace core-site.xml.abfs-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.abfs-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%ABFS_ACCESS_KEY%|${ABFS_ACCESS_KEY}|g" \
-    -e "s|%ABFS_ACCOUNT%|${ABFS_ACCOUNT}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.abfs-template ABFS_ACCESS_KEY ABFS_ACCOUNT
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-metastore
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "abfs://${ABFS_CONTAINER}@${ABFS_ACCOUNT}.dfs.core.windows.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 

presto-hive-hadoop2/bin/run_hive_adl_tests.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail -x
 
-. ${BASH_SOURCE%/*}/common.sh
+. "${BASH_SOURCE%/*}/common.sh"
 
 test -v ADL_NAME
 test -v ADL_CLIENT_ID
@@ -13,44 +13,17 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="adl://${ADL_NAME}.azuredatalakestore.net/${test_directory}"
 
 # insert Azure credentials
 # TODO replace core-site.xml.adl-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.adl-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%ADL_CLIENT_ID%|${ADL_CLIENT_ID}|g" \
-    -e "s|%ADL_CREDENTIAL%|${ADL_CREDENTIAL}|g" \
-    -e "s|%ADL_REFRESH_URL%|${ADL_REFRESH_URL}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.adl-template \
+    ADL_CLIENT_ID ADL_CREDENTIAL ADL_REFRESH_URL
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-metastore
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "adl://${ADL_NAME}.azuredatalakestore.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 

presto-hive-hadoop2/bin/run_hive_wasb_tests.sh
@@ -12,43 +12,16 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="wasb://${WASB_CONTAINER}@${WASB_ACCOUNT}.blob.core.windows.net/${test_directory}"
 
 # insert Azure credentials
 # TODO replace core-site.xml.wasb-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.wasb-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%WASB_ACCESS_KEY%|${WASB_ACCESS_KEY}|g" \
-    -e "s|%WASB_ACCOUNT%|${WASB_ACCOUNT}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.wasb-template WASB_ACCESS_KEY WASB_ACCOUNT
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-metastore
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "wasb://${WASB_CONTAINER}@${WASB_ACCOUNT}.blob.core.windows.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 

trinodb/trino

presto-hive-hadoop2/bin/run_hive_abfs_tests.sh
@@ -12,43 +12,16 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="abfs://${ABFS_CONTAINER}@${ABFS_ACCOUNT}.dfs.core.windows.net/${test_directory}"
 
 # insert Azure credentials
-# TODO replace core-site.xml.abfs-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.abfs-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%ABFS_ACCESS_KEY%|${ABFS_ACCESS_KEY}|g" \
-    -e "s|%ABFS_ACCOUNT%|${ABFS_ACCOUNT}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.abfs-template \
+    ABFS_ACCESS_KEY ABFS_ACCOUNT
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-server2
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "abfs://${ABFS_CONTAINER}@${ABFS_ACCOUNT}.dfs.core.windows.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 

presto-hive-hadoop2/bin/run_hive_adl_tests.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail -x
 
-. ${BASH_SOURCE%/*}/common.sh
+. "${BASH_SOURCE%/*}/common.sh"
 
 test -v ADL_NAME
 test -v ADL_CLIENT_ID
@@ -13,44 +13,16 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="adl://${ADL_NAME}.azuredatalakestore.net/${test_directory}"
 
 # insert Azure credentials
-# TODO replace core-site.xml.adl-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.adl-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%ADL_CLIENT_ID%|${ADL_CLIENT_ID}|g" \
-    -e "s|%ADL_CREDENTIAL%|${ADL_CREDENTIAL}|g" \
-    -e "s|%ADL_REFRESH_URL%|${ADL_REFRESH_URL}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.adl-template \
+    ADL_CLIENT_ID ADL_CREDENTIAL ADL_REFRESH_URL
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-server2
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "adl://${ADL_NAME}.azuredatalakestore.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 

presto-hive-hadoop2/bin/run_hive_wasb_tests.sh
@@ -12,43 +12,16 @@ cleanup_hadoop_docker_containers
 start_hadoop_docker_containers
 
 test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
-test_root="wasb://${WASB_CONTAINER}@${WASB_ACCOUNT}.blob.core.windows.net/${test_directory}"
 
 # insert Azure credentials
-# TODO replace core-site.xml.wasb-template with apply-site-xml-override.sh
-exec_in_hadoop_master_container cp /docker/files/core-site.xml.wasb-template /etc/hadoop/conf/core-site.xml
-exec_in_hadoop_master_container sed -i \
-    -e "s|%WASB_ACCESS_KEY%|${WASB_ACCESS_KEY}|g" \
-    -e "s|%WASB_ACCOUNT%|${WASB_ACCOUNT}|g" \
-    /etc/hadoop/conf/core-site.xml
+deploy_core_site_xml core-site.xml.wasb-template \
+    WASB_ACCESS_KEY WASB_ACCOUNT
 
 # restart hive-server2 to apply changes in core-site.xml
-docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+exec_in_hadoop_master_container supervisorctl restart hive-server2
 retry check_hadoop
 
-# create test table
-table_path="${test_root}/presto_test_external_fs/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
-
-table_path="${test_root}/presto_test_external_fs_with_header/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='1')"
-
-table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
-exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
-exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
-exec_in_hadoop_master_container /usr/bin/hive -e "
-    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
-    STORED AS TEXTFILE
-    LOCATION '${table_path}'
-    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
+create_test_tables "wasb://${WASB_CONTAINER}@${WASB_ACCOUNT}.blob.core.windows.net/${test_directory}"
 
 stop_unnecessary_hadoop_services
 