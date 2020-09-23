7c4ea67c69aa0487f75fc9a992359fa53c397823
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-09-23 12:45:35 +0200

    Add SAP HANA basic plugin test

---
+++ plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/TestSapHanaPlugin.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.saphana;
+
+import com.google.common.collect.ImmutableMap;
+import io.prestosql.spi.Plugin;
+import io.prestosql.spi.connector.ConnectorFactory;
+import io.prestosql.testing.TestingConnectorContext;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+public class TestSapHanaPlugin
+{
+    @Test
+    public void testLicenseRequired()
+    {
+        Plugin plugin = new SapHanaPlugin();
+        ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
+        Assertions.assertThatThrownBy(() -> factory.create("test", ImmutableMap.of("connection-url", "test"), new TestingConnectorContext()))
+                .isInstanceOf(RuntimeException.class)
+                .hasToString("com.starburstdata.presto.license.PrestoLicenseException: Couldn't find a valid license, please make sure the license file path is correct");
+    }
+
+    @Test
+    public void testCreateConnector()
+    {
+        Plugin plugin = new TestingSapHanaPlugin();
+        ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
+        factory.create("test", ImmutableMap.of("connection-url", "test"), new TestingConnectorContext());
+    }
+}
