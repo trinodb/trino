7b3a219e9d1013e04dc8342a45f8a34dfeb8e0fd
s2lomon <s2lomon@gmail.com>
2020-09-23 15:07:38 +0200

    Add OAuth2 support to JDBC
    Add ExternalAuthorizer to JDBC that handles the "Bearer"
    WWW-Authenticate challenge and blocks until the external
    authentication flow is complete.

--- client/trino-jdbc/src/main/java/io/trino/jdbc/TrinoDriverUri.java
+++ client/trino-jdbc/src/main/java/io/trino/jdbc/TrinoDriverUri.java
@@ -13,24 +13,32 @@
  */
 package io.trino.jdbc;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.net.HostAndPort;
 import io.trino.client.ClientException;
 import io.trino.client.ClientSelectedRole;
+import io.trino.client.auth.external.DesktopBrowserRedirectHandler;
+import io.trino.client.auth.external.ExternalAuthenticator;
+import io.trino.client.auth.external.HttpTokenPoller;
+import io.trino.client.auth.external.RedirectHandler;
+import io.trino.client.auth.external.TokenPoller;
 import okhttp3.OkHttpClient;
 
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static io.trino.client.KerberosUtil.defaultCredentialCachePath;
@@ -47,6 +55,8 @@ import static io.trino.jdbc.ConnectionProperties.APPLICATION_NAME_PREFIX;
 import static io.trino.jdbc.ConnectionProperties.CLIENT_INFO;
 import static io.trino.jdbc.ConnectionProperties.CLIENT_TAGS;
 import static io.trino.jdbc.ConnectionProperties.DISABLE_COMPRESSION;
+import static io.trino.jdbc.ConnectionProperties.EXTERNAL_AUTHENTICATION;
+import static io.trino.jdbc.ConnectionProperties.EXTERNAL_AUTHENTICATION_TIMEOUT;
 import static io.trino.jdbc.ConnectionProperties.EXTRA_CREDENTIALS;
 import static io.trino.jdbc.ConnectionProperties.HTTP_PROXY;
 import static io.trino.jdbc.ConnectionProperties.KERBEROS_CONFIG_PATH;
@@ -90,6 +100,7 @@ public final class TrinoDriverUri
 
     private static final Splitter QUERY_SPLITTER = Splitter.on('&').omitEmptyStrings();
     private static final Splitter ARG_SPLITTER = Splitter.on('=').limit(2);
+    private static final AtomicReference<RedirectHandler> REDIRECT_HANDLER = new AtomicReference<>(new DesktopBrowserRedirectHandler());
 
     private final HostAndPort address;
     private final URI uri;
@@ -285,6 +296,24 @@ public final class TrinoDriverUri
                 }
                 builder.addInterceptor(tokenAuth(ACCESS_TOKEN.getValue(properties).get()));
             }
+
+            if (EXTERNAL_AUTHENTICATION.getValue(properties).orElse(false)) {
+                if (!useSecureConnection) {
+                    throw new SQLException("Authentication using external authorization requires SSL to be enabled");
+                }
+
+                // create HTTP client that shares the same settings, but without the external authenticator
+                TokenPoller poller = new HttpTokenPoller(builder.build());
+
+                Duration timeout = EXTERNAL_AUTHENTICATION_TIMEOUT.getValue(properties)
+                        .map(value -> Duration.ofMillis(value.toMillis()))
+                        .orElse(Duration.ofMinutes(2));
+
+                ExternalAuthenticator authenticator = new ExternalAuthenticator(REDIRECT_HANDLER.get(), poller, timeout);
+
+                builder.authenticator(authenticator);
+                builder.addInterceptor(authenticator);
+            }
         }
         catch (ClientException e) {
             throw new SQLException(e.getMessage(), e);
@@ -435,5 +464,11 @@ public final class TrinoDriverUri
         for (ConnectionProperty<?> property : ConnectionProperties.allProperties()) {
             property.validate(connectionProperties);
         }
+    }
+
+    @VisibleForTesting
+    static void setRedirectHandler(RedirectHandler handler)
+    {
+        REDIRECT_HANDLER.set(requireNonNull(handler, "handler is null"));
     }
 }
