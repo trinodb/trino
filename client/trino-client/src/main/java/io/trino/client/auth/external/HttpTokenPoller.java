693cfb624d7a72c12a349ea4157b66bc8a43b93c
s2lomon <s2lomon@gmail.com>
2020-09-23 15:07:38 +0200

    Add OAuth2 support to JDBC
    Add ExternalAuthorizer to JDBC that handles the "Bearer"
    WWW-Authenticate challenge and blocks until the external
    authentication flow is complete.

---
+++ client/trino-client/src/main/java/io/trino/client/auth/external/HttpTokenPoller.java
@@ -0,0 +1,148 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.client.auth.external;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.airlift.json.JsonCodec;
+import io.trino.client.JsonResponse;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.FailsafeException;
+import net.jodah.failsafe.RetryPolicy;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.time.Duration;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.net.HttpHeaders.USER_AGENT;
+import static io.airlift.json.JsonCodec.jsonCodec;
+import static io.trino.client.JsonResponse.execute;
+import static java.lang.String.format;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.HTTP_UNAVAILABLE;
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.util.Objects.requireNonNull;
+
+public class HttpTokenPoller
+        implements TokenPoller
+{
+    private static final JsonCodec<TokenPollRepresentation> TOKEN_POLL_CODEC = jsonCodec(TokenPollRepresentation.class);
+    private static final String USER_AGENT_VALUE = "TrinoTokenPoller/" +
+            firstNonNull(HttpTokenPoller.class.getPackage().getImplementationVersion(), "unknown");
+
+    private final OkHttpClient client;
+
+    public HttpTokenPoller(OkHttpClient client)
+    {
+        this.client = requireNonNull(client, "client is null");
+    }
+
+    @Override
+    public TokenPollResult pollForToken(URI tokenUri, Duration timeout)
+    {
+        try {
+            return Failsafe.with(new RetryPolicy<TokenPollResult>()
+                    .withMaxAttempts(-1)
+                    .withMaxDuration(timeout)
+                    .withBackoff(100, 500, MILLIS)
+                    .handle(IOException.class))
+                    .get(() -> executePoll(createRequest(tokenUri)));
+        }
+        catch (FailsafeException e) {
+            if (e.getCause() instanceof IOException) {
+                throw new UncheckedIOException((IOException) e.getCause());
+            }
+            throw e;
+        }
+    }
+
+    private static Request createRequest(URI tokenUri)
+    {
+        HttpUrl url = HttpUrl.get(tokenUri);
+        if (url == null) {
+            throw new IllegalArgumentException("Invalid token URI: " + tokenUri);
+        }
+
+        return new Request.Builder()
+                .url(url)
+                .addHeader(USER_AGENT, USER_AGENT_VALUE)
+                .build();
+    }
+
+    private TokenPollResult executePoll(Request request)
+            throws IOException
+    {
+        JsonResponse<TokenPollRepresentation> response = executeRequest(request);
+
+        if ((response.getStatusCode() == HTTP_OK) && response.hasValue()) {
+            return response.getValue().toResult();
+        }
+
+        String message = format("Request to %s failed: %s [Error: %s]", request.url(), response, response.getResponseBody());
+
+        if (response.getStatusCode() == HTTP_UNAVAILABLE) {
+            throw new IOException(message);
+        }
+
+        return TokenPollResult.failed(message);
+    }
+
+    private JsonResponse<TokenPollRepresentation> executeRequest(Request request)
+            throws IOException
+    {
+        try {
+            return execute(TOKEN_POLL_CODEC, client, request);
+        }
+        catch (UncheckedIOException e) {
+            throw e.getCause();
+        }
+    }
+
+    public static class TokenPollRepresentation
+    {
+        private final String token;
+        private final URI nextUri;
+        private final String error;
+
+        @JsonCreator
+        public TokenPollRepresentation(
+                @JsonProperty("token") String token,
+                @JsonProperty("nextUri") URI nextUri,
+                @JsonProperty("error") String error)
+        {
+            this.token = token;
+            this.nextUri = nextUri;
+            this.error = error;
+        }
+
+        TokenPollResult toResult()
+        {
+            if (token != null) {
+                return TokenPollResult.successful(new Token(token));
+            }
+            if (error != null) {
+                return TokenPollResult.failed(error);
+            }
+            if (nextUri != null) {
+                return TokenPollResult.pending(nextUri);
+            }
+            return TokenPollResult.failed("Failed to poll for token. No fields set in response.");
+        }
+    }
+}
