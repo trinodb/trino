8a07138fd1ad7f0f8ce10b3e06356d52bd844e53
David Phillips <david@acz.org>
2020-09-23 18:28:40 -0700

    Extract IcebergTableName from IcebergTableHandle

--- presto-iceberg/src/test/java/io/prestosql/plugin/iceberg/TestIcebergTableName.java
+++ presto-iceberg/src/test/java/io/prestosql/plugin/iceberg/TestIcebergTableName.java
@@ -13,7 +13,6 @@
  */
 package io.prestosql.plugin.iceberg;
 
-import io.prestosql.spi.connector.SchemaTableName;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
@@ -22,10 +21,8 @@ import static io.prestosql.spi.StandardErrorCode.NOT_SUPPORTED;
 import static io.prestosql.testing.assertions.PrestoExceptionAssert.assertPrestoExceptionThrownBy;
 import static org.testng.Assert.assertEquals;
 
-public class TestIcebergTableHandle
+public class TestIcebergTableName
 {
-    private static final String SCHEMA = "test";
-
     @Test
     public void testFrom()
     {
@@ -40,31 +37,25 @@ public class TestIcebergTableHandle
         assertFrom("abc$history", "abc", TableType.HISTORY, Optional.empty());
         assertFrom("abc$snapshots", "abc", TableType.SNAPSHOTS, Optional.empty());
 
-        assertInvalid("abc@xyz", "Invalid Iceberg table name: test.abc@xyz");
-        assertInvalid("abc$what", "Invalid Iceberg table name (unknown type 'what'): test.abc$what");
-        assertInvalid("abc@123$data@456", "Invalid Iceberg table name (cannot specify two @ versions): test.abc@123$data@456");
-        assertInvalid("abc@123$snapshots", "Invalid Iceberg table name (cannot use @ version with table type 'SNAPSHOTS'): test.abc@123$snapshots");
-        assertInvalid("abc$snapshots@456", "Invalid Iceberg table name (cannot use @ version with table type 'SNAPSHOTS'): test.abc$snapshots@456");
+        assertInvalid("abc@xyz", "Invalid Iceberg table name: abc@xyz");
+        assertInvalid("abc$what", "Invalid Iceberg table name (unknown type 'what'): abc$what");
+        assertInvalid("abc@123$data@456", "Invalid Iceberg table name (cannot specify two @ versions): abc@123$data@456");
+        assertInvalid("abc@123$snapshots", "Invalid Iceberg table name (cannot use @ version with table type 'SNAPSHOTS'): abc@123$snapshots");
+        assertInvalid("abc$snapshots@456", "Invalid Iceberg table name (cannot use @ version with table type 'SNAPSHOTS'): abc$snapshots@456");
     }
 
     private static void assertInvalid(String inputName, String message)
     {
-        assertPrestoExceptionThrownBy(() -> handleFrom(inputName))
+        assertPrestoExceptionThrownBy(() -> IcebergTableName.from(inputName))
                 .hasErrorCode(NOT_SUPPORTED)
                 .hasMessage(message);
     }
 
     private static void assertFrom(String inputName, String tableName, TableType tableType, Optional<Long> snapshotId)
     {
-        IcebergTableHandle handle = handleFrom(inputName);
-        assertEquals(handle.getSchemaName(), SCHEMA);
-        assertEquals(handle.getTableName(), tableName);
-        assertEquals(handle.getTableType(), tableType);
-        assertEquals(handle.getSnapshotId(), snapshotId);
-    }
-
-    private static IcebergTableHandle handleFrom(String tableName)
-    {
-        return IcebergTableHandle.from(new SchemaTableName(SCHEMA, tableName));
+        IcebergTableName name = IcebergTableName.from(inputName);
+        assertEquals(name.getTableName(), tableName);
+        assertEquals(name.getTableType(), tableType);
+        assertEquals(name.getSnapshotId(), snapshotId);
     }
 }
