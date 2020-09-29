424a83e7185c0a25b9961d3966e457f6474a5b3b
Raphael Claude <r.claude@criteo.com>
2020-09-29 16:08:40 +0200

    Implement getCatalogSessionProperties
    What: Add support for setting default catalog session properties in FileSessionPropertyManager and DbSessionPropertyManager
    How: Use the convention that the first dot in a property name is a separator between a catalog name and a the catalog specific property name

--- presto-session-property-managers/src/test/java/io/prestosql/plugin/session/file/TestFileSessionPropertyManager.java
+++ presto-session-property-managers/src/test/java/io/prestosql/plugin/session/file/TestFileSessionPropertyManager.java
@@ -32,14 +32,15 @@ public class TestFileSessionPropertyManager
         extends AbstractTestSessionPropertyManager
 {
     @Override
-    protected void assertProperties(Map<String, String> properties, SessionMatchSpec... spec)
+    protected void assertProperties(Map<String, String> systemProperties, Map<String, Map<String, String>> catalogProperties, SessionMatchSpec... specs)
             throws IOException
     {
         try (TempFile tempFile = new TempFile()) {
             Path configurationFile = tempFile.path();
-            Files.write(configurationFile, CODEC.toJsonBytes(Arrays.asList(spec)));
+            Files.write(configurationFile, CODEC.toJsonBytes(Arrays.asList(specs)));
             SessionPropertyConfigurationManager manager = new FileSessionPropertyManager(new FileSessionPropertyManagerConfig().setConfigFile(configurationFile.toFile()));
-            assertEquals(manager.getSystemSessionProperties(CONTEXT), properties);
+            assertEquals(manager.getSystemSessionProperties(CONTEXT), systemProperties);
+            assertEquals(manager.getCatalogSessionProperties(CONTEXT), catalogProperties);
         }
     }
 }
