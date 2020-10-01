ff805515ee698b842ede698deea727a10091930a
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-10-01 16:53:50 +0200

    Fix isCorrectlyPushedDown with session

--- presto-testing/src/main/java/io/prestosql/testing/AbstractTestingPrestoClient.java
+++ presto-testing/src/main/java/io/prestosql/testing/AbstractTestingPrestoClient.java
@@ -22,6 +22,7 @@ import io.prestosql.client.Column;
 import io.prestosql.client.QueryError;
 import io.prestosql.client.QueryStatusInfo;
 import io.prestosql.client.StatementClient;
+import io.prestosql.connector.CatalogName;
 import io.prestosql.metadata.MetadataUtil;
 import io.prestosql.metadata.QualifiedObjectName;
 import io.prestosql.metadata.QualifiedTablePrefix;
@@ -125,6 +126,11 @@ public abstract class AbstractTestingPrestoClient<T>
     {
         ImmutableMap.Builder<String, String> properties = ImmutableMap.builder();
         properties.putAll(session.getSystemProperties());
+        for (Entry<CatalogName, Map<String, String>> catalogAndConnectorProperties : session.getConnectorProperties().entrySet()) {
+            for (Entry<String, String> connectorProperties : catalogAndConnectorProperties.getValue().entrySet()) {
+                properties.put(catalogAndConnectorProperties.getKey() + "." + connectorProperties.getKey(), connectorProperties.getValue());
+            }
+        }
         for (Entry<String, Map<String, String>> connectorProperties : session.getUnprocessedCatalogProperties().entrySet()) {
             for (Entry<String, String> entry : connectorProperties.getValue().entrySet()) {
                 properties.put(connectorProperties.getKey() + "." + entry.getKey(), entry.getValue());
