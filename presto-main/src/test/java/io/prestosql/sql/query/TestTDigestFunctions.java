45163d95969293f8be90e6559b23461e60a8bfde
kasiafi <30203062+kasiafi@users.noreply.github.com>
2020-10-03 20:57:12 +0200

    Improve efficiency of T-Digest functions
    Use `TDigest.valuesAt()` method to compute values at multiple quantiles
    in a single pass of the T-Digest structure instead of multiple
    calls to `TDigest.valueAt()` method.
    Applies to:
    - approx_percentile() aggregation,
    - values_at_quantiles() function.
    NOTE: In function values_at_quantiles(tdigest, array<double>),
    it is required that the input percentile array be sorted in ascending order.
    The similar function values_at_quantiles(qdigest, array<double>) allows
    arbitrary order of percentiles.
    In approx_percentile() aggregation ordering of percentiles is not required.

--- presto-main/src/test/java/io/prestosql/sql/query/TestTDigestFunctions.java
+++ presto-main/src/test/java/io/prestosql/sql/query/TestTDigestFunctions.java
@@ -24,6 +24,7 @@ import java.util.Random;
 
 import static java.lang.Math.round;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TestTDigestFunctions
 {
@@ -78,6 +79,11 @@ public class TestTDigestFunctions
                 "SELECT values_at_quantiles(tdigest_agg(d), ARRAY[0.0001e0, 0.75e0, 0.85e0]) " +
                         "FROM (VALUES 0.1e0, 0.1e0, 0.1e0, 0.1e0, 10e0) T(d)"))
                 .matches("VALUES ARRAY[0.1e0, 0.1e0, 10.0e0]");
+
+        assertThatThrownBy(() -> assertions.query(
+                "SELECT values_at_quantiles(tdigest_agg(d), ARRAY[1e0, 0e0]) " +
+                        "FROM (VALUES 0.1e0) T(d)"))
+                .hasMessage("percentiles must be sorted in increasing order");
     }
 
     @Test
