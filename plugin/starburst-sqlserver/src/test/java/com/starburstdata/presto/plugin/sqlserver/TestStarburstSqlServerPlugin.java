7a537d4bf8091d97a2b0caee2a62c9870cea4f33
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-10-08 14:51:24 +0200

    Add StarburstSqlServerPlugin test

---
+++ plugin/starburst-sqlserver/src/test/java/com/starburstdata/presto/plugin/sqlserver/TestStarburstSqlServerPlugin.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.sqlserver;
+
+import com.google.common.collect.ImmutableMap;
+import io.prestosql.spi.Plugin;
+import io.prestosql.spi.connector.ConnectorFactory;
+import io.prestosql.testing.TestingConnectorContext;
+import org.testng.annotations.Test;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+public class TestStarburstSqlServerPlugin
+{
+    @Test
+    public void testCreateConnector()
+    {
+        Plugin plugin = new StarburstSqlServerPlugin();
+        ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
+        factory.create("test", ImmutableMap.of("connection-url", "test"), new TestingConnectorContext());
+    }
+}
