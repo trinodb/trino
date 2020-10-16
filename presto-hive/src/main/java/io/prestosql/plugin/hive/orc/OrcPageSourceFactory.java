faeaf786d5b561dcc02ede9893158885adcd7a81
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-10-16 12:13:44 +0200

    Improve error message when wrong ACID schema

--- presto-hive/src/main/java/io/prestosql/plugin/hive/orc/OrcPageSourceFactory.java
+++ presto-hive/src/main/java/io/prestosql/plugin/hive/orc/OrcPageSourceFactory.java
@@ -64,6 +64,7 @@ import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.Maps.uniqueIndex;
 import static io.prestosql.memory.context.AggregatedMemoryContext.newSimpleAggregatedMemoryContext;
@@ -419,8 +420,17 @@ public class OrcPageSourceFactory
     static void verifyAcidSchema(OrcReader orcReader, Path path)
     {
         OrcColumn rootColumn = orcReader.getRootColumn();
-        if (rootColumn.getNestedColumns().size() != 6) {
-            throw new PrestoException(HIVE_BAD_DATA, "ORC ACID file should have 6 columns: " + path);
+        List<OrcColumn> nestedColumns = rootColumn.getNestedColumns();
+        if (nestedColumns.size() != 6) {
+            throw new PrestoException(
+                    HIVE_BAD_DATA,
+                    format(
+                            "ORC ACID file should have 6 columns, found %s %s in %s",
+                            nestedColumns.size(),
+                            nestedColumns.stream()
+                                    .map(column -> format("%s (%s)", column.getColumnName(), column.getColumnType()))
+                                    .collect(toImmutableList()),
+                            path));
         }
         verifyAcidColumn(orcReader, 0, ACID_COLUMN_OPERATION, INT, path);
         verifyAcidColumn(orcReader, 1, ACID_COLUMN_ORIGINAL_TRANSACTION, LONG, path);
