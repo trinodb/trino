71f3669ed8ff705e1a757f222316c13bbce70cbb
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-10-16 20:47:28 +0200

    Avoid using deprecated method

--- presto-base-jdbc/src/main/java/io/prestosql/plugin/jdbc/JdbcConnectorFactory.java
+++ presto-base-jdbc/src/main/java/io/prestosql/plugin/jdbc/JdbcConnectorFactory.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
-import static io.airlift.configuration.ConfigurationAwareModules.combine;
+import static io.airlift.configuration.ConfigurationAwareModule.combine;
 import static java.util.Objects.requireNonNull;
 
 public class JdbcConnectorFactory
