c9a52dd105e6bb81283f551e741f8427e6c2a599
tooptoop4 <33283496+tooptoop4@users.noreply.github.com>
2020-10-17 02:36:03 +0100

    fix count() pushdown

--- presto-oracle/src/main/java/io/prestosql/plugin/oracle/OracleClient.java
+++ presto-oracle/src/main/java/io/prestosql/plugin/oracle/OracleClient.java
@@ -175,7 +175,7 @@ public class OracleClient
         requireNonNull(oracleConfig, "oracle config is null");
         this.synonymsEnabled = oracleConfig.isSynonymsEnabled();
 
-        JdbcTypeHandle bigintTypeHandle = new JdbcTypeHandle(Types.BIGINT, Optional.of("bigint"), 0, 0, Optional.empty(), Optional.empty());
+        JdbcTypeHandle bigintTypeHandle = new JdbcTypeHandle(Types.NUMERIC, Optional.of("decimal"), 38, 0, Optional.empty(), Optional.empty());
         this.aggregateFunctionRewriter = new AggregateFunctionRewriter(
                 this::quoted,
                 ImmutableSet.<AggregateFunctionRule>builder()
@@ -312,7 +312,14 @@ public class OracleClient
                 int scale = max(decimalDigits, 0);
                 Optional<Integer> numberDefaultScale = getNumberDefaultScale(session);
                 RoundingMode roundingMode = getNumberRoundingMode(session);
-                if (precision < scale) {
+                if (precision == 38 && scale == 0) {
+                    return Optional.of(ColumnMapping.longMapping(
+                            BIGINT,
+                            ResultSet::getLong,
+                            bigintWriteFunction(),
+                            OracleClient::fullPushdownIfSupported));
+                }
+                else if (precision < scale) {
                     if (roundingMode == RoundingMode.UNNECESSARY) {
                         break;
                     }
