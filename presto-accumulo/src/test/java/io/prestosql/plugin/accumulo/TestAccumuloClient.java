addd62324d092f7cc67e42c7326cc6260f4dc61e
Adam J. Shook <shook@datacatessen.com>
2020-10-19 13:39:29 +0000

    Use Accumulo in Docker for unit tests

--- presto-accumulo/src/test/java/io/prestosql/plugin/accumulo/TestAccumuloClient.java
+++ presto-accumulo/src/test/java/io/prestosql/plugin/accumulo/TestAccumuloClient.java
@@ -48,7 +48,7 @@ public class TestAccumuloClient
                 .setUsername("root")
                 .setPassword("secret");
 
-        Connector connector = AccumuloQueryRunner.getAccumuloConnector();
+        Connector connector = TestingAccumuloServer.getInstance().getConnector();
         config.setZooKeepers(connector.getInstance().getZooKeepers());
         zooKeeperMetadataManager = new ZooKeeperMetadataManager(config, new InternalTypeManager(createTestMetadataManager(), new TypeOperators()));
         client = new AccumuloClient(connector, config, zooKeeperMetadataManager, new AccumuloTableManager(connector), new IndexLookup(connector, new ColumnCardinalityCache(connector, config)));
