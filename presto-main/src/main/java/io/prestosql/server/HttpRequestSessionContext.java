d66daa9a40a779273158751ed51005c31dd69ebc
brandboat <brandboat@gmail.com>
2020-10-28 22:59:36 +0800

    Remove unused var in presto-main

--- presto-main/src/main/java/io/prestosql/server/HttpRequestSessionContext.java
+++ presto-main/src/main/java/io/prestosql/server/HttpRequestSessionContext.java
@@ -17,7 +17,6 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import io.airlift.log.Logger;
 import io.airlift.units.DataSize;
 import io.airlift.units.Duration;
 import io.prestosql.Session.ResourceEstimateBuilder;
@@ -80,8 +79,6 @@ import static java.util.Objects.requireNonNull;
 public final class HttpRequestSessionContext
         implements SessionContext
 {
-    private static final Logger log = Logger.get(HttpRequestSessionContext.class);
-
     private static final Splitter DOT_SPLITTER = Splitter.on('.');
     public static final String AUTHENTICATED_IDENTITY = "presto.authenticated-identity";
 
--- presto-main/src/test/java/io/prestosql/sql/planner/iterative/rule/TestSwapAdjacentWindowsBySpecifications.java
+++ presto-main/src/test/java/io/prestosql/sql/planner/iterative/rule/TestSwapAdjacentWindowsBySpecifications.java
@@ -22,7 +22,6 @@ import io.prestosql.sql.planner.iterative.rule.test.BaseRuleTest;
 import io.prestosql.sql.planner.plan.WindowNode;
 import io.prestosql.sql.tree.QualifiedName;
 import io.prestosql.sql.tree.SymbolReference;
-import io.prestosql.sql.tree.Window;
 import io.prestosql.sql.tree.WindowFrame;
 import org.testng.annotations.Test;
 
@@ -92,9 +91,6 @@ public class TestSwapAdjacentWindowsBySpecifications
         ExpectedValueProvider<WindowNode.Specification> specificationA = specification(ImmutableList.of(columnAAlias), ImmutableList.of(), ImmutableMap.of());
         ExpectedValueProvider<WindowNode.Specification> specificationAB = specification(ImmutableList.of(columnAAlias, columnBAlias), ImmutableList.of(), ImmutableMap.of());
 
-        Optional<Window> windowAB = Optional.of(new Window(ImmutableList.of(new SymbolReference("a"), new SymbolReference("b")), Optional.empty(), Optional.empty()));
-        Optional<Window> windowA = Optional.of(new Window(ImmutableList.of(new SymbolReference("a")), Optional.empty(), Optional.empty()));
-
         tester().assertThat(new GatherAndMergeWindows.SwapAdjacentWindowsBySpecifications(0))
                 .on(p ->
                         p.window(new WindowNode.Specification(
@@ -121,8 +117,6 @@ public class TestSwapAdjacentWindowsBySpecifications
     @Test
     public void dependentWindowsAreNotReordered()
     {
-        Optional<Window> windowA = Optional.of(new Window(ImmutableList.of(new SymbolReference("a")), Optional.empty(), Optional.empty()));
-
         tester().assertThat(new GatherAndMergeWindows.SwapAdjacentWindowsBySpecifications(0))
                 .on(p ->
                         p.window(new WindowNode.Specification(
