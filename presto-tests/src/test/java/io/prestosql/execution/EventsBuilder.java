e16c81fa62b8108b2ac11bcc4f12cba5a317b963
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-11-05 18:30:03 +0100

    Add query filtering logic to EventsBuilder

--- presto-tests/src/test/java/io/prestosql/execution/EventsBuilder.java
+++ presto-tests/src/test/java/io/prestosql/execution/EventsBuilder.java
@@ -16,19 +16,35 @@ package io.prestosql.execution;
 import com.google.common.collect.ImmutableList;
 import io.prestosql.spi.eventlistener.QueryCompletedEvent;
 import io.prestosql.spi.eventlistener.QueryCreatedEvent;
+import io.prestosql.spi.eventlistener.QueryMetadata;
 import io.prestosql.spi.eventlistener.SplitCompletedEvent;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static com.google.common.base.Predicates.alwaysTrue;
+import static java.util.Objects.requireNonNull;
 
 class EventsBuilder
 {
+    private final Predicate<QueryMetadata> queryFilter;
     private ImmutableList.Builder<QueryCreatedEvent> queryCreatedEvents;
     private ImmutableList.Builder<QueryCompletedEvent> queryCompletedEvents;
     private ImmutableList.Builder<SplitCompletedEvent> splitCompletedEvents;
 
     private CountDownLatch eventsLatch;
+
+    public EventsBuilder()
+    {
+        this(alwaysTrue());
+    }
+
+    public EventsBuilder(Predicate<QueryMetadata> queryFilter)
+    {
+        this.queryFilter = requireNonNull(queryFilter, "filter is null");
+    }
 
     public synchronized void initialize(int numEvents)
     {
@@ -47,12 +63,18 @@ class EventsBuilder
 
     public synchronized void addQueryCreated(QueryCreatedEvent event)
     {
+        if (!queryFilter.test(event.getMetadata())) {
+            return;
+        }
         queryCreatedEvents.add(event);
         eventsLatch.countDown();
     }
 
     public synchronized void addQueryCompleted(QueryCompletedEvent event)
     {
+        if (!queryFilter.test(event.getMetadata())) {
+            return;
+        }
         queryCompletedEvents.add(event);
         eventsLatch.countDown();
     }
