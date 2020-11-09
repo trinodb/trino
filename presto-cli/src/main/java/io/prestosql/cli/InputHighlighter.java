299f6d90835d508f4b7dabf3c26c7e1c3e911db3
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2020-11-09 09:50:13 +0100

    Update JLine to 3.17.1

--- presto-cli/src/main/java/io/prestosql/cli/InputHighlighter.java
+++ presto-cli/src/main/java/io/prestosql/cli/InputHighlighter.java
@@ -24,6 +24,7 @@ import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.prestosql.cli.Console.STATEMENT_DELIMITERS;
@@ -87,6 +88,12 @@ public class InputHighlighter
 
         return builder.toAttributedString();
     }
+
+    @Override
+    public void setErrorPattern(Pattern pattern) {}
+
+    @Override
+    public void setErrorIndex(int i) {}
 
     private static boolean isKeyword(String text)
     {
