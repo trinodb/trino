d63bde4deda6ac649d2d48112b99a6143e1d407e
Dain Sundstrom <dain@iq80.com>
2020-11-08 23:05:34 -0800

    Add LdapAuthenticatorClient abstraction

--- presto-password-authenticators/src/main/java/io/prestosql/plugin/password/ldap/LdapAuthenticator.java
+++ presto-password-authenticators/src/main/java/io/prestosql/plugin/password/ldap/LdapAuthenticator.java
@@ -18,11 +18,8 @@ import com.google.common.base.CharMatcher;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import io.airlift.log.Logger;
-import io.airlift.security.pem.PemReader;
 import io.prestosql.plugin.password.Credential;
 import io.prestosql.spi.classloader.ThreadContextClassLoader;
 import io.prestosql.spi.security.AccessDeniedException;
@@ -30,40 +27,18 @@ import io.prestosql.spi.security.BasicPrincipal;
 import io.prestosql.spi.security.PasswordAuthenticator;
 
 import javax.inject.Inject;
-import javax.naming.AuthenticationException;
-import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.SearchControls;
-import javax.naming.directory.SearchResult;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
 import java.security.Principal;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Throwables.throwIfInstanceOf;
 import static com.google.common.collect.Iterables.getOnlyElement;
-import static io.prestosql.plugin.password.jndi.JndiUtils.createDirContext;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static javax.naming.Context.INITIAL_CONTEXT_FACTORY;
-import static javax.naming.Context.PROVIDER_URL;
-import static javax.naming.Context.REFERRAL;
-import static javax.naming.Context.SECURITY_AUTHENTICATION;
-import static javax.naming.Context.SECURITY_CREDENTIALS;
-import static javax.naming.Context.SECURITY_PRINCIPAL;
 
 public class LdapAuthenticator
         implements PasswordAuthenticator
@@ -72,19 +47,21 @@ public class LdapAuthenticator
     private static final CharMatcher SPECIAL_CHARACTERS = CharMatcher.anyOf(",=+<>#;*()\"\\\u0000");
     private static final CharMatcher WHITESPACE = CharMatcher.anyOf(" \r");
 
+    private final LdapAuthenticatorClient client;
+
     private final Optional<String> userBindSearchPattern;
     private final Optional<String> groupAuthorizationSearchPattern;
     private final Optional<String> userBaseDistinguishedName;
     private final Optional<String> bindDistinguishedName;
     private final Optional<String> bindPassword;
-    private final Map<String, String> basicEnvironment;
+
     private final LoadingCache<Credential, Principal> authenticationCache;
-    private final Optional<SSLContext> sslContext;
 
     @Inject
-    public LdapAuthenticator(LdapConfig ldapConfig)
+    public LdapAuthenticator(LdapAuthenticatorClient client, LdapConfig ldapConfig)
     {
-        String ldapUrl = requireNonNull(ldapConfig.getLdapUrl(), "ldapUrl is null");
+        this.client = requireNonNull(client, "client is null");
+
         this.userBindSearchPattern = Optional.ofNullable(ldapConfig.getUserBindSearchPattern());
         this.groupAuthorizationSearchPattern = Optional.ofNullable(ldapConfig.getGroupAuthorizationSearchPattern());
         this.userBaseDistinguishedName = Optional.ofNullable(ldapConfig.getUserBaseDistinguishedName());
@@ -104,24 +81,11 @@ public class LdapAuthenticator
                 bindDistinguishedName.isPresent() || userBindSearchPattern.isPresent(),
                 "Either user bind search pattern or bind distinguished name must be provided");
 
-        if (ldapUrl.startsWith("ldap://")) {
-            log.warn("Passwords will be sent in the clear to the LDAP server. Please consider using SSL to connect.");
-        }
-
-        this.basicEnvironment = ImmutableMap.<String, String>builder()
-                .put(INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
-                .put(PROVIDER_URL, ldapUrl)
-                .put(REFERRAL, ldapConfig.isIgnoreReferrals() ? "ignore" : "follow")
-                .build();
-
         this.authenticationCache = CacheBuilder.newBuilder()
                 .expireAfterWrite(ldapConfig.getLdapCacheTtl().toMillis(), MILLISECONDS)
                 .build(CacheLoader.from(bindDistinguishedName.isPresent()
                         ? this::authenticateWithBindDistinguishedName
                         : this::authenticateWithUserBind));
-
-        this.sslContext = Optional.ofNullable(ldapConfig.getTrustCertificate())
-                .map(LdapAuthenticator::createSslContext);
     }
 
     @Override
@@ -148,14 +112,14 @@ public class LdapAuthenticator
                 // user password is also validated as user DN and password is used for querying LDAP
                 String searchBase = userBaseDistinguishedName.orElseThrow();
                 String groupSearch = replaceUser(groupAuthorizationSearchPattern.get(), user);
-                if (!isGroupMember(searchBase, groupSearch, userDistinguishedName, credential.getPassword())) {
+                if (!client.isGroupMember(searchBase, groupSearch, userDistinguishedName, credential.getPassword())) {
                     String message = format("User [%s] not a member of an authorized group", user);
                     log.debug(message);
                     throw new AccessDeniedException(message);
                 }
             }
             else {
-                validatePassword(userDistinguishedName, credential.getPassword());
+                client.validatePassword(userDistinguishedName, credential.getPassword());
             }
             log.debug("Authentication successful for user [%s]", user);
         }
@@ -174,7 +138,7 @@ public class LdapAuthenticator
         }
         try {
             String userDistinguishedName = lookupUserDistinguishedName(user);
-            validatePassword(userDistinguishedName, credential.getPassword());
+            client.validatePassword(userDistinguishedName, credential.getPassword());
             log.debug("Authentication successful for user [%s]", user);
         }
         catch (NamingException e) {
@@ -187,24 +151,6 @@ public class LdapAuthenticator
     private String createUserDistinguishedName(String user)
     {
         return replaceUser(userBindSearchPattern.orElseThrow(), user);
-    }
-
-    private boolean isGroupMember(String searchBase, String groupSearch, String contextUserDistinguishedName, String contextPassword)
-            throws NamingException
-    {
-        DirContext context = createUserDirContext(contextUserDistinguishedName, contextPassword);
-        try {
-            NamingEnumeration<SearchResult> search = searchContext(searchBase, groupSearch, context);
-            try {
-                return search.hasMore();
-            }
-            finally {
-                search.close();
-            }
-        }
-        finally {
-            context.close();
-        }
     }
 
     /**
@@ -228,7 +174,7 @@ public class LdapAuthenticator
     {
         String searchBase = userBaseDistinguishedName.orElseThrow();
         String searchFilter = replaceUser(groupAuthorizationSearchPattern.orElseThrow(), user);
-        Set<String> userDistinguishedNames = lookupUserDistinguishedName(searchBase, searchFilter, bindDistinguishedName.orElseThrow(), bindPassword.orElseThrow());
+        Set<String> userDistinguishedNames = client.lookupUserDistinguishedNames(searchBase, searchFilter, bindDistinguishedName.orElseThrow(), bindPassword.orElseThrow());
         if (userDistinguishedNames.isEmpty()) {
             String message = format("User [%s] not a member of an authorized group", user);
             log.debug(message);
@@ -242,101 +188,8 @@ public class LdapAuthenticator
         return getOnlyElement(userDistinguishedNames);
     }
 
-    private Set<String> lookupUserDistinguishedName(String searchBase, String searchFilter, String contextUserDistinguishedName, String contextPassword)
-            throws NamingException
-    {
-        DirContext context = createUserDirContext(contextUserDistinguishedName, contextPassword);
-        try {
-            ImmutableSet.Builder<String> distinguishedNames = ImmutableSet.builder();
-            NamingEnumeration<SearchResult> search = searchContext(searchBase, searchFilter, context);
-            try {
-                while (search.hasMore()) {
-                    distinguishedNames.add(search.next().getNameInNamespace());
-                }
-                return distinguishedNames.build();
-            }
-            finally {
-                search.close();
-            }
-        }
-        finally {
-            context.close();
-        }
-    }
-
-    private static NamingEnumeration<SearchResult> searchContext(String searchBase, String searchFilter, DirContext context)
-            throws NamingException
-    {
-        SearchControls searchControls = new SearchControls();
-        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-        return context.search(searchBase, searchFilter, searchControls);
-    }
-
-    private void validatePassword(String userDistinguishedName, String password)
-            throws NamingException
-    {
-        createUserDirContext(userDistinguishedName, password).close();
-    }
-
-    private DirContext createUserDirContext(String userDistinguishedName, String password)
-            throws NamingException
-    {
-        Map<String, String> environment = createEnvironment(userDistinguishedName, password);
-        try {
-            // This is the actual Authentication piece. Will throw javax.naming.AuthenticationException
-            // if the users password is not correct. Other exceptions may include IO (server not found) etc.
-            DirContext context = createDirContext(environment);
-            log.debug("Password validation successful for user DN [%s]", userDistinguishedName);
-            return context;
-        }
-        catch (AuthenticationException e) {
-            log.debug("Password validation failed for user DN [%s]: %s", userDistinguishedName, e.getMessage());
-            throw new AccessDeniedException("Invalid credentials");
-        }
-    }
-
-    private Map<String, String> createEnvironment(String userDistinguishedName, String password)
-    {
-        ImmutableMap.Builder<String, String> environment = ImmutableMap.<String, String>builder()
-                .putAll(basicEnvironment)
-                .put(SECURITY_AUTHENTICATION, "simple")
-                .put(SECURITY_PRINCIPAL, userDistinguishedName)
-                .put(SECURITY_CREDENTIALS, password);
-
-        sslContext.ifPresent(context -> {
-            LdapSslSocketFactory.setSslContextForCurrentThread(context);
-
-            // see https://docs.oracle.com/javase/jndi/tutorial/ldap/security/ssl.html
-            environment.put("java.naming.ldap.factory.socket", LdapSslSocketFactory.class.getName());
-        });
-
-        return environment.build();
-    }
-
     private static String replaceUser(String pattern, String user)
     {
         return pattern.replace("${USER}", user);
-    }
-
-    private static SSLContext createSslContext(File trustCertificate)
-    {
-        try {
-            KeyStore trustStore = PemReader.loadTrustStore(trustCertificate);
-
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(trustStore);
-
-            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-                throw new RuntimeException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
-            }
-
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustManagers, null);
-            return sslContext;
-        }
-        catch (GeneralSecurityException | IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
