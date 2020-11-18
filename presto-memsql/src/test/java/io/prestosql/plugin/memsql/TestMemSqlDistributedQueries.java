trinodb/trino

commit 8508bfd85eab29a95f0d28a77d78234293e28d8a

Add boolean to DataMappingSmokeTest

presto-mysql/src/test/java/io/prestosql/plugin/mysql/TestMySqlDistributedQueries.java
@@ -146,6 +146,11 @@ public class TestMySqlDistributedQueries
             return Optional.empty();
         }
 
+        if (typeName.equals("boolean")) {
+            // MySql does not have built-in support for boolean type. MySQL provides BOOLEAN as the synonym of TINYINT(1)
+            return Optional.empty();
+        }
+
         return Optional.of(dataMappingTestSetup);
     }
 

presto-oracle/src/test/java/io/prestosql/plugin/oracle/BaseTestOracleDistributedQueries.java
@@ -79,6 +79,10 @@ public abstract class BaseTestOracleDistributedQueries
         if (typeName.equals("time")) {
             return Optional.empty();
         }
+        if (typeName.equals("boolean")) {
+            // Oracle does not have native support for boolean however usually it is represented as number(1)
+            return Optional.empty();
+        }
 
         return Optional.of(dataMappingTestSetup);
     }

presto-testing/src/main/java/io/prestosql/testing/AbstractTestDistributedQueries.java
@@ -1473,6 +1473,7 @@ public abstract class AbstractTestDistributedQueries
     private List<DataMappingTestSetup> testDataMappingSmokeTestData()
     {
         return ImmutableList.<DataMappingTestSetup>builder()
+                .add(new DataMappingTestSetup("boolean", "false", "true"))
                 .add(new DataMappingTestSetup("tinyint", "37", "127"))
                 .add(new DataMappingTestSetup("smallint", "32123", "32767"))
                 .add(new DataMappingTestSetup("integer", "1274942432", "2147483647"))

commit 1b1b78f2bef4c7ca7b95768ed1ee9f8893f960aa

Fix ATDQ.testDataMappingSmokeTest for MemSQL
It was broken since 8508bfd85eab29a95f0d28a77d78234293e28d8a, because
MemSQL tests do not run as part of CI for PR builds.

presto-memsql/src/test/java/io/prestosql/plugin/memsql/TestMemSqlDistributedQueries.java
@@ -97,6 +97,13 @@ public class TestMemSqlDistributedQueries
     protected Optional<DataMappingTestSetup> filterDataMappingSmokeTestData(DataMappingTestSetup dataMappingTestSetup)
     {
         String typeName = dataMappingTestSetup.getPrestoTypeName();
+
+        if (typeName.equals("boolean")) {
+            // MemSQL does not have built-in support for boolean type. MemSQL provides BOOLEAN as the synonym of TINYINT(1)
+            // Querying the column with a boolean predicate subsequently fails with "Cannot apply operator: tinyint = boolean"
+            return Optional.empty();
+        }
+
         if (typeName.equals("time")
                 || typeName.equals("timestamp(3) with time zone")) {
             return Optional.of(dataMappingTestSetup.asUnsupported());

presto-mysql/src/test/java/io/prestosql/plugin/mysql/TestMySqlDistributedQueries.java
@@ -149,6 +149,7 @@ public class TestMySqlDistributedQueries
 
         if (typeName.equals("boolean")) {
             // MySql does not have built-in support for boolean type. MySQL provides BOOLEAN as the synonym of TINYINT(1)
+            // Querying the column with a boolean predicate subsequently fails with "Cannot apply operator: tinyint = boolean"
             return Optional.empty();
         }
 