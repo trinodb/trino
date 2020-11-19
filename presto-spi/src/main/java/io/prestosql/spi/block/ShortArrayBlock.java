b3cf839c57ccab6acbefd31e17f10a2ece3ad308
skrzypo987 <krzysztof.skrzypczynski@starburstdata.com>
2020-11-19 15:23:06 +0100

    Improve serde performance for short blocks
    Benchmarks before:
    BenchmarkBlockSerde.deserializeShort                   0  avgt   60   5,347 ± 0,012  ns/op
    BenchmarkBlockSerde.deserializeShort                 .01  avgt   60   6,660 ± 0,027  ns/op
    BenchmarkBlockSerde.deserializeShort                 .10  avgt   60   7,090 ± 0,066  ns/op
    BenchmarkBlockSerde.deserializeShort                 .50  avgt   60   8,898 ± 0,044  ns/op
    BenchmarkBlockSerde.deserializeShort                 .90  avgt   60   2,789 ± 0,008  ns/op
    BenchmarkBlockSerde.deserializeShort                 .99  avgt   60   1,422 ± 0,003  ns/op
    BenchmarkBlockSerde.serializeShort                     0  avgt   60   8.873 ± 0.271  ns/op
    BenchmarkBlockSerde.serializeShort                   .01  avgt   60   9.499 ± 0.080  ns/op
    BenchmarkBlockSerde.serializeShort                   .10  avgt   60  10.377 ± 0.135  ns/op
    BenchmarkBlockSerde.serializeShort                   .50  avgt   60  11.053 ± 0.157  ns/op
    BenchmarkBlockSerde.serializeShort                   .90  avgt   60   4.682 ± 0.110  ns/op
    BenchmarkBlockSerde.serializeShort                   .99  avgt   60   2.927 ± 0.102  ns/op
    
    after:
    BenchmarkBlockSerde.deserializeShort                   0  avgt   60  0,270 ± 0,002  ns/op
    BenchmarkBlockSerde.deserializeShort                 .01  avgt   60  1,762 ± 0,005  ns/op
    BenchmarkBlockSerde.deserializeShort                 .10  avgt   60  3,147 ± 0,018  ns/op
    BenchmarkBlockSerde.deserializeShort                 .50  avgt   60  2,148 ± 0,006  ns/op
    BenchmarkBlockSerde.deserializeShort                 .90  avgt   60  2,837 ± 0,018  ns/op
    BenchmarkBlockSerde.deserializeShort                 .99  avgt   60  1,098 ± 0,002  ns/op
    BenchmarkBlockSerde.serializeShort                     0  avgt   60  0.414 ±  0.001  ns/op
    BenchmarkBlockSerde.serializeShort                   .01  avgt   60  2.282 ±  0.281  ns/op
    BenchmarkBlockSerde.serializeShort                   .10  avgt   60  3.750 ±  0.234  ns/op
    BenchmarkBlockSerde.serializeShort                   .50  avgt   60  2.399 ±  0.022  ns/op
    BenchmarkBlockSerde.serializeShort                   .90  avgt   60  2.807 ±  0.020  ns/op
    BenchmarkBlockSerde.serializeShort                   .99  avgt   60  1.567 ±  0.007  ns/op

--- presto-spi/src/main/java/io/prestosql/spi/block/ShortArrayBlock.java
+++ presto-spi/src/main/java/io/prestosql/spi/block/ShortArrayBlock.java
@@ -13,6 +13,8 @@
  */
 package io.prestosql.spi.block;
 
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 import org.openjdk.jol.info.ClassLayout;
 
 import javax.annotation.Nullable;
@@ -215,6 +217,11 @@ public class ShortArrayBlock
         sb.append("positionCount=").append(getPositionCount());
         sb.append('}');
         return sb.toString();
+    }
+
+    Slice getValuesSlice()
+    {
+        return Slices.wrappedShortArray(values, arrayOffset, positionCount);
     }
 
     private void checkReadablePosition(int position)
