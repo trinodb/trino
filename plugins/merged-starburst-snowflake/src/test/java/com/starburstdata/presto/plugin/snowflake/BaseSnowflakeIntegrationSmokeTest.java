f7c4ec16ea205735e9b807aa7a32c8e7b9c729e1
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-11-25 13:22:24 +0100

    Add test covering TIMESTAMP w/TZ with different precisions on Snowflake
    (cherry-picked from commit: 60d89baca74f47c644a58f7df50d7c6e689396e3)

--- plugins/merged-starburst-snowflake/src/test/java/com/starburstdata/presto/plugin/snowflake/BaseSnowflakeIntegrationSmokeTest.java
+++ plugins/merged-starburst-snowflake/src/test/java/com/starburstdata/presto/plugin/snowflake/BaseSnowflakeIntegrationSmokeTest.java
@@ -436,6 +436,61 @@ public abstract class BaseSnowflakeIntegrationSmokeTest
     }
 
     @Test
+    public void testSnowflakeTimestampWithTimeZoneWithPrecision()
+    {
+        try (TestTable testTable = new TestTable(snowflakeExecutor::execute, getSession().getSchema().orElseThrow() + ".test_timestamptz_with_precision",
+                "(" +
+                        "timestamptz0 timestamp_tz(0)," +
+                        "timestamptz1 timestamp_tz(1)," +
+                        "timestamptz2 timestamp_tz(2)," +
+                        "timestamptz3 timestamp_tz(3)," +
+                        "timestamptz4 timestamp_tz(4)," +
+                        "timestamptz5 timestamp_tz(5)," +
+                        "timestamptz6 timestamp_tz(6)," +
+                        "timestamptz7 timestamp_tz(7)," +
+                        "timestamptz8 timestamp_tz(8)," +
+                        "timestamptz9 timestamp_tz(9))",
+                ImmutableList.of("" +
+                        "'1901-02-03 04:05:06 +02:00'," +
+                        "'1901-02-03 04:05:06.1 +02:00'," +
+                        "'1901-02-03 04:05:06.12 +02:00'," +
+                        "'1901-02-03 04:05:06.123 +02:00'," +
+                        "'1901-02-03 04:05:06.1234 +02:00'," +
+                        "'1901-02-03 04:05:06.12345 +02:00'," +
+                        "'1901-02-03 04:05:06.123456 +02:00'," +
+                        "'1901-02-03 04:05:06.1234567 +02:00'," +
+                        "'1901-02-03 04:05:06.12345678 +02:00'," +
+                        "'1901-02-03 04:05:06.123456789 +02:00'"))) {
+            assertThat((String) computeActual("SHOW CREATE TABLE " + testTable.getName()).getOnlyValue())
+                    .matches("CREATE TABLE \\w+\\.\\w+\\.\\w+ \\Q(\n" +
+                            "   timestamptz0 timestamp(3) with time zone,\n" +
+                            "   timestamptz1 timestamp(3) with time zone,\n" +
+                            "   timestamptz2 timestamp(3) with time zone,\n" +
+                            "   timestamptz3 timestamp(3) with time zone,\n" +
+                            "   timestamptz4 timestamp(3) with time zone,\n" +
+                            "   timestamptz5 timestamp(3) with time zone,\n" +
+                            "   timestamptz6 timestamp(3) with time zone,\n" +
+                            "   timestamptz7 timestamp(3) with time zone,\n" +
+                            "   timestamptz8 timestamp(3) with time zone,\n" +
+                            "   timestamptz9 timestamp(3) with time zone\n" +
+                            ")");
+
+            assertThat(query("SELECT * FROM " + testTable.getName()))
+                    .matches("VALUES (" +
+                            "TIMESTAMP '1901-02-03 04:05:06.000 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.100 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.120 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00'," +
+                            "TIMESTAMP '1901-02-03 04:05:06.123 +02:00')");
+        }
+    }
+
+    @Test
     public void testSnowflakeTimeWithPrecision()
     {
         try (TestTable testTable = new TestTable(snowflakeExecutor::execute, getSession().getSchema().orElseThrow() + ".test_time_with_precision",
