95a1348b840c574c7829f3399ec79c8b0d8db4ae
Karol Sobczak <napewnotrafi@gmail.com>
2020-11-26 21:12:31 +0100

    Remove redundant tests
    LocalDynamicFilterConsumer does not require actual plan for
    testing. Dynamic filtering planning is already covered by other
    tests. Removed tests did not test any new LocalDynamicFilterConsumer
    use case.

--- presto-main/src/test/java/io/prestosql/sql/planner/TestLocalDynamicFilterConsumer.java
+++ presto-main/src/test/java/io/prestosql/sql/planner/TestLocalDynamicFilterConsumer.java
@@ -18,26 +18,21 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
-import io.prestosql.Session;
 import io.prestosql.spi.predicate.Domain;
 import io.prestosql.spi.predicate.TupleDomain;
 import io.prestosql.sql.analyzer.FeaturesConfig.JoinDistributionType;
 import io.prestosql.sql.analyzer.FeaturesConfig.JoinReorderingStrategy;
 import io.prestosql.sql.planner.assertions.BasePlanTest;
 import io.prestosql.sql.planner.iterative.rule.test.PlanBuilder;
-import io.prestosql.sql.planner.optimizations.PlanNodeSearcher;
 import io.prestosql.sql.planner.plan.DynamicFilterId;
 import io.prestosql.sql.planner.plan.JoinNode;
 import io.prestosql.sql.planner.plan.JoinNode.EquiJoinClause;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.collect.Iterables.getOnlyElement;
 import static io.prestosql.SystemSessionProperties.ENABLE_DYNAMIC_FILTERING;
 import static io.prestosql.SystemSessionProperties.FORCE_SINGLE_NODE_OUTPUT;
 import static io.prestosql.SystemSessionProperties.JOIN_DISTRIBUTION_TYPE;
@@ -46,7 +41,6 @@ import static io.prestosql.metadata.AbstractMockMetadata.dummyMetadata;
 import static io.prestosql.spi.type.BigintType.BIGINT;
 import static io.prestosql.spi.type.IntegerType.INTEGER;
 import static io.prestosql.spi.type.SmallintType.SMALLINT;
-import static io.prestosql.sql.planner.LogicalPlanner.Stage.OPTIMIZED_AND_VALIDATED;
 import static io.prestosql.sql.planner.plan.JoinNode.Type.INNER;
 import static io.prestosql.testing.assertions.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -261,150 +255,5 @@ public class TestLocalDynamicFilterConsumer
         assertEquals(
                 consumer.getDynamicFilterDomains().get(),
                 ImmutableMap.of(filter1, Domain.none(BIGINT), filter3, Domain.none(SMALLINT)));
-    }
-
-    @Test
-    public void testCreateSingleColumn()
-            throws Exception
-    {
-        SubPlan subplan = subplan(
-                "SELECT count() FROM lineitem, orders WHERE lineitem.orderkey = orders.orderkey " +
-                        "AND orders.custkey < 10",
-                OPTIMIZED_AND_VALIDATED,
-                false);
-        JoinNode joinNode = searchJoins(subplan.getChildren().get(0).getFragment()).findOnlyElement();
-        LocalDynamicFilterConsumer filter = LocalDynamicFilterConsumer.create(
-                joinNode,
-                ImmutableList.copyOf(subplan.getFragment().getSymbols().values()),
-                1,
-                joinNode.getDynamicFilters().keySet());
-        DynamicFilterId filterId = getOnlyElement(filter.getBuildChannels().keySet());
-
-        filter.getTupleDomainConsumer().accept(TupleDomain.withColumnDomains(ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 3L))));
-        assertEquals(filter.getDynamicFilterDomains().get(), ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 3L)));
-    }
-
-    @Test
-    public void testCreateDistributedJoin()
-            throws Exception
-    {
-        Session session = Session.builder(getQueryRunner().getDefaultSession())
-                .setSystemProperty(JOIN_DISTRIBUTION_TYPE, "PARTITIONED")
-                .build();
-        SubPlan subplan = subplan(
-                "SELECT count() FROM nation, region WHERE nation.regionkey = region.regionkey " +
-                        "AND region.comment = 'abc'",
-                OPTIMIZED_AND_VALIDATED,
-                false,
-                session);
-        JoinNode joinNode = searchJoins(subplan.getChildren().get(0).getFragment()).findOnlyElement();
-        LocalDynamicFilterConsumer filter = LocalDynamicFilterConsumer.create(
-                joinNode,
-                ImmutableList.copyOf(subplan.getFragment().getSymbols().values()),
-                1,
-                joinNode.getDynamicFilters().keySet());
-        DynamicFilterId filterId = getOnlyElement(filter.getBuildChannels().keySet());
-        assertFalse(joinNode.getDynamicFilters().isEmpty());
-
-        filter.getTupleDomainConsumer().accept(TupleDomain.withColumnDomains(ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 3L))));
-        assertEquals(filter.getDynamicFilterDomains().get(), ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 3L)));
-    }
-
-    @Test
-    public void testCreateMultipleCriteria()
-            throws Exception
-    {
-        SubPlan subplan = subplan(
-                "SELECT count() FROM lineitem, partsupp " +
-                        "WHERE lineitem.partkey = partsupp.partkey AND lineitem.suppkey = partsupp.suppkey " +
-                        "AND partsupp.availqty < 10",
-                OPTIMIZED_AND_VALIDATED,
-                false);
-
-        JoinNode joinNode = searchJoins(subplan.getChildren().get(0).getFragment()).findOnlyElement();
-        LocalDynamicFilterConsumer filter = LocalDynamicFilterConsumer.create(
-                joinNode,
-                ImmutableList.copyOf(subplan.getFragment().getSymbols().values()),
-                1,
-                joinNode.getDynamicFilters().keySet());
-        List<DynamicFilterId> filterIds = filter
-                .getBuildChannels()
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .collect(toImmutableList());
-
-        Map<DynamicFilterId, Domain> domainMap = ImmutableMap.of(
-                filterIds.get(0), Domain.singleValue(BIGINT, 4L),
-                filterIds.get(1), Domain.singleValue(BIGINT, 5L));
-        filter.getTupleDomainConsumer().accept(TupleDomain.withColumnDomains(domainMap));
-
-        assertEquals(filter.getDynamicFilterDomains().get(), domainMap);
-    }
-
-    @Test
-    public void testCreateMultipleJoins()
-            throws Exception
-    {
-        SubPlan subplan = subplan(
-                "SELECT count() FROM lineitem, orders, part " +
-                        "WHERE lineitem.orderkey = orders.orderkey AND lineitem.partkey = part.partkey " +
-                        "AND orders.custkey < 10 AND part.name = 'abc'",
-                OPTIMIZED_AND_VALIDATED,
-                false);
-
-        List<JoinNode> joinNodes = searchJoins(subplan.getChildren().get(0).getFragment()).findAll();
-        assertEquals(joinNodes.size(), 2);
-        for (JoinNode joinNode : joinNodes) {
-            LocalDynamicFilterConsumer filter = LocalDynamicFilterConsumer.create(
-                    joinNode,
-                    ImmutableList.copyOf(subplan.getFragment().getSymbols().values()),
-                    1,
-                    joinNode.getDynamicFilters().keySet());
-            DynamicFilterId filterId = getOnlyElement(filter.getBuildChannels().keySet());
-
-            filter.getTupleDomainConsumer().accept(TupleDomain.withColumnDomains(ImmutableMap.of(
-                    filterId, Domain.singleValue(BIGINT, 6L))));
-            assertEquals(filter.getDynamicFilterDomains().get(), ImmutableMap.of(
-                    filterId, Domain.singleValue(BIGINT, 6L)));
-        }
-    }
-
-    @Test
-    public void testCreateProbeSideUnion()
-            throws Exception
-    {
-        SubPlan subplan = subplan(
-                "WITH union_table(key) AS " +
-                        "((SELECT partkey FROM part) UNION (SELECT suppkey FROM supplier)) " +
-                        "SELECT count() FROM union_table, nation WHERE union_table.key = nation.nationkey " +
-                        "AND nation.comment = 'abc'",
-                OPTIMIZED_AND_VALIDATED,
-                true);
-
-        JoinNode joinNode = searchJoins(subplan.getFragment()).findOnlyElement();
-        LocalDynamicFilterConsumer filter = LocalDynamicFilterConsumer.create(
-                joinNode,
-                ImmutableList.copyOf(subplan.getFragment().getSymbols().values()),
-                1,
-                joinNode.getDynamicFilters().keySet());
-        DynamicFilterId filterId = getOnlyElement(filter.getBuildChannels().keySet());
-
-        filter.getTupleDomainConsumer().accept(TupleDomain.withColumnDomains(ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 7L))));
-        assertEquals(filter.getDynamicFilterDomains().get(), ImmutableMap.of(
-                filterId, Domain.singleValue(BIGINT, 7L)));
-    }
-
-    private PlanNodeSearcher searchJoins(PlanFragment fragment)
-    {
-        return PlanNodeSearcher
-                .searchFrom(fragment.getRoot())
-                .where(node -> node instanceof JoinNode);
     }
 }
