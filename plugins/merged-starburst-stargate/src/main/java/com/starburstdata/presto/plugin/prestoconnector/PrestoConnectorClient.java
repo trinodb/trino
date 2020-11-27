1f83d3fd537f46ba032b003397612ae30c1e084c
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-11-27 22:58:07 +0100

    Push aggregations in Presto Connector
    (cherry-picked from commit: 7bae659c4b31e33fc075c632a84aeee7f49f6cc2)

--- plugins/merged-starburst-stargate/src/main/java/com/starburstdata/presto/plugin/prestoconnector/PrestoConnectorClient.java
+++ plugins/merged-starburst-stargate/src/main/java/com/starburstdata/presto/plugin/prestoconnector/PrestoConnectorClient.java
@@ -9,37 +9,94 @@
  */
 package com.starburstdata.presto.plugin.prestoconnector;
 
+import com.google.common.base.VerifyException;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableSet;
+import io.airlift.log.Logger;
 import io.prestosql.plugin.jdbc.BaseJdbcClient;
 import io.prestosql.plugin.jdbc.BaseJdbcConfig;
+import io.prestosql.plugin.jdbc.ColumnMapping;
 import io.prestosql.plugin.jdbc.ConnectionFactory;
 import io.prestosql.plugin.jdbc.JdbcColumnHandle;
+import io.prestosql.plugin.jdbc.JdbcExpression;
 import io.prestosql.plugin.jdbc.JdbcOutputTableHandle;
 import io.prestosql.plugin.jdbc.JdbcTableHandle;
+import io.prestosql.plugin.jdbc.JdbcTypeHandle;
+import io.prestosql.plugin.jdbc.WriteMapping;
+import io.prestosql.plugin.jdbc.expression.AggregateFunctionRewriter;
 import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.connector.AggregateFunction;
+import io.prestosql.spi.connector.ColumnHandle;
 import io.prestosql.spi.connector.ColumnMetadata;
 import io.prestosql.spi.connector.ConnectorSession;
 import io.prestosql.spi.connector.ConnectorTableMetadata;
 import io.prestosql.spi.connector.SchemaTableName;
+import io.prestosql.spi.type.CharType;
+import io.prestosql.spi.type.DecimalType;
+import io.prestosql.spi.type.TimeType;
+import io.prestosql.spi.type.TimeWithTimeZoneType;
+import io.prestosql.spi.type.TimestampType;
+import io.prestosql.spi.type.TimestampWithTimeZoneType;
+import io.prestosql.spi.type.Type;
+import io.prestosql.spi.type.VarcharType;
 
 import javax.inject.Inject;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
 
+import static com.google.common.base.Verify.verify;
 import static io.prestosql.spi.StandardErrorCode.NOT_SUPPORTED;
+import static io.prestosql.spi.type.BigintType.BIGINT;
+import static io.prestosql.spi.type.BooleanType.BOOLEAN;
+import static io.prestosql.spi.type.DateType.DATE;
+import static io.prestosql.spi.type.DoubleType.DOUBLE;
+import static io.prestosql.spi.type.IntegerType.INTEGER;
+import static io.prestosql.spi.type.RealType.REAL;
+import static io.prestosql.spi.type.SmallintType.SMALLINT;
+import static io.prestosql.spi.type.TinyintType.TINYINT;
+import static io.prestosql.spi.type.VarbinaryType.VARBINARY;
 import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 public class PrestoConnectorClient
         extends BaseJdbcClient
 {
+    private static final Logger log = Logger.get(PrestoConnectorClient.class);
+
+    private enum FunctionsCacheKey
+    {
+        SINGLETON
+    }
+
     private final boolean enableWrites;
+    private final Cache<FunctionsCacheKey, Set<String>> supportedAggregateFunctions;
+    private final AggregateFunctionRewriter aggregateFunctionRewriter;
 
     @Inject
     public PrestoConnectorClient(BaseJdbcConfig config, ConnectionFactory connectionFactory, @EnableWrites boolean enableWrites)
     {
         super(config, "\"", connectionFactory);
         this.enableWrites = enableWrites;
+
+        this.supportedAggregateFunctions = CacheBuilder.newBuilder()
+                .expireAfterWrite(30, MINUTES)
+                .build();
+        this.aggregateFunctionRewriter = new AggregateFunctionRewriter(this::quoted, Set.of(
+                new PrestoAggregateFunctionRewriteRule(
+                        this::getSupportedAggregateFunctions,
+                        this::toTypeHandle)));
     }
 
     @Override
@@ -127,6 +184,154 @@ public class PrestoConnectorClient
     }
 
     @Override
+    public Optional<ColumnMapping> toPrestoType(ConnectorSession session, Connection connection, JdbcTypeHandle typeHandle)
+    {
+        Optional<ColumnMapping> columnMapping = convertToPrestoType(session, connection, typeHandle);
+        columnMapping.ifPresent(mapping -> {
+            // Ensure toTypeHandle stays up to date when we add new type mappings
+            Type type = mapping.getType();
+            JdbcTypeHandle syntheticTypeHandle = toTypeHandle(type)
+                    .orElseThrow(() -> new VerifyException(format("Cannot convert type %s [%s] back to JdbcTypeHandle", type, typeHandle)));
+            ColumnMapping mappingForSyntheticHandle = convertToPrestoType(session, connection, typeHandle)
+                    .orElseThrow(() -> new VerifyException(format("JdbcTypeHandle %s constructed for %s [%s] cannot be converted to type", syntheticTypeHandle, type, typeHandle)));
+            verify(
+                    mappingForSyntheticHandle.getType().equals(type),
+                    "Type mismatch, original type is %s [%s], converted type is %s [%s]",
+                    type,
+                    typeHandle,
+                    mappingForSyntheticHandle.getType(),
+                    syntheticTypeHandle);
+        });
+        return columnMapping;
+    }
+
+    private Optional<ColumnMapping> convertToPrestoType(ConnectorSession session, Connection connection, JdbcTypeHandle typeHandle)
+    {
+        // TODO
+        return super.toPrestoType(session, connection, typeHandle);
+    }
+
+    @Override
+    public WriteMapping toWriteMapping(ConnectorSession session, Type type)
+    {
+        // TODO
+        return super.toWriteMapping(session, type);
+    }
+
+    private Optional<JdbcTypeHandle> toTypeHandle(Type type)
+    {
+        requireNonNull(type, "type is null");
+
+        if (type == BOOLEAN) {
+            return Optional.of(jdbcTypeHandle(Types.BOOLEAN));
+        }
+
+        if (type == TINYINT) {
+            return Optional.of(jdbcTypeHandle(Types.TINYINT));
+        }
+        if (type == SMALLINT) {
+            return Optional.of(jdbcTypeHandle(Types.SMALLINT));
+        }
+        if (type == INTEGER) {
+            return Optional.of(jdbcTypeHandle(Types.INTEGER));
+        }
+        if (type == BIGINT) {
+            return Optional.of(jdbcTypeHandle(Types.BIGINT));
+        }
+
+        if (type == REAL) {
+            return Optional.of(jdbcTypeHandle(Types.REAL));
+        }
+        if (type == DOUBLE) {
+            return Optional.of(jdbcTypeHandle(Types.DOUBLE));
+        }
+
+        if (type instanceof DecimalType) {
+            DecimalType decimalType = (DecimalType) type;
+            return Optional.of(new JdbcTypeHandle(Types.DECIMAL, Optional.empty(), decimalType.getPrecision(), Optional.of(decimalType.getScale()), Optional.empty(), Optional.empty()));
+        }
+
+        if (type instanceof CharType) {
+            return Optional.of(jdbcTypeHandleWithColumnSize(Types.CHAR, ((CharType) type).getLength()));
+        }
+        if (type instanceof VarcharType) {
+            // See io.prestosql.connector.system.jdbc.ColumnJdbcTable#columnSize
+            int columnSize = ((VarcharType) type).getLength().orElse(VarcharType.UNBOUNDED_LENGTH);
+            return Optional.of(jdbcTypeHandleWithColumnSize(Types.VARCHAR, columnSize));
+        }
+
+        if (type == VARBINARY) {
+            return Optional.of(jdbcTypeHandle(Types.VARBINARY));
+        }
+
+        if (type == DATE) {
+            return Optional.of(jdbcTypeHandle(Types.DATE));
+        }
+
+        if (type instanceof TimeType) {
+            return Optional.of(jdbcTypeHandleWithDecimalDigits(Types.TIME, ((TimeType) type).getPrecision()));
+        }
+        if (type instanceof TimeWithTimeZoneType) {
+            return Optional.of(jdbcTypeHandleWithDecimalDigits(Types.TIME_WITH_TIMEZONE, ((TimeWithTimeZoneType) type).getPrecision()));
+        }
+
+        if (type instanceof TimestampType) {
+            return Optional.of(jdbcTypeHandleWithDecimalDigits(Types.TIME, ((TimestampType) type).getPrecision()));
+        }
+        if (type instanceof TimestampWithTimeZoneType) {
+            return Optional.of(jdbcTypeHandleWithDecimalDigits(Types.TIME_WITH_TIMEZONE, ((TimestampWithTimeZoneType) type).getPrecision()));
+        }
+
+        log.debug("Type cannot be converted to JdbcTypeHandle: %s", type);
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<JdbcExpression> implementAggregation(ConnectorSession session, AggregateFunction aggregate, Map<String, ColumnHandle> assignments)
+    {
+        // TODO support complex ConnectorExpressions
+        return aggregateFunctionRewriter.rewrite(session, aggregate, assignments);
+    }
+
+    private Set<String> getSupportedAggregateFunctions(ConnectorSession session)
+    {
+        try {
+            return supportedAggregateFunctions.get(FunctionsCacheKey.SINGLETON, () -> {
+                try {
+                    return listAggregateFunctions(session);
+                }
+                // Catch exceptions from the driver only. Any other exception is likely bug in the code.
+                catch (SQLException e) {
+                    log.warn(e, "Failed to list aggregate functions");
+                    // If we reached aggregation pushdown, the remote cluster is likely up & running and so it may not
+                    // be safe to retry the listing immediately. Cache the failure.
+                    return Set.of();
+                }
+            });
+        }
+        catch (ExecutionException e) {
+            // Impossible, as the loader does not throw checked exceptions
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Set<String> listAggregateFunctions(ConnectorSession session)
+            throws SQLException
+    {
+        try (Connection connection = connectionFactory.openConnection(session);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SHOW FUNCTIONS")) {
+            ImmutableSet.Builder<String> functions = ImmutableSet.builder();
+            while (resultSet.next()) {
+                if ("aggregate".equals(resultSet.getString("Function Type"))) {
+                    functions.add(resultSet.getString("Function"));
+                }
+            }
+            return functions.build();
+        }
+    }
+
+    @Override
     protected Optional<BiFunction<String, Long, String>> limitFunction()
     {
         return Optional.of((sql, limit) -> sql + " LIMIT " + limit);
@@ -136,5 +341,20 @@ public class PrestoConnectorClient
     public boolean isLimitGuaranteed(ConnectorSession session)
     {
         return true;
+    }
+
+    private static JdbcTypeHandle jdbcTypeHandle(int jdbcType)
+    {
+        return new JdbcTypeHandle(jdbcType, Optional.empty(), 0, Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    private static JdbcTypeHandle jdbcTypeHandleWithColumnSize(int jdbcType, int columnSize)
+    {
+        return new JdbcTypeHandle(jdbcType, Optional.empty(), columnSize, Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    private static JdbcTypeHandle jdbcTypeHandleWithDecimalDigits(int jdbcType, int decimalDigits)
+    {
+        return new JdbcTypeHandle(jdbcType, Optional.empty(), 0, Optional.of(decimalDigits), Optional.empty(), Optional.empty());
     }
 }
--- plugins/merged-starburst-stargate/src/test/java/com/starburstdata/presto/plugin/prestoconnector/TestPrestoConnectorIntegrationSmokeTest.java
+++ plugins/merged-starburst-stargate/src/test/java/com/starburstdata/presto/plugin/prestoconnector/TestPrestoConnectorIntegrationSmokeTest.java
@@ -13,6 +13,7 @@ import io.prestosql.sql.planner.plan.AggregationNode;
 import io.prestosql.testing.AbstractTestIntegrationSmokeTest;
 import io.prestosql.testing.DistributedQueryRunner;
 import io.prestosql.testing.QueryRunner;
+import io.prestosql.testing.sql.TestTable;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -21,23 +22,115 @@ import java.util.Map;
 import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.createPrestoConnectorQueryRunner;
 import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.createRemotePrestoQueryRunner;
 import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.prestoConnectorConnectionUrl;
+import static io.airlift.testing.Closeables.closeAllSuppress;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TestPrestoConnectorIntegrationSmokeTest
         extends AbstractTestIntegrationSmokeTest
 {
+    private DistributedQueryRunner remotePresto;
+
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
-        DistributedQueryRunner remotePresto = closeAfterClass(createRemotePrestoQueryRunner(
+        remotePresto = closeAfterClass(createRemotePrestoQueryRunner(
                 Map.of(),
                 List.of()));
-        return createPrestoConnectorQueryRunner(
+        DistributedQueryRunner queryRunner = createPrestoConnectorQueryRunner(
                 false,
                 Map.of(),
                 Map.of("connection-url", prestoConnectorConnectionUrl(remotePresto, "tpch")));
+        try {
+            queryRunner.createCatalog("p2p_memory", "presto-connector", Map.of(
+                    "connection-user", "p2p",
+                    "connection-url", prestoConnectorConnectionUrl(remotePresto, "memory")));
+        }
+        catch (Exception e) {
+            closeAllSuppress(e, queryRunner);
+            throw e;
+        }
+
+        return queryRunner;
+    }
+
+    @Test
+    public void testAggregationPushdown()
+    {
+        // TODO support aggregation pushdown with GROUPING SETS
+        // TODO support aggregation over expressions
+
+        // SELECT DISTINCT
+        assertThat(query("SELECT DISTINCT regionkey FROM nation")).isFullyPushedDown();
+
+        // count()
+        assertThat(query("SELECT count(*) FROM nation")).isFullyPushedDown();
+        assertThat(query("SELECT count(nationkey) FROM nation")).isFullyPushedDown();
+        assertThat(query("SELECT count(1) FROM nation")).isFullyPushedDown();
+        assertThat(query("SELECT count() FROM nation")).isFullyPushedDown();
+        assertThat(query("SELECT count(DISTINCT regionkey) FROM nation")).isNotFullyPushedDown(AggregationNode.class);
+
+        // GROUP BY
+        assertThat(query("SELECT regionkey, min(nationkey) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+        assertThat(query("SELECT regionkey, max(nationkey) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+        assertThat(query("SELECT regionkey, sum(nationkey) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+        assertThat(query("SELECT regionkey, avg(nationkey) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+
+        // GROUP BY and WHERE on bigint column
+        // GROUP BY and WHERE on aggregation key
+        assertThat(query("SELECT regionkey, sum(nationkey) FROM nation WHERE regionkey < 4 GROUP BY regionkey")).isFullyPushedDown();
+
+        // GROUP BY and WHERE on varchar column
+        // GROUP BY and WHERE on "other" (not aggregation key, not aggregation input)
+        assertThat(query("SELECT regionkey, sum(nationkey) FROM nation WHERE regionkey < 4 AND name > 'AAA' GROUP BY regionkey")).isFullyPushedDown();
+
+        // decimals
+        try (TestTable testTable = new TestTable(
+                remotePresto::execute,
+                "memory.default.test_aggregation_pushdown",
+                "(short_decimal decimal(9, 3), long_decimal decimal(30, 10))",
+                List.of(
+                        "(100.000, 100000000.000000000)",
+                        "(123.321, 123456789.987654321)"))) {
+            String tableName = "p2p_" + testTable.getName();
+            assertThat(query("SELECT min(short_decimal), min(long_decimal) FROM " + tableName)).isFullyPushedDown();
+            assertThat(query("SELECT max(short_decimal), max(long_decimal) FROM " + tableName)).isFullyPushedDown();
+            assertThat(query("SELECT sum(short_decimal), sum(long_decimal) FROM " + tableName)).isFullyPushedDown();
+            assertThat(query("SELECT avg(short_decimal), avg(long_decimal) FROM " + tableName)).isFullyPushedDown();
+        }
+    }
+
+    @Test
+    public void testAggregationPushdownWithPrestoSpecificFunctions()
+    {
+        assertThat(query("SELECT approx_distinct(regionkey) FROM nation")).isFullyPushedDown();
+        assertThat(query("SELECT approx_distinct(name) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+
+        assertThat(query("SELECT max_by(name, nationkey) FROM nation GROUP BY regionkey")).isFullyPushedDown();
+
+        // checksum returns varbinary
+        assertThat(query("SELECT checksum(nationkey) FROM nation")).isFullyPushedDown();
+
+        // geometric_mean returns double
+        assertThat(query("SELECT geometric_mean(nationkey) FROM nation")).isFullyPushedDown();
+    }
+
+    @Test
+    public void testAggregationWithUnsupportedResultType()
+    {
+        // array_agg returns array, which is not supported
+        assertThat(query("SELECT array_agg(nationkey) FROM nation")).isNotFullyPushedDown(AggregationNode.class);
+
+        // histogram returns map, which is not supported
+        assertThat(query("SELECT histogram(regionkey) FROM nation")).isNotFullyPushedDown(AggregationNode.class);
+
+        // multimap_agg returns multimap, which is not supported
+        assertThat(query("SELECT multimap_agg(regionkey, nationkey) FROM nation")).isNotFullyPushedDown(AggregationNode.class);
+
+        // approx_set returns HyperLogLog, which is not supported
+        // TODO enable after https://github.com/prestosql/presto/pull/6203
+        // assertThat(query("SELECT approx_set(nationkey) FROM nation")).isNotFullyPushedDown(AggregationNode.class);
     }
 
     @Test
@@ -52,13 +145,13 @@ public class TestPrestoConnectorIntegrationSmokeTest
         assertThat(query("SELECT name FROM nation WHERE name < 'EEE' LIMIT 5")).isFullyPushedDown();
 
         // with aggregation
-        assertThat(query("SELECT max(regionkey) FROM nation LIMIT 5")).isNotFullyPushedDown(AggregationNode.class); // global aggregation, LIMIT removed
-        assertThat(query("SELECT regionkey, max(name) FROM nation GROUP BY regionkey LIMIT 5")).isNotFullyPushedDown(AggregationNode.class);
+        assertThat(query("SELECT max(regionkey) FROM nation LIMIT 5")).isFullyPushedDown(); // global aggregation, LIMIT removed
+        assertThat(query("SELECT regionkey, max(name) FROM nation GROUP BY regionkey LIMIT 5")).isFullyPushedDown();
         assertThat(query("SELECT DISTINCT regionkey FROM nation LIMIT 5")).isFullyPushedDown();
 
         // with filter and aggregation
-        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE nationkey < 5 GROUP BY regionkey LIMIT 3")).isNotFullyPushedDown(AggregationNode.class);
-        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE name < 'EGYPT' GROUP BY regionkey LIMIT 3")).isNotFullyPushedDown(AggregationNode.class);
+        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE nationkey < 5 GROUP BY regionkey LIMIT 3")).isFullyPushedDown();
+        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE name < 'EGYPT' GROUP BY regionkey LIMIT 3")).isFullyPushedDown();
     }
 
     @Test
