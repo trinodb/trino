d6528818f7b002868889906a3807efbbc8a33201
ashasomayajula <somayajulaas@gmail.com>
2020-11-27 22:15:45 -0600

    Revert "Fix for Use assertThatThrownBy in TestSetSessionTask #6082. All relevant methods have been updated to use the assertThatThrownBy method."
    This reverts commit 5b9de7ce

--- presto-main/src/test/java/io/prestosql/execution/TestSetSessionTask.java
+++ presto-main/src/test/java/io/prestosql/execution/TestSetSessionTask.java
@@ -57,7 +57,6 @@ import static io.prestosql.transaction.InMemoryTransactionManager.createTestTran
 import static java.lang.String.format;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.Executors.newCachedThreadPool;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
@@ -152,19 +151,28 @@ public class TestSetSessionTask
     {
         testSetSession("positive_property", new LongLiteral("0"), "0");
         testSetSession("positive_property", new LongLiteral("2"), "2");
-        assertThatThrownBy(() -> testSetSession("positive_property", new LongLiteral("-1"), "-1"))
-                .isInstanceOf(PrestoException.class)
-                .hasMessage(MUST_BE_POSITIVE);
+
+        try {
+            testSetSession("positive_property", new LongLiteral("-1"), "-1");
+            fail();
+        }
+        catch (PrestoException e) {
+            assertEquals(e.getMessage(), MUST_BE_POSITIVE);
+        }
     }
 
     @Test
     public void testSetSessionWithInvalidEnum()
     {
-        assertThatThrownBy(() -> testSetSession("size_property", new StringLiteral("XL"), "XL"))
-                .isInstanceOf(PrestoException.class)
-                .hasMessage("Invalid value [XL]. Valid values: [SMALL, MEDIUM, LARGE]");
+        try {
+            testSetSession("size_property", new StringLiteral("XL"), "XL");
+            fail();
+        }
+        catch (PrestoException e) {
+            assertEquals(INVALID_SESSION_PROPERTY.toErrorCode(), e.getErrorCode());
+            assertEquals("Invalid value [XL]. Valid values: [SMALL, MEDIUM, LARGE]", e.getMessage());
+        }
     }
-
 
     @Test
     public void testSetSessionWithParameters()
