db91859b87ff51f38c9f82ce44162d2d1a286211
GaruGaru <garuglieritommaso.dev@gmail.com>
2020-12-02 18:01:06 +0100

    Glue metastore statistics integration

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/GlueColumnStatisticsProvider.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/GlueColumnStatisticsProvider.java
@@ -13,8 +13,6 @@
  */
 package io.trino.plugin.hive.metastore.glue;
 
-import com.amazonaws.services.glue.model.PartitionInput;
-import com.amazonaws.services.glue.model.TableInput;
 import io.trino.plugin.hive.metastore.HiveColumnStatistics;
 import io.trino.plugin.hive.metastore.Partition;
 import io.trino.plugin.hive.metastore.Table;
@@ -32,7 +30,7 @@ public interface GlueColumnStatisticsProvider
 
     Map<String, HiveColumnStatistics> getPartitionColumnStatistics(Partition partition);
 
-    void updateTableColumnStatistics(TableInput table, Map<String, HiveColumnStatistics> columnStatistics);
+    void updateTableColumnStatistics(Table table, Map<String, HiveColumnStatistics> columnStatistics);
 
-    void updatePartitionStatistics(PartitionInput partition, Map<String, HiveColumnStatistics> columnStatistics);
+    void updatePartitionStatistics(Partition partition, Map<String, HiveColumnStatistics> columnStatistics);
 }
