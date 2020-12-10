91983f82aca659b9a930f80e1f98e19db5e942fc
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-12-10 13:02:13 +0100

    Set default and max domain compaction thresholds for Presto connector
    (cherry-picked from commit: 37cda90f3353d6b2791f541cf1436e6d63a4d304)

--- plugins/merged-starburst-stargate/src/main/java/com/starburstdata/presto/plugin/prestoconnector/PrestoConnectorModule.java
+++ plugins/merged-starburst-stargate/src/main/java/com/starburstdata/presto/plugin/prestoconnector/PrestoConnectorModule.java
@@ -10,21 +10,30 @@
 package com.starburstdata.presto.plugin.prestoconnector;
 
 import com.google.inject.Binder;
+import com.google.inject.Key;
 import com.google.inject.Scopes;
 import com.starburstdata.presto.plugin.jdbc.dynamicfiltering.ForDynamicFiltering;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.prestosql.plugin.jdbc.ForBaseJdbc;
 import io.prestosql.plugin.jdbc.JdbcClient;
+import io.prestosql.plugin.jdbc.JdbcMetadataConfig;
 import io.prestosql.plugin.jdbc.JdbcRecordSetProvider;
 import io.prestosql.plugin.jdbc.JdbcSplitManager;
+import io.prestosql.plugin.jdbc.MaxDomainCompactionThreshold;
 import io.prestosql.spi.connector.ConnectorRecordSetProvider;
 import io.prestosql.spi.connector.ConnectorSplitManager;
 
+import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.configuration.ConfigBinder.configBinder;
 
 public class PrestoConnectorModule
         extends AbstractConfigurationAwareModule
 {
+    // Limit was determined experimentally by running modified TestPrestoConnectorDistributedQueries#testLargeIn.
+    // We started observing parser problems at around 1000 values with cold Presto code
+    // and at around 2000 values after JIT triggered.
+    public static final int PRESTO_CONNECTOR_DEFAULT_DOMAIN_COMPACTION_THRESHOLD = 500;
+
     @Override
     public void setup(Binder binder)
     {
@@ -34,6 +43,14 @@ public class PrestoConnectorModule
 
         binder.bind(ConnectorSplitManager.class).annotatedWith(ForDynamicFiltering.class).to(JdbcSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorRecordSetProvider.class).annotatedWith(ForDynamicFiltering.class).to(JdbcRecordSetProvider.class).in(Scopes.SINGLETON);
+
+        configBinder(binder).bindConfigDefaults(JdbcMetadataConfig.class, config -> {
+            config.setDomainCompactionThreshold(PRESTO_CONNECTOR_DEFAULT_DOMAIN_COMPACTION_THRESHOLD);
+        });
+        // TODO(https://github.com/prestosql/presto/issues/6075, https://starburstdata.atlassian.net/browse/PRESTO-4833)
+        //  Setting threshold to more than 500 may result in stack overflow errors when pushed down query is parsed on remote Presto end.
+        //  we should be able to drop this constraint after fixing linked issue, so `NOT IN` predicates are retained in this for in pushed-down predicate.
+        newOptionalBinder(binder, Key.get(int.class, MaxDomainCompactionThreshold.class)).setBinding().toInstance(PRESTO_CONNECTOR_DEFAULT_DOMAIN_COMPACTION_THRESHOLD);
 
         install(new PrestoConnectorAuthenticationModule());
     }
---
+++ plugins/merged-starburst-stargate/src/test/java/com/starburstdata/presto/plugin/prestoconnector/TestPrestoConnectorWithWritesEnabledExtraTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.prestoconnector;
+
+import io.prestosql.testing.AbstractTestDistributedQueries;
+import io.prestosql.testing.AbstractTestQueryFramework;
+import io.prestosql.testing.DistributedQueryRunner;
+import io.prestosql.testing.QueryRunner;
+import io.prestosql.tpch.TpchTable;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.createPrestoConnectorQueryRunner;
+import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.createRemotePrestoQueryRunner;
+import static com.starburstdata.presto.plugin.prestoconnector.PrestoConnectorQueryRunner.prestoConnectorConnectionUrl;
+import static io.prestosql.testing.sql.TestTable.randomTableSuffix;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.range;
+
+// Extra Presto connector tests which require enabled writes and do not fit
+// in TestPrestoConnectorDistributedQueriesWritesEnabled.
+public class TestPrestoConnectorWithWritesEnabledExtraTests
+        extends AbstractTestQueryFramework
+{
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        DistributedQueryRunner remotePresto = closeAfterClass(createRemotePrestoQueryRunner(
+                Map.of(),
+                TpchTable.getTables()));
+        return createPrestoConnectorQueryRunner(
+                true,
+                Map.of(),
+                Map.of(
+                        "connection-url", prestoConnectorConnectionUrl(remotePresto, "memory"),
+                        "allow-drop-table", "true"));
+    }
+
+    @Test(dataProvider = "largeInValuesCount", dataProviderClass = AbstractTestDistributedQueries.class)
+    public void testLargeInLongColumnName(int valuesCount)
+    {
+        String tableName = "test_large_column_name_" + randomTableSuffix();
+        String columnName = "this_is_a_very_looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong_column_name";
+        assertUpdate(format("CREATE TABLE %s (%s bigint)", tableName, columnName));
+
+        String longValues = range(0, valuesCount)
+                .mapToObj(Integer::toString)
+                .collect(joining(", "));
+
+        assertQuery(format("SELECT * FROM %s WHERE %s IN (%s)", tableName, columnName, longValues), "SELECT 1 WHERE 1=2");
+        assertQuery(format("SELECT * FROM %s WHERE %s NOT IN (%s)", tableName, columnName, longValues), "SELECT 1 WHERE 1=2");
+
+        assertQuery(format("SELECT * FROM %s WHERE %s IN (mod(1000, %s), %s)", tableName, columnName, columnName, longValues), "SELECT 1 WHERE 1=2");
+        assertQuery(format("SELECT * FROM %s WHERE %s NOT IN (mod(1000, %s), %s)", tableName, columnName, columnName, longValues), "SELECT 1 WHERE 1=2");
+    }
+}
