5fdc6da91df7c208191584543e50a3d9ee6b924b
s2lomon <s2lomon@gmail.com>
2020-12-14 20:24:00 +0100

    Add token polling for query submission authenticator

--- core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2Service.java
+++ core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2Service.java
@@ -32,22 +32,25 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.TemporalAmount;
 import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
 import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.base.Verify.verify;
 import static com.google.common.hash.Hashing.sha256;
-import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
 
 public class OAuth2Service
 {
-    private static final String STATE_AUDIENCE = "trino_oauth";
+    private static final String STATE_AUDIENCE_UI = "trino_oauth_ui";
+    private static final String STATE_AUDIENCE_REST = "trino_oauth_rest";
     private static final String FAILURE_REPLACEMENT_TEXT = "<!-- ERROR_MESSAGE -->";
 
     private final OAuth2Client client;
     private final JwtParser jwtParser;
 
+    private final String successHtml;
     private final String failureHtml;
 
     private final TemporalAmount challengeTimeout;
@@ -60,6 +63,7 @@ public class OAuth2Service
         this.client = requireNonNull(client, "client is null");
         this.jwtParser = Jwts.parser().setSigningKeyResolver(signingKeyResolver);
 
+        this.successHtml = Resources.toString(Resources.getResource(getClass(), "/oauth2/success.html"), UTF_8);
         this.failureHtml = Resources.toString(Resources.getResource(getClass(), "/oauth2/failure.html"), UTF_8);
         verify(failureHtml.contains(FAILURE_REPLACEMENT_TEXT), "login.html does not contain the replacement text");
 
@@ -70,11 +74,23 @@ public class OAuth2Service
                 .orElseGet(() -> secureRandomBytes(32));
     }
 
-    public URI startChallenge(URI callbackUri)
+    public URI startWebUiChallenge(URI callbackUri)
     {
         String state = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, stateHmac)
-                .setAudience(STATE_AUDIENCE)
+                .setAudience(STATE_AUDIENCE_UI)
+                .setExpiration(Date.from(Instant.now().plus(challengeTimeout)))
+                .compact();
+
+        return client.getAuthorizationUri(state, callbackUri);
+    }
+
+    public URI startRestChallenge(URI callbackUri, UUID authId)
+    {
+        String state = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, stateHmac)
+                .setId(authId.toString())
+                .setAudience(STATE_AUDIENCE_REST)
                 .setExpiration(Date.from(Instant.now().plus(challengeTimeout)))
                 .compact();
 
@@ -89,9 +105,21 @@ public class OAuth2Service
         requireNonNull(code, "code is null");
 
         Claims stateClaims = parseState(state);
-        if (!STATE_AUDIENCE.equals(stateClaims.getAudience())) {
+        Optional<UUID> authId;
+        if (STATE_AUDIENCE_UI.equals(stateClaims.getAudience())) {
+            authId = Optional.empty();
+        }
+        else if (STATE_AUDIENCE_REST.equals(stateClaims.getAudience())) {
+            try {
+                authId = Optional.of(UUID.fromString(stateClaims.getId()));
+            }
+            catch (IllegalArgumentException e) {
+                throw new ChallengeFailedException("State is does not contain an auth ID");
+            }
+        }
+        else {
             // this is very unlikely, but is a good safety check
-            throw new ChallengeFailedException(format("Unexpected state audience: %s. Expected audience: %s.", stateClaims.getAudience(), STATE_AUDIENCE));
+            throw new ChallengeFailedException("Unexpected state audience");
         }
 
         // fetch access token
@@ -105,7 +133,7 @@ public class OAuth2Service
                 .map(instant -> Ordering.natural().min(instant, parsedToken.getExpiration().toInstant()))
                 .orElse(parsedToken.getExpiration().toInstant());
 
-        return new OAuthResult(accessToken.getAccessToken(), validUntil);
+        return new OAuthResult(authId, accessToken.getAccessToken(), validUntil);
     }
 
     private Claims parseState(String state)
@@ -125,6 +153,11 @@ public class OAuth2Service
     public Jws<Claims> parseClaimsJws(String token)
     {
         return jwtParser.parseClaimsJws(token);
+    }
+
+    public String getSuccessHtml()
+    {
+        return successHtml;
     }
 
     public String getCallbackErrorHtml(String errorCode)
@@ -162,13 +195,24 @@ public class OAuth2Service
 
     public static class OAuthResult
     {
+        private final Optional<UUID> authId;
         private final String accessToken;
         private final Instant tokenExpiration;
 
-        public OAuthResult(String accessToken, Instant tokenExpiration)
+        public OAuthResult(Optional<UUID> authId, String accessToken, Instant tokenExpiration)
         {
+            this.authId = requireNonNull(authId, "authId is null");
             this.accessToken = requireNonNull(accessToken, "accessToken is null");
             this.tokenExpiration = requireNonNull(tokenExpiration, "tokenExpiration is null");
+        }
+
+        /**
+         * Get auth ID if this is a REST client request.
+         * This will be empty if the authentication request is a web UI login.
+         */
+        public Optional<UUID> getAuthId()
+        {
+            return authId;
         }
 
         public String getAccessToken()
