01ae8c61025f74efd1aea6b7eb9a9885ab3e98c6
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-12-22 15:34:23 +0100

    Extract SchemaParser in Kafka Schema Registry

---
+++ presto-kafka/src/main/java/io/prestosql/plugin/kafka/schema/confluent/AvroSchemaParser.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.kafka.schema.confluent;
+
+import com.google.common.collect.ImmutableList;
+import io.prestosql.decoder.avro.AvroRowDecoderFactory;
+import io.prestosql.plugin.kafka.KafkaTopicFieldDescription;
+import io.prestosql.plugin.kafka.KafkaTopicFieldGroup;
+import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.type.Type;
+import io.prestosql.spi.type.TypeManager;
+import org.apache.avro.Schema;
+
+import javax.inject.Inject;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static io.prestosql.plugin.kafka.schema.confluent.ConfluentSessionProperties.getEmptyFieldStrategy;
+import static java.util.Objects.requireNonNull;
+
+public class AvroSchemaParser
+        implements SchemaParser
+{
+    private final TypeManager typeManager;
+
+    @Inject
+    public AvroSchemaParser(TypeManager typeManager)
+    {
+        this.typeManager = requireNonNull(typeManager, "typeManager is null");
+    }
+
+    @Override
+    public KafkaTopicFieldGroup parse(ConnectorSession session, String subject, String rawSchema)
+    {
+        Schema schema = new Schema.Parser().parse(rawSchema);
+        AvroSchemaConverter schemaConverter = new AvroSchemaConverter(typeManager, getEmptyFieldStrategy(session));
+        List<Type> types = schemaConverter.convertAvroSchema(schema);
+        ImmutableList.Builder<KafkaTopicFieldDescription> fieldsBuilder = ImmutableList.builder();
+        if (schema.getType() != Schema.Type.RECORD) {
+            checkState(types.size() == 1, "incompatible schema");
+            fieldsBuilder.add(new KafkaTopicFieldDescription(
+                    subject,
+                    getOnlyElement(types),
+                    subject,
+                    null,
+                    null,
+                    null,
+                    false));
+        }
+        else {
+            List<Schema.Field> avroFields = schema.getFields();
+            checkState(avroFields.size() == types.size(), "incompatible schema");
+
+            for (int i = 0; i < types.size(); i++) {
+                Schema.Field field = avroFields.get(i);
+                fieldsBuilder.add(new KafkaTopicFieldDescription(
+                        field.name(),
+                        types.get(i),
+                        field.name(),
+                        null,
+                        null,
+                        null,
+                        false));
+            }
+        }
+        return new KafkaTopicFieldGroup(AvroRowDecoderFactory.NAME, Optional.empty(), Optional.of(subject), fieldsBuilder.build());
+    }
+}
