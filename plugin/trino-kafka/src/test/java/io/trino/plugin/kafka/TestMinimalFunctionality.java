3c1e1f34d64e6ac27ac0bf0551aa865d76a91a68
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-12-25 20:59:41 +0100

    Code cleanup
     - limit the scope of variable
     - remove extra throws clauses

--- plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/TestMinimalFunctionality.java
+++ plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/TestMinimalFunctionality.java
@@ -56,7 +56,6 @@ public class TestMinimalFunctionality
 
     @AfterClass(alwaysRun = true)
     public void stopKafka()
-            throws Exception
     {
         if (testingKafka != null) {
             testingKafka.close();
