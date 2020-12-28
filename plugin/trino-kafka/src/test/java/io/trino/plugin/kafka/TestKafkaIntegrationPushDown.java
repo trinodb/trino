2ea5e690f73faae13389eaee5bdb6b89620157cb
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-12-28 14:31:40 +0100

    Remove debug timestamp test dump
    It is only useful for local development when debugging issues.
    Likely during debugging simple additional logging would be much easier
    and less complex for that.
    
    This code makes tests code to be less maintainable. As it make tests
    cases non trivial.

--- plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/TestKafkaIntegrationPushDown.java
+++ plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/TestKafkaIntegrationPushDown.java
@@ -13,7 +13,6 @@
  */
 package io.trino.plugin.kafka;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.Futures;
 import io.trino.Session;
@@ -22,7 +21,6 @@ import io.trino.spi.connector.SchemaTableName;
 import io.trino.testing.AbstractTestQueryFramework;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.MaterializedResult;
-import io.trino.testing.MaterializedRow;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.ResultWithQueryId;
 import io.trino.testing.kafka.BasicTestingKafka;
@@ -35,7 +33,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -138,9 +135,7 @@ public class TestKafkaIntegrationPushDown
 
         // timestamp_upper_bound_force_push_down_enabled default as false.
         ResultWithQueryId<MaterializedResult> queryResult = queryRunner.executeWithQueryId(getSession(), sql);
-        String debugDumpString = buildDebugDumpString(topicNameCreateTime, recordMessage, queryRunner, sql, queryResult);
         assertThat(getQueryInfo(queryRunner, queryResult).getQueryStats().getProcessedInputPositions())
-                .describedAs("with dump:\n%s", debugDumpString)
                 .isEqualTo(998);
 
         // timestamp_upper_bound_force_push_down_enabled set as true.
@@ -149,9 +144,7 @@ public class TestKafkaIntegrationPushDown
                 .build();
 
         queryResult = queryRunner.executeWithQueryId(sessionWithUpperBoundPushDownEnabled, sql);
-        debugDumpString = buildDebugDumpString(topicNameCreateTime, recordMessage, queryRunner, sql, queryResult);
         assertThat(getQueryInfo(queryRunner, queryResult).getQueryStats().getProcessedInputPositions())
-                .describedAs("with dump:\n%s", debugDumpString)
                 .isEqualTo(2);
     }
 
@@ -169,55 +162,8 @@ public class TestKafkaIntegrationPushDown
                 recordMessage.getEndTime());
         ResultWithQueryId<MaterializedResult> queryResult = queryRunner.executeWithQueryId(getSession(), sql);
 
-        String debugDumpString = buildDebugDumpString(topicNameLogAppend, recordMessage, queryRunner, sql, queryResult);
         assertThat(getQueryInfo(queryRunner, queryResult).getQueryStats().getProcessedInputPositions())
-                .describedAs("with dump:\n%s", debugDumpString)
                 .isEqualTo(2);
-    }
-
-    private String buildDebugDumpString(String topic,
-            RecordMessage recordMessage,
-            DistributedQueryRunner queryRunner,
-            String sql,
-            ResultWithQueryId<MaterializedResult> queryResult)
-    {
-        StringBuilder sb = new StringBuilder();
-        // dump main messages
-        sb.append("Main messages:").append("\n");
-        for (int i = 0; i < recordMessage.getTestMessageSignatures().size(); i++) {
-            String dumpMessage = recordMessage.getTestMessageSignatures().get(i);
-            sb.append("  ").append(dumpMessage);
-            if (i == TIMESTAMP_TEST_START_INDEX) {
-                sb.append(", startTime:").append(recordMessage.getStartTime());
-            }
-            else if (i == TIMESTAMP_TEST_END_INDEX) {
-                sb.append(", endTime:").append(recordMessage.getEndTime());
-            }
-            sb.append("\n");
-        }
-
-        // dump sql and result
-        sb.append(format("test sql:%s\n", sql));
-        sb.append("test sql result:").append("\n").append(buildResultsDebugDumpString(queryResult.getResult()));
-
-        // dump data in kafka
-        sql = format(
-                "SELECT _partition_id,_partition_offset,_timestamp FROM default.%s WHERE _partition_offset between %s and %s order by _partition_id, _timestamp",
-                topic,
-                recordMessage.getStartOffset(),
-                recordMessage.getStartOffset() + MESSAGE_NUM);
-        MaterializedResult rows = queryRunner.execute(getSession(), sql);
-        sb.append("data check result:").append("\n").append(buildResultsDebugDumpString(rows)).append("\n");
-        return sb.toString();
-    }
-
-    private static String buildResultsDebugDumpString(MaterializedResult rows)
-    {
-        StringBuilder sb = new StringBuilder();
-        for (MaterializedRow row : rows) {
-            sb.append(format("  dump values:%s\n", row.toString()));
-        }
-        return sb.toString();
     }
 
     private static QueryInfo getQueryInfo(DistributedQueryRunner queryRunner, ResultWithQueryId<MaterializedResult> queryResult)
@@ -230,8 +176,6 @@ public class TestKafkaIntegrationPushDown
     {
         String startTime = null;
         String endTime = null;
-        long startOffset = -1;
-        ImmutableList.Builder<String> testMethodSignatures = ImmutableList.builder();
         Future<RecordMetadata> lastSendFuture = Futures.immediateFuture(null);
         long lastTimeStamp = -1;
         // Avoid last test case has impact on this test case when invocationCount of @Test enabled
@@ -246,11 +190,7 @@ public class TestKafkaIntegrationPushDown
                     RecordMetadata r = lastSendFuture.get();
                     assertTrue(lastTimeStamp != r.timestamp());
                     lastTimeStamp = r.timestamp();
-                    testMethodSignatures.add(format("timestamp:%s: partitionId:%s, offset:%s", r.timestamp(), r.partition(), r.offset()));
-                    if (messageNum == 0) {
-                        startOffset = r.offset();
-                    }
-                    else if (messageNum == TIMESTAMP_TEST_START_INDEX) {
+                    if (messageNum == TIMESTAMP_TEST_START_INDEX) {
                         startTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
                                 .format(LocalDateTime.ofInstant(Instant.ofEpochMilli(r.timestamp()), ZoneId.of("UTC")));
                     }
@@ -266,7 +206,7 @@ public class TestKafkaIntegrationPushDown
         lastSendFuture.get();
         requireNonNull(startTime, "startTime result is none");
         requireNonNull(endTime, "endTime result is none");
-        return new RecordMessage(startTime, endTime, startOffset, testMethodSignatures.build());
+        return new RecordMessage(startTime, endTime);
     }
 
     private void createMessages(String topicName)
@@ -287,15 +227,11 @@ public class TestKafkaIntegrationPushDown
     {
         private final String startTime;
         private final String endTime;
-        private final long startOffset;
-        private final List<String> testMessageSignatures;
 
-        public RecordMessage(String startTime, String endTime, Long startOffset, List<String> testMessageSignatures)
+        public RecordMessage(String startTime, String endTime)
         {
             this.startTime = startTime;
             this.endTime = endTime;
-            this.startOffset = startOffset;
-            this.testMessageSignatures = testMessageSignatures;
         }
 
         public String getStartTime()
@@ -306,16 +242,6 @@ public class TestKafkaIntegrationPushDown
         public String getEndTime()
         {
             return endTime;
-        }
-
-        public Long getStartOffset()
-        {
-            return startOffset;
-        }
-
-        public List<String> getTestMessageSignatures()
-        {
-            return testMessageSignatures;
         }
     }
 }
