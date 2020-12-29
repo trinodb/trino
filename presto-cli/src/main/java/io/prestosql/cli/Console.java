knwg145/presto

Adds set session authorization as a command

presto-cli/src/main/java/io/prestosql/cli/Console.java
@@ -364,6 +364,10 @@ public class Console
 
             ClientSession.Builder builder = ClientSession.builder(session);
 
+            if (query.getAuthorizationUser().isPresent()) {
+                builder = builder.withAuthorizationUser(query.getAuthorizationUser());
+            }
+
             if (query.getStartedTransactionId() != null) {
                 builder = builder.withTransactionId(query.getStartedTransactionId());
             }

presto-main/src/main/java/io/prestosql/server/SessionContext.java
@@ -29,6 +29,8 @@ public interface SessionContext
 
     Identity getIdentity();
 
+    Identity getAuthorizationIdentity();
+
     @Nullable
     String getCatalog();
 

trinodb/trino

presto-cli/src/main/java/io/prestosql/cli/Console.java
@@ -372,6 +372,10 @@ public class Console
 
             ClientSession.Builder builder = ClientSession.builder(session);
 
+            if (query.getSetAuthorizationUser().isPresent()) {
+                builder = builder.withAuthorizationUser(query.getSetAuthorizationUser());
+            }
+
             if (query.getStartedTransactionId() != null) {
                 builder = builder.withTransactionId(query.getStartedTransactionId());
             }

presto-main/src/main/java/io/prestosql/server/SessionContext.java
@@ -27,6 +27,8 @@ public interface SessionContext
 {
     Optional<Identity> getAuthenticatedIdentity();
 
+    Identity getOriginalIdentity();
+
     Identity getIdentity();
 
     @Nullable