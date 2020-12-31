0f6d36787bcbd9f0fc7d53039b9a684d77be80a6
David Phillips <david@acz.org>
2020-12-30 19:18:53 -0800

    Move docker directory to core

--- core/docker/build-local.sh
+++ core/docker/build-local.sh
@@ -2,22 +2,24 @@
 
 set -euxo pipefail
 
+SOURCE_DIR="../.."
+
 # Retrieve the script directory.
 SCRIPT_DIR="${BASH_SOURCE%/*}"
 cd ${SCRIPT_DIR}
 
 # Move to the root directory to run maven for current version.
-pushd ..
+pushd ${SOURCE_DIR}
 PRESTO_VERSION=$(./mvnw --quiet help:evaluate -Dexpression=project.version -DforceStdout)
 popd
 
 WORK_DIR="$(mktemp -d)"
-cp ../presto-server/target/presto-server-${PRESTO_VERSION}.tar.gz ${WORK_DIR}
+cp ${SOURCE_DIR}/presto-server/target/presto-server-${PRESTO_VERSION}.tar.gz ${WORK_DIR}
 tar -C ${WORK_DIR} -xzf ${WORK_DIR}/presto-server-${PRESTO_VERSION}.tar.gz
 rm ${WORK_DIR}/presto-server-${PRESTO_VERSION}.tar.gz
 cp -R bin default ${WORK_DIR}/presto-server-${PRESTO_VERSION}
 
-cp ../presto-cli/target/presto-cli-${PRESTO_VERSION}-executable.jar ${WORK_DIR}
+cp ${SOURCE_DIR}/presto-cli/target/presto-cli-${PRESTO_VERSION}-executable.jar ${WORK_DIR}
 
 docker build ${WORK_DIR} --pull -f Dockerfile --build-arg "PRESTO_VERSION=${PRESTO_VERSION}" -t "presto:${PRESTO_VERSION}"
 
