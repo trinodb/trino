phd3/presto-1

Implement getTableProperties for Iceberg Connector

presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergMetadata.java
@@ -17,6 +17,7 @@ import com.google.common.base.Splitter;
 import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import io.airlift.json.JsonCodec;
 import io.airlift.log.Logger;
 import io.airlift.slice.Slice;
@@ -49,6 +50,7 @@ import io.prestosql.spi.connector.ConnectorTableMetadata;
 import io.prestosql.spi.connector.ConnectorTableProperties;
 import io.prestosql.spi.connector.Constraint;
 import io.prestosql.spi.connector.ConstraintApplicationResult;
+import io.prestosql.spi.connector.DiscretePredicates;
 import io.prestosql.spi.connector.MaterializedViewFreshness;
 import io.prestosql.spi.connector.MaterializedViewNotFoundException;
 import io.prestosql.spi.connector.SchemaNotFoundException;
@@ -57,6 +59,7 @@ import io.prestosql.spi.connector.SchemaTablePrefix;
 import io.prestosql.spi.connector.SystemTable;
 import io.prestosql.spi.connector.TableNotFoundException;
 import io.prestosql.spi.predicate.Domain;
+import io.prestosql.spi.predicate.NullableValue;
 import io.prestosql.spi.predicate.TupleDomain;
 import io.prestosql.spi.security.PrestoPrincipal;
 import io.prestosql.spi.statistics.ComputedStatistics;
@@ -66,6 +69,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.iceberg.AppendFiles;
 import org.apache.iceberg.DataFiles;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.PartitionField;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.PartitionSpecParser;
@@ -74,7 +78,9 @@ import org.apache.iceberg.SchemaParser;
 import org.apache.iceberg.Snapshot;
 import org.apache.iceberg.TableMetadata;
 import org.apache.iceberg.TableOperations;
+import org.apache.iceberg.TableScan;
 import org.apache.iceberg.Transaction;
+import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.types.Types;
@@ -94,6 +100,7 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -118,10 +125,12 @@ import static io.prestosql.plugin.iceberg.IcebergTableProperties.PARTITIONING_PR
 import static io.prestosql.plugin.iceberg.IcebergTableProperties.getFileFormat;
 import static io.prestosql.plugin.iceberg.IcebergTableProperties.getPartitioning;
 import static io.prestosql.plugin.iceberg.IcebergTableProperties.getTableLocation;
+import static io.prestosql.plugin.iceberg.IcebergUtil.deserializePartitionValue;
 import static io.prestosql.plugin.iceberg.IcebergUtil.getColumns;
 import static io.prestosql.plugin.iceberg.IcebergUtil.getDataPath;
 import static io.prestosql.plugin.iceberg.IcebergUtil.getFileFormat;
 import static io.prestosql.plugin.iceberg.IcebergUtil.getIcebergTable;
+import static io.prestosql.plugin.iceberg.IcebergUtil.getPartitionKeys;
 import static io.prestosql.plugin.iceberg.IcebergUtil.getTableComment;
 import static io.prestosql.plugin.iceberg.IcebergUtil.isIcebergTable;
 import static io.prestosql.plugin.iceberg.PartitionFields.parsePartitionFields;
@@ -270,7 +279,67 @@ public class IcebergMetadata
     @Override
     public ConnectorTableProperties getTableProperties(ConnectorSession session, ConnectorTableHandle tableHandle)
     {
-        return new ConnectorTableProperties();
+        IcebergTableHandle table = (IcebergTableHandle) tableHandle;
+        org.apache.iceberg.Table icebergTable = getIcebergTable(metastore, hdfsEnvironment, session, table.getSchemaTableName());
+
+        TupleDomain<IcebergColumnHandle> enforcedPredicate = table.getEnforcedPredicate();
+
+        TableScan tableScan = icebergTable.newScan()
+                .useSnapshot(table.getSnapshotId().get())
+                .filter(toIcebergExpression(enforcedPredicate))
+                .includeColumnStats();
+
+        CloseableIterable<FileScanTask> files = tableScan.planFiles();
+
+        // Extract identity partition fields that are present in all partition specs, for creating the discrete predicates.
+        Set<Integer> partitionSourceIds = identityPartitionColumnsInAllSpecs(icebergTable);
+
+        DiscretePredicates discretePredicates = null;
+        if (!partitionSourceIds.isEmpty()) {
+            // Extract identity partition columns
+            Map<Integer, IcebergColumnHandle> columns = getColumns(icebergTable.schema(), typeManager).stream()
+                    .filter(column -> partitionSourceIds.contains(column.getId()))
+                    .collect(toImmutableMap(IcebergColumnHandle::getId, Function.identity()));
+
+            Iterable<TupleDomain<ColumnHandle>> discreteTupleDomain = Iterables.transform(files, fileScan -> {
+                // Extract partition values in the data file
+                Map<Integer, String> partitionColumnValueStrings = getPartitionKeys(fileScan);
+                Map<ColumnHandle, NullableValue> partitionValues = partitionSourceIds.stream()
+                        .filter(partitionColumnValueStrings::containsKey)
+                        .collect(toImmutableMap(
+                                columns::get,
+                                columnId -> {
+                                    IcebergColumnHandle column = columns.get(columnId);
+                                    Object prestoValue = deserializePartitionValue(
+                                            column.getType(),
+                                            partitionColumnValueStrings.get(columnId),
+                                            column.getName(),
+                                            session.getTimeZoneKey());
+
+                                    return NullableValue.of(column.getType(), prestoValue);
+                                }));
+
+                return TupleDomain.fromFixedValues(partitionValues);
+            });
+
+            discretePredicates = new DiscretePredicates(
+                    columns.values().stream()
+                            .map(ColumnHandle.class::cast)
+                            .collect(toImmutableList()),
+                    discreteTupleDomain);
+        }
+
+        return new ConnectorTableProperties(
+                // Using the predicate here directly avoids eagerly loading all partition values. Logically, this
+                // still keeps predicate and discretePredicates evaluation the same on every row of the table. This
+                // can be further optimized by intersecting with partition values at the cost of iterating
+                // over all tableScan.planFiles() and caching partition values in table handle.
+                enforcedPredicate.transform(ColumnHandle.class::cast),
+                // TODO: implement table partitioning
+                Optional.empty(),
+                Optional.empty(),
+                Optional.ofNullable(discretePredicates),
+                ImmutableList.of());
     }
 
     @Override

presto-iceberg/src/test/java/io/prestosql/plugin/iceberg/TestMetadataQueryOptimization.java
@@ -1,0 +1,119 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.iceberg;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
+import io.prestosql.Session;
+import io.prestosql.plugin.hive.authentication.HiveIdentity;
+import io.prestosql.plugin.hive.metastore.Database;
+import io.prestosql.plugin.hive.metastore.HiveMetastore;
+import io.prestosql.spi.security.PrincipalType;
+import io.prestosql.sql.planner.assertions.BasePushdownPlanTest;
+import io.prestosql.sql.tree.LongLiteral;
+import io.prestosql.testing.LocalQueryRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Optional;
+
+import static com.google.common.io.MoreFiles.deleteRecursively;
+import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
+import static io.prestosql.plugin.hive.metastore.file.FileHiveMetastore.createTestingFileHiveMetastore;
+import static io.prestosql.sql.planner.assertions.PlanMatchPattern.anyTree;
+import static io.prestosql.sql.planner.assertions.PlanMatchPattern.values;
+import static io.prestosql.testing.TestingSession.testSessionBuilder;
+import static java.lang.String.format;
+
+public class TestMetadataQueryOptimization
+        extends BasePushdownPlanTest
+{
+    private static final String ICEBERG_CATALOG = "iceberg";
+    private static final String SCHEMA_NAME = "test_schema";
+    private File baseDir;
+
+    @Override
+    protected LocalQueryRunner createLocalQueryRunner()
+    {
+        Session session = testSessionBuilder()
+                .setCatalog(ICEBERG_CATALOG)
+                .setSchema(SCHEMA_NAME)
+                .build();
+
+        baseDir = Files.createTempDir();
+        HiveMetastore metastore = createTestingFileHiveMetastore(baseDir);
+        LocalQueryRunner queryRunner = LocalQueryRunner.create(session);
+
+        queryRunner.createCatalog(
+                ICEBERG_CATALOG,
+                new TestingIcebergConnectorFactory(Optional.of(metastore)),
+                ImmutableMap.of());
+
+        Database database = Database.builder()
+                .setDatabaseName(SCHEMA_NAME)
+                .setOwnerName("public")
+                .setOwnerType(PrincipalType.ROLE)
+                .build();
+        metastore.createDatabase(new HiveIdentity(session.toConnectorSession()), database);
+
+        return queryRunner;
+    }
+
+    @Test
+    public void testOptimization()
+    {
+        String testTable = "test_metadata_optimization";
+
+        getQueryRunner().execute(format(
+                "CREATE TABLE %s (a, b, c) WITH (PARTITIONING = ARRAY['b', 'c']) AS VALUES (5, 6, 7), (8, 9, 10)",
+                testTable));
+
+        Session session = Session.builder(getQueryRunner().getDefaultSession())
+                .setSystemProperty("optimize_metadata_queries", "true")
+                .build();
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s ORDER BY b", testTable),
+                session,
+                anyTree(values(
+                        ImmutableList.of("b", "c"),
+                        ImmutableList.of(
+                                ImmutableList.of(new LongLiteral("6"), new LongLiteral("7")),
+                                ImmutableList.of(new LongLiteral("9"), new LongLiteral("10"))))));
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s WHERE b > 7", testTable),
+                session,
+                anyTree(values(
+                        ImmutableList.of("b", "c"),
+                        ImmutableList.of(ImmutableList.of(new LongLiteral("9"), new LongLiteral("10"))))));
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s WHERE b > 7 AND c < 8", testTable),
+                session,
+                anyTree(
+                        values(ImmutableList.of("b", "c"), ImmutableList.of())));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanup()
+            throws Exception
+    {
+        if (baseDir != null) {
+            deleteRecursively(baseDir.toPath(), ALLOW_INSECURE);
+        }
+    }
+}

trinodb/trino

plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -17,6 +17,7 @@ import com.google.common.base.Splitter;
 import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import io.airlift.json.JsonCodec;
 import io.airlift.log.Logger;
 import io.airlift.slice.Slice;
@@ -49,6 +50,7 @@ import io.trino.spi.connector.ConnectorTableMetadata;
 import io.trino.spi.connector.ConnectorTableProperties;
 import io.trino.spi.connector.Constraint;
 import io.trino.spi.connector.ConstraintApplicationResult;
+import io.trino.spi.connector.DiscretePredicates;
 import io.trino.spi.connector.MaterializedViewFreshness;
 import io.trino.spi.connector.MaterializedViewNotFoundException;
 import io.trino.spi.connector.SchemaNotFoundException;
@@ -57,6 +59,7 @@ import io.trino.spi.connector.SchemaTablePrefix;
 import io.trino.spi.connector.SystemTable;
 import io.trino.spi.connector.TableNotFoundException;
 import io.trino.spi.predicate.Domain;
+import io.trino.spi.predicate.NullableValue;
 import io.trino.spi.predicate.TupleDomain;
 import io.trino.spi.security.TrinoPrincipal;
 import io.trino.spi.statistics.ComputedStatistics;
@@ -66,6 +69,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.iceberg.AppendFiles;
 import org.apache.iceberg.DataFiles;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.PartitionField;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.PartitionSpecParser;
@@ -74,7 +78,9 @@ import org.apache.iceberg.SchemaParser;
 import org.apache.iceberg.Snapshot;
 import org.apache.iceberg.TableMetadata;
 import org.apache.iceberg.TableOperations;
+import org.apache.iceberg.TableScan;
 import org.apache.iceberg.Transaction;
+import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.types.Types;
@@ -94,6 +100,7 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -118,10 +125,12 @@ import static io.trino.plugin.iceberg.IcebergTableProperties.PARTITIONING_PROPER
 import static io.trino.plugin.iceberg.IcebergTableProperties.getFileFormat;
 import static io.trino.plugin.iceberg.IcebergTableProperties.getPartitioning;
 import static io.trino.plugin.iceberg.IcebergTableProperties.getTableLocation;
+import static io.trino.plugin.iceberg.IcebergUtil.deserializePartitionValue;
 import static io.trino.plugin.iceberg.IcebergUtil.getColumns;
 import static io.trino.plugin.iceberg.IcebergUtil.getDataPath;
 import static io.trino.plugin.iceberg.IcebergUtil.getFileFormat;
 import static io.trino.plugin.iceberg.IcebergUtil.getIcebergTable;
+import static io.trino.plugin.iceberg.IcebergUtil.getPartitionKeys;
 import static io.trino.plugin.iceberg.IcebergUtil.getTableComment;
 import static io.trino.plugin.iceberg.IcebergUtil.isIcebergTable;
 import static io.trino.plugin.iceberg.PartitionFields.parsePartitionFields;
@@ -270,7 +279,67 @@ public class IcebergMetadata
     @Override
     public ConnectorTableProperties getTableProperties(ConnectorSession session, ConnectorTableHandle tableHandle)
     {
-        return new ConnectorTableProperties();
+        IcebergTableHandle table = (IcebergTableHandle) tableHandle;
+        org.apache.iceberg.Table icebergTable = getIcebergTable(metastore, hdfsEnvironment, session, table.getSchemaTableName());
+
+        TupleDomain<IcebergColumnHandle> enforcedPredicate = table.getEnforcedPredicate();
+
+        TableScan tableScan = icebergTable.newScan()
+                .useSnapshot(table.getSnapshotId().get())
+                .filter(toIcebergExpression(enforcedPredicate))
+                .includeColumnStats();
+
+        CloseableIterable<FileScanTask> files = tableScan.planFiles();
+
+        // Extract identity partition fields that are present in all partition specs, for creating the discrete predicates.
+        Set<Integer> partitionSourceIds = identityPartitionColumnsInAllSpecs(icebergTable);
+
+        DiscretePredicates discretePredicates = null;
+        if (!partitionSourceIds.isEmpty()) {
+            // Extract identity partition columns
+            Map<Integer, IcebergColumnHandle> columns = getColumns(icebergTable.schema(), typeManager).stream()
+                    .filter(column -> partitionSourceIds.contains(column.getId()))
+                    .collect(toImmutableMap(IcebergColumnHandle::getId, Function.identity()));
+
+            Iterable<TupleDomain<ColumnHandle>> discreteTupleDomain = Iterables.transform(files, fileScan -> {
+                // Extract partition values in the data file
+                Map<Integer, String> partitionColumnValueStrings = getPartitionKeys(fileScan);
+                Map<ColumnHandle, NullableValue> partitionValues = partitionSourceIds.stream()
+                        .filter(partitionColumnValueStrings::containsKey)
+                        .collect(toImmutableMap(
+                                columns::get,
+                                columnId -> {
+                                    IcebergColumnHandle column = columns.get(columnId);
+                                    Object prestoValue = deserializePartitionValue(
+                                            column.getType(),
+                                            partitionColumnValueStrings.get(columnId),
+                                            column.getName(),
+                                            session.getTimeZoneKey());
+
+                                    return NullableValue.of(column.getType(), prestoValue);
+                                }));
+
+                return TupleDomain.fromFixedValues(partitionValues);
+            });
+
+            discretePredicates = new DiscretePredicates(
+                    columns.values().stream()
+                            .map(ColumnHandle.class::cast)
+                            .collect(toImmutableList()),
+                    discreteTupleDomain);
+        }
+
+        return new ConnectorTableProperties(
+                // Using the predicate here directly avoids eagerly loading all partition values. Logically, this
+                // still keeps predicate and discretePredicates evaluation the same on every row of the table. This
+                // can be further optimized by intersecting with partition values at the cost of iterating
+                // over all tableScan.planFiles() and caching partition values in table handle.
+                enforcedPredicate.transform(ColumnHandle.class::cast),
+                // TODO: implement table partitioning
+                Optional.empty(),
+                Optional.empty(),
+                Optional.ofNullable(discretePredicates),
+                ImmutableList.of());
     }
 
     @Override

plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestMetadataQueryOptimization.java
@@ -1,0 +1,120 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.iceberg;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
+import io.trino.Session;
+import io.trino.plugin.hive.authentication.HiveIdentity;
+import io.trino.plugin.hive.metastore.Database;
+import io.trino.plugin.hive.metastore.HiveMetastore;
+import io.trino.plugin.iceberg.TestingIcebergConnectorFactory;
+import io.trino.spi.security.PrincipalType;
+import io.trino.sql.planner.assertions.BasePushdownPlanTest;
+import io.trino.sql.tree.LongLiteral;
+import io.trino.testing.LocalQueryRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Optional;
+
+import static com.google.common.io.MoreFiles.deleteRecursively;
+import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
+import static io.trino.plugin.hive.metastore.file.FileHiveMetastore.createTestingFileHiveMetastore;
+import static io.trino.sql.planner.assertions.PlanMatchPattern.anyTree;
+import static io.trino.sql.planner.assertions.PlanMatchPattern.values;
+import static io.trino.testing.TestingSession.testSessionBuilder;
+import static java.lang.String.format;
+
+public class TestMetadataQueryOptimization
+        extends BasePushdownPlanTest
+{
+    private static final String ICEBERG_CATALOG = "iceberg";
+    private static final String SCHEMA_NAME = "test_schema";
+    private File baseDir;
+
+    @Override
+    protected LocalQueryRunner createLocalQueryRunner()
+    {
+        Session session = testSessionBuilder()
+                .setCatalog(ICEBERG_CATALOG)
+                .setSchema(SCHEMA_NAME)
+                .build();
+
+        baseDir = Files.createTempDir();
+        HiveMetastore metastore = createTestingFileHiveMetastore(baseDir);
+        LocalQueryRunner queryRunner = LocalQueryRunner.create(session);
+
+        queryRunner.createCatalog(
+                ICEBERG_CATALOG,
+                new TestingIcebergConnectorFactory(Optional.of(metastore)),
+                ImmutableMap.of());
+
+        Database database = Database.builder()
+                .setDatabaseName(SCHEMA_NAME)
+                .setOwnerName("public")
+                .setOwnerType(PrincipalType.ROLE)
+                .build();
+        metastore.createDatabase(new HiveIdentity(session.toConnectorSession()), database);
+
+        return queryRunner;
+    }
+
+    @Test
+    public void testOptimization()
+    {
+        String testTable = "test_metadata_optimization";
+
+        getQueryRunner().execute(format(
+                "CREATE TABLE %s (a, b, c) WITH (PARTITIONING = ARRAY['b', 'c']) AS VALUES (5, 6, 7), (8, 9, 10)",
+                testTable));
+
+        Session session = Session.builder(getQueryRunner().getDefaultSession())
+                .setSystemProperty("optimize_metadata_queries", "true")
+                .build();
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s ORDER BY b", testTable),
+                session,
+                anyTree(values(
+                        ImmutableList.of("b", "c"),
+                        ImmutableList.of(
+                                ImmutableList.of(new LongLiteral("6"), new LongLiteral("7")),
+                                ImmutableList.of(new LongLiteral("9"), new LongLiteral("10"))))));
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s WHERE b > 7", testTable),
+                session,
+                anyTree(values(
+                        ImmutableList.of("b", "c"),
+                        ImmutableList.of(ImmutableList.of(new LongLiteral("9"), new LongLiteral("10"))))));
+
+        assertPlan(
+                format("SELECT DISTINCT b, c FROM %s WHERE b > 7 AND c < 8", testTable),
+                session,
+                anyTree(
+                        values(ImmutableList.of("b", "c"), ImmutableList.of())));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanup()
+            throws Exception
+    {
+        if (baseDir != null) {
+            deleteRecursively(baseDir.toPath(), ALLOW_INSECURE);
+        }
+    }
+}