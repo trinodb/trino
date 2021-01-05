shiroyuki/presto

Add support for version function

presto-main/src/main/java/io/prestosql/metadata/FunctionRegistry.java
@@ -141,6 +141,7 @@ import io.prestosql.operator.scalar.TryFunction;
 import io.prestosql.operator.scalar.TypeOfFunction;
 import io.prestosql.operator.scalar.UrlFunctions;
 import io.prestosql.operator.scalar.VarbinaryFunctions;
+import io.prestosql.operator.scalar.VersionFunction;
 import io.prestosql.operator.scalar.WilsonInterval;
 import io.prestosql.operator.scalar.WordStemFunction;
 import io.prestosql.operator.scalar.timestamp.DateAdd;
@@ -378,7 +379,7 @@ public class FunctionRegistry
     private final Cache<FunctionBinding, WindowFunctionSupplier> specializedWindowCache;
     private volatile FunctionMap functions = new FunctionMap();
 
-    public FunctionRegistry(Supplier<BlockEncodingSerde> blockEncodingSerdeSupplier, FeaturesConfig featuresConfig)
+    public FunctionRegistry(Supplier<BlockEncodingSerde> blockEncodingSerdeSupplier, String nodeVersion, FeaturesConfig featuresConfig)
     {
         // We have observed repeated compilation of MethodHandle that leads to full GCs.
         // We notice that flushing the following caches mitigate the problem.
@@ -632,6 +633,7 @@ public class FunctionRegistry
                 .function(FORMAT_FUNCTION)
                 .function(TRY_CAST)
                 .function(new LiteralFunction(blockEncodingSerdeSupplier))
+                .function(new VersionFunction(nodeVersion))
                 .aggregate(MergeSetDigestAggregation.class)
                 .aggregate(BuildSetDigestAggregation.class)
                 .scalars(SetDigestFunctions.class)

presto-main/src/main/java/io/prestosql/metadata/MetadataManager.java
@@ -22,6 +22,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import io.airlift.slice.Slice;
 import io.prestosql.Session;
+import io.prestosql.client.NodeVersion;
 import io.prestosql.connector.CatalogName;
 import io.prestosql.metadata.ResolvedFunction.ResolvedFunctionDecoder;
 import io.prestosql.operator.aggregation.InternalAggregationFunction;
@@ -193,10 +194,12 @@ public final class MetadataManager
             TablePropertyManager tablePropertyManager,
             ColumnPropertyManager columnPropertyManager,
             AnalyzePropertyManager analyzePropertyManager,
-            TransactionManager transactionManager)
+            TransactionManager transactionManager,
+            NodeVersion nodeVersion)
     {
+        requireNonNull(nodeVersion, "nodeVersion is null");
         typeRegistry = new TypeRegistry(featuresConfig);
-        functions = new FunctionRegistry(this::getBlockEncodingSerde, featuresConfig);
+        functions = new FunctionRegistry(this::getBlockEncodingSerde, nodeVersion.getVersion(), featuresConfig);
         functionResolver = new FunctionResolver(this);
 
         this.procedures = new ProcedureRegistry(this);
@@ -258,7 +261,8 @@ public final class MetadataManager
                 new TablePropertyManager(),
                 new ColumnPropertyManager(),
                 new AnalyzePropertyManager(),
-                transactionManager);
+                transactionManager,
+                NodeVersion.UNKNOWN);
     }
 
     @Override

presto-main/src/main/java/io/prestosql/operator/scalar/VersionFunction.java
@@ -1,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.operator.scalar;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.slice.Slice;
+import io.prestosql.annotation.UsedByGeneratedCode;
+import io.prestosql.metadata.FunctionBinding;
+import io.prestosql.metadata.FunctionMetadata;
+import io.prestosql.metadata.Signature;
+import io.prestosql.metadata.SqlScalarFunction;
+
+import java.lang.invoke.MethodHandle;
+
+import static io.airlift.slice.Slices.utf8Slice;
+import static io.prestosql.metadata.FunctionKind.SCALAR;
+import static io.prestosql.spi.function.InvocationConvention.InvocationReturnConvention.FAIL_ON_NULL;
+import static io.prestosql.spi.type.VarcharType.VARCHAR;
+import static io.prestosql.util.Reflection.methodHandle;
+
+public final class VersionFunction
+        extends SqlScalarFunction
+{
+    private static final MethodHandle METHOD_HANDLE = methodHandle(VersionFunction.class, "getVersion", String.class);
+    private final String nodeVersion;
+
+    public VersionFunction(String nodeVersion)
+    {
+        super(new FunctionMetadata(
+                new Signature(
+                        "version",
+                        ImmutableList.of(),
+                        ImmutableList.of(),
+                        VARCHAR.getTypeSignature(),
+                        ImmutableList.of(),
+                        false),
+                false,
+                ImmutableList.of(),
+                true,
+                true,
+                "Return server version",
+                SCALAR));
+        this.nodeVersion = nodeVersion;
+    }
+
+    @Override
+    public ScalarFunctionImplementation specialize(FunctionBinding functionBinding)
+    {
+        MethodHandle methodHandle = METHOD_HANDLE.bindTo(nodeVersion);
+        return new ScalarFunctionImplementation(
+                FAIL_ON_NULL,
+                ImmutableList.of(),
+                methodHandle);
+    }
+
+    @UsedByGeneratedCode
+    public static Slice getVersion(String version)
+    {
+        return utf8Slice(version);
+    }
+}

presto-main/src/test/java/io/prestosql/operator/scalar/TestVersionFunction.java
@@ -1,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.operator.scalar;
+
+import org.testng.annotations.Test;
+
+import static io.prestosql.spi.type.VarcharType.VARCHAR;
+
+public class TestVersionFunction
+        extends AbstractTestFunctions
+{
+    @Test
+    public void testVersion()
+    {
+        assertFunction("version()", VARCHAR, "<unknown>");
+    }
+}

trinodb/trino

core/trino-main/src/main/java/io/trino/metadata/FunctionRegistry.java
@@ -150,6 +150,7 @@ import io.trino.operator.scalar.TryFunction;
 import io.trino.operator.scalar.TypeOfFunction;
 import io.trino.operator.scalar.UrlFunctions;
 import io.trino.operator.scalar.VarbinaryFunctions;
+import io.trino.operator.scalar.VersionFunction;
 import io.trino.operator.scalar.WilsonInterval;
 import io.trino.operator.scalar.WordStemFunction;
 import io.trino.operator.scalar.time.LocalTimeFunction;
@@ -375,7 +376,8 @@ public class FunctionRegistry
             Supplier<BlockEncodingSerde> blockEncodingSerdeSupplier,
             FeaturesConfig featuresConfig,
             TypeOperators typeOperators,
-            BlockTypeOperators blockTypeOperators)
+            BlockTypeOperators blockTypeOperators,
+            String nodeVersion)
     {
         // We have observed repeated compilation of MethodHandle that leads to full GCs.
         // We notice that flushing the following caches mitigate the problem.
@@ -598,6 +600,7 @@ public class FunctionRegistry
                 .function(new GenericComparisonOperator(typeOperators))
                 .function(new GenericLessThanOperator(typeOperators))
                 .function(new GenericLessThanOrEqualOperator(typeOperators))
+                .function(new VersionFunction(nodeVersion))
                 .aggregate(MergeSetDigestAggregation.class)
                 .aggregate(BuildSetDigestAggregation.class)
                 .scalars(SetDigestFunctions.class)

core/trino-main/src/main/java/io/trino/metadata/MetadataManager.java
@@ -22,6 +22,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import io.airlift.slice.Slice;
 import io.trino.Session;
+import io.trino.client.NodeVersion;
 import io.trino.connector.CatalogName;
 import io.trino.metadata.ResolvedFunction.ResolvedFunctionDecoder;
 import io.trino.operator.aggregation.InternalAggregationFunction;
@@ -205,10 +206,12 @@ public final class MetadataManager
             AnalyzePropertyManager analyzePropertyManager,
             TransactionManager transactionManager,
             TypeOperators typeOperators,
-            BlockTypeOperators blockTypeOperators)
+            BlockTypeOperators blockTypeOperators,
+            NodeVersion nodeVersion)
     {
+        requireNonNull(nodeVersion, "nodeVersion is null");
         typeRegistry = new TypeRegistry(featuresConfig);
-        functions = new FunctionRegistry(this::getBlockEncodingSerde, featuresConfig, typeOperators, blockTypeOperators);
+        functions = new FunctionRegistry(this::getBlockEncodingSerde, featuresConfig, typeOperators, blockTypeOperators, nodeVersion.getVersion());
         functionResolver = new FunctionResolver(this);
 
         this.procedures = new ProcedureRegistry();
@@ -274,7 +277,8 @@ public final class MetadataManager
                 new AnalyzePropertyManager(),
                 transactionManager,
                 typeOperators,
-                new BlockTypeOperators(typeOperators));
+                new BlockTypeOperators(typeOperators),
+                NodeVersion.UNKNOWN);
     }
 
     @Override

core/trino-main/src/main/java/io/trino/operator/scalar/VersionFunction.java
@@ -1,0 +1,73 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.operator.scalar;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.slice.Slice;
+import io.trino.annotation.UsedByGeneratedCode;
+import io.trino.metadata.FunctionBinding;
+import io.trino.metadata.FunctionMetadata;
+import io.trino.metadata.Signature;
+import io.trino.metadata.SqlScalarFunction;
+
+import java.lang.invoke.MethodHandle;
+
+import static io.airlift.slice.Slices.utf8Slice;
+import static io.trino.metadata.FunctionKind.SCALAR;
+import static io.trino.spi.function.InvocationConvention.InvocationReturnConvention.FAIL_ON_NULL;
+import static io.trino.spi.type.VarcharType.VARCHAR;
+import static io.trino.util.Reflection.methodHandle;
+
+public final class VersionFunction
+        extends SqlScalarFunction
+{
+    private static final MethodHandle METHOD_HANDLE = methodHandle(VersionFunction.class, "getVersion", String.class);
+    private final String nodeVersion;
+
+    public VersionFunction(String nodeVersion)
+    {
+        super(new FunctionMetadata(
+                new Signature(
+                        "version",
+                        ImmutableList.of(),
+                        ImmutableList.of(),
+                        VARCHAR.getTypeSignature(),
+                        ImmutableList.of(),
+                        false),
+                false,
+                ImmutableList.of(),
+                true,
+                true,
+                "Return server version",
+                SCALAR));
+        this.nodeVersion = nodeVersion;
+    }
+
+    @Override
+    public ScalarFunctionImplementation specialize(FunctionBinding functionBinding)
+    {
+        MethodHandle methodHandle = METHOD_HANDLE.bindTo(nodeVersion);
+        return new ChoicesScalarFunctionImplementation(
+                functionBinding,
+                FAIL_ON_NULL,
+                ImmutableList.of(),
+                methodHandle);
+    }
+
+    @UsedByGeneratedCode
+    public static Slice getVersion(String version)
+    {
+        return utf8Slice(version);
+    }
+}

core/trino-main/src/test/java/io/trino/operator/scalar/TestVersionFunction.java
@@ -1,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.operator.scalar;
+
+import org.testng.annotations.Test;
+
+import static io.trino.spi.type.VarcharType.VARCHAR;
+
+public class TestVersionFunction
+        extends AbstractTestFunctions
+{
+    @Test
+    public void testVersion()
+    {
+        assertFunction("version()", VARCHAR, "<unknown>");
+    }
+}