phd3/presto-1

Move some helper methods to IcebergUtil

presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergPageSource.java
@@ -13,51 +13,23 @@
  */
 package io.prestosql.plugin.iceberg;
 
-import io.airlift.slice.Slice;
-import io.airlift.slice.SliceUtf8;
 import io.prestosql.spi.Page;
 import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.block.Block;
 import io.prestosql.spi.block.RunLengthEncodedBlock;
 import io.prestosql.spi.connector.ConnectorPageSource;
 import io.prestosql.spi.predicate.Utils;
-import io.prestosql.spi.type.DecimalType;
-import io.prestosql.spi.type.Decimals;
 import io.prestosql.spi.type.TimeZoneKey;
 import io.prestosql.spi.type.Type;
-import io.prestosql.spi.type.VarbinaryType;
-import io.prestosql.spi.type.VarcharType;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Throwables.throwIfInstanceOf;
-import static io.airlift.slice.Slices.utf8Slice;
 import static io.prestosql.plugin.iceberg.IcebergErrorCode.ICEBERG_BAD_DATA;
-import static io.prestosql.plugin.iceberg.IcebergErrorCode.ICEBERG_INVALID_PARTITION_VALUE;
-import static io.prestosql.plugin.iceberg.util.Timestamps.timestampTzFromMicros;
-import static io.prestosql.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
-import static io.prestosql.spi.type.BigintType.BIGINT;
-import static io.prestosql.spi.type.BooleanType.BOOLEAN;
-import static io.prestosql.spi.type.DateType.DATE;
-import static io.prestosql.spi.type.Decimals.isLongDecimal;
-import static io.prestosql.spi.type.Decimals.isShortDecimal;
-import static io.prestosql.spi.type.DoubleType.DOUBLE;
-import static io.prestosql.spi.type.IntegerType.INTEGER;
-import static io.prestosql.spi.type.RealType.REAL;
-import static io.prestosql.spi.type.TimeType.TIME_MICROS;
-import static io.prestosql.spi.type.TimestampType.TIMESTAMP_MICROS;
-import static io.prestosql.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
-import static io.prestosql.spi.type.Timestamps.PICOSECONDS_PER_MICROSECOND;
-import static java.lang.Double.parseDouble;
-import static java.lang.Float.floatToRawIntBits;
-import static java.lang.Float.parseFloat;
-import static java.lang.Long.parseLong;
-import static java.lang.String.format;
+import static io.prestosql.plugin.iceberg.IcebergUtil.deserializePartitionValue;
 import static java.util.Objects.requireNonNull;
 
 public class IcebergPageSource
@@ -178,78 +150,5 @@ public class IcebergPageSource
                 throwable.addSuppressed(e);
             }
         }
-    }
-
-    private static Object deserializePartitionValue(Type type, String valueString, String name, TimeZoneKey timeZoneKey)
-    {
-        if (valueString == null) {
-            return null;
-        }
-
-        try {
-            if (type.equals(BOOLEAN)) {
-                if (valueString.equalsIgnoreCase("true")) {
-                    return true;
-                }
-                if (valueString.equalsIgnoreCase("false")) {
-                    return false;
-                }
-                throw new IllegalArgumentException();
-            }
-            if (type.equals(INTEGER)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(BIGINT)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(REAL)) {
-                return (long) floatToRawIntBits(parseFloat(valueString));
-            }
-            if (type.equals(DOUBLE)) {
-                return parseDouble(valueString);
-            }
-            if (type.equals(DATE)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIME_MICROS)) {
-                return parseLong(valueString) * PICOSECONDS_PER_MICROSECOND;
-            }
-            if (type.equals(TIMESTAMP_MICROS)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIMESTAMP_TZ_MICROS)) {
-                return timestampTzFromMicros(parseLong(valueString), timeZoneKey);
-            }
-            if (type instanceof VarcharType) {
-                Slice value = utf8Slice(valueString);
-                VarcharType varcharType = (VarcharType) type;
-                if (!varcharType.isUnbounded() && SliceUtf8.countCodePoints(value) > varcharType.getBoundedLength()) {
-                    throw new IllegalArgumentException();
-                }
-                return value;
-            }
-            if (type.equals(VarbinaryType.VARBINARY)) {
-                return utf8Slice(valueString);
-            }
-            if (isShortDecimal(type) || isLongDecimal(type)) {
-                DecimalType decimalType = (DecimalType) type;
-                BigDecimal decimal = new BigDecimal(valueString);
-                decimal = decimal.setScale(decimalType.getScale(), BigDecimal.ROUND_UNNECESSARY);
-                if (decimal.precision() > decimalType.getPrecision()) {
-                    throw new IllegalArgumentException();
-                }
-                BigInteger unscaledValue = decimal.unscaledValue();
-                return isShortDecimal(type) ? unscaledValue.longValue() : Decimals.encodeUnscaledValue(unscaledValue);
-            }
-        }
-        catch (IllegalArgumentException e) {
-            throw new PrestoException(ICEBERG_INVALID_PARTITION_VALUE, format(
-                    "Invalid partition value '%s' for %s partition key: %s",
-                    valueString,
-                    type.getDisplayName(),
-                    name));
-        }
-        // Iceberg tables don't partition by non-primitive-type columns.
-        throw new PrestoException(GENERIC_INTERNAL_ERROR, "Invalid partition type " + type.toString());
     }
 }

presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergSplitSource.java
@@ -20,31 +20,20 @@ import io.prestosql.spi.connector.ConnectorSplit;
 import io.prestosql.spi.connector.ConnectorSplitSource;
 import org.apache.iceberg.CombinedScanTask;
 import org.apache.iceberg.FileScanTask;
-import org.apache.iceberg.PartitionField;
-import org.apache.iceberg.PartitionSpec;
-import org.apache.iceberg.StructLike;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.types.Type;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static com.google.common.collect.Iterators.limit;
-import static io.prestosql.plugin.iceberg.IcebergUtil.getIdentityPartitions;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static io.prestosql.plugin.iceberg.IcebergUtil.getPartitionKeys;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.CompletableFuture.completedFuture;
-import static org.apache.iceberg.types.Type.TypeID.BINARY;
-import static org.apache.iceberg.types.Type.TypeID.FIXED;
 
 public class IcebergSplitSource
         implements ConnectorSplitSource
@@ -107,37 +96,5 @@ public class IcebergSplitSource
                 task.file().format(),
                 ImmutableList.of(),
                 getPartitionKeys(task));
-    }
-
-    private static Map<Integer, String> getPartitionKeys(FileScanTask scanTask)
-    {
-        StructLike partition = scanTask.file().partition();
-        PartitionSpec spec = scanTask.spec();
-        Map<PartitionField, Integer> fieldToIndex = getIdentityPartitions(spec);
-        Map<Integer, String> partitionKeys = new HashMap<>();
-
-        fieldToIndex.forEach((field, index) -> {
-            int id = field.sourceId();
-            Type type = spec.schema().findType(id);
-            Class<?> javaClass = type.typeId().javaClass();
-            Object value = partition.get(index, javaClass);
-
-            if (value == null) {
-                partitionKeys.put(id, null);
-            }
-            else {
-                String partitionValue;
-                if (type.typeId() == FIXED || type.typeId() == BINARY) {
-                    // this is safe because Iceberg PartitionData directly wraps the byte array
-                    partitionValue = new String(((ByteBuffer) value).array(), UTF_8);
-                }
-                else {
-                    partitionValue = value.toString();
-                }
-                partitionKeys.put(id, partitionValue);
-            }
-        });
-
-        return Collections.unmodifiableMap(partitionKeys);
     }
 }

trinodb/trino

plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSource.java
@@ -13,51 +13,23 @@
  */
 package io.trino.plugin.iceberg;
 
-import io.airlift.slice.Slice;
-import io.airlift.slice.SliceUtf8;
 import io.trino.spi.Page;
 import io.trino.spi.TrinoException;
 import io.trino.spi.block.Block;
 import io.trino.spi.block.RunLengthEncodedBlock;
 import io.trino.spi.connector.ConnectorPageSource;
 import io.trino.spi.predicate.Utils;
-import io.trino.spi.type.DecimalType;
-import io.trino.spi.type.Decimals;
 import io.trino.spi.type.TimeZoneKey;
 import io.trino.spi.type.Type;
-import io.trino.spi.type.VarbinaryType;
-import io.trino.spi.type.VarcharType;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Throwables.throwIfInstanceOf;
-import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.plugin.iceberg.IcebergErrorCode.ICEBERG_BAD_DATA;
-import static io.trino.plugin.iceberg.IcebergErrorCode.ICEBERG_INVALID_PARTITION_VALUE;
-import static io.trino.plugin.iceberg.util.Timestamps.timestampTzFromMicros;
-import static io.trino.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
-import static io.trino.spi.type.BigintType.BIGINT;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.DateType.DATE;
-import static io.trino.spi.type.Decimals.isLongDecimal;
-import static io.trino.spi.type.Decimals.isShortDecimal;
-import static io.trino.spi.type.DoubleType.DOUBLE;
-import static io.trino.spi.type.IntegerType.INTEGER;
-import static io.trino.spi.type.RealType.REAL;
-import static io.trino.spi.type.TimeType.TIME_MICROS;
-import static io.trino.spi.type.TimestampType.TIMESTAMP_MICROS;
-import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
-import static io.trino.spi.type.Timestamps.PICOSECONDS_PER_MICROSECOND;
-import static java.lang.Double.parseDouble;
-import static java.lang.Float.floatToRawIntBits;
-import static java.lang.Float.parseFloat;
-import static java.lang.Long.parseLong;
-import static java.lang.String.format;
+import static io.trino.plugin.iceberg.IcebergUtil.deserializePartitionValue;
 import static java.util.Objects.requireNonNull;
 
 public class IcebergPageSource
@@ -178,78 +150,5 @@ public class IcebergPageSource
                 throwable.addSuppressed(e);
             }
         }
-    }
-
-    private static Object deserializePartitionValue(Type type, String valueString, String name, TimeZoneKey timeZoneKey)
-    {
-        if (valueString == null) {
-            return null;
-        }
-
-        try {
-            if (type.equals(BOOLEAN)) {
-                if (valueString.equalsIgnoreCase("true")) {
-                    return true;
-                }
-                if (valueString.equalsIgnoreCase("false")) {
-                    return false;
-                }
-                throw new IllegalArgumentException();
-            }
-            if (type.equals(INTEGER)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(BIGINT)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(REAL)) {
-                return (long) floatToRawIntBits(parseFloat(valueString));
-            }
-            if (type.equals(DOUBLE)) {
-                return parseDouble(valueString);
-            }
-            if (type.equals(DATE)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIME_MICROS)) {
-                return parseLong(valueString) * PICOSECONDS_PER_MICROSECOND;
-            }
-            if (type.equals(TIMESTAMP_MICROS)) {
-                return parseLong(valueString);
-            }
-            if (type.equals(TIMESTAMP_TZ_MICROS)) {
-                return timestampTzFromMicros(parseLong(valueString), timeZoneKey);
-            }
-            if (type instanceof VarcharType) {
-                Slice value = utf8Slice(valueString);
-                VarcharType varcharType = (VarcharType) type;
-                if (!varcharType.isUnbounded() && SliceUtf8.countCodePoints(value) > varcharType.getBoundedLength()) {
-                    throw new IllegalArgumentException();
-                }
-                return value;
-            }
-            if (type.equals(VarbinaryType.VARBINARY)) {
-                return utf8Slice(valueString);
-            }
-            if (isShortDecimal(type) || isLongDecimal(type)) {
-                DecimalType decimalType = (DecimalType) type;
-                BigDecimal decimal = new BigDecimal(valueString);
-                decimal = decimal.setScale(decimalType.getScale(), BigDecimal.ROUND_UNNECESSARY);
-                if (decimal.precision() > decimalType.getPrecision()) {
-                    throw new IllegalArgumentException();
-                }
-                BigInteger unscaledValue = decimal.unscaledValue();
-                return isShortDecimal(type) ? unscaledValue.longValue() : Decimals.encodeUnscaledValue(unscaledValue);
-            }
-        }
-        catch (IllegalArgumentException e) {
-            throw new TrinoException(ICEBERG_INVALID_PARTITION_VALUE, format(
-                    "Invalid partition value '%s' for %s partition key: %s",
-                    valueString,
-                    type.getDisplayName(),
-                    name));
-        }
-        // Iceberg tables don't partition by non-primitive-type columns.
-        throw new TrinoException(GENERIC_INTERNAL_ERROR, "Invalid partition type " + type.toString());
     }
 }

plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergSplitSource.java
@@ -20,31 +20,20 @@ import io.trino.spi.connector.ConnectorSplit;
 import io.trino.spi.connector.ConnectorSplitSource;
 import org.apache.iceberg.CombinedScanTask;
 import org.apache.iceberg.FileScanTask;
-import org.apache.iceberg.PartitionField;
-import org.apache.iceberg.PartitionSpec;
-import org.apache.iceberg.StructLike;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.types.Type;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static com.google.common.collect.Iterators.limit;
-import static io.trino.plugin.iceberg.IcebergUtil.getIdentityPartitions;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static io.trino.plugin.iceberg.IcebergUtil.getPartitionKeys;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.CompletableFuture.completedFuture;
-import static org.apache.iceberg.types.Type.TypeID.BINARY;
-import static org.apache.iceberg.types.Type.TypeID.FIXED;
 
 public class IcebergSplitSource
         implements ConnectorSplitSource
@@ -107,37 +96,5 @@ public class IcebergSplitSource
                 task.file().format(),
                 ImmutableList.of(),
                 getPartitionKeys(task));
-    }
-
-    private static Map<Integer, String> getPartitionKeys(FileScanTask scanTask)
-    {
-        StructLike partition = scanTask.file().partition();
-        PartitionSpec spec = scanTask.spec();
-        Map<PartitionField, Integer> fieldToIndex = getIdentityPartitions(spec);
-        Map<Integer, String> partitionKeys = new HashMap<>();
-
-        fieldToIndex.forEach((field, index) -> {
-            int id = field.sourceId();
-            Type type = spec.schema().findType(id);
-            Class<?> javaClass = type.typeId().javaClass();
-            Object value = partition.get(index, javaClass);
-
-            if (value == null) {
-                partitionKeys.put(id, null);
-            }
-            else {
-                String partitionValue;
-                if (type.typeId() == FIXED || type.typeId() == BINARY) {
-                    // this is safe because Iceberg PartitionData directly wraps the byte array
-                    partitionValue = new String(((ByteBuffer) value).array(), UTF_8);
-                }
-                else {
-                    partitionValue = value.toString();
-                }
-                partitionKeys.put(id, partitionValue);
-            }
-        });
-
-        return Collections.unmodifiableMap(partitionKeys);
     }
 }