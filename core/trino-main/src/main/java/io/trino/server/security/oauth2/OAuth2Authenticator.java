lukasz-walkiewicz/presto

Add token polling for query submission authenticator

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuth2Authenticator.java
@@ -1,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.server.security.oauth2;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.prestosql.server.security.AuthenticationException;
+import io.prestosql.server.security.Authenticator;
+import io.prestosql.server.security.UserMapping;
+import io.prestosql.server.security.UserMappingException;
+import io.prestosql.spi.security.BasicPrincipal;
+import io.prestosql.spi.security.Identity;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.prestosql.server.security.oauth2.OAuth2CallbackResource.CALLBACK_ENDPOINT;
+import static io.prestosql.server.security.oauth2.OAuth2TokenExchangeResource.getTokenUri;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+public class OAuth2Authenticator
+        implements Authenticator
+{
+    private final OAuth2Service service;
+    private final UserMapping userMapping;
+
+    @Inject
+    public OAuth2Authenticator(OAuth2Service service, OAuth2Config oauth2Config)
+    {
+        this.service = requireNonNull(service, "service is null");
+        requireNonNull(oauth2Config, "oauth2Config is null");
+        this.userMapping = UserMapping.createUserMapping(oauth2Config.getUserMappingPattern(), oauth2Config.getUserMappingFile());
+    }
+
+    @Override
+    public Identity authenticate(ContainerRequestContext request)
+            throws AuthenticationException
+    {
+        String header = nullToEmpty(request.getHeaders().getFirst(AUTHORIZATION));
+
+        int space = header.indexOf(' ');
+        if ((space < 0) || !header.substring(0, space).equalsIgnoreCase("bearer")) {
+            throw needAuthentication(request, null);
+        }
+        String token = header.substring(space + 1).trim();
+        if (token.isEmpty()) {
+            throw needAuthentication(request, null);
+        }
+
+        try {
+            Jws<Claims> claimsJws = service.parseClaimsJws(token);
+            String subject = claimsJws.getBody().getSubject();
+            String authenticatedUser = userMapping.mapUser(subject);
+            return Identity.forUser(authenticatedUser)
+                    .withPrincipal(new BasicPrincipal(subject))
+                    .build();
+        }
+        catch (JwtException | UserMappingException e) {
+            throw needAuthentication(request, e.getMessage());
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Authentication error", e);
+        }
+    }
+
+    private AuthenticationException needAuthentication(ContainerRequestContext request, String message)
+    {
+        UUID authId = UUID.randomUUID();
+        URI redirectUri = service.startRestChallenge(request.getUriInfo().getBaseUri().resolve(CALLBACK_ENDPOINT), authId);
+        URI tokenUri = request.getUriInfo().getBaseUri().resolve(getTokenUri(authId));
+        return new AuthenticationException(message, format("Bearer x_redirect_server=\"%s\", x_token_server=\"%s\"", redirectUri, tokenUri));
+    }
+}

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuth2TokenExchange.java
@@ -1,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.server.security.oauth2;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableScheduledFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.SettableFuture;
+import io.airlift.units.Duration;
+import io.prestosql.dispatcher.DispatchExecutor;
+
+import javax.inject.Inject;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.google.common.util.concurrent.Futures.nonCancellationPropagating;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class OAuth2TokenExchange
+{
+    public static final Duration MAX_POLL_TIME = new Duration(9, SECONDS);
+    private static final TokenPoll TOKEN_POLL_TIMED_OUT = TokenPoll.error("Authentication has timed out");
+
+    private final LoadingCache<UUID, SettableFuture<TokenPoll>> cache;
+    private final ListeningExecutorService responseExecutor;
+
+    @Inject
+    public OAuth2TokenExchange(OAuth2Config config, DispatchExecutor executor)
+    {
+        this.responseExecutor = requireNonNull(executor, "responseExecutor is null").getExecutor();
+
+        long challengeTimeoutMs = config.getChallengeTimeout().toMillis();
+        ListeningScheduledExecutorService scheduledExecutor = executor.getScheduledExecutor();
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite((challengeTimeoutMs + MAX_POLL_TIME.toMillis()) * 2, MILLISECONDS)
+                .<UUID, SettableFuture<TokenPoll>>removalListener(notification -> notification.getValue().set(TOKEN_POLL_TIMED_OUT))
+                .build(new CacheLoader<>()
+                {
+                    @Override
+                    public SettableFuture<TokenPoll> load(UUID authId)
+                    {
+                        SettableFuture<TokenPoll> future = SettableFuture.create();
+                        ListenableScheduledFuture<?> timeout = scheduledExecutor.schedule(() -> future.set(TOKEN_POLL_TIMED_OUT), challengeTimeoutMs, MILLISECONDS);
+                        future.addListener(() -> timeout.cancel(true), responseExecutor);
+                        return future;
+                    }
+                });
+    }
+
+    public void setAccessToken(UUID authId, String accessToken)
+    {
+        cache.getUnchecked(authId).set(TokenPoll.token(accessToken));
+    }
+
+    public void setTokenExchangeError(UUID authId, String message)
+    {
+        cache.getUnchecked(authId).set(TokenPoll.error(message));
+    }
+
+    public ListenableFuture<TokenPoll> getTokenPoll(UUID authId)
+    {
+        return nonCancellationPropagating(cache.getUnchecked(authId));
+    }
+
+    public void dropToken(UUID authId)
+    {
+        cache.invalidate(authId);
+    }
+
+    public static class TokenPoll
+    {
+        private final Optional<String> token;
+        private final Optional<String> error;
+
+        private TokenPoll(String token, String error)
+        {
+            this.token = Optional.ofNullable(token);
+            this.error = Optional.ofNullable(error);
+        }
+
+        static TokenPoll token(String token)
+        {
+            requireNonNull(token, "token is null");
+
+            return new TokenPoll(token, null);
+        }
+
+        static TokenPoll error(String error)
+        {
+            requireNonNull(error, "token is null");
+
+            return new TokenPoll(null, error);
+        }
+
+        public Optional<String> getToken()
+        {
+            return token;
+        }
+
+        public Optional<String> getError()
+        {
+            return error;
+        }
+    }
+}

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuthAuthenticationSupporterModule.java
@@ -11,25 +11,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.server.ui;
+package io.prestosql.server.security.oauth2;
 
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.prestosql.server.security.oauth2.OAuth2ServiceModule;
 
-import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
 
-public class OAuth2WebUiModule
+public class OAuthAuthenticationSupporterModule
         extends AbstractConfigurationAwareModule
 {
     @Override
     protected void setup(Binder binder)
     {
-        newOptionalBinder(binder, OAuth2WebUiInstalled.class).setBinding().toInstance(OAuth2WebUiInstalled.INSTANCE);
-        binder.bind(WebUiAuthenticationFilter.class).to(OAuth2WebUiAuthenticationFilter.class).in(Scopes.SINGLETON);
-        jaxrsBinder(binder).bind(OAuth2WebUiLogoutResource.class);
+        binder.bind(OAuth2TokenExchange.class).in(Scopes.SINGLETON);
+        jaxrsBinder(binder).bind(OAuth2TokenExchangeResource.class);
         install(new OAuth2ServiceModule());
     }
 }

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuthAuthenticator.java
@@ -1,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.server.security.oauth2;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.prestosql.server.security.AuthenticationException;
+import io.prestosql.server.security.Authenticator;
+import io.prestosql.server.security.UserMapping;
+import io.prestosql.server.security.UserMappingException;
+import io.prestosql.spi.security.BasicPrincipal;
+import io.prestosql.spi.security.Identity;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.prestosql.server.security.oauth2.OAuth2CallbackResource.CALLBACK_ENDPOINT;
+import static io.prestosql.server.security.oauth2.OAuth2TokenExchangeResource.getTokenUri;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+public class OAuthAuthenticator
+        implements Authenticator
+{
+    private final OAuth2Service service;
+    private final UserMapping userMapping;
+
+    @Inject
+    public OAuthAuthenticator(OAuth2Service service, OAuth2Config oauth2Config)
+    {
+        this.service = requireNonNull(service, "service is null");
+        requireNonNull(oauth2Config, "oauth2Config is null");
+        this.userMapping = UserMapping.createUserMapping(oauth2Config.getUserMappingPattern(), oauth2Config.getUserMappingFile());
+    }
+
+    @Override
+    public Identity authenticate(ContainerRequestContext request)
+            throws AuthenticationException
+    {
+        String header = nullToEmpty(request.getHeaders().getFirst(AUTHORIZATION));
+
+        int space = header.indexOf(' ');
+        if ((space < 0) || !header.substring(0, space).equalsIgnoreCase("bearer")) {
+            throw needAuthentication(request, null);
+        }
+        String token = header.substring(space + 1).trim();
+        if (token.isEmpty()) {
+            throw needAuthentication(request, null);
+        }
+
+        try {
+            Jws<Claims> claimsJws = service.parseClaimsJws(token);
+            String subject = claimsJws.getBody().getSubject();
+            String authenticatedUser = userMapping.mapUser(subject);
+            return Identity.forUser(authenticatedUser)
+                    .withPrincipal(new BasicPrincipal(subject))
+                    .build();
+        }
+        catch (JwtException | UserMappingException e) {
+            throw needAuthentication(request, e.getMessage());
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Authentication error", e);
+        }
+    }
+
+    private AuthenticationException needAuthentication(ContainerRequestContext request, String message)
+    {
+        UUID authId = UUID.randomUUID();
+        URI redirectUri = service.startRestChallenge(request.getUriInfo().getBaseUri().resolve(CALLBACK_ENDPOINT), authId);
+        URI tokenUri = request.getUriInfo().getBaseUri().resolve(getTokenUri(authId));
+        return new AuthenticationException(message, format("Bearer x_redirect_server=\"%s\", x_token_server=\"%s\"", redirectUri, tokenUri));
+    }
+}

trinodb/trino

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2Authenticator.java
@@ -1,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.server.security.oauth2;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.trino.server.security.AuthenticationException;
+import io.trino.server.security.Authenticator;
+import io.trino.server.security.UserMapping;
+import io.trino.server.security.UserMappingException;
+import io.trino.spi.security.BasicPrincipal;
+import io.trino.spi.security.Identity;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.trino.server.security.oauth2.OAuth2CallbackResource.CALLBACK_ENDPOINT;
+import static io.trino.server.security.oauth2.OAuth2TokenExchangeResource.getTokenUri;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+public class OAuth2Authenticator
+        implements Authenticator
+{
+    private final OAuth2Service service;
+    private final UserMapping userMapping;
+
+    @Inject
+    public OAuth2Authenticator(OAuth2Service service, OAuth2Config oauth2Config)
+    {
+        this.service = requireNonNull(service, "service is null");
+        requireNonNull(oauth2Config, "oauth2Config is null");
+        this.userMapping = UserMapping.createUserMapping(oauth2Config.getUserMappingPattern(), oauth2Config.getUserMappingFile());
+    }
+
+    @Override
+    public Identity authenticate(ContainerRequestContext request)
+            throws AuthenticationException
+    {
+        String header = nullToEmpty(request.getHeaders().getFirst(AUTHORIZATION));
+
+        int space = header.indexOf(' ');
+        if ((space < 0) || !header.substring(0, space).equalsIgnoreCase("bearer")) {
+            throw needAuthentication(request, null);
+        }
+        String token = header.substring(space + 1).trim();
+        if (token.isEmpty()) {
+            throw needAuthentication(request, null);
+        }
+
+        try {
+            Jws<Claims> claimsJws = service.parseClaimsJws(token);
+            String subject = claimsJws.getBody().getSubject();
+            String authenticatedUser = userMapping.mapUser(subject);
+            return Identity.forUser(authenticatedUser)
+                    .withPrincipal(new BasicPrincipal(subject))
+                    .build();
+        }
+        catch (JwtException | UserMappingException e) {
+            throw needAuthentication(request, e.getMessage());
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Authentication error", e);
+        }
+    }
+
+    private AuthenticationException needAuthentication(ContainerRequestContext request, String message)
+    {
+        UUID authId = UUID.randomUUID();
+        URI redirectUri = service.startRestChallenge(request.getUriInfo().getBaseUri().resolve(CALLBACK_ENDPOINT), authId);
+        URI tokenUri = request.getUriInfo().getBaseUri().resolve(getTokenUri(authId));
+        return new AuthenticationException(message, format("Bearer x_redirect_server=\"%s\", x_token_server=\"%s\"", redirectUri, tokenUri));
+    }
+}

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2TokenExchange.java
@@ -1,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.server.security.oauth2;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableScheduledFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.SettableFuture;
+import io.airlift.units.Duration;
+import io.trino.dispatcher.DispatchExecutor;
+
+import javax.inject.Inject;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.google.common.util.concurrent.Futures.nonCancellationPropagating;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class OAuth2TokenExchange
+{
+    public static final Duration MAX_POLL_TIME = new Duration(9, SECONDS);
+    private static final TokenPoll TOKEN_POLL_TIMED_OUT = TokenPoll.error("Authentication has timed out");
+
+    private final LoadingCache<UUID, SettableFuture<TokenPoll>> cache;
+    private final ListeningExecutorService responseExecutor;
+
+    @Inject
+    public OAuth2TokenExchange(OAuth2Config config, DispatchExecutor executor)
+    {
+        this.responseExecutor = requireNonNull(executor, "responseExecutor is null").getExecutor();
+
+        long challengeTimeoutMs = config.getChallengeTimeout().toMillis();
+        ListeningScheduledExecutorService scheduledExecutor = executor.getScheduledExecutor();
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite((challengeTimeoutMs + MAX_POLL_TIME.toMillis()) * 2, MILLISECONDS)
+                .<UUID, SettableFuture<TokenPoll>>removalListener(notification -> notification.getValue().set(TOKEN_POLL_TIMED_OUT))
+                .build(new CacheLoader<>()
+                {
+                    @Override
+                    public SettableFuture<TokenPoll> load(UUID authId)
+                    {
+                        SettableFuture<TokenPoll> future = SettableFuture.create();
+                        ListenableScheduledFuture<?> timeout = scheduledExecutor.schedule(() -> future.set(TOKEN_POLL_TIMED_OUT), challengeTimeoutMs, MILLISECONDS);
+                        future.addListener(() -> timeout.cancel(true), responseExecutor);
+                        return future;
+                    }
+                });
+    }
+
+    public void setAccessToken(UUID authId, String accessToken)
+    {
+        cache.getUnchecked(authId).set(TokenPoll.token(accessToken));
+    }
+
+    public void setTokenExchangeError(UUID authId, String message)
+    {
+        cache.getUnchecked(authId).set(TokenPoll.error(message));
+    }
+
+    public ListenableFuture<TokenPoll> getTokenPoll(UUID authId)
+    {
+        return nonCancellationPropagating(cache.getUnchecked(authId));
+    }
+
+    public void dropToken(UUID authId)
+    {
+        cache.invalidate(authId);
+    }
+
+    public static class TokenPoll
+    {
+        private final Optional<String> token;
+        private final Optional<String> error;
+
+        private TokenPoll(String token, String error)
+        {
+            this.token = Optional.ofNullable(token);
+            this.error = Optional.ofNullable(error);
+        }
+
+        static TokenPoll token(String token)
+        {
+            requireNonNull(token, "token is null");
+
+            return new TokenPoll(token, null);
+        }
+
+        static TokenPoll error(String error)
+        {
+            requireNonNull(error, "token is null");
+
+            return new TokenPoll(null, error);
+        }
+
+        public Optional<String> getToken()
+        {
+            return token;
+        }
+
+        public Optional<String> getError()
+        {
+            return error;
+        }
+    }
+}

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuthAuthenticationSupporterModule.java
@@ -11,25 +11,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.server.ui;
+package io.trino.server.security.oauth2;
 
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.trino.server.security.oauth2.OAuth2ServiceModule;
 
-import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
 
-public class OAuth2WebUiModule
+public class OAuthAuthenticationSupporterModule
         extends AbstractConfigurationAwareModule
 {
     @Override
     protected void setup(Binder binder)
     {
-        newOptionalBinder(binder, OAuth2WebUiInstalled.class).setBinding().toInstance(OAuth2WebUiInstalled.INSTANCE);
-        binder.bind(WebUiAuthenticationFilter.class).to(OAuth2WebUiAuthenticationFilter.class).in(Scopes.SINGLETON);
-        jaxrsBinder(binder).bind(OAuth2WebUiLogoutResource.class);
+        binder.bind(OAuth2TokenExchange.class).in(Scopes.SINGLETON);
+        jaxrsBinder(binder).bind(OAuth2TokenExchangeResource.class);
         install(new OAuth2ServiceModule());
     }
 }

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuthAuthenticator.java
@@ -1,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.server.security.oauth2;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.trino.server.security.AuthenticationException;
+import io.trino.server.security.Authenticator;
+import io.trino.server.security.UserMapping;
+import io.trino.server.security.UserMappingException;
+import io.trino.spi.security.BasicPrincipal;
+import io.trino.spi.security.Identity;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.trino.server.security.oauth2.OAuth2CallbackResource.CALLBACK_ENDPOINT;
+import static io.trino.server.security.oauth2.OAuth2TokenExchangeResource.getTokenUri;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+public class OAuthAuthenticator
+        implements Authenticator
+{
+    private final OAuth2Service service;
+    private final UserMapping userMapping;
+
+    @Inject
+    public OAuthAuthenticator(OAuth2Service service, OAuth2Config oauth2Config)
+    {
+        this.service = requireNonNull(service, "service is null");
+        requireNonNull(oauth2Config, "oauth2Config is null");
+        this.userMapping = UserMapping.createUserMapping(oauth2Config.getUserMappingPattern(), oauth2Config.getUserMappingFile());
+    }
+
+    @Override
+    public Identity authenticate(ContainerRequestContext request)
+            throws AuthenticationException
+    {
+        String header = nullToEmpty(request.getHeaders().getFirst(AUTHORIZATION));
+
+        int space = header.indexOf(' ');
+        if ((space < 0) || !header.substring(0, space).equalsIgnoreCase("bearer")) {
+            throw needAuthentication(request, null);
+        }
+        String token = header.substring(space + 1).trim();
+        if (token.isEmpty()) {
+            throw needAuthentication(request, null);
+        }
+
+        try {
+            Jws<Claims> claimsJws = service.parseClaimsJws(token);
+            String subject = claimsJws.getBody().getSubject();
+            String authenticatedUser = userMapping.mapUser(subject);
+            return Identity.forUser(authenticatedUser)
+                    .withPrincipal(new BasicPrincipal(subject))
+                    .build();
+        }
+        catch (JwtException | UserMappingException e) {
+            throw needAuthentication(request, e.getMessage());
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Authentication error", e);
+        }
+    }
+
+    private AuthenticationException needAuthentication(ContainerRequestContext request, String message)
+    {
+        UUID authId = UUID.randomUUID();
+        URI redirectUri = service.startRestChallenge(request.getUriInfo().getBaseUri().resolve(CALLBACK_ENDPOINT), authId);
+        URI tokenUri = request.getUriInfo().getBaseUri().resolve(getTokenUri(authId));
+        return new AuthenticationException(message, format("Bearer x_redirect_server=\"%s\", x_token_server=\"%s\"", redirectUri, tokenUri));
+    }
+}