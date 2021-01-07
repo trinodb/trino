9a51e5c77d6163c60789d8402edd44ad5f509262
David Phillips <david@acz.org>
2021-01-06 17:46:13 -0800

    Update to OkHttp 3.14.9

--- client/trino-client/src/main/java/io/trino/client/OkHttpUtil.java
+++ client/trino-client/src/main/java/io/trino/client/OkHttpUtil.java
@@ -25,6 +25,7 @@ import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import okhttp3.Request;
+import okhttp3.internal.tls.LegacyHostnameVerifier;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
@@ -238,6 +239,7 @@ public final class OkHttpUtil
             sslContext.init(keyManagers, new TrustManager[] {trustManager}, null);
 
             clientBuilder.sslSocketFactory(sslContext.getSocketFactory(), trustManager);
+            clientBuilder.hostnameVerifier(LegacyHostnameVerifier.INSTANCE);
         }
         catch (GeneralSecurityException | IOException e) {
             throw new ClientException("Error setting up SSL: " + e.getMessage(), e);
