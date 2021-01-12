c309cf5cf6f2957e01921fd31b26cc03eaee416c
skrzypo987 <krzysztof.skrzypczynski@starburstdata.com>
2021-01-12 09:35:47 +0100

    Parametrize BenchmarkPartitionedOutputOperator

--- core/trino-main/src/test/java/io/trino/operator/BenchmarkPartitionedOutputOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/BenchmarkPartitionedOutputOperator.java
@@ -35,12 +35,14 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,7 +50,6 @@ import java.util.OptionalInt;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Function;
 
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.airlift.slice.Slices.utf8Slice;
@@ -66,13 +67,14 @@ import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.function.Function.identity;
 import static org.openjdk.jmh.annotations.Mode.AverageTime;
 import static org.openjdk.jmh.annotations.Scope.Thread;
 import static org.openjdk.jmh.runner.options.VerboseMode.NORMAL;
 
 @State(Thread)
 @OutputTimeUnit(MILLISECONDS)
-@Fork(2)
+@Fork(3)
 @Warmup(iterations = 20, time = 500, timeUnit = MILLISECONDS)
 @Measurement(iterations = 20, time = 500, timeUnit = MILLISECONDS)
 @BenchmarkMode(AverageTime)
@@ -91,19 +93,28 @@ public class BenchmarkPartitionedOutputOperator
     @State(Thread)
     public static class BenchmarkData
     {
-        private static final int PAGE_COUNT = 5000;
-        private static final int PARTITION_COUNT = 512;
-        private static final int ENTRIES_PER_PAGE = 256;
+        private static final int ENTRIES = 256 * 1024;
+
         private static final DataSize MAX_MEMORY = DataSize.of(1, GIGABYTE);
         private static final List<Type> TYPES = ImmutableList.of(BIGINT, VARCHAR, VARCHAR, VARCHAR);
         private static final ExecutorService EXECUTOR = newCachedThreadPool(daemonThreadsNamed("BenchmarkPartitionedOutputOperator-executor-%s"));
         private static final ScheduledExecutorService SCHEDULER = newScheduledThreadPool(1, daemonThreadsNamed("BenchmarkPartitionedOutputOperator-scheduledExecutor-%s"));
 
+        @Param({"4", "16", "64", "512"})
+        protected int partitionCount = 512;
+
+        @Param({"256", "1024"})
+        protected int entriesPerPage = 256;
+        protected int pageCount = ENTRIES / entriesPerPage;
+
+        @Param({"0", ".1", ".3"})
+        protected double nullChance;
+
         private final Page dataPage = createPage();
 
         private int getPageCount()
         {
-            return PAGE_COUNT;
+            return pageCount;
         }
 
         public Page getDataPage()
@@ -115,43 +126,49 @@ public class BenchmarkPartitionedOutputOperator
         {
             BlockTypeOperators blockTypeOperators = new BlockTypeOperators(new TypeOperators());
             PartitionFunction partitionFunction = new LocalPartitionGenerator(
-                    new InterpretedHashGenerator(ImmutableList.of(BIGINT), new int[] {0}, blockTypeOperators), PARTITION_COUNT);
+                    new InterpretedHashGenerator(ImmutableList.of(BIGINT), new int[] {0}, blockTypeOperators), partitionCount);
             PagesSerdeFactory serdeFactory = new PagesSerdeFactory(createTestMetadataManager().getBlockEncodingSerde(), false);
             OutputBuffers buffers = createInitialEmptyOutputBuffers(PARTITIONED);
-            for (int partition = 0; partition < PARTITION_COUNT; partition++) {
+            for (int partition = 0; partition < partitionCount; partition++) {
                 buffers = buffers.withBuffer(new OutputBuffers.OutputBufferId(partition), partition);
             }
             PartitionedOutputBuffer buffer = createPartitionedBuffer(
                     buffers.withNoMoreBufferIds(),
                     DataSize.ofBytes(Long.MAX_VALUE)); // don't let output buffer block
+            OptionalInt nullChannel = nullChance == 0 ? OptionalInt.empty() : OptionalInt.of(0);
             PartitionedOutputFactory operatorFactory = new PartitionedOutputFactory(
                     partitionFunction,
                     ImmutableList.of(0),
                     ImmutableList.of(Optional.empty()),
                     false,
-                    OptionalInt.empty(),
+                    nullChannel,
                     buffer,
                     DataSize.of(1, GIGABYTE));
             return (PartitionedOutputOperator) operatorFactory
-                    .createOutputOperator(0, new PlanNodeId("plan-node-0"), TYPES, Function.identity(), serdeFactory)
+                    .createOutputOperator(0, new PlanNodeId("plan-node-0"), TYPES, identity(), serdeFactory)
                     .createOperator(createDriverContext());
         }
 
         private Page createPage()
         {
-            Object[][] testRows = generateTestData(ImmutableList.of(VARCHAR, VARCHAR, VARCHAR), ENTRIES_PER_PAGE);
+            Object[][] testRows = generateTestData(ImmutableList.of(VARCHAR, VARCHAR, VARCHAR), entriesPerPage);
             PageBuilder pageBuilder = new PageBuilder(TYPES);
             BlockBuilder bigintBlockBuilder = pageBuilder.getBlockBuilder(0);
             BlockBuilder varcharBlockBuilder = pageBuilder.getBlockBuilder(1);
             BlockBuilder varcharBlockBuilder2 = pageBuilder.getBlockBuilder(2);
             BlockBuilder varcharBlockBuilder3 = pageBuilder.getBlockBuilder(3);
-            for (int i = 0; i < ENTRIES_PER_PAGE; i++) {
-                BIGINT.writeLong(bigintBlockBuilder, i);
+            for (int i = 0; i < entriesPerPage; i++) {
+                if (ThreadLocalRandom.current().nextDouble() < nullChance) {
+                    bigintBlockBuilder.appendNull();
+                }
+                else {
+                    BIGINT.writeLong(bigintBlockBuilder, i);
+                }
                 VARCHAR.writeSlice(varcharBlockBuilder, utf8Slice((String) testRows[0][i]));
                 VARCHAR.writeSlice(varcharBlockBuilder2, utf8Slice((String) testRows[1][i]));
                 VARCHAR.writeSlice(varcharBlockBuilder3, utf8Slice((String) testRows[2][i]));
             }
-            pageBuilder.declarePositions(ENTRIES_PER_PAGE);
+            pageBuilder.declarePositions(entriesPerPage);
             return pageBuilder.build();
         }
 
@@ -194,6 +211,12 @@ public class BenchmarkPartitionedOutputOperator
                     () -> new SimpleLocalMemoryContext(newSimpleAggregatedMemoryContext(), "test"),
                     SCHEDULER);
         }
+    }
+
+    @Test
+    public void testAddPage()
+    {
+        addPage(new BenchmarkData());
     }
 
     public static void main(String[] args)
