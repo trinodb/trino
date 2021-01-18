lukasz-walkiewicz/presto

Add token polling for query submission authenticator

presto-main/src/main/java/io/prestosql/server/security/ServerSecurityModule.java
@@ -28,6 +28,8 @@ import io.airlift.http.server.HttpServerConfig;
 import io.airlift.jmx.MBeanResource;
 import io.prestosql.server.security.jwt.JwtAuthenticator;
 import io.prestosql.server.security.jwt.JwtAuthenticatorSupportModule;
+import io.prestosql.server.security.oauth2.OAuth2AuthenticationSupportModule;
+import io.prestosql.server.security.oauth2.OAuth2Authenticator;
 
 import java.util.List;
 import java.util.Map;
@@ -73,6 +75,7 @@ public class ServerSecurityModule
         installAuthenticator("kerberos", KerberosAuthenticator.class, KerberosConfig.class);
         installAuthenticator("password", PasswordAuthenticator.class, PasswordAuthenticatorConfig.class);
         install(authenticatorModule("jwt", JwtAuthenticator.class, new JwtAuthenticatorSupportModule()));
+        install(authenticatorModule("oauth2", OAuth2Authenticator.class, new OAuth2AuthenticationSupportModule()));
 
         configBinder(binder).bindConfig(InsecureAuthenticatorConfig.class);
         binder.bind(InsecureAuthenticator.class).in(Scopes.SINGLETON);

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuth2AuthenticationSupportModule.java
@@ -11,25 +11,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.server.ui;
+package io.prestosql.server.security.oauth2;
 
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.prestosql.server.security.oauth2.OAuth2ServiceModule;
 
-import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
 
-public class OAuth2WebUiModule
+public class OAuth2AuthenticationSupportModule
         extends AbstractConfigurationAwareModule
 {
     @Override
     protected void setup(Binder binder)
     {
-        newOptionalBinder(binder, OAuth2WebUiInstalled.class).setBinding().toInstance(OAuth2WebUiInstalled.INSTANCE);
-        binder.bind(WebUiAuthenticationFilter.class).to(OAuth2WebUiAuthenticationFilter.class).in(Scopes.SINGLETON);
-        jaxrsBinder(binder).bind(OAuth2WebUiLogoutResource.class);
+        binder.bind(OAuth2TokenExchange.class).in(Scopes.SINGLETON);
+        jaxrsBinder(binder).bind(OAuth2TokenExchangeResource.class);
         install(new OAuth2ServiceModule());
     }
 }

presto-main/src/main/java/io/prestosql/server/security/oauth2/OAuth2ServiceModule.java
@@ -23,6 +23,7 @@ import io.airlift.units.Duration;
 import io.jsonwebtoken.SigningKeyResolver;
 import io.prestosql.server.security.jwt.JwkService;
 import io.prestosql.server.security.jwt.JwkSigningKeyResolver;
+import io.prestosql.server.ui.OAuth2WebUiInstalled;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
@@ -39,6 +40,8 @@ public class OAuth2ServiceModule
     protected void setup(Binder binder)
     {
         jaxrsBinder(binder).bind(OAuth2CallbackResource.class);
+        newOptionalBinder(binder, OAuth2WebUiInstalled.class);
+        newOptionalBinder(binder, OAuth2TokenExchange.class);
 
         configBinder(binder).bindConfig(OAuth2Config.class);
         binder.bind(OAuth2Service.class).in(Scopes.SINGLETON);

presto-main/src/main/java/io/prestosql/server/ui/OAuth2WebUiInstalled.java
@@ -1,0 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.server.ui;
+
+public enum OAuth2WebUiInstalled
+{
+    INSTANCE
+}

presto-main/src/test/java/io/prestosql/server/ui/TestWebUi.java
@@ -514,7 +514,7 @@ public class TestWebUi
 
         String state = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, Hashing.sha256().hashString("test-state-key", UTF_8).asBytes())
-                .setAudience("presto_oauth")
+                .setAudience("presto_oauth_ui")
                 .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(10).toInstant()))
                 .compact();
 

trinodb/trino

core/trino-main/src/main/java/io/trino/server/security/ServerSecurityModule.java
@@ -28,6 +28,8 @@ import io.airlift.http.server.HttpServerConfig;
 import io.airlift.jmx.MBeanResource;
 import io.trino.server.security.jwt.JwtAuthenticator;
 import io.trino.server.security.jwt.JwtAuthenticatorSupportModule;
+import io.trino.server.security.oauth2.OAuth2AuthenticationSupportModule;
+import io.trino.server.security.oauth2.OAuth2Authenticator;
 
 import java.util.List;
 import java.util.Map;
@@ -73,6 +75,7 @@ public class ServerSecurityModule
         installAuthenticator("kerberos", KerberosAuthenticator.class, KerberosConfig.class);
         installAuthenticator("password", PasswordAuthenticator.class, PasswordAuthenticatorConfig.class);
         install(authenticatorModule("jwt", JwtAuthenticator.class, new JwtAuthenticatorSupportModule()));
+        install(authenticatorModule("oauth2", OAuth2Authenticator.class, new OAuth2AuthenticationSupportModule()));
 
         configBinder(binder).bindConfig(InsecureAuthenticatorConfig.class);
         binder.bind(InsecureAuthenticator.class).in(Scopes.SINGLETON);

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2AuthenticationSupportModule.java
@@ -11,25 +11,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.server.ui;
+package io.trino.server.security.oauth2;
 
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.trino.server.security.oauth2.OAuth2ServiceModule;
 
-import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
 
-public class OAuth2WebUiModule
+public class OAuth2AuthenticationSupportModule
         extends AbstractConfigurationAwareModule
 {
     @Override
     protected void setup(Binder binder)
     {
-        newOptionalBinder(binder, OAuth2WebUiInstalled.class).setBinding().toInstance(OAuth2WebUiInstalled.INSTANCE);
-        binder.bind(WebUiAuthenticationFilter.class).to(OAuth2WebUiAuthenticationFilter.class).in(Scopes.SINGLETON);
-        jaxrsBinder(binder).bind(OAuth2WebUiLogoutResource.class);
+        binder.bind(OAuth2TokenExchange.class).in(Scopes.SINGLETON);
+        jaxrsBinder(binder).bind(OAuth2TokenExchangeResource.class);
         install(new OAuth2ServiceModule());
     }
 }

core/trino-main/src/main/java/io/trino/server/security/oauth2/OAuth2ServiceModule.java
@@ -23,6 +23,7 @@ import io.airlift.units.Duration;
 import io.jsonwebtoken.SigningKeyResolver;
 import io.trino.server.security.jwt.JwkService;
 import io.trino.server.security.jwt.JwkSigningKeyResolver;
+import io.trino.server.ui.OAuth2WebUiInstalled;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
@@ -39,6 +40,8 @@ public class OAuth2ServiceModule
     protected void setup(Binder binder)
     {
         jaxrsBinder(binder).bind(OAuth2CallbackResource.class);
+        newOptionalBinder(binder, OAuth2WebUiInstalled.class);
+        newOptionalBinder(binder, OAuth2TokenExchange.class);
 
         configBinder(binder).bindConfig(OAuth2Config.class);
         binder.bind(OAuth2Service.class).in(Scopes.SINGLETON);

core/trino-main/src/main/java/io/trino/server/ui/OAuth2WebUiInstalled.java
@@ -1,0 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.server.ui;
+
+public enum OAuth2WebUiInstalled
+{
+    INSTANCE
+}

core/trino-main/src/test/java/io/trino/server/ui/TestWebUi.java
@@ -514,7 +514,7 @@ public class TestWebUi
 
         String state = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, Hashing.sha256().hashString("test-state-key", UTF_8).asBytes())
-                .setAudience("trino_oauth")
+                .setAudience("trino_oauth_ui")
                 .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(10).toInstant()))
                 .compact();
 