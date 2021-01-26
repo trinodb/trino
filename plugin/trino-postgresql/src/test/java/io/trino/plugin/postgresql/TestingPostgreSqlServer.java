ffd5341c0fe31be885669bcd7a57bcdcda5c2a63
Michał Ślizak <michal.slizak+github@gmail.com>
2021-01-26 21:03:38 +0100

    Implement statistical aggregation pushdown in PostgreSQL connector

--- plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestingPostgreSqlServer.java
+++ plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestingPostgreSqlServer.java
@@ -45,17 +45,18 @@ public class TestingPostgreSqlServer
     }
 
     public void execute(String sql)
-            throws SQLException
     {
         execute(getJdbcUrl(), getProperties(), sql);
     }
 
     private static void execute(String url, Properties properties, String sql)
-            throws SQLException
     {
         try (Connection connection = DriverManager.getConnection(url, properties);
                 Statement statement = connection.createStatement()) {
             statement.execute(sql);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
