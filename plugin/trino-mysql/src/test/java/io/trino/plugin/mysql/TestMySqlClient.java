ede59df364f3eb6350a65857f710d4befa06c7de
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2021-01-28 22:10:35 +0100

    Make method static

--- plugin/trino-mysql/src/test/java/io/trino/plugin/mysql/TestMySqlClient.java
+++ plugin/trino-mysql/src/test/java/io/trino/plugin/mysql/TestMySqlClient.java
@@ -144,7 +144,7 @@ public class TestMySqlClient
                 Optional.empty()); // filter not supported
     }
 
-    private void testImplementAggregation(AggregateFunction aggregateFunction, Map<String, ColumnHandle> assignments, Optional<String> expectedExpression)
+    private static void testImplementAggregation(AggregateFunction aggregateFunction, Map<String, ColumnHandle> assignments, Optional<String> expectedExpression)
     {
         Optional<JdbcExpression> result = JDBC_CLIENT.implementAggregation(SESSION, aggregateFunction, assignments);
         if (expectedExpression.isEmpty()) {
