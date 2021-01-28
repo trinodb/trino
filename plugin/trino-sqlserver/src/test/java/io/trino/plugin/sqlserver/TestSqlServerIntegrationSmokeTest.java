1644b978486c5585a41e8b2d247038146cd3870d
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2021-01-28 22:10:34 +0100

    Remove extra not needed annotations

--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerIntegrationSmokeTest.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerIntegrationSmokeTest.java
@@ -41,7 +41,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-@Test
 public class TestSqlServerIntegrationSmokeTest
         extends AbstractTestIntegrationSmokeTest
 {
