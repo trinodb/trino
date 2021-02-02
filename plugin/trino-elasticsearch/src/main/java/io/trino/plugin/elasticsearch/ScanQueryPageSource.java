8c7c5b0d1860880722f572a4e5e5671f9584bd0e
Sasha Sheikin <myminitrue@gmail.com>
2021-02-02 12:18:28 +0400

    Make `getColumnMetadata()` consistent with `tableMetadata.getColumns()`
    `ElasticsearchMetadata.getColumnMetadata()` checks for metadata within
    BuiltinColumns first. The same way as it is populated
    in `ConnectorTableMetadata.getColumns()`

--- plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
+++ plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
@@ -59,6 +59,7 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.ID;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SCORE;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SOURCE;
+import static io.trino.plugin.elasticsearch.BuiltinColumns.isBuiltinColumn;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryBuilder.buildSearchQuery;
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.BooleanType.BOOLEAN;
@@ -120,7 +121,7 @@ public class ScanQueryPageSource
 
         List<String> requiredFields = columns.stream()
                 .map(ElasticsearchColumnHandle::getName)
-                .filter(name -> !BuiltinColumns.NAMES.contains(name))
+                .filter(name -> !isBuiltinColumn(name))
                 .collect(toList());
 
         // sorting by _doc (index order) get special treatment in Elasticsearch and is more efficient
