19d0a5b644a40561df5d8116c47e667ec3b6d18c
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-02-03 23:13:51 +0100

    Bump allowed error margins in TestSynapseTableStatistics
    (cherry-picked from commit: cf697506eada6d0c04603e93d14130b6b44fb6d3)

--- plugins/merged-starburst-synapse/src/test/java/com/starburstdata/presto/plugin/synapse/TestSynapseTableStatistics.java
+++ plugins/merged-starburst-synapse/src/test/java/com/starburstdata/presto/plugin/synapse/TestSynapseTableStatistics.java
@@ -386,13 +386,13 @@ public class TestSynapseTableStatistics
             SoftAssertions softly = new SoftAssertions();
 
             softly.assertThat(stats.getDistinctValuesCount().getValue())
-                    .isCloseTo(distinctValues, withinPercentage(40.0));
+                    .isCloseTo(distinctValues, withinPercentage(60.0));
 
             softly.assertThat(stats.getNullsFraction().getValue())
-                    .isCloseTo(nullsFraction, withinPercentage(40.0));
+                    .isCloseTo(nullsFraction, withinPercentage(60.0));
 
             softly.assertThat(stats.getDataSize().getValue())
-                    .isCloseTo(dataSize, withinPercentage(40.0));
+                    .isCloseTo(dataSize, withinPercentage(60.0));
 
             softly.assertThat(stats.getRange()).isEmpty();
             softly.assertAll();
