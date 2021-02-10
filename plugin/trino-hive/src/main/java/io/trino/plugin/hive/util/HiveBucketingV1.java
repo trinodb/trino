930b0b0443708eac925baa4ad82c32e88068b574
Raunaq Morarka <raunaqmorarka@gmail.com>
2021-02-11 02:38:07 +0530

    Improve parallelism for writes to bucketed partitions
    Added an implementation of BucketFunction which allows distributing
    writes to hive buckets in different hive partitions to different nodes
    to improve parallelism during writes.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/util/HiveBucketingV1.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/util/HiveBucketingV1.java
@@ -38,11 +38,10 @@ final class HiveBucketingV1
 {
     private HiveBucketingV1() {}
 
-    static int getBucketHashCode(List<TypeInfo> types, Page page, int position)
+    static int getBucketHashCode(List<TypeInfo> types, Page page, int prefixChannels, int position)
     {
-        checkArgument(types.size() == page.getChannelCount());
         int result = 0;
-        for (int i = 0; i < page.getChannelCount(); i++) {
+        for (int i = 0; i < prefixChannels; i++) {
             int fieldHash = hash(types.get(i), page.getBlock(i), position);
             result = result * 31 + fieldHash;
         }
