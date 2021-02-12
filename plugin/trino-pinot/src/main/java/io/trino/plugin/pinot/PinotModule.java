ab31e59f52145cc95dba041f7256152aeb118268
Elon Azoulay <elon.azoulay@gmail.com>
2021-02-12 08:28:46 -0800

    Use pinot table name
    Pinot table names can be mixed case.
    Create a mapping from presto tableName
    to pinotTableName.

--- plugin/trino-pinot/src/main/java/io/trino/plugin/pinot/PinotModule.java
+++ plugin/trino-pinot/src/main/java/io/trino/plugin/pinot/PinotModule.java
@@ -40,7 +40,7 @@ import javax.inject.Inject;
 import javax.management.MBeanServer;
 
 import java.io.IOException;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
@@ -80,7 +80,7 @@ public class PinotModule
         binder.bind(PinotPageSourceProvider.class).in(Scopes.SINGLETON);
         binder.bind(PinotClient.class).in(Scopes.SINGLETON);
         binder.bind(PinotQueryClient.class).in(Scopes.SINGLETON);
-        binder.bind(Executor.class).annotatedWith(ForPinot.class)
+        binder.bind(ExecutorService.class).annotatedWith(ForPinot.class)
                 .toInstance(newCachedThreadPool(threadsNamed("pinot-metadata-fetcher-" + catalogName)));
 
         binder.bind(PinotSessionProperties.class).in(Scopes.SINGLETON);
--- plugin/trino-pinot/src/main/java/io/trino/plugin/pinot/PinotTableHandle.java
+++ plugin/trino-pinot/src/main/java/io/trino/plugin/pinot/PinotTableHandle.java
@@ -26,7 +26,6 @@ import java.util.Optional;
 import java.util.OptionalLong;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
-import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
 public class PinotTableHandle
@@ -53,7 +52,7 @@ public class PinotTableHandle
 
     {
         this.schemaName = requireNonNull(schemaName, "schemaName is null");
-        this.tableName = requireNonNull(tableName, "tableName is null").toLowerCase(ENGLISH);
+        this.tableName = requireNonNull(tableName, "tableName is null");
         this.constraint = requireNonNull(constraint, "constraint is null");
         this.limit = requireNonNull(limit, "limit is null");
         this.query = requireNonNull(query, "query is null");
