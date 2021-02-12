17cac412a6e7658b77ab169bb829b5f61e1af2f9
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2021-02-12 10:51:22 +0100

    Add Error Prone check: GetClassOnAnnotation
    The problem here is that the Annotation object can be a proxy, in which
    case we won't get meaningful output. The fix for the only instance is
    assuming that the eventual proxy will have a meaningful toString
    implementation, which might be too optimistic, though.

--- core/trino-main/src/main/java/io/trino/operator/annotations/ImplementationDependency.java
+++ core/trino-main/src/main/java/io/trino/operator/annotations/ImplementationDependency.java
@@ -139,7 +139,7 @@ public interface ImplementationDependency
                         type);
             }
 
-            throw new IllegalArgumentException("Unsupported annotation " + annotation.getClass().getSimpleName());
+            throw new IllegalArgumentException("Unsupported annotation " + annotation);
         }
 
         private static InvocationConvention toInvocationConvention(Convention convention)
