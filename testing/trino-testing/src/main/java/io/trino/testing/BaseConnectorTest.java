569081993a239443bef02cfbeea6cdc823dd892f
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-02-19 09:31:23 +0100

    Indicate constraint in JdbcTableHandle.toString
    The `toString` is used by `EXPLAIN` to provide information for the user.

--- testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
@@ -169,6 +169,15 @@ public abstract class BaseConnectorTest
     }
 
     @Test
+    public void testPredicateReflectedInExplain()
+    {
+        // Even if the predicate is pushed down into the table scan, it should still be reflected in EXPLAIN (via ConnectorTableHandle.toString)
+        assertExplain(
+                "EXPLAIN SELECT name FROM nation WHERE nationkey = 42",
+                "(predicate|filterPredicate|constraint).{0,10}(nationkey|NATIONKEY)");
+    }
+
+    @Test
     public void testConcurrentScans()
     {
         String unionMultipleTimes = join(" UNION ALL ", nCopies(25, "SELECT * FROM orders"));
