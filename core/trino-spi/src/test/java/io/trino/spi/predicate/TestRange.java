47195a97c143dc3eeea12bf9c018301ad5283803
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-02-22 13:33:31 +0100

    Fix logical merge conflict

--- core/trino-spi/src/test/java/io/trino/spi/predicate/TestRange.java
+++ core/trino-spi/src/test/java/io/trino/spi/predicate/TestRange.java
@@ -21,6 +21,7 @@ import static io.trino.spi.type.BooleanType.BOOLEAN;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
