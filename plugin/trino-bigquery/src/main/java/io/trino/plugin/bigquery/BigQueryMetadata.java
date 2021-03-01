shiroyuki/presto

Add support for CREATE and DROP TABLE in BigQuery

presto-bigquery/src/main/java/io/prestosql/plugin/bigquery/BigQueryMetadata.java
@@ -16,6 +16,7 @@ package io.prestosql.plugin.bigquery;
 import com.google.cloud.bigquery.DatasetId;
 import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardTableDefinition;
 import com.google.cloud.bigquery.Table;
 import com.google.cloud.bigquery.TableDefinition;
 import com.google.cloud.bigquery.TableId;
@@ -43,6 +44,7 @@ import io.prestosql.spi.connector.SchemaTablePrefix;
 import io.prestosql.spi.connector.TableNotFoundException;
 import io.prestosql.spi.expression.ConnectorExpression;
 import io.prestosql.spi.predicate.TupleDomain;
+import io.prestosql.spi.type.Type;
 
 import javax.inject.Inject;
 
@@ -54,6 +56,7 @@ import java.util.Set;
 import static com.google.cloud.bigquery.TableDefinition.Type.TABLE;
 import static com.google.cloud.bigquery.TableDefinition.Type.VIEW;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.prestosql.plugin.bigquery.BigQueryType.toField;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
@@ -239,6 +242,20 @@ public class BigQueryMetadata
     }
 
     @Override
+    public void createTable(ConnectorSession session, ConnectorTableMetadata tableMetadata, boolean ignoreExisting)
+    {
+        createTable(tableMetadata);
+    }
+
+    @Override
+    public void dropTable(ConnectorSession session, ConnectorTableHandle tableHandle)
+    {
+        BigQueryTableHandle bigqueryTable = (BigQueryTableHandle) tableHandle;
+        TableId tableId = TableId.of(projectId, bigqueryTable.getSchemaName(), bigqueryTable.getTableName());
+        bigQueryClient.dropTable(tableId);
+    }
+
+    @Override
     public Optional<LimitApplicationResult<ConnectorTableHandle>> applyLimit(
             ConnectorSession session,
             ConnectorTableHandle handle,
@@ -302,5 +319,33 @@ public class BigQueryMetadata
         BigQueryTableHandle updatedHandle = bigQueryTableHandle.withConstraint(newDomain);
 
         return Optional.of(new ConstraintApplicationResult<>(updatedHandle, constraint.getSummary()));
+    }
+
+    private BigQueryOutputTableHandle createTable(ConnectorTableMetadata tableMetadata)
+    {
+        ImmutableList.Builder<String> columnNames = ImmutableList.builder();
+        ImmutableList.Builder<Type> columnTypes = ImmutableList.builder();
+        ImmutableList.Builder<Field> fields = ImmutableList.builder();
+        for (ColumnMetadata column : tableMetadata.getColumns()) {
+            columnNames.add(column.getName());
+            columnTypes.add(column.getType());
+            fields.add(toField(column.getName(), column.getType()));
+        }
+
+        SchemaTableName schemaTableName = tableMetadata.getTable();
+        String schemaName = schemaTableName.getSchemaName();
+        String tableName = schemaTableName.getTableName();
+
+        TableId tableId = TableId.of(schemaName, tableName);
+        TableDefinition tableDefinition = StandardTableDefinition.of(Schema.of(fields.build()));
+        TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
+
+        bigQueryClient.createTable(tableInfo);
+
+        return new BigQueryOutputTableHandle(
+                schemaName,
+                tableName,
+                columnNames.build(),
+                columnTypes.build());
     }
 }

presto-bigquery/src/main/java/io/prestosql/plugin/bigquery/BigQueryType.java
@@ -14,19 +14,26 @@
 package io.prestosql.plugin.bigquery;
 
 import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.FieldList;
+import com.google.cloud.bigquery.LegacySQLTypeName;
+import com.google.cloud.bigquery.StandardSQLTypeName;
 import io.airlift.slice.Slice;
 import io.prestosql.spi.type.ArrayType;
 import io.prestosql.spi.type.BigintType;
 import io.prestosql.spi.type.BooleanType;
+import io.prestosql.spi.type.CharType;
 import io.prestosql.spi.type.DateTimeEncoding;
 import io.prestosql.spi.type.DateType;
 import io.prestosql.spi.type.DecimalType;
 import io.prestosql.spi.type.Decimals;
 import io.prestosql.spi.type.DoubleType;
+import io.prestosql.spi.type.IntegerType;
 import io.prestosql.spi.type.RowType;
+import io.prestosql.spi.type.SmallintType;
 import io.prestosql.spi.type.TimeWithTimeZoneType;
 import io.prestosql.spi.type.TimestampType;
 import io.prestosql.spi.type.TimestampWithTimeZoneType;
+import io.prestosql.spi.type.TinyintType;
 import io.prestosql.spi.type.Type;
 import io.prestosql.spi.type.VarbinaryType;
 import io.prestosql.spi.type.VarcharType;
@@ -41,7 +48,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static com.google.cloud.bigquery.LegacySQLTypeName.legacySQLTypeName;
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.prestosql.plugin.bigquery.BigQueryMetadata.NUMERIC_DATA_TYPE_PRECISION;
 import static io.prestosql.plugin.bigquery.BigQueryMetadata.NUMERIC_DATA_TYPE_SCALE;
@@ -178,6 +187,55 @@ public enum BigQueryType
     {
         Slice slice = (Slice) value;
         return format("FROM_BASE64('%s')", Base64.getEncoder().encodeToString(slice.getBytes()));
+    }
+
+    public static Field toField(String name, Type type)
+    {
+        if (type instanceof ArrayType) {
+            Type elementType = ((ArrayType) type).getElementType();
+            LegacySQLTypeName legacySqlTypeName = legacySQLTypeName(toStandardSqlTypeName(elementType));
+            return Field.newBuilder(name, legacySqlTypeName).setMode(Field.Mode.REPEATED).build();
+        }
+        if (type instanceof RowType) {
+            RowType rowType = (RowType) type;
+            List<Field> subFields = rowType.getFields().stream()
+                    .map(field -> Field.of(field.getName().orElse("field" + rowType.getFields().indexOf(field)), toStandardSqlTypeName(field.getType())))
+                    .collect(Collectors.toUnmodifiableList());
+            return Field.of(name, StandardSQLTypeName.STRUCT, FieldList.of(subFields));
+        }
+        return Field.of(name, toStandardSqlTypeName(type));
+    }
+
+    private static StandardSQLTypeName toStandardSqlTypeName(Type type)
+    {
+        if (type == BooleanType.BOOLEAN) {
+            return StandardSQLTypeName.BOOL;
+        }
+        if (type == TinyintType.TINYINT || type == SmallintType.SMALLINT || type == IntegerType.INTEGER || type == BigintType.BIGINT) {
+            return StandardSQLTypeName.INT64;
+        }
+        if (type == DoubleType.DOUBLE) {
+            return StandardSQLTypeName.FLOAT64;
+        }
+        if (type instanceof DecimalType) {
+            return StandardSQLTypeName.NUMERIC;
+        }
+        if (type == DateType.DATE) {
+            return StandardSQLTypeName.DATE;
+        }
+        if (type == TimestampType.TIMESTAMP) {
+            return StandardSQLTypeName.DATETIME;
+        }
+        if (type == TimeWithTimeZoneType.TIME_WITH_TIME_ZONE) {
+            return StandardSQLTypeName.TIME;
+        }
+        if (type == TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE) {
+            return StandardSQLTypeName.TIMESTAMP;
+        }
+        if (type instanceof VarcharType || type instanceof CharType) {
+            return StandardSQLTypeName.STRING;
+        }
+        throw new IllegalArgumentException("Unsupported column type: " + type);
     }
 
     private static String quote(String value)

trinodb/trino

plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryMetadata.java
@@ -13,8 +13,13 @@
  */
 package io.trino.plugin.bigquery;
 
+import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.DatasetId;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardTableDefinition;
 import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableDefinition;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
 import com.google.cloud.bigquery.ViewDefinition;
@@ -61,6 +66,7 @@ import static com.google.cloud.bigquery.TableDefinition.Type.TABLE;
 import static com.google.cloud.bigquery.TableDefinition.Type.VIEW;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.trino.plugin.bigquery.BigQueryType.toField;
 import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
@@ -284,6 +290,44 @@ public class BigQueryMetadata
     {
         log.debug("getTableProperties(session=%s, prefix=%s)", session, table);
         return new ConnectorTableProperties();
+    }
+
+    @Override
+    public void createTable(ConnectorSession session, ConnectorTableMetadata tableMetadata, boolean ignoreExisting)
+    {
+        try {
+            createTable(tableMetadata);
+        }
+        catch (BigQueryException e) {
+            if (ignoreExisting && e.getCode() == 409) {
+                return;
+            }
+            throw e;
+        }
+    }
+
+    private void createTable(ConnectorTableMetadata tableMetadata)
+    {
+        SchemaTableName schemaTableName = tableMetadata.getTable();
+        String schemaName = schemaTableName.getSchemaName();
+        String tableName = schemaTableName.getTableName();
+        List<Field> fields = tableMetadata.getColumns().stream()
+                .map(column -> toField(column.getName(), column.getType()))
+                .collect(toImmutableList());
+
+        TableId tableId = TableId.of(schemaName, tableName);
+        TableDefinition tableDefinition = StandardTableDefinition.of(Schema.of(fields));
+        TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
+
+        bigQueryClient.createTable(tableInfo);
+    }
+
+    @Override
+    public void dropTable(ConnectorSession session, ConnectorTableHandle tableHandle)
+    {
+        BigQueryTableHandle bigQueryTable = (BigQueryTableHandle) tableHandle;
+        TableId tableId = bigQueryTable.getRemoteTableName().toTableId();
+        bigQueryClient.dropTable(tableId);
     }
 
     @Override

plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryType.java
@@ -14,19 +14,27 @@
 package io.trino.plugin.bigquery;
 
 import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.FieldList;
+import com.google.cloud.bigquery.StandardSQLTypeName;
+import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
+import io.trino.spi.TrinoException;
 import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.BigintType;
 import io.trino.spi.type.BooleanType;
+import io.trino.spi.type.CharType;
 import io.trino.spi.type.DateTimeEncoding;
 import io.trino.spi.type.DateType;
 import io.trino.spi.type.DecimalType;
 import io.trino.spi.type.Decimals;
 import io.trino.spi.type.DoubleType;
+import io.trino.spi.type.IntegerType;
 import io.trino.spi.type.RowType;
+import io.trino.spi.type.SmallintType;
 import io.trino.spi.type.TimeWithTimeZoneType;
 import io.trino.spi.type.TimestampType;
 import io.trino.spi.type.TimestampWithTimeZoneType;
+import io.trino.spi.type.TinyintType;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.VarbinaryType;
 import io.trino.spi.type.VarcharType;
@@ -42,9 +50,13 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
+import static com.google.cloud.bigquery.Field.Mode.REPEATED;
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.trino.plugin.bigquery.BigQueryMetadata.NUMERIC_DATA_TYPE_PRECISION;
 import static io.trino.plugin.bigquery.BigQueryMetadata.NUMERIC_DATA_TYPE_SCALE;
+import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
+import static io.trino.spi.type.TimeWithTimeZoneType.DEFAULT_PRECISION;
+import static io.trino.spi.type.TimeWithTimeZoneType.createTimeWithTimeZoneType;
 import static io.trino.spi.type.Timestamps.MICROSECONDS_PER_MILLISECOND;
 import static io.trino.spi.type.VarcharType.createUnboundedVarcharType;
 import static java.lang.Integer.parseInt;
@@ -182,6 +194,82 @@ public enum BigQueryType
         return format("FROM_BASE64('%s')", Base64.getEncoder().encodeToString(slice.getBytes()));
     }
 
+    public static Field toField(String name, Type type)
+    {
+        if (type instanceof ArrayType) {
+            Type elementType = ((ArrayType) type).getElementType();
+            return toInnerField(name, elementType, true);
+        }
+        return toInnerField(name, type, false);
+    }
+
+    private static Field toInnerField(String name, Type type, boolean repeated)
+    {
+        Field.Builder builder;
+        if (type instanceof RowType) {
+            builder = Field.newBuilder(name, StandardSQLTypeName.STRUCT, toFieldList((RowType) type));
+        }
+        else {
+            builder = Field.newBuilder(name, toStandardSqlTypeName(type));
+        }
+        if (repeated) {
+            builder = builder.setMode(REPEATED);
+        }
+        return builder.build();
+    }
+
+    private static FieldList toFieldList(RowType rowType)
+    {
+        ImmutableList.Builder<Field> fields = new ImmutableList.Builder<>();
+        for (RowType.Field field : rowType.getFields()) {
+            String fieldName = field.getName()
+                    .orElseThrow(() -> new TrinoException(NOT_SUPPORTED, "ROW type does not have field names declared: " + rowType));
+            fields.add(toField(fieldName, field.getType()));
+        }
+        return FieldList.of(fields.build());
+    }
+
+    private static StandardSQLTypeName toStandardSqlTypeName(Type type)
+    {
+        if (type == BooleanType.BOOLEAN) {
+            return StandardSQLTypeName.BOOL;
+        }
+        if (type == TinyintType.TINYINT || type == SmallintType.SMALLINT || type == IntegerType.INTEGER || type == BigintType.BIGINT) {
+            return StandardSQLTypeName.INT64;
+        }
+        if (type == DoubleType.DOUBLE) {
+            return StandardSQLTypeName.FLOAT64;
+        }
+        if (type instanceof DecimalType) {
+            return StandardSQLTypeName.NUMERIC;
+        }
+        if (type == DateType.DATE) {
+            return StandardSQLTypeName.DATE;
+        }
+        if (type == createTimeWithTimeZoneType(DEFAULT_PRECISION)) {
+            return StandardSQLTypeName.TIME;
+        }
+        if (type == TimestampType.TIMESTAMP_MILLIS) {
+            return StandardSQLTypeName.DATETIME;
+        }
+        if (type == TimestampWithTimeZoneType.TIMESTAMP_TZ_MILLIS) {
+            return StandardSQLTypeName.TIMESTAMP;
+        }
+        if (type instanceof CharType || type instanceof VarcharType) {
+            return StandardSQLTypeName.STRING;
+        }
+        if (type == VarbinaryType.VARBINARY) {
+            return StandardSQLTypeName.BYTES;
+        }
+        if (type instanceof ArrayType) {
+            return StandardSQLTypeName.ARRAY;
+        }
+        if (type instanceof RowType) {
+            return StandardSQLTypeName.STRUCT;
+        }
+        throw new TrinoException(NOT_SUPPORTED, "Unsupported column type: " + type.getDisplayName());
+    }
+
     private static String quote(String value)
     {
         return "'" + value + "'";
@@ -217,7 +305,7 @@ public enum BigQueryType
         default Type getTrinoType()
         {
             Type rawType = getBigQueryType().getNativeType(this);
-            return getMode() == Field.Mode.REPEATED ? new ArrayType(rawType) : rawType;
+            return getMode() == REPEATED ? new ArrayType(rawType) : rawType;
         }
     }
 