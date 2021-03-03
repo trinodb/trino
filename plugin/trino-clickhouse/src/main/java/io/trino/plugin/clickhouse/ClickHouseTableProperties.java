2623d1e9071d05bf3c0a0d1bee27215c30a99edd
wgzhao <wgzhao@gmail.com>
2021-03-03 16:43:27 +0800

    Add support for create table with MergeTree engine.
    2021-03-06
    1. Changes engine property to enum class
    2. Validates required property
    
    2021-03-08
    1. Add test cases for table properties
    
    2021-03-09
    1. move ClickHouseTableProperties#formatPropety to ClickHouse
    2. Add all passed table properties regardless of table engine
    3. more tests
    
    2021-03-10
    1. Fixed excepted message for test

--- plugin/trino-clickhouse/src/main/java/io/trino/plugin/clickhouse/ClickHouseTableProperties.java
+++ plugin/trino-clickhouse/src/main/java/io/trino/plugin/clickhouse/ClickHouseTableProperties.java
@@ -16,6 +16,7 @@ package io.trino.plugin.clickhouse;
 import com.google.common.collect.ImmutableList;
 import io.trino.plugin.jdbc.TablePropertiesProvider;
 import io.trino.spi.session.PropertyMetadata;
+import io.trino.spi.type.ArrayType;
 
 import javax.inject.Inject;
 
@@ -23,7 +24,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static io.trino.spi.session.PropertyMetadata.enumProperty;
 import static io.trino.spi.session.PropertyMetadata.stringProperty;
+import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -36,7 +39,13 @@ public final class ClickHouseTableProperties
         implements TablePropertiesProvider
 {
     public static final String ENGINE_PROPERTY = "engine";
-    public static final String DEFAULT_TABLE_ENGINE = "Log";
+    // MergeTree engine properties
+    public static final String ORDER_BY_PROPERTY = "order_by"; //required
+    public static final String PARTITION_BY_PROPERTY = "partition_by"; //optional
+    public static final String PRIMARY_KEY_PROPERTY = "primary_key"; //optional
+    public static final String SAMPLE_BY_PROPERTY = "sample_by"; //optional
+
+    public static final ClickHouseEngineType DEFAULT_TABLE_ENGINE = ClickHouseEngineType.LOG;
 
     private final List<PropertyMetadata<?>> tableProperties;
 
@@ -44,18 +53,78 @@ public final class ClickHouseTableProperties
     public ClickHouseTableProperties()
     {
         tableProperties = ImmutableList.of(
-                stringProperty(
+                enumProperty(
                         ENGINE_PROPERTY,
                         "ClickHouse Table Engine, defaults to Log",
+                        ClickHouseEngineType.class,
                         DEFAULT_TABLE_ENGINE,
+                        false),
+                new PropertyMetadata<>(
+                        ORDER_BY_PROPERTY,
+                        "columns to be the sorting key, it's required for table MergeTree engine family",
+                        new ArrayType(VARCHAR),
+                        List.class,
+                        ImmutableList.of(),
+                        false,
+                        value -> (List<?>) value,
+                        value -> value),
+                new PropertyMetadata<>(
+                        PARTITION_BY_PROPERTY,
+                        "columns to be the partition key. it's optional for table MergeTree engine family",
+                        new ArrayType(VARCHAR),
+                        List.class,
+                        ImmutableList.of(),
+                        false,
+                        value -> (List<?>) value,
+                        value -> value),
+                new PropertyMetadata<>(
+                        PRIMARY_KEY_PROPERTY,
+                        "columns to be the primary key. it's optional for table MergeTree engine family",
+                        new ArrayType(VARCHAR),
+                        List.class,
+                        ImmutableList.of(),
+                        false,
+                        value -> (List<?>) value,
+                        value -> value),
+                stringProperty(
+                        SAMPLE_BY_PROPERTY,
+                        "An expression for sampling. it's optional for table MergeTree engine family",
+                        null,
                         false));
     }
 
-    public static Optional<String> getEngine(Map<String, Object> tableProperties)
+    public static ClickHouseEngineType getEngine(Map<String, Object> tableProperties)
     {
-        requireNonNull(tableProperties);
+        requireNonNull(tableProperties, "tableProperties is null");
+        return (ClickHouseEngineType) tableProperties.get(ENGINE_PROPERTY);
+    }
 
-        return Optional.ofNullable(tableProperties.get(ENGINE_PROPERTY)).map(String.class::cast);
+    @SuppressWarnings("unchecked")
+    public static List<String> getOrderBy(Map<String, Object> tableProperties)
+    {
+        requireNonNull(tableProperties, "tableProperties is null");
+        return (List<String>) tableProperties.get(ORDER_BY_PROPERTY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<String> getPartitionBy(Map<String, Object> tableProperties)
+    {
+        requireNonNull(tableProperties, "tableProperties is null");
+        return (List<String>) tableProperties.get(PARTITION_BY_PROPERTY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<String> getPrimaryKey(Map<String, Object> tableProperties)
+    {
+        requireNonNull(tableProperties, "tableProperties is null");
+        return (List<String>) tableProperties.get(PRIMARY_KEY_PROPERTY);
+    }
+
+    public static Optional<String> getSampleBy(Map<String, Object> tableProperties)
+    {
+        requireNonNull(tableProperties, "tableProperties is null");
+
+        return Optional.ofNullable(tableProperties.get(SAMPLE_BY_PROPERTY)).map(String.class::cast);
     }
 
     @Override
