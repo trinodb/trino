3e9363670783cd81ee7450a252cb1bf1b90ce9f6
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-03-04 14:17:12 +0100

    Move default behavior logic to TestingConnectorBehavior

--- testing/trino-testing/src/main/java/io/trino/testing/TestingConnectorBehavior.java
+++ testing/trino-testing/src/main/java/io/trino/testing/TestingConnectorBehavior.java
@@ -13,16 +13,57 @@
  */
 package io.trino.testing;
 
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
 public enum TestingConnectorBehavior
 {
     SUPPORTS_PREDICATE_PUSHDOWN,
-    SUPPORTS_PREDICATE_PUSHDOWN_WITH_VARCHAR_EQUALITY,
-    SUPPORTS_PREDICATE_PUSHDOWN_WITH_VARCHAR_INEQUALITY,
+    SUPPORTS_PREDICATE_PUSHDOWN_WITH_VARCHAR_EQUALITY(SUPPORTS_PREDICATE_PUSHDOWN),
+    SUPPORTS_PREDICATE_PUSHDOWN_WITH_VARCHAR_INEQUALITY(SUPPORTS_PREDICATE_PUSHDOWN),
     SUPPORTS_LIMIT_PUSHDOWN,
     SUPPORTS_TOPN_PUSHDOWN,
     SUPPORTS_AGGREGATION_PUSHDOWN,
-    SUPPORTS_JOIN_PUSHDOWN,
-    SUPPORTS_JOIN_PUSHDOWN_WITH_FULL_JOIN,
-    SUPPORTS_JOIN_PUSHDOWN_WITH_DISTINCT_FROM,
+    SUPPORTS_JOIN_PUSHDOWN(
+            // Currently no connector supports Join pushdown by default. JDBC connectors may support Join pushdown and BaseJdbcConnectorTest
+            // verifies truthfulness of SUPPORTS_JOIN_PUSHDOWN declaration, so it is a safe default.
+            false),
+    SUPPORTS_JOIN_PUSHDOWN_WITH_FULL_JOIN(SUPPORTS_JOIN_PUSHDOWN),
+    SUPPORTS_JOIN_PUSHDOWN_WITH_DISTINCT_FROM(SUPPORTS_JOIN_PUSHDOWN),
     /**/;
+
+    private final Predicate<Predicate<TestingConnectorBehavior>> hasBehaviorByDefault;
+
+    TestingConnectorBehavior()
+    {
+        this(true);
+    }
+
+    TestingConnectorBehavior(boolean hasBehaviorByDefault)
+    {
+        this(fallback -> hasBehaviorByDefault);
+    }
+
+    TestingConnectorBehavior(TestingConnectorBehavior defaultBehaviorSource)
+    {
+        this(fallback -> fallback.test(defaultBehaviorSource));
+    }
+
+    TestingConnectorBehavior(Predicate<Predicate<TestingConnectorBehavior>> hasBehaviorByDefault)
+    {
+        this.hasBehaviorByDefault = requireNonNull(hasBehaviorByDefault, "hasBehaviorByDefault is null");
+    }
+
+    /**
+     * Determines whether a connector should be assumed to have this behavior.
+     *
+     * @param fallback callback to be used to inspect connector behaviors, if this behavior is assumed to be had conditionally
+     */
+    // Intentionally package private. This is just a convenience method for sharing behavior between reusable test base classes.
+    // If it was to be public, a different API would need to be provided.
+    boolean hasBehaviorByDefault(Predicate<TestingConnectorBehavior> fallback)
+    {
+        return hasBehaviorByDefault.test(fallback);
+    }
 }
