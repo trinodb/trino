0b27eac2cf2743a65e4aeb59ffa751f48ed0885b
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2021-03-09 16:07:37 +0100

    Test PostgreSQL query cancelling

--- plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestingPostgreSqlServer.java
+++ plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestingPostgreSqlServer.java
@@ -13,6 +13,11 @@
  */
 package io.trino.plugin.postgresql;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import io.trino.plugin.jdbc.RemoteDatabaseEvent;
+import net.jodah.failsafe.function.CheckedRunnable;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.io.Closeable;
@@ -20,9 +25,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Stream;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static io.trino.plugin.jdbc.RemoteDatabaseEvent.Status.CANCELLED;
+import static io.trino.plugin.jdbc.RemoteDatabaseEvent.Status.RUNNING;
 import static java.lang.String.format;
+import static java.util.function.Predicate.not;
+import static org.testcontainers.containers.BindMode.READ_ONLY;
 import static org.testcontainers.containers.PostgreSQLContainer.POSTGRESQL_PORT;
 
 public class TestingPostgreSqlServer
@@ -40,8 +53,12 @@ public class TestingPostgreSqlServer
         dockerContainer = new PostgreSQLContainer<>("postgres:9.5")
                 .withDatabaseName(DATABASE)
                 .withUsername(USER)
-                .withPassword(PASSWORD);
+                .withPassword(PASSWORD)
+                .withClasspathResourceMapping("postgresql.conf", "/etc/postgresql/postgresql.conf", READ_ONLY)
+                .withCommand("postgres", "-c", "config_file=/etc/postgresql/postgresql.conf");
         dockerContainer.start();
+
+        execute("CREATE VIEW sleep_for_a_minute AS SELECT 1 FROM pg_sleep(60)");
     }
 
     public void execute(String sql)
@@ -58,6 +75,38 @@ public class TestingPostgreSqlServer
         catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected List<RemoteDatabaseEvent> captureRemoteEventsDuring(CheckedRunnable runnable)
+            throws Throwable
+    {
+        ImmutableSet<String> before = getLogs();
+        runnable.run();
+        ImmutableSet<String> after = getLogs();
+        ImmutableList.Builder<RemoteDatabaseEvent> events = ImmutableList.builder();
+        Sets.SetView<String> logs = Sets.difference(after, before);
+        Iterator<String> logsIterator = logs.iterator();
+        while (logsIterator.hasNext()) {
+            String logLine = logsIterator.next();
+            if (logLine.startsWith("LOG:  execute <unnamed>: ")) {
+                events.add(new RemoteDatabaseEvent(logLine.substring(25), RUNNING));
+            }
+            if (logLine.equals("ERROR:  canceling statement due to user request")) {
+                String cancelledStatementLogLine = logsIterator.next();
+                if (cancelledStatementLogLine.startsWith("STATEMENT: ")) {
+                    events.add(new RemoteDatabaseEvent(cancelledStatementLogLine.substring(12), CANCELLED));
+                }
+            }
+            // ignore unsupported log lines
+        }
+        return events.build();
+    }
+
+    private ImmutableSet<String> getLogs()
+    {
+        return Stream.of(dockerContainer.getLogs().split("\n"))
+                .filter(not(String::isBlank))
+                .collect(toImmutableSet());
     }
 
     public String getUser()
