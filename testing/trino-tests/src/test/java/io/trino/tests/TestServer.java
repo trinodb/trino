56019ce7e6d406061e08477d4cd7d036adecc100
kasiafi <30203062+kasiafi@users.noreply.github.com>
2021-03-12 21:55:18 +0100

    Suppress exceptions during expression optimization
    This change introduces clear distinction between
    the two possible actions performed by `ExpressionInterpreter`:
    expression optimization and expression evaluation.
    
    1. Expression optimization is meant for the stage of query
    optimization. It is not the execution of the expression,
    but an attempt to simplify it, and compute the actual value
    if possible.
    - After this change, expression optimization never fails in case
    when the expression causes exception (e.g. 0/0).
    Instead, the incorrect expression remains unchanged. When the
    expression is executed, it will throw exception.
    This is to take advantage of the possibility that some other
    optimization might remove the erroneous expression from the plan.
    Before this change, error suppression was limited to certain
    types of expressions (CASE and IF).
    - Also, before this change, any detected incorrect expressions
    were replaced by a call to `fail()` function, which threw exception
    when the expression was executed. Introducing the 'fail()' function
    changed the reported error code from the original code
    (e.g. DIVISION_BY_ZERO) to GENERIC_USER_ERROR. Now, we always get
    the original error code.
    - Another advantage of this change for expression optimization
    is finding the errors on the innermost level, which allows
    the remaining part of the expression to be further optimized.
    
    2. Expression evaluation should be considered as the actual
    expression execution. E.g. executing `ValuesNode` is based
    on this mechanism.
    After this change, all errors that occur during expression evaluation
    are propagated. Before, errors were suppressed in the same cases as
    with expression optimization (in CASE and IF expressions). This could
    cause ClassCastException when executing Values due to unresolved
    expressions.

--- testing/trino-tests/src/test/java/io/trino/tests/TestServer.java
+++ testing/trino-tests/src/test/java/io/trino/tests/TestServer.java
@@ -249,8 +249,8 @@ public class TestServer
         checkVersionOnError("SELECT query that fails parsing", "ParsingException: line 1:19: mismatched input 'fails'. Expecting");
         // fails during analysis
         checkVersionOnError("SELECT foo FROM some_catalog.some_schema.no_such_table", "TrinoException: line 1:17: Catalog 'some_catalog' does not exist");
-        // fails during optimization
-        checkVersionOnError("SELECT 1 / 0", "TrinoException: Division by zero(?s:.*)at io.trino.sql.planner.iterative.rule.SimplifyExpressions");
+        // fails during execution
+        checkVersionOnError("SELECT 1 / 0", "TrinoException: Division by zero(?s:.*)at io.trino.operator.Driver.processInternal");
         // fails during execution
         checkVersionOnError("select 1 / a from (values 0) t(a)", "TrinoException: Division by zero(?s:.*)at io.trino.operator.Driver.processInternal");
     }
