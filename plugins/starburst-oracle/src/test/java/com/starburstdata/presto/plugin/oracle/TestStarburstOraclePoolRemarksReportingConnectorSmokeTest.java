354575247492f96a6130b5592d3451b498888bee
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2021-03-15 11:17:02 +0100

    Run TestStarburstOraclePoolRemarksReporting single threaded
    (cherry-picked from commit: 4cfca7ad7a05bccd8219f5c00034ee509002aac5)

--- plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/TestStarburstOraclePoolRemarksReportingConnectorSmokeTest.java
+++ plugins/starburst-oracle/src/test/java/com/starburstdata/presto/plugin/oracle/TestStarburstOraclePoolRemarksReportingConnectorSmokeTest.java
@@ -12,7 +12,11 @@ package com.starburstdata.presto.plugin.oracle;
 import com.google.common.collect.ImmutableMap;
 import io.trino.plugin.oracle.TestOraclePoolRemarksReportingConnectorSmokeTest;
 import io.trino.testing.QueryRunner;
+import org.testng.annotations.Test;
 
+// Oracle tests with remarks enabled are executed single threaded due to ORA-12847
+// that happens on concurrent DDL operations.
+@Test(singleThreaded = true)
 public class TestStarburstOraclePoolRemarksReportingConnectorSmokeTest
         extends TestOraclePoolRemarksReportingConnectorSmokeTest
 {
