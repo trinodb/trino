4126abfa60fc74573f2d2c505f437c87e3245430
Raunaq Morarka <raunaqmorarka@gmail.com>
2021-03-24 15:28:21 +0530

    Move SessionPropertiesProvider to trino-plugin-toolkit

--- lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/session/SessionPropertiesProvider.java
+++ lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/session/SessionPropertiesProvider.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.jdbc;
+package io.trino.plugin.base.session;
 
 import io.trino.spi.session.PropertyMetadata;
 
