6addd0f662b190fa1390be7d8639a63bb1fba9f3
Sasha Sheikin <myminitrue@gmail.com>
2021-03-26 20:31:33 +0100

    Update Trino version to 354-54-gfcd7f327
    - Remove `testing_catalog` from tests
    - Changes for HdfsContext
    - Changes for SessionPropertiesProvider move to trino-plugin-toolkit
    
    (cherry-picked from commit: 76aeffa88ff8ad05dcbec19c62e6d6ba28f00cc8)

--- plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/StarburstOracleSessionProperties.java
+++ plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/StarburstOracleSessionProperties.java
@@ -11,7 +11,7 @@ package com.starburstdata.presto.plugin.oracle;
 
 import com.google.common.collect.ImmutableList;
 import com.starburstdata.presto.license.LicenseManager;
-import io.trino.plugin.jdbc.SessionPropertiesProvider;
+import io.trino.plugin.base.session.SessionPropertiesProvider;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.session.PropertyMetadata;
 
