2c67238b3344fb822185890c70e52974f86ca768
praveenkrishna <praveenkrishna@tutanota.com>
2021-04-02 15:25:01 +0530

    Allow MockConnector to select/drop materialized view

--- testing/trino-tests/src/test/java/io/trino/tests/TestMockConnector.java
+++ testing/trino-tests/src/test/java/io/trino/tests/TestMockConnector.java
@@ -14,13 +14,23 @@
 package io.trino.tests;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import io.trino.connector.MockConnectorFactory;
 import io.trino.connector.MockConnectorPlugin;
+import io.trino.connector.MockConnectorTableHandle;
+import io.trino.plugin.tpch.TpchPlugin;
+import io.trino.spi.connector.ColumnMetadata;
+import io.trino.spi.connector.ConnectorMaterializedViewDefinition;
+import io.trino.spi.connector.ConnectorMaterializedViewDefinition.Column;
+import io.trino.spi.connector.SchemaTableName;
 import io.trino.testing.AbstractTestQueryFramework;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.QueryRunner;
 import org.testng.annotations.Test;
 
+import java.util.Optional;
+
+import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.testing.TestingSession.testSessionBuilder;
 
 public class TestMockConnector
@@ -32,10 +42,25 @@ public class TestMockConnector
     {
         DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(testSessionBuilder().build()).build();
 
+        queryRunner.installPlugin(new TpchPlugin());
+        queryRunner.createCatalog("tpch", "tpch");
         queryRunner.installPlugin(
                 new MockConnectorPlugin(
                         MockConnectorFactory.builder()
                                 .withListSchemaNames(connectionSession -> ImmutableList.of("default"))
+                                .withGetColumns(schemaTableName -> ImmutableList.of(new ColumnMetadata("nationkey", BIGINT)))
+                                .withGetTableHandle((session, tableName) -> new MockConnectorTableHandle(tableName))
+                                .withGetMaterializedViews((session, schemaTablePrefix) -> ImmutableMap.of(
+                                        new SchemaTableName("default", "test_materialized_view"),
+                                        new ConnectorMaterializedViewDefinition(
+                                                "SELECT nationkey FROM mock.default.test_table",
+                                                Optional.of("test_storage"),
+                                                Optional.of("mock"),
+                                                Optional.of("default"),
+                                                ImmutableList.of(new Column("nationkey", BIGINT.getTypeId())),
+                                                Optional.empty(),
+                                                Optional.empty(),
+                                                ImmutableMap.of())))
                                 .build()));
         queryRunner.createCatalog("mock", "mock");
         return queryRunner;
@@ -57,5 +82,23 @@ public class TestMockConnector
     public void testRenameSchema()
     {
         assertUpdate("ALTER SCHEMA mock.default RENAME to renamed");
+    }
+
+    @Test
+    public void testCreateMaterializedView()
+    {
+        assertUpdate("CREATE MATERIALIZED VIEW mock.default.materialized_view AS SELECT * FROM tpch.tiny.nation");
+    }
+
+    @Test
+    public void testRefreshMaterializedView()
+    {
+        assertUpdate("REFRESH MATERIALIZED VIEW mock.default.test_materialized_view", 0);
+    }
+
+    @Test
+    public void testDropMaterializedView()
+    {
+        assertUpdate("DROP MATERIALIZED VIEW mock.default.test_materialized_view");
     }
 }
