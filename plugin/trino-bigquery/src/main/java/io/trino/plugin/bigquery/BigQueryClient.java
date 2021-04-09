993a7117122f3ffd2c7956fd08f5e0a69ef86194
yuya.ebihara <yuya.ebihara@linecorp.com>
2021-04-10 00:17:51 +0900

    Add support for CREATE and DROP SCHEMA in BigQuery

--- plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryClient.java
+++ plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryClient.java
@@ -202,6 +202,16 @@ class BigQueryClient
         return bigQuery.update(table);
     }
 
+    public void createSchema(DatasetInfo datasetInfo)
+    {
+        bigQuery.create(datasetInfo);
+    }
+
+    public void dropSchema(DatasetId datasetId)
+    {
+        bigQuery.delete(datasetId);
+    }
+
     public void createTable(TableInfo tableInfo)
     {
         bigQuery.create(tableInfo);
