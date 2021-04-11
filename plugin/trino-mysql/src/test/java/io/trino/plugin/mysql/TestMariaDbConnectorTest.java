1772fb621ed9c2789d48d7fcc5d52f665554ecbe
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-04-11 15:52:02 +0530

    Add MariaDB compatibility connector tests

---
+++ plugin/trino-mysql/src/test/java/io/trino/plugin/mysql/TestMariaDbConnectorTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.mysql;
+
+import com.google.common.collect.ImmutableMap;
+import io.trino.testing.QueryRunner;
+import io.trino.testing.sql.SqlExecutor;
+
+import static io.trino.plugin.mysql.MariaDbQueryRunner.createMariaDbQueryRunner;
+
+public class TestMariaDbConnectorTest
+        extends BaseMySqlConnectorTest
+{
+    private TestingMariaDbServer mariaDbServer;
+
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        mariaDbServer = closeAfterClass(new TestingMariaDbServer());
+        return createMariaDbQueryRunner(mariaDbServer, ImmutableMap.of(), ImmutableMap.of(), REQUIRED_TPCH_TABLES);
+    }
+
+    @Override
+    protected SqlExecutor onRemoteDatabase()
+    {
+        return mariaDbServer::execute;
+    }
+}
