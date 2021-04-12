fe85f264317d2725c13bf7882a1e5c8d511bcd05
Karol Sobczak <napewnotrafi@gmail.com>
2021-04-12 15:58:28 +0200

    Initialize planner beans lazily

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -228,7 +228,6 @@ import io.trino.sql.planner.optimizations.UnaliasSymbolReferences;
 import io.trino.sql.planner.optimizations.WindowFilterPushDown;
 import org.weakref.jmx.MBeanExporter;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
@@ -244,6 +243,8 @@ public class PlanOptimizers
     private final RuleStatsRecorder ruleStats;
     private final OptimizerStatsRecorder optimizerStats = new OptimizerStatsRecorder();
     private final MBeanExporter exporter;
+
+    private boolean initialized;
 
     @Inject
     public PlanOptimizers(
@@ -277,13 +278,6 @@ public class PlanOptimizers
                 taskCountEstimator,
                 ruleStats,
                 nodePartitioningManager);
-    }
-
-    @PostConstruct
-    public void initialize()
-    {
-        ruleStats.export(exporter);
-        optimizerStats.export(exporter);
     }
 
     @PreDestroy
@@ -896,6 +890,18 @@ public class PlanOptimizers
     @Override
     public List<PlanOptimizer> get()
     {
+        initialize();
         return optimizers;
+    }
+
+    private synchronized void initialize()
+    {
+        if (initialized) {
+            return;
+        }
+
+        ruleStats.export(exporter);
+        optimizerStats.export(exporter);
+        initialized = true;
     }
 }
