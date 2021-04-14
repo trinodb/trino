5aa1073b497d9f66fa071ce48795490cf96c1790
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2021-04-14 10:58:19 +0200

    Cancel PostgreSQL remote query on thread interruption

--- plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/BaseJdbcConnectorTest.java
+++ plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/BaseJdbcConnectorTest.java
@@ -13,7 +13,9 @@
  */
 package io.trino.plugin.jdbc;
 
+import io.airlift.units.Duration;
 import io.trino.Session;
+import io.trino.spi.QueryId;
 import io.trino.spi.connector.JoinCondition;
 import io.trino.spi.connector.SortOrder;
 import io.trino.sql.planner.assertions.PlanMatchPattern;
@@ -24,22 +26,32 @@ import io.trino.sql.planner.plan.TableScanNode;
 import io.trino.sql.planner.plan.TopNNode;
 import io.trino.sql.query.QueryAssertions.QueryAssert;
 import io.trino.testing.BaseConnectorTest;
+import io.trino.testing.MaterializedResult;
 import io.trino.testing.TestingConnectorBehavior;
 import io.trino.testing.sql.TestTable;
+import io.trino.testing.sql.TestView;
 import org.intellij.lang.annotations.Language;
 import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.MoreCollectors.toOptional;
+import static io.airlift.concurrent.Threads.daemonThreadsNamed;
+import static io.trino.plugin.jdbc.RemoteDatabaseEvent.Status.CANCELLED;
+import static io.trino.plugin.jdbc.RemoteDatabaseEvent.Status.RUNNING;
 import static io.trino.sql.planner.assertions.PlanMatchPattern.anyTree;
 import static io.trino.sql.planner.assertions.PlanMatchPattern.exchange;
 import static io.trino.sql.planner.assertions.PlanMatchPattern.node;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_AGGREGATION_PUSHDOWN;
+import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_CANCELLATION;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_JOIN_PUSHDOWN;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_JOIN_PUSHDOWN_WITH_DISTINCT_FROM;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_JOIN_PUSHDOWN_WITH_FULL_JOIN;
@@ -50,12 +62,24 @@ import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_PREDICATE_PUSHD
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_PREDICATE_PUSHDOWN_WITH_VARCHAR_INEQUALITY;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_TOPN_PUSHDOWN;
 import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_TOPN_PUSHDOWN_WITH_VARCHAR;
+import static io.trino.testing.assertions.Assert.assertEventually;
 import static java.lang.String.format;
+import static java.util.Locale.ENGLISH;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public abstract class BaseJdbcConnectorTest
         extends BaseConnectorTest
 {
+    private final ExecutorService executor = newCachedThreadPool(daemonThreadsNamed(getClass().getName()));
+
+    @AfterClass(alwaysRun = true)
+    public void afterClass()
+    {
+        executor.shutdownNow();
+    }
+
     @Override
     protected boolean hasBehavior(TestingConnectorBehavior connectorBehavior)
     {
@@ -566,5 +590,62 @@ public abstract class BaseJdbcConnectorTest
         return Session.builder(session)
                 .setCatalogSessionProperty(session.getCatalog().orElseThrow(), "join_pushdown_enabled", "true")
                 .build();
+    }
+
+    @Test(timeOut = 60_000)
+    public void testCancellation()
+            throws Exception
+    {
+        if (!hasBehavior(SUPPORTS_CANCELLATION)) {
+            throw new SkipException("Cancellation is not supported by given connector");
+        }
+
+        try (TestView sleepingView = createSleepingView(new Duration(1, MINUTES))) {
+            String query = "SELECT * FROM " + sleepingView.getName();
+            Future<?> future = executor.submit(() -> assertQueryFails(query, "Query killed. Message: Killed by test"));
+            QueryId queryId = getQueryId(query);
+
+            assertEventually(() -> assertRemoteQueryStatus(sleepingView.getName(), RUNNING));
+            assertUpdate(format("CALL system.runtime.kill_query(query_id => '%s', message => '%s')", queryId, "Killed by test"));
+            future.get();
+            assertEventually(() -> assertRemoteQueryStatus(sleepingView.getName(), CANCELLED));
+        }
+    }
+
+    private void assertRemoteQueryStatus(String tableNameToScan, RemoteDatabaseEvent.Status status)
+    {
+        String lowerCasedTableName = tableNameToScan.toLowerCase(ENGLISH);
+        assertThat(getRemoteDatabaseEvents())
+                .filteredOn(event -> event.getQuery().toLowerCase(ENGLISH).contains(lowerCasedTableName))
+                .map(RemoteDatabaseEvent::getStatus)
+                .contains(status);
+    }
+
+    private QueryId getQueryId(String query)
+            throws Exception
+    {
+        for (int i = 0; i < 100; i++) {
+            MaterializedResult queriesResult = getQueryRunner().execute(format(
+                    "SELECT query_id FROM system.runtime.queries WHERE query = '%s' AND query NOT LIKE '%%system.runtime.queries%%'",
+                    query));
+            int rowCount = queriesResult.getRowCount();
+            if (rowCount == 0) {
+                Thread.sleep(100);
+                continue;
+            }
+            checkState(rowCount == 1, "Too many (%s) query ids were found for: %s", rowCount, query);
+            return new QueryId((String) queriesResult.getOnlyValue());
+        }
+        throw new IllegalStateException("Query id not found for: " + query);
+    }
+
+    protected List<RemoteDatabaseEvent> getRemoteDatabaseEvents()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    protected TestView createSleepingView(Duration minimalSleepDuration)
+    {
+        throw new UnsupportedOperationException();
     }
 }
