0b6377ee8fbcb1d63efbc1fb0a6ee1b386e93ea7
Krzysztof Skrzypczynski <krzysztof.skrzypczynski@starburstdata.com>
2021-04-15 12:56:04 +0200

    Extract StarburstSynapseAuthenticationModule
    (cherry-picked from commit: d255ccd48bd9c0de3561af5f4c818092c069dd7e)

---
+++ plugins/merged-starburst-synapse/src/main/java/com/starburstdata/presto/plugin/synapse/StarburstSynapseAuthenticationModule.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.synapse;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+import com.starburstdata.presto.plugin.jdbc.auth.ForAuthentication;
+import com.starburstdata.presto.plugin.jdbc.auth.NoImpersonationModule;
+import com.starburstdata.presto.plugin.jdbc.authtolocal.AuthToLocalModule;
+import com.starburstdata.presto.plugin.sqlserver.SqlServerImpersonatingConnectionFactory;
+import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.trino.plugin.jdbc.BaseJdbcConfig;
+import io.trino.plugin.jdbc.ConnectionFactory;
+import io.trino.plugin.jdbc.DriverConnectionFactory;
+import io.trino.plugin.jdbc.ForBaseJdbc;
+import io.trino.plugin.jdbc.credential.CredentialProvider;
+import io.trino.plugin.jdbc.credential.CredentialProviderModule;
+
+import java.util.Properties;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.starburstdata.presto.plugin.synapse.SynapseConfig.SynapseAuthenticationType.ACTIVE_DIRECTORY_PASSWORD;
+import static com.starburstdata.presto.plugin.synapse.SynapseConfig.SynapseAuthenticationType.PASSWORD;
+import static io.airlift.configuration.ConditionalModule.installModuleIf;
+
+public class StarburstSynapseAuthenticationModule
+        extends AbstractConfigurationAwareModule
+{
+    @Override
+    protected void setup(Binder binder)
+    {
+        install(installModuleIf(
+                SynapseConfig.class,
+                SynapseConfig::isImpersonationEnabled,
+                new ImpersonationModule(),
+                new NoImpersonationModule()));
+
+        install(installModuleIf(
+                SynapseConfig.class,
+                config -> config.getAuthenticationType() == ACTIVE_DIRECTORY_PASSWORD,
+                new ActiveDirectoryPasswordModule()));
+
+        install(installModuleIf(
+                SynapseConfig.class,
+                config -> config.getAuthenticationType() == PASSWORD,
+                new PasswordModule()));
+    }
+
+    private static class ImpersonationModule
+            implements Module
+    {
+        @Override
+        public void configure(Binder binder)
+        {
+            binder.install(new AuthToLocalModule());
+            binder.bind(ConnectionFactory.class).annotatedWith(ForBaseJdbc.class).to(SqlServerImpersonatingConnectionFactory.class).in(Scopes.SINGLETON);
+        }
+    }
+
+    private class ActiveDirectoryPasswordModule
+            implements Module
+    {
+        @Override
+        public void configure(Binder binder)
+        {
+            install(new CredentialProviderModule());
+        }
+
+        @Provides
+        @Singleton
+        @ForAuthentication
+        public ConnectionFactory getConnectionFactory(
+                BaseJdbcConfig baseJdbcConfig,
+                CredentialProvider credentialProvider)
+        {
+            checkState(
+                    !baseJdbcConfig.getConnectionUrl().contains("authentication="),
+                    "Cannot specify 'authentication' parameter in JDBC URL when using Active Directory password authentication: %s",
+                    baseJdbcConfig.getConnectionUrl());
+            Properties properties = new Properties();
+            properties.setProperty("authentication", "ActiveDirectoryPassword");
+
+            return new DriverConnectionFactory(
+                    new SQLServerDriver(),
+                    baseJdbcConfig.getConnectionUrl(),
+                    properties,
+                    credentialProvider);
+        }
+    }
+
+    private class PasswordModule
+            implements Module
+    {
+        @Override
+        public void configure(Binder binder)
+        {
+            install(new CredentialProviderModule());
+        }
+
+        @Provides
+        @Singleton
+        @ForAuthentication
+        public ConnectionFactory getConnectionFactory(BaseJdbcConfig baseJdbcConfig, CredentialProvider credentialProvider)
+        {
+            return new DriverConnectionFactory(new SQLServerDriver(), baseJdbcConfig, credentialProvider);
+        }
+    }
+}
--- plugins/merged-starburst-synapse/src/main/java/com/starburstdata/presto/plugin/synapse/StarburstSynapseClientModule.java
+++ plugins/merged-starburst-synapse/src/main/java/com/starburstdata/presto/plugin/synapse/StarburstSynapseClientModule.java
@@ -11,41 +11,22 @@ package com.starburstdata.presto.plugin.synapse;
 
 import com.google.inject.Binder;
 import com.google.inject.Key;
-import com.google.inject.Module;
-import com.google.inject.Provides;
 import com.google.inject.Scopes;
-import com.google.inject.Singleton;
-import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import com.starburstdata.presto.plugin.jdbc.JdbcJoinPushdownSupportModule;
-import com.starburstdata.presto.plugin.jdbc.auth.ForAuthentication;
-import com.starburstdata.presto.plugin.jdbc.auth.NoImpersonationModule;
-import com.starburstdata.presto.plugin.jdbc.authtolocal.AuthToLocalModule;
 import com.starburstdata.presto.plugin.jdbc.dynamicfiltering.ForDynamicFiltering;
 import com.starburstdata.presto.plugin.jdbc.redirection.JdbcTableScanRedirectionModule;
 import com.starburstdata.presto.plugin.jdbc.stats.JdbcStatisticsConfig;
-import com.starburstdata.presto.plugin.sqlserver.SqlServerImpersonatingConnectionFactory;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.trino.plugin.jdbc.BaseJdbcConfig;
-import io.trino.plugin.jdbc.ConnectionFactory;
-import io.trino.plugin.jdbc.DriverConnectionFactory;
 import io.trino.plugin.jdbc.ForBaseJdbc;
 import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcRecordSetProvider;
 import io.trino.plugin.jdbc.JdbcSplitManager;
 import io.trino.plugin.jdbc.MaxDomainCompactionThreshold;
-import io.trino.plugin.jdbc.credential.CredentialProvider;
-import io.trino.plugin.jdbc.credential.CredentialProviderModule;
 import io.trino.plugin.sqlserver.SqlServerConfig;
 import io.trino.spi.connector.ConnectorRecordSetProvider;
 import io.trino.spi.connector.ConnectorSplitManager;
 
-import java.util.Properties;
-
-import static com.google.common.base.Preconditions.checkState;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
-import static com.starburstdata.presto.plugin.synapse.SynapseConfig.SynapseAuthenticationType.ACTIVE_DIRECTORY_PASSWORD;
-import static com.starburstdata.presto.plugin.synapse.SynapseConfig.SynapseAuthenticationType.PASSWORD;
-import static io.airlift.configuration.ConditionalModule.installModuleIf;
 import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.trino.plugin.sqlserver.SqlServerClient.SQL_SERVER_MAX_LIST_EXPRESSIONS;
 
@@ -71,83 +52,8 @@ public class StarburstSynapseClientModule
         binder.bind(ConnectorSplitManager.class).annotatedWith(ForDynamicFiltering.class).to(JdbcSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorRecordSetProvider.class).annotatedWith(ForDynamicFiltering.class).to(JdbcRecordSetProvider.class).in(Scopes.SINGLETON);
 
-        install(installModuleIf(
-                SynapseConfig.class,
-                SynapseConfig::isImpersonationEnabled,
-                new ImpersonationModule(),
-                new NoImpersonationModule()));
-
-        install(installModuleIf(
-                SynapseConfig.class,
-                config -> config.getAuthenticationType() == ACTIVE_DIRECTORY_PASSWORD,
-                new ActiveDirectoryPasswordModule()));
-
-        install(installModuleIf(
-                SynapseConfig.class,
-                config -> config.getAuthenticationType() == PASSWORD,
-                new PasswordModule()));
-
+        install(new StarburstSynapseAuthenticationModule());
         install(new JdbcJoinPushdownSupportModule());
         install(new JdbcTableScanRedirectionModule());
-    }
-
-    private static class ImpersonationModule
-            implements Module
-    {
-        @Override
-        public void configure(Binder binder)
-        {
-            binder.install(new AuthToLocalModule());
-            binder.bind(ConnectionFactory.class).annotatedWith(ForBaseJdbc.class).to(SqlServerImpersonatingConnectionFactory.class).in(Scopes.SINGLETON);
-        }
-    }
-
-    private class ActiveDirectoryPasswordModule
-            implements Module
-    {
-        @Override
-        public void configure(Binder binder)
-        {
-            install(new CredentialProviderModule());
-        }
-
-        @Provides
-        @Singleton
-        @ForAuthentication
-        public ConnectionFactory getConnectionFactory(
-                BaseJdbcConfig baseJdbcConfig,
-                CredentialProvider credentialProvider)
-        {
-            checkState(
-                    !baseJdbcConfig.getConnectionUrl().contains("authentication="),
-                    "Cannot specify 'authentication' parameter in JDBC URL when using Active Directory password authentication: %s",
-                    baseJdbcConfig.getConnectionUrl());
-            Properties properties = new Properties();
-            properties.setProperty("authentication", "ActiveDirectoryPassword");
-
-            return new DriverConnectionFactory(
-                    new SQLServerDriver(),
-                    baseJdbcConfig.getConnectionUrl(),
-                    properties,
-                    credentialProvider);
-        }
-    }
-
-    private class PasswordModule
-            implements Module
-    {
-        @Override
-        public void configure(Binder binder)
-        {
-            install(new CredentialProviderModule());
-        }
-
-        @Provides
-        @Singleton
-        @ForAuthentication
-        public ConnectionFactory getConnectionFactory(BaseJdbcConfig baseJdbcConfig, CredentialProvider credentialProvider)
-        {
-            return new DriverConnectionFactory(new SQLServerDriver(), baseJdbcConfig, credentialProvider);
-        }
     }
 }
