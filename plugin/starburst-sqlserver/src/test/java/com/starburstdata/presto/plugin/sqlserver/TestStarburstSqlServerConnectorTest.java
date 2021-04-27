a4abb407ed7058633e91ad14c72d3dfe127d34d0
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-04-27 21:18:03 +0100

    Disable join pushdown in TestStarburstSqlServerConnectorTest.joinPushdownDisabled

--- plugin/starburst-sqlserver/src/test/java/com/starburstdata/presto/plugin/sqlserver/TestStarburstSqlServerConnectorTest.java
+++ plugin/starburst-sqlserver/src/test/java/com/starburstdata/presto/plugin/sqlserver/TestStarburstSqlServerConnectorTest.java
@@ -10,16 +10,20 @@
 package com.starburstdata.presto.plugin.sqlserver;
 
 import com.google.common.collect.ImmutableMap;
+import com.starburstdata.presto.testing.SessionMutator;
 import io.trino.Session;
 import io.trino.plugin.sqlserver.TestSqlServerConnectorTest;
 import io.trino.plugin.sqlserver.TestingSqlServer;
 import io.trino.testing.QueryRunner;
 
 import static com.starburstdata.presto.plugin.sqlserver.StarburstSqlServerQueryRunner.createStarburstSqlServerQueryRunner;
+import static io.trino.plugin.jdbc.JdbcMetadataSessionProperties.JOIN_PUSHDOWN_ENABLED;
 
 public class TestStarburstSqlServerConnectorTest
         extends TestSqlServerConnectorTest
 {
+    private final SessionMutator sessionMutator = new SessionMutator(super::getSession);
+
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
@@ -30,11 +34,29 @@ public class TestStarburstSqlServerConnectorTest
     }
 
     @Override
+    protected Session getSession()
+    {
+        return sessionMutator.getSession();
+    }
+
+    @Override
     protected Session joinPushdownEnabled(Session session)
     {
         return Session.builder(super.joinPushdownEnabled(session))
                 // strategy is AUTOMATIC by default and would not work for certain test cases (even if statistics are collected)
                 .setCatalogSessionProperty(session.getCatalog().orElseThrow(), "join_pushdown_strategy", "EAGER")
                 .build();
+    }
+
+    @Override
+    public void testJoinPushdownDisabled()
+    {
+        // disabling join pushdown as SQL Server collects stats by default and AUTOMATIC join pushdown triggers
+        // failing assertions defined in OSS, where join pushdown is disabled.
+        sessionMutator.withModifiedSession(
+                session -> Session.builder(session)
+                        .setCatalogSessionProperty(session.getCatalog().get(), JOIN_PUSHDOWN_ENABLED, "false")
+                        .build())
+                .call(super::testJoinPushdownDisabled);
     }
 }
