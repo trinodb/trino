trinodb/trino

commit f3dda9c96289c38ac99024ac64393debdf2dab9d

Initialize planner beans lazily

core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -228,7 +228,6 @@ import io.trino.sql.planner.optimizations.UnaliasSymbolReferences;
 import io.trino.sql.planner.optimizations.WindowFilterPushDown;
 import org.weakref.jmx.MBeanExporter;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
@@ -244,6 +243,8 @@ public class PlanOptimizers
     private final RuleStatsRecorder ruleStats;
     private final OptimizerStatsRecorder optimizerStats = new OptimizerStatsRecorder();
     private final MBeanExporter exporter;
+
+    private boolean initialized;
 
     @Inject
     public PlanOptimizers(
@@ -277,13 +278,6 @@ public class PlanOptimizers
                 taskCountEstimator,
                 ruleStats,
                 nodePartitioningManager);
-    }
-
-    @PostConstruct
-    public void initialize()
-    {
-        ruleStats.export(exporter);
-        optimizerStats.export(exporter);
     }
 
     @PreDestroy
@@ -896,6 +890,18 @@ public class PlanOptimizers
     @Override
     public List<PlanOptimizer> get()
     {
+        initialize();
         return optimizers;
+    }
+
+    private synchronized void initialize()
+    {
+        if (initialized) {
+            return;
+        }
+
+        ruleStats.export(exporter);
+        optimizerStats.export(exporter);
+        initialized = true;
     }
 }

commit 9a2f410fd4d5e7b20f8147d8ad763027865d989a

Initialize mbeans in @PostConstruct method
The change in f3dda9c96289c38ac99024ac64393debdf2dab9d caused mbeans
to be exported unconditionally the first time PlanOptimizers.get() is
called. LocalQueryRunner.getPlanOptimizers creates a new instance
every time, so this causes the mbeans to be exported on every
invocation and results in tests spending the majority of their time
in mbean export.

There is some cleanup we can do to make PlanOptimizers a "stateless"
object, but it will require a bigger refactoring. This places
initialization back in the @PostConstruct-annotated method as
a temporary workaround.

core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -229,7 +229,9 @@ import io.trino.sql.planner.optimizations.UnaliasSymbolReferences;
 import io.trino.sql.planner.optimizations.WindowFilterPushDown;
 import org.weakref.jmx.MBeanExporter;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.concurrent.GuardedBy;
 import javax.inject.Inject;
 
 import java.util.List;
@@ -245,6 +247,7 @@ public class PlanOptimizers
     private final OptimizerStatsRecorder optimizerStats = new OptimizerStatsRecorder();
     private final MBeanExporter exporter;
 
+    @GuardedBy("this")
     private boolean initialized;
 
     @Inject
@@ -281,11 +284,24 @@ public class PlanOptimizers
                 nodePartitioningManager);
     }
 
-    @PreDestroy
-    public void destroy()
+    @PostConstruct
+    public synchronized void initialize()
     {
-        ruleStats.unexport(exporter);
-        optimizerStats.unexport(exporter);
+        if (!initialized) {
+            ruleStats.export(exporter);
+            optimizerStats.export(exporter);
+            initialized = true;
+        }
+    }
+
+    @PreDestroy
+    public synchronized void destroy()
+    {
+        if (initialized) {
+            ruleStats.unexport(exporter);
+            optimizerStats.unexport(exporter);
+            initialized = false;
+        }
     }
 
     public PlanOptimizers(
@@ -894,18 +910,6 @@ public class PlanOptimizers
     @Override
     public List<PlanOptimizer> get()
     {
-        initialize();
         return optimizers;
-    }
-
-    private synchronized void initialize()
-    {
-        if (initialized) {
-            return;
-        }
-
-        ruleStats.export(exporter);
-        optimizerStats.export(exporter);
-        initialized = true;
     }
 }