kasiafi/presto

Add rule to inline projection assignments into ValuesNode

core/trino-main/src/test/java/io/trino/sql/planner/optimizations/TestAddExchangesPlans.java
@@ -29,7 +29,8 @@ import io.trino.sql.planner.plan.ExchangeNode;
 import io.trino.sql.planner.plan.FilterNode;
 import io.trino.sql.planner.plan.JoinNode.DistributionType;
 import io.trino.sql.planner.plan.MarkDistinctNode;
-import io.trino.sql.planner.plan.ValuesNode;
+import io.trino.sql.tree.GenericLiteral;
+import io.trino.sql.tree.LongLiteral;
 import io.trino.testing.LocalQueryRunner;
 import org.testng.annotations.Test;
 
@@ -119,7 +120,7 @@ public class TestAddExchangesPlans
                                         anyTree(
                                                 exchange(REMOTE, REPARTITION,
                                                         anyTree(
-                                                                values())))))));
+                                                                values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new GenericLiteral("BIGINT", "1")))))))))));
     }
 
     @Test
@@ -155,8 +156,8 @@ public class TestAddExchangesPlans
                                                 anyTree(
                                                         tableScan("nation", ImmutableMap.of("nationkey", "nationkey")))),
                                         exchange(REMOTE, REPARTITION,
-                                                anyTree(
-                                                        values()))),
+                                                project(
+                                                        values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new GenericLiteral("BIGINT", "1"))))))),
                                 anyTree(
                                         exchange(REMOTE, REPARTITION,
                                                 anyTree(
@@ -207,13 +208,15 @@ public class TestAddExchangesPlans
                         node(MarkDistinctNode.class,
                                 anyTree(
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition1", "partition2"),
-                                                anyTree(
-                                                        values(ImmutableList.of("partition1", "partition2")))),
+                                                project(
+                                                        values(
+                                                                ImmutableList.of("partition1", "partition2", "field"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("2"), new LongLiteral("1")))))),
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition3", "partition3"),
                                                 project(
-                                                        project(ImmutableMap.of("partition3", expression("3"), "partition4", expression("4")),
-                                                                anyTree(
-                                                                        node(ValuesNode.class)))))))));
+                                                        values(
+                                                                ImmutableList.of("field_0", "partition3", "partition4"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("3"), new LongLiteral("4"))))))))));
 
         assertDistributedPlan(
                 query,
@@ -224,13 +227,15 @@ public class TestAddExchangesPlans
                         node(MarkDistinctNode.class,
                                 anyTree(
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition1"),
-                                                anyTree(
-                                                        values(ImmutableList.of("partition1", "partition2")))),
+                                                project(
+                                                        values(
+                                                                ImmutableList.of("partition1", "partition2", "field"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("2"), new LongLiteral("1")))))),
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition3"),
                                                 project(
-                                                        project(ImmutableMap.of("partition3", expression("3"), "partition4", expression("4")),
-                                                                anyTree(
-                                                                        node(ValuesNode.class)))))))));
+                                                        values(
+                                                                ImmutableList.of("field_0", "partition3", "partition4"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("3"), new LongLiteral("4"))))))))));
     }
 
     @Test
@@ -426,8 +431,7 @@ public class TestAddExchangesPlans
         assertPlan(
                 "SELECT 10, a FROM (VALUES 1) t(a)",
                 anyTree(
-                        project(
-                                values("a"))));
+                        values(ImmutableList.of("a", "expr"), ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("10"))))));
 
         assertPlan(
                 "SELECT 1 UNION ALL SELECT 1",
@@ -435,10 +439,8 @@ public class TestAddExchangesPlans
                         exchange(
                                 LOCAL,
                                 REPARTITION,
-                                project(
-                                        values()),
-                                project(
-                                        values()))));
+                                values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new LongLiteral("1")))),
+                                values(ImmutableList.of("expr_0"), ImmutableList.of(ImmutableList.of(new LongLiteral("1")))))));
     }
 
     private Session spillEnabledWithJoinDistributionType(JoinDistributionType joinDistributionType)

trinodb/trino

core/trino-main/src/test/java/io/trino/sql/planner/optimizations/TestAddExchangesPlans.java
@@ -29,7 +29,8 @@ import io.trino.sql.planner.plan.ExchangeNode;
 import io.trino.sql.planner.plan.FilterNode;
 import io.trino.sql.planner.plan.JoinNode.DistributionType;
 import io.trino.sql.planner.plan.MarkDistinctNode;
-import io.trino.sql.planner.plan.ValuesNode;
+import io.trino.sql.tree.GenericLiteral;
+import io.trino.sql.tree.LongLiteral;
 import io.trino.testing.LocalQueryRunner;
 import org.testng.annotations.Test;
 
@@ -119,7 +120,7 @@ public class TestAddExchangesPlans
                                         anyTree(
                                                 exchange(REMOTE, REPARTITION,
                                                         anyTree(
-                                                                values())))))));
+                                                                values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new GenericLiteral("BIGINT", "1")))))))))));
     }
 
     @Test
@@ -155,8 +156,8 @@ public class TestAddExchangesPlans
                                                 anyTree(
                                                         tableScan("nation", ImmutableMap.of("nationkey", "nationkey")))),
                                         exchange(REMOTE, REPARTITION,
-                                                anyTree(
-                                                        values()))),
+                                                project(
+                                                        values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new GenericLiteral("BIGINT", "1"))))))),
                                 anyTree(
                                         exchange(REMOTE, REPARTITION,
                                                 anyTree(
@@ -207,13 +208,15 @@ public class TestAddExchangesPlans
                         node(MarkDistinctNode.class,
                                 anyTree(
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition1", "partition2"),
-                                                anyTree(
-                                                        values(ImmutableList.of("partition1", "partition2")))),
+                                                project(
+                                                        values(
+                                                                ImmutableList.of("field", "partition2", "partition1"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("2"), new LongLiteral("1")))))),
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition3", "partition3"),
                                                 project(
-                                                        project(ImmutableMap.of("partition3", expression("3"), "partition4", expression("4")),
-                                                                anyTree(
-                                                                        node(ValuesNode.class)))))))));
+                                                        values(
+                                                                ImmutableList.of("partition3", "partition4", "field_0"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("3"), new LongLiteral("4"), new LongLiteral("1"))))))))));
 
         assertDistributedPlan(
                 query,
@@ -224,13 +227,15 @@ public class TestAddExchangesPlans
                         node(MarkDistinctNode.class,
                                 anyTree(
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition1"),
-                                                anyTree(
-                                                        values(ImmutableList.of("partition1", "partition2")))),
+                                                project(
+                                                        values(
+                                                                ImmutableList.of("field", "partition2", "partition1"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("2"), new LongLiteral("1")))))),
                                         exchange(REMOTE, REPARTITION, ImmutableList.of(), ImmutableSet.of("partition3"),
                                                 project(
-                                                        project(ImmutableMap.of("partition3", expression("3"), "partition4", expression("4")),
-                                                                anyTree(
-                                                                        node(ValuesNode.class)))))))));
+                                                        values(
+                                                                ImmutableList.of("partition3", "partition4", "field_0"),
+                                                                ImmutableList.of(ImmutableList.of(new LongLiteral("3"), new LongLiteral("4"), new LongLiteral("1"))))))))));
     }
 
     @Test
@@ -426,8 +431,7 @@ public class TestAddExchangesPlans
         assertPlan(
                 "SELECT 10, a FROM (VALUES 1) t(a)",
                 anyTree(
-                        project(
-                                values("a"))));
+                        values(ImmutableList.of("a", "expr"), ImmutableList.of(ImmutableList.of(new LongLiteral("1"), new LongLiteral("10"))))));
 
         assertPlan(
                 "SELECT 1 UNION ALL SELECT 1",
@@ -435,10 +439,8 @@ public class TestAddExchangesPlans
                         exchange(
                                 LOCAL,
                                 REPARTITION,
-                                project(
-                                        values()),
-                                project(
-                                        values()))));
+                                values(ImmutableList.of("expr"), ImmutableList.of(ImmutableList.of(new LongLiteral("1")))),
+                                values(ImmutableList.of("expr_0"), ImmutableList.of(ImmutableList.of(new LongLiteral("1")))))));
     }
 
     private Session spillEnabledWithJoinDistributionType(JoinDistributionType joinDistributionType)