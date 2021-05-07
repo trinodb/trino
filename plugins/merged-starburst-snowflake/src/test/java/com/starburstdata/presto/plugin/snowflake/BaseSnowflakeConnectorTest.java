d0ae713e0e7d04d7d0dca7c019d62b6092759887
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-05-07 23:30:57 +0530

    Use limit pushdown tests from BaseJdbcConnectorTest
    (cherry-picked from commit: 030cc4b07e0dbfd5c64c3bf55d9ce6d589cbaa5b)

--- plugins/merged-starburst-snowflake/src/test/java/com/starburstdata/presto/plugin/snowflake/BaseSnowflakeConnectorTest.java
+++ plugins/merged-starburst-snowflake/src/test/java/com/starburstdata/presto/plugin/snowflake/BaseSnowflakeConnectorTest.java
@@ -271,28 +271,6 @@ public abstract class BaseSnowflakeConnectorTest
     }
 
     @Test
-    @Override
-    public void testLimitPushdown()
-    {
-        assertThat(query("SELECT name FROM nation LIMIT 30")).isFullyPushedDown(); // Use high limit for result determinism
-
-        // with filter over numeric column
-        assertThat(query("SELECT name FROM nation WHERE regionkey = 3 LIMIT 5")).isFullyPushedDown();
-
-        // with filter over varchar column
-        assertThat(query("SELECT name FROM nation WHERE name < 'EEE' LIMIT 5")).isFullyPushedDown();
-
-        // with aggregation
-        assertThat(query("SELECT max(regionkey) FROM nation LIMIT 5")).isFullyPushedDown(); // global aggregation, LIMIT removed
-        assertThat(query("SELECT regionkey, max(name) FROM nation GROUP BY regionkey LIMIT 5")).isFullyPushedDown();
-        assertThat(query("SELECT DISTINCT regionkey FROM nation LIMIT 5")).isFullyPushedDown();
-
-        // with filter and aggregation
-        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE nationkey < 5 GROUP BY regionkey LIMIT 3")).isFullyPushedDown();
-        assertThat(query("SELECT regionkey, count(*) FROM nation WHERE name < 'EGYPT' GROUP BY regionkey LIMIT 3")).isFullyPushedDown();
-    }
-
-    @Test
     public void testTooLargeDomainCompactionThreshold()
     {
         assertQueryFails(
