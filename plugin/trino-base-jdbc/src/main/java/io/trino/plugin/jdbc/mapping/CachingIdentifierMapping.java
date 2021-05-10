ef7003bdceee2412f40113e2075f0b1b568160a1
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2021-05-10 10:43:44 +0200

    Do not fail all queries if there are duplicates
    Only fail the query when an ambiguous object is accessed.

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/mapping/CachingIdentifierMapping.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/mapping/CachingIdentifierMapping.java
@@ -16,24 +16,33 @@ package io.trino.plugin.jdbc.mapping;
 import com.google.common.base.CharMatcher;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import io.trino.plugin.jdbc.BaseJdbcClient;
 import io.trino.plugin.jdbc.JdbcIdentity;
 import io.trino.plugin.jdbc.mapping.IdentifierMappingModule.ForCachingIdentifierMapping;
 import io.trino.spi.TrinoException;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Verify.verify;
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static io.trino.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -41,8 +50,8 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public final class CachingIdentifierMapping
         implements IdentifierMapping
 {
-    private final Cache<JdbcIdentity, Map<String, String>> remoteSchemaNames;
-    private final Cache<RemoteTableNameCacheKey, Map<String, String>> remoteTableNames;
+    private final Cache<JdbcIdentity, Mapping> remoteSchemaNames;
+    private final Cache<RemoteTableNameCacheKey, Mapping> remoteTableNames;
     private final IdentifierMapping identifierMapping;
     private final Provider<BaseJdbcClient> baseJdbcClient;
 
@@ -87,8 +96,8 @@ public final class CachingIdentifierMapping
         verify(CharMatcher.forPredicate(Character::isUpperCase).matchesNoneOf(schemaName), "Expected schema name from internal metadata to be lowercase: %s", schemaName);
 
         try {
-            Map<String, String> mapping = remoteSchemaNames.getIfPresent(identity);
-            if (mapping != null && !mapping.containsKey(schemaName)) {
+            Mapping mapping = remoteSchemaNames.getIfPresent(identity);
+            if (mapping != null && !mapping.hasRemoteObject(schemaName)) {
                 // This might be a schema that has just been created. Force reload.
                 mapping = null;
             }
@@ -117,8 +126,8 @@ public final class CachingIdentifierMapping
 
         try {
             RemoteTableNameCacheKey cacheKey = new RemoteTableNameCacheKey(identity, remoteSchema);
-            Map<String, String> mapping = remoteTableNames.getIfPresent(cacheKey);
-            if (mapping != null && !mapping.containsKey(tableName)) {
+            Mapping mapping = remoteTableNames.getIfPresent(cacheKey);
+            if (mapping != null && !mapping.hasRemoteObject(tableName)) {
                 // This might be a table that has just been created. Force reload.
                 mapping = null;
             }
@@ -144,26 +153,70 @@ public final class CachingIdentifierMapping
         return identifierMapping.toRemoteColumnName(connection, columnName);
     }
 
-    private Map<String, String> createSchemaMapping(Connection connection)
+    private Mapping createSchemaMapping(Connection connection)
     {
-        return baseJdbcClient.get().listSchemas(connection).stream()
-                // will throw on collisions to avoid ambiguity
-                .collect(toImmutableMap(identifierMapping::fromRemoteSchemaName, schemaName -> schemaName));
+        return createMapping(baseJdbcClient.get().listSchemas(connection), identifierMapping::fromRemoteSchemaName);
     }
 
-    private Map<String, String> createTableMapping(Connection connection, String remoteSchema)
+    private Mapping createTableMapping(Connection connection, String remoteSchema)
+    {
+        return createMapping(
+                getTables(connection, remoteSchema),
+                remoteTableName -> identifierMapping.fromRemoteTableName(remoteSchema, remoteTableName));
+    }
+
+    private static Mapping createMapping(Collection<String> remoteNames, Function<String, String> mapping)
+    {
+        Map<String, String> map = new HashMap<>();
+        Set<String> duplicates = new HashSet<>();
+        for (String remoteName : remoteNames) {
+            String name = mapping.apply(remoteName);
+            if (duplicates.contains(name)) {
+                continue;
+            }
+            if (map.put(name, remoteName) != null) {
+                duplicates.add(name);
+                map.remove(name);
+            }
+        }
+        return new Mapping(map, duplicates);
+    }
+
+    private List<String> getTables(Connection connection, String remoteSchema)
     {
         try (ResultSet resultSet = baseJdbcClient.get().getTables(connection, Optional.of(remoteSchema), Optional.empty())) {
-            ImmutableMap.Builder<String, String> map = ImmutableMap.builder();
+            ImmutableList.Builder<String> tableNames = ImmutableList.builder();
             while (resultSet.next()) {
-                String remoteTableName = resultSet.getString("TABLE_NAME");
-                map.put(identifierMapping.fromRemoteTableName(remoteSchema, remoteTableName), remoteTableName);
+                tableNames.add(resultSet.getString("TABLE_NAME"));
             }
-            // will throw on collisions to avoid ambiguity
-            return map.build();
+            return tableNames.build();
         }
         catch (SQLException e) {
             throw new TrinoException(JDBC_ERROR, e);
+        }
+    }
+
+    private static final class Mapping
+    {
+        private final Map<String, String> mapping;
+        private final Set<String> duplicates;
+
+        public Mapping(Map<String, String> mapping, Set<String> duplicates)
+        {
+            this.mapping = ImmutableMap.copyOf(requireNonNull(mapping, "mapping is null"));
+            this.duplicates = ImmutableSet.copyOf(requireNonNull(duplicates, "duplicates is null"));
+        }
+
+        public boolean hasRemoteObject(String remoteName)
+        {
+            return mapping.containsKey(remoteName) || duplicates.contains(remoteName);
+        }
+
+        @Nullable
+        public String get(String remoteName)
+        {
+            checkArgument(!duplicates.contains(remoteName), "Ambiguous name: %s", remoteName);
+            return mapping.get(remoteName);
         }
     }
 }
