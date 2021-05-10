82869ceb9d314e6cb23d91513c371f11b2876612
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-05-10 16:18:23 +0200

    Workaround broken auto-commit reporting in ClickHouse JDBC

--- plugin/trino-clickhouse/src/main/java/io/trino/plugin/clickhouse/ClickHouseClientModule.java
+++ plugin/trino-clickhouse/src/main/java/io/trino/plugin/clickhouse/ClickHouseClientModule.java
@@ -47,6 +47,6 @@ public class ClickHouseClientModule
     @ForBaseJdbc
     public static ConnectionFactory createConnectionFactory(BaseJdbcConfig config, CredentialProvider credentialProvider)
     {
-        return new DriverConnectionFactory(new ClickHouseDriver(), config, credentialProvider);
+        return new ClickHouseConnectionFactory(new DriverConnectionFactory(new ClickHouseDriver(), config, credentialProvider));
     }
 }
