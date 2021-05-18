1a3f791567ba44e03e132e8c380aad9a8e921ef1
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-05-18 22:46:34 +0200

    Simplify TupleDomain transformation

--- core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/RemoveRedundantTableScanPredicate.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/RemoveRedundantTableScanPredicate.java
@@ -15,7 +15,6 @@ package io.trino.sql.planner.iterative.rule;
 
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import io.trino.Session;
 import io.trino.matching.Capture;
 import io.trino.matching.Captures;
@@ -24,6 +23,7 @@ import io.trino.metadata.Metadata;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.predicate.Domain;
 import io.trino.spi.predicate.TupleDomain;
+import io.trino.spi.type.Type;
 import io.trino.spi.type.TypeOperators;
 import io.trino.sql.planner.DomainTranslator;
 import io.trino.sql.planner.PlanNodeIdAllocator;
@@ -38,6 +38,7 @@ import io.trino.sql.tree.Expression;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkState;
 import static io.trino.matching.Capture.newCapture;
@@ -120,24 +121,19 @@ public class RemoveRedundantTableScanPredicate
 
         TupleDomain<ColumnHandle> unenforcedDomain;
         if (predicateDomain.getDomains().isPresent()) {
-            Map<ColumnHandle, Domain> predicateColumnDomains = predicateDomain.getDomains().get();
-
             // table scans with none domain should be converted to ValuesNode
             checkState(node.getEnforcedConstraint().getDomains().isPresent());
             Map<ColumnHandle, Domain> enforcedColumnDomains = node.getEnforcedConstraint().getDomains().get();
 
-            ImmutableMap.Builder<ColumnHandle, Domain> unenforcedColumnDomains = ImmutableMap.builder();
-            for (Map.Entry<ColumnHandle, Domain> entry : predicateColumnDomains.entrySet()) {
-                ColumnHandle columnHandle = entry.getKey();
-                Domain predicateColumnDomain = entry.getValue();
-                Domain enforcedColumnDomain = enforcedColumnDomains.getOrDefault(columnHandle, Domain.all(predicateColumnDomain.getType()));
-                predicateColumnDomain = predicateColumnDomain.intersect(enforcedColumnDomain);
-                if (!predicateColumnDomain.contains(enforcedColumnDomain)) {
-                    unenforcedColumnDomains.put(columnHandle, predicateColumnDomain);
+            unenforcedDomain = predicateDomain.transformDomains((columnHandle, predicateColumnDomain) -> {
+                Type type = predicateColumnDomain.getType();
+                Domain enforcedColumnDomain = Optional.ofNullable(enforcedColumnDomains.get(columnHandle)).orElseGet(() -> Domain.all(type));
+                if (predicateColumnDomain.contains(enforcedColumnDomain)) {
+                    // full enforced
+                    return Domain.all(type);
                 }
-            }
-
-            unenforcedDomain = TupleDomain.withColumnDomains(unenforcedColumnDomains.build());
+                return predicateColumnDomain.intersect(enforcedColumnDomain);
+            });
         }
         else {
             // TODO: DomainTranslator.fromPredicate can infer that the expression is "false" in some cases (TupleDomain.none()).
