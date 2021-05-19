f03d4f878d92d9c071962e78ae0ceae6f308171a
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-05-19 16:42:46 +0200

    Add connector test for TPCH connector

--- testing/trino-tests/src/test/java/io/trino/tests/tpch/TestTpchConnectorTest.java
+++ testing/trino-tests/src/test/java/io/trino/tests/tpch/TestTpchConnectorTest.java
@@ -23,9 +23,10 @@ import io.trino.spi.connector.CatalogSchemaTableName;
 import io.trino.spi.type.Type;
 import io.trino.sql.planner.planprinter.IoPlanPrinter;
 import io.trino.sql.planner.planprinter.IoPlanPrinter.EstimatedStatsAndCost;
-import io.trino.testing.AbstractTestQueries;
+import io.trino.testing.BaseConnectorTest;
 import io.trino.testing.MaterializedResult;
 import io.trino.testing.QueryRunner;
+import io.trino.testing.TestingConnectorBehavior;
 import io.trino.type.TypeDeserializer;
 import org.testng.annotations.Test;
 
@@ -35,16 +36,51 @@ import static com.google.common.collect.Iterables.getOnlyElement;
 import static io.trino.spi.type.VarcharType.createVarcharType;
 import static io.trino.sql.planner.planprinter.IoPlanPrinter.FormattedMarker.Bound.EXACTLY;
 import static io.trino.testing.TestingSession.testSessionBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertEquals;
 
-public class TestTpchDistributedQueries
-        extends AbstractTestQueries
+public class TestTpchConnectorTest
+        extends BaseConnectorTest
 {
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
         return TpchQueryRunnerBuilder.builder().build();
+    }
+
+    @Override
+    protected boolean hasBehavior(TestingConnectorBehavior connectorBehavior)
+    {
+        switch (connectorBehavior) {
+            case SUPPORTS_TOPN_PUSHDOWN:
+                return false;
+
+            case SUPPORTS_CREATE_SCHEMA:
+                return false;
+
+            case SUPPORTS_CREATE_TABLE:
+                return false;
+
+            case SUPPORTS_RENAME_TABLE:
+                return false;
+
+            case SUPPORTS_COMMENT_ON_TABLE:
+            case SUPPORTS_COMMENT_ON_COLUMN:
+                return false;
+
+            case SUPPORTS_INSERT:
+                return false;
+
+            case SUPPORTS_DELETE:
+                return false;
+
+            case SUPPORTS_ARRAY:
+                return false;
+
+            default:
+                return super.hasBehavior(connectorBehavior);
+        }
     }
 
     @Test
@@ -105,6 +141,23 @@ public class TestTpchDistributedQueries
         assertQuerySucceeds("SHOW TABLES FROM sf1");
         assertQuerySucceeds("SHOW TABLES FROM \"sf1.0\"");
         assertQueryFails("SHOW TABLES FROM sf0", "line 1:1: Schema 'sf0' does not exist");
+    }
+
+    @Override
+    public void testShowCreateTable()
+    {
+        assertThat(computeActual("SHOW CREATE TABLE orders").getOnlyValue())
+                .isEqualTo("CREATE TABLE tpch.tiny.orders (\n" +
+                        "   orderkey bigint NOT NULL,\n" +
+                        "   custkey bigint NOT NULL,\n" +
+                        "   orderstatus varchar(1) NOT NULL,\n" +
+                        "   totalprice double NOT NULL,\n" +
+                        "   orderdate date NOT NULL,\n" +
+                        "   orderpriority varchar(15) NOT NULL,\n" +
+                        "   clerk varchar(15) NOT NULL,\n" +
+                        "   shippriority integer NOT NULL,\n" +
+                        "   comment varchar(79) NOT NULL\n" +
+                        ")");
     }
 
     private Session createSession(String schemaName)
