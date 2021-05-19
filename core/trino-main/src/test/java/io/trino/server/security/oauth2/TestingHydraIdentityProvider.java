55c028f6beb5063fd4ac8bae2995f4ac233f51f9
Łukasz Walkiewicz <lukasz.walkiewicz@starburstdata.com>
2021-05-19 10:34:00 +0200

    Remove selenium from OAuth2 web UI tests
    Adding a custom implementation of login & consent server which accepts all
    requests makes the authorization code flow automatic from client's point
    of view and therefore following redirects is enough to get a valid
    access token during testing.

--- core/trino-main/src/test/java/io/trino/server/security/oauth2/TestingHydraIdentityProvider.java
+++ core/trino-main/src/test/java/io/trino/server/security/oauth2/TestingHydraIdentityProvider.java
@@ -13,13 +13,26 @@
  */
 package io.trino.server.security.oauth2;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
+import io.airlift.http.server.HttpServerConfig;
+import io.airlift.http.server.HttpServerInfo;
+import io.airlift.http.server.testing.TestingHttpServer;
+import io.airlift.node.NodeInfo;
 import io.trino.server.testing.TestingTrinoServer;
 import io.trino.server.ui.WebUiModule;
 import io.trino.util.AutoCloseableCloser;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
@@ -27,62 +40,74 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.utility.MountableFile;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.List;
 
+import static io.trino.client.OkHttpUtil.setupInsecureSsl;
 import static io.trino.server.security.oauth2.TokenEndpointAuthMethod.CLIENT_SECRET_BASIC;
+import static java.util.Objects.requireNonNull;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.testng.Assert.assertNotNull;
 
 public class TestingHydraIdentityProvider
         implements AutoCloseable
 {
-    static final int TTL_ACCESS_TOKEN_IN_SECONDS = 5;
     private static final String HYDRA_IMAGE = "oryd/hydra:v1.9.0-sqlite";
 
     private final Network network = Network.newNetwork();
-
-    private final FixedHostPortGenericContainer<?> consentContainer = new FixedHostPortGenericContainer<>("oryd/hydra-login-consent-node:v1.4.2")
-            .withNetwork(network)
-            .withNetworkAliases("consent")
-            .withExposedPorts(3000)
-            .withEnv("HYDRA_ADMIN_URL", "https://hydra:4445")
-            .withEnv("NODE_TLS_REJECT_UNAUTHORIZED", "0")
-            .waitingFor(Wait.forHttp("/").forStatusCode(200));
-
-    private final FixedHostPortGenericContainer<?> hydraContainer = createHydraContainer()
-            .withNetworkAliases("hydra")
-            .withExposedPorts(4444, 4445)
-            .withEnv("DSN", "memory")
-            .withEnv("URLS_SELF_ISSUER", "https://hydra:4444/")
-            .withEnv("URLS_CONSENT", "http://consent:3000/consent")
-            .withEnv("URLS_LOGIN", "http://consent:3000/login")
-            .withEnv("SERVE_TLS_KEY_PATH", "/tmp/certs/localhost.pem")
-            .withEnv("SERVE_TLS_CERT_PATH", "/tmp/certs/localhost.pem")
-            .withEnv("STRATEGIES_ACCESS_TOKEN", "jwt")
-            .withEnv("TTL_ACCESS_TOKEN", TTL_ACCESS_TOKEN_IN_SECONDS + "s")
-            .withCommand("serve", "all")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("/cert"), "/tmp/certs")
-            .waitingFor(new WaitAllStrategy()
-                    .withStrategy(Wait.forLogMessage(".*Setting up http server on :4444.*", 1))
-                    .withStrategy(Wait.forLogMessage(".*Setting up http server on :4445.*", 1)));
-
     private final AutoCloseableCloser closer = AutoCloseableCloser.create();
+    private final Duration ttlAccessToken;
+    private GenericContainer<?> hydraContainer;
 
-    TestingHydraIdentityProvider()
+    public TestingHydraIdentityProvider()
     {
+        this(Duration.ofMinutes(30));
+    }
+
+    public TestingHydraIdentityProvider(Duration ttlAccessToken)
+    {
+        this.ttlAccessToken = requireNonNull(ttlAccessToken, "ttlAccessToken is null");
         closer.register(network);
-        closer.register(consentContainer);
-        closer.register(hydraContainer);
     }
 
     public void start()
+            throws Exception
     {
-        consentContainer.start();
+        TestingHttpServer loginAndConsentServer = createTestingLoginAndConsentServer();
+        closer.register(loginAndConsentServer::stop);
+        loginAndConsentServer.start();
+        URI loginAndConsentBaseUrl = loginAndConsentServer.getBaseUrl();
+
+        hydraContainer = createHydraContainer()
+                .withNetworkAliases("hydra")
+                .withExposedPorts(4444, 4445)
+                .withEnv("DSN", "memory")
+                .withEnv("URLS_SELF_ISSUER", "https://localhost:4444/")
+                .withEnv("URLS_CONSENT", loginAndConsentBaseUrl + "/consent")
+                .withEnv("URLS_LOGIN", loginAndConsentBaseUrl + "/login")
+                .withEnv("SERVE_TLS_KEY_PATH", "/tmp/certs/localhost.pem")
+                .withEnv("SERVE_TLS_CERT_PATH", "/tmp/certs/localhost.pem")
+                .withEnv("STRATEGIES_ACCESS_TOKEN", "jwt")
+                .withEnv("TTL_ACCESS_TOKEN", ttlAccessToken.getSeconds() + "s")
+                .withCommand("serve", "all")
+                .withCopyFileToContainer(MountableFile.forClasspathResource("/cert"), "/tmp/certs")
+                .waitingFor(new WaitAllStrategy()
+                        .withStrategy(Wait.forLogMessage(".*Setting up http server on :4444.*", 1))
+                        .withStrategy(Wait.forLogMessage(".*Setting up http server on :4445.*", 1)));
+        closer.register(hydraContainer);
         hydraContainer.start();
     }
 
-    public FixedHostPortGenericContainer<?> createHydraContainer()
+    public GenericContainer<?> createHydraContainer()
     {
-        return new FixedHostPortGenericContainer<>(HYDRA_IMAGE).withNetwork(network);
+        return new GenericContainer<>(HYDRA_IMAGE).withNetwork(network);
     }
 
     public void createClient(
@@ -110,7 +135,7 @@ public class TestingHydraIdentityProvider
 
     public String getToken(String clientId, String clientSecret, List<String> audiences)
     {
-        FixedHostPortGenericContainer<?> container = createHydraContainer()
+        GenericContainer<?> container = createHydraContainer()
                 .withCommand("token", "client",
                         "--endpoint", "https://hydra:4444",
                         "--skip-tls-verify",
@@ -122,14 +147,14 @@ public class TestingHydraIdentityProvider
         return container.getLogs(OutputFrame.OutputType.STDOUT).replaceAll("\\s+", "");
     }
 
-    public Network getNetwork()
-    {
-        return network;
-    }
-
-    public int getHydraPort()
+    public int getAuthPort()
     {
         return hydraContainer.getMappedPort(4444);
+    }
+
+    public int getAdminPort()
+    {
+        return hydraContainer.getMappedPort(4445);
     }
 
     @Override
@@ -139,21 +164,129 @@ public class TestingHydraIdentityProvider
         closer.close();
     }
 
+    private TestingHttpServer createTestingLoginAndConsentServer()
+            throws IOException
+    {
+        NodeInfo nodeInfo = new NodeInfo("test");
+        HttpServerConfig config = new HttpServerConfig().setHttpPort(0);
+        HttpServerInfo httpServerInfo = new HttpServerInfo(config, nodeInfo);
+        return new TestingHttpServer(httpServerInfo, nodeInfo, config, new AcceptAllLoginsAndConsentsServlet(), ImmutableMap.of());
+    }
+
+    private class AcceptAllLoginsAndConsentsServlet
+            extends HttpServlet
+    {
+        private final ObjectMapper mapper = new ObjectMapper();
+        private final OkHttpClient httpClient;
+
+        public AcceptAllLoginsAndConsentsServlet()
+        {
+            OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+            setupInsecureSsl(httpClientBuilder);
+            httpClient = httpClientBuilder.build();
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws IOException
+        {
+            if (request.getPathInfo().equals("/login")) {
+                acceptLogin(request, response);
+                return;
+            }
+            if (request.getPathInfo().contains("/consent")) {
+                acceptConsent(request, response);
+                return;
+            }
+            response.setStatus(SC_NOT_FOUND);
+        }
+
+        private void acceptLogin(HttpServletRequest request, HttpServletResponse response)
+                throws IOException
+        {
+            String loginChallenge = request.getParameter("login_challenge");
+            try (Response loginAcceptResponse = acceptLogin(loginChallenge)) {
+                sendRedirect(loginAcceptResponse, response);
+            }
+        }
+
+        private void acceptConsent(HttpServletRequest request, HttpServletResponse response)
+                throws IOException
+        {
+            String consentChallenge = request.getParameter("consent_challenge");
+            JsonNode consentRequest = getConsentRequest(consentChallenge);
+            try (Response acceptConsentResponse = acceptConsent(consentChallenge, consentRequest)) {
+                sendRedirect(acceptConsentResponse, response);
+            }
+        }
+
+        private Response acceptLogin(String loginChallenge)
+                throws IOException
+        {
+            return httpClient.newCall(
+                    new Request.Builder()
+                            .url("https://localhost:" + getAdminPort() + "/oauth2/auth/requests/login/accept?login_challenge=" + loginChallenge)
+                            .put(RequestBody.create(
+                                    MediaType.get(APPLICATION_JSON),
+                                    mapper.writeValueAsString(mapper.createObjectNode().put("subject", "foo@bar.com"))))
+                            .build())
+                    .execute();
+        }
+
+        private JsonNode getConsentRequest(String consentChallenge)
+                throws IOException
+        {
+            try (Response response = httpClient.newCall(
+                    new Request.Builder()
+                            .url("https://localhost:" + getAdminPort() + "/oauth2/auth/requests/consent?consent_challenge=" + consentChallenge)
+                            .get()
+                            .build())
+                    .execute()) {
+                assertNotNull(response.body());
+                return mapper.readTree(response.body().byteStream());
+            }
+        }
+
+        private Response acceptConsent(String consentChallenge, JsonNode consentRequest)
+                throws IOException
+        {
+            return httpClient.newCall(
+                    new Request.Builder()
+                            .url("https://localhost:" + getAdminPort() + "/oauth2/auth/requests/consent/accept?consent_challenge=" + consentChallenge)
+                            .put(RequestBody.create(
+                                    MediaType.get(APPLICATION_JSON),
+                                    mapper.writeValueAsString(mapper.createObjectNode()
+                                            .<ObjectNode>set("grant_scope", consentRequest.get("requested_scope"))
+                                            .<ObjectNode>set("grant_access_token_audience", consentRequest.get("requested_access_token_audience")))))
+                            .build())
+                    .execute();
+        }
+
+        private void sendRedirect(Response redirectResponse, HttpServletResponse response)
+                throws IOException
+        {
+            assertNotNull(redirectResponse.body());
+            response.sendRedirect(
+                    toHostUrl(mapper.readTree(redirectResponse.body().byteStream())
+                            .get("redirect_to")
+                            .textValue()));
+        }
+
+        private String toHostUrl(String url)
+        {
+            return HttpUrl.get(url)
+                    .newBuilder()
+                    .port(getAuthPort())
+                    .toString();
+        }
+    }
+
     public static void main(String[] args)
             throws Exception
     {
         try (TestingHydraIdentityProvider service = new TestingHydraIdentityProvider()) {
-            // expose containers ports & override environment variables
-            service.consentContainer
-                    .withFixedExposedPort(9020, 3000);
-            service.hydraContainer
-                    .withFixedExposedPort(9001, 4444)
-                    .withFixedExposedPort(9002, 4445)
-                    .withEnv("URLS_SELF_ISSUER", "https://localhost:9001/")
-                    .withEnv("URLS_CONSENT", "http://localhost:9020/consent")
-                    .withEnv("URLS_LOGIN", "http://localhost:9020/login")
-                    .withEnv("TTL_ACCESS_TOKEN", "30m");
             service.start();
+            String authServerUrl = "https://localhost:" + service.getAuthPort();
             service.createClient(
                     "trino-client",
                     "trino-secret",
@@ -172,9 +305,9 @@ public class TestingHydraIdentityProvider
                                     .put("http-server.https.keystore.path", Resources.getResource("cert/localhost.pem").getPath())
                                     .put("http-server.https.keystore.key", "")
                                     .put("http-server.authentication.type", "oauth2")
-                                    .put("http-server.authentication.oauth2.auth-url", "https://localhost:9001/oauth2/auth")
-                                    .put("http-server.authentication.oauth2.token-url", "https://localhost:9001/oauth2/token")
-                                    .put("http-server.authentication.oauth2.jwks-url", "https://localhost:9001/.well-known/jwks.json")
+                                    .put("http-server.authentication.oauth2.auth-url", authServerUrl + "/oauth2/auth")
+                                    .put("http-server.authentication.oauth2.token-url", authServerUrl + "/oauth2/token")
+                                    .put("http-server.authentication.oauth2.jwks-url", authServerUrl + "/.well-known/jwks.json")
                                     .put("http-server.authentication.oauth2.client-id", "trino-client")
                                     .put("http-server.authentication.oauth2.client-secret", "trino-secret")
                                     .put("http-server.authentication.oauth2.audience", "https://localhost:8443/ui")
