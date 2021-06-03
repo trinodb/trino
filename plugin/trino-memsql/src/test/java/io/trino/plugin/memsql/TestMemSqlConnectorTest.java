93cd0154d33533db35d2f4b4a29269dd648f5894
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-06-03 22:18:33 +0530

    Re-enable testLargeIn tests on MemSQL

--- plugin/trino-memsql/src/test/java/io/trino/plugin/memsql/TestMemSqlConnectorTest.java
+++ plugin/trino-memsql/src/test/java/io/trino/plugin/memsql/TestMemSqlConnectorTest.java
@@ -154,15 +154,6 @@ public class TestMemSqlConnectorTest
     }
 
     @Override
-    public void testLargeIn(int valuesCount)
-    {
-        // Running this tests on MemSQL results in
-        // "Available disk space is below the value of 'minimal_disk_space' global variable (100 MB). This query cannot be executed"
-        // on followup tests
-        throw new SkipException("Running testLargeIn on MemSQL results in out-of-disk space errors");
-    }
-
-    @Override
     public void testInsertUnicode()
     {
         // MemSQL's utf8 encoding is 3 bytes and truncates strings upon encountering a 4 byte sequence
