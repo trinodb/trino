1912b61effce5339261443d54a56a289793cd871
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2021-06-10 12:59:04 +0200

    Correct keytabs and keystores location

--- testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/two-kerberos-hives/hadoop-master-2-copy-keytabs.sh
+++ testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/two-kerberos-hives/hadoop-master-2-copy-keytabs.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 echo "Copying kerberos keytabs to /presto_keytabs/"
-cp /etc/presto/conf/hive-presto-master.keytab /presto_keytabs/other-hive-presto-master.keytab
-cp /etc/presto/conf/presto-server.keytab /presto_keytabs/other-presto-server.keytab
+cp /etc/trino/conf/hive-presto-master.keytab /presto_keytabs/other-hive-presto-master.keytab
+cp /etc/trino/conf/presto-server.keytab /presto_keytabs/other-presto-server.keytab
