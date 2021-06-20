a60a3466de1f0013a1919c1a8e8eced7129e7602
chenjian2664 <chenjian2664@gmail.com>
2021-06-20 11:50:08 +0800

    Remove unused improt

--- plugin/trino-ignite/src/test/java/io/trino/plugin/ignite/TestIgniteConnectorTest.java
+++ plugin/trino-ignite/src/test/java/io/trino/plugin/ignite/TestIgniteConnectorTest.java
@@ -26,7 +26,6 @@ import io.trino.testing.TestingConnectorBehavior;
 import io.trino.testing.sql.SqlExecutor;
 import io.trino.testing.sql.TestTable;
 import org.intellij.lang.annotations.Language;
-import org.testng.annotations.Test;
 
 import java.util.List;
 
