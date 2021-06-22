fb62a3425a05b60a5659ed804104b93445e5f2c0
Raunaq Morarka <raunaqmorarka@gmail.com>
2021-06-22 18:46:12 +0530

    Add INSERT and DELETE to GRANT/REVOKE tests

--- testing/trino-tests/src/test/java/io/trino/execution/TestRevokeOnTable.java
+++ testing/trino-tests/src/test/java/io/trino/execution/TestRevokeOnTable.java
@@ -46,7 +46,9 @@ public class TestRevokeOnTable
     private static final Session admin = sessionOf("admin");
     private static final Session userWithAllPrivileges = sessionOf(randomUsername());
     private static final Session userWithSelect = sessionOf(randomUsername());
+    private static final Session userWithInsert = sessionOf(randomUsername());
     private static final Session userWithUpdate = sessionOf(randomUsername());
+    private static final Session userWithDelete = sessionOf(randomUsername());
     private DistributedQueryRunner queryRunner;
     private QueryAssertions assertions;
 
@@ -60,7 +62,9 @@ public class TestRevokeOnTable
         tableGrants.grant(new TrinoPrincipal(USER, admin.getUser()), table, EnumSet.allOf(Privilege.class), true);
         tableGrants.grant(new TrinoPrincipal(USER, userWithAllPrivileges.getUser()), table, EnumSet.allOf(Privilege.class), true);
         tableGrants.grant(new TrinoPrincipal(USER, userWithSelect.getUser()), table, ImmutableSet.of(Privilege.SELECT), true);
+        tableGrants.grant(new TrinoPrincipal(USER, userWithInsert.getUser()), table, ImmutableSet.of(Privilege.INSERT), true);
         tableGrants.grant(new TrinoPrincipal(USER, userWithUpdate.getUser()), table, ImmutableSet.of(Privilege.UPDATE), true);
+        tableGrants.grant(new TrinoPrincipal(USER, userWithDelete.getUser()), table, ImmutableSet.of(Privilege.DELETE), true);
         MockConnectorFactory connectorFactory = MockConnectorFactory.builder()
                 .withListSchemaNames(session -> ImmutableList.of("default"))
                 .withListTables((session, schemaName) -> "default".equalsIgnoreCase(schemaName) ? ImmutableList.of(table) : ImmutableList.of())
@@ -125,7 +129,9 @@ public class TestRevokeOnTable
     {
         return new Object[][] {
                 {"SELECT", userWithSelect},
+                {"INSERT", userWithInsert},
                 {"UPDATE", userWithUpdate},
+                {"DELETE", userWithDelete},
                 {"ALL PRIVILEGES", userWithAllPrivileges}
         };
     }
@@ -135,7 +141,9 @@ public class TestRevokeOnTable
     {
         return new Object[][] {
                 {"SELECT"},
+                {"INSERT"},
                 {"UPDATE"},
+                {"DELETE"},
                 {"ALL PRIVILEGES"}
         };
     }
