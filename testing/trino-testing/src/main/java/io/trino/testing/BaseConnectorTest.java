8d122b2d30e0050a23b794327f7611382d3d5f91
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-07-09 18:49:24 +0530

    Add tests to verify DELETE support declared by connectors

--- testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
@@ -15,6 +15,7 @@ package io.trino.testing;
 
 import io.trino.Session;
 import io.trino.sql.analyzer.FeaturesConfig.JoinDistributionType;
+import io.trino.testing.sql.TestTable;
 import org.intellij.lang.annotations.Language;
 import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
@@ -725,6 +726,34 @@ public abstract class BaseConnectorTest
 
         assertFalse(getQueryRunner().tableExists(getSession(), tableName));
         assertFalse(getQueryRunner().tableExists(getSession(), renamedTable));
+    }
+
+    @Test
+    public void verifySupportsDeleteDeclaration()
+    {
+        if (hasBehavior(SUPPORTS_DELETE)) {
+            // Covered by testDeleteAllDataFromTable
+            return;
+        }
+
+        skipTestUnless(hasBehavior(SUPPORTS_CREATE_TABLE));
+        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_delete", "AS SELECT * FROM region")) {
+            assertQueryFails("DELETE FROM " + table.getName(), "This connector does not support deletes");
+        }
+    }
+
+    @Test
+    public void verifySupportsRowLevelDeleteDeclaration()
+    {
+        if (hasBehavior(SUPPORTS_ROW_LEVEL_DELETE)) {
+            // Covered by testRowLevelDelete
+            return;
+        }
+
+        skipTestUnless(hasBehavior(SUPPORTS_CREATE_TABLE));
+        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_delete", "AS SELECT * FROM region")) {
+            assertQueryFails("DELETE FROM " + table.getName() + " WHERE regionkey = 2", "This connector does not support deletes");
+        }
     }
 
     @Test
