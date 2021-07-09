raunaqmorarka/presto

Send dynamic filters from coordinator to workers

core/trino-main/src/main/java/io/trino/server/TaskResource.java
@@ -134,7 +134,8 @@ public class TaskResource
                 taskUpdateRequest.getFragment(),
                 taskUpdateRequest.getSources(),
                 taskUpdateRequest.getOutputIds(),
-                taskUpdateRequest.getTotalPartitions());
+                taskUpdateRequest.getTotalPartitions(),
+                taskUpdateRequest.getDynamicFilter());
 
         if (shouldSummarize(uriInfo)) {
             taskInfo = taskInfo.summarize();

trinodb/trino

core/trino-main/src/main/java/io/trino/server/TaskResource.java
@@ -134,7 +134,8 @@ public class TaskResource
                 taskUpdateRequest.getFragment(),
                 taskUpdateRequest.getSources(),
                 taskUpdateRequest.getOutputIds(),
-                taskUpdateRequest.getTotalPartitions());
+                taskUpdateRequest.getTotalPartitions(),
+                taskUpdateRequest.getDynamicFilterDomains());
 
         if (shouldSummarize(uriInfo)) {
             taskInfo = taskInfo.summarize();