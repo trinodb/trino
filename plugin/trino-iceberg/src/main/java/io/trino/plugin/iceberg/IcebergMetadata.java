784a31be1d011e826558e55822d1f037dd0aa604
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-07-17 16:43:20 +0200

    Mark view creator in metastore
    To be able to track down what created given view. I.e. to distinguish
    between things created by Hive connector and Iceberg connector, if they
    share metastore.

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -184,6 +184,7 @@ public class IcebergMetadata
     public static final String DEPENDS_ON_TABLES = "dependsOnTables";
 
     // Be compatible with views defined by the Hive connector, which can be useful under certain conditions.
+    private static final String TRINO_CREATED_BY = HiveMetadata.TRINO_CREATED_BY;
     private static final String PRESTO_VIEW_COMMENT = HiveMetadata.PRESTO_VIEW_COMMENT;
     private static final String PRESTO_VERSION_NAME = HiveMetadata.PRESTO_VERSION_NAME;
     private static final String PRESTO_QUERY_ID_NAME = HiveMetadata.PRESTO_QUERY_ID_NAME;
@@ -794,6 +795,7 @@ public class IcebergMetadata
         HiveIdentity identity = new HiveIdentity(session);
         Map<String, String> properties = ImmutableMap.<String, String>builder()
                 .put(PRESTO_VIEW_FLAG, "true")
+                .put(TRINO_CREATED_BY, "Trino Iceberg connector")
                 .put(PRESTO_VERSION_NAME, trinoVersion)
                 .put(PRESTO_QUERY_ID_NAME, session.getQueryId())
                 .put(TABLE_COMMENT, PRESTO_VIEW_COMMENT)
@@ -1056,6 +1058,7 @@ public class IcebergMetadata
                 .put(PRESTO_QUERY_ID_NAME, session.getQueryId())
                 .put(STORAGE_TABLE, storageTableName)
                 .put(PRESTO_VIEW_FLAG, "true")
+                .put(TRINO_CREATED_BY, "Trino Iceberg connector")
                 .put(TABLE_COMMENT, ICEBERG_MATERIALIZED_VIEW_COMMENT)
                 .build();
 
