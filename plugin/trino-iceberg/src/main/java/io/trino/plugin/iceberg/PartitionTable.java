880fcb8a265c1c5bbd21c4ff1369760cf1266f01
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-07-23 14:04:26 +0200

    Inline PartitionTable#toMap
    The method is oneliner.
    
    This also makes `PartitionTable#getPartitions` even more similar to
    analogous code inside `TableStatisticsMaker#makeTableStatistics` method.

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/PartitionTable.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/PartitionTable.java
@@ -59,6 +59,7 @@ import java.util.stream.Collectors;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.plugin.iceberg.IcebergUtil.getIdentityPartitions;
+import static io.trino.plugin.iceberg.Partition.toMap;
 import static io.trino.plugin.iceberg.TypeConverter.toTrinoType;
 import static io.trino.plugin.iceberg.util.Timestamps.timestampTzFromMicros;
 import static io.trino.spi.type.BigintType.BIGINT;
@@ -185,8 +186,8 @@ public class PartitionTable
                             partitionStruct,
                             dataFile.recordCount(),
                             dataFile.fileSizeInBytes(),
-                            toMap(dataFile.lowerBounds()),
-                            toMap(dataFile.upperBounds()),
+                            toMap(idToTypeMapping, dataFile.lowerBounds()),
+                            toMap(idToTypeMapping, dataFile.upperBounds()),
                             dataFile.nullValueCounts(),
                             dataFile.columnSizes());
                     partitions.put(partitionWrapper, partition);
@@ -197,8 +198,8 @@ public class PartitionTable
                 partition.incrementFileCount();
                 partition.incrementRecordCount(dataFile.recordCount());
                 partition.incrementSize(dataFile.fileSizeInBytes());
-                partition.updateMin(toMap(dataFile.lowerBounds()), dataFile.nullValueCounts(), dataFile.recordCount());
-                partition.updateMax(toMap(dataFile.upperBounds()), dataFile.nullValueCounts(), dataFile.recordCount());
+                partition.updateMin(toMap(idToTypeMapping, dataFile.lowerBounds()), dataFile.nullValueCounts(), dataFile.recordCount());
+                partition.updateMax(toMap(idToTypeMapping, dataFile.upperBounds()), dataFile.nullValueCounts(), dataFile.recordCount());
                 partition.updateNullCount(dataFile.nullValueCounts());
             }
 
@@ -274,11 +275,6 @@ public class PartitionTable
 
         rowBlockBuilder.closeEntry();
         return columnMetricType.getObject(rowBlockBuilder, 0);
-    }
-
-    private Map<Integer, Object> toMap(Map<Integer, ByteBuffer> idToMetricMap)
-    {
-        return Partition.toMap(idToTypeMapping, idToMetricMap);
     }
 
     /**
