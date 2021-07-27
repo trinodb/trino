f9ccd13155dd11eafb41691323ecdd13db140b85
praveenkrishna <praveenkrishna@tutanota.com>
2021-07-27 21:03:55 +0530

    Update Trino version to 359-167-g30f2ecce

--- plugin/starburst-sap-hana/src/main/java/com/starburstdata/presto/plugin/saphana/SapHanaAuthenticationModule.java
+++ plugin/starburst-sap-hana/src/main/java/com/starburstdata/presto/plugin/saphana/SapHanaAuthenticationModule.java
@@ -15,7 +15,7 @@ import com.google.inject.Singleton;
 import com.sap.db.jdbc.Driver;
 import com.starburstdata.presto.plugin.jdbc.JdbcConnectionPoolConfig;
 import com.starburstdata.presto.plugin.jdbc.PoolingConnectionFactory;
-import com.starburstdata.presto.plugin.jdbc.auth.AuthenticationBasedJdbcIdentityCacheMappingModule;
+import com.starburstdata.presto.plugin.jdbc.auth.AuthenticationBasedIdentityCacheMappingModule;
 import com.starburstdata.presto.plugin.jdbc.auth.PasswordPassThroughModule;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.trino.plugin.base.CatalogName;
@@ -75,7 +75,7 @@ public class SapHanaAuthenticationModule
         {
             configBinder(binder).bindConfig(JdbcConnectionPoolConfig.class);
             install(new PasswordPassThroughModule<>(SapHanaAuthenticationConfig.class, config -> false));
-            install(new AuthenticationBasedJdbcIdentityCacheMappingModule());
+            install(new AuthenticationBasedIdentityCacheMappingModule());
         }
 
         @Provides
