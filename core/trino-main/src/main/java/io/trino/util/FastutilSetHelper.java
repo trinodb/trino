3dc1e44efc44a18679190600db58e8efbe0875f1
Raunaq Morarka <raunaqmorarka@gmail.com>
2021-08-06 23:50:49 +0530

    Move FastutilSetHelper to trino-main
    This reverts commit cdbf4535b0fa96b1680e8be6fefeef0b6e967e04.

--- core/trino-main/src/main/java/io/trino/util/FastutilSetHelper.java
+++ core/trino-main/src/main/java/io/trino/util/FastutilSetHelper.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.base.util;
+package io.trino.util;
 
 import io.trino.spi.TrinoException;
 import io.trino.spi.type.Type;
