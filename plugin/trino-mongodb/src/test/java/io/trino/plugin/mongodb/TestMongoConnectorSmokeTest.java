cd5f820a5333a8e6b57669e00df1e0fe6a0eb246
Yuya Ebihara <ebyhry@gmail.com>
2021-08-11 20:13:43 +0900

    Upgrade minimum MongoDB requirement to version 4.0
    All 3.x versions reached to EOL.
    - MongoDB 3.4: January 2020
    - MongoDB 3.6: April 2021
    https://www.mongodb.com/support-policy/legacy

--- plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoConnectorSmokeTest.java
+++ plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoConnectorSmokeTest.java
@@ -18,14 +18,14 @@ import io.trino.testing.QueryRunner;
 
 import static io.trino.plugin.mongodb.MongoQueryRunner.createMongoQueryRunner;
 
-public class TestMongo3LatestConnectorSmokeTest
+public class TestMongoConnectorSmokeTest
         extends BaseMongoConnectorSmokeTest
 {
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
-        MongoServer server = closeAfterClass(new MongoServer("3.6.20"));
+        MongoServer server = closeAfterClass(new MongoServer());
         return createMongoQueryRunner(server, ImmutableMap.of(), REQUIRED_TPCH_TABLES);
     }
 }
