6b3cce591481561c74e75b0d0d1bd56186ba2f73
James Petty <pettja@amazon.com>
2021-08-25 15:35:35 -0400

    Use blockBuilders.length in PageBuilder#reset()

--- core/trino-spi/src/main/java/io/trino/spi/PageBuilder.java
+++ core/trino-spi/src/main/java/io/trino/spi/PageBuilder.java
@@ -93,7 +93,7 @@ public class PageBuilder
 
         declaredPositions = 0;
 
-        for (int i = 0; i < types.size(); i++) {
+        for (int i = 0; i < blockBuilders.length; i++) {
             blockBuilders[i] = blockBuilders[i].newBlockBuilderLike(pageBuilderStatus.createBlockBuilderStatus());
         }
     }
