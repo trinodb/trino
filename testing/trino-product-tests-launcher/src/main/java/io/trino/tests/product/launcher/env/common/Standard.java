329c48d5c75e2e4a5a474b8f31bc85edd5a37002
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-09-01 18:28:02 +0200

    Convert deprecation comment into a javadoc

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/common/Standard.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/common/Standard.java
@@ -52,9 +52,11 @@ import static org.testcontainers.containers.wait.strategy.Wait.forHealthcheck;
 import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage;
 import static org.testcontainers.utility.MountableFile.forHostPath;
 
-// Use io.trino.tests.product.launcher.env.common.StandardMultinode instead
-// Product tests should mimic production like use case.
-// Single node environment does not do it well enough and some issues are only exposed with multi node installations.
+/**
+ * @deprecated Product tests should mimic production like use case.
+ * Single node environment does not do it well enough and some issues are
+ * only exposed with multi node installations. Use {@link StandardMultinode} instead.
+ */
 @Deprecated
 public final class Standard
         implements EnvironmentExtender
