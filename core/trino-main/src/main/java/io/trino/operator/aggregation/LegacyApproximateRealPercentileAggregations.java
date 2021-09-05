3140cad22834a943be8ba37705256da4828f6974
Dain Sundstrom <dain@iq80.com>
2021-09-04 20:04:30 -0700

    Move legacy approx_percentile to separate classes

--- core/trino-main/src/main/java/io/trino/operator/aggregation/LegacyApproximateRealPercentileAggregations.java
+++ core/trino-main/src/main/java/io/trino/operator/aggregation/LegacyApproximateRealPercentileAggregations.java
@@ -13,18 +13,21 @@
  */
 package io.trino.operator.aggregation;
 
-import io.airlift.stats.TDigest;
-import io.trino.operator.aggregation.state.TDigestAndPercentileState;
+import io.airlift.stats.QuantileDigest;
+import io.trino.operator.aggregation.state.QuantileDigestAndPercentileState;
 import io.trino.spi.block.BlockBuilder;
 import io.trino.spi.function.AggregationFunction;
 import io.trino.spi.function.AggregationState;
 import io.trino.spi.function.CombineFunction;
+import io.trino.spi.function.Description;
 import io.trino.spi.function.InputFunction;
 import io.trino.spi.function.OutputFunction;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.type.StandardTypes;
 
 import static com.google.common.base.Preconditions.checkState;
+import static io.trino.operator.aggregation.FloatingPointBitsConverterUtil.floatToSortableInt;
+import static io.trino.operator.aggregation.FloatingPointBitsConverterUtil.sortableIntToFloat;
 import static io.trino.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
 import static io.trino.spi.type.RealType.REAL;
 import static io.trino.util.Failures.checkCondition;
@@ -32,32 +35,29 @@ import static java.lang.Float.floatToRawIntBits;
 import static java.lang.Float.intBitsToFloat;
 
 @AggregationFunction("approx_percentile")
-public final class ApproximateRealPercentileAggregations
+public final class LegacyApproximateRealPercentileAggregations
 {
-    private ApproximateRealPercentileAggregations() {}
+    private LegacyApproximateRealPercentileAggregations() {}
 
+    // This function is deprecated. It uses QuantileDigest while other 'approx_percentile' functions use TDigest. TDigest does not accept the accuracy parameter.
+    @Deprecated
+    @Description("(DEPRECATED) Use approx_percentile(x, weight, percentile) instead")
     @InputFunction
-    public static void input(@AggregationState TDigestAndPercentileState state, @SqlType(StandardTypes.REAL) long value, @SqlType(StandardTypes.DOUBLE) double percentile)
+    public static void weightedInput(@AggregationState QuantileDigestAndPercentileState state, @SqlType(StandardTypes.REAL) long value, @SqlType(StandardTypes.DOUBLE) double weight, @SqlType(StandardTypes.DOUBLE) double percentile, @SqlType(StandardTypes.DOUBLE) double accuracy)
     {
-        ApproximateDoublePercentileAggregations.input(state, intBitsToFloat((int) value), percentile);
-    }
-
-    @InputFunction
-    public static void weightedInput(@AggregationState TDigestAndPercentileState state, @SqlType(StandardTypes.REAL) long value, @SqlType(StandardTypes.DOUBLE) double weight, @SqlType(StandardTypes.DOUBLE) double percentile)
-    {
-        ApproximateDoublePercentileAggregations.weightedInput(state, intBitsToFloat((int) value), weight, percentile);
+        LegacyApproximateLongPercentileAggregations.weightedInput(state, floatToSortableInt(intBitsToFloat((int) value)), weight, percentile, accuracy);
     }
 
     @CombineFunction
-    public static void combine(@AggregationState TDigestAndPercentileState state, @AggregationState TDigestAndPercentileState otherState)
+    public static void combine(@AggregationState QuantileDigestAndPercentileState state, @AggregationState QuantileDigestAndPercentileState otherState)
     {
-        ApproximateDoublePercentileAggregations.combine(state, otherState);
+        LegacyApproximateLongPercentileAggregations.combine(state, otherState);
     }
 
     @OutputFunction(StandardTypes.REAL)
-    public static void output(@AggregationState TDigestAndPercentileState state, BlockBuilder out)
+    public static void output(@AggregationState QuantileDigestAndPercentileState state, BlockBuilder out)
     {
-        TDigest digest = state.getDigest();
+        QuantileDigest digest = state.getDigest();
         double percentile = state.getPercentile();
         if (digest == null || digest.getCount() == 0.0) {
             out.appendNull();
@@ -65,7 +65,7 @@ public final class ApproximateRealPercentileAggregations
         else {
             checkState(percentile != -1.0, "Percentile is missing");
             checkCondition(0 <= percentile && percentile <= 1, INVALID_FUNCTION_ARGUMENT, "Percentile must be between 0 and 1");
-            REAL.writeLong(out, floatToRawIntBits((float) digest.valueAt(percentile)));
+            REAL.writeLong(out, floatToRawIntBits(sortableIntToFloat((int) digest.getQuantile(percentile))));
         }
     }
 }
