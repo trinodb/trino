82191f288122b23806b51135ceaeee230d4c4ec0
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-09-10 20:33:37 +0200

    Add test for system.jdbc.* with broken Hive view

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveViews.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveViews.java
@@ -36,17 +36,17 @@ public class TestHiveViews
         extends AbstractTestHiveViews
 {
     @Test(groups = HIVE_VIEWS)
-    public void testFailingHiveViewsForInformationSchema()
+    public void testFailingHiveViewsWithMetadataListing()
     {
+        setupBrokenView();
+        testFailingHiveViewsWithInformationSchema();
+        testFailingHiveViewsWithSystemJdbc();
+        // cleanup
         onHive().executeQuery("DROP SCHEMA IF EXISTS test_list_failing_views CASCADE");
-        onHive().executeQuery("CREATE SCHEMA test_list_failing_views");
-        onHive().executeQuery("CREATE VIEW test_list_failing_views.correct_view AS SELECT * FROM nation limit 5");
+    }
 
-        // Create a view for which the translation is guaranteed to fail
-        onTrino().executeQuery("CREATE TABLE test_list_failing_views.table_dropped (col0 BIGINT)");
-        onHive().executeQuery("CREATE VIEW test_list_failing_views.failing_view AS SELECT * FROM test_list_failing_views.table_dropped");
-        onTrino().executeQuery("DROP TABLE test_list_failing_views.table_dropped");
-
+    private void testFailingHiveViewsWithInformationSchema()
+    {
         // The expected behavior is different across hive versions. For hive 3, the call "getTableNamesByType" is
         // used in ThriftHiveMetastore#getAllViews. For older versions, the fallback to doGetTablesWithParameter
         // is used, so Trino's information_schema.views table does not include translated Hive views.
@@ -82,6 +82,68 @@ public class TestHiveViews
 
         assertThatThrownBy(() -> query("SELECT * FROM information_schema.columns WHERE table_schema = 'test_list_failing_views' AND table_name = 'failing_view'"))
                 .hasMessageContaining("Failed to translate Hive view 'test_list_failing_views.failing_view'");
+    }
+
+    private void testFailingHiveViewsWithSystemJdbc()
+    {
+        // The expected behavior is different across hive versions. For hive 3, the call "getTableNamesByType" is
+        // used in ThriftHiveMetastore#getAllViews. For older versions, the fallback to doGetTablesWithParameter
+        // is used, so Trino's system.jdbc.tables table does not include translated Hive views.
+        String withSchemaFilter = "SELECT table_name FROM system.jdbc.tables WHERE " +
+                "table_cat = 'hive' AND " +
+                "table_schem = 'test_list_failing_views' AND " +
+                "table_type = 'VIEW'";
+        String withNoFilter = "SELECT table_name FROM system.jdbc.tables WHERE table_cat = 'hive' AND table_type = 'VIEW'";
+        if (getHiveVersionMajor() == 3) {
+            assertThat(query(withSchemaFilter)).containsOnly(row("correct_view"), row("failing_view"));
+            assertThat(query(withNoFilter)).contains(row("correct_view"), row("failing_view"));
+        }
+        else {
+            assertThat(query(withSchemaFilter)).hasNoRows();
+            Assertions.assertThat(query(withNoFilter).rows()).doesNotContain(ImmutableList.of("correct_view"));
+        }
+
+        // Queries with filters on table_schema and table_name are optimized to only fetch the specified table and uses
+        // a different API. so the Hive version does not matter here.
+        assertThat(query(
+                "SELECT table_name FROM system.jdbc.tables WHERE " +
+                        "table_cat = 'hive' AND " +
+                        "table_schem = 'test_list_failing_views' AND " +
+                        "table_name = 'correct_view'"))
+                .containsOnly(row("correct_view"));
+
+        // Listing fails when metadata for the problematic view is queried specifically
+        assertThatThrownBy(() -> query(
+                "SELECT table_name FROM system.jdbc.tables WHERE " +
+                        "table_cat = 'hive' AND " +
+                        "table_schem = 'test_list_failing_views' AND " +
+                        "table_name = 'failing_view'"))
+                .hasMessageContaining("Failed to translate Hive view 'test_list_failing_views.failing_view'");
+
+        // Queries on system.jdbc.columns also trigger ConnectorMetadata#getViews. Columns from failing_view are
+        // listed too since HiveMetadata#listTableColumns does not ignore views.
+        assertThat(query("SELECT table_name, column_name FROM system.jdbc.columns WHERE table_cat = 'hive' AND table_schem = 'test_list_failing_views'"))
+                .containsOnly(
+                        row("correct_view", "n_nationkey"),
+                        row("correct_view", "n_name"),
+                        row("correct_view", "n_regionkey"),
+                        row("correct_view", "n_comment"),
+                        row("failing_view", "col0"));
+
+        assertThatThrownBy(() -> query("SELECT * FROM system.jdbc.columns WHERE table_cat = 'hive' AND table_schem = 'test_list_failing_views' AND table_name = 'failing_view'"))
+                .hasMessageContaining("Failed to translate Hive view 'test_list_failing_views.failing_view'");
+    }
+
+    private static void setupBrokenView()
+    {
+        onHive().executeQuery("DROP SCHEMA IF EXISTS test_list_failing_views CASCADE");
+        onHive().executeQuery("CREATE SCHEMA test_list_failing_views");
+        onHive().executeQuery("CREATE VIEW test_list_failing_views.correct_view AS SELECT * FROM nation limit 5");
+
+        // Create a view for which the translation is guaranteed to fail
+        onTrino().executeQuery("CREATE TABLE test_list_failing_views.table_dropped (col0 BIGINT)");
+        onHive().executeQuery("CREATE VIEW test_list_failing_views.failing_view AS SELECT * FROM test_list_failing_views.table_dropped");
+        onTrino().executeQuery("DROP TABLE test_list_failing_views.table_dropped");
     }
 
     @Test(groups = HIVE_VIEWS)
