36dbd6ee6a480635bb22877a890934ee4c40e2a9
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-09-14 16:40:28 +0200

    Add parser/analyzer support for ALTER TABLE ... EXECUTE

--- core/trino-parser/src/main/java/io/trino/sql/SqlFormatter.java
+++ core/trino-parser/src/main/java/io/trino/sql/SqlFormatter.java
@@ -117,6 +117,7 @@ import io.trino.sql.tree.ShowTables;
 import io.trino.sql.tree.SingleColumn;
 import io.trino.sql.tree.StartTransaction;
 import io.trino.sql.tree.Table;
+import io.trino.sql.tree.TableExecute;
 import io.trino.sql.tree.TableSubquery;
 import io.trino.sql.tree.TransactionAccessMode;
 import io.trino.sql.tree.TransactionMode;
@@ -1378,6 +1379,20 @@ public final class SqlFormatter
             }
             builder.append(formatExpression(node.getColumn()));
 
+            return null;
+        }
+
+        protected Void visitTableExecute(TableExecute node, Integer indent)
+        {
+            builder.append("ALTER TABLE ");
+            builder.append(formatName(node.getTable().getName()));
+            builder.append(" EXECUTE ");
+            builder.append(formatExpression(node.getProcedureName()));
+            builder.append(formatPropertiesMultiLine(node.getProperties()));
+            node.getWhere().ifPresent(where ->
+                    builder.append("\n")
+                            .append(indentString(indent))
+                            .append("WHERE ").append(formatExpression(where)));
             return null;
         }
 
