martint/presto

Flatten AND and OR expressions
Allow these expressions to have more than two terms to avoid
deep expression trees for trivial chains of AND or OR expressions
and the ensuing StackOverflow errors.

core/trino-parser/src/main/java/io/trino/sql/tree/DefaultTraversalVisitor.java
@@ -423,10 +423,11 @@ public abstract class DefaultTraversalVisitor<C>
     }
 
     @Override
-    protected Void visitLogicalBinaryExpression(LogicalBinaryExpression node, C context)
+    protected Void visitLogicalExpression(LogicalExpression node, C context)
     {
-        process(node.getLeft(), context);
-        process(node.getRight(), context);
+        for (Node child : node.getChildren()) {
+            process(child, context);
+        }
 
         return null;
     }

trinodb/trino

core/trino-parser/src/main/java/io/trino/sql/tree/DefaultTraversalVisitor.java
@@ -452,10 +452,11 @@ public abstract class DefaultTraversalVisitor<C>
     }
 
     @Override
-    protected Void visitLogicalBinaryExpression(LogicalBinaryExpression node, C context)
+    protected Void visitLogicalExpression(LogicalExpression node, C context)
     {
-        process(node.getLeft(), context);
-        process(node.getRight(), context);
+        for (Node child : node.getTerms()) {
+            process(child, context);
+        }
 
         return null;
     }