eef0d8ead021f3e4a2a047da189152a7a37fee32
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-10-14 14:05:17 +0200

    restore other tests in suite 6

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/Suite6NonGeneric.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/Suite6NonGeneric.java
@@ -18,6 +18,14 @@ import io.trino.tests.product.launcher.env.EnvironmentConfig;
 import io.trino.tests.product.launcher.env.EnvironmentDefaults;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafka;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafkaSsl;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeCassandra;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosKmsHdfsImpersonation;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosKmsHdfsNoImpersonation;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdap;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapAndFile;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapBindDn;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapInsecure;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapReferrals;
 import io.trino.tests.product.launcher.suite.Suite;
 import io.trino.tests.product.launcher.suite.SuiteTestRun;
 
@@ -35,6 +43,14 @@ public class Suite6NonGeneric
         verify(config.getHadoopBaseImage().equals(EnvironmentDefaults.HADOOP_BASE_IMAGE), "The suite should be run with default HADOOP_BASE_IMAGE. Leave HADOOP_BASE_IMAGE unset.");
 
         return ImmutableList.of(
+                testOnEnvironment(EnvSinglenodeLdap.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapAndFile.class).withGroups("ldap", "ldap_and_file", "ldap_cli", "ldap_and_file_cli").build(),
+                testOnEnvironment(EnvSinglenodeLdapInsecure.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapReferrals.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapBindDn.class).withGroups("ldap").withExcludedGroups("ldap_multiple_binds").build(),
+                testOnEnvironment(EnvSinglenodeKerberosKmsHdfsNoImpersonation.class).withGroups("storage_formats").build(),
+                testOnEnvironment(EnvSinglenodeKerberosKmsHdfsImpersonation.class).withGroups("storage_formats").build(),
+                testOnEnvironment(EnvSinglenodeCassandra.class).withGroups("cassandra").build(),
                 testOnEnvironment(EnvMultinodeKafka.class).withGroups("kafka").build(),
                 testOnEnvironment(EnvMultinodeKafkaSsl.class).withGroups("kafka").build());
     }
