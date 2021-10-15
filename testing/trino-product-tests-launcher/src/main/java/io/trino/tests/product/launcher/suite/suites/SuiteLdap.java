e8b7506b302b9adb01df9002942f813cf7ff5468
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-10-15 22:39:49 +0200

    Split out suite-ldap from suite-6
    `multinode-kafka-ssl` was observed to be flaky on CI, however, it was
    not observed to be flaky in isolation (i.e. with all non-kafka images
    removed from suite-6).  The commit redeuces number of different
    containers started by a GHA job.

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteLdap.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteLdap.java
@@ -16,11 +16,11 @@ package io.trino.tests.product.launcher.suite.suites;
 import com.google.common.collect.ImmutableList;
 import io.trino.tests.product.launcher.env.EnvironmentConfig;
 import io.trino.tests.product.launcher.env.EnvironmentDefaults;
-import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafka;
-import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafkaSsl;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeCassandra;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosKmsHdfsImpersonation;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosKmsHdfsNoImpersonation;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdap;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapAndFile;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapBindDn;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapInsecure;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeLdapReferrals;
 import io.trino.tests.product.launcher.suite.Suite;
 import io.trino.tests.product.launcher.suite.SuiteTestRun;
 
@@ -29,7 +29,7 @@ import java.util.List;
 import static com.google.common.base.Verify.verify;
 import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
 
-public class Suite6NonGeneric
+public class SuiteLdap
         extends Suite
 {
     @Override
@@ -38,10 +38,10 @@ public class Suite6NonGeneric
         verify(config.getHadoopBaseImage().equals(EnvironmentDefaults.HADOOP_BASE_IMAGE), "The suite should be run with default HADOOP_BASE_IMAGE. Leave HADOOP_BASE_IMAGE unset.");
 
         return ImmutableList.of(
-                testOnEnvironment(EnvSinglenodeKerberosKmsHdfsNoImpersonation.class).withGroups("storage_formats").build(),
-                testOnEnvironment(EnvSinglenodeKerberosKmsHdfsImpersonation.class).withGroups("storage_formats").build(),
-                testOnEnvironment(EnvSinglenodeCassandra.class).withGroups("cassandra").build(),
-                testOnEnvironment(EnvMultinodeKafka.class).withGroups("kafka").build(),
-                testOnEnvironment(EnvMultinodeKafkaSsl.class).withGroups("kafka").build());
+                testOnEnvironment(EnvSinglenodeLdap.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapAndFile.class).withGroups("ldap", "ldap_and_file", "ldap_cli", "ldap_and_file_cli").build(),
+                testOnEnvironment(EnvSinglenodeLdapInsecure.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapReferrals.class).withGroups("ldap").build(),
+                testOnEnvironment(EnvSinglenodeLdapBindDn.class).withGroups("ldap").withExcludedGroups("ldap_multiple_binds").build());
     }
 }
