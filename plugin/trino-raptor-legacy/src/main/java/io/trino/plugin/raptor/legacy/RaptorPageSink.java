a01e6d987269df2c2f121cd7f572a121399c6651
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2021-10-20 13:38:21 +0200

    Collect directly to immutable collection
    In places where we collect to a collection and then wrap it in an
    immutable wrapper or (worse) copy into an immutable collection, we can
    just use the appropriate collector which produces an immutable
    collection directly.

--- plugin/trino-raptor-legacy/src/main/java/io/trino/plugin/raptor/legacy/RaptorPageSink.java
+++ plugin/trino-raptor-legacy/src/main/java/io/trino/plugin/raptor/legacy/RaptorPageSink.java
@@ -42,6 +42,7 @@ import java.util.OptionalInt;
 import java.util.concurrent.CompletableFuture;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.concurrent.MoreFutures.allAsList;
 import static io.airlift.json.JsonCodec.jsonCodec;
 import static io.trino.spi.type.DateType.DATE;
@@ -89,7 +90,7 @@ public class RaptorPageSink
         this.storageManager = requireNonNull(storageManager, "storageManager is null");
         this.maxBufferBytes = maxBufferSize.toBytes();
 
-        this.sortFields = ImmutableList.copyOf(sortColumnIds.stream().map(columnIds::indexOf).collect(toList()));
+        this.sortFields = sortColumnIds.stream().map(columnIds::indexOf).collect(toImmutableList());
         this.sortOrders = ImmutableList.copyOf(requireNonNull(sortOrders, "sortOrders is null"));
 
         this.bucketCount = bucketCount;
