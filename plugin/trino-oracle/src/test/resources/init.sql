17bb36574a8910a36cf9d44b18e0493c4eb33eb7
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2021-10-25 16:53:36 +0200

    Update testcontainers to 1.16.2
    This brings improvements around startup checks:
    
    - https://github.com/testcontainers/testcontainers-java/releases/tag/1.16.1
    - https://github.com/testcontainers/testcontainers-java/releases/tag/1.16.2

--- plugin/trino-oracle/src/test/resources/init.sql
+++ plugin/trino-oracle/src/test/resources/init.sql
@@ -1,2 +1,8 @@
-/* Drop apex_040000 because it has 601 tables and leads to slow tests */
-DROP USER apex_040000 CASCADE;
+/*
+this is added to allow more processes on database, otherwise the tests end up giving
+ORA-12519, TNS:no appropriate service handler found
+ORA-12505, TNS:listener does not currently know of SID given in connect descriptor
+to fix this we have to change the number of processes of SPFILE
+*/
+ALTER SYSTEM SET processes=1000 SCOPE=SPFILE;
+ALTER SYSTEM SET disk_asynch_io = FALSE SCOPE = SPFILE;
