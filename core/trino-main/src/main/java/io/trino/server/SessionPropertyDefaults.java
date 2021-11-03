7c717e5f90f34a66a03bd4cf666dc192db620f33
Arkadiusz Czajkowski <arek@starburstdata.com>
2021-11-03 12:01:14 +0100

    Inject AccessControl to SessionPropertyDefaults

--- core/trino-main/src/main/java/io/trino/server/SessionPropertyDefaults.java
+++ core/trino-main/src/main/java/io/trino/server/SessionPropertyDefaults.java
@@ -17,6 +17,7 @@ import com.google.common.annotations.VisibleForTesting;
 import io.airlift.log.Logger;
 import io.airlift.node.NodeInfo;
 import io.trino.Session;
+import io.trino.security.AccessControl;
 import io.trino.spi.classloader.ThreadContextClassLoader;
 import io.trino.spi.resourcegroups.ResourceGroupId;
 import io.trino.spi.resourcegroups.SessionPropertyConfigurationManagerContext;
@@ -38,6 +39,7 @@ import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static io.airlift.configuration.ConfigurationLoader.loadPropertiesFrom;
 import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
 
 public class SessionPropertyDefaults
 {
@@ -50,10 +52,13 @@ public class SessionPropertyDefaults
     private final Map<String, SessionPropertyConfigurationManagerFactory> factories = new ConcurrentHashMap<>();
     private final AtomicReference<SessionPropertyConfigurationManager> delegate = new AtomicReference<>();
 
+    private final AccessControl accessControl;
+
     @Inject
-    public SessionPropertyDefaults(NodeInfo nodeInfo)
+    public SessionPropertyDefaults(NodeInfo nodeInfo, AccessControl accessControl)
     {
         this.configurationManagerContext = new SessionPropertyConfigurationManagerContextInstance(nodeInfo.getEnvironment());
+        this.accessControl = requireNonNull(accessControl, "accessControl is null");
     }
 
     public void addConfigurationManagerFactory(SessionPropertyConfigurationManagerFactory sessionConfigFactory)
