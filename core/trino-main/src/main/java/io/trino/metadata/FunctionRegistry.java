shiroyuki/presto

Implement contains function for IP address

presto-main/src/main/java/io/prestosql/metadata/FunctionRegistry.java
@@ -109,6 +109,7 @@ import io.prestosql.operator.scalar.EmptyMapConstructor;
 import io.prestosql.operator.scalar.FailureFunction;
 import io.prestosql.operator.scalar.HmacFunctions;
 import io.prestosql.operator.scalar.HyperLogLogFunctions;
+import io.prestosql.operator.scalar.IpAddressFunctions;
 import io.prestosql.operator.scalar.JoniRegexpCasts;
 import io.prestosql.operator.scalar.JoniRegexpFunctions;
 import io.prestosql.operator.scalar.JoniRegexpReplaceLambdaFunction;
@@ -520,6 +521,7 @@ public class FunctionRegistry
                 .scalars(HyperLogLogOperators.class)
                 .scalars(QuantileDigestOperators.class)
                 .scalars(IpAddressOperators.class)
+                .scalars(IpAddressFunctions.class)
                 .scalar(IpAddressOperators.IpAddressDistinctFromOperator.class)
                 .scalars(UuidOperators.class)
                 .scalar(UuidOperators.UuidDistinctFromOperator.class)

trinodb/trino

Co-authored-by: pellicano <pellicano@gmail.com>

core/trino-main/src/main/java/io/trino/metadata/FunctionRegistry.java
@@ -126,6 +126,7 @@ import io.trino.operator.scalar.GenericLessThanOrEqualOperator;
 import io.trino.operator.scalar.GenericXxHash64Operator;
 import io.trino.operator.scalar.HmacFunctions;
 import io.trino.operator.scalar.HyperLogLogFunctions;
+import io.trino.operator.scalar.IpAddressFunctions;
 import io.trino.operator.scalar.JoniRegexpCasts;
 import io.trino.operator.scalar.JoniRegexpFunctions;
 import io.trino.operator.scalar.JoniRegexpReplaceLambdaFunction;
@@ -514,6 +515,7 @@ public class FunctionRegistry
                 .scalars(QuantileDigestOperators.class)
                 .scalars(TDigestOperators.class)
                 .scalars(IpAddressOperators.class)
+                .scalars(IpAddressFunctions.class)
                 .scalars(UuidOperators.class)
                 .scalars(LikeFunctions.class)
                 .scalars(ArrayFunctions.class)