56e7b8bc9618b99603d28102f5de3ef0ae2cfd74
Eric Hwang <eric.hwang@starburstdata.com>
2021-11-08 18:08:54 -0800

    Make event-listener SPI objects fully jackson serializable

--- core/trino-spi/src/main/java/io/trino/spi/eventlistener/QueryCreatedEvent.java
+++ core/trino-spi/src/main/java/io/trino/spi/eventlistener/QueryCreatedEvent.java
@@ -13,11 +13,17 @@
  */
 package io.trino.spi.eventlistener;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.Instant;
 import java.util.StringJoiner;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * This class is JSON serializable for convenience and serialization compatibility is not guaranteed across versions.
+ */
 public class QueryCreatedEvent
 {
     private final Instant createTime;
@@ -25,6 +31,7 @@ public class QueryCreatedEvent
     private final QueryContext context;
     private final QueryMetadata metadata;
 
+    @JsonCreator
     public QueryCreatedEvent(Instant createTime, QueryContext context, QueryMetadata metadata)
     {
         this.createTime = requireNonNull(createTime, "createTime is null");
@@ -32,16 +39,19 @@ public class QueryCreatedEvent
         this.metadata = requireNonNull(metadata, "metadata is null");
     }
 
+    @JsonProperty
     public Instant getCreateTime()
     {
         return createTime;
     }
 
+    @JsonProperty
     public QueryContext getContext()
     {
         return context;
     }
 
+    @JsonProperty
     public QueryMetadata getMetadata()
     {
         return metadata;
