ff2be9b5b8d48e16047893ed8897ce23f44230c2
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-11-26 15:34:24 +0100

    Revert "Only delete non-empty directories when dropping Hive schemas"
    This reverts commit a89d7c18af214c9f2fcbde1700266ca9e854fa10.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/SemiTransactionalHiveMetastore.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/SemiTransactionalHiveMetastore.java
@@ -41,11 +41,9 @@ import io.trino.plugin.hive.authentication.HiveIdentity;
 import io.trino.plugin.hive.security.SqlStandardAccessControlMetadataMetastore;
 import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ConnectorSession;
-import io.trino.spi.connector.SchemaNotFoundException;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.connector.TableNotFoundException;
 import io.trino.spi.predicate.TupleDomain;
-import io.trino.spi.security.ConnectorIdentity;
 import io.trino.spi.security.PrincipalType;
 import io.trino.spi.security.RoleGrant;
 import io.trino.spi.statistics.ColumnStatisticType;
@@ -368,38 +366,7 @@ public class SemiTransactionalHiveMetastore
 
     public synchronized void dropDatabase(HiveIdentity identity, String schemaName)
     {
-        HdfsContext context = new HdfsContext(
-                identity.getUsername()
-                        .map(ConnectorIdentity::ofUser)
-                        .orElseThrow(() -> new IllegalStateException("username is null")));
-
-        Optional<Path> location = delegate.getDatabase(schemaName)
-                .orElseThrow(() -> new SchemaNotFoundException(schemaName))
-                .getLocation()
-                .map(Path::new);
-
-        setExclusive((delegate, hdfsEnvironment) -> {
-            delegate.dropDatabase(identity, schemaName);
-
-            location.ifPresent(path -> {
-                try {
-                    FileSystem fs = hdfsEnvironment.getFileSystem(context, path);
-                    // If no files in schema directory, delete it
-                    if (!fs.listFiles(path, false).hasNext()) {
-                        log.debug("Deleting location of dropped schema (%s)", path);
-                        fs.delete(path, true);
-                    }
-                    else {
-                        log.info("Skipped deleting schema location with external files (%s)", path);
-                    }
-                }
-                catch (IOException e) {
-                    throw new TrinoException(
-                            HIVE_FILESYSTEM_ERROR,
-                            format("Error checking or deleting schema directory '%s'", path), e);
-                }
-            });
-        });
+        setExclusive((delegate, hdfsEnvironment) -> delegate.dropDatabase(identity, schemaName));
     }
 
     public synchronized void renameDatabase(HiveIdentity identity, String source, String target)
