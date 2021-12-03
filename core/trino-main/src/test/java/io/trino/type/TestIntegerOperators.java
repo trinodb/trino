ba51b86c0bc42e38b5a84ff8c164d8f31f2318e4
kasiafi <30203062+kasiafi@users.noreply.github.com>
2021-12-03 14:02:23 +0100

    Fix cast from integer to varchar
    Fail if the resulting string does not fit in the
    bounded length of the varchar type.

--- core/trino-main/src/test/java/io/trino/type/TestIntegerOperators.java
+++ core/trino-main/src/test/java/io/trino/type/TestIntegerOperators.java
@@ -213,7 +213,7 @@ public class TestIntegerOperators
         assertFunction("cast(INTEGER '17' as varchar)", VARCHAR, "17");
         assertFunction("cast(INTEGER '123' as varchar(3))", createVarcharType(3), "123");
         assertFunction("cast(INTEGER '123' as varchar(50))", createVarcharType(50), "123");
-        assertFunctionThrowsIncorrectly("cast(INTEGER '123' as varchar(2))", IllegalArgumentException.class, "Character count exceeds length limit 2.*");
+        assertInvalidCast("cast(INTEGER '123' as varchar(2))", "Value 123 cannot be represented as varchar(2)");
     }
 
     @Test
