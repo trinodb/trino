257f7805c9120fb9929c146d989a8296614119b4
Yuya Ebihara <yuya.ebihara@starburstdata.com>
2021-12-07 19:02:55 +0900

    Allow columns with ; in MemSQL

--- plugin/trino-memsql/src/test/java/io/trino/plugin/memsql/TestMemSqlConnectorTest.java
+++ plugin/trino-memsql/src/test/java/io/trino/plugin/memsql/TestMemSqlConnectorTest.java
@@ -30,7 +30,6 @@ import org.testng.annotations.Test;
 
 import java.util.Optional;
 
-import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static io.trino.plugin.memsql.MemSqlQueryRunner.createMemSqlQueryRunner;
 import static io.trino.spi.type.VarcharType.VARCHAR;
@@ -145,12 +144,6 @@ public class TestMemSqlConnectorTest
         }
 
         return Optional.of(dataMappingTestSetup);
-    }
-
-    @Override
-    protected boolean isColumnNameRejected(Exception exception, String columnName, boolean delimited)
-    {
-        return nullToEmpty(exception.getMessage()).matches(".*Incorrect column name.*");
     }
 
     @Override
