d291731c7240b8bc4e69912429ea37a941c32221
Yuya Ebihara <yuya.ebihara@starburstdata.com>
2021-12-10 10:52:45 +0900

    Restore SAP HANA connector test without connection pool

---
+++ plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/TestSapHanaConnectorTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package com.starburstdata.presto.plugin.saphana;
+
+import com.google.common.collect.ImmutableMap;
+import io.trino.testing.QueryRunner;
+import io.trino.tpch.TpchTable;
+
+import static com.starburstdata.presto.plugin.saphana.SapHanaQueryRunner.createSapHanaQueryRunner;
+
+public class TestSapHanaConnectorTest
+        extends BaseSapHanaConnectorTest
+{
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        server = closeAfterClass(TestingSapHanaServer.create());
+        return createSapHanaQueryRunner(
+                server,
+                ImmutableMap.<String, String>builder()
+                        .put("metadata.cache-ttl", "0m")
+                        .put("metadata.cache-missing", "false")
+                        .build(),
+                ImmutableMap.of(),
+                TpchTable.getTables());
+    }
+}
