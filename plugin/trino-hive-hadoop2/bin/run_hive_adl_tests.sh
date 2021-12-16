1d81b4f21a0eb63ce8fb375e2a34d11d6ade6831
Jan Was <jan.was@starburstdata.com>
2021-12-16 15:10:07 +0100

    Pass Maven test flags from CI to Hive tests

--- plugin/trino-hive-hadoop2/bin/run_hive_adl_tests.sh
+++ plugin/trino-hive-hadoop2/bin/run_hive_adl_tests.sh
@@ -26,7 +26,7 @@ stop_unnecessary_hadoop_services
 # run product tests
 pushd $PROJECT_ROOT
 set +e
-./mvnw -B -pl :trino-hive-hadoop2 test -P test-hive-hadoop2-adl \
+./mvnw ${MAVEN_TEST:--B} -pl :trino-hive-hadoop2 test -P test-hive-hadoop2-adl \
     -DHADOOP_USER_NAME=hive \
     -Dhive.hadoop2.metastoreHost=localhost \
     -Dhive.hadoop2.metastorePort=9083 \
--- plugin/trino-hive-hadoop2/bin/run_hive_tests.sh
+++ plugin/trino-hive-hadoop2/bin/run_hive_tests.sh
@@ -21,7 +21,7 @@ HADOOP_MASTER_IP=$(hadoop_master_ip)
 # run product tests
 pushd "${PROJECT_ROOT}"
 set +e
-./mvnw -B -pl :trino-hive-hadoop2 test -P test-hive-hadoop2 \
+./mvnw ${MAVEN_TEST:--B} -pl :trino-hive-hadoop2 test -P test-hive-hadoop2 \
     -DHADOOP_USER_NAME=hive \
     -Dhive.hadoop2.metastoreHost=localhost \
     -Dhive.hadoop2.metastorePort=9083 \
