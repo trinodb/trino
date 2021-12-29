d96547ff56d8bc1adeec726871549f063aa3f14b
Dain Sundstrom <dain@iq80.com>
2021-12-28 18:49:38 -0800

    Convert avg(REAL) aggregation to annotated function

--- core/trino-main/src/main/java/io/trino/metadata/SystemFunctionBundle.java
+++ core/trino-main/src/main/java/io/trino/metadata/SystemFunctionBundle.java
@@ -67,6 +67,7 @@ import io.trino.operator.aggregation.MinNAggregationFunction;
 import io.trino.operator.aggregation.QuantileDigestAggregationFunction.BigintQuantileDigestAggregationFunction;
 import io.trino.operator.aggregation.QuantileDigestAggregationFunction.DoubleQuantileDigestAggregationFunction;
 import io.trino.operator.aggregation.QuantileDigestAggregationFunction.RealQuantileDigestAggregationFunction;
+import io.trino.operator.aggregation.RealAverageAggregation;
 import io.trino.operator.aggregation.RealCorrelationAggregation;
 import io.trino.operator.aggregation.RealCovarianceAggregation;
 import io.trino.operator.aggregation.RealGeometricMeanAggregations;
@@ -267,7 +268,6 @@ import io.trino.type.setdigest.SetDigestOperators;
 
 import static io.trino.operator.aggregation.DecimalAverageAggregation.DECIMAL_AVERAGE_AGGREGATION;
 import static io.trino.operator.aggregation.DecimalSumAggregation.DECIMAL_SUM_AGGREGATION;
-import static io.trino.operator.aggregation.RealAverageAggregation.REAL_AVERAGE_AGGREGATION;
 import static io.trino.operator.aggregation.ReduceAggregationFunction.REDUCE_AGG;
 import static io.trino.operator.aggregation.arrayagg.ArrayAggregationFunction.ARRAY_AGG;
 import static io.trino.operator.scalar.ArrayConcatFunction.ARRAY_CONCAT_FUNCTION;
@@ -391,7 +391,7 @@ public final class SystemFunctionBundle
                 .aggregates(IntervalDayToSecondSumAggregation.class)
                 .aggregates(IntervalYearToMonthSumAggregation.class)
                 .aggregates(AverageAggregations.class)
-                .function(REAL_AVERAGE_AGGREGATION)
+                .aggregates(RealAverageAggregation.class)
                 .aggregates(IntervalDayToSecondAverageAggregation.class)
                 .aggregates(IntervalYearToMonthAverageAggregation.class)
                 .aggregates(GeometricMeanAggregations.class)
