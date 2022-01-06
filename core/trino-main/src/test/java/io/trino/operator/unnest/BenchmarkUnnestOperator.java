04327a38aa9346f7f0d203b9145f50d0da08aac5
Pratham Desai <prathamd94@gmail.com>
2022-01-05 21:21:17 -0500

    Refactor BenchmarkUnnestOperator
    * Reuse block generation code from BlockAssertions
    * Use one null ratio value as row nulls ratio isn't super useful
      in the benchmark matrix.
    * Make unnest type inputs more generic.
    
    Comparable changes from https://github.com/prestodb/presto/pull/13746
    
    Co-authored-by: Ying Su <yingsu@fb.com>

--- core/trino-main/src/test/java/io/trino/operator/unnest/BenchmarkUnnestOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/unnest/BenchmarkUnnestOperator.java
@@ -15,6 +15,7 @@ package io.trino.operator.unnest;
 
 import com.google.common.collect.ImmutableList;
 import io.airlift.units.DataSize;
+import io.trino.block.BlockAssertions;
 import io.trino.operator.DriverContext;
 import io.trino.operator.Operator;
 import io.trino.operator.OperatorFactory;
@@ -22,8 +23,6 @@ import io.trino.operator.TaskContext;
 import io.trino.operator.unnest.UnnestOperator.UnnestOperatorFactory;
 import io.trino.spi.Page;
 import io.trino.spi.block.Block;
-import io.trino.spi.block.BlockBuilder;
-import io.trino.spi.block.RowBlock;
 import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.MapType;
 import io.trino.spi.type.RowType;
@@ -31,8 +30,10 @@ import io.trino.spi.type.Type;
 import io.trino.sql.planner.plan.PlanNodeId;
 import io.trino.testing.TestingTaskContext;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
@@ -47,17 +48,15 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.airlift.units.DataSize.Unit.GIGABYTE;
 import static io.trino.SessionTestUtils.TEST_SESSION;
 import static io.trino.jmh.Benchmarks.benchmark;
+import static io.trino.operator.unnest.BenchmarkUnnestOperator.BenchmarkContext.produceBlock;
 import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static io.trino.type.InternalTypeManager.TESTING_TYPE_MANAGER;
@@ -67,44 +66,41 @@ import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static org.testng.Assert.assertEquals;
 
 @State(Scope.Thread)
-@OutputTimeUnit(TimeUnit.SECONDS)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Fork(3)
-@Warmup(iterations = 20, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 20, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 8, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 8, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.AverageTime)
 public class BenchmarkUnnestOperator
 {
     private static final int TOTAL_POSITIONS = 10000;
-
-    private static final String ARRAY_OF_VARCHAR = "array(varchar)";
-    private static final String MAP_STRING_TO_STRING = "map(varchar,varchar)";
-    private static final String ARRAY_OF_ROW_THREE_STRINGS = "array(row(varchar, varchar, varchar))";
 
     @State(Scope.Thread)
     public static class BenchmarkContext
     {
         @Param("varchar")
-        private String replicateType = "varchar";
+        private String replicateType = "bigint";
 
-        @Param({ARRAY_OF_VARCHAR, MAP_STRING_TO_STRING, ARRAY_OF_ROW_THREE_STRINGS})
-        private String nestedTypeOne = ARRAY_OF_ROW_THREE_STRINGS;
-
-        @Param({"NONE", ARRAY_OF_VARCHAR})
-        private String nestedTypeTwo = "NONE";
-
-        @Param({"0.0", "0.2"})
-        private double primitiveNullsRatioNestedOne;  // % of nulls in input primitive elements
+        @Param({
+                "array(varchar)",
+                "map(varchar,varchar)",
+                "array(row(varchar,varchar,varchar))",
+                "array(array(varchar))",
+                "array(varchar)|array(varchar)"
+        })
+        private String nestedType = "array(varchar)";
 
         @Param({"0.0", "0.05"})
-        private double rowNullsRatioNestedOne;    // % of nulls in row type elements
+        private float nullsRatio;
 
         @Param("1000")
         private int positionsPerPage = 1000;
 
-        @Param("50")
-        private int stringLengths = 50;    // max length of varchars
-
         @Param("300")
         private int nestedLengths = 300;   // max entries in one nested structure (array, map)
+
+        @Param("true")
+        private boolean withOrdinality;
 
         private ExecutorService executor;
         private ScheduledExecutorService scheduledExecutor;
@@ -117,48 +113,44 @@ public class BenchmarkUnnestOperator
             executor = newCachedThreadPool(daemonThreadsNamed(getClass().getSimpleName() + "-%s"));
             scheduledExecutor = newScheduledThreadPool(2, daemonThreadsNamed(getClass().getSimpleName() + "-scheduledExecutor-%s"));
 
-            InputGenerator inputGenerator = new InputGenerator(0, nestedLengths, stringLengths);
-
             ImmutableList.Builder<Type> typesBuilder = ImmutableList.builder();
-            ImmutableList.Builder<Integer> channelsBuilder = ImmutableList.builder();
+            ImmutableList.Builder<Type> replicatedTypesBuilder = ImmutableList.builder();
+            ImmutableList.Builder<Type> unnestTypesBuilder = ImmutableList.builder();
+            ImmutableList.Builder<Integer> replicatedChannelsBuilder = ImmutableList.builder();
+            ImmutableList.Builder<Integer> unnestChannelsBuilder = ImmutableList.builder();
 
-            Type replicateType = getType(this.replicateType).get();
-            typesBuilder.add(replicateType);
-            channelsBuilder.add(0);
-
-            Type nestedTypeOne = getType(this.nestedTypeOne).get();
-            typesBuilder.add(nestedTypeOne);
-            channelsBuilder.add(1);
-
-            if (!nestedTypeTwo.equals("NONE")) {
-                Type nestedTypeTwo = getType(this.nestedTypeTwo).get();
-                typesBuilder.add(nestedTypeTwo);
-                channelsBuilder.add(2);
+            String[] replicatedTypes = replicateType.split("\\|");
+            for (int i = 0; i < replicatedTypes.length; i++) {
+                Type replicateType = getType(replicatedTypes[i]);
+                typesBuilder.add(replicateType);
+                replicatedTypesBuilder.add(replicateType);
+                replicatedChannelsBuilder.add(i);
             }
 
-            List<Type> types = typesBuilder.build();
-            List<Integer> channels = channelsBuilder.build();
+            String[] unnestTypes = nestedType.split("\\|");
+            for (int i = 0; i < unnestTypes.length; i++) {
+                Type unnestType = getType(unnestTypes[i]);
+                typesBuilder.add(unnestType);
+                unnestTypesBuilder.add(unnestType);
+                unnestChannelsBuilder.add(i + replicatedTypes.length);
+            }
 
-            this.pages = createInputPages(positionsPerPage, typesBuilder.build(), inputGenerator,
-                    primitiveNullsRatioNestedOne, rowNullsRatioNestedOne);
+            this.pages = createInputPages(positionsPerPage, typesBuilder.build(), nullsRatio, nestedLengths);
 
             operatorFactory = new UnnestOperatorFactory(
                     0,
                     new PlanNodeId("test"),
-                    channels.subList(0, 1),
-                    types.subList(0, 1),
-                    channels.subList(1, channels.size()),
-                    types.subList(1, types.size()),
-                    true,
+                    replicatedChannelsBuilder.build(),
+                    replicatedTypesBuilder.build(),
+                    unnestChannelsBuilder.build(),
+                    unnestTypesBuilder.build(),
+                    withOrdinality,
                     false);
         }
 
-        public static Optional<Type> getType(String typeString)
+        public static Type getType(String typeString)
         {
-            if (typeString.equals("NONE")) {
-                return Optional.empty();
-            }
-            return Optional.of(TESTING_TYPE_MANAGER.fromSqlType(typeString));
+            return TESTING_TYPE_MANAGER.fromSqlType(typeString);
         }
 
         @TearDown
@@ -183,22 +175,41 @@ public class BenchmarkUnnestOperator
             return pages;
         }
 
-        private static List<Page> createInputPages(int positionsPerPage, List<Type> types, InputGenerator inputGenerator, double primitiveNullsRatioUnnest, double rowNullsRatioUnnest)
+        static List<Page> createInputPages(
+                int positionsPerPage,
+                List<Type> types,
+                float nullsRatio,
+                int maxNestedCardinality)
         {
             ImmutableList.Builder<Page> pages = ImmutableList.builder();
             int pageCount = TOTAL_POSITIONS / positionsPerPage;
 
             for (int i = 0; i < pageCount; i++) {
                 Block[] blocks = new Block[types.size()];
-                blocks[0] = inputGenerator.produceBlock(types.get(0), positionsPerPage, 0.0, 0.0);
-                blocks[1] = inputGenerator.produceBlock(types.get(1), positionsPerPage, primitiveNullsRatioUnnest, rowNullsRatioUnnest);
-                if (blocks.length == 3) {
-                    blocks[2] = inputGenerator.produceBlock(types.get(2), positionsPerPage, 0.0, 0.0);
+                // Replicate block
+                blocks[0] = produceBlock(types.get(0), positionsPerPage, 0.0f, maxNestedCardinality);
+                // nested blocks
+                for (int nestedColumn = 1; nestedColumn < blocks.length; nestedColumn++) {
+                    blocks[nestedColumn] = produceBlock(types.get(nestedColumn), positionsPerPage, nullsRatio, maxNestedCardinality);
                 }
+
                 pages.add(new Page(blocks));
             }
 
             return pages.build();
+        }
+
+        static Block produceBlock(
+                Type type,
+                int positionsCount,
+                float nullsRatio,
+                int maxNestedCardinality)
+        {
+            if (type instanceof ArrayType || type instanceof MapType) {
+                return BlockAssertions.createRandomBlockForNestedType(type, positionsCount, nullsRatio, maxNestedCardinality);
+            }
+
+            return BlockAssertions.createRandomBlockForType(type, positionsCount, nullsRatio);
         }
     }
 
@@ -233,177 +244,19 @@ public class BenchmarkUnnestOperator
         return outputPages.build();
     }
 
-    public static class InputGenerator
-    {
-        private static final int INT_UPPER_LIMIT = 1_000_000_000;
-        private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
-
-        private final Random random = new Random();
-
-        // lower and upper bounds on nested structures in the input
-        private final int minNestedCardinality;
-        private final int maxNestedCardinality;
-
-        // upper bound on length of strings generated for VARCHAR elements
-        private final int maxStringLength;
-
-        public InputGenerator(int minNestedCardinality, int maxNestedCardinality, int maxStringLength)
-        {
-            checkArgument(minNestedCardinality >= 0, "minNestedCardinality must be >= 0");
-            checkArgument(minNestedCardinality <= maxNestedCardinality, "minNestedCardinality must be <= maxNestedCardinality");
-            checkArgument(maxStringLength >= 0, "maxStringLength must be >= 0");
-            this.minNestedCardinality = minNestedCardinality;
-            this.maxNestedCardinality = maxNestedCardinality;
-            this.maxStringLength = maxStringLength;
-        }
-
-        /**
-         * Generates a block with {@code entries} positions given an input {@code type}.
-         * <p>
-         * {@code primitiveNullsRatio} indicates what percentage of primitive elements
-         * (VARCHAR or INT) should be null. Similarly, the value {@code rowNullsRatio}
-         * indicates the percentage for Row elements.
-         * <p>
-         * The null percentages are not propagated inside a MapBlock's keyBlock. Everything
-         * is always non-null inside the returned keyBlock.
-         * <p>
-         * The set S of valid input for {@code type} can be defined as:
-         * S = {VARCHAR, INTEGER, ROW(x), ARRAY(x), MAP(x, x)} where x belongs to S
-         */
-        public Block produceBlock(Type type, int entries, double primitiveNullsRatio, double rowNullsRatio)
-        {
-            if (type instanceof ArrayType) {
-                return produceArrayBlock((ArrayType) type, entries, primitiveNullsRatio, rowNullsRatio);
-            }
-            if (type instanceof MapType) {
-                return produceMapBlock((MapType) type, entries, primitiveNullsRatio, rowNullsRatio);
-            }
-            if (type instanceof RowType) {
-                return produceRowBlock((RowType) type, entries, primitiveNullsRatio, rowNullsRatio);
-            }
-            if (type == VARCHAR) {
-                return produceStringBlock(entries, primitiveNullsRatio);
-            }
-            if (type == INTEGER) {
-                return produceIntBlock(entries, primitiveNullsRatio);
-            }
-
-            throw new RuntimeException("not supported");
-        }
-
-        private Block produceStringBlock(int entries, double primitiveNullsRatio)
-        {
-            BlockBuilder builder = VARCHAR.createBlockBuilder(null, 100);
-
-            for (int i = 0; i < entries; i++) {
-                if (random.nextDouble() <= primitiveNullsRatio) {
-                    builder.appendNull();
-                }
-                else {
-                    VARCHAR.writeString(builder, generateRandomString(random, random.nextInt(maxStringLength)));
-                }
-            }
-
-            return builder.build();
-        }
-
-        private Block produceIntBlock(int entries, double primitiveNullsRatio)
-        {
-            BlockBuilder builder = INTEGER.createBlockBuilder(null, 100);
-
-            for (int i = 0; i < entries; i++) {
-                if (random.nextDouble() < primitiveNullsRatio) {
-                    builder.appendNull();
-                }
-                else {
-                    INTEGER.writeLong(builder, random.nextInt(INT_UPPER_LIMIT));
-                }
-            }
-
-            return builder.build();
-        }
-
-        private Block produceArrayBlock(ArrayType arrayType, int entries, double primitiveNullsRatio, double rowNullsRatio)
-        {
-            BlockBuilder builder = arrayType.createBlockBuilder(null, 100);
-            Type elementType = arrayType.getElementType();
-
-            for (int i = 0; i < entries; i++) {
-                int arrayLength = minNestedCardinality + random.nextInt(maxNestedCardinality - minNestedCardinality);
-                arrayType.writeObject(builder, produceBlock(elementType, arrayLength, primitiveNullsRatio, rowNullsRatio));
-            }
-            return builder.build();
-        }
-
-        private Block produceMapBlock(MapType mapType, int entries, double primitiveNullsRatio, double rowNullsRatio)
-        {
-            int[] mapLengths = new int[entries];
-            int[] offsets = new int[entries + 1];
-
-            int elementCount = 0;
-            for (int i = 0; i < entries; i++) {
-                mapLengths[i] = minNestedCardinality + random.nextInt(maxNestedCardinality - minNestedCardinality);
-                offsets[i + 1] = elementCount;
-                elementCount += mapLengths[i];
-            }
-
-            // No null map objects OR null keys
-            // This stops propagation of null percentages, but for benchmarks this is okay
-            Block keyBlock = produceBlock(mapType.getKeyType(), elementCount, 0.0, 0.0);
-            Block valueBlock = produceBlock(mapType.getValueType(), elementCount, primitiveNullsRatio, rowNullsRatio);
-
-            return mapType.createBlockFromKeyValue(Optional.empty(), offsets, keyBlock, valueBlock);
-        }
-
-        public Block produceRowBlock(RowType rowType, int entries, double primitiveNullsRatio, double rowNullsRatio)
-        {
-            boolean[] rowIsNull = new boolean[entries];
-            int nonNullCount = 0;
-            for (int i = 0; i < entries; i++) {
-                if (random.nextDouble() < rowNullsRatio) {
-                    rowIsNull[i] = true;
-                }
-                else {
-                    rowIsNull[i] = false;
-                    nonNullCount++;
-                }
-            }
-
-            int fieldCount = rowType.getTypeParameters().size();
-            Block[] fieldBlocks = new Block[fieldCount];
-
-            for (int i = 0; i < fieldCount; i++) {
-                fieldBlocks[i] = produceBlock(rowType.getTypeParameters().get(i), nonNullCount, primitiveNullsRatio, rowNullsRatio);
-            }
-
-            return RowBlock.fromFieldBlocks(entries, Optional.of(rowIsNull), fieldBlocks);
-        }
-
-        public static String generateRandomString(Random random, int length)
-        {
-            char[] chars = new char[length];
-            for (int i = 0; i < length; i++) {
-                chars[i] = LETTERS.charAt(random.nextInt(LETTERS.length()));
-            }
-            return new String(chars);
-        }
-    }
-
     @Test
     public void testBlocks()
     {
-        InputGenerator generator = new InputGenerator(0, 50, 50);
-
-        Block block = generator.produceBlock(new ArrayType(VARCHAR), 100, 0.1, 0.1);
+        Block block = produceBlock(new ArrayType(VARCHAR), 100, 0.1f, 50);
         assertEquals(block.getPositionCount(), 100);
 
-        block = generator.produceBlock(mapType(VARCHAR, INTEGER), 100, 0.1, 0.1);
+        block = produceBlock(mapType(VARCHAR, INTEGER), 100, 0.1f, 50);
         assertEquals(block.getPositionCount(), 100);
 
-        block = generator.produceBlock(RowType.anonymous(Arrays.asList(VARCHAR, VARCHAR)), 100, 0.1, 0.1);
+        block = produceBlock(RowType.anonymous(Arrays.asList(VARCHAR, VARCHAR)), 100, 0.1f, 50);
         assertEquals(block.getPositionCount(), 100);
 
-        block = generator.produceBlock(new ArrayType(RowType.anonymous(Arrays.asList(VARCHAR, VARCHAR, VARCHAR))), 100, 0.1, 0.1);
+        block = produceBlock(new ArrayType(RowType.anonymous(Arrays.asList(VARCHAR, VARCHAR, VARCHAR))), 100, 0.1f, 50);
         assertEquals(block.getPositionCount(), 100);
     }
 
