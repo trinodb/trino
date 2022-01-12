aea5f621ee520f85f8a609147e5b13580707f84f
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-01-12 09:31:05 +0100

    Document safe & unsafe Cache invalidations

--- core/trino-main/src/main/java/io/trino/type/BlockTypeOperators.java
+++ core/trino-main/src/main/java/io/trino/type/BlockTypeOperators.java
@@ -284,6 +284,8 @@ public final class BlockTypeOperators
     @Managed
     public void cacheReset()
     {
+        // Note: this may not invalidate ongoing loads (https://github.com/trinodb/trino/issues/10512, https://github.com/google/guava/issues/1881)
+        // This is acceptable, since this operation is invoked manually, and not relied upon for correctness.
         generatedBlockOperatorCache.invalidateAll();
     }
 }
--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/CachingDirectoryLister.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/CachingDirectoryLister.java
@@ -144,6 +144,8 @@ public class CachingDirectoryLister
     @Managed
     public void flushCache()
     {
+        // Note: this may not invalidate ongoing loads (https://github.com/trinodb/trino/issues/10512, https://github.com/google/guava/issues/1881)
+        // This is acceptable, since this operation is invoked manually, and not relied upon for correctness.
         cache.invalidateAll();
     }
 
