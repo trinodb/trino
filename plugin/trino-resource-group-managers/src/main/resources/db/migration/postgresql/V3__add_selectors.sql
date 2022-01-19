417c1d28126a56ed1e4e2bd215c478d4694aea7a
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2022-01-19 16:16:41 -0500

    Add PostgreSQL migration files for DB resource groups

---
+++ plugin/trino-resource-group-managers/src/main/resources/db/migration/postgresql/V3__add_selectors.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS selectors (
+     resource_group_id BIGINT NOT NULL,
+     priority BIGINT NOT NULL,
+     user_regex VARCHAR(512),
+     source_regex VARCHAR(512),
+     query_type VARCHAR(512),
+     client_tags VARCHAR(512),
+     selector_resource_estimate VARCHAR(1024),
+     FOREIGN KEY (resource_group_id) REFERENCES resource_groups (resource_group_id) ON DELETE CASCADE
+);
