37f13422b3d36116ffac828a0acd0ed25db8bbca
Yuya Ebihara <yuya.ebihara@starburstdata.com>
2022-01-26 15:14:08 +0900

    fixup! Print server version before and after executing query

--- plugin/trino-clickhouse/src/test/java/io/trino/plugin/clickhouse/TestingClickHouseServer.java
+++ plugin/trino-clickhouse/src/test/java/io/trino/plugin/clickhouse/TestingClickHouseServer.java
@@ -14,18 +14,26 @@
 package io.trino.plugin.clickhouse;
 
 import org.testcontainers.containers.ClickHouseContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.shaded.com.google.common.collect.Sets;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Set;
 
 import static java.lang.String.format;
-import static org.testcontainers.containers.ClickHouseContainer.HTTP_PORT;
 import static org.testcontainers.utility.MountableFile.forClasspathResource;
 
 public class TestingClickHouseServer
+        extends ClickHouseContainer
         implements Closeable
 {
     private static final DockerImageName CLICKHOUSE_IMAGE = DockerImageName.parse("yandex/clickhouse-server");
@@ -37,7 +45,8 @@ public class TestingClickHouseServer
     public static final DockerImageName ALTINITY_LATEST_IMAGE = ALTINITY_IMAGE.withTag("21.8.13.1.altinitystable");
     public static final DockerImageName ALTINITY_DEFAULT_IMAGE = ALTINITY_IMAGE.withTag("20.3.12-aes"); // EOL is 24 June 2022
 
-    private final ClickHouseContainer dockerContainer;
+    private static final SecureRandom RANDOM = new SecureRandom();
+    private int port;
 
     public TestingClickHouseServer()
     {
@@ -46,12 +55,37 @@ public class TestingClickHouseServer
 
     public TestingClickHouseServer(DockerImageName image)
     {
-        dockerContainer = (ClickHouseContainer) new ClickHouseContainer(image)
-                .withCopyFileToContainer(forClasspathResource("custom.xml"), "/etc/clickhouse-server/config.d/custom.xml")
-                .withExposedPorts(HTTP_PORT)
-                .withStartupAttempts(10);
+        super(image);
+        port = RANDOM.nextInt();
+        super.withCopyFileToContainer(forClasspathResource("custom.xml"), "/etc/clickhouse-server/config.d/custom.xml");
+        super.withStartupAttempts(10);
+        start();
+    }
 
-        dockerContainer.start();
+    private static Path generateConfig(int port)
+            throws IOException
+    {
+        File tempFile = File.createTempFile("custom-", ".xml");
+        String string = format("" +
+                "<?xml version=\"1.0\"?>\n" +
+                "<yandex>\n" +
+                "    <!-- To avoid \"failed to response\" error message during copy of TPCH tables -->\n" +
+                "    <keep_alive_timeout>10</keep_alive_timeout>\n" +
+                "    <http_port>%s</http_port>\n" +
+                "</yandex>\n", port);
+        Files.writeString(tempFile.toPath(), string);
+        return tempFile.toPath();
+    }
+
+    @Override
+    public GenericContainer<?> withExposedPorts(Integer... ports)
+    {
+        return super.withExposedPorts(9999);
+    }
+
+    @Override
+    public Set<Integer> getLivenessCheckPortNumbers() {
+        return Sets.newHashSet(new Integer[]{9999});
     }
 
     public void execute(String sql)
@@ -65,15 +99,15 @@ public class TestingClickHouseServer
         }
     }
 
+    @Override
     public String getJdbcUrl()
     {
-        return format("jdbc:clickhouse://%s:%s/", dockerContainer.getContainerIpAddress(),
-                dockerContainer.getMappedPort(HTTP_PORT));
+        return format("jdbc:clickhouse://%s:%s/", getContainerIpAddress(), getMappedPort(9999));
     }
 
     @Override
     public void close()
     {
-        dockerContainer.stop();
+        stop();
     }
 }
