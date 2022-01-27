trinodb/trino

commit 613bd2f3feb42cc4aedf637260a2bc6672f323d4

Extract method HttpRemoteTask#triggerUpdate

core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
@@ -359,8 +359,7 @@ public final class HttpRemoteTask
         updateSplitQueueSpace();
 
         if (needsUpdate) {
-            this.needsUpdate.set(true);
-            scheduleUpdate();
+            triggerUpdate();
         }
     }
 
@@ -372,16 +371,14 @@ public final class HttpRemoteTask
         }
 
         noMoreSplits.put(sourceId, true);
-        needsUpdate.set(true);
-        scheduleUpdate();
+        triggerUpdate();
     }
 
     @Override
     public synchronized void noMoreSplits(PlanNodeId sourceId, Lifespan lifespan)
     {
         if (pendingNoMoreSplitsForLifespan.put(sourceId, lifespan)) {
-            needsUpdate.set(true);
-            scheduleUpdate();
+            triggerUpdate();
         }
     }
 
@@ -394,8 +391,7 @@ public final class HttpRemoteTask
 
         if (newOutputBuffers.getVersion() > outputBuffers.get().getVersion()) {
             outputBuffers.set(newOutputBuffers);
-            needsUpdate.set(true);
-            scheduleUpdate();
+            triggerUpdate();
         }
     }
 
@@ -509,6 +505,13 @@ public final class HttpRemoteTask
     private void scheduleUpdate()
     {
         executor.execute(this::sendUpdate);
+    }
+
+    private synchronized void triggerUpdate()
+    {
+        // synchronized so that needsUpdate is not cleared in sendUpdate before actual request is sent
+        needsUpdate.set(true);
+        sendUpdate();
     }
 
     private synchronized void sendUpdate()

commit 5bbcdd77b48561e252c31e9e8b731fbbdf5a7678

Run HttpRemoteTask#sendUpdate in separate thread
Sending update is potentially expensive operation
therefore it should be run from separate thread.
This fixes bug introduced by
https://github.com/trinodb/trino/commit/613bd2f3feb42cc4aedf637260a2bc6672f323d4

core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
@@ -578,7 +578,7 @@ public final class HttpRemoteTask
     {
         // synchronized so that needsUpdate is not cleared in sendUpdate before actual request is sent
         needsUpdate.set(true);
-        sendUpdate();
+        scheduleUpdate();
     }
 
     private synchronized void sendUpdate()