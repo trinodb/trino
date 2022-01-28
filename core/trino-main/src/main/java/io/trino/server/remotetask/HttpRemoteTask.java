583fcabd51a3757bd4e4ccb4401ebc59e2b5dafc
Ritesh Varyani <riteshvaryani@gmail.com>
2022-01-27 18:13:30 -0800

    Run HttpRemoteTask#sendUpdate in separate thread
    Sending update is potentially expensive operation
    therefore it should be run from separate thread.
    This fixes bug introduced by
    https://github.com/trinodb/trino/commit/613bd2f3feb42cc4aedf637260a2bc6672f323d4

--- core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
+++ core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
@@ -578,7 +578,7 @@ public final class HttpRemoteTask
     {
         // synchronized so that needsUpdate is not cleared in sendUpdate before actual request is sent
         needsUpdate.set(true);
-        sendUpdate();
+        scheduleUpdate();
     }
 
     private synchronized void sendUpdate()
