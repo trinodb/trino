fed993ca65c73782f275ed612da7ca031e3895a8
Karol Sobczak <napewnotrafi@gmail.com>
2022-02-08 18:02:36 +0100

    Skip RemoveRedundantTableScanPredicate if predicate doesn't change

--- core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/RemoveRedundantTableScanPredicate.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/RemoveRedundantTableScanPredicate.java
@@ -25,22 +25,25 @@ import io.trino.spi.predicate.TupleDomain;
 import io.trino.spi.type.Type;
 import io.trino.sql.PlannerContext;
 import io.trino.sql.planner.DomainTranslator;
-import io.trino.sql.planner.PlanNodeIdAllocator;
+import io.trino.sql.planner.DomainTranslator.ExtractionResult;
 import io.trino.sql.planner.Symbol;
-import io.trino.sql.planner.SymbolAllocator;
 import io.trino.sql.planner.TypeAnalyzer;
+import io.trino.sql.planner.TypeProvider;
 import io.trino.sql.planner.iterative.Rule;
 import io.trino.sql.planner.plan.FilterNode;
-import io.trino.sql.planner.plan.PlanNode;
 import io.trino.sql.planner.plan.TableScanNode;
 import io.trino.sql.planner.plan.ValuesNode;
 import io.trino.sql.tree.Expression;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.trino.matching.Capture.newCapture;
+import static io.trino.spi.predicate.TupleDomain.intersect;
+import static io.trino.sql.ExpressionUtils.combineConjuncts;
+import static io.trino.sql.ExpressionUtils.extractConjuncts;
 import static io.trino.sql.ExpressionUtils.filterDeterministicConjuncts;
 import static io.trino.sql.ExpressionUtils.filterNonDeterministicConjuncts;
 import static io.trino.sql.planner.iterative.rule.PushPredicateIntoTableScan.createResultingPredicate;
@@ -48,7 +51,11 @@ import static io.trino.sql.planner.plan.Patterns.filter;
 import static io.trino.sql.planner.plan.Patterns.source;
 import static io.trino.sql.planner.plan.Patterns.tableScan;
 import static io.trino.sql.tree.BooleanLiteral.TRUE_LITERAL;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class RemoveRedundantTableScanPredicate
         implements Rule<FilterNode>
@@ -57,7 +64,9 @@ public class RemoveRedundantTableScanPredicate
 
     private static final Pattern<FilterNode> PATTERN =
             filter().with(source().matching(
-                    tableScan().capturedAs(TABLE_SCAN)));
+                    tableScan().capturedAs(TABLE_SCAN)
+                            // avoid extra computations if table scan doesn't have any enforced predicate
+                            .matching(node -> !node.getEnforcedConstraint().isAll())));
 
     private final PlannerContext plannerContext;
     private final TypeAnalyzer typeAnalyzer;
@@ -77,38 +86,22 @@ public class RemoveRedundantTableScanPredicate
     @Override
     public Result apply(FilterNode filterNode, Captures captures, Context context)
     {
-        TableScanNode tableScan = captures.get(TABLE_SCAN);
+        Session session = context.getSession();
+        TableScanNode node = captures.get(TABLE_SCAN);
+        Expression predicate = filterNode.getPredicate();
 
-        PlanNode rewritten = removeRedundantTableScanPredicate(
-                tableScan,
-                filterNode.getPredicate(),
-                context.getSession(),
-                context.getSymbolAllocator(),
-                context.getIdAllocator());
-
-        if (rewritten instanceof FilterNode
-                && Objects.equals(((FilterNode) rewritten).getPredicate(), filterNode.getPredicate())) {
-            return Result.empty();
-        }
-
-        return Result.ofPlanNode(rewritten);
-    }
-
-    private PlanNode removeRedundantTableScanPredicate(
-            TableScanNode node,
-            Expression predicate,
-            Session session,
-            SymbolAllocator symbolAllocator,
-            PlanNodeIdAllocator idAllocator)
-    {
         Expression deterministicPredicate = filterDeterministicConjuncts(plannerContext.getMetadata(), predicate);
         Expression nonDeterministicPredicate = filterNonDeterministicConjuncts(plannerContext.getMetadata(), predicate);
 
-        DomainTranslator.ExtractionResult decomposedPredicate = DomainTranslator.getExtractionResult(
-                plannerContext,
+        ExtractionResult decomposedPredicate = getFullyExtractedPredicates(
                 session,
                 deterministicPredicate,
-                symbolAllocator.getTypes());
+                context.getSymbolAllocator().getTypes());
+
+        if (decomposedPredicate.getTupleDomain().isAll()) {
+            // no conjunct could be fully converted to tuple domain
+            return Result.empty();
+        }
 
         TupleDomain<ColumnHandle> predicateDomain = decomposedPredicate.getTupleDomain()
                 .transformKeys(node.getAssignments()::get);
@@ -117,12 +110,12 @@ public class RemoveRedundantTableScanPredicate
             // TODO: DomainTranslator.fromPredicate can infer that the expression is "false" in some cases (TupleDomain.none()).
             // This should move to another rule that simplifies the filter using that logic and then rely on RemoveTrivialFilters
             // to turn the subtree into a Values node
-            return new ValuesNode(node.getId(), node.getOutputSymbols(), ImmutableList.of());
+            return Result.ofPlanNode(new ValuesNode(node.getId(), node.getOutputSymbols(), ImmutableList.of()));
         }
 
         if (node.getEnforcedConstraint().isNone()) {
             // table scans with none domain should be converted to ValuesNode
-            return new ValuesNode(node.getId(), node.getOutputSymbols(), ImmutableList.of());
+            return Result.ofPlanNode(new ValuesNode(node.getId(), node.getOutputSymbols(), ImmutableList.of()));
         }
 
         Map<ColumnHandle, Domain> enforcedColumnDomains = node.getEnforcedConstraint().getDomains().orElseThrow(); // is not NONE
@@ -137,20 +130,41 @@ public class RemoveRedundantTableScanPredicate
             return predicateColumnDomain.intersect(enforcedColumnDomain);
         });
 
+        if (unenforcedDomain.equals(predicateDomain)) {
+            // no change in filter predicate
+            return Result.empty();
+        }
+
         Map<ColumnHandle, Symbol> assignments = ImmutableBiMap.copyOf(node.getAssignments()).inverse();
         Expression resultingPredicate = createResultingPredicate(
                 plannerContext,
                 session,
-                symbolAllocator,
+                context.getSymbolAllocator(),
                 typeAnalyzer,
                 new DomainTranslator(plannerContext).toPredicate(session, unenforcedDomain.transformKeys(assignments::get)),
                 nonDeterministicPredicate,
                 decomposedPredicate.getRemainingExpression());
 
         if (!TRUE_LITERAL.equals(resultingPredicate)) {
-            return new FilterNode(idAllocator.getNextId(), node, resultingPredicate);
+            return Result.ofPlanNode(new FilterNode(context.getIdAllocator().getNextId(), node, resultingPredicate));
         }
 
-        return node;
+        return Result.ofPlanNode(node);
+    }
+
+    private ExtractionResult getFullyExtractedPredicates(Session session, Expression predicate, TypeProvider types)
+    {
+        Map<Boolean, List<ExtractionResult>> extractedPredicates = extractConjuncts(predicate).stream()
+                .map(conjunct -> DomainTranslator.getExtractionResult(plannerContext, session, conjunct, types))
+                .collect(groupingBy(result -> result.getRemainingExpression().equals(TRUE_LITERAL), toList()));
+        return new ExtractionResult(
+                intersect(extractedPredicates.getOrDefault(TRUE, ImmutableList.of()).stream()
+                        .map(ExtractionResult::getTupleDomain)
+                        .collect(toImmutableList())),
+                combineConjuncts(
+                        plannerContext.getMetadata(),
+                        extractedPredicates.getOrDefault(FALSE, ImmutableList.of()).stream()
+                                .map(ExtractionResult::getRemainingExpression)
+                                .collect(toImmutableList())));
     }
 }
