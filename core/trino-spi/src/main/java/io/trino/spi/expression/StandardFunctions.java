2655af686b3e58f743d742f9f72fdc4e3ab5a844
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-02-17 16:55:34 +0100

    Translate comparisons to connnector expression

--- core/trino-spi/src/main/java/io/trino/spi/expression/StandardFunctions.java
+++ core/trino-spi/src/main/java/io/trino/spi/expression/StandardFunctions.java
@@ -17,5 +17,13 @@ public final class StandardFunctions
 {
     private StandardFunctions() {}
 
+    public static final FunctionName EQUAL_OPERATOR_FUNCTION_NAME = new FunctionName("$equal");
+    public static final FunctionName NOT_EQUAL_OPERATOR_FUNCTION_NAME = new FunctionName("$not_equal");
+    public static final FunctionName LESS_THAN_OPERATOR_FUNCTION_NAME = new FunctionName("$less_than");
+    public static final FunctionName LESS_THAN_OR_EQUAL_OPERATOR_FUNCTION_NAME = new FunctionName("$less_than_or_equal");
+    public static final FunctionName GREATER_THAN_OPERATOR_FUNCTION_NAME = new FunctionName("$greater_than");
+    public static final FunctionName GREATER_THAN_OR_EQUAL_OPERATOR_FUNCTION_NAME = new FunctionName("$greater_than_or_equal");
+    public static final FunctionName IS_DISTINCT_FROM_OPERATOR_FUNCTION_NAME = new FunctionName("$is_distinct_from");
+
     public static final FunctionName LIKE_PATTERN_FUNCTION_NAME = new FunctionName("$like_pattern");
 }
