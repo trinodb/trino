2c6a169d938ef29fd482791530e5a25d5bd71955
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-02-18 11:32:01 +0100

    Compress large prepared statements in HTTP headers
    This allows working with larger prepared statements before hitting
    header size limits (on either server, or intermediate proxy).
    
    Since the client should not make assumption on any specific format of
    prepared statements in the headers, the compression is applied
    unconditionally.

--- core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
+++ core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
@@ -101,6 +101,7 @@ import io.trino.server.ExpressionSerialization.ExpressionSerializer;
 import io.trino.server.PluginManager.PluginsProvider;
 import io.trino.server.SliceSerialization.SliceDeserializer;
 import io.trino.server.SliceSerialization.SliceSerializer;
+import io.trino.server.protocol.PreparedStatementEncoder;
 import io.trino.server.remotetask.HttpLocationFactory;
 import io.trino.spi.PageIndexerFactory;
 import io.trino.spi.PageSorter;
@@ -207,6 +208,7 @@ public class ServerMainModule
             httpServerConfig.setAdminEnabled(false);
         });
 
+        binder.bind(PreparedStatementEncoder.class).in(Scopes.SINGLETON);
         binder.bind(HttpRequestSessionContextFactory.class).in(Scopes.SINGLETON);
         install(new InternalCommunicationModule());
 
