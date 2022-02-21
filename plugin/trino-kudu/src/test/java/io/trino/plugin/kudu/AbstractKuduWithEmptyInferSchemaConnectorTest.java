dd2f1ae76965542429589177b546f5f508d68336
Yuya Ebihara <yuya.ebihara@starburstdata.com>
2022-02-21 14:48:03 +0900

    Migrate Kudu to use BaseConnectorTest
    Merge TestKuduDistributedQueries into AbstractKuduConnectorTest.

--- plugin/trino-kudu/src/test/java/io/trino/plugin/kudu/AbstractKuduWithEmptyInferSchemaConnectorTest.java
+++ plugin/trino-kudu/src/test/java/io/trino/plugin/kudu/AbstractKuduWithEmptyInferSchemaConnectorTest.java
@@ -17,7 +17,9 @@ import org.testng.annotations.Test;
 
 import java.util.Optional;
 
-public abstract class AbstractKuduSmokeTestWithEmptyInferSchema
+import static io.trino.testing.sql.TestTable.randomTableSuffix;
+
+public abstract class AbstractKuduWithEmptyInferSchemaConnectorTest
         extends AbstractKuduConnectorTest
 {
     @Override
@@ -30,5 +32,23 @@ public abstract class AbstractKuduSmokeTestWithEmptyInferSchema
     public void testListingOfTableForDefaultSchema()
     {
         assertQuery("SHOW TABLES FROM default", "VALUES '$schemas'");
+    }
+
+    @Test
+    @Override
+    public void testDropNonEmptySchema()
+    {
+        // Set column and table properties in CREATE TABLE statement
+        String schemaName = "test_drop_non_empty_schema_" + randomTableSuffix();
+
+        try {
+            assertUpdate("CREATE SCHEMA " + schemaName);
+            assertUpdate("CREATE TABLE " + schemaName + ".t(x int WITH (primary_key=true)) WITH (partition_by_hash_columns=ARRAY['x'], partition_by_hash_buckets=2)");
+            assertQueryFails("DROP SCHEMA " + schemaName, ".*Cannot drop non-empty schema '\\Q" + schemaName + "\\E'");
+        }
+        finally {
+            assertUpdate("DROP TABLE IF EXISTS " + schemaName + ".t");
+            assertUpdate("DROP SCHEMA IF EXISTS " + schemaName);
+        }
     }
 }
