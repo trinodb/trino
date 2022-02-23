c5712aa977eb4e18314a3b5c7779b7f177c285c6
Andrii Rosa <andriyrosa@gmail.com>
2022-02-23 14:28:02 -0500

    Support decimal columns in Tpch statistics provider

--- plugin/trino-tpch/src/main/java/io/trino/plugin/tpch/TpchMetadata.java
+++ plugin/trino-tpch/src/main/java/io/trino/plugin/tpch/TpchMetadata.java
@@ -52,6 +52,7 @@ import io.trino.spi.statistics.DoubleRange;
 import io.trino.spi.statistics.Estimate;
 import io.trino.spi.statistics.TableStatistics;
 import io.trino.spi.statistics.TableStatisticsMetadata;
+import io.trino.spi.type.DecimalType;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.VarcharType;
 import io.trino.tpch.Distributions;
@@ -368,7 +369,7 @@ public class TpchMetadata
             if (columnType.equals(BIGINT) || columnType.equals(INTEGER) || columnType.equals(DATE)) {
                 return ((Number) value).longValue();
             }
-            if (columnType.equals(DOUBLE)) {
+            if (columnType.equals(DOUBLE) || columnType instanceof DecimalType) {
                 return ((Number) value).doubleValue();
             }
         }
