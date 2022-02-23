90ea68bad24f31017b9c6424432365e13c591945
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-02-23 20:59:05 +0100

    Pass tasks memory reservation as part of worker mermory information
    Extend MemoryInfo data structure passed from workers to coordinator
    via `v1/memory` resource with memory reservation for all tasks which
    are running as part of queries with task-level retries enabled.
    
    The extra information is needed for implementing task aware cluster
    out of memory killer.

--- core/trino-main/src/main/java/io/trino/memory/MemoryResource.java
+++ core/trino-main/src/main/java/io/trino/memory/MemoryResource.java
@@ -13,7 +13,12 @@
  */
 package io.trino.memory;
 
+import com.google.common.collect.ListMultimap;
+import io.trino.TaskMemoryInfo;
+import io.trino.execution.SqlTask;
+import io.trino.execution.SqlTaskManager;
 import io.trino.server.security.ResourceSecurity;
+import io.trino.spi.QueryId;
 import io.trino.spi.memory.MemoryPoolInfo;
 
 import javax.inject.Inject;
@@ -23,6 +28,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.util.List;
+
+import static com.google.common.collect.ImmutableListMultimap.toImmutableListMultimap;
+import static io.trino.SystemSessionProperties.getRetryPolicy;
+import static io.trino.operator.RetryPolicy.TASK;
 import static io.trino.server.security.ResourceSecurity.AccessType.INTERNAL_ONLY;
 import static java.util.Objects.requireNonNull;
 
@@ -33,11 +43,13 @@ import static java.util.Objects.requireNonNull;
 public class MemoryResource
 {
     private final LocalMemoryManager memoryManager;
+    private final SqlTaskManager taskManager;
 
     @Inject
-    public MemoryResource(LocalMemoryManager memoryManager)
+    public MemoryResource(LocalMemoryManager memoryManager, SqlTaskManager taskManager)
     {
         this.memoryManager = requireNonNull(memoryManager, "memoryManager is null");
+        this.taskManager = requireNonNull(taskManager, "taskManager is null");
     }
 
     @ResourceSecurity(INTERNAL_ONLY)
@@ -45,7 +57,26 @@ public class MemoryResource
     @Produces(MediaType.APPLICATION_JSON)
     public MemoryInfo getMemoryInfo()
     {
-        return memoryManager.getInfo();
+        return memoryManager.getInfo().withTasksMemoryInfo(buildTasksMemoryInfo());
+    }
+
+    private ListMultimap<QueryId, TaskMemoryInfo> buildTasksMemoryInfo()
+    {
+        List<SqlTask> tasks = taskManager.getAllTasks();
+        return tasks.stream()
+                .filter(task -> !task.getTaskState().isDone())
+                // we are providing task memory information only for queries which are run with task-level retries.
+                // task memory information is consumed by low memory killer and it does not make sense to kill individual tasks
+                // for queries which does not allow task retries.
+                .filter(task -> task.getTaskContext().map(context -> getRetryPolicy(context.getSession()) == TASK).orElse(false))
+                .collect(toImmutableListMultimap(
+                        task -> task.getTaskId().getQueryId(),
+                        task -> new TaskMemoryInfo(
+                                task.getTaskId(),
+                                task.getTaskContext()
+                                        .map(taskContext -> taskContext.getMemoryReservation().toBytes())
+                                        // task context may no longer be available if task completes
+                                        .orElse(0L))));
     }
 
     private Response toSuccessfulResponse(MemoryPoolInfo memoryInfo)
