265f5304cec0ad8d210832903e23840846f2575b
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-02-25 21:29:59 +0100

    Rename hash partition configuration property
    Remove "initial" from the name. It could be understood as if execution
    is adaptive. Presumably, "initial" meant that the value can be
    overridden by a session property. If that's the case, this is not
    necessary & not how we name properties.
    
    The new property name is aligned with session property
    (`hash_partition_count`).

--- core/trino-main/src/main/java/io/trino/execution/QueryManagerConfig.java
+++ core/trino-main/src/main/java/io/trino/execution/QueryManagerConfig.java
@@ -51,7 +51,7 @@ public class QueryManagerConfig
     private int maxConcurrentQueries = 1000;
     private int maxQueuedQueries = 5000;
 
-    private int initialHashPartitions = 100;
+    private int hashPartitionCount = 100;
     private Duration minQueryExpireAge = new Duration(15, TimeUnit.MINUTES);
     private int maxQueryHistory = 100;
     private int maxQueryLength = 1_000_000;
@@ -144,15 +144,17 @@ public class QueryManagerConfig
     }
 
     @Min(1)
-    public int getInitialHashPartitions()
+    public int getHashPartitionCount()
     {
-        return initialHashPartitions;
+        return hashPartitionCount;
     }
 
-    @Config("query.initial-hash-partitions")
-    public QueryManagerConfig setInitialHashPartitions(int initialHashPartitions)
+    @Config("query.hash-partition-count")
+    @LegacyConfig("query.initial-hash-partitions")
+    @ConfigDescription("Number of partitions for distributed joins and aggregations")
+    public QueryManagerConfig setHashPartitionCount(int hashPartitionCount)
     {
-        this.initialHashPartitions = initialHashPartitions;
+        this.hashPartitionCount = hashPartitionCount;
         return this;
     }
 
