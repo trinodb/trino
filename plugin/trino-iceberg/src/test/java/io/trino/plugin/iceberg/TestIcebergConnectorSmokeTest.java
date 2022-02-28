a60e7a9ebdb4bcaa301d2817a570360f79d94381
Arkadiusz Czajkowski <arek@starburstdata.com>
2022-02-28 13:31:53 +0100

    Extract base iceberg connector smoke test

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergConnectorSmokeTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergConnectorSmokeTest.java
@@ -13,74 +13,19 @@
  */
 package io.trino.plugin.iceberg;
 
-import io.trino.testing.BaseConnectorSmokeTest;
 import io.trino.testing.QueryRunner;
-import io.trino.testing.TestingConnectorBehavior;
-import org.testng.annotations.Test;
-
-import java.io.File;
 
 import static io.trino.plugin.iceberg.IcebergQueryRunner.createIcebergQueryRunner;
-import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 // Redundant over TestIcebergOrcConnectorTest, but exists to exercise BaseConnectorSmokeTest
 // Some features like materialized views may be supported by Iceberg only.
 public class TestIcebergConnectorSmokeTest
-        extends BaseConnectorSmokeTest
+        extends BaseIcebergConnectorSmokeTest
 {
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
         return createIcebergQueryRunner(REQUIRED_TPCH_TABLES);
-    }
-
-    @Override
-    protected boolean hasBehavior(TestingConnectorBehavior connectorBehavior)
-    {
-        switch (connectorBehavior) {
-            case SUPPORTS_TOPN_PUSHDOWN:
-                return false;
-
-            case SUPPORTS_CREATE_VIEW:
-                return true;
-
-            case SUPPORTS_CREATE_MATERIALIZED_VIEW:
-                return true;
-
-            case SUPPORTS_DELETE:
-                return true;
-            default:
-                return super.hasBehavior(connectorBehavior);
-        }
-    }
-
-    @Test
-    @Override
-    public void testRowLevelDelete()
-    {
-        // Deletes are covered AbstractTestIcebergConnectorTest
-        assertThatThrownBy(super::testRowLevelDelete)
-                .hasStackTraceContaining("This connector only supports delete where one or more identity-transformed partitions are deleted entirely");
-    }
-
-    @Test
-    @Override
-    public void testShowCreateTable()
-    {
-        File tempDir = getDistributedQueryRunner().getCoordinator().getBaseDataDir().toFile();
-        assertThat((String) computeScalar("SHOW CREATE TABLE region"))
-                .isEqualTo("" +
-                        "CREATE TABLE iceberg.tpch.region (\n" +
-                        "   regionkey bigint,\n" +
-                        "   name varchar,\n" +
-                        "   comment varchar\n" +
-                        ")\n" +
-                        "WITH (\n" +
-                        "   format = 'ORC',\n" +
-                        format("   location = '%s/iceberg_data/tpch/region'\n", tempDir) +
-                        ")");
     }
 }
