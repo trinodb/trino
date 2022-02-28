27cf25deda317dff295a07275319127a6f09adfe
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2022-02-28 11:27:45 +0100

    Reuse thread-local Zstd de/compressor in ResolvedFunction to QualifiedName conversions

--- core/trino-main/src/main/java/io/trino/metadata/ResolvedFunction.java
+++ core/trino-main/src/main/java/io/trino/metadata/ResolvedFunction.java
@@ -30,7 +30,10 @@ import io.trino.sql.tree.QualifiedName;
 import io.trino.type.TypeDeserializer;
 import io.trino.type.TypeSignatureDeserializer;
 import io.trino.type.TypeSignatureKeyDeserializer;
+import io.trino.util.ThreadLocalCompressorDecompressor;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -40,13 +43,14 @@ import java.util.function.Function;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.io.BaseEncoding.base32Hex;
+import static io.trino.metadata.ResolvedFunction.ResolvedFunctionDecoder.serialize;
 import static java.lang.Math.toIntExact;
+import static java.nio.ByteBuffer.allocate;
 import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
 public class ResolvedFunction
 {
-    private static final JsonCodec<ResolvedFunction> SERIALIZE_JSON_CODEC = new JsonCodecFactory().jsonCodec(ResolvedFunction.class);
     private static final String PREFIX = "@";
     private final BoundSignature signature;
     private final FunctionId functionId;
@@ -125,17 +129,7 @@ public class ResolvedFunction
 
     public QualifiedName toQualifiedName()
     {
-        byte[] json = SERIALIZE_JSON_CODEC.toJsonBytes(this);
-
-        // json can be large so use zstd to compress
-        ZstdCompressor compressor = new ZstdCompressor();
-        byte[] compressed = new byte[compressor.maxCompressedLength(json.length)];
-        int outputSize = compressor.compress(json, 0, json.length, compressed, 0, compressed.length);
-
-        // names are case insensitive, so use base32 instead of base64
-        String base32 = base32Hex().encode(compressed, 0, outputSize);
-        // add name so expressions are still readable
-        return QualifiedName.of(PREFIX + signature.getName() + PREFIX + base32);
+        return ResolvedFunctionDecoder.serialize(this);
     }
 
     public static String extractFunctionName(QualifiedName qualifiedName)
@@ -181,6 +175,9 @@ public class ResolvedFunction
 
     public static class ResolvedFunctionDecoder
     {
+        private static final JsonCodec<ResolvedFunction> SERIALIZE_JSON_CODEC = new JsonCodecFactory().jsonCodec(ResolvedFunction.class);
+        private static final ThreadLocalCompressorDecompressor COMPRESSOR_DECOMPRESSOR = new ThreadLocalCompressorDecompressor(ZstdCompressor::new, ZstdDecompressor::new);
+
         private final JsonCodec<ResolvedFunction> jsonCodec;
 
         public ResolvedFunctionDecoder(Function<TypeId, Type> typeLoader)
@@ -196,26 +193,42 @@ public class ResolvedFunction
 
         public Optional<ResolvedFunction> fromQualifiedName(QualifiedName qualifiedName)
         {
-            String data = qualifiedName.getSuffix();
-            if (!data.startsWith(PREFIX)) {
+            if (!qualifiedName.getSuffix().startsWith(PREFIX)) {
                 return Optional.empty();
             }
-            List<String> parts = Splitter.on(PREFIX).splitToList(data.subSequence(1, data.length()));
-            checkArgument(parts.size() == 2, "Expected encoded resolved function to contain two parts: %s", qualifiedName);
-            String name = parts.get(0);
 
+            return Optional.of(deserialize(qualifiedName));
+        }
+
+        private ResolvedFunction deserialize(QualifiedName qualifiedName)
+        {
+            String data = qualifiedName.getSuffix();
+            List<String> parts = Splitter.on(PREFIX).splitToList(data.substring(1));
+            checkArgument(parts.size() == 2, "Expected encoded resolved function to contain two parts: %s", qualifiedName);
             String base32 = parts.get(1);
             // name may have been lower cased, but base32 decoder requires upper case
             base32 = base32.toUpperCase(ENGLISH);
             byte[] compressed = base32Hex().decode(base32);
 
-            byte[] json = new byte[toIntExact(ZstdDecompressor.getDecompressedSize(compressed, 0, compressed.length))];
-            new ZstdDecompressor().decompress(compressed, 0, compressed.length, json, 0, json.length);
+            ByteBuffer decompressed = allocate(toIntExact(ZstdDecompressor.getDecompressedSize(compressed, 0, compressed.length)));
+            COMPRESSOR_DECOMPRESSOR.decompress(ByteBuffer.wrap(compressed), decompressed);
 
-            ResolvedFunction resolvedFunction = jsonCodec.fromJson(json);
-            checkArgument(resolvedFunction.getSignature().getName().equalsIgnoreCase(name),
-                    "Expected decoded function to have name %s, but name is %s", resolvedFunction.getSignature().getName(), name);
-            return Optional.of(resolvedFunction);
+            ResolvedFunction resolvedFunction = jsonCodec.fromJson(Arrays.copyOf(decompressed.array(), decompressed.position()));
+            checkArgument(resolvedFunction.getSignature().getName().equalsIgnoreCase(parts.get(0)),
+                    "Expected decoded function to have name %s, but name is %s", resolvedFunction.getSignature().getName(), parts.get(0));
+            return resolvedFunction;
+        }
+
+        private static QualifiedName serialize(ResolvedFunction function)
+        {
+            // json can be large so use zstd to compress
+            byte[] value = SERIALIZE_JSON_CODEC.toJsonBytes(function);
+            ByteBuffer compressed = allocate(COMPRESSOR_DECOMPRESSOR.maxCompressedLength(value.length));
+            COMPRESSOR_DECOMPRESSOR.compress(ByteBuffer.wrap(value), compressed);
+            // names are case insensitive, so use base32 instead of base64
+            String base32 = base32Hex().encode(compressed.array(), 0, compressed.position());
+            // add name so expressions are still readable
+            return QualifiedName.of(PREFIX + function.signature.getName() + PREFIX + base32);
         }
     }
 }
