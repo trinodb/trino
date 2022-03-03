692d86ab3241093e5ed0298a365242886e6d8499
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-03-03 23:24:39 +0100

    Use current name of the configuration property
    Was renamed in f85eaabaff57953713e43b006f602f14d9541fe0.

--- testing/trino-testing/src/main/java/io/trino/testing/FaultTolerantExecutionConnectorTestHelper.java
+++ testing/trino-testing/src/main/java/io/trino/testing/FaultTolerantExecutionConnectorTestHelper.java
@@ -25,7 +25,7 @@ public final class FaultTolerantExecutionConnectorTestHelper
     {
         return ImmutableMap.<String, String>builder()
                 .put("retry-policy", "TASK")
-                .put("query.initial-hash-partitions", "5")
+                .put("query.hash-partition-count", "5")
                 .put("fault-tolerant-execution-target-task-input-size", "10MB")
                 .put("fault-tolerant-execution-target-task-split-count", "4")
                 // to trigger spilling
