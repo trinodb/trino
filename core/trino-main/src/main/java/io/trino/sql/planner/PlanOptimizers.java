9f5b09ebb7515bd0f66696b049cd4b5121cc750e
Arijit Banerjee <arijit.banerjee@gmail.com>
2022-03-07 23:19:52 -0800

    fixing checkstyle issues

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.trino.sql.planner;
 
 import com.google.common.annotations.VisibleForTesting;
