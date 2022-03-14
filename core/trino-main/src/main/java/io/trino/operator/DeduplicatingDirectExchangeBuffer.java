349b3ce2d433b42fc468d3ab81cd119f30b26f92
Zebing Lin <linzebing1995@gmail.com>
2022-03-14 15:03:57 -0700

    Improve isBlocked logic in DeduplicatingDirectExchangeBuffer

--- core/trino-main/src/main/java/io/trino/operator/DeduplicatingDirectExchangeBuffer.java
+++ core/trino-main/src/main/java/io/trino/operator/DeduplicatingDirectExchangeBuffer.java
@@ -19,6 +19,7 @@ import com.google.common.collect.ListMultimap;
 import com.google.common.io.Closer;
 import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import io.airlift.log.Logger;
@@ -141,7 +142,14 @@ public class DeduplicatingDirectExchangeBuffer
         }
 
         if (!outputReady.isDone()) {
-            return nonCancellationPropagating(outputReady);
+            return nonCancellationPropagating(Futures.transformAsync(outputReady, ignored -> {
+                synchronized (this) {
+                    if (outputSource != null) {
+                        return outputSource.isBlocked();
+                    }
+                    return immediateVoidFuture();
+                }
+            }, directExecutor()));
         }
 
         checkState(outputSource != null, "outputSource is expected to be set");
@@ -821,7 +829,10 @@ public class DeduplicatingDirectExchangeBuffer
                 return immediateVoidFuture();
             }
             if (!exchangeSourceFuture.isDone()) {
-                return nonCancellationPropagating(asVoid(exchangeSourceFuture));
+                return nonCancellationPropagating(asVoid(Futures.transformAsync(
+                        exchangeSourceFuture,
+                        exchangeSource -> toListenableFuture(exchangeSource.isBlocked()),
+                        directExecutor())));
             }
             if (exchangeSource != null) {
                 CompletableFuture<Void> blocked = exchangeSource.isBlocked();
