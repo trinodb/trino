5e6b8e55b93e1cd8e824ce5e8c61692e94ec3fb1
brandboat <brandboat@gmail.com>
2022-03-16 00:18:55 +0800

    Fix incorrect error message
    After d649dca65b14c683814b3d1885118ccb93b1e7a3,
    toTrinoType was renamed to toColumnMapping

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/BaseJdbcClient.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/BaseJdbcClient.java
@@ -266,7 +266,7 @@ public abstract class BaseJdbcClient
                     UnsupportedTypeHandling unsupportedTypeHandling = getUnsupportedTypeHandling(session);
                     verify(
                             unsupportedTypeHandling == IGNORE,
-                            "Unsupported type handling is set to %s, but toTrinoType() returned empty for %s",
+                            "Unsupported type handling is set to %s, but toColumnMapping() returned empty for %s",
                             unsupportedTypeHandling,
                             typeHandle);
                 }
