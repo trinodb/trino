cbc5f0c2c71fab79882751bd83c58ccca5d513f5
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-03-18 11:22:55 +0100

    Reject unsupported type in Raptor, Cassandra properly
    - use `TrinoException` with `NOT_SUPPORTED`
    - use clear message

--- plugin/trino-raptor-legacy/src/test/java/io/trino/plugin/raptor/legacy/BaseRaptorConnectorTest.java
+++ plugin/trino-raptor-legacy/src/test/java/io/trino/plugin/raptor/legacy/BaseRaptorConnectorTest.java
@@ -93,7 +93,7 @@ public abstract class BaseRaptorConnectorTest
     public void testCharVarcharComparison()
     {
         assertThatThrownBy(super::testCharVarcharComparison)
-                .hasMessage("No storage type for type: char(3)");
+                .hasMessage("Unsupported type: char(3)");
     }
 
     @Test
