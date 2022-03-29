e5cfcc665e28ab2f96ad5793094c5db2d5abb694
Andrii Rosa <andriyrosa@gmail.com>
2022-03-29 16:08:28 -0400

    Refactor VerifyCommand
    Bind data source in Guice context so it can be reused for other Dao

--- service/trino-verifier/src/main/java/io/trino/verifier/VerifyCommand.java
+++ service/trino-verifier/src/main/java/io/trino/verifier/VerifyCommand.java
@@ -18,9 +18,15 @@ import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import com.google.inject.Binder;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import io.airlift.bootstrap.Bootstrap;
 import io.airlift.bootstrap.LifeCycleManager;
@@ -94,6 +100,7 @@ import static io.trino.verifier.QueryType.CREATE;
 import static io.trino.verifier.QueryType.MODIFY;
 import static io.trino.verifier.QueryType.READ;
 import static io.trino.verifier.VerifyCommand.VersionProvider;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static picocli.CommandLine.IVersionProvider;
@@ -126,6 +133,7 @@ public class VerifyCommand
 
         ImmutableList.Builder<Module> builder = ImmutableList.<Module>builder()
                 .add(new PrestoVerifierModule())
+                .add(new DataSourceModule(this))
                 .addAll(getAdditionalModules());
 
         Bootstrap app = new Bootstrap(builder.build());
@@ -147,9 +155,7 @@ public class VerifyCommand
             }
             Set<EventClient> eventClients = injector.getInstance(Key.get(new TypeLiteral<Set<EventClient>>() {}));
 
-            VerifierDao dao = Jdbi.create(getQueryDatabase(injector))
-                    .installPlugin(new SqlObjectPlugin())
-                    .onDemand(VerifierDao.class);
+            VerifierDao dao = injector.getInstance(VerifierDao.class);
 
             ImmutableList.Builder<QueryPair> queriesBuilder = ImmutableList.builder();
             for (String suite : config.getSuites()) {
@@ -498,6 +504,56 @@ public class VerifyCommand
         {
             String version = getClass().getPackage().getImplementationVersion();
             return new String[] {spec.name() + " " + firstNonNull(version, "(version unknown)")};
+        }
+    }
+
+    private static class DataSourceModule
+            implements Module
+    {
+        private final VerifyCommand command;
+
+        private DataSourceModule(VerifyCommand command)
+        {
+            this.command = requireNonNull(command, "command is null");
+        }
+
+        @Override
+        public void configure(Binder binder)
+        {
+            binder.bind(VerifierDao.class).toProvider(VerifierDaoProvider.class).in(Scopes.SINGLETON);
+        }
+
+        @Singleton
+        @Provides
+        public ConnectionFactory createConnectionFactory(Injector injector)
+        {
+            return command.getQueryDatabase(injector);
+        }
+
+        @Singleton
+        @Provides
+        public static Jdbi createJdbi(ConnectionFactory connectionFactory)
+        {
+            return Jdbi.create(connectionFactory)
+                    .installPlugin(new SqlObjectPlugin());
+        }
+    }
+
+    private static class VerifierDaoProvider
+            implements Provider<VerifierDao>
+    {
+        private final VerifierDao dao;
+
+        @Inject
+        public VerifierDaoProvider(Jdbi jdbi)
+        {
+            this.dao = jdbi.onDemand(VerifierDao.class);
+        }
+
+        @Override
+        public VerifierDao get()
+        {
+            return dao;
         }
     }
 }
