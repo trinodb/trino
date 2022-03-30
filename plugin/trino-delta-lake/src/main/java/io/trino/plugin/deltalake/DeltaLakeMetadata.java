2307ed93456da8f86019b5dc21e685abf9e50f21
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-03-30 12:57:42 +0200

    Force checkpoint in Delta's OPTIMIZE
    For optimal optimize result it is reasonable to create a checkpoint.

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -1272,7 +1272,6 @@ public class DeltaLakeMetadata
     {
         DeltaTableOptimizeHandle optimizeHandle = (DeltaTableOptimizeHandle) executeHandle.getProcedureHandle();
         long readVersion = optimizeHandle.getCurrentVersion().orElseThrow(() -> new IllegalArgumentException("currentVersion not set"));
-        Optional<Long> checkpointInterval = optimizeHandle.getMetadataEntry().getCheckpointInterval();
         String tableLocation = executeHandle.getTableLocation();
 
         // paths to be deleted
@@ -1322,6 +1321,7 @@ public class DeltaLakeMetadata
 
             transactionLogWriter.flush();
             writeCommitted = true;
+            Optional<Long> checkpointInterval = Optional.of(1L); // force checkpoint
             writeCheckpointIfNeeded(session, executeHandle.getSchemaTableName(), checkpointInterval, commitVersion);
         }
         catch (Exception e) {
