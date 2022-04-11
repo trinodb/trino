7146165af95269e1e7006e49294124824baf8181
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-04-11 13:00:50 +0200

    Take runtime node memory usage into account in node allocator

--- core/trino-main/src/test/java/io/trino/execution/scheduler/TestBinPackingNodeAllocator.java
+++ core/trino-main/src/test/java/io/trino/execution/scheduler/TestBinPackingNodeAllocator.java
@@ -20,6 +20,8 @@ import io.airlift.units.DataSize;
 import io.trino.Session;
 import io.trino.client.NodeVersion;
 import io.trino.connector.CatalogName;
+import io.trino.execution.StageId;
+import io.trino.execution.TaskId;
 import io.trino.memory.MemoryInfo;
 import io.trino.metadata.InMemoryNodeManager;
 import io.trino.metadata.InternalNode;
@@ -35,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
@@ -67,7 +70,9 @@ public class TestBinPackingNodeAllocator
     private static final List<CatalogName> ALL_CATALOGS = ImmutableList.of(CATALOG_1, CATALOG_2);
 
     private static final NodeRequirements REQ_32 = new NodeRequirements(Optional.empty(), Set.of(), DataSize.of(32, GIGABYTE));
+    private static final NodeRequirements REQ_20 = new NodeRequirements(Optional.empty(), Set.of(), DataSize.of(16, GIGABYTE));
     private static final NodeRequirements REQ_16 = new NodeRequirements(Optional.empty(), Set.of(), DataSize.of(16, GIGABYTE));
+    private static final NodeRequirements REQ_1 = new NodeRequirements(Optional.empty(), Set.of(), DataSize.of(1, GIGABYTE));
     private static final NodeRequirements REQ_NODE_1_32 = new NodeRequirements(Optional.empty(), Set.of(NODE_1_ADDRESS), DataSize.of(32, GIGABYTE));
     private static final NodeRequirements REQ_NODE_2_32 = new NodeRequirements(Optional.empty(), Set.of(NODE_2_ADDRESS), DataSize.of(32, GIGABYTE));
     private static final NodeRequirements REQ_CATALOG_1_32 = new NodeRequirements(Optional.of(CATALOG_1), Set.of(), DataSize.of(32, GIGABYTE));
@@ -76,24 +81,47 @@ public class TestBinPackingNodeAllocator
     private static final long TEST_TIMEOUT = BinPackingNodeAllocatorService.PROCESS_PENDING_ACQUIRES_DELAY_SECONDS * 1000 / 2;
 
     private BinPackingNodeAllocatorService nodeAllocatorService;
+    private ConcurrentHashMap<String, Optional<MemoryInfo>> workerMemoryInfos;
 
     private void setupNodeAllocatorService(InMemoryNodeManager nodeManager)
     {
         shutdownNodeAllocatorService(); // just in case
 
-        MemoryInfo memoryInfo = new MemoryInfo(4, new MemoryPoolInfo(DataSize.of(64, GIGABYTE).toBytes(), 0, 0, ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()));
-
-        Map<String, Optional<MemoryInfo>> workerMemoryInfos = ImmutableMap.of(
-                NODE_1.getNodeIdentifier(), Optional.of(memoryInfo),
-                NODE_2.getNodeIdentifier(), Optional.of(memoryInfo),
-                NODE_3.getNodeIdentifier(), Optional.of(memoryInfo),
-                NODE_4.getNodeIdentifier(), Optional.of(memoryInfo));
+        workerMemoryInfos = new ConcurrentHashMap<>();
+        MemoryInfo memoryInfo = buildWorkerMemoryInfo(DataSize.ofBytes(0), ImmutableMap.of());
+        workerMemoryInfos.put(NODE_1.getNodeIdentifier(), Optional.of(memoryInfo));
+        workerMemoryInfos.put(NODE_2.getNodeIdentifier(), Optional.of(memoryInfo));
+        workerMemoryInfos.put(NODE_3.getNodeIdentifier(), Optional.of(memoryInfo));
+        workerMemoryInfos.put(NODE_4.getNodeIdentifier(), Optional.of(memoryInfo));
 
         nodeAllocatorService = new BinPackingNodeAllocatorService(
                 nodeManager,
                 () -> workerMemoryInfos,
                 false);
         nodeAllocatorService.start();
+    }
+
+    private void updateWorkerUsedMemory(InternalNode node, DataSize usedMemory, Map<TaskId, DataSize> taskMemoryUsage)
+    {
+        workerMemoryInfos.put(node.getNodeIdentifier(), Optional.of(buildWorkerMemoryInfo(usedMemory, taskMemoryUsage)));
+    }
+
+    private MemoryInfo buildWorkerMemoryInfo(DataSize usedMemory, Map<TaskId, DataSize> taskMemoryUsage)
+    {
+        return new MemoryInfo(
+                4,
+                new MemoryPoolInfo(
+                        DataSize.of(64, GIGABYTE).toBytes(),
+                        usedMemory.toBytes(),
+                        0,
+                        ImmutableMap.of(),
+                        ImmutableMap.of(),
+                        ImmutableMap.of(),
+                        taskMemoryUsage.entrySet().stream()
+                                .collect(toImmutableMap(
+                                        entry -> entry.getKey().toString(),
+                                        entry -> entry.getValue().toBytes())),
+                        ImmutableMap.of()));
     }
 
     @AfterMethod(alwaysRun = true)
@@ -353,6 +381,166 @@ public class TestBinPackingNodeAllocator
             // pending acquisition should be unblocked
             assertEventually(() -> assertAcquired(acquire3));
         }
+    }
+
+    @Test(timeOut = TEST_TIMEOUT)
+    public void testAllocateNotEnoughRuntimeMemory()
+    {
+        InMemoryNodeManager nodeManager = testingNodeManager(basicNodesMap(NODE_1, NODE_2));
+        setupNodeAllocatorService(nodeManager);
+
+        try (NodeAllocator nodeAllocator = nodeAllocatorService.getNodeAllocator(SESSION)) {
+            // first allocation is fine
+            NodeAllocator.NodeLease acquire1 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire1, NODE_1);
+            acquire1.attachTaskId(taskId(1));
+
+            // bump memory usage on NODE_1
+            updateWorkerUsedMemory(NODE_1,
+                    DataSize.of(33, GIGABYTE),
+                    ImmutableMap.of(taskId(1), DataSize.of(33, GIGABYTE)));
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+
+            // second allocation of 32GB should go to another node
+            NodeAllocator.NodeLease acquire2 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire2, NODE_2);
+            acquire2.attachTaskId(taskId(2));
+
+            // third allocation of 32GB should also use NODE_2 as there is not enough runtime memory on NODE_1
+            // second allocation of 32GB should go to another node
+            NodeAllocator.NodeLease acquire3 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire3, NODE_2);
+            acquire3.attachTaskId(taskId(3));
+
+            // fourth allocation of 16 should fit on NODE_1
+            NodeAllocator.NodeLease acquire4 = nodeAllocator.acquire(REQ_16);
+            assertAcquired(acquire4, NODE_1);
+            acquire4.attachTaskId(taskId(4));
+
+            // fifth allocation of 16 should should no longer fit on NODE_1. There is 16GB unreserved but only 15GB taking runtime usage into account
+            NodeAllocator.NodeLease acquire5 = nodeAllocator.acquire(REQ_16);
+            assertNotAcquired(acquire5);
+
+            // even tiny allocations should not fit now
+            NodeAllocator.NodeLease acquire6 = nodeAllocator.acquire(REQ_1);
+            assertNotAcquired(acquire6);
+
+            // if memory usage decreases on NODE_1 the pending 16GB allocation should complete
+            updateWorkerUsedMemory(NODE_1,
+                    DataSize.of(32, GIGABYTE),
+                    ImmutableMap.of(taskId(1), DataSize.of(32, GIGABYTE)));
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+            nodeAllocatorService.processPendingAcquires();
+            assertAcquired(acquire5, NODE_1);
+            acquire5.attachTaskId(taskId(5));
+
+            //  acquire6 should still be pending
+            assertNotAcquired(acquire6);
+        }
+    }
+
+    @Test(timeOut = TEST_TIMEOUT)
+    public void testAllocateRuntimeMemoryDiscrepancies()
+    {
+        InMemoryNodeManager nodeManager = testingNodeManager(basicNodesMap(NODE_1));
+
+        setupNodeAllocatorService(nodeManager);
+        // test when global memory usage on node is greater than per task usage
+        try (NodeAllocator nodeAllocator = nodeAllocatorService.getNodeAllocator(SESSION)) {
+            // first allocation is fine
+            NodeAllocator.NodeLease acquire1 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire1, NODE_1);
+            acquire1.attachTaskId(taskId(1));
+
+            // bump memory usage on NODE_1; per-task usage is kept small
+            updateWorkerUsedMemory(NODE_1,
+                    DataSize.of(33, GIGABYTE),
+                    ImmutableMap.of(taskId(1), DataSize.of(4, GIGABYTE)));
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+
+            // global (greater) memory usage should take precedence
+            NodeAllocator.NodeLease acquire2 = nodeAllocator.acquire(REQ_32);
+            assertNotAcquired(acquire2);
+        }
+
+        setupNodeAllocatorService(nodeManager);
+        // test when global memory usage on node is smaller than per task usage
+        try (NodeAllocator nodeAllocator = nodeAllocatorService.getNodeAllocator(SESSION)) {
+            // first allocation is fine
+            NodeAllocator.NodeLease acquire1 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire1, NODE_1);
+            acquire1.attachTaskId(taskId(1));
+
+            // bump memory usage on NODE_1; per-task usage is 33GB and global is 4GB
+            updateWorkerUsedMemory(NODE_1,
+                    DataSize.of(4, GIGABYTE),
+                    ImmutableMap.of(taskId(1), DataSize.of(33, GIGABYTE)));
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+
+            // per-task (greater) memory usage should take precedence
+            NodeAllocator.NodeLease acquire2 = nodeAllocator.acquire(REQ_32);
+            assertNotAcquired(acquire2);
+        }
+
+        setupNodeAllocatorService(nodeManager);
+        // test when per-task memory usage not present at all
+        try (NodeAllocator nodeAllocator = nodeAllocatorService.getNodeAllocator(SESSION)) {
+            // first allocation is fine
+            NodeAllocator.NodeLease acquire1 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire1, NODE_1);
+            acquire1.attachTaskId(taskId(1));
+
+            // bump memory usage on NODE_1; per-task usage is 33GB and global is 4GB
+            updateWorkerUsedMemory(NODE_1, DataSize.of(33, GIGABYTE), ImmutableMap.of());
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+
+            // global memory usage should be used (not per-task usage)
+            NodeAllocator.NodeLease acquire2 = nodeAllocator.acquire(REQ_32);
+            assertNotAcquired(acquire2);
+        }
+    }
+
+    @Test(timeOut = TEST_TIMEOUT)
+    public void testSpaceReservedOnPrimaryNodeIfNoNodeWithEnoughRuntimeMemoryAvailable()
+    {
+        InMemoryNodeManager nodeManager = testingNodeManager(basicNodesMap(NODE_1, NODE_2));
+        setupNodeAllocatorService(nodeManager);
+
+        // test when global memory usage on node is greater than per task usage
+        try (NodeAllocator nodeAllocator = nodeAllocatorService.getNodeAllocator(SESSION)) {
+            // reserve 32GB on NODE_1 and 16GB on NODE_2
+            NodeAllocator.NodeLease acquire1 = nodeAllocator.acquire(REQ_32);
+            assertAcquired(acquire1, NODE_1);
+            acquire1.attachTaskId(taskId(1));
+            NodeAllocator.NodeLease acquire2 = nodeAllocator.acquire(REQ_16);
+            assertAcquired(acquire2, NODE_2);
+            acquire2.attachTaskId(taskId(2));
+
+            // make actual usage on NODE_2 greater than on NODE_1
+            updateWorkerUsedMemory(NODE_1,
+                    DataSize.of(40, GIGABYTE),
+                    ImmutableMap.of(taskId(1), DataSize.of(40, GIGABYTE)));
+            updateWorkerUsedMemory(NODE_2,
+                    DataSize.of(41, GIGABYTE),
+                    ImmutableMap.of(taskId(2), DataSize.of(41, GIGABYTE)));
+            nodeAllocatorService.refreshNodePoolMemoryInfos();
+
+            // try to allocate 32GB task
+            // it will not fit on neither of nodes. space should be reserved on NODE_2 as it has more memory available
+            // when you do not take runtime memory into account
+            NodeAllocator.NodeLease acquire3 = nodeAllocator.acquire(REQ_32);
+            assertNotAcquired(acquire3);
+
+            // to check that is the case try to allocate 20GB; NODE_1 should be picked
+            NodeAllocator.NodeLease acquire4 = nodeAllocator.acquire(REQ_20);
+            assertAcquired(acquire4, NODE_1);
+            acquire4.attachTaskId(taskId(2));
+        }
+    }
+
+    private TaskId taskId(int partition)
+    {
+        return new TaskId(new StageId("test_query", 0), partition, 0);
     }
 
     private InMemoryNodeManager testingNodeManager(Map<InternalNode, List<CatalogName>> nodeMap)
