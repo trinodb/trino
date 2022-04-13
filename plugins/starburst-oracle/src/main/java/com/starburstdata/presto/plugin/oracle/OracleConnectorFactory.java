f35d6df1cf0ae08fcb90eca2bfd6f44d552fbd97
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-04-13 17:24:04 +0200

    Update Plugins to use composition instead of extending JdbcConnectorFactory
    (cherry-picked from commit: 485c04c67503e4562f8180ffd560ec51017982b7)

--- plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/OracleConnectorFactory.java
+++ plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/OracleConnectorFactory.java
@@ -11,20 +11,39 @@ package com.starburstdata.presto.plugin.oracle;
 
 import com.starburstdata.presto.license.LicenseManager;
 import io.trino.plugin.jdbc.JdbcConnectorFactory;
+import io.trino.spi.connector.Connector;
+import io.trino.spi.connector.ConnectorContext;
+import io.trino.spi.connector.ConnectorFactory;
+
+import java.util.Map;
 
 import static io.airlift.configuration.ConfigurationAwareModule.combine;
 import static java.util.Objects.requireNonNull;
 
 public class OracleConnectorFactory
-        extends JdbcConnectorFactory
+        implements ConnectorFactory
 {
+    private final ConnectorFactory delegate;
+
     public OracleConnectorFactory(LicenseManager licenseManager)
     {
-        super("oracle", catalogName -> {
+        this.delegate = new JdbcConnectorFactory("oracle", catalogName -> {
             requireNonNull(licenseManager, "licenseManager is null");
             return combine(
                     binder -> binder.bind(LicenseManager.class).toInstance(licenseManager),
                     new OracleClientModule(catalogName, licenseManager));
         });
+    }
+
+    @Override
+    public String getName()
+    {
+        return this.delegate.getName();
+    }
+
+    @Override
+    public Connector create(String catalogName, Map<String, String> config, ConnectorContext context)
+    {
+        return this.delegate.create(catalogName, config, context);
     }
 }
