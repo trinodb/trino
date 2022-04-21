2bd48cc407b150c45655e664628eae80e0b1b728
Andrii Rosa <andriyrosa@gmail.com>
2022-04-20 23:22:18 -0400

    Improve task out of memory error message

--- core/trino-main/src/main/java/io/trino/execution/scheduler/FaultTolerantStageScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/FaultTolerantStageScheduler.java
@@ -98,6 +98,7 @@ import static io.trino.spi.ErrorType.USER_ERROR;
 import static io.trino.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static io.trino.spi.StandardErrorCode.REMOTE_HOST_GONE;
 import static io.trino.util.Failures.toFailure;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class FaultTolerantStageScheduler
@@ -637,7 +638,12 @@ public class FaultTolerantStageScheduler
                                 log.debug("Computed next memory requirements for task from stage %s; previous=%s; new=%s; peak=%s; estimator=%s", stage.getStageId(), memoryLimits, newMemoryLimits, taskStatus.getPeakMemoryReservation(), partitionMemoryEstimator);
 
                                 if (errorCode != null && isOutOfMemoryError(errorCode) && newMemoryLimits.getRequiredMemory().toBytes() * 0.99 <= taskStatus.getPeakMemoryReservation().toBytes()) {
-                                    failure = new TrinoException(() -> errorCode, "Cannot allocate enough memory for task", failureInfo.toException());
+                                    String message = format(
+                                            "Cannot allocate enough memory for task %s. Reported peak memory reservation: %s. Maximum possible reservation: %s.",
+                                            taskId,
+                                            taskStatus.getPeakMemoryReservation(),
+                                            newMemoryLimits.getRequiredMemory());
+                                    failure = new TrinoException(() -> errorCode, message, failureInfo.toException());
                                     break;
                                 }
 
