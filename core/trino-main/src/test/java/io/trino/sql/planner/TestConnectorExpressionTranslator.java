trinodb/trino

commit 808b677b510552c1f8808e826442aad13b81f03e

Translate all effective literals to connector expression

core/trino-main/src/main/java/io/trino/sql/planner/ConnectorExpressionTranslator.java
@@ -37,11 +37,13 @@ import io.trino.sql.analyzer.TypeSignatureProvider;
 import io.trino.sql.tree.AstVisitor;
 import io.trino.sql.tree.BinaryLiteral;
 import io.trino.sql.tree.BooleanLiteral;
+import io.trino.sql.tree.Cast;
 import io.trino.sql.tree.CharLiteral;
 import io.trino.sql.tree.DecimalLiteral;
 import io.trino.sql.tree.DoubleLiteral;
 import io.trino.sql.tree.Expression;
 import io.trino.sql.tree.FunctionCall;
+import io.trino.sql.tree.GenericLiteral;
 import io.trino.sql.tree.LikePredicate;
 import io.trino.sql.tree.LongLiteral;
 import io.trino.sql.tree.NodeRef;
@@ -62,6 +64,7 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.slice.SliceUtf8.countCodePoints;
 import static io.trino.SystemSessionProperties.isComplexExpressionPushdown;
 import static io.trino.spi.expression.StandardFunctions.LIKE_PATTERN_FUNCTION_NAME;
+import static io.trino.sql.ExpressionUtils.isEffectivelyLiteral;
 import static io.trino.sql.planner.ExpressionInterpreter.evaluateConstantExpression;
 import static java.util.Objects.requireNonNull;
 
@@ -245,10 +248,32 @@ public final class ConnectorExpressionTranslator
         }
 
         @Override
+        protected Optional<ConnectorExpression> visitGenericLiteral(GenericLiteral node, Void context)
+        {
+            if (!isComplexExpressionPushdown(session)) {
+                return Optional.empty();
+            }
+            return Optional.of(constantFor(node));
+        }
+
+        @Override
+        protected Optional<ConnectorExpression> visitCast(Cast node, Void context)
+        {
+            if (isEffectivelyLiteral(plannerContext, session, node)) {
+                return Optional.of(constantFor(node));
+            }
+            return Optional.empty();
+        }
+
+        @Override
         protected Optional<ConnectorExpression> visitFunctionCall(FunctionCall node, Void context)
         {
             if (!isComplexExpressionPushdown(session)) {
                 return Optional.empty();
+            }
+
+            if (isEffectivelyLiteral(plannerContext, session, node)) {
+                return Optional.of(constantFor(node));
             }
 
             if (node.getFilter().isPresent() || node.getOrderBy().isPresent() || node.getWindow().isPresent() || node.getNullTreatment().isPresent() || node.isDistinct()) {
@@ -283,6 +308,19 @@ public final class ConnectorExpressionTranslator
             // TODO Translate catalog/schema qualifier when available.
             FunctionName name = new FunctionName(functionName);
             return Optional.of(new Call(typeOf(node), name, arguments.build()));
+        }
+
+        private ConnectorExpression constantFor(Expression node)
+        {
+            Type type = typeOf(node);
+            Object value = evaluateConstantExpression(
+                    node,
+                    type,
+                    plannerContext,
+                    session,
+                    new AllowAllAccessControl(),
+                    ImmutableMap.of());
+            return new Constant(value, type);
         }
 
         @Override

core/trino-main/src/test/java/io/trino/sql/planner/TestConnectorExpressionTranslator.java
@@ -40,12 +40,18 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.spi.expression.StandardFunctions.LIKE_PATTERN_FUNCTION_NAME;
+import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.BooleanType.BOOLEAN;
+import static io.trino.spi.type.DecimalType.createDecimalType;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.IntegerType.INTEGER;
+import static io.trino.spi.type.RealType.REAL;
 import static io.trino.spi.type.RowType.field;
 import static io.trino.spi.type.RowType.rowType;
+import static io.trino.spi.type.SmallintType.SMALLINT;
+import static io.trino.spi.type.TinyintType.TINYINT;
 import static io.trino.spi.type.VarcharType.createVarcharType;
 import static io.trino.sql.planner.ConnectorExpressionTranslator.translate;
 import static io.trino.sql.planner.TestingPlannerContext.PLANNER_CONTEXT;
@@ -71,6 +77,31 @@ public class TestConnectorExpressionTranslator
     private static final TypeProvider TYPE_PROVIDER = TypeProvider.copyOf(symbols);
     private static final Map<String, Symbol> variableMappings = symbols.entrySet().stream()
             .collect(toImmutableMap(entry -> entry.getKey().getName(), Map.Entry::getKey));
+
+    @Test
+    public void testTranslateConstant()
+    {
+        testTranslateConstant(true, BOOLEAN);
+
+        testTranslateConstant(42L, TINYINT);
+        testTranslateConstant(42L, SMALLINT);
+        testTranslateConstant(42L, INTEGER);
+        testTranslateConstant(42L, BIGINT);
+
+        testTranslateConstant(42L, REAL);
+        testTranslateConstant(42d, DOUBLE);
+
+        testTranslateConstant(4200L, createDecimalType(4, 2));
+        testTranslateConstant(4200L, createDecimalType(8, 2));
+
+        testTranslateConstant(utf8Slice("abc"), createVarcharType(3));
+        testTranslateConstant(utf8Slice("abc"), createVarcharType(33));
+    }
+
+    private void testTranslateConstant(Object nativeValue, Type type)
+    {
+        assertTranslationRoundTrips(LITERAL_ENCODER.toExpression(TEST_SESSION, nativeValue, type), new Constant(nativeValue, type));
+    }
 
     @Test
     public void testTranslationToConnectorExpression()
@@ -131,6 +162,8 @@ public class TestConnectorExpressionTranslator
     @Test
     public void testTranslationFromConnectorExpression()
     {
+        // TODO convert to assertTranslationRoundTrips where possible
+
         assertTranslationFromConnectorExpression(new Variable("double_symbol_1", DOUBLE), new SymbolReference("double_symbol_1"));
 
         assertTranslationFromConnectorExpression(
@@ -160,6 +193,12 @@ public class TestConnectorExpressionTranslator
                         .setName(QualifiedName.of(("lower")))
                         .addArgument(VARCHAR_TYPE, new SymbolReference("varchar_symbol_1"))
                         .build());
+    }
+
+    private void assertTranslationRoundTrips(Expression expression, ConnectorExpression connectorExpression)
+    {
+        assertTranslationToConnectorExpression(TEST_SESSION, expression, Optional.of(connectorExpression));
+        assertTranslationFromConnectorExpression(connectorExpression, expression);
     }
 
     private void assertTranslationToConnectorExpression(Session session, Expression expression, Optional<ConnectorExpression> connectorExpression)

commit 8cb18ad0f5b28bc668bf134eea2453765af26f70

Fix translation of regular expressions to connector expressions
This got broken by 808b677b510552c1f8808e826442aad13b81f03e -- the
special handling of literal functions became unreachable.

core/trino-main/src/main/java/io/trino/sql/planner/ConnectorExpressionTranslator.java
@@ -65,6 +65,7 @@ import io.trino.sql.tree.SubscriptExpression;
 import io.trino.sql.tree.SymbolReference;
 import io.trino.type.JoniRegexp;
 import io.trino.type.Re2JRegexp;
+import io.trino.type.Re2JRegexpType;
 
 import java.util.List;
 import java.util.Map;
@@ -102,6 +103,7 @@ import static io.trino.sql.ExpressionUtils.isEffectivelyLiteral;
 import static io.trino.sql.analyzer.TypeSignatureTranslator.toSqlType;
 import static io.trino.sql.analyzer.TypeSignatureTranslator.toTypeSignature;
 import static io.trino.sql.planner.ExpressionInterpreter.evaluateConstantExpression;
+import static io.trino.type.JoniRegexpType.JONI_REGEXP;
 import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
 
@@ -629,19 +631,8 @@ public final class ConnectorExpressionTranslator
 
             String functionName = ResolvedFunction.extractFunctionName(node.getName());
             checkArgument(!DynamicFilters.Function.NAME.equals(functionName), "Dynamic filter has no meaning for a connector, it should not be translated into ConnectorExpression");
-
-            if (LiteralFunction.LITERAL_FUNCTION_NAME.equalsIgnoreCase(functionName)) {
-                Object value = evaluateConstant(node);
-                if (value instanceof JoniRegexp) {
-                    Slice pattern = ((JoniRegexp) value).pattern();
-                    return Optional.of(new Constant(pattern, createVarcharType(countCodePoints(pattern))));
-                }
-                if (value instanceof Re2JRegexp) {
-                    Slice pattern = Slices.utf8Slice(((Re2JRegexp) value).pattern());
-                    return Optional.of(new Constant(pattern, createVarcharType(countCodePoints(pattern))));
-                }
-                return Optional.of(new Constant(value, types.get(NodeRef.of(node))));
-            }
+            // literals should be handled by isEffectivelyLiteral case above
+            checkArgument(!LiteralFunction.LITERAL_FUNCTION_NAME.equalsIgnoreCase(functionName), "Unexpected literal function");
 
             ImmutableList.Builder<ConnectorExpression> arguments = ImmutableList.builder();
             for (Expression argumentExpression : node.getArguments()) {
@@ -700,6 +691,15 @@ public final class ConnectorExpressionTranslator
                     session,
                     new AllowAllAccessControl(),
                     ImmutableMap.of());
+
+            if (type == JONI_REGEXP) {
+                Slice pattern = ((JoniRegexp) value).pattern();
+                return new Constant(pattern, createVarcharType(countCodePoints(pattern)));
+            }
+            if (type instanceof Re2JRegexpType) {
+                Slice pattern = Slices.utf8Slice(((Re2JRegexp) value).pattern());
+                return new Constant(pattern, createVarcharType(countCodePoints(pattern)));
+            }
             return new Constant(value, type);
         }
 

core/trino-main/src/test/java/io/trino/sql/planner/TestConnectorExpressionTranslator.java
@@ -46,6 +46,7 @@ import io.trino.sql.tree.SubscriptExpression;
 import io.trino.sql.tree.SymbolReference;
 import io.trino.testing.TestingSession;
 import io.trino.transaction.TestingTransactionManager;
+import io.trino.type.JoniRegexpType;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -56,6 +57,7 @@ import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static io.airlift.slice.Slices.utf8Slice;
+import static io.trino.operator.scalar.JoniRegexpCasts.joniRegexp;
 import static io.trino.spi.expression.StandardFunctions.AND_FUNCTION_NAME;
 import static io.trino.spi.expression.StandardFunctions.CAST_FUNCTION_NAME;
 import static io.trino.spi.expression.StandardFunctions.GREATER_THAN_OR_EQUAL_OPERATOR_FUNCTION_NAME;
@@ -380,6 +382,31 @@ public class TestConnectorExpressionTranslator
                             new Call(VARCHAR_TYPE,
                                     new FunctionName("lower"),
                                     List.of(new Variable("varchar_symbol_1", VARCHAR_TYPE))));
+                });
+    }
+
+    @Test
+    public void testTranslateRegularExpression()
+    {
+        // Regular expression types (JoniRegexpType, Re2JRegexpType) are considered implementation detail of the engine
+        // and are not exposed to connectors within ConnectorExpression. Instead, they are replaced with a varchar pattern.
+
+        transaction(new TestingTransactionManager(), new AllowAllAccessControl())
+                .readOnly()
+                .execute(TEST_SESSION, transactionSession -> {
+                    assertTranslationToConnectorExpression(
+                            transactionSession,
+                            FunctionCallBuilder.resolve(TEST_SESSION, PLANNER_CONTEXT.getMetadata())
+                                    .setName(QualifiedName.of(("regexp_like")))
+                                    .addArgument(VARCHAR_TYPE, new SymbolReference("varchar_symbol_1"))
+                                    .addArgument(JoniRegexpType.JONI_REGEXP, LITERAL_ENCODER.toExpression(TEST_SESSION, joniRegexp(utf8Slice("a+")), JoniRegexpType.JONI_REGEXP))
+                                    .build(),
+                            new Call(
+                                    BOOLEAN,
+                                    new FunctionName("regexp_like"),
+                                    List.of(
+                                            new Variable("varchar_symbol_1", VARCHAR_TYPE),
+                                            new Constant(utf8Slice("a+"), createVarcharType(2)))));
                 });
     }
 