28e5adec74e3e434fab084507c1beae27c4c1e5e
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-04-22 18:18:58 +0200

    Add per task memory information in Web UI

--- core/trino-main/src/test/java/io/trino/execution/TestingRemoteTaskFactory.java
+++ core/trino-main/src/test/java/io/trino/execution/TestingRemoteTaskFactory.java
@@ -42,6 +42,7 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -73,6 +74,7 @@ public class TestingRemoteTaskFactory
             OutputBuffers outputBuffers,
             PartitionedSplitCountTracker partitionedSplitCountTracker,
             Set<DynamicFilterId> outboundDynamicFilterIds,
+            Optional<DataSize> estimatedMemory,
             boolean summarizeTaskInfo)
     {
         TestingRemoteTask task = new TestingRemoteTask(taskId, node.getNodeIdentifier(), fragment);
@@ -147,6 +149,7 @@ public class TestingRemoteTaskFactory
                             ImmutableList.of()),
                     ImmutableSet.copyOf(noMoreSplits),
                     new TaskStats(DateTime.now(), null),
+                    Optional.empty(),
                     false);
         }
 
