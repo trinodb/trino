65e6059d56fb38b812e45db689fb817df1975894
Jacob I. Komissar <jacob.komissar@starburstdata.com>
2022-04-27 14:39:41 -0400

    Add bulk insert support to SQL Server connector
    Co-Authored-By: Sasha Sheikin <myminitrue@gmail.com>

--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerConnectorTest.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerConnectorTest.java
@@ -13,11 +13,33 @@
  */
 package io.trino.plugin.sqlserver;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import io.trino.Session;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.sql.SqlExecutor;
+import io.trino.testing.sql.TestTable;
+import io.trino.testng.services.Flaky;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static io.trino.plugin.jdbc.JdbcWriteSessionProperties.NON_TRANSACTIONAL_INSERT;
+import static io.trino.plugin.sqlserver.SqlServerQueryRunner.CATALOG;
 import static io.trino.plugin.sqlserver.SqlServerQueryRunner.createSqlServerQueryRunner;
+import static io.trino.plugin.sqlserver.SqlServerSessionProperties.BULK_COPY_FOR_WRITE;
+import static io.trino.plugin.sqlserver.SqlServerSessionProperties.BULK_COPY_FOR_WRITE_LOCK_DESTINATION_TABLE;
+import static io.trino.testing.DataProviders.cartesianProduct;
+import static io.trino.testing.DataProviders.trueFalse;
+import static io.trino.testing.sql.TestTable.randomTableSuffix;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestSqlServerConnectorTest
         extends BaseSqlServerConnectorTest
@@ -36,5 +58,141 @@ public class TestSqlServerConnectorTest
     protected SqlExecutor onRemoteDatabase()
     {
         return sqlServer::execute;
+    }
+
+    @Flaky(issue = "fn_dblog() returns information only about the active portion of the transaction log, therefore it is flaky", match = ".*")
+    @Test(dataProvider = "doubleTrueFalse")
+    public void testCreateTableAsSelectWriteBulkiness(boolean bulkCopyForWrite, boolean bulkCopyLock)
+            throws SQLException
+    {
+        String table = "bulk_copy_ctas_" + randomTableSuffix();
+        Session session = Session.builder(getSession())
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE, Boolean.toString(bulkCopyForWrite))
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE_LOCK_DESTINATION_TABLE, Boolean.toString(bulkCopyLock))
+                .build();
+
+        // there should be enough rows in source table to minimal logging be enabled. `nation` table is too small.
+        assertQuerySucceeds(session, format("CREATE TABLE %s as SELECT * FROM tpch.tiny.customer", table));
+
+        // check whether minimal logging was applied.
+        // Unlike fully logged operations, which use the transaction log to keep track of every row change,
+        // minimally logged operations keep track of extent allocations and meta-data changes only.
+        assertThat(getTableOperationsCount("LOP_INSERT_ROWS", table))
+                .isEqualTo(bulkCopyForWrite && bulkCopyLock ? 0 : 1500);
+
+        // check that there are no locks remaining on the target table after bulk copy
+        assertQuery("SELECT count(*) FROM " + table, "SELECT count(*) FROM customer");
+        assertUpdate(format("INSERT INTO %s SELECT * FROM tpch.tiny.customer LIMIT 1", table), 1);
+        assertQuery("SELECT count(*) FROM " + table, "SELECT count(*) + 1 FROM customer");
+
+        assertUpdate("DROP TABLE " + table);
+    }
+
+    @Flaky(issue = "fn_dblog() returns information only about the active portion of the transaction log, therefore it is flaky", match = ".*")
+    @Test(dataProvider = "tripleTrueFalse")
+    public void testInsertWriteBulkiness(boolean nonTransactionalInsert, boolean bulkCopyForWrite, boolean bulkCopyForWriteLockDestinationTable)
+            throws SQLException
+    {
+        String table = "bulk_copy_insert_" + randomTableSuffix();
+        assertQuerySucceeds(format("CREATE TABLE %s as SELECT * FROM tpch.tiny.customer WHERE 0 = 1", table));
+        Session session = Session.builder(getSession())
+                .setCatalogSessionProperty(CATALOG, NON_TRANSACTIONAL_INSERT, Boolean.toString(nonTransactionalInsert))
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE, Boolean.toString(bulkCopyForWrite))
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE_LOCK_DESTINATION_TABLE, Boolean.toString(bulkCopyForWriteLockDestinationTable))
+                .build();
+
+        // there should be enough rows in source table to minimal logging be enabled. `nation` table is too small.
+        assertQuerySucceeds(session, format("INSERT INTO %s SELECT * FROM tpch.tiny.customer", table));
+
+        // check whether minimal logging was applied.
+        // Unlike fully logged operations, which use the transaction log to keep track of every row change,
+        // minimally logged operations keep track of extent allocations and meta-data changes only.
+        assertThat(getTableOperationsCount("LOP_INSERT_ROWS", table))
+                .isEqualTo(bulkCopyForWrite && bulkCopyForWriteLockDestinationTable ? 0 : 1500);
+
+        // check that there are no locks remaining on the target table after bulk copy
+        assertQuery("SELECT count(*) FROM " + table, "SELECT count(*) FROM customer");
+        assertUpdate(format("INSERT INTO %s SELECT * FROM tpch.tiny.customer LIMIT 1", table), 1);
+        assertQuery("SELECT count(*) FROM " + table, "SELECT count(*) + 1 FROM customer");
+
+        assertUpdate("DROP TABLE " + table);
+    }
+
+    @Test(dataProvider = "timestampTypes")
+    public void testInsertWriteBulkinessWithTimestamps(String timestampType)
+    {
+        Session session = Session.builder(getSession())
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE, "true")
+                .setCatalogSessionProperty(CATALOG, BULK_COPY_FOR_WRITE_LOCK_DESTINATION_TABLE, "true")
+                .build();
+
+        try (TestTable table = new TestTable((String sql) -> getQueryRunner().execute(session, sql), "bulk_copy_insert", format("(timestamp_col %s)", timestampType))) {
+            // Insert values without using TestTable to ensure all the rows are written in a single batch
+            List<String> timestampValues = ImmutableList.of(
+                    "TIMESTAMP '1958-01-01 13:18:03'",
+                    "TIMESTAMP '1958-01-01 13:18:03.1'",
+                    "TIMESTAMP '1958-01-01 13:18:03.123'",
+                    "TIMESTAMP '1958-01-01 13:18:03.123000'",
+                    "TIMESTAMP '1958-01-01 13:18:03.123000000'",
+                    "TIMESTAMP '1958-01-01 13:18:03.123000000000'",
+                    "TIMESTAMP '2019-03-18 10:01:17.987000'",
+                    "TIMESTAMP '2018-10-28 01:33:17.456000000'",
+                    "TIMESTAMP '1970-01-01 00:00:00.000000000'",
+                    "TIMESTAMP '1970-01-01 00:13:42.000000000'",
+                    "TIMESTAMP '2018-04-01 02:13:55.123000000'",
+                    "TIMESTAMP '1986-01-01 00:13:07.000000000000'");
+            String valuesList = timestampValues.stream().map(s -> format("(%s)", s)).collect(joining(","));
+            assertUpdate(format("INSERT INTO %s VALUES %s", table.getName(), valuesList), 12);
+
+            // check that there are no locks remaining on the target table after bulk copy
+            assertQuery("SELECT count(*) FROM " + table.getName(), "SELECT 12");
+            assertUpdate(format("INSERT INTO %s VALUES (TIMESTAMP '2022-01-01 00:13:07.0000000')", table.getName()), 1);
+            assertQuery("SELECT count(*) FROM " + table.getName(), "SELECT 13");
+        }
+    }
+
+    private int getTableOperationsCount(String operation, String table)
+            throws SQLException
+    {
+        try (Connection connection = sqlServer.createConnection();
+                Handle handle = Jdbi.open(connection)) {
+            // fn_dblog() function only returns information about the active portion of the transaction log such as open transactions or the last activity
+            // therefore tests which use this function are flaky, but it's almost not possible to reproduce this flakiness.
+            // There was no better option found to test if minimal logging was enabled than to query `LOP_INSERT_ROWS` from fn_dblog(NULL,NULL)
+            return handle.createQuery("" +
+                            "SELECT COUNT(*) as cnt " +
+                            "FROM fn_dblog(NULL,NULL) " +
+                            "WHERE Operation = :operation " +
+                            "AND AllocUnitName = CONCAT('dbo.', :table_name)")
+                    .bind("operation", operation)
+                    .bind("table_name", table)
+                    .mapTo(Integer.class)
+                    .one();
+        }
+    }
+
+    @DataProvider
+    public static Object[][] doubleTrueFalse()
+    {
+        return cartesianProduct(trueFalse(), trueFalse());
+    }
+
+    @DataProvider
+    public static Object[][] tripleTrueFalse()
+    {
+        return cartesianProduct(trueFalse(), trueFalse(), trueFalse());
+    }
+
+    @DataProvider
+    public static Object[][] timestampTypes()
+    {
+        // Timestamp with timezone is not supported by the SqlServer connector
+        return new Object[][] {
+                {"timestamp"},
+                {"timestamp(3)"},
+                {"timestamp(6)"},
+                {"timestamp(9)"},
+                {"timestamp(12)"}
+        };
     }
 }
