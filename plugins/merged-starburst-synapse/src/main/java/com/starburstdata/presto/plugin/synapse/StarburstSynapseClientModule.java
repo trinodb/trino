3c39a0ef89bbf9917a185d3c0e8296b39413347d
Jacob I. Komissar <jacob.komissar@starburstdata.com>
2022-05-05 16:54:25 -0400

    Convert JDBC bindings to default bindings so SEP can override them
    (cherry-picked from commit: 0ec6a8b713616dd936a274cb0fbea1bb224af35e)

--- plugins/merged-starburst-synapse/src/main/java/com/starburstdata/presto/plugin/synapse/StarburstSynapseClientModule.java
+++ plugins/merged-starburst-synapse/src/main/java/com/starburstdata/presto/plugin/synapse/StarburstSynapseClientModule.java
@@ -11,8 +11,15 @@ package com.starburstdata.presto.plugin.synapse;
 
 import com.google.inject.Binder;
 import com.google.inject.Key;
+import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.name.Names;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.trino.plugin.jdbc.BaseJdbcConfig;
+import io.trino.plugin.jdbc.ConnectionFactory;
+import io.trino.plugin.jdbc.DriverConnectionFactory;
 import io.trino.plugin.jdbc.ForBaseJdbc;
 import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcJoinPushdownSupportModule;
@@ -20,9 +27,13 @@ import io.trino.plugin.jdbc.JdbcRecordSetProvider;
 import io.trino.plugin.jdbc.JdbcSplitManager;
 import io.trino.plugin.jdbc.JdbcStatisticsConfig;
 import io.trino.plugin.jdbc.MaxDomainCompactionThreshold;
+import io.trino.plugin.jdbc.credential.CredentialProvider;
 import io.trino.plugin.sqlserver.SqlServerConfig;
+import io.trino.plugin.sqlserver.SqlServerConnectionFactory;
 import io.trino.spi.connector.ConnectorRecordSetProvider;
 import io.trino.spi.connector.ConnectorSplitManager;
+
+import javax.inject.Named;
 
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.configuration.ConfigBinder.configBinder;
@@ -33,7 +44,7 @@ public class StarburstSynapseClientModule
         extends AbstractConfigurationAwareModule
 {
     @Override
-    protected void setup(Binder binder)
+    protected final void setup(Binder binder)
     {
         configBinder(binder).bindConfig(SqlServerConfig.class);
         // The SNAPSHOT ISOLATION seems not supported by Synapse, but the docs (
@@ -41,16 +52,45 @@ public class StarburstSynapseClientModule
         // whether this is the expected behavior.
         configBinder(binder).bindConfigDefaults(SqlServerConfig.class, config -> config.setSnapshotIsolationDisabled(true));
 
-        binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(StarburstSynapseClient.class).in(Scopes.SINGLETON);
         newOptionalBinder(binder, Key.get(int.class, MaxDomainCompactionThreshold.class)).setBinding().toInstance(SQL_SERVER_MAX_LIST_EXPRESSIONS);
 
         configBinder(binder).bindConfig(JdbcStatisticsConfig.class);
 
         bindSessionPropertiesProvider(binder, StarburstSynapseSessionProperties.class);
 
-        binder.bind(ConnectorSplitManager.class).annotatedWith(ForBaseJdbc.class).to(JdbcSplitManager.class).in(Scopes.SINGLETON);
-        binder.bind(ConnectorRecordSetProvider.class).annotatedWith(ForBaseJdbc.class).to(JdbcRecordSetProvider.class).in(Scopes.SINGLETON);
-
         install(new JdbcJoinPushdownSupportModule());
+
+        newOptionalBinder(binder, Key.get(JdbcClient.class, ForBaseJdbc.class))
+                .setDefault()
+                .to(StarburstSynapseClient.class)
+                .in(Scopes.SINGLETON);
+
+        newOptionalBinder(binder, Key.get(ConnectorSplitManager.class, ForBaseJdbc.class))
+                .setDefault()
+                .to(JdbcSplitManager.class)
+                .in(Scopes.SINGLETON);
+
+        newOptionalBinder(binder, Key.get(ConnectorRecordSetProvider.class, ForBaseJdbc.class))
+                .setDefault()
+                .to(JdbcRecordSetProvider.class)
+                .in(Scopes.SINGLETON);
+
+        newOptionalBinder(binder, Key.get(ConnectionFactory.class, ForBaseJdbc.class))
+                .setDefault()
+                .to(Key.get(ConnectionFactory.class, Names.named("default synapse connection factory")))
+                .in(Scopes.SINGLETON);
+    }
+
+    @Provides
+    @Singleton
+    @Named("default synapse connection factory")
+    public static ConnectionFactory getConnectionFactory(
+            BaseJdbcConfig config,
+            SqlServerConfig sqlServerConfig,
+            CredentialProvider credentialProvider)
+    {
+        return new SqlServerConnectionFactory(
+                new DriverConnectionFactory(new SQLServerDriver(), config, credentialProvider),
+                sqlServerConfig.isSnapshotIsolationDisabled());
     }
 }
