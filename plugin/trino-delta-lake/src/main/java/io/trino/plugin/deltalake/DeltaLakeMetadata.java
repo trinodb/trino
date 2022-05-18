096ec7b149999637b4eec1244db17185bf0f6b40
Yuya Ebihara <ebyhry@gmail.com>
2022-05-18 16:36:40 +0900

    Support creating tables with table comment in Delta Lake

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -403,7 +403,7 @@ public class DeltaLakeMetadata
                 tableHandle.getSchemaTableName(),
                 columns,
                 properties.buildOrThrow(),
-                Optional.empty());
+                Optional.ofNullable(tableHandle.getMetadataEntry().getDescription()));
     }
 
     @Override
@@ -626,7 +626,8 @@ public class DeltaLakeMetadata
                         CREATE_TABLE_OPERATION,
                         session,
                         nodeVersion,
-                        nodeId);
+                        nodeId,
+                        tableMetadata.getComment());
 
                 setRollback(() -> deleteRecursivelyIfExists(new HdfsContext(session), hdfsEnvironment, deltaLogDirectory));
                 transactionLogWriter.flush();
@@ -735,7 +736,8 @@ public class DeltaLakeMetadata
                 tableMetadata.getColumns().stream().map(column -> toColumnHandle(column, partitionedBy)).collect(toImmutableList()),
                 location,
                 DeltaLakeTableProperties.getCheckpointInterval(tableMetadata.getProperties()),
-                external);
+                external,
+                tableMetadata.getComment());
     }
 
     private Optional<String> getSchemaLocation(Database database)
@@ -885,7 +887,8 @@ public class DeltaLakeMetadata
                     CREATE_TABLE_AS_OPERATION,
                     session,
                     nodeVersion,
-                    nodeId);
+                    nodeId,
+                    handle.getComment());
             appendAddFileEntries(transactionLogWriter, dataFileInfos, handle.getPartitionedBy(), true);
             transactionLogWriter.flush();
             PrincipalPrivileges principalPrivileges = buildInitialPrivilegeSet(table.getOwner().orElseThrow());
@@ -960,7 +963,8 @@ public class DeltaLakeMetadata
                     ADD_COLUMN_OPERATION,
                     session,
                     nodeVersion,
-                    nodeId);
+                    nodeId,
+                    Optional.ofNullable(handle.getMetadataEntry().getDescription()));
             transactionLogWriter.flush();
         }
         catch (Exception e) {
@@ -978,7 +982,8 @@ public class DeltaLakeMetadata
             String operation,
             ConnectorSession session,
             String nodeVersion,
-            String nodeId)
+            String nodeId,
+            Optional<String> comment)
     {
         long createdTime = System.currentTimeMillis();
         transactionLogWriter.appendCommitInfoEntry(
@@ -1002,7 +1007,7 @@ public class DeltaLakeMetadata
                 new MetadataEntry(
                         tableId,
                         null,
-                        null,
+                        comment.orElse(null),
                         new Format("parquet", ImmutableMap.of()),
                         serializeSchemaAsJson(columns),
                         partitionColumnNames,
