a39c854c5897cc3fd94975373cf513571a94e28a
automation-starburstdata <automation-starburstdata@users.noreply.github.com>
2022-06-01 02:23:43 +0000

    Update Trino version to 382-94-g12dda626
    - Fix checkCanExecuteFunction
    - Add workaround for AddColumnWithComment expected exception message
    - Fix SUPPORTS_CREATE_TABLE_WITH_(TABLE|COLUMN)_COMMENT
    - Fix unbiding HiveConfig from iceberg

--- plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/BaseSapHanaConnectorTest.java
+++ plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/BaseSapHanaConnectorTest.java
@@ -47,6 +47,8 @@ public abstract class BaseSapHanaConnectorTest
             case SUPPORTS_COMMENT_ON_TABLE:
             case SUPPORTS_COMMENT_ON_COLUMN:
             case SUPPORTS_ADD_COLUMN_WITH_COMMENT:
+            case SUPPORTS_CREATE_TABLE_WITH_TABLE_COMMENT:
+            case SUPPORTS_CREATE_TABLE_WITH_COLUMN_COMMENT:
                 return false;
 
             case SUPPORTS_RENAME_SCHEMA:
