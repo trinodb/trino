7bba76bbe127325056490e4b8c0aecb65f2b027c
Dain Sundstrom <dain@iq80.com>
2022-06-11 13:52:44 -0700

    Add ThriftMetastoreFactory

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakePerTransactionMetastoreCache.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakePerTransactionMetastoreCache.java
@@ -30,10 +30,11 @@ import io.trino.plugin.hive.metastore.thrift.MetastoreLocator;
 import io.trino.plugin.hive.metastore.thrift.StaticMetastoreConfig;
 import io.trino.plugin.hive.metastore.thrift.StaticMetastoreLocator;
 import io.trino.plugin.hive.metastore.thrift.ThriftHiveMetastore;
-import io.trino.plugin.hive.metastore.thrift.ThriftMetastore;
+import io.trino.plugin.hive.metastore.thrift.ThriftHiveMetastoreFactory;
 import io.trino.plugin.hive.metastore.thrift.ThriftMetastoreAuthenticationModule;
 import io.trino.plugin.hive.metastore.thrift.ThriftMetastoreClientFactory;
 import io.trino.plugin.hive.metastore.thrift.ThriftMetastoreConfig;
+import io.trino.plugin.hive.metastore.thrift.ThriftMetastoreFactory;
 import io.trino.plugin.hive.metastore.thrift.TokenDelegationThriftMetastoreFactory;
 import io.trino.spi.security.ConnectorIdentity;
 import io.trino.testing.DistributedQueryRunner;
@@ -109,8 +110,9 @@ public class TestDeltaLakePerTransactionMetastoreCache
                         configBinder(binder).bindConfig(StaticMetastoreConfig.class);
                         configBinder(binder).bindConfig(ThriftMetastoreConfig.class);
                         binder.bind(TokenDelegationThriftMetastoreFactory.class);
-                        binder.bind(ThriftMetastore.class).to(ThriftHiveMetastore.class).in(Scopes.SINGLETON);
-                        newExporter(binder).export(ThriftMetastore.class).as((generator) -> generator.generatedNameOf(ThriftHiveMetastore.class));
+                        binder.bind(ThriftMetastoreFactory.class).to(ThriftHiveMetastoreFactory.class).in(Scopes.SINGLETON);
+                        newExporter(binder).export(ThriftMetastoreFactory.class)
+                                .as(generator -> generator.generatedNameOf(ThriftHiveMetastore.class));
                         install(new ThriftMetastoreAuthenticationModule());
                         binder.bind(BridgingHiveMetastoreFactory.class).in(Scopes.SINGLETON);
                     }
---
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/ThriftHiveMetastoreFactory.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.hive.metastore.thrift;
+
+import io.airlift.units.Duration;
+import io.trino.plugin.hive.HdfsEnvironment;
+import io.trino.plugin.hive.HideDeltaLakeTables;
+import org.weakref.jmx.Flatten;
+import org.weakref.jmx.Managed;
+
+import javax.inject.Inject;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+public class ThriftHiveMetastoreFactory
+        implements ThriftMetastoreFactory
+{
+    private final HdfsEnvironment hdfsEnvironment;
+    private final TokenDelegationThriftMetastoreFactory metastoreFactory;
+    private final double backoffScaleFactor;
+    private final Duration minBackoffDelay;
+    private final Duration maxBackoffDelay;
+    private final Duration maxRetryTime;
+    private final Duration maxWaitForLock;
+    private final int maxRetries;
+    private final boolean impersonationEnabled;
+    private final boolean deleteFilesOnDrop;
+    private final boolean translateHiveViews;
+    private final boolean assumeCanonicalPartitionKeys;
+    private final ThriftMetastoreStats stats = new ThriftMetastoreStats();
+
+    @Inject
+    public ThriftHiveMetastoreFactory(
+            TokenDelegationThriftMetastoreFactory metastoreFactory,
+            @HideDeltaLakeTables boolean hideDeltaLakeTables,
+            @TranslateHiveViews boolean translateHiveViews,
+            ThriftMetastoreConfig thriftConfig,
+            HdfsEnvironment hdfsEnvironment)
+    {
+        this.metastoreFactory = requireNonNull(metastoreFactory, "metastoreFactory is null");
+        this.hdfsEnvironment = requireNonNull(hdfsEnvironment, "hdfsEnvironment is null");
+        this.backoffScaleFactor = thriftConfig.getBackoffScaleFactor();
+        this.minBackoffDelay = thriftConfig.getMinBackoffDelay();
+        this.maxBackoffDelay = thriftConfig.getMaxBackoffDelay();
+        this.maxRetryTime = thriftConfig.getMaxRetryTime();
+        this.maxRetries = thriftConfig.getMaxRetries();
+        this.impersonationEnabled = thriftConfig.isImpersonationEnabled();
+        this.deleteFilesOnDrop = thriftConfig.isDeleteFilesOnDrop();
+        this.translateHiveViews = translateHiveViews;
+        checkArgument(!hideDeltaLakeTables, "Hiding Delta Lake tables is not supported"); // TODO
+        this.maxWaitForLock = thriftConfig.getMaxWaitForTransactionLock();
+
+        this.assumeCanonicalPartitionKeys = thriftConfig.isAssumeCanonicalPartitionKeys();
+    }
+
+    @Managed
+    @Flatten
+    public ThriftMetastoreStats getStats()
+    {
+        return stats;
+    }
+
+    @Override
+    public boolean isImpersonationEnabled()
+    {
+        return impersonationEnabled;
+    }
+
+    @Override
+    public ThriftMetastore createMetastore()
+    {
+        return new ThriftHiveMetastore(
+                hdfsEnvironment,
+                metastoreFactory,
+                backoffScaleFactor,
+                minBackoffDelay,
+                maxBackoffDelay,
+                maxRetryTime,
+                maxWaitForLock,
+                maxRetries,
+                deleteFilesOnDrop,
+                translateHiveViews,
+                assumeCanonicalPartitionKeys,
+                stats);
+    }
+}
