f4400c711b7fc4ec75fad23766a3a1e4f0b0da85
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2022-06-27 22:19:59 +0530

    Update Trino version to 387
    Co-authored-by: Anton Tanasenko <anton.tanasenko@starburstdata.com>
    Co-authored-by: Ashhar Hasan <ashhar.hasan@starburstdata.com>
    
    (cherry-picked from commit: ee3e767f426540bcb0bde193d37037b7bcf9ab87)

--- plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/OracleSplitManager.java
+++ plugins/starburst-oracle/src/main/java/com/starburstdata/presto/plugin/oracle/OracleSplitManager.java
@@ -23,6 +23,7 @@ import io.trino.spi.connector.ConnectorSplitManager;
 import io.trino.spi.connector.ConnectorSplitSource;
 import io.trino.spi.connector.ConnectorTableHandle;
 import io.trino.spi.connector.ConnectorTransactionHandle;
+import io.trino.spi.connector.Constraint;
 import io.trino.spi.connector.DynamicFilter;
 import io.trino.spi.connector.FixedSplitSource;
 import io.trino.spi.predicate.TupleDomain;
@@ -72,7 +73,8 @@ public class OracleSplitManager
             ConnectorSession session,
             ConnectorTableHandle table,
             SplitSchedulingStrategy splitSchedulingStrategy,
-            DynamicFilter dynamicFilter)
+            DynamicFilter dynamicFilter,
+            Constraint constraint)
     {
         return new FixedSplitSource(listSplits(
                 session,
