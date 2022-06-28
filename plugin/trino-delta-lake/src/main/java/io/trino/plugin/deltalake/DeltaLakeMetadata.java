trinodb/trino

commit 4f04191a712c184ff4a1a49791a281ef13a6aa8c

Support setting column comments in Delta Lake

plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -135,6 +135,7 @@ import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -244,6 +245,7 @@ public class DeltaLakeMetadata
     public static final String UPDATE_OPERATION = "UPDATE";
     public static final String OPTIMIZE_OPERATION = "OPTIMIZE";
     public static final String SET_TBLPROPERTIES_OPERATION = "SET TBLPROPERTIES";
+    public static final String CHANGE_COLUMN_OPERATION = "CHANGE COLUMN";
     public static final String ISOLATION_LEVEL = "WriteSerializable";
     private static final int READER_VERSION = 1;
     private static final int WRITER_VERSION = 2;
@@ -997,6 +999,53 @@ public class DeltaLakeMetadata
         }
         catch (Exception e) {
             throw new TrinoException(DELTA_LAKE_BAD_WRITE, format("Unable to comment on table: %s.%s", handle.getSchemaName(), handle.getTableName()), e);
+        }
+    }
+
+    @Override
+    public void setColumnComment(ConnectorSession session, ConnectorTableHandle tableHandle, ColumnHandle column, Optional<String> comment)
+    {
+        DeltaLakeTableHandle deltaLakeTableHandle = (DeltaLakeTableHandle) tableHandle;
+        DeltaLakeColumnHandle deltaLakeColumnHandle = (DeltaLakeColumnHandle) column;
+        checkSupportedWriterVersion(session, deltaLakeTableHandle.getSchemaTableName());
+
+        ConnectorTableMetadata tableMetadata = getTableMetadata(session, deltaLakeTableHandle);
+
+        try {
+            long commitVersion = deltaLakeTableHandle.getReadVersion() + 1;
+
+            List<String> partitionColumns = getPartitionedBy(tableMetadata.getProperties());
+            List<DeltaLakeColumnHandle> columns = tableMetadata.getColumns().stream()
+                    .filter(columnMetadata -> !columnMetadata.isHidden())
+                    .map(columnMetadata -> toColumnHandle(columnMetadata, partitionColumns))
+                    .collect(toImmutableList());
+
+            ImmutableMap.Builder<String, String> columnComments = ImmutableMap.builder();
+            columnComments.putAll(getColumnComments(deltaLakeTableHandle.getMetadataEntry()).entrySet().stream()
+                    .filter(e -> !e.getKey().equals(deltaLakeColumnHandle.getName()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+            comment.ifPresent(s -> columnComments.put(deltaLakeColumnHandle.getName(), s));
+
+            Optional<Long> checkpointInterval = DeltaLakeTableProperties.getCheckpointInterval(tableMetadata.getProperties());
+
+            TransactionLogWriter transactionLogWriter = transactionLogWriterFactory.newWriter(session, deltaLakeTableHandle.getLocation());
+            appendTableEntries(
+                    commitVersion,
+                    transactionLogWriter,
+                    deltaLakeTableHandle.getMetadataEntry().getId(),
+                    columns,
+                    partitionColumns,
+                    columnComments.buildOrThrow(),
+                    buildDeltaMetadataConfiguration(checkpointInterval),
+                    CHANGE_COLUMN_OPERATION,
+                    session,
+                    nodeVersion,
+                    nodeId,
+                    Optional.ofNullable(deltaLakeTableHandle.getMetadataEntry().getDescription()));
+            transactionLogWriter.flush();
+        }
+        catch (Exception e) {
+            throw new TrinoException(DELTA_LAKE_BAD_WRITE, format("Unable to add '%s' column comment for: %s.%s", deltaLakeColumnHandle.getName(), deltaLakeTableHandle.getSchemaName(), deltaLakeTableHandle.getTableName()), e);
         }
     }
 

plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/BaseDeltaLakeMinioConnectorTest.java
@@ -110,7 +110,6 @@ public abstract class BaseDeltaLakeMinioConnectorTest
             case SUPPORTS_AGGREGATION_PUSHDOWN:
             case SUPPORTS_DROP_COLUMN:
             case SUPPORTS_RENAME_COLUMN:
-            case SUPPORTS_COMMENT_ON_COLUMN:
             case SUPPORTS_RENAME_SCHEMA:
             case SUPPORTS_NOT_NULL_CONSTRAINT:
                 return false;

testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/singlenode-delta-lake-databricks/hive.properties
@@ -7,5 +7,6 @@ hive.non-managed-table-writes-enabled=true
 # Required by some product tests
 hive.hive-views.enabled=true
 hive.allow-comment-table=true
+hive.allow-comment-column=true
 hive.allow-rename-table=true
 hive.delta-lake-catalog-name=delta

testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/singlenode-delta-lake-oss/hive.properties
@@ -10,5 +10,6 @@ hive.s3.ssl.enabled=false
 # Required by some product tests
 hive.hive-views.enabled=true
 hive.allow-comment-table=true
+hive.allow-comment-column=true
 hive.allow-rename-table=true
 hive.delta-lake-catalog-name=delta

testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeAlterTableCompatibility.java
@@ -122,4 +122,48 @@ public class TestDeltaLakeAlterTableCompatibility
             onTrino().executeQuery("DROP TABLE delta.default." + tableName);
         }
     }
+
+    @Test(groups = {DELTA_LAKE_DATABRICKS, DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
+    public void testCommentOnColumn()
+    {
+        String tableName = "test_dl_comment_column_" + randomTableSuffix();
+        String tableDirectory = "databricks-compatibility-test-" + tableName;
+
+        onTrino().executeQuery(format("CREATE TABLE delta.default.%s (col INT) WITH (location = 's3://%s/%s')",
+                tableName,
+                bucketName,
+                tableDirectory));
+
+        try {
+            onTrino().executeQuery("COMMENT ON COLUMN delta.default." + tableName + ".col IS 'test column comment'");
+            assertEquals(getColumnCommentOnTrino("default", tableName, "col"), "test column comment");
+            assertEquals(getColumnCommentOnDelta("default", tableName, "col"), "test column comment");
+        }
+        finally {
+            onTrino().executeQuery("DROP TABLE delta.default." + tableName);
+        }
+    }
+
+    @Test(groups = {DELTA_LAKE_DATABRICKS, DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
+    public void testCommentOnColumnUnsupportedWriterVersion()
+    {
+        String tableName = "test_dl_comment_column_unsupported_writer_" + randomTableSuffix();
+        String tableDirectory = "databricks-compatibility-test-" + tableName;
+
+        onDelta().executeQuery(format("" +
+                        "CREATE TABLE default.%s (col int) " +
+                        "USING DELTA LOCATION 's3://%s/%s'" +
+                        "TBLPROPERTIES ('delta.minWriterVersion'='3')",
+                tableName,
+                bucketName,
+                tableDirectory));
+
+        try {
+            assertQueryFailure(() -> onTrino().executeQuery("COMMENT ON COLUMN delta.default." + tableName + ".col IS 'test column comment'"))
+                    .hasMessageMatching(".* Table .* requires Delta Lake writer version 3 which is not supported");
+        }
+        finally {
+            onTrino().executeQuery("DROP TABLE delta.default." + tableName);
+        }
+    }
 }

testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestHiveAndDeltaLakeRedirect.java
@@ -489,6 +489,51 @@ public class TestHiveAndDeltaLakeRedirect
     }
 
     @Test(groups = {DELTA_LAKE_DATABRICKS, DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
+    public void testDeltaToHiveCommentColumn()
+    {
+        String tableName = "hive_comment_column_by_delta_" + randomTableSuffix();
+        String columnName = "id";
+
+        onTrino().executeQuery(createTableInHiveConnector("default", tableName, true));
+        try {
+            assertColumnComment("hive", "default", tableName, columnName).isNull();
+            assertColumnComment("delta", "default", tableName, columnName).isNull();
+
+            String columnComment = "Internal identifier";
+            onTrino().executeQuery(format("COMMENT ON COLUMN delta.default.%s.%s IS '%s'", tableName, columnName, columnComment));
+
+            assertColumnComment("hive", "default", tableName, columnName).isEqualTo(columnComment);
+            assertColumnComment("delta", "default", tableName, columnName).isEqualTo(columnComment);
+        }
+        finally {
+            onTrino().executeQuery("DROP TABLE hive.default." + tableName);
+        }
+    }
+
+    @Test(groups = {DELTA_LAKE_DATABRICKS, DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
+    public void testHiveToDeltaCommentColumn()
+    {
+        String tableName = "delta_comment_column_by_hive_" + randomTableSuffix();
+        String columnName = "nationkey";
+
+        onDelta().executeQuery(createTableInDatabricks(tableName, true));
+
+        try {
+            assertColumnComment("hive", "default", tableName, columnName).isNull();
+            assertColumnComment("delta", "default", tableName, columnName).isNull();
+
+            String columnComment = "Internal identifier for the nation";
+            onTrino().executeQuery(format("COMMENT ON COLUMN hive.default.%s.%s IS '%s'", tableName, columnName, columnComment));
+
+            assertColumnComment("hive", "default", tableName, columnName).isEqualTo(columnComment);
+            assertColumnComment("delta", "default", tableName, columnName).isEqualTo(columnComment);
+        }
+        finally {
+            onDelta().executeQuery("DROP TABLE " + tableName);
+        }
+    }
+
+    @Test(groups = {DELTA_LAKE_DATABRICKS, DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
     public void testInsertIntoDeltaTableFromHiveNonDefaultSchemaRedirect()
     {
         String destSchema = "extraordinary";
@@ -837,6 +882,21 @@ public class TestHiveAndDeltaLakeRedirect
                 param(VARCHAR, catalog),
                 param(VARCHAR, schema),
                 param(VARCHAR, tableName));
+    }
+
+    private static AbstractStringAssert<?> assertColumnComment(String catalog, String schema, String tableName, String columnName)
+    {
+        QueryResult queryResult = readColumnComment(catalog, schema, tableName, columnName);
+        return Assertions.assertThat((String) getOnlyElement(getOnlyElement(queryResult.rows())));
+    }
+
+    private static QueryResult readColumnComment(String catalog, String schema, String tableName, String columnName)
+    {
+        return onTrino().executeQuery(
+                format("SELECT comment FROM %s.information_schema.columns WHERE table_schema = ? AND table_name = ? AND column_name = ?", catalog),
+                param(VARCHAR, schema),
+                param(VARCHAR, tableName),
+                param(VARCHAR, columnName));
     }
 
     private static void assertResultsEqual(QueryResult first, QueryResult second)

commit 1bd03fcc69779daba8b999df340effc170df873d

Fix compilation failure
Caused by a logical merge conflict introduced in 4f04191a712c184ff4a1a49791a281ef13a6aa8c

plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -976,7 +976,7 @@ public class DeltaLakeMetadata
             List<String> partitionColumns = getPartitionedBy(tableMetadata.getProperties());
             List<DeltaLakeColumnHandle> columns = tableMetadata.getColumns().stream()
                     .filter(column -> !column.isHidden())
-                    .map(column -> toColumnHandle(column, partitionColumns))
+                    .map(column -> toColumnHandle(column, column.getName(), column.getType(), partitionColumns))
                     .collect(toImmutableList());
 
             Optional<Long> checkpointInterval = DeltaLakeTableProperties.getCheckpointInterval(tableMetadata.getProperties());
@@ -1017,7 +1017,7 @@ public class DeltaLakeMetadata
             List<String> partitionColumns = getPartitionedBy(tableMetadata.getProperties());
             List<DeltaLakeColumnHandle> columns = tableMetadata.getColumns().stream()
                     .filter(columnMetadata -> !columnMetadata.isHidden())
-                    .map(columnMetadata -> toColumnHandle(columnMetadata, partitionColumns))
+                    .map(columnMetadata -> toColumnHandle(columnMetadata, columnMetadata.getName(), columnMetadata.getType(), partitionColumns))
                     .collect(toImmutableList());
 
             ImmutableMap.Builder<String, String> columnComments = ImmutableMap.builder();