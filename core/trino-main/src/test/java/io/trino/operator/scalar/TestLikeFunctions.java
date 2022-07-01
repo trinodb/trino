cb02d882875b4b676874181853177f4df86a0b86
tangjiangling <surpass.simple@gmail.com>
2022-07-01 15:22:11 +0800

    Move TestLikeFunctions to scalar package
    This test class is related to scalar functions,
    so it should be placed under the scalar package

--- core/trino-main/src/test/java/io/trino/operator/scalar/TestLikeFunctions.java
+++ core/trino-main/src/test/java/io/trino/operator/scalar/TestLikeFunctions.java
@@ -11,11 +11,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.sql;
+package io.trino.operator.scalar;
 
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
-import io.trino.operator.scalar.AbstractTestFunctions;
 import io.trino.spi.TrinoException;
 import io.trino.spi.expression.StandardFunctions;
 import io.trino.type.JoniRegexp;
