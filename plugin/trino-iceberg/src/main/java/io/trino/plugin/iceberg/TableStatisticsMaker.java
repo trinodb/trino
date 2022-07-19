6ba50354879da343d9499044952581eb93c41d9c
David Phillips <david@acz.org>
2022-07-18 19:03:03 -0700

    Remove unused code in TableStatisticsMaker

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/TableStatisticsMaker.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/TableStatisticsMaker.java
@@ -13,7 +13,6 @@
  */
 package io.trino.plugin.iceberg;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.predicate.TupleDomain;
@@ -23,12 +22,10 @@ import io.trino.spi.statistics.Estimate;
 import io.trino.spi.statistics.TableStatistics;
 import io.trino.spi.type.TypeManager;
 import org.apache.iceberg.FileScanTask;
-import org.apache.iceberg.PartitionField;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableScan;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 
 import java.io.IOException;
@@ -38,9 +35,6 @@ import java.util.Map;
 
 import static io.trino.plugin.iceberg.ExpressionConverter.toIcebergExpression;
 import static io.trino.plugin.iceberg.IcebergUtil.getColumns;
-import static io.trino.plugin.iceberg.IcebergUtil.primitiveFieldTypes;
-import static io.trino.plugin.iceberg.TypeConverter.toTrinoType;
-import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toUnmodifiableMap;
 
@@ -79,25 +73,9 @@ public class TableStatisticsMaker
         Schema icebergTableSchema = icebergTable.schema();
         List<Types.NestedField> columns = icebergTableSchema.columns();
 
-        Map<Integer, Type.PrimitiveType> idToTypeMapping = primitiveFieldTypes(icebergTableSchema);
-        List<PartitionField> partitionFields = icebergTable.spec().fields();
-
-        List<Type> icebergPartitionTypes = partitionTypes(partitionFields, idToTypeMapping);
         List<IcebergColumnHandle> columnHandles = getColumns(icebergTableSchema, typeManager);
         Map<Integer, IcebergColumnHandle> idToColumnHandle = columnHandles.stream()
                 .collect(toUnmodifiableMap(IcebergColumnHandle::getId, identity()));
-
-        ImmutableMap.Builder<Integer, ColumnFieldDetails> idToDetailsBuilder = ImmutableMap.builder();
-        for (int index = 0; index < partitionFields.size(); index++) {
-            PartitionField field = partitionFields.get(index);
-            Type type = icebergPartitionTypes.get(index);
-            idToDetailsBuilder.put(field.fieldId(), new ColumnFieldDetails(
-                    field,
-                    idToColumnHandle.get(field.sourceId()),
-                    type,
-                    toTrinoType(type, typeManager),
-                    type.typeId().javaClass()));
-        }
 
         TableScan tableScan = icebergTable.newScan()
                 .filter(toIcebergExpression(enforcedPredicate))
@@ -143,59 +121,5 @@ public class TableStatisticsMaker
             columnHandleBuilder.put(columnHandle, columnBuilder.build());
         }
         return new TableStatistics(Estimate.of(recordCount), columnHandleBuilder.buildOrThrow());
-    }
-
-    public List<Type> partitionTypes(List<PartitionField> partitionFields, Map<Integer, Type.PrimitiveType> idToTypeMapping)
-    {
-        ImmutableList.Builder<Type> partitionTypeBuilder = ImmutableList.builder();
-        for (PartitionField partitionField : partitionFields) {
-            Type.PrimitiveType sourceType = idToTypeMapping.get(partitionField.sourceId());
-            Type type = partitionField.transform().getResultType(sourceType);
-            partitionTypeBuilder.add(type);
-        }
-        return partitionTypeBuilder.build();
-    }
-
-    private static class ColumnFieldDetails
-    {
-        private final PartitionField field;
-        private final IcebergColumnHandle columnHandle;
-        private final Type icebergType;
-        private final io.trino.spi.type.Type trinoType;
-        private final Class<?> javaClass;
-
-        public ColumnFieldDetails(PartitionField field, IcebergColumnHandle columnHandle, Type icebergType, io.trino.spi.type.Type trinoType, Class<?> javaClass)
-        {
-            this.field = requireNonNull(field, "field is null");
-            this.columnHandle = requireNonNull(columnHandle, "columnHandle is null");
-            this.icebergType = requireNonNull(icebergType, "icebergType is null");
-            this.trinoType = requireNonNull(trinoType, "trinoType is null");
-            this.javaClass = requireNonNull(javaClass, "javaClass is null");
-        }
-
-        public PartitionField getField()
-        {
-            return field;
-        }
-
-        public IcebergColumnHandle getColumnHandle()
-        {
-            return columnHandle;
-        }
-
-        public Type getIcebergType()
-        {
-            return icebergType;
-        }
-
-        public io.trino.spi.type.Type getTrinoType()
-        {
-            return trinoType;
-        }
-
-        public Class<?> getJavaClass()
-        {
-            return javaClass;
-        }
     }
 }
