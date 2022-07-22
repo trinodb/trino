6e81a098a15e2449733629e1cb875200856c3d72
Raunaq Morarka <raunaqmorarka@gmail.com>
2022-07-23 01:25:05 +0530

    Implement dynamic filtering for JDBC connectors
    Added JDBC catalog properties dynamic-filtering.enabled and
    dynamic-filtering.wait-timeout to configure pushdown of
    dynamic filters to JDBC queries.
    
    Co-authored-by: Karol Sobczak <napewnotrafi@gmail.com>

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcSplit.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcSplit.java
@@ -19,6 +19,7 @@ import com.google.common.collect.ImmutableList;
 import io.airlift.slice.SizeOf;
 import io.trino.spi.HostAddress;
 import io.trino.spi.connector.ConnectorSplit;
+import io.trino.spi.predicate.TupleDomain;
 import org.openjdk.jol.info.ClassLayout;
 
 import java.util.List;
@@ -33,18 +34,37 @@ public class JdbcSplit
     private static final int INSTANCE_SIZE = ClassLayout.parseClass(JdbcSplit.class).instanceSize();
 
     private final Optional<String> additionalPredicate;
+    private final TupleDomain<JdbcColumnHandle> dynamicFilter;
+
+    public JdbcSplit(Optional<String> additionalPredicate)
+    {
+        this(additionalPredicate, TupleDomain.all());
+    }
 
     @JsonCreator
     public JdbcSplit(
-            @JsonProperty("additionalPredicate") Optional<String> additionalPredicate)
+            @JsonProperty("additionalPredicate") Optional<String> additionalPredicate,
+            @JsonProperty("dynamicFilter") TupleDomain<JdbcColumnHandle> dynamicFilter)
     {
         this.additionalPredicate = requireNonNull(additionalPredicate, "additionalPredicate is null");
+        this.dynamicFilter = requireNonNull(dynamicFilter, "dynamicFilter is null");
+    }
+
+    public JdbcSplit withDynamicFilter(TupleDomain<JdbcColumnHandle> dynamicFilter)
+    {
+        return new JdbcSplit(additionalPredicate, dynamicFilter);
     }
 
     @JsonProperty
     public Optional<String> getAdditionalPredicate()
     {
         return additionalPredicate;
+    }
+
+    @JsonProperty
+    public TupleDomain<JdbcColumnHandle> getDynamicFilter()
+    {
+        return dynamicFilter;
     }
 
     @Override
@@ -69,6 +89,7 @@ public class JdbcSplit
     public long getRetainedSizeInBytes()
     {
         return INSTANCE_SIZE
-                + sizeOf(additionalPredicate, SizeOf::estimatedSizeOf);
+                + sizeOf(additionalPredicate, SizeOf::estimatedSizeOf)
+                + dynamicFilter.getRetainedSizeInBytes(JdbcColumnHandle::getRetainedSizeInBytes);
     }
 }
