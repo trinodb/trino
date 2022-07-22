481fa3f2c3cb6e86114c2ac11a46e9e3d893811f
Dain Sundstrom <dain@iq80.com>
2022-07-21 18:46:16 -0700

    Mark the PTF SPIs as Experimental

--- core/trino-spi/src/main/java/io/trino/spi/ptf/DescriptorArgument.java
+++ core/trino-spi/src/main/java/io/trino/spi/ptf/DescriptorArgument.java
@@ -15,6 +15,7 @@ package io.trino.spi.ptf;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.trino.spi.Experimental;
 import io.trino.spi.expression.ConnectorExpression;
 
 import java.util.Optional;
@@ -27,6 +28,7 @@ import static java.util.Objects.requireNonNull;
  * This representation should be considered experimental. Eventually, {@link ConnectorExpression}
  * should be extended to include this kind of argument.
  */
+@Experimental(eta = "2022-10-31")
 public class DescriptorArgument
         extends Argument
 {
