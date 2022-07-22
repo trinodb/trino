0729d78a9b40cca33862e8bfa1bbe1681f9f043c
Marius Grama <findinpath@gmail.com>
2022-07-22 17:33:29 +0200

    Add config for the caching duration of Delta table active data files

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakePlugin.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakePlugin.java
@@ -117,4 +117,16 @@ public class TestDeltaLakePlugin
                         "delta.metadata.cache-ttl", "0s"),
                 new TestingConnectorContext());
     }
+
+    @Test
+    public void testNoActiveDataFilesCaching()
+    {
+        Plugin plugin = new DeltaLakePlugin();
+        ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
+        factory.create("test",
+                ImmutableMap.of(
+                        "hive.metastore.uri", "thrift://foo:1234",
+                        "delta.metadata.live-files.cache-ttl", "0s"),
+                new TestingConnectorContext());
+    }
 }
