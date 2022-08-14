ae638af8521ff6ce0bb573271debf55dcfcbf207
David Phillips <david@acz.org>
2022-08-13 23:14:06 -0700

    Move Thrift metastore authentication classes

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/HiveMetastoreAuthentication.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/HiveMetastoreAuthentication.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.authentication;
+package io.trino.plugin.hive.metastore.thrift;
 
 import org.apache.thrift.transport.TTransport;
 
