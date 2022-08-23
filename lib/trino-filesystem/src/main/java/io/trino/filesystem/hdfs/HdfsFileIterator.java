60c30c8c527cfa8dfa78a2036635914242b68988
David Phillips <david@acz.org>
2022-08-22 23:49:03 -0700

    Fix listing file names containing spaces

--- lib/trino-filesystem/src/main/java/io/trino/filesystem/hdfs/HdfsFileIterator.java
+++ lib/trino-filesystem/src/main/java/io/trino/filesystem/hdfs/HdfsFileIterator.java
@@ -55,7 +55,7 @@ class HdfsFileIterator
 
         verify(status.isFile(), "iterator returned a non-file: %s", status);
 
-        URI pathUri = URI.create(status.getPath().toString());
+        URI pathUri = status.getPath().toUri();
         URI relativeUri = listingUri.relativize(pathUri);
         verify(!relativeUri.equals(pathUri), "cannot relativize [%s] against [%s]", pathUri, listingUri);
 
