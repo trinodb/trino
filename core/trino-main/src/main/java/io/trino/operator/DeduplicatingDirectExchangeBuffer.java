a977d88b051e6616d1fd9c355362627383abec29
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-08-23 18:37:20 +0200

    Change data ownership contract in call of ExchangeSink.add
    After the change the contract changes in a way that ownership of data
    passed as an argument is passed from caller to callee.
    Apart from usage in DeduplicatingDirectExchangeBuffer the Slices passed
    as to ExchangeSink.add were single-use and not mutataded/reclaimed
    afterwards.
    
    The semantics change allows for simplere and more effective
    implementations of ExchangeSinks as no extra data copying is enforced.

--- core/trino-main/src/main/java/io/trino/operator/DeduplicatingDirectExchangeBuffer.java
+++ core/trino-main/src/main/java/io/trino/operator/DeduplicatingDirectExchangeBuffer.java
@@ -26,6 +26,7 @@ import io.airlift.log.Logger;
 import io.airlift.slice.DynamicSliceOutput;
 import io.airlift.slice.Slice;
 import io.airlift.slice.SliceOutput;
+import io.airlift.slice.Slices;
 import io.airlift.units.DataSize;
 import io.trino.exchange.ExchangeManagerRegistry;
 import io.trino.execution.StageId;
@@ -579,7 +580,7 @@ public class DeduplicatingDirectExchangeBuffer
                 writeBuffer.writeInt(taskId.getPartitionId());
                 writeBuffer.writeInt(taskId.getAttemptId());
                 writeBuffer.writeBytes(page);
-                exchangeSink.add(0, writeBuffer.slice());
+                exchangeSink.add(0, Slices.copyOf(writeBuffer.slice()));
                 writeBuffer.reset();
                 spilledBytes += page.length();
                 spilledPageCount++;
