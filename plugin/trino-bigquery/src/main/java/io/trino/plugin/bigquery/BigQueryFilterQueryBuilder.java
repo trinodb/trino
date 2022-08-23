597b74725f57ca159d153117207c4b56a9a88252
Yuya Ebihara <ebyhry@gmail.com>
2022-08-23 22:10:06 +0900

    Extract quote method to BigQueryUtil

--- plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryFilterQueryBuilder.java
+++ plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/BigQueryFilterQueryBuilder.java
@@ -26,14 +26,13 @@ import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Iterables.getOnlyElement;
+import static io.trino.plugin.bigquery.BigQueryUtil.quote;
 import static io.trino.plugin.bigquery.BigQueryUtil.toBigQueryColumnName;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class BigQueryFilterQueryBuilder
 {
-    private static final String QUOTE = "`";
-    private static final String ESCAPED_QUOTE = "``";
     private final TupleDomain<ColumnHandle> tupleDomain;
 
     public static Optional<String> buildFilter(TupleDomain<ColumnHandle> tupleDomain)
@@ -150,10 +149,5 @@ public class BigQueryFilterQueryBuilder
             return Optional.empty();
         }
         return Optional.of(quote(columnName) + " " + operator + " " + valueAsString.get());
-    }
-
-    private String quote(String name)
-    {
-        return QUOTE + name.replace(QUOTE, ESCAPED_QUOTE) + QUOTE;
     }
 }
