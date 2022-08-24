91c7e4ec71e3d9d2b3f302febfaa64be246081d1
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2022-08-24 10:04:53 +0200

    Squash Phoenix environment extender into EnvMultinodePhoenix5

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/EnvironmentModule.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/EnvironmentModule.java
@@ -27,7 +27,6 @@ import io.trino.tests.product.launcher.env.common.KafkaSaslPlaintext;
 import io.trino.tests.product.launcher.env.common.KafkaSsl;
 import io.trino.tests.product.launcher.env.common.Kerberos;
 import io.trino.tests.product.launcher.env.common.Minio;
-import io.trino.tests.product.launcher.env.common.Phoenix;
 import io.trino.tests.product.launcher.env.common.Standard;
 import io.trino.tests.product.launcher.env.common.StandardMultinode;
 import io.trino.tests.product.launcher.testcontainers.PortBinder;
@@ -74,7 +73,6 @@ public final class EnvironmentModule
         binder.bind(KafkaSaslPlaintext.class).in(SINGLETON);
         binder.bind(Standard.class).in(SINGLETON);
         binder.bind(StandardMultinode.class).in(SINGLETON);
-        binder.bind(Phoenix.class).in(SINGLETON);
         binder.bind(Kerberos.class).in(SINGLETON);
         binder.bind(Minio.class).in(SINGLETON);
 
