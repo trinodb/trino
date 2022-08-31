8a11ce08447f6568376133b6e7887a482575ac84
Andrii Rosa <andriyrosa@gmail.com>
2022-08-31 12:05:35 -0400

    Reduce invocation count of TestFaultTolerantStageScheduler#testIsFinished
    Added for testing, committed by mistake

--- core/trino-main/src/test/java/io/trino/execution/scheduler/TestFaultTolerantStageScheduler.java
+++ core/trino-main/src/test/java/io/trino/execution/scheduler/TestFaultTolerantStageScheduler.java
@@ -826,7 +826,7 @@ public class TestFaultTolerantStageScheduler
         }
     }
 
-    @Test(invocationCount = 1000)
+    @Test
     public void testIsFinished()
             throws Exception
     {
