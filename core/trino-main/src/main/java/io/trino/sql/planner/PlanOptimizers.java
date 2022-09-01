224ed1af4afac8b5f74ab0d239c9cfbafd5e880a
Martin Traverso <mtraverso@gmail.com>
2022-09-01 08:37:09 -0700

    Move desugaring of current_schema to planner
    This is to support the split of AST from IR

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -45,7 +45,6 @@ import io.trino.sql.planner.iterative.rule.DecorrelateLeftUnnestWithGlobalAggreg
 import io.trino.sql.planner.iterative.rule.DecorrelateUnnest;
 import io.trino.sql.planner.iterative.rule.DesugarArrayConstructor;
 import io.trino.sql.planner.iterative.rule.DesugarAtTimeZone;
-import io.trino.sql.planner.iterative.rule.DesugarCurrentSchema;
 import io.trino.sql.planner.iterative.rule.DesugarLambdaExpression;
 import io.trino.sql.planner.iterative.rule.DesugarLike;
 import io.trino.sql.planner.iterative.rule.DesugarTryExpression;
@@ -402,7 +401,6 @@ public class PlanOptimizers
                         ImmutableSet.<Rule<?>>builder()
                                 .addAll(new DesugarLambdaExpression().rules())
                                 .addAll(new DesugarAtTimeZone(metadata, typeAnalyzer).rules())
-                                .addAll(new DesugarCurrentSchema(metadata).rules())
                                 .addAll(new DesugarTryExpression(metadata, typeAnalyzer).rules())
                                 .build()),
                 new IterativeOptimizer(
