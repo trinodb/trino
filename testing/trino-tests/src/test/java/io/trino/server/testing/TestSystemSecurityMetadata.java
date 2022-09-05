42d21703953104ef1e5e48ec4445a55928e12f41
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2022-09-05 15:30:25 +0200

    Move TestingSystemSecurityMetadata to trino-main
    Currently it's used only for tests of `SystemSecurityMetadata`, but it
    can be useful for way more than that.

--- testing/trino-tests/src/test/java/io/trino/server/testing/TestSystemSecurityMetadata.java
+++ testing/trino-tests/src/test/java/io/trino/server/testing/TestSystemSecurityMetadata.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.security;
+package io.trino.server.testing;
 
 import com.google.common.collect.ImmutableSet;
 import io.trino.Session;
