365dc964a0cf01afb9aa8f72ccc02c59e23fda9d
Yuya Ebihara <ebyhry@gmail.com>
2022-09-07 22:00:43 +0900

    Simplify TestDeltaLakeRenameToWithGlueMetastore

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/metastore/glue/TestDeltaLakeRenameToWithGlueMetastore.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/metastore/glue/TestDeltaLakeRenameToWithGlueMetastore.java
@@ -13,20 +13,9 @@
  */
 package io.trino.plugin.deltalake.metastore.glue;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import io.trino.Session;
-import io.trino.hdfs.DynamicHdfsConfiguration;
-import io.trino.hdfs.HdfsConfig;
-import io.trino.hdfs.HdfsConfigurationInitializer;
-import io.trino.hdfs.HdfsEnvironment;
-import io.trino.hdfs.authentication.NoHdfsAuthentication;
-import io.trino.plugin.deltalake.DeltaLakePlugin;
-import io.trino.plugin.hive.metastore.HiveMetastore;
-import io.trino.plugin.hive.metastore.glue.DefaultGlueColumnStatisticsProviderFactory;
-import io.trino.plugin.hive.metastore.glue.GlueHiveMetastore;
-import io.trino.plugin.hive.metastore.glue.GlueHiveMetastoreConfig;
+import io.trino.plugin.deltalake.DeltaLakeQueryRunner;
 import io.trino.testing.AbstractTestQueryFramework;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.QueryRunner;
@@ -34,10 +23,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.Optional;
 
-import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
-import static io.trino.plugin.deltalake.DeltaLakeConnectorFactory.CONNECTOR_NAME;
 import static io.trino.testing.TestingSession.testSessionBuilder;
 import static io.trino.testing.sql.TestTable.randomTableSuffix;
 import static java.lang.String.format;
@@ -47,9 +33,8 @@ public class TestDeltaLakeRenameToWithGlueMetastore
 {
     protected static final String SCHEMA = "test_delta_lake_rename_to_with_glue_" + randomTableSuffix();
     protected static final String CATALOG_NAME = "test_delta_lake_rename_to_with_glue";
-    protected File metastoreDir;
-    protected HiveMetastore metastore;
-    protected HdfsEnvironment hdfsEnvironment;
+
+    private File schemaLocation;
 
     @Override
     protected QueryRunner createQueryRunner()
@@ -60,38 +45,13 @@ public class TestDeltaLakeRenameToWithGlueMetastore
                 .setSchema(SCHEMA)
                 .build();
 
-        DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(deltaLakeSession).build();
-
-        this.metastoreDir = new File(queryRunner.getCoordinator().getBaseDataDir().resolve("delta_lake_data").toString());
-        this.metastoreDir.deleteOnExit();
-
-        queryRunner.installPlugin(new DeltaLakePlugin());
-        queryRunner.createCatalog(
-                CATALOG_NAME,
-                CONNECTOR_NAME,
-                ImmutableMap.<String, String>builder()
-                        .put("hive.metastore", "glue")
-                        .put("hive.metastore.glue.region", "us-east-2")
-                        .put("hive.metastore.glue.default-warehouse-dir", metastoreDir.getPath())
-                        .buildOrThrow());
-
-        HdfsConfig hdfsConfig = new HdfsConfig();
-        hdfsEnvironment = new HdfsEnvironment(
-                new DynamicHdfsConfiguration(new HdfsConfigurationInitializer(hdfsConfig), ImmutableSet.of()),
-                hdfsConfig,
-                new NoHdfsAuthentication());
-        GlueHiveMetastoreConfig glueConfig = new GlueHiveMetastoreConfig()
-                .setGlueRegion("us-east-2");
-        metastore = new GlueHiveMetastore(
-                hdfsEnvironment,
-                glueConfig,
-                DefaultAWSCredentialsProviderChain.getInstance(),
-                directExecutor(),
-                new DefaultGlueColumnStatisticsProviderFactory(directExecutor(), directExecutor()),
-                Optional.empty(),
-                table -> true);
-
-        queryRunner.execute("CREATE SCHEMA " + SCHEMA + " WITH (location = '" + metastoreDir.getPath() + "')");
+        DistributedQueryRunner queryRunner = DeltaLakeQueryRunner.builder(deltaLakeSession)
+                .setCatalogName(CATALOG_NAME)
+                .setDeltaProperties(ImmutableMap.of("hive.metastore", "glue"))
+                .build();
+        schemaLocation = new File(queryRunner.getCoordinator().getBaseDataDir().resolve("delta_lake_data").toString());
+        schemaLocation.deleteOnExit();
+        queryRunner.execute("CREATE SCHEMA " + SCHEMA + " WITH (location = '" + schemaLocation.getPath() + "')");
         return queryRunner;
     }
 
@@ -100,7 +60,7 @@ public class TestDeltaLakeRenameToWithGlueMetastore
     {
         String oldTable = "test_table_external_to_be_renamed_" + randomTableSuffix();
         String newTable = "test_table_external_renamed_" + randomTableSuffix();
-        String location = metastoreDir.getAbsolutePath() + "/tableLocation/";
+        String location = schemaLocation.getPath() + "/tableLocation/";
         try {
             assertUpdate(format("CREATE TABLE %s WITH (location = '%s') AS SELECT 1 AS val ", oldTable, location), 1);
             String oldLocation = (String) computeScalar("SELECT \"$path\" FROM " + oldTable);
