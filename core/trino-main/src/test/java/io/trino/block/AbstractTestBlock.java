18cb183098fa55b7726f8eefcdeee4c6bb18b8e1
Dain Sundstrom <dain@iq80.com>
2022-09-10 22:47:11 -0700

    Change BlockBuilder to not extend Block

--- core/trino-main/src/test/java/io/trino/block/AbstractTestBlock.java
+++ core/trino-main/src/test/java/io/trino/block/AbstractTestBlock.java
@@ -24,7 +24,6 @@ import io.trino.spi.block.BlockBuilderStatus;
 import io.trino.spi.block.BlockEncodingSerde;
 import io.trino.spi.block.DictionaryBlock;
 import io.trino.spi.block.DictionaryId;
-import io.trino.spi.block.MapBlockBuilder;
 import io.trino.spi.block.MapHashTables;
 import io.trino.spi.block.TestingBlockEncodingSerde;
 import io.trino.spi.block.VariableWidthBlockBuilder;
@@ -113,10 +112,10 @@ public abstract class AbstractTestBlock
                         retainedSize += BlockBuilderStatus.INSTANCE_SIZE;
                     }
                 }
-                else if (type == BlockBuilder.class || type == Block.class) {
+                else if (type == Block.class) {
                     retainedSize += ((Block) field.get(block)).getRetainedSizeInBytes();
                 }
-                else if (type == BlockBuilder[].class || type == Block[].class) {
+                else if (type == Block[].class) {
                     Block[] blocks = (Block[]) field.get(block);
                     for (Block innerBlock : blocks) {
                         assertRetainedSize(innerBlock);
@@ -147,18 +146,11 @@ public abstract class AbstractTestBlock
                 else if (type == MapHashTables.class) {
                     retainedSize += ((MapHashTables) field.get(block)).getRetainedSizeInBytes();
                 }
-                else if (type.getEnclosingClass() == MapBlockBuilder.class) {
-                    // ignore nested enum
-                }
                 else if (type == MethodHandle.class) {
-                    // MethodHandles are only used in MapBlock/MapBlockBuilder,
+                    // MethodHandles are only used in MapBlock
                     // and they are shared among blocks created by the same MapType.
                     // So we don't account for the memory held onto by MethodHandle instances.
                     // Otherwise, we will be counting it multiple times.
-                }
-                else if (field.getName().equals("fieldBlockBuildersList")) {
-                    // RowBlockBuilder fieldBlockBuildersList is a simple wrapper around the
-                    // array already accounted for in the instance
                 }
                 else {
                     throw new IllegalArgumentException(format("Unknown type encountered: %s", type));
--- core/trino-spi/src/main/java/io/trino/spi/block/IntArrayBlockBuilder.java
+++ core/trino-spi/src/main/java/io/trino/spi/block/IntArrayBlockBuilder.java
@@ -16,14 +16,9 @@ package io.trino.spi.block;
 import jakarta.annotation.Nullable;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.function.ObjLongConsumer;
 
 import static io.airlift.slice.SizeOf.instanceSize;
 import static io.airlift.slice.SizeOf.sizeOf;
-import static io.trino.spi.block.BlockUtil.checkArrayRange;
-import static io.trino.spi.block.BlockUtil.checkReadablePosition;
-import static io.trino.spi.block.BlockUtil.checkValidRegion;
 import static java.lang.Math.max;
 
 public class IntArrayBlockBuilder
@@ -128,27 +123,9 @@ public class IntArrayBlockBuilder
     }
 
     @Override
-    public OptionalInt fixedSizeInBytesPerPosition()
-    {
-        return OptionalInt.of(IntArrayBlock.SIZE_IN_BYTES_PER_POSITION);
-    }
-
-    @Override
     public long getSizeInBytes()
     {
         return IntArrayBlock.SIZE_IN_BYTES_PER_POSITION * (long) positionCount;
-    }
-
-    @Override
-    public long getRegionSizeInBytes(int position, int length)
-    {
-        return IntArrayBlock.SIZE_IN_BYTES_PER_POSITION * (long) length;
-    }
-
-    @Override
-    public long getPositionsSizeInBytes(boolean[] positions, int selectedPositionsCount)
-    {
-        return IntArrayBlock.SIZE_IN_BYTES_PER_POSITION * (long) selectedPositionsCount;
     }
 
     @Override
@@ -158,114 +135,9 @@ public class IntArrayBlockBuilder
     }
 
     @Override
-    public long getEstimatedDataSizeForStats(int position)
-    {
-        return isNull(position) ? 0 : Integer.BYTES;
-    }
-
-    @Override
-    public void retainedBytesForEachPart(ObjLongConsumer<Object> consumer)
-    {
-        consumer.accept(values, sizeOf(values));
-        consumer.accept(valueIsNull, sizeOf(valueIsNull));
-        consumer.accept(this, INSTANCE_SIZE);
-    }
-
-    @Override
     public int getPositionCount()
     {
         return positionCount;
-    }
-
-    @Override
-    public int getInt(int position, int offset)
-    {
-        checkReadablePosition(this, position);
-        if (offset != 0) {
-            throw new IllegalArgumentException("offset must be zero");
-        }
-        return values[position];
-    }
-
-    @Override
-    public boolean mayHaveNull()
-    {
-        return hasNullValue;
-    }
-
-    @Override
-    public boolean isNull(int position)
-    {
-        checkReadablePosition(this, position);
-        return valueIsNull[position];
-    }
-
-    @Override
-    public Block getSingleValueBlock(int position)
-    {
-        checkReadablePosition(this, position);
-        return new IntArrayBlock(
-                0,
-                1,
-                valueIsNull[position] ? new boolean[] {true} : null,
-                new int[] {values[position]});
-    }
-
-    @Override
-    public Block copyPositions(int[] positions, int offset, int length)
-    {
-        checkArrayRange(positions, offset, length);
-
-        if (!hasNonNullValue) {
-            return RunLengthEncodedBlock.create(NULL_VALUE_BLOCK, length);
-        }
-        boolean[] newValueIsNull = null;
-        if (hasNullValue) {
-            newValueIsNull = new boolean[length];
-        }
-        int[] newValues = new int[length];
-        for (int i = 0; i < length; i++) {
-            int position = positions[offset + i];
-            checkReadablePosition(this, position);
-            if (hasNullValue) {
-                newValueIsNull[i] = valueIsNull[position];
-            }
-            newValues[i] = values[position];
-        }
-        return new IntArrayBlock(0, length, newValueIsNull, newValues);
-    }
-
-    @Override
-    public Block getRegion(int positionOffset, int length)
-    {
-        checkValidRegion(getPositionCount(), positionOffset, length);
-
-        if (!hasNonNullValue) {
-            return RunLengthEncodedBlock.create(NULL_VALUE_BLOCK, length);
-        }
-        return new IntArrayBlock(positionOffset, length, hasNullValue ? valueIsNull : null, values);
-    }
-
-    @Override
-    public Block copyRegion(int positionOffset, int length)
-    {
-        checkValidRegion(getPositionCount(), positionOffset, length);
-
-        if (!hasNonNullValue) {
-            return RunLengthEncodedBlock.create(NULL_VALUE_BLOCK, length);
-        }
-        boolean[] newValueIsNull = null;
-        if (hasNullValue) {
-            newValueIsNull = Arrays.copyOfRange(valueIsNull, positionOffset, positionOffset + length);
-        }
-        int[] newValues = Arrays.copyOfRange(values, positionOffset, positionOffset + length);
-        return new IntArrayBlock(0, length, newValueIsNull, newValues);
-    }
-
-    @Override
-    public String getEncodingName()
-    {
-        return IntArrayBlockEncoding.NAME;
     }
 
     @Override
