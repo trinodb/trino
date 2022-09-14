6d86b566fa169d256c8b6e023b78f684a7bacda0
dnnanuti <dnnanuti@amazon.com>
2022-09-14 14:10:43 +0100

    Enable hive splits for uncompressed JSON files with S3 Select pushdown Scan range allows S3 Select to query uncompressed files at a finer granularity than the entire object, by providing a byte range to SelectObjectContent requests. This change enables hive internal splits for S3 Select uncompressed JSON input.

--- plugin/trino-hive-hadoop2/src/test/java/io/trino/plugin/hive/s3select/TestHiveFileSystemS3SelectJsonPushdownWithSplits.java
+++ plugin/trino-hive-hadoop2/src/test/java/io/trino/plugin/hive/s3select/TestHiveFileSystemS3SelectJsonPushdownWithSplits.java
@@ -13,11 +13,9 @@
  */
 package io.trino.plugin.hive.s3select;
 
-import com.google.common.collect.ImmutableList;
 import io.airlift.units.DataSize;
 import io.trino.plugin.hive.HiveConfig;
 import io.trino.spi.connector.ColumnHandle;
-import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.testing.MaterializedResult;
 import org.testng.annotations.BeforeClass;
@@ -25,22 +23,20 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.LongStream;
 
 import static io.airlift.units.DataSize.Unit.KILOBYTE;
 import static io.trino.plugin.hive.HiveColumnHandle.ColumnType.REGULAR;
 import static io.trino.plugin.hive.HiveColumnHandle.createBaseColumn;
-import static io.trino.plugin.hive.HiveFileSystemTestUtils.filterTable;
-import static io.trino.plugin.hive.HiveFileSystemTestUtils.getSplitsCount;
 import static io.trino.plugin.hive.HiveFileSystemTestUtils.newSession;
 import static io.trino.plugin.hive.HiveType.HIVE_INT;
+import static io.trino.plugin.hive.s3select.S3SelectTestHelper.expectedResult;
+import static io.trino.plugin.hive.s3select.S3SelectTestHelper.isSplitCountInOpenInterval;
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.testing.QueryAssertions.assertEqualsIgnoreOrder;
 import static org.testng.Assert.assertTrue;
 
-public class TestHiveFileSystemS3SelectPushdownWithSplits
+public class TestHiveFileSystemS3SelectJsonPushdownWithSplits
 {
     private String host;
     private int port;
@@ -49,7 +45,8 @@ public class TestHiveFileSystemS3SelectPushdownWithSplits
     private String awsSecretKey;
     private String writableBucket;
     private String testDirectory;
-    private SchemaTableName tableWithSplits;
+
+    private SchemaTableName tableJsonWithSplits;
 
     @Parameters({
             "hive.hadoop2.metastoreHost",
@@ -71,21 +68,20 @@ public class TestHiveFileSystemS3SelectPushdownWithSplits
         this.writableBucket = writableBucket;
         this.testDirectory = testDirectory;
 
-        tableWithSplits = new SchemaTableName(databaseName, "trino_s3select_test_csv_scan_range_pushdown");
+        this.tableJsonWithSplits = new SchemaTableName(databaseName, "trino_s3select_test_json_scan_range_pushdown");
     }
 
     @DataProvider(name = "testSplitSize")
     public static Object[][] splitSizeParametersProvider()
     {
-        return new Object[][] {{3, 2, 15, 30}, {50, 30, 2, 4}};
+        return new Object[][] {{15, 10, 6, 12}, {50, 30, 2, 4}};
     }
 
     @Test(dataProvider = "testSplitSize")
-    public void testQueryPushdownWithSplitSize(int maxSplitSizeKB,
-                                               int maxInitialSplitSizeKB,
-                                               int minSplitCount,
-                                               int maxSplitCount)
-            throws Exception
+    public void testQueryPushdownWithSplitSizeForJson(int maxSplitSizeKB,
+                                                      int maxInitialSplitSizeKB,
+                                                      int minSplitCount,
+                                                      int maxSplitCount)
     {
         S3SelectTestHelper s3SelectTestHelper = null;
         try {
@@ -103,47 +99,18 @@ public class TestHiveFileSystemS3SelectPushdownWithSplits
                     testDirectory,
                     hiveConfig);
 
-            int splitsCount = getSplitsCount(
-                    tableWithSplits,
-                    s3SelectTestHelper.getTransactionManager(),
-                    s3SelectTestHelper.getHiveConfig(),
-                    s3SelectTestHelper.getSplitManager());
-            assertTrue(isSplitCountWithinBounds(splitsCount, minSplitCount, maxSplitCount));
+            int tableSplitsCount = s3SelectTestHelper.getTableSplitsCount(tableJsonWithSplits);
+            assertTrue(isSplitCountInOpenInterval(tableSplitsCount, minSplitCount, maxSplitCount));
 
-            List<ColumnHandle> indexColumn = ImmutableList.of(
-                    createBaseColumn("index", 0, HIVE_INT, BIGINT, REGULAR, Optional.empty()));
-
-            assertEqualsIgnoreOrder(
-                    filterTable(
-                            tableWithSplits,
-                            indexColumn,
-                            s3SelectTestHelper.getTransactionManager(),
-                            s3SelectTestHelper.getHiveConfig(),
-                            s3SelectTestHelper.getPageSourceProvider(),
-                            s3SelectTestHelper.getSplitManager()),
-                    expectedResult(newSession(s3SelectTestHelper.getHiveConfig())));
+            ColumnHandle indexColumn = createBaseColumn("col_1", 0, HIVE_INT, BIGINT, REGULAR, Optional.empty());
+            MaterializedResult filteredTableResult = s3SelectTestHelper.getFilteredTableResult(tableJsonWithSplits, indexColumn);
+            assertEqualsIgnoreOrder(filteredTableResult,
+                    expectedResult(newSession(s3SelectTestHelper.getHiveConfig()), 1, 300));
         }
         finally {
             if (s3SelectTestHelper != null) {
                 s3SelectTestHelper.tearDown();
             }
         }
-    }
-
-    private MaterializedResult expectedResult(ConnectorSession session)
-    {
-        // The test is selecting the index column from test_table_csv_scan_range_select_pushdown_*.csv files
-        MaterializedResult.Builder builder = MaterializedResult.resultBuilder(session, BIGINT);
-        LongStream.rangeClosed(1, 300).forEach(builder::row);
-        return builder.build();
-    }
-
-    private boolean isSplitCountWithinBounds(int splitCount,
-                                             int minSplitCount,
-                                             int maxSplitCount)
-    {
-        // Split number may vary, the minimum number of splits being obtained with
-        // the first split of maxInitialSplitSize and the rest of maxSplitSize
-        return minSplitCount < splitCount && splitCount < maxSplitCount;
     }
 }
