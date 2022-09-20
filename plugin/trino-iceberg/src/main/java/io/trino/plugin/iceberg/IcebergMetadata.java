8a8e601e6f4650fb1a0a07feb719bd707ba781a2
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-09-20 13:19:37 +0200

    Simplify error message construction

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -1542,7 +1542,7 @@ public class IcebergMetadata
                     updateProperties.set(TRINO_STATS_NDV_FORMAT.formatted(columnId), Long.toString(ndv));
                 }
                 else {
-                    throw new UnsupportedOperationException("Unsupported statistic type: " + statisticMetadata.getStatisticType());
+                    throw new UnsupportedOperationException("Unsupported statistic: " + statisticMetadata);
                 }
             }
         }
