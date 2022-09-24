c909583700ee08e5a1a4a0d7a3746085c65d4d82
Martin Traverso <mtraverso@gmail.com>
2022-09-24 10:48:10 -0700

    Migrate TestZipFunction to new assertions

--- core/trino-main/src/test/java/io/trino/operator/scalar/TestZipFunction.java
+++ core/trino-main/src/test/java/io/trino/operator/scalar/TestZipFunction.java
@@ -16,7 +16,11 @@ package io.trino.operator.scalar;
 import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.RowType;
 import io.trino.spi.type.Type;
-import org.testng.annotations.Test;
+import io.trino.sql.query.QueryAssertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -27,100 +31,113 @@ import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static io.trino.spi.type.VarcharType.createVarcharType;
 import static io.trino.type.UnknownType.UNKNOWN;
-import static java.lang.String.format;
-import static java.lang.String.join;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+@TestInstance(PER_CLASS)
 public class TestZipFunction
-        extends AbstractTestFunctions
 {
+    private QueryAssertions assertions;
+
+    @BeforeAll
+    public void init()
+    {
+        assertions = new QueryAssertions();
+    }
+
+    @AfterAll
+    public void teardown()
+    {
+        assertions.close();
+        assertions = null;
+    }
+
     @Test
     public void testSameLength()
     {
-        assertFunction("zip(ARRAY[1, 2], ARRAY['a', 'b'])",
-                zipReturnType(INTEGER, createVarcharType(1)),
-                list(list(1, "a"), list(2, "b")));
+        assertThat(assertions.function("zip", "ARRAY[1, 2]", "ARRAY['a', 'b']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1)))
+                .isEqualTo(list(list(1, "a"), list(2, "b")));
 
-        assertFunction("zip(ARRAY[1, 2], ARRAY['a', VARCHAR 'b'])",
-                zipReturnType(INTEGER, VARCHAR),
-                list(list(1, "a"), list(2, "b")));
+        assertThat(assertions.function("zip", "ARRAY[1, 2]", "ARRAY['a', VARCHAR 'b']"))
+                .hasType(zipReturnType(INTEGER, VARCHAR))
+                .isEqualTo(list(list(1, "a"), list(2, "b")));
 
-        assertFunction("zip(ARRAY[1, 2, 3, 4], ARRAY['a', 'b', 'c', 'd'])",
-                zipReturnType(INTEGER, createVarcharType(1)),
-                list(list(1, "a"), list(2, "b"), list(3, "c"), list(4, "d")));
+        assertThat(assertions.function("zip", "ARRAY[1, 2, 3, 4]", "ARRAY['a', 'b', 'c', 'd']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1)))
+                .isEqualTo(list(list(1, "a"), list(2, "b"), list(3, "c"), list(4, "d")));
 
-        assertFunction("zip(ARRAY[1, 2], ARRAY['a', 'b'],  ARRAY['c', 'd'])",
-                zipReturnType(INTEGER, createVarcharType(1), createVarcharType(1)),
-                list(list(1, "a", "c"), list(2, "b", "d")));
+        assertThat(assertions.function("zip", "ARRAY[1, 2]", "ARRAY['a', 'b']", " ARRAY['c', 'd']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1), createVarcharType(1)))
+                .isEqualTo(list(list(1, "a", "c"), list(2, "b", "d")));
 
-        assertFunction("zip(ARRAY[1, 2], ARRAY['a', 'b'],  ARRAY['c', 'd'], ARRAY['e', 'f'])",
-                zipReturnType(INTEGER, createVarcharType(1), createVarcharType(1), createVarcharType(1)),
-                list(list(1, "a", "c", "e"), list(2, "b", "d", "f")));
+        assertThat(assertions.function("zip", "ARRAY[1, 2]", "ARRAY['a', 'b']", "ARRAY['c', 'd']", "ARRAY['e', 'f']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1), createVarcharType(1), createVarcharType(1)))
+                .isEqualTo(list(list(1, "a", "c", "e"), list(2, "b", "d", "f")));
 
-        assertFunction("zip(ARRAY[], ARRAY[])",
-                zipReturnType(UNKNOWN, UNKNOWN),
-                list());
+        assertThat(assertions.function("zip", "ARRAY[]", "ARRAY[]"))
+                .hasType(zipReturnType(UNKNOWN, UNKNOWN))
+                .isEqualTo(list());
 
-        assertFunction("zip(ARRAY[], ARRAY[], ARRAY[])",
-                zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN),
-                list());
+        assertThat(assertions.function("zip", "ARRAY[]", "ARRAY[]", "ARRAY[]"))
+                .hasType(zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN))
+                .isEqualTo(list());
 
-        assertFunction("zip(ARRAY[], ARRAY[], ARRAY[], ARRAY[])",
-                zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN),
-                list());
+        assertThat(assertions.function("zip", "ARRAY[]", "ARRAY[]", "ARRAY[]", "ARRAY[]"))
+                .hasType(zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN))
+                .isEqualTo(list());
 
-        assertFunction("zip(ARRAY[NULL], ARRAY[NULL])",
-                zipReturnType(UNKNOWN, UNKNOWN),
-                list(list(null, null)));
+        assertThat(assertions.function("zip", "ARRAY[NULL]", "ARRAY[NULL]"))
+                .hasType(zipReturnType(UNKNOWN, UNKNOWN))
+                .isEqualTo(list(list(null, null)));
 
-        assertFunction("zip(ARRAY[ARRAY[1, 1], ARRAY[1, 2]], ARRAY[ARRAY[2, 1], ARRAY[2, 2]])",
-                zipReturnType(new ArrayType(INTEGER), new ArrayType(INTEGER)),
-                list(list(list(1, 1), list(2, 1)), list(list(1, 2), list(2, 2))));
+        assertThat(assertions.function("zip", "ARRAY[ARRAY[1, 1], ARRAY[1, 2]]", "ARRAY[ARRAY[2, 1], ARRAY[2, 2]]"))
+                .hasType(zipReturnType(new ArrayType(INTEGER), new ArrayType(INTEGER)))
+                .isEqualTo(list(list(list(1, 1), list(2, 1)), list(list(1, 2), list(2, 2))));
     }
 
     @Test
     public void testDifferentLength()
     {
-        assertFunction("zip(ARRAY[1], ARRAY['a', 'b'])",
-                zipReturnType(INTEGER, createVarcharType(1)),
-                list(list(1, "a"), list(null, "b")));
+        assertThat(assertions.function("zip", "ARRAY[1]", "ARRAY['a', 'b']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1)))
+                .isEqualTo(list(list(1, "a"), list(null, "b")));
 
-        assertFunction("zip(ARRAY[NULL, 2], ARRAY['a'])",
-                zipReturnType(INTEGER, createVarcharType(1)),
-                list(list(null, "a"), list(2, null)));
+        assertThat(assertions.function("zip", "ARRAY[NULL, 2]", "ARRAY['a']"))
+                .hasType(zipReturnType(INTEGER, createVarcharType(1)))
+                .isEqualTo(list(list(null, "a"), list(2, null)));
 
-        assertFunction("zip(ARRAY[], ARRAY[1], ARRAY[1, 2], ARRAY[1, 2, 3])",
-                zipReturnType(UNKNOWN, INTEGER, INTEGER, INTEGER),
-                list(list(null, 1, 1, 1), list(null, null, 2, 2), list(null, null, null, 3)));
+        assertThat(assertions.function("zip", "ARRAY[]", "ARRAY[1]", "ARRAY[1, 2]", "ARRAY[1, 2, 3]"))
+                .hasType(zipReturnType(UNKNOWN, INTEGER, INTEGER, INTEGER))
+                .isEqualTo(list(list(null, 1, 1, 1), list(null, null, 2, 2), list(null, null, null, 3)));
 
-        assertFunction("zip(ARRAY[], ARRAY[NULL], ARRAY[NULL, NULL])",
-                zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN),
-                list(list(null, null, null), list(null, null, null)));
+        assertThat(assertions.function("zip", "ARRAY[]", "ARRAY[NULL]", "ARRAY[NULL, NULL]"))
+                .hasType(zipReturnType(UNKNOWN, UNKNOWN, UNKNOWN))
+                .isEqualTo(list(list(null, null, null), list(null, null, null)));
     }
 
     @Test
     public void testWithNull()
     {
-        assertFunction("zip(CAST(NULL AS ARRAY(UNKNOWN)), ARRAY[],  ARRAY[1])",
-                zipReturnType(UNKNOWN, UNKNOWN, INTEGER),
-                null);
+        assertThat(assertions.function("zip", "CAST(NULL AS ARRAY(UNKNOWN))", "ARRAY[]", "ARRAY[1]"))
+                .isNull(zipReturnType(UNKNOWN, UNKNOWN, INTEGER));
     }
 
     @Test
     public void testAllArities()
     {
         for (int arity = MIN_ARITY; arity <= MAX_ARITY; arity++) {
-            String[] arguments = IntStream.rangeClosed(1, arity)
+            List<String> arguments = IntStream.rangeClosed(1, arity)
                     .mapToObj(index -> "ARRAY[" + index + "]")
-                    .toArray(String[]::new);
+                    .collect(toList());
             Type[] types = IntStream.rangeClosed(1, arity)
                     .mapToObj(index -> INTEGER)
                     .toArray(Type[]::new);
-            assertFunction(
-                    format("zip(%s)", join(", ", list(arguments))),
-                    zipReturnType(types),
-                    list(IntStream.rangeClosed(1, arity).boxed().collect(toList())));
+            assertThat(assertions.function("zip", arguments))
+                    .hasType(zipReturnType(types))
+                    .isEqualTo(list(IntStream.rangeClosed(1, arity).boxed().collect(toList())));
         }
     }
 
