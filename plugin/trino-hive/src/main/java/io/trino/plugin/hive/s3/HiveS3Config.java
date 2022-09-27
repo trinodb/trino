2e605a38d94ef49c76403355d9e0942638435358
skrzypo987 <krzysztof.skrzypczynski@starburstdata.com>
2022-09-27 09:58:34 +0200

    Add option to force S3 region to connect to

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/s3/HiveS3Config.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/s3/HiveS3Config.java
@@ -42,6 +42,7 @@ public class HiveS3Config
     private String s3AwsAccessKey;
     private String s3AwsSecretKey;
     private String s3Endpoint;
+    private String s3Region;
     private TrinoS3StorageClass s3StorageClass = TrinoS3StorageClass.STANDARD;
     private TrinoS3SignerType s3SignerType;
     private String s3SignerClass;
@@ -115,6 +116,18 @@ public class HiveS3Config
     public HiveS3Config setS3Endpoint(String s3Endpoint)
     {
         this.s3Endpoint = s3Endpoint;
+        return this;
+    }
+
+    public String getS3Region()
+    {
+        return s3Region;
+    }
+
+    @Config("hive.s3.region")
+    public HiveS3Config setS3Region(String s3Region)
+    {
+        this.s3Region = s3Region;
         return this;
     }
 
