1b45107bf2287f55b13d3aa220a30ecc17f52fba
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-09-29 13:50:03 +0200

    Move Trino to Iceberg value conversion utility method
    Move `ExpressionConverter.getIcebergLiteralValue` to `IcebergTypes` for
    re-use. Name it `convertTrinoValueToIceberg` to be consistent with
    existing `convertIcebergValueToTrino` method in that class.

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergTypes.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergTypes.java
@@ -13,9 +13,21 @@
  */
 package io.trino.plugin.iceberg;
 
+import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
+import io.trino.spi.type.BigintType;
+import io.trino.spi.type.BooleanType;
+import io.trino.spi.type.DateType;
 import io.trino.spi.type.DecimalType;
 import io.trino.spi.type.Decimals;
+import io.trino.spi.type.DoubleType;
+import io.trino.spi.type.Int128;
+import io.trino.spi.type.IntegerType;
+import io.trino.spi.type.LongTimestampWithTimeZone;
+import io.trino.spi.type.RealType;
+import io.trino.spi.type.UuidType;
+import io.trino.spi.type.VarbinaryType;
+import io.trino.spi.type.VarcharType;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 
@@ -26,14 +38,89 @@ import java.util.UUID;
 
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.plugin.iceberg.util.Timestamps.timestampTzFromMicros;
+import static io.trino.plugin.iceberg.util.Timestamps.timestampTzToMicros;
+import static io.trino.spi.type.TimeType.TIME_MICROS;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_MICROS;
+import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
 import static io.trino.spi.type.Timestamps.PICOSECONDS_PER_MICROSECOND;
 import static io.trino.spi.type.UuidType.javaUuidToTrinoUuid;
+import static io.trino.spi.type.UuidType.trinoUuidToJavaUuid;
 import static java.lang.Float.floatToIntBits;
+import static java.lang.Float.intBitsToFloat;
 import static java.lang.Math.multiplyExact;
+import static java.lang.Math.toIntExact;
+import static java.util.Objects.requireNonNull;
 
 public final class IcebergTypes
 {
     private IcebergTypes() {}
+
+    /**
+     * Convert value from Trino representation to Iceberg representation.
+     *
+     * @apiNote This accepts a Trino type because, currently, no two Iceberg types translate to one Trino type.
+     */
+    public static Object convertTrinoValueToIceberg(io.trino.spi.type.Type type, Object trinoNativeValue)
+    {
+        requireNonNull(trinoNativeValue, "trinoNativeValue is null");
+
+        if (type instanceof BooleanType) {
+            return (boolean) trinoNativeValue;
+        }
+
+        if (type instanceof IntegerType) {
+            return toIntExact((long) trinoNativeValue);
+        }
+
+        if (type instanceof BigintType) {
+            return (long) trinoNativeValue;
+        }
+
+        if (type instanceof RealType) {
+            return intBitsToFloat(toIntExact((long) trinoNativeValue));
+        }
+
+        if (type instanceof DoubleType) {
+            return (double) trinoNativeValue;
+        }
+
+        if (type instanceof DateType) {
+            return toIntExact(((Long) trinoNativeValue));
+        }
+
+        if (type.equals(TIME_MICROS)) {
+            return ((long) trinoNativeValue) / PICOSECONDS_PER_MICROSECOND;
+        }
+
+        if (type.equals(TIMESTAMP_MICROS)) {
+            return (long) trinoNativeValue;
+        }
+
+        if (type.equals(TIMESTAMP_TZ_MICROS)) {
+            return timestampTzToMicros((LongTimestampWithTimeZone) trinoNativeValue);
+        }
+
+        if (type instanceof VarcharType) {
+            return ((Slice) trinoNativeValue).toStringUtf8();
+        }
+
+        if (type instanceof VarbinaryType) {
+            return ByteBuffer.wrap(((Slice) trinoNativeValue).getBytes());
+        }
+
+        if (type instanceof UuidType) {
+            return trinoUuidToJavaUuid(((Slice) trinoNativeValue));
+        }
+
+        if (type instanceof DecimalType decimalType) {
+            if (decimalType.isShort()) {
+                return BigDecimal.valueOf((long) trinoNativeValue).movePointLeft(decimalType.getScale());
+            }
+            return new BigDecimal(((Int128) trinoNativeValue).toBigInteger(), decimalType.getScale());
+        }
+
+        throw new UnsupportedOperationException("Unsupported type: " + type);
+    }
 
     /**
      * Convert value from Iceberg representation to Trino representation.
