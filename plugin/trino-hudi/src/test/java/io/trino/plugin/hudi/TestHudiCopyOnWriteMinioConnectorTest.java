846187b7bf12de9d703db9970e7a7155ea72fe1d
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2022-10-02 23:39:08 +0530

    Refactor Hudi BaseConnectorTests
    The base test classes shouldn't provide the QueryRunner to make sure
    that subclasses configure things correctly and the base class is more
    re-usable by just providing the TestingConnectorBehavior and any test
    overrides as needed.

--- plugin/trino-hudi/src/test/java/io/trino/plugin/hudi/TestHudiCopyOnWriteMinioConnectorTest.java
+++ plugin/trino-hudi/src/test/java/io/trino/plugin/hudi/TestHudiCopyOnWriteMinioConnectorTest.java
@@ -13,13 +13,34 @@
  */
 package io.trino.plugin.hudi;
 
+import com.google.common.collect.ImmutableMap;
+import io.trino.plugin.hive.containers.HiveMinioDataLake;
+import io.trino.plugin.hudi.testing.TpchHudiTablesInitializer;
+import io.trino.testing.QueryRunner;
+
+import static io.trino.plugin.hudi.S3HudiQueryRunner.HUDI_MINIO_TESTS;
+import static io.trino.testing.sql.TestTable.randomTableSuffix;
 import static org.apache.hudi.common.model.HoodieTableType.COPY_ON_WRITE;
 
 public class TestHudiCopyOnWriteMinioConnectorTest
         extends BaseHudiMinioConnectorTest
 {
-    public TestHudiCopyOnWriteMinioConnectorTest()
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
     {
-        super(COPY_ON_WRITE);
+        String bucketName = "test-hudi-connector-" + randomTableSuffix();
+        hiveMinioDataLake = closeAfterClass(new HiveMinioDataLake(bucketName));
+        hiveMinioDataLake.start();
+        hiveMinioDataLake.getMinioClient().ensureBucketExists(bucketName);
+
+        return S3HudiQueryRunner.create(
+                HUDI_MINIO_TESTS.getCatalogName(),
+                HUDI_MINIO_TESTS.getSchemaName(),
+                ImmutableMap.<String, String>builder()
+                        .put("hudi.columns-to-hide", columnsToHide())
+                        .buildOrThrow(),
+                new TpchHudiTablesInitializer(COPY_ON_WRITE, REQUIRED_TPCH_TABLES),
+                hiveMinioDataLake);
     }
 }
