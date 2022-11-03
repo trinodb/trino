43f58d81373784be06632c8072a4c31c063d2952
Manish Malhotra <osscm.bunny@gmail.com>
2022-11-03 16:00:54 -0400

    Support the Iceberg `sorted_by` table property
    Co-authored-by: Alex Jo <jo.alex2144@gmail.com>

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/TrinoCatalog.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/TrinoCatalog.java
@@ -23,6 +23,7 @@ import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.security.TrinoPrincipal;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.SortOrder;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.Transaction;
 
@@ -70,6 +71,7 @@ public interface TrinoCatalog
             SchemaTableName schemaTableName,
             Schema schema,
             PartitionSpec partitionSpec,
+            SortOrder sortOrder,
             String location,
             Map<String, String> properties);
 
