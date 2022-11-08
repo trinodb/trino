9590a989209d7624e0f33cb00b0d9d9cd714201a
Yuya Ebihara <ebyhry@gmail.com>
2022-11-08 18:38:01 +0900

    Fix broken TestDeltaLakeSharedGlueMetastoreWithTableRedirections.testShowSchemas
    Glue tests didn't run for 442e31c628 because the PR came from forked repository.

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakeSharedGlueMetastoreWithTableRedirections.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakeSharedGlueMetastoreWithTableRedirections.java
@@ -111,7 +111,6 @@ public class TestDeltaLakeSharedGlueMetastoreWithTableRedirections
     protected String getExpectedHiveCreateSchema(String catalogName)
     {
         String expectedHiveCreateSchema = "CREATE SCHEMA %s.%s\n" +
-                "AUTHORIZATION ROLE public\n" +
                 "WITH (\n" +
                 "   location = '%s'\n" +
                 ")";
