edb29e0c6b7c78c7f708ebf1b2bec9b8eac51cca
Michiel De Smet <michiel.desmet@starburstdata.com>
2022-11-08 14:04:32 +0100

    Round `TimeWithTimeZone` when `supportsParametricDateTime` is `false`

--- core/trino-spi/src/test/java/io/trino/spi/type/TestSqlTimeWithTimeZone.java
+++ core/trino-spi/src/test/java/io/trino/spi/type/TestSqlTimeWithTimeZone.java
@@ -43,4 +43,38 @@ public class TestSqlTimeWithTimeZone
         assertThat(SqlTimeWithTimeZone.newInstance(12, 0, 0))
                 .isNotEqualTo(SqlTimeWithTimeZone.newInstance(12, 0, 1));
     }
+
+    @Test
+    public void testRoundTo()
+    {
+        // round down
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(0)).isEqualTo(SqlTimeWithTimeZone.newInstance(0, 0, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(1)).isEqualTo(SqlTimeWithTimeZone.newInstance(1, 100000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(2)).isEqualTo(SqlTimeWithTimeZone.newInstance(2, 110000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(3)).isEqualTo(SqlTimeWithTimeZone.newInstance(3, 111000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(4)).isEqualTo(SqlTimeWithTimeZone.newInstance(4, 111100000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(5)).isEqualTo(SqlTimeWithTimeZone.newInstance(5, 111110000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(6)).isEqualTo(SqlTimeWithTimeZone.newInstance(6, 111111000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(7)).isEqualTo(SqlTimeWithTimeZone.newInstance(7, 111111100000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(8)).isEqualTo(SqlTimeWithTimeZone.newInstance(8, 111111110000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(9)).isEqualTo(SqlTimeWithTimeZone.newInstance(9, 111111111000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(10)).isEqualTo(SqlTimeWithTimeZone.newInstance(10, 111111111100L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(11)).isEqualTo(SqlTimeWithTimeZone.newInstance(11, 111111111110L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1).roundTo(12)).isEqualTo(SqlTimeWithTimeZone.newInstance(12, 111111111111L, 1));
+
+        // round up
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(0)).isEqualTo(SqlTimeWithTimeZone.newInstance(0, 1000000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(1)).isEqualTo(SqlTimeWithTimeZone.newInstance(1, 600000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(2)).isEqualTo(SqlTimeWithTimeZone.newInstance(2, 560000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(3)).isEqualTo(SqlTimeWithTimeZone.newInstance(3, 556000000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(4)).isEqualTo(SqlTimeWithTimeZone.newInstance(4, 555600000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(5)).isEqualTo(SqlTimeWithTimeZone.newInstance(5, 555560000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(6)).isEqualTo(SqlTimeWithTimeZone.newInstance(6, 555556000000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(7)).isEqualTo(SqlTimeWithTimeZone.newInstance(7, 555555600000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(8)).isEqualTo(SqlTimeWithTimeZone.newInstance(8, 555555560000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(9)).isEqualTo(SqlTimeWithTimeZone.newInstance(9, 555555556000L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(10)).isEqualTo(SqlTimeWithTimeZone.newInstance(10, 555555555600L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(11)).isEqualTo(SqlTimeWithTimeZone.newInstance(11, 555555555560L, 1));
+        assertThat(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1).roundTo(12)).isEqualTo(SqlTimeWithTimeZone.newInstance(12, 555555555555L, 1));
+    }
 }
