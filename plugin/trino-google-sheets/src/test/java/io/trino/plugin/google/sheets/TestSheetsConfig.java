a7c7fee74ff244e42bc3258a1cea096a05903fc3
Yuya Ebihara <ebyhry@gmail.com>
2022-11-16 10:42:56 +0900

    Rename config properties in Google Sheets

--- plugin/trino-google-sheets/src/test/java/io/trino/plugin/google/sheets/TestSheetsConfig.java
+++ plugin/trino-google-sheets/src/test/java/io/trino/plugin/google/sheets/TestSheetsConfig.java
@@ -23,6 +23,7 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import static io.airlift.configuration.testing.ConfigAssertions.assertDeprecatedEquivalence;
 import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
 import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
 import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
@@ -46,10 +47,10 @@ public class TestSheetsConfig
         Path credentialsFile = Files.createTempFile(null, null);
 
         Map<String, String> properties = ImmutableMap.<String, String>builder()
-                .put("credentials-path", credentialsFile.toString())
-                .put("metadata-sheet-id", "foo_bar_sheet_id#Sheet1")
-                .put("sheets-data-max-cache-size", "2000")
-                .put("sheets-data-expire-after-write", "10m")
+                .put("gsheets.credentials-path", credentialsFile.toString())
+                .put("gsheets.metadata-sheet-id", "foo_bar_sheet_id#Sheet1")
+                .put("gsheets.max-data-cache-size", "2000")
+                .put("gsheets.data-cache-ttl", "10m")
                 .buildOrThrow();
 
         SheetsConfig expected = new SheetsConfig()
@@ -59,5 +60,28 @@ public class TestSheetsConfig
                 .setSheetsDataExpireAfterWrite(new Duration(10, TimeUnit.MINUTES));
 
         assertFullMapping(properties, expected);
+    }
+
+    @Test
+    public void testLegacyPropertyMappings()
+            throws IOException
+    {
+        Path credentialsFile = Files.createTempFile(null, null);
+
+        Map<String, String> properties = ImmutableMap.<String, String>builder()
+                .put("gsheets.credentials-path", credentialsFile.toString())
+                .put("gsheets.metadata-sheet-id", "foo_bar_sheet_id#Sheet1")
+                .put("gsheets.max-data-cache-size", "2000")
+                .put("gsheets.data-cache-ttl", "10m")
+                .buildOrThrow();
+
+        Map<String, String> oldProperties = ImmutableMap.<String, String>builder()
+                .put("credentials-path", credentialsFile.toString())
+                .put("metadata-sheet-id", "foo_bar_sheet_id#Sheet1")
+                .put("sheets-data-max-cache-size", "2000")
+                .put("sheets-data-expire-after-write", "10m")
+                .buildOrThrow();
+
+        assertDeprecatedEquivalence(SheetsConfig.class, properties, oldProperties);
     }
 }
