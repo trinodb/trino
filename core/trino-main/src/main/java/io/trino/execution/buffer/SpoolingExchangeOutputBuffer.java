53fccff7cf182858d853e3631c0397f9e7b8b7e4
Andrii Rosa <andriyrosa@gmail.com>
2022-11-18 14:46:17 -0500

    Revert "Collect uncompressed data size for output statistics"
    This reverts commit d54734207b54fdc434d529a2c3b224682b370cde.

--- core/trino-main/src/main/java/io/trino/execution/buffer/SpoolingExchangeOutputBuffer.java
+++ core/trino-main/src/main/java/io/trino/execution/buffer/SpoolingExchangeOutputBuffer.java
@@ -32,7 +32,6 @@ import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.util.concurrent.Futures.immediateVoidFuture;
 import static io.airlift.concurrent.MoreFutures.toListenableFuture;
 import static io.trino.execution.buffer.PagesSerde.getSerializedPagePositionCount;
-import static io.trino.execution.buffer.PagesSerde.getSerializedPageUncompressedSizeInBytes;
 import static java.util.Objects.requireNonNull;
 
 @ThreadSafe
@@ -194,7 +193,7 @@ public class SpoolingExchangeOutputBuffer
         checkState(sink != null, "exchangeSink is null");
         long dataSizeInBytes = 0;
         for (Slice page : pages) {
-            dataSizeInBytes += getSerializedPageUncompressedSizeInBytes(page);
+            dataSizeInBytes += page.length();
             sink.add(partition, page);
             totalRowsAdded.addAndGet(getSerializedPagePositionCount(page));
         }
