19ad1c62a675f7ffa22d3f595ecabcd9bd23a39a
skrzypo987 <59822059+skrzypo987@users.noreply.github.com>
2022-11-26 07:38:10 +0100

    Update Trino version to 403 93 g97543f6fdc (#8529)
    Co-authored-by: Anton Tanasenko <anton.tanasenko@starburstdata.com>
    Co-authored-by: Vlad Lyutenko <vlad.lyutenko@starburstdata.com>
    (cherry-picked from commit: c38d8c81bc3a3024a482a55a6f0079e15b034cce)

--- plugins/starburst-stargate/src/test/java/com/starburstdata/trino/plugin/stargate/TestStargateWithHiveConnectorTest.java
+++ plugins/starburst-stargate/src/test/java/com/starburstdata/trino/plugin/stargate/TestStargateWithHiveConnectorTest.java
@@ -10,6 +10,7 @@
 package com.starburstdata.trino.plugin.stargate;
 
 import io.trino.Session;
+import io.trino.plugin.jdbc.JoinOperator;
 import io.trino.sql.planner.plan.FilterNode;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.TestingConnectorBehavior;
@@ -441,11 +442,12 @@ public class TestStargateWithHiveConnectorTest
      * tables using the Remote Starburst instance.
      */
     @Override
-    public void testJoinPushdown()
+    @Test(dataProvider = "joinOperators")
+    public void testJoinPushdown(JoinOperator joinOperator)
     {
         // Make sure that the test still fails how we expect it to, on table creation instead
         // of on join pushdown.
-        assertThatThrownBy(super::testJoinPushdown)
+        assertThatThrownBy(() -> super.testJoinPushdown(joinOperator))
                 .hasMessageMatching("This connector does not support creating tables.*");
         throw new SkipException("test requires table creation");
     }
