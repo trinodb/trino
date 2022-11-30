0aaab67639b07cddc1ac7f3af6cc54af6a4de4c0
kasiafi <30203062+kasiafi@users.noreply.github.com>
2022-11-30 13:03:21 +0100

    Analyze table function's required input columns

--- core/trino-main/src/main/java/io/trino/sql/analyzer/StatementAnalyzer.java
+++ core/trino-main/src/main/java/io/trino/sql/analyzer/StatementAnalyzer.java
@@ -292,6 +292,7 @@ import static io.trino.spi.StandardErrorCode.DUPLICATE_RANGE_VARIABLE;
 import static io.trino.spi.StandardErrorCode.DUPLICATE_WINDOW_NAME;
 import static io.trino.spi.StandardErrorCode.EXPRESSION_NOT_CONSTANT;
 import static io.trino.spi.StandardErrorCode.EXPRESSION_NOT_IN_DISTINCT;
+import static io.trino.spi.StandardErrorCode.FUNCTION_IMPLEMENTATION_ERROR;
 import static io.trino.spi.StandardErrorCode.FUNCTION_NOT_FOUND;
 import static io.trino.spi.StandardErrorCode.FUNCTION_NOT_WINDOW;
 import static io.trino.spi.StandardErrorCode.INVALID_ARGUMENTS;
@@ -1570,6 +1571,35 @@ class StatementAnalyzer
                 properColumnsDescriptor = ((DescribedTable) returnTypeSpecification).getDescriptor();
             }
 
+            // validate the required input columns
+            Map<String, List<Integer>> requiredColumns = functionAnalysis.getRequiredColumns();
+            Map<String, TableArgumentAnalysis> tableArgumentsByName = argumentsAnalysis.getTableArgumentAnalyses().stream()
+                    .collect(toImmutableMap(TableArgumentAnalysis::getArgumentName, Function.identity()));
+            Set<String> allInputs = ImmutableSet.copyOf(tableArgumentsByName.keySet());
+            requiredColumns.forEach((name, columns) -> {
+                if (!allInputs.contains(name)) {
+                    throw new TrinoException(FUNCTION_IMPLEMENTATION_ERROR, format("Table function %s specifies required columns from table argument %s which cannot be found", node.getName(), name));
+                }
+                if (columns.isEmpty()) {
+                    throw new TrinoException(FUNCTION_IMPLEMENTATION_ERROR, format("Table function %s specifies empty list of required columns from table argument %s", node.getName(), name));
+                }
+                // the scope is recorded, because table arguments are already analyzed
+                Scope inputScope = analysis.getScope(tableArgumentsByName.get(name).getRelation());
+                columns.stream()
+                        .filter(column -> column < 0 || column >= inputScope.getRelationType().getAllFieldCount()) // hidden columns can be required as well as visible columns
+                        .findFirst()
+                        .ifPresent(column -> {
+                            throw new TrinoException(FUNCTION_IMPLEMENTATION_ERROR, format("Invalid index: %s of required column from table argument %s", column, name));
+                        });
+            });
+            Set<String> requiredInputs = ImmutableSet.copyOf(requiredColumns.keySet());
+            allInputs.stream()
+                    .filter(input -> !requiredInputs.contains(input))
+                    .findFirst()
+                    .ifPresent(input -> {
+                        throw new TrinoException(FUNCTION_IMPLEMENTATION_ERROR, format("Table function %s does not specify required input columns from table argument %s", node.getName(), input));
+                    });
+
             // The result relation type of a table function consists of:
             // 1. columns created by the table function, called the proper columns.
             // 2. passed columns from input tables:
@@ -1590,8 +1620,6 @@ class StatementAnalyzer
                     .filter(argumentSpecification -> argumentSpecification instanceof TableArgumentSpecification)
                     .map(ArgumentSpecification::getName)
                     .collect(toImmutableList());
-            Map<String, TableArgumentAnalysis> tableArgumentsByName = argumentsAnalysis.getTableArgumentAnalyses().stream()
-                    .collect(toImmutableMap(TableArgumentAnalysis::getArgumentName, Function.identity()));
 
             // table arguments in order of argument declarations
             ImmutableList.Builder<TableArgumentAnalysis> orderedTableArguments = ImmutableList.builder();
@@ -1616,6 +1644,7 @@ class StatementAnalyzer
                     function.getName(),
                     argumentsAnalysis.getPassedArguments(),
                     orderedTableArguments.build(),
+                    functionAnalysis.getRequiredColumns(),
                     copartitioningLists,
                     properColumnsDescriptor == null ? 0 : properColumnsDescriptor.getFields().size(),
                     functionAnalysis.getHandle(),
