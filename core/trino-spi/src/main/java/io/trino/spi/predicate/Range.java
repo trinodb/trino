020a1bc46a61468236c6384dd94f14543ca5f9a3
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-12-05 20:16:43 +0100

    Prevent accidental operator lookup when creating Range
    Remove convenience constructor that takes no `comparisonOperator` and
    looks it up, preventing future code from using it accidentally, where
    an operator is known.

--- core/trino-spi/src/main/java/io/trino/spi/predicate/Range.java
+++ core/trino-spi/src/main/java/io/trino/spi/predicate/Range.java
@@ -44,11 +44,6 @@ public final class Range
     private final MethodHandle comparisonOperator;
     private final boolean isSingleValue;
 
-    Range(Type type, boolean lowInclusive, Optional<Object> lowValue, boolean highInclusive, Optional<Object> highValue)
-    {
-        this(type, lowInclusive, lowValue, highInclusive, highValue, getComparisonOperator(type));
-    }
-
     Range(Type type, boolean lowInclusive, Optional<Object> lowValue, boolean highInclusive, Optional<Object> highValue, MethodHandle comparisonOperator)
     {
         requireNonNull(type, "type is null");
@@ -104,45 +99,45 @@ public final class Range
 
     public static Range all(Type type)
     {
-        return new Range(type, false, Optional.empty(), false, Optional.empty());
+        return new Range(type, false, Optional.empty(), false, Optional.empty(), getComparisonOperator(type));
     }
 
     public static Range greaterThan(Type type, Object low)
     {
         requireNonNull(low, "low is null");
-        return new Range(type, false, Optional.of(low), false, Optional.empty());
+        return new Range(type, false, Optional.of(low), false, Optional.empty(), getComparisonOperator(type));
     }
 
     public static Range greaterThanOrEqual(Type type, Object low)
     {
         requireNonNull(low, "low is null");
-        return new Range(type, true, Optional.of(low), false, Optional.empty());
+        return new Range(type, true, Optional.of(low), false, Optional.empty(), getComparisonOperator(type));
     }
 
     public static Range lessThan(Type type, Object high)
     {
         requireNonNull(high, "high is null");
-        return new Range(type, false, Optional.empty(), false, Optional.of(high));
+        return new Range(type, false, Optional.empty(), false, Optional.of(high), getComparisonOperator(type));
     }
 
     public static Range lessThanOrEqual(Type type, Object high)
     {
         requireNonNull(high, "high is null");
-        return new Range(type, false, Optional.empty(), true, Optional.of(high));
+        return new Range(type, false, Optional.empty(), true, Optional.of(high), getComparisonOperator(type));
     }
 
     public static Range equal(Type type, Object value)
     {
         requireNonNull(value, "value is null");
         Optional<Object> valueAsOptional = Optional.of(value);
-        return new Range(type, true, valueAsOptional, true, valueAsOptional);
+        return new Range(type, true, valueAsOptional, true, valueAsOptional, getComparisonOperator(type));
     }
 
     public static Range range(Type type, Object low, boolean lowInclusive, Object high, boolean highInclusive)
     {
         requireNonNull(low, "low is null");
         requireNonNull(high, "high is null");
-        return new Range(type, lowInclusive, Optional.of(low), highInclusive, Optional.of(high));
+        return new Range(type, lowInclusive, Optional.of(low), highInclusive, Optional.of(high), getComparisonOperator(type));
     }
 
     public Type getType()
