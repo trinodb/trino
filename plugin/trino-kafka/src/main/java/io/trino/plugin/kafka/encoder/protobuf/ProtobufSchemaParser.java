8d4b25e6ff9c20422c238d203e52e3d16d5f08e0
Maxim Lukyanenko <maxim.lukyanenko@starburstdata.com>
2022-12-05 23:09:05 +0200

    Prevent protobuf schema message parsing dead loop recursion
    Kafka protobuf schema registry parsing falls to dead loop if has included references to the same object.
    It's need to abort the recursion loop with appropriated parsing error.

--- plugin/trino-kafka/src/main/java/io/trino/plugin/kafka/encoder/protobuf/ProtobufSchemaParser.java
+++ plugin/trino-kafka/src/main/java/io/trino/plugin/kafka/encoder/protobuf/ProtobufSchemaParser.java
@@ -21,18 +21,22 @@ import io.trino.decoder.protobuf.ProtobufRowDecoder;
 import io.trino.plugin.kafka.KafkaTopicFieldDescription;
 import io.trino.plugin.kafka.KafkaTopicFieldGroup;
 import io.trino.plugin.kafka.schema.confluent.SchemaParser;
+import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.MapType;
 import io.trino.spi.type.RowType;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.TypeManager;
+import org.pcollections.Empty;
+import org.pcollections.PSet;
 
 import javax.inject.Inject;
 
 import java.util.Optional;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.BooleanType.BOOLEAN;
 import static io.trino.spi.type.DoubleType.DOUBLE;
@@ -41,6 +45,7 @@ import static io.trino.spi.type.RealType.REAL;
 import static io.trino.spi.type.TimestampType.createTimestampType;
 import static io.trino.spi.type.VarbinaryType.VARBINARY;
 import static io.trino.spi.type.VarcharType.createUnboundedVarcharType;
+import static java.lang.String.join;
 import static java.util.Objects.requireNonNull;
 
 public class ProtobufSchemaParser
@@ -66,7 +71,7 @@ public class ProtobufSchemaParser
                 protobufSchema.toDescriptor().getFields().stream()
                         .map(field -> new KafkaTopicFieldDescription(
                                 field.getName(),
-                                getType(field),
+                                getType(field, Empty.orderedSet()),
                                 field.getName(),
                                 null,
                                 null,
@@ -75,7 +80,7 @@ public class ProtobufSchemaParser
                         .collect(toImmutableList()));
     }
 
-    private Type getType(FieldDescriptor fieldDescriptor)
+    private Type getType(FieldDescriptor fieldDescriptor, PSet<String> processedMessages)
     {
         Type baseType = switch (fieldDescriptor.getJavaType()) {
             case BOOLEAN -> BOOLEAN;
@@ -85,31 +90,39 @@ public class ProtobufSchemaParser
             case DOUBLE -> DOUBLE;
             case BYTE_STRING -> VARBINARY;
             case STRING, ENUM -> createUnboundedVarcharType();
-            case MESSAGE -> getTypeForMessage(fieldDescriptor);
+            case MESSAGE -> getTypeForMessage(fieldDescriptor, processedMessages);
         };
 
-        // Protobuf does not support adding repeated label for map type but schema registry incorrecty adds it
+        // Protobuf does not support adding repeated label for map type but schema registry incorrectly adds it
         if (fieldDescriptor.isRepeated() && !fieldDescriptor.isMapField()) {
             return new ArrayType(baseType);
         }
         return baseType;
     }
 
-    private Type getTypeForMessage(FieldDescriptor fieldDescriptor)
+    private Type getTypeForMessage(FieldDescriptor fieldDescriptor, PSet<String> processedMessages)
     {
         Descriptor descriptor = fieldDescriptor.getMessageType();
         if (descriptor.getFullName().equals(TIMESTAMP_TYPE_NAME)) {
             return createTimestampType(6);
         }
+
+        if (processedMessages.contains(descriptor.getFullName())) {
+            throw new TrinoException(NOT_SUPPORTED, "Cannot parse registry schema for nested object with the same object reference: %s > %s"
+                    .formatted(join(" > ", processedMessages),
+                    descriptor.getFullName()));
+        }
+        PSet<String> newProcessedMessages = processedMessages.plus(descriptor.getFullName());
+
         if (fieldDescriptor.isMapField()) {
             return new MapType(
-                    getType(descriptor.findFieldByNumber(1)),
-                    getType(descriptor.findFieldByNumber(2)),
+                    getType(descriptor.findFieldByNumber(1), newProcessedMessages),
+                    getType(descriptor.findFieldByNumber(2), newProcessedMessages),
                     typeManager.getTypeOperators());
         }
         return RowType.from(
                 descriptor.getFields().stream()
-                        .map(field -> RowType.field(field.getName(), getType(field)))
+                        .map(field -> RowType.field(field.getName(), getType(field, newProcessedMessages)))
                         .collect(toImmutableList()));
     }
 }
