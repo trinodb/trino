3c6845bad9cebda9551ce0c562ffc95b0d04dca8
Anu Sudarsan <anu.at.infy@gmail.com>
2022-12-28 13:32:20 -0500

    Override tests using smaller tables to avoid timeout
    Synapse FTE tests were hitting the 2h timeout, so use smaller tables in FTE Tests.
    
    (cherry-picked from commit: e38344087adcb4813fa4bfdebd2a1d8f65a8ec3e)

--- plugins/starburst-synapse/src/test/java/com/starburstdata/trino/plugins/synapse/faulttolerant/BaseSynapseFailureRecoveryTest.java
+++ plugins/starburst-synapse/src/test/java/com/starburstdata/trino/plugins/synapse/faulttolerant/BaseSynapseFailureRecoveryTest.java
@@ -11,15 +11,19 @@ package com.starburstdata.trino.plugins.synapse.faulttolerant;
 
 import com.google.common.collect.ImmutableMap;
 import com.starburstdata.trino.plugins.synapse.SynapseServer;
+import io.trino.execution.FailureInjector;
 import io.trino.operator.RetryPolicy;
 import io.trino.plugin.exchange.filesystem.FileSystemExchangePlugin;
 import io.trino.plugin.jdbc.BaseJdbcFailureRecoveryTest;
 import io.trino.testing.QueryRunner;
 import io.trino.testng.services.ManageTestResources;
 import io.trino.tpch.TpchTable;
+import org.assertj.core.api.Assertions;
+import org.testng.SkipException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.starburstdata.trino.plugins.synapse.SynapseQueryRunner.DEFAULT_CATALOG_NAME;
 import static com.starburstdata.trino.plugins.synapse.SynapseQueryRunner.createSynapseQueryRunner;
@@ -55,5 +59,85 @@ public abstract class BaseSynapseFailureRecoveryTest
                     runner.loadExchangeManager("filesystem", ImmutableMap.of(
                             "exchange.base-directories", System.getProperty("java.io.tmpdir") + "/trino-local-file-system-exchange-manager"));
                 });
+    }
+
+    @Override
+    public void testExplainAnalyze()
+    {
+        this.testSelect("EXPLAIN ANALYZE SELECT regionkey, count(*) FROM nation GROUP BY regionkey");
+        this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation WITH NO DATA"), "EXPLAIN ANALYZE INSERT INTO <table> SELECT * FROM nation", Optional.of("DROP TABLE <table>"));
+    }
+
+    @Override
+    public void testCreateTable()
+    {
+        this.testTableModification(Optional.empty(), "CREATE TABLE <table> AS SELECT * FROM nation", Optional.of("DROP TABLE <table>"));
+    }
+
+    @Override
+    public void testInsert()
+    {
+        this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation WITH NO DATA"), "INSERT INTO <table> SELECT * FROM nation", Optional.of("DROP TABLE <table>"));
+    }
+
+    @Override
+    public void testDeleteWithSubquery()
+    {
+        Assertions.assertThatThrownBy(() -> {
+            this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation"), "DELETE FROM <table> WHERE nationkey IN (SELECT custkey FROM customer WHERE nationkey = 1)", Optional.of("DROP TABLE <table>"));
+        }).hasMessageContaining("This connector does not support modifying table rows");
+        throw new SkipException("skipped");
+    }
+
+    @Override
+    public void testRefreshMaterializedView()
+    {
+        Assertions.assertThatThrownBy(() -> {
+            this.testTableModification(Optional.of("CREATE MATERIALIZED VIEW <table> AS SELECT * FROM nation"), "REFRESH MATERIALIZED VIEW <table>", Optional.of("DROP MATERIALIZED VIEW <table>"));
+        }).hasMessageContaining("This connector does not support creating materialized views");
+        throw new SkipException("skipped");
+    }
+
+    @Override
+    public void testUpdate()
+    {
+        Assertions.assertThatThrownBy(() -> {
+            this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation"), "UPDATE <table> SET name = 'BRASIL' WHERE nationkey = 2", Optional.of("DROP TABLE <table>"));
+        }).hasMessageContaining("This connector does not support modifying table rows");
+        throw new SkipException("skipped");
+    }
+
+    @Override
+    public void testUpdateWithSubquery()
+    {
+        Assertions.assertThatThrownBy(() -> {
+            this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation"), "UPDATE <table> SET name = 'Brasil' WHERE nationkey = (SELECT min(custkey) + 1 FROM customer)", Optional.of("DROP TABLE <table>"));
+        }).hasMessageContaining("This connector does not support modifying table rows");
+        throw new SkipException("skipped");
+    }
+
+    @Override
+    public void testMerge()
+    {
+        Assertions.assertThatThrownBy(() -> {
+            this.testTableModification(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation"), "MERGE INTO <table> t\nUSING (SELECT nationkey, 'new_nation' name FROM <table>) s\nON t.nationkey = s.nationkey\nWHEN MATCHED AND s.nationkey > 10\n    THEN UPDATE SET name = t.name || s.name\nWHEN MATCHED AND s.nationkey <= 10\n    THEN DELETE\n", Optional.of("DROP TABLE <table>"));
+        }).hasMessageContaining("This connector does not support modifying table rows");
+        throw new SkipException("skipped");
+    }
+
+    @Override
+    public void testRequestTimeouts()
+    {
+        this.assertThatQuery("SELECT * FROM nation").experiencing(FailureInjector.InjectedFailureType.TASK_MANAGEMENT_REQUEST_TIMEOUT).at(leafStage()).failsWithoutRetries((failure) -> {
+            failure.hasMessageContaining("Encountered too many errors talking to a worker node");
+        }).finishesSuccessfully();
+        this.assertThatQuery("SELECT * FROM nation").experiencing(FailureInjector.InjectedFailureType.TASK_MANAGEMENT_REQUEST_TIMEOUT).at(boundaryDistributedStage()).failsWithoutRetries((failure) -> {
+            failure.hasMessageContaining("Encountered too many errors talking to a worker node");
+        }).finishesSuccessfully();
+        if (this.areWriteRetriesSupported()) {
+            this.assertThatQuery("INSERT INTO <table> SELECT * FROM nation").withSetupQuery(Optional.of("CREATE TABLE <table> AS SELECT * FROM nation WITH NO DATA")).withCleanupQuery(Optional.of("DROP TABLE <table>")).experiencing(FailureInjector.InjectedFailureType.TASK_GET_RESULTS_REQUEST_TIMEOUT).at(leafStage()).failsWithoutRetries((failure) -> {
+                failure.hasMessageFindingMatch("Encountered too many errors talking to a worker node|Error closing remote buffer");
+            }).finishesSuccessfullyWithoutTaskFailures();
+        }
     }
 }
