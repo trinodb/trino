66586a3f102320594de025d97b733373ad31c9a0
Maxim Lukyanenko <maxim.lukyanenko@starburstdata.com>
2022-12-29 14:57:50 +0200

    Add tests Hive to Hudi table redirections

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/TestGroups.java
@@ -50,6 +50,7 @@ public final class TestGroups
     public static final String HIVE_VIEW_COMPATIBILITY = "hive_view_compatibility";
     public static final String HIVE_CACHING = "hive_caching";
     public static final String HIVE_ICEBERG_REDIRECTIONS = "hive_iceberg_redirections";
+    public static final String HIVE_HUDI_REDIRECTIONS = "hive_hudi_redirections";
     public static final String AUTHORIZATION = "authorization";
     public static final String HIVE_COERCION = "hive_coercion";
     public static final String AZURE = "azure";
