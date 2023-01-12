31d4091e55e08cc8dfb47c1416df9db9720555e3
praveenkrishna <praveenkrishna@tutanota.com>
2023-01-12 11:34:33 +0100

    Remove default constructor for DefaultQueryBuilder
    Defensive check to ensure classes that extend DefaultQueryBuilder
    requires RemoteQueryModifier to be injected/specified during creation.

--- plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestingH2JdbcClient.java
+++ plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestingH2JdbcClient.java
@@ -90,7 +90,7 @@ class TestingH2JdbcClient
 
     public TestingH2JdbcClient(BaseJdbcConfig config, ConnectionFactory connectionFactory, IdentifierMapping identifierMapping)
     {
-        super(config, "\"", connectionFactory, new DefaultQueryBuilder(), identifierMapping, RemoteQueryModifier.NONE);
+        super(config, "\"", connectionFactory, new DefaultQueryBuilder(RemoteQueryModifier.NONE), identifierMapping, RemoteQueryModifier.NONE);
     }
 
     @Override
