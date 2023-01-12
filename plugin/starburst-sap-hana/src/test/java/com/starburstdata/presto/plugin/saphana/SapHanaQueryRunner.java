9f827fb95f5f5c652555827bffdae8c5510a6ce9
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2023-01-12 21:10:21 +0100

    Merge StarburstDistributedQueryRunner with StarburstQueryRunner

--- plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/SapHanaQueryRunner.java
+++ plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/SapHanaQueryRunner.java
@@ -11,7 +11,7 @@ package com.starburstdata.presto.plugin.saphana;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.starburstdata.presto.testing.StarburstDistributedQueryRunner;
+import com.starburstdata.presto.server.StarburstQueryRunner;
 import io.airlift.log.Logger;
 import io.airlift.log.Logging;
 import io.trino.Session;
@@ -50,7 +50,7 @@ public final class SapHanaQueryRunner
     {
         DistributedQueryRunner queryRunner = null;
         try {
-            DistributedQueryRunner.Builder<?> builder = StarburstDistributedQueryRunner.builder(createSession());
+            DistributedQueryRunner.Builder<?> builder = StarburstQueryRunner.builder(createSession());
             extraProperties.forEach(builder::addExtraProperty);
             queryRunner = builder.build();
 
