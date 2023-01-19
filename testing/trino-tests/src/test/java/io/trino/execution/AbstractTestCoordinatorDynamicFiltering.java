60bd797cb11d6099454def3e59658e5579d7f4e4
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-01-19 15:44:48 +0530

    Add dynamic filtering test with multi column join
    Tests with a build side containing a high cardinality
    as well as a low cardinality column

--- testing/trino-tests/src/test/java/io/trino/execution/AbstractTestCoordinatorDynamicFiltering.java
+++ testing/trino-tests/src/test/java/io/trino/execution/AbstractTestCoordinatorDynamicFiltering.java
@@ -143,6 +143,19 @@ public abstract class AbstractTestCoordinatorDynamicFiltering
     }
 
     @Test(timeOut = 30_000, dataProvider = "testJoinDistributionType")
+    public void testMultiColumnJoinWithDifferentCardinalitiesInBuildSide(JoinDistributionType joinDistributionType, boolean coordinatorDynamicFiltersDistribution)
+    {
+        // orderkey has high cardinality, suppkey has low cardinality due to filter
+        assertQueryDynamicFilters(
+                noJoinReordering(joinDistributionType, coordinatorDynamicFiltersDistribution),
+                "SELECT * FROM lineitem l1 " +
+                        "JOIN tpch.tiny.lineitem l2 ON l1.orderkey = l2.orderkey AND l1.suppkey = l2.suppkey " +
+                        "WHERE l2.suppkey BETWEEN 1 AND 10",
+                Set.of(ORDERKEY_HANDLE, SUPP_KEY_HANDLE),
+                TupleDomain.all());
+    }
+
+    @Test(timeOut = 30_000, dataProvider = "testJoinDistributionType")
     public void testJoinWithSelectiveBuildSide(JoinDistributionType joinDistributionType, boolean coordinatorDynamicFiltersDistribution)
     {
         assertQueryDynamicFilters(
