24c722d0a7e84b295020ca2673438159bd03b93f
Michał Ślizak <michal.slizak+github@gmail.com>
2023-02-05 00:24:44 +0100

    Add argument verification to check-commits jobs

--- .github/bin/prepare-check-commits-matrix.py
+++ .github/bin/prepare-check-commits-matrix.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import json
 import logging
 import sys
 import tempfile
 import unittest
 
-import csv
-import json
-
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Choose commits to compile using their CSV descriptions."
-    )
+    parser = argparse.ArgumentParser(description="Choose commits to compile using their CSV descriptions.")
     parser.add_argument(
         "-i",
         "--input",
@@ -41,7 +37,7 @@ def main():
     parser.add_argument(
         "-t",
         "--test",
-        action='store_true',
+        action="store_true",
         help="test this script instead of executing it",
     )
 
@@ -71,13 +67,17 @@ def build(input_file, output_file):
             # ignore lines which don't match the expected CSV pattern
             pass
     if len(commits) > 0:
-        json.dump({'include': [{'commit': commit} for commit in commits]}, output_file, separators=(',', ':'), sort_keys=True)
+        json.dump(
+            {"include": [{"commit": commit} for commit in commits]}, output_file, separators=(",", ":"), sort_keys=True
+        )
 
 
 def has_followup(entries, i, commit_hash, subject):
-    for later_entry in entries[i+1:]:
+    for later_entry in entries[i + 1 :]:
         _, _, later_subject = later_entry
-        if later_subject.startswith(('fixup! ', 'squash! ', 'amend! ')) and (subject in later_subject or commit_hash in later_subject):
+        if later_subject.startswith(("fixup! ", "squash! ", "amend! ")) and (
+            subject in later_subject
+        ):
             return True
     return False
 
@@ -87,32 +87,16 @@ class TestBuild(unittest.TestCase):
 
     def test_build(self):
         cases = [
-            (
-                "Empty test",
-                (),
-                []
-            ),
-            (
-                "Malformed input test",
-                (
-                    "c1,t1\n"
-                ),
-                []
-            ),
-            (
-                "Basic test",
-                (
-                    "c1,t1,Hello World\n",
-                ),
-                ['{"include":[{"commit":"c1"}]}']
-            ),
+            ("Empty test", (), []),
+            ("Malformed input test", ("c1,t1\n"), []),
+            ("Basic test", ("c1,t1,Hello World\n",), ['{"include":[{"commit":"c1"}]}']),
             (
                 "Add a new entry",
                 (
                     "c1,t1,Hello World\n",
                     "c2,t2,Quick brown fox\n",
                 ),
-                ['{"include":[{"commit":"c1"},{"commit":"c2"}]}']
+                ['{"include":[{"commit":"c1"},{"commit":"c2"}]}'],
             ),
             (
                 "Add a fixup",
@@ -121,7 +105,7 @@ class TestBuild(unittest.TestCase):
                     "c2,t2,Quick brown fox\n",
                     "c3,t3,fixup! Hello World - fixed a bug\n",
                 ),
-                ['{"include":[{"commit":"c2"},{"commit":"c3"}]}']
+                ['{"include":[{"commit":"c2"},{"commit":"c3"}]}'],
             ),
             (
                 "Add a floating fixup",
@@ -130,7 +114,7 @@ class TestBuild(unittest.TestCase):
                     "c2,t2,Quick brown fox\n",
                     "c3,t3,fixup! Unknown commits are fun!\n",
                 ),
-                ['{"include":[{"commit":"c1"},{"commit":"c2"},{"commit":"c3"}]}']
+                ['{"include":[{"commit":"c1"},{"commit":"c2"},{"commit":"c3"}]}'],
             ),
             (
                 "Add a squash",
@@ -140,7 +124,7 @@ class TestBuild(unittest.TestCase):
                     "c3,t3,fixup! Hello World - fixed a bug\n",
                     "c4,t4,squash! Quick brown fox - refactoring\n",
                 ),
-                ['{"include":[{"commit":"c3"},{"commit":"c4"}]}']
+                ['{"include":[{"commit":"c3"},{"commit":"c4"}]}'],
             ),
             (
                 "Add an amend",
@@ -151,83 +135,45 @@ class TestBuild(unittest.TestCase):
                     "c4,t4,squash! Quick brown fox - refactoring\n",
                     "c5,t5,amend! fixup! Hello World - fixed a bug\n",
                 ),
-                ['{"include":[{"commit":"c4"},{"commit":"c5"}]}']
-            ),
-            (
-                "Add a fixup using commit hash",
-                (
-                    "c1,t1,Hello World\n",
-                    "c2,t2,Quick brown fox\n",
-                    "c3,t3,fixup! Hello World - fixed a bug\n",
-                    "c4,t4,squash! Quick brown fox - refactoring\n",
-                    "c5,t5,amend! fixup! Hello World - fixed a bug\n",
-                    "c6,t6,fixup! c3\n",
-                ),
-                ['{"include":[{"commit":"c4"},{"commit":"c5"},{"commit":"c6"}]}']
-            ),
-            (
-                "Add a squash using commit hash",
-                (
-                    "c1,t1,Hello World\n",
-                    "c2,t2,Quick brown fox\n",
-                    "c3,t3,fixup! Hello World - fixed a bug\n",
-                    "c4,t4,squash! Quick brown fox - refactoring\n",
-                    "c5,t5,amend! fixup! Hello World - fixed a bug\n",
-                    "c6,t6,fixup! c3\n",
-                    "c7,t7,squash! c4\n",
-                ),
-                ['{"include":[{"commit":"c5"},{"commit":"c6"},{"commit":"c7"}]}']
-            ),
-            (
-                "Add an amend using commit hash",
-                (
-                    "c1,t1,Hello World\n",
-                    "c2,t2,Quick brown fox\n",
-                    "c3,t3,fixup! Hello World - fixed a bug\n",
-                    "c4,t4,squash! Quick brown fox - refactoring\n",
-                    "c5,t5,amend! fixup! Hello World - fixed a bug\n",
-                    "c6,t6,fixup! c3\n",
-                    "c7,t7,squash! c4\n",
-                    "c8,t8,amend! c5\n",
-                ),
-                ['{"include":[{"commit":"c6"},{"commit":"c7"},{"commit":"c8"}]}']
+                ['{"include":[{"commit":"c4"},{"commit":"c5"}]}'],
             ),
             (
                 "Quoted newline test",
-                (
-                    'c1,t1,"Hello\nWorld"\n',
-                    'c2,t2,"fixup! Hello\nWorld"\n'
-                ),
-                ['{"include":[{"commit":"c2"}]}']
+                ('c1,t1,"Hello\nWorld"\n', 'c2,t2,"fixup! Hello\nWorld"\n'),
+                ['{"include":[{"commit":"c2"}]}'],
             ),
             (
                 "Quoted quote test",
-                (
-                    'c1,t1,""Hello"World"\n',
-                    'c2,t2,"fixup! "Hello"World"\n'
-                ),
-                ['{"include":[{"commit":"c2"}]}']
+                ('c1,t1,""Hello"World"\n', 'c2,t2,"fixup! "Hello"World"\n'),
+                ['{"include":[{"commit":"c2"}]}'],
             ),
             (
                 "Performance test",
                 # O(n^2) case for our algorithm is a list of commits where the last commit is the fixup to the first one, the second last to the second, etc.
                 # Generate the commits that will be fixed later
-                [f"c{i},t{i},Fix me\n" for i in range(self.PERFORMANCE_TEST_SIZE)] +
-                    # Followed by fixups
-                    [f"c{self.PERFORMANCE_TEST_SIZE + i},t{self.PERFORMANCE_TEST_SIZE + i},fixup! c{self.PERFORMANCE_TEST_SIZE - (i + 1)}\n" for i in range(self.PERFORMANCE_TEST_SIZE)],
+                [f"c{i},t{i},title{i}\n" for i in range(self.PERFORMANCE_TEST_SIZE)] +
+                # Followed by fixups
+                [
+                    f"c{self.PERFORMANCE_TEST_SIZE + i},t{self.PERFORMANCE_TEST_SIZE + i},fixup! title{self.PERFORMANCE_TEST_SIZE - (i + 1)}\n"
+                    for i in range(self.PERFORMANCE_TEST_SIZE)
+                ],
                 # Expect fixups to survive
-                [f'{{"include":[{self.calculate_expected(range(self.PERFORMANCE_TEST_SIZE, 2 * self.PERFORMANCE_TEST_SIZE))}]}}']
+                [
+                    f'{{"include":[{self.calculate_expected(range(self.PERFORMANCE_TEST_SIZE, 2 * self.PERFORMANCE_TEST_SIZE))}]}}'
+                ],
             ),
         ]
         for test_name, input_lines, expected in cases:
-            with self.subTest(test_name), tempfile.TemporaryFile("w+") as input_file, tempfile.TemporaryFile("w+") as output_file:
+            with self.subTest(test_name), tempfile.TemporaryFile("w+") as input_file, tempfile.TemporaryFile(
+                "w+"
+            ) as output_file:
                 # given
                 input_file.writelines(input_lines)
                 input_file.seek(0)
                 # when
                 build(input_file, output_file)
                 output_file.seek(0)
-                output = [line.rstrip() for line in output_file.readlines()]    # Remove trailing newlines
+                output = [line.rstrip() for line in output_file.readlines()]  # Remove trailing newlines
                 # then
                 self.assertEqual(output, expected)
 
