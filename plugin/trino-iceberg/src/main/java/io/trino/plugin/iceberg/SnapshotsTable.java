83b7b089a8fcf62d8bad83e9c53a015dd21010f0
Vikash Kumar <vikash.kumar@starburstdata.com>
2023-02-21 14:33:34 +0530

    Avoid materializing all results in Iceberg `$snapshots` system table at once

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/SnapshotsTable.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/SnapshotsTable.java
@@ -14,38 +14,45 @@
 package io.trino.plugin.iceberg;
 
 import com.google.common.collect.ImmutableList;
-import io.trino.plugin.iceberg.util.PageListBuilder;
-import io.trino.spi.Page;
+import io.trino.spi.block.BlockBuilder;
 import io.trino.spi.connector.ColumnMetadata;
-import io.trino.spi.connector.ConnectorPageSource;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorTableMetadata;
 import io.trino.spi.connector.ConnectorTransactionHandle;
-import io.trino.spi.connector.FixedPageSource;
+import io.trino.spi.connector.InMemoryRecordSet;
+import io.trino.spi.connector.RecordCursor;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.connector.SystemTable;
 import io.trino.spi.predicate.TupleDomain;
 import io.trino.spi.type.TimeZoneKey;
+import io.trino.spi.type.Type;
 import io.trino.spi.type.TypeManager;
-import io.trino.spi.type.TypeSignature;
+import org.apache.iceberg.Snapshot;
 import org.apache.iceberg.Table;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.trino.spi.type.BigintType.BIGINT;
+import static io.trino.spi.type.DateTimeEncoding.packDateTimeWithZone;
 import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MILLIS;
+import static io.trino.spi.type.TypeSignature.mapType;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 
 public class SnapshotsTable
         implements SystemTable
 {
+    private final TypeManager typeManager;
     private final ConnectorTableMetadata tableMetadata;
     private final Table icebergTable;
 
     public SnapshotsTable(SchemaTableName tableName, TypeManager typeManager, Table icebergTable)
     {
-        requireNonNull(typeManager, "typeManager is null");
+        this.typeManager = requireNonNull(typeManager, "typeManager is null");
 
         this.icebergTable = requireNonNull(icebergTable, "icebergTable is null");
         tableMetadata = new ConnectorTableMetadata(requireNonNull(tableName, "tableName is null"),
@@ -55,7 +62,7 @@ public class SnapshotsTable
                         .add(new ColumnMetadata("parent_id", BIGINT))
                         .add(new ColumnMetadata("operation", VARCHAR))
                         .add(new ColumnMetadata("manifest_list", VARCHAR))
-                        .add(new ColumnMetadata("summary", typeManager.getType(TypeSignature.mapType(VARCHAR.getTypeSignature(), VARCHAR.getTypeSignature()))))
+                        .add(new ColumnMetadata("summary", typeManager.getType(mapType(VARCHAR.getTypeSignature(), VARCHAR.getTypeSignature()))))
                         .build());
     }
 
@@ -72,44 +79,79 @@ public class SnapshotsTable
     }
 
     @Override
-    public ConnectorPageSource pageSource(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
+    public RecordCursor cursor(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
     {
-        return new FixedPageSource(buildPages(tableMetadata, session, icebergTable));
+        List<io.trino.spi.type.Type> types = tableMetadata.getColumns().stream()
+                .map(ColumnMetadata::getType)
+                .collect(toImmutableList());
+
+        SnapshotsIterable snapshotsIterable = new SnapshotsIterable(icebergTable.snapshots(), types, session.getTimeZoneKey());
+        return snapshotsIterable.cursor();
     }
 
-    private static List<Page> buildPages(ConnectorTableMetadata tableMetadata, ConnectorSession session, Table icebergTable)
+    private class SnapshotsIterable
+            implements Iterable<List<Object>>
     {
-        PageListBuilder pagesBuilder = PageListBuilder.forTable(tableMetadata);
+        private final Iterable<Snapshot> snapshots;
+        private final List<Type> types;
+        private final TimeZoneKey timeZoneKey;
+        private final Type summaryMapType;
 
-        TimeZoneKey timeZoneKey = session.getTimeZoneKey();
-        icebergTable.snapshots().forEach(snapshot -> {
-            pagesBuilder.beginRow();
-            pagesBuilder.appendTimestampTzMillis(snapshot.timestampMillis(), timeZoneKey);
-            pagesBuilder.appendBigint(snapshot.snapshotId());
-            if (checkNonNull(snapshot.parentId(), pagesBuilder)) {
-                pagesBuilder.appendBigint(snapshot.parentId());
-            }
-            if (checkNonNull(snapshot.operation(), pagesBuilder)) {
-                pagesBuilder.appendVarchar(snapshot.operation());
-            }
-            if (checkNonNull(snapshot.manifestListLocation(), pagesBuilder)) {
-                pagesBuilder.appendVarchar(snapshot.manifestListLocation());
-            }
-            if (checkNonNull(snapshot.summary(), pagesBuilder)) {
-                pagesBuilder.appendVarcharVarcharMap(snapshot.summary());
-            }
-            pagesBuilder.endRow();
-        });
-
-        return pagesBuilder.build();
-    }
-
-    private static boolean checkNonNull(Object object, PageListBuilder pagesBuilder)
-    {
-        if (object == null) {
-            pagesBuilder.appendNull();
-            return false;
+        public SnapshotsIterable(Iterable<Snapshot> snapshots, List<Type> types, TimeZoneKey timeZoneKey)
+        {
+            this.snapshots = requireNonNull(snapshots, "snapshots is null");
+            this.types = requireNonNull(types, "types is null");
+            this.timeZoneKey = requireNonNull(timeZoneKey, "timeZoneKey is null");
+            this.summaryMapType = typeManager.getType(mapType(VARCHAR.getTypeSignature(), VARCHAR.getTypeSignature()));
         }
-        return true;
+
+        public RecordCursor cursor()
+        {
+            return new InMemoryRecordSet.InMemoryRecordCursor(types, this.iterator());
+        }
+
+        @Override
+        public Iterator<List<Object>> iterator()
+        {
+            Iterator<Snapshot> snapshotsIterator = snapshots.iterator();
+
+            return new Iterator<>() {
+                @Override
+                public boolean hasNext()
+                {
+                    return snapshotsIterator.hasNext();
+                }
+
+                @Override
+                public List<Object> next()
+                {
+                    return getRecord(snapshotsIterator.next());
+                }
+            };
+        }
+
+        private List<Object> getRecord(Snapshot snapshot)
+        {
+            List<Object> columns = new ArrayList<>();
+            columns.add(packDateTimeWithZone(snapshot.timestampMillis(), timeZoneKey));
+            columns.add(snapshot.snapshotId());
+            columns.add(snapshot.parentId());
+            columns.add(snapshot.operation());
+            columns.add(snapshot.manifestListLocation());
+            columns.add(getSummaryMap(snapshot.summary()));
+            return columns;
+        }
+
+        private Object getSummaryMap(Map<String, String> values)
+        {
+            BlockBuilder blockBuilder = summaryMapType.createBlockBuilder(null, 1);
+            BlockBuilder singleMapBlockBuilder = blockBuilder.beginBlockEntry();
+            values.forEach((key, value) -> {
+                VARCHAR.writeString(singleMapBlockBuilder, key);
+                VARCHAR.writeString(singleMapBlockBuilder, value);
+            });
+            blockBuilder.closeEntry();
+            return summaryMapType.getObject(blockBuilder, 0);
+        }
     }
 }
