3f6d84c8aa9bbcdff98bd5b9b6d077d774ac3b93
Vikash Kumar <vikash.kumar@starburstdata.com>
2023-02-21 12:17:59 +0530

    Use cursor-based implementation for the Iceberg `$properties` system table

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/PropertiesTable.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/PropertiesTable.java
@@ -14,20 +14,16 @@
 package io.trino.plugin.iceberg;
 
 import com.google.common.collect.ImmutableList;
-import io.trino.plugin.iceberg.util.PageListBuilder;
-import io.trino.spi.Page;
 import io.trino.spi.connector.ColumnMetadata;
-import io.trino.spi.connector.ConnectorPageSource;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorTableMetadata;
 import io.trino.spi.connector.ConnectorTransactionHandle;
-import io.trino.spi.connector.FixedPageSource;
+import io.trino.spi.connector.InMemoryRecordSet;
+import io.trino.spi.connector.RecordCursor;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.connector.SystemTable;
 import io.trino.spi.predicate.TupleDomain;
 import org.apache.iceberg.Table;
-
-import java.util.List;
 
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
@@ -62,22 +58,10 @@ public class PropertiesTable
     }
 
     @Override
-    public ConnectorPageSource pageSource(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
+    public RecordCursor cursor(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
     {
-        return new FixedPageSource(buildPages(tableMetadata, icebergTable));
-    }
-
-    private static List<Page> buildPages(ConnectorTableMetadata tableMetadata, Table icebergTable)
-    {
-        PageListBuilder pagesBuilder = PageListBuilder.forTable(tableMetadata);
-
-        icebergTable.properties().entrySet().forEach(prop -> {
-            pagesBuilder.beginRow();
-            pagesBuilder.appendVarchar(prop.getKey());
-            pagesBuilder.appendVarchar(prop.getValue());
-            pagesBuilder.endRow();
-        });
-
-        return pagesBuilder.build();
+        InMemoryRecordSet.Builder table = InMemoryRecordSet.builder(tableMetadata);
+        icebergTable.properties().forEach(table::addRow);
+        return table.build().cursor();
     }
 }
