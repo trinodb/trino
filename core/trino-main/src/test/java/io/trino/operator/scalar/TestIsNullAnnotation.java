ff7f7ed9ac83f6b824bd67f35b1044a7748bfcb2
Martin Traverso <mtraverso@gmail.com>
2023-02-22 21:58:14 -0800

    Migrate TestIsNullAnnotation to new assertions

--- core/trino-main/src/test/java/io/trino/operator/scalar/TestIsNullAnnotation.java
+++ core/trino-main/src/test/java/io/trino/operator/scalar/TestIsNullAnnotation.java
@@ -14,28 +14,44 @@
 package io.trino.operator.scalar;
 
 import io.airlift.slice.Slice;
+import io.trino.metadata.InternalFunctionBundle;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.function.IsNull;
 import io.trino.spi.function.ScalarFunction;
 import io.trino.spi.function.SqlNullable;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.type.StandardTypes;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import io.trino.sql.query.QueryAssertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.airlift.slice.Slices.utf8Slice;
-import static io.trino.spi.type.BigintType.BIGINT;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
 import static io.trino.spi.type.VarcharType.VARCHAR;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+@TestInstance(PER_CLASS)
 public class TestIsNullAnnotation
-        extends AbstractTestFunctions
 {
-    @BeforeClass
-    public void setUp()
+    private QueryAssertions assertions;
+
+    @BeforeAll
+    public void init()
     {
-        registerScalar(getClass());
+        assertions = new QueryAssertions();
+        assertions.addFunctions(InternalFunctionBundle.builder()
+                .scalars(TestIsNullAnnotation.class)
+                .build());
+    }
+
+    @AfterAll
+    public void teardown()
+    {
+        assertions.close();
+        assertions = null;
     }
 
     @ScalarFunction("test_is_null_simple")
@@ -94,17 +110,38 @@ public class TestIsNullAnnotation
     @Test
     public void testIsNull()
     {
-        assertFunction("test_is_null_simple(-100)", BIGINT, -200L);
-        assertFunction("test_is_null_simple(23)", BIGINT, 46L);
-        assertFunction("test_is_null_simple(null)", BIGINT, 100L);
-        assertFunction("test_is_null_simple(cast(null as bigint))", BIGINT, 100L);
+        assertThat(assertions.function("test_is_null_simple", "-100"))
+                .isEqualTo(-200L);
 
-        assertFunction("test_is_null(23, 'aaa', 'bbb', 'ccc')", VARCHAR, "23:aaa:bbb:ccc");
-        assertFunction("test_is_null(null, 'aaa', 'bbb', 'ccc')", VARCHAR, ":aaa:bbb:ccc");
-        assertFunction("test_is_null(null, 'aaa', null, 'ccc')", VARCHAR, ":aaa::ccc");
-        assertFunction("test_is_null(23, 'aaa', null, null)", VARCHAR, "23:aaa::");
-        assertFunction("test_is_null(23, null, 'bbb', 'ccc')", VARCHAR, null);
+        assertThat(assertions.function("test_is_null_simple", "23"))
+                .isEqualTo(46L);
 
-        assertFunction("test_is_null_void(null)", BOOLEAN, true);
+        assertThat(assertions.function("test_is_null_simple", "null"))
+                .isEqualTo(100L);
+
+        assertThat(assertions.function("test_is_null_simple", "cast(null as bigint)"))
+                .isEqualTo(100L);
+
+        assertThat(assertions.function("test_is_null", "23", "'aaa'", "'bbb'", "'ccc'"))
+                .hasType(VARCHAR)
+                .isEqualTo("23:aaa:bbb:ccc");
+
+        assertThat(assertions.function("test_is_null", "null", "'aaa'", "'bbb'", "'ccc'"))
+                .hasType(VARCHAR)
+                .isEqualTo(":aaa:bbb:ccc");
+
+        assertThat(assertions.function("test_is_null", "null", "'aaa'", "null", "'ccc'"))
+                .hasType(VARCHAR)
+                .isEqualTo(":aaa::ccc");
+
+        assertThat(assertions.function("test_is_null", "23", "'aaa'", "null", "null"))
+                .hasType(VARCHAR)
+                .isEqualTo("23:aaa::");
+
+        assertThat(assertions.function("test_is_null", "23", "null", "'bbb'", "'ccc'"))
+                .isNull(VARCHAR);
+
+        assertThat(assertions.function("test_is_null_void", "null"))
+                .isEqualTo(true);
     }
 }
