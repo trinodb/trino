fa6da9560392337202d22ec7becebb480d8802c1
Slawomir Pajak <slawomir.pajak@starburstdata.com>
2023-02-24 13:22:23 +0100

    Add tests for Locations

---
+++ lib/trino-filesystem/src/test/java/io/trino/filesystem/TestLocations.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.filesystem;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static io.trino.filesystem.Locations.appendPath;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TestLocations
+{
+    private static Stream<Arguments> locations()
+    {
+        return Stream.of(
+                Arguments.of("test_dir", "", "test_dir/"),
+                Arguments.of("", "test_file.txt", "/test_file.txt"),
+                Arguments.of("test_dir", "test_file.txt", "test_dir/test_file.txt"),
+                Arguments.of("/test_dir", "test_file.txt", "/test_dir/test_file.txt"),
+                Arguments.of("test_dir/", "test_file.txt", "test_dir/test_file.txt"),
+                Arguments.of("/test_dir/", "test_file.txt", "/test_dir/test_file.txt"),
+                Arguments.of("test_dir", "test_dir2/", "test_dir/test_dir2/"),
+                Arguments.of("test_dir/", "test_dir2/", "test_dir/test_dir2/"),
+                Arguments.of("s3:/test_dir", "test_file.txt", "s3:/test_dir/test_file.txt"),
+                Arguments.of("s3://test_dir", "test_file.txt", "s3://test_dir/test_file.txt"),
+                Arguments.of("s3://test_dir/", "test_file.txt", "s3://test_dir/test_file.txt"),
+                Arguments.of("s3://dir_with_space ", "test_file.txt", "s3://dir_with_space /test_file.txt"),
+                Arguments.of("s3://dir_with_double_space  ", "test_file.txt", "s3://dir_with_double_space  /test_file.txt"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("locations")
+    public void testAppendPath(String location, String path, String expected)
+    {
+        assertThat(appendPath(location, path)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> invalidLocations()
+    {
+        return Stream.of(
+                Arguments.of("location?", "location contains a query string.*"),
+                Arguments.of("location#", "location contains a fragment.*"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidLocations")
+    public void testInvalidLocationInAppendPath(String location, String exceptionMessageRegexp)
+    {
+        assertThatThrownBy(() -> appendPath(location, "test"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching(exceptionMessageRegexp);
+    }
+}
