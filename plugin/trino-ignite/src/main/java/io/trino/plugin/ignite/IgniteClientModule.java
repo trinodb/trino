770c9c4e4004bb6ace51087f52d1254e7a22218d
chenjian2664 <chenjian2664@gmail.com>
2023-02-27 14:30:42 +0800

    Add Ignite jdbc config class to deny invalid connector url

--- plugin/trino-ignite/src/main/java/io/trino/plugin/ignite/IgniteClientModule.java
+++ plugin/trino-ignite/src/main/java/io/trino/plugin/ignite/IgniteClientModule.java
@@ -27,14 +27,10 @@ import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcMetadataFactory;
 import io.trino.plugin.jdbc.credential.CredentialProvider;
 import org.apache.ignite.IgniteJdbcThinDriver;
-import org.apache.ignite.internal.jdbc.thin.JdbcThinUtils;
 
-import java.util.Properties;
-
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
+import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.trino.plugin.jdbc.JdbcModule.bindTablePropertiesProvider;
-import static java.util.Objects.requireNonNull;
 
 public class IgniteClientModule
         implements Module
@@ -44,6 +40,7 @@ public class IgniteClientModule
     {
         binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(IgniteClient.class).in(Scopes.SINGLETON);
         newOptionalBinder(binder, JdbcMetadataFactory.class).setBinding().to(IgniteJdbcMetadataFactory.class).in(Scopes.SINGLETON);
+        configBinder(binder).bindConfig(IgniteJdbcConfig.class);
         bindTablePropertiesProvider(binder, IgniteTableProperties.class);
         binder.install(new DecimalModule());
     }
@@ -53,13 +50,9 @@ public class IgniteClientModule
     @ForBaseJdbc
     public static ConnectionFactory createConnectionFactory(BaseJdbcConfig config, CredentialProvider credentialProvider)
     {
-        // https://github.com/trinodb/trino/issues/16148
-        checkArgument(requireNonNull(config.getConnectionUrl()).startsWith(JdbcThinUtils.URL_PREFIX), "Invalid JDBC URL for Ignite connector, connection url should start with 'jdbc:ignite:thin://', actual get %s", config.getConnectionUrl());
-        Properties connectionProperties = new Properties();
         return new DriverConnectionFactory(
                 new IgniteJdbcThinDriver(),
-                config.getConnectionUrl(),
-                connectionProperties,
+                config,
                 credentialProvider);
     }
 }
