837008e86c5d036f5f4a282ec87c8ba9039139d5
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2023-02-28 20:29:40 +0100

    Simplify license check
    We usually generate always licenses with * anyway. Generation of a license with
    some features enabled and disabled could lead to some undefined
    behaviour. Field people are not even aware of features that we have
    defined here. Unless they are explicit ones.
    
    The next step for license check in SEP is to verify the license before
    server start. In other words license will be required to start the SEP.
    Possibility to run SEP without a license defeats the purpose of using
    SEP as it exposes poor UX.
    
    (cherry-picked from commit: 6dab8df09426c6761a7aac42a111d10fcae04d4c)

--- plugins/starburst-salesforce/src/test/java/com/starburstdata/presto/plugin/salesforce/TestSalesforcePlugin.java
+++ plugins/starburst-salesforce/src/test/java/com/starburstdata/presto/plugin/salesforce/TestSalesforcePlugin.java
@@ -27,7 +27,7 @@ public class TestSalesforcePlugin
         ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
         assertThatThrownBy(() -> factory.create("test", ImmutableMap.of(), new TestingConnectorContext()))
                 .isInstanceOf(RuntimeException.class)
-                .hasStackTraceContaining("StarburstLicenseException: Valid license required to use the feature: salesforce");
+                .hasMessageContaining("Starburst Enterprise requires valid license");
     }
 
     @Test
