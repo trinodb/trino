9809706c3a9029206ef95efe881f35b820aecdbb
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-02-28 09:14:42 +0100

    Remove redundant generics from InMemoryRecordCursor
    This also addresses compiler warnings where the class is used without
    generics.

--- core/trino-spi/src/main/java/io/trino/spi/connector/InMemoryRecordSet.java
+++ core/trino-spi/src/main/java/io/trino/spi/connector/InMemoryRecordSet.java
@@ -68,15 +68,15 @@ public class InMemoryRecordSet
         return new InMemoryRecordCursor(types, records.iterator());
     }
 
-    public static class InMemoryRecordCursor<T extends Iterator<? extends List<?>>>
+    public static class InMemoryRecordCursor
             implements RecordCursor
     {
         private final List<Type> types;
-        protected T records;
+        private Iterator<? extends List<?>> records;
         private List<?> record;
         private long completedBytes;
 
-        protected InMemoryRecordCursor(List<Type> types, T records)
+        protected InMemoryRecordCursor(List<Type> types, Iterator<? extends List<?>> records)
         {
             this.types = types;
             this.records = records;
