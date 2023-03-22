0172e0a052ecc78c6fe195e96553e8b6515bb2f1
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-03-22 11:39:20 +0100

    Remove kill-switch to disable automatic fetch-size selection
    The configuration toggles `xxx.disable-automatic-fetch-size` were
    introduced only temporarily, as a kill-switch.

--- plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestPostgreSqlConfig.java
+++ plugin/trino-postgresql/src/test/java/io/trino/plugin/postgresql/TestPostgreSqlConfig.java
@@ -28,7 +28,6 @@ public class TestPostgreSqlConfig
     public void testDefaults()
     {
         assertRecordedDefaults(recordDefaults(PostgreSqlConfig.class)
-                .setDisableAutomaticFetchSize(false)
                 .setArrayMapping(PostgreSqlConfig.ArrayMapping.DISABLED)
                 .setIncludeSystemTables(false)
                 .setEnableStringPushdownWithCollate(false));
@@ -38,14 +37,12 @@ public class TestPostgreSqlConfig
     public void testExplicitPropertyMappings()
     {
         Map<String, String> properties = ImmutableMap.<String, String>builder()
-                .put("postgresql.disable-automatic-fetch-size", "true")
                 .put("postgresql.array-mapping", "AS_ARRAY")
                 .put("postgresql.include-system-tables", "true")
                 .put("postgresql.experimental.enable-string-pushdown-with-collate", "true")
                 .buildOrThrow();
 
         PostgreSqlConfig expected = new PostgreSqlConfig()
-                .setDisableAutomaticFetchSize(true)
                 .setArrayMapping(PostgreSqlConfig.ArrayMapping.AS_ARRAY)
                 .setIncludeSystemTables(true)
                 .setEnableStringPushdownWithCollate(true);
