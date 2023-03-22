cbceadf01c863b8a6278b400f30ecf409f709664
starburstdata-automation <starburstdata-automation@users.noreply.github.com>
2023-03-22 05:47:49 +0000

    Update Trino version to 410-184-g7bb2f193
    - Fix build
    - Update dependencies
    - jetty-server and jetty-servlet are not used explicitly in SEP, no need
      to override default Airlift version
    - Update Plugins to the same trino version
    - Adapt testNativeQueryColumnAlias
    - Fix error message assertions for testSchemaNameClash
    - Fix warp speed
    - Fix JSON serialization for DirectResource
    - Add missing license
    - Fix tests for json reader
    
    Co-authored-by: Sasha Sheikin <myminitrue@gmail.com>
    Co-authored-by: Patryk Owczarek <patryk.owczarek@starburstdata.com>
    Co-authored-by: shlomi.alfasi <shlomi.alfasi@starburstdata.com>
    Co-authored-by: Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
    Co-authored-by: Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>

--- plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/BaseSapHanaConnectorTest.java
+++ plugin/starburst-sap-hana/src/test/java/com/starburstdata/presto/plugin/saphana/BaseSapHanaConnectorTest.java
@@ -354,6 +354,20 @@ public abstract class BaseSapHanaConnectorTest
                 .hasStackTraceContaining("TrinoException: This connector does not support modifying table rows");
     }
 
+    @Override
+    @Test
+    public void testNativeQueryColumnAlias()
+    {
+        // override because Sap Hana uppercase column names by default
+        assertThat(query(format("SELECT * FROM TABLE(system.query(query => 'SELECT name AS region_name FROM %s.region WHERE regionkey = 0'))", getSession().getSchema().orElseThrow())))
+                .hasColumnNames("REGION_NAME")
+                .matches("VALUES CAST('AFRICA' AS VARCHAR(25))");
+
+        assertThat(query(format("SELECT region_name FROM TABLE(system.query(query => 'SELECT name AS region_name FROM %s.region WHERE regionkey = 0'))", getSession().getSchema().orElseThrow())))
+                .hasColumnNames("region_name")
+                .matches("VALUES CAST('AFRICA' AS VARCHAR(25))");
+    }
+
     @Test
     @Override
     public void testAddNotNullColumnToNonEmptyTable()
