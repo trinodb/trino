fd183d6367dad38895cf6f84c612ae2716846c29
Jan Waś <jan.was@starburstdata.com>
2023-03-22 11:42:40 -0400

    Document the all connectors smoke test suite

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteAllConnectorsSmoke.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteAllConnectorsSmoke.java
@@ -23,6 +23,11 @@ import java.util.List;
 
 import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
 
+/**
+ * Suite that verifies that the cluster starts with as many connectors
+ * enabled as possible. The catalogs do not have to have valid configuration,
+ * so it might not be possible to execute queries using them.
+ */
 public class SuiteAllConnectorsSmoke
         extends Suite
 {
