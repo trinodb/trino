1c24eec750b2e043e032147e4dfb8e82b747e3c9
kasiafi <30203062+kasiafi@users.noreply.github.com>
2023-03-24 14:54:59 +0100

    Support returning anonymous columns by table functions
    Per SQL standard, all columns must be named. In Trino,
    we support unnamed columns.
    This change adjusts table functions so that they can return
    anonymous columns.
    It is achieved by changing the Descriptor structure so that
    field name is optional. This optionality can only be used
    for the returned type of table functions. Descriptor arguments
    pased by the user as well as default values for descriptor
    argumens have mandatory field names.

--- core/trino-spi/src/main/java/io/trino/spi/ptf/DescriptorArgument.java
+++ core/trino-spi/src/main/java/io/trino/spi/ptf/DescriptorArgument.java
@@ -21,6 +21,7 @@ import io.trino.spi.expression.ConnectorExpression;
 import java.util.Objects;
 import java.util.Optional;
 
+import static io.trino.spi.ptf.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -40,6 +41,9 @@ public class DescriptorArgument
     public DescriptorArgument(@JsonProperty("descriptor") Optional<Descriptor> descriptor)
     {
         this.descriptor = requireNonNull(descriptor, "descriptor is null");
+        descriptor.ifPresent(descriptorValue -> checkArgument(
+                descriptorValue.getFields().stream().allMatch(field -> field.getName().isPresent()),
+                "All fields of a descriptor argument must have names"));
     }
 
     @JsonProperty
