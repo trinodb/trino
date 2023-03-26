43d3d2c377460a7405e0e3f5c5c94a3191e9c8ea
Bernd Ahlers <bernd@tuneafish.de>
2023-03-26 14:50:37 +0200

    Add missing AWS dependencies to the Iceberg connector
    The Iceberg connector threw an error when using the REST catalog as
    described in #16213.
    
        Cannot initialize FileIO, missing no-arg constructor:
        org.apache.iceberg.aws.s3.S3FileIO
    
    This change adds the missing AWS SDK v2 dependencies and sets the
    Apache HTTP client option to make the S3FileIO backend work with the
    REST catalog.
    
    The REST catalog is now also configured with the following "hive.s3"
    settings to allow the usage of a custom endpoint.
    
    - "hive.s3.endpoint"
    - "hive.s3.aws-access-key"
    - "hive.s3.aws-secret-key"
    - "hive.s3.path-style-access"
    
    Fixes #16213

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoIcebergRestCatalogFactory.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoIcebergRestCatalogFactory.java
@@ -17,12 +17,14 @@ import com.google.common.collect.ImmutableMap;
 import io.trino.hdfs.ConfigurationUtils;
 import io.trino.plugin.base.CatalogName;
 import io.trino.plugin.hive.NodeVersion;
+import io.trino.plugin.hive.s3.HiveS3Config;
 import io.trino.plugin.iceberg.IcebergConfig;
 import io.trino.plugin.iceberg.catalog.TrinoCatalog;
 import io.trino.plugin.iceberg.catalog.TrinoCatalogFactory;
 import io.trino.plugin.iceberg.catalog.rest.IcebergRestCatalogConfig.SessionType;
 import io.trino.spi.security.ConnectorIdentity;
 import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.aws.AwsProperties;
 import org.apache.iceberg.rest.RESTSessionCatalog;
 
 import javax.annotation.concurrent.GuardedBy;
@@ -40,6 +42,10 @@ public class TrinoIcebergRestCatalogFactory
     private final String trinoVersion;
     private final URI serverUri;
     private final Optional<String> warehouse;
+    private final Optional<String> s3AccessKey;
+    private final Optional<String> s3SecretKey;
+    private final Optional<String> s3Endpoint;
+    private final boolean s3PathStyleAccess;
     private final SessionType sessionType;
     private final SecurityProperties securityProperties;
     private final boolean uniqueTableLocation;
@@ -53,6 +59,7 @@ public class TrinoIcebergRestCatalogFactory
             IcebergRestCatalogConfig restConfig,
             SecurityProperties securityProperties,
             IcebergConfig icebergConfig,
+            HiveS3Config hiveS3Config,
             NodeVersion nodeVersion)
     {
         this.catalogName = requireNonNull(catalogName, "catalogName is null");
@@ -60,6 +67,10 @@ public class TrinoIcebergRestCatalogFactory
         requireNonNull(restConfig, "restConfig is null");
         this.serverUri = restConfig.getBaseUri();
         this.warehouse = restConfig.getWarehouse();
+        this.s3Endpoint = Optional.ofNullable(hiveS3Config.getS3Endpoint());
+        this.s3PathStyleAccess = hiveS3Config.isS3PathStyleAccess();
+        this.s3AccessKey = Optional.ofNullable(hiveS3Config.getS3AwsAccessKey());
+        this.s3SecretKey = Optional.ofNullable(hiveS3Config.getS3AwsSecretKey());
         this.sessionType = restConfig.getSessionType();
         this.securityProperties = requireNonNull(securityProperties, "securityProperties is null");
         requireNonNull(icebergConfig, "icebergConfig is null");
@@ -75,6 +86,11 @@ public class TrinoIcebergRestCatalogFactory
             ImmutableMap.Builder<String, String> properties = ImmutableMap.builder();
             properties.put(CatalogProperties.URI, serverUri.toString());
             warehouse.ifPresent(location -> properties.put(CatalogProperties.WAREHOUSE_LOCATION, location));
+            s3Endpoint.ifPresent(endpoint -> properties.put(AwsProperties.S3FILEIO_ENDPOINT, endpoint));
+            s3AccessKey.ifPresent(accessKey -> properties.put(AwsProperties.S3FILEIO_ACCESS_KEY_ID, accessKey));
+            s3SecretKey.ifPresent(secretKey -> properties.put(AwsProperties.S3FILEIO_SECRET_ACCESS_KEY, secretKey));
+            properties.put(AwsProperties.S3FILEIO_PATH_STYLE_ACCESS, String.valueOf(s3PathStyleAccess));
+            properties.put(AwsProperties.HTTP_CLIENT_TYPE, AwsProperties.HTTP_CLIENT_TYPE_APACHE);
             properties.put("trino-version", trinoVersion);
             properties.putAll(securityProperties.get());
             RESTSessionCatalog icebergCatalogInstance = new RESTSessionCatalog();
