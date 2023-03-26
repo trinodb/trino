267eea45a579f48a482c4c5958e236a89ddd53c2
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-03-26 10:59:39 +0530

    Improve size accounting of SingleLongDecimalWithOverflowAndLongState

--- core/trino-main/src/main/java/io/trino/operator/aggregation/state/LongDecimalWithOverflowAndLongStateFactory.java
+++ core/trino-main/src/main/java/io/trino/operator/aggregation/state/LongDecimalWithOverflowAndLongStateFactory.java
@@ -19,6 +19,7 @@ import io.trino.spi.function.AccumulatorStateFactory;
 
 import javax.annotation.Nullable;
 
+import static io.airlift.slice.SizeOf.SIZE_OF_LONG;
 import static io.airlift.slice.SizeOf.instanceSize;
 import static io.airlift.slice.SizeOf.sizeOf;
 import static java.lang.System.arraycopy;
@@ -139,7 +140,7 @@ public class LongDecimalWithOverflowAndLongStateFactory
             implements LongDecimalWithOverflowAndLongState
     {
         private static final int INSTANCE_SIZE = instanceSize(SingleLongDecimalWithOverflowAndLongState.class);
-        private static final int SIZE = (int) sizeOf(new long[2]);
+        private static final int SIZE = (int) sizeOf(new long[2]) + SIZE_OF_LONG + SIZE_OF_LONG;
 
         private final long[] unscaledDecimal = new long[2];
         private long longValue;
