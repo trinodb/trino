32671c175167481448054eea1f4614607f6ca633
Slawomir Pajak <slawomir.pajak@starburstdata.com>
2023-03-27 11:04:46 +0200

    Move data description to README file

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakeBasic.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/TestDeltaLakeBasic.java
@@ -148,13 +148,7 @@ public class TestDeltaLakeBasic
     @Test
     public void testNoColumnStats()
     {
-        // Data generated using:
-        // CREATE TABLE no_column_stats
-        // USING delta
-        // LOCATION 's3://starburst-alex/delta/no_column_stats'
-        // TBLPROPERTIES (delta.dataSkippingNumIndexedCols=0)   -- collects only table stats (row count), but no column stats
-        // AS
-        // SELECT 42 AS c_int, 'foo' AS c_str
+        // The table was created with delta.dataSkippingNumIndexedCols=0 property
         assertQuery("SELECT c_str FROM no_column_stats WHERE c_int = 42", "VALUES 'foo'");
     }
 
