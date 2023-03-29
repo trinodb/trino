ecf0f9b2732ddeae9d9b571b3012ae1f6faecb46
Konrad Dziedzic <konraddziedzic@gmail.com>
2023-03-29 20:52:49 +0200

    Make LeafTableFunctionOperator handle EmptySplit correctly

--- testing/trino-tests/src/test/java/io/trino/tests/TestTableFunctionInvocation.java
+++ testing/trino-tests/src/test/java/io/trino/tests/TestTableFunctionInvocation.java
@@ -731,6 +731,11 @@ public class TestTableFunctionInvocation
                 FROM TABLE(system.constant(2, 1000000))
                 """))
                 .matches("VALUES (BIGINT '1000000', BIGINT '1', 2)");
+
+        assertQueryReturnsEmptyResult("""
+                SELECT *
+                FROM TABLE(system.constant(5, 0))
+                """);
     }
 
     @Test
