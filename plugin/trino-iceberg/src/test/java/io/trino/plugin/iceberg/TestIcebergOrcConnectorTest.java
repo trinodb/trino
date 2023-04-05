4983bbe8eb1ba75f492bd7322ca6b3344db98748
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-04-05 11:36:21 +0200

    Extract helper for testTinyintType and testSmallintType
    Also add a note about resource file size having to match the generated
    file size so that we can replace one with the other.

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergOrcConnectorTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergOrcConnectorTest.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.io.Resources.getResource;
 import static io.trino.plugin.iceberg.IcebergFileFormat.ORC;
 import static io.trino.plugin.iceberg.IcebergTestUtils.checkOrcFileSorting;
@@ -59,31 +60,27 @@ public class TestIcebergOrcConnectorTest
     public void testTinyintType()
             throws Exception
     {
-        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_tinyint", "(\"_col0\") AS VALUES (127), (NULL)")) {
-            Path orcFilePath = Path.of((String) computeScalar(format("SELECT DISTINCT file_path FROM \"%s$files\"", table.getName())));
-            Files.copy(new File(getResource("single-tinyint-column.orc").toURI()).toPath(), orcFilePath, REPLACE_EXISTING);
-            Files.delete(orcFilePath.resolveSibling(format(".%s.crc", orcFilePath.getFileName())));
-
-            assertThat(query("DESCRIBE " + table.getName()))
-                    .projected("Type")
-                    .matches("VALUES varchar 'integer'");
-            assertQuery("SELECT * FROM " + table.getName(), "VALUES 127, NULL");
-        }
+        testReadSingleIntegerColumnOrcFile(101, "single-tinyint-column.orc", 127);
     }
 
     @Test
     public void testSmallintType()
             throws Exception
     {
-        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_smallint", "(\"_col0\") AS VALUES (32767), (NULL)")) {
+        testReadSingleIntegerColumnOrcFile(31234, "single-smallint-column.orc", 32767);
+    }
+
+    private void testReadSingleIntegerColumnOrcFile(int initialTableValue, String orcFileResourceName, int expectedValue)
+            throws Exception
+    {
+        checkArgument(initialTableValue != expectedValue); // Needs to be close to expected so that generated file size matches the one from test resources
+        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_read_as_integer", "(\"_col0\") AS VALUES " + initialTableValue + ", NULL")) {
             Path orcFilePath = Path.of((String) computeScalar(format("SELECT DISTINCT file_path FROM \"%s$files\"", table.getName())));
-            Files.copy(new File(getResource("single-smallint-column.orc").toURI()).toPath(), orcFilePath, REPLACE_EXISTING);
+            Files.copy(new File(getResource(orcFileResourceName).toURI()).toPath(), orcFilePath, REPLACE_EXISTING);
             Files.delete(orcFilePath.resolveSibling(format(".%s.crc", orcFilePath.getFileName())));
 
-            assertThat(query("DESCRIBE " + table.getName()))
-                    .projected("Type")
-                    .matches("VALUES varchar 'integer'");
-            assertQuery("SELECT * FROM " + table.getName(), "VALUES 32767, NULL");
+            assertThat(query("TABLE " + table.getName()))
+                    .matches("VALUES NULL, " + expectedValue);
         }
     }
 
