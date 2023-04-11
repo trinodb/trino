4a5967c43e140f56621b5b2f553f7866435133ec
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2023-04-11 15:39:53 +0530

    Remove redundant test override

--- plugin/trino-oracle/src/test/java/io/trino/plugin/oracle/BaseOracleConnectorTest.java
+++ plugin/trino-oracle/src/test/java/io/trino/plugin/oracle/BaseOracleConnectorTest.java
@@ -389,13 +389,6 @@ public abstract class BaseOracleConnectorTest
     }
 
     @Override
-    public void testNativeQueryColumnAlias()
-    {
-        assertThat(query(format("SELECT region_name FROM TABLE(system.query(query => 'SELECT name AS region_name FROM %s.region WHERE regionkey = 0'))", getSession().getSchema().orElseThrow())))
-                .matches("VALUES CAST('AFRICA' AS VARCHAR(25))");
-    }
-
-    @Override
     public void testNativeQueryParameters()
     {
         // override because Oracle requires the FROM clause, and it needs explicit type
