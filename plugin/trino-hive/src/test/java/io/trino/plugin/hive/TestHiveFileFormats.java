d841f9db9c5c93263272561cd46c2beafe6dfa8f
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-04-13 15:35:54 +0200

    Fix TestHiveFileFormats flakiness due to Apache Parquet reader

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestHiveFileFormats.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestHiveFileFormats.java
@@ -117,6 +117,10 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
+// Failing on multiple threads because of org.apache.hadoop.hive.ql.io.parquet.write.ParquetRecordWriterWrapper
+// uses a single record writer across all threads.
+// For example org.apache.parquet.column.values.factory.DefaultValuesWriterFactory#DEFAULT_V1_WRITER_FACTORY is shared mutable state.
+@Test(singleThreaded = true)
 public class TestHiveFileFormats
         extends AbstractTestHiveFileFormats
 {
