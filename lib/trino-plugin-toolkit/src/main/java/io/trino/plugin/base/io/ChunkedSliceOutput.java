e511ac4e80343f2e798305776e3d73e50b2bca13
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-04-14 14:43:19 +0200

    Remove duplicate ChunkedSliceOutput class

--- lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/io/ChunkedSliceOutput.java
+++ lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/io/ChunkedSliceOutput.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.orc;
+package io.trino.plugin.base.io;
 
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
