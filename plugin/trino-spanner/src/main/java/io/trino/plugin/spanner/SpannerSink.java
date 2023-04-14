dbe61fc6e154b134eb7860abb32817625dac21c5
taherk77 <taherk77@gmail.com>
2023-04-14 16:29:59 +0530

    Added licenses and changed sink

--- plugin/trino-spanner/src/main/java/io/trino/plugin/spanner/SpannerSink.java
+++ plugin/trino-spanner/src/main/java/io/trino/plugin/spanner/SpannerSink.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.trino.plugin.spanner;
 
 import com.google.cloud.NoCredentials;
@@ -49,7 +62,8 @@ public class SpannerSink
     private final SpannerSessionProperties.Mode writeMode;
     private List<Mutation> mutations = new LinkedList<>();
 
-    public SpannerSink(ConnectorSession session, JdbcOutputTableHandle handle, SpannerClient spannerClient, ConnectorPageSinkId pageSinkId, RemoteQueryModifier remoteQueryModifier)
+    public SpannerSink(ConnectorSession session, JdbcOutputTableHandle handle,
+            ConnectorPageSinkId pageSinkId)
     {
         this.options = SpannerOptions
                 .newBuilder()
--- plugin/trino-spanner/src/main/java/io/trino/plugin/spanner/SpannerSinkProvider.java
+++ plugin/trino-spanner/src/main/java/io/trino/plugin/spanner/SpannerSinkProvider.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.trino.plugin.spanner;
 
 import com.google.inject.Inject;
@@ -6,18 +19,12 @@ import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcOutputTableHandle;
 import io.trino.plugin.jdbc.logging.RemoteQueryModifier;
 import io.trino.spi.connector.ConnectorInsertTableHandle;
-import io.trino.spi.connector.ConnectorMergeSink;
-import io.trino.spi.connector.ConnectorMergeTableHandle;
 import io.trino.spi.connector.ConnectorOutputTableHandle;
 import io.trino.spi.connector.ConnectorPageSink;
 import io.trino.spi.connector.ConnectorPageSinkId;
 import io.trino.spi.connector.ConnectorPageSinkProvider;
 import io.trino.spi.connector.ConnectorSession;
-import io.trino.spi.connector.ConnectorTableExecuteHandle;
 import io.trino.spi.connector.ConnectorTransactionHandle;
-import io.trino.spi.session.PropertyMetadata;
-
-import java.util.List;
 
 public class SpannerSinkProvider
         implements ConnectorPageSinkProvider
@@ -39,34 +46,17 @@ public class SpannerSinkProvider
         this.modifier = modifier;
         System.out.println("TABLE PROPS in SINK " + propertiesProvider.getTableProperties());
         this.spannerTableProperties = propertiesProvider;
-        PropertyMetadata<?> propertyMetadata = propertiesProvider.getTableProperties().get(0);
-        System.out.println(propertyMetadata.getDefaultValue());
     }
 
     @Override
     public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorOutputTableHandle outputTableHandle, ConnectorPageSinkId pageSinkId)
     {
-        return new SpannerSink(session, (JdbcOutputTableHandle) outputTableHandle, client, pageSinkId, modifier);
+        return new SpannerSink(session, (JdbcOutputTableHandle) outputTableHandle, pageSinkId);
     }
 
     @Override
-    public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorInsertTableHandle insertTableHandle, ConnectorPageSinkId pageSinkId)
+    public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorInsertTableHandle tableHandle, ConnectorPageSinkId pageSinkId)
     {
-        System.out.println("SinkProvider 2");
-        return null;
-    }
-
-    @Override
-    public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorTableExecuteHandle tableExecuteHandle, ConnectorPageSinkId pageSinkId)
-    {
-        System.out.println("SinkProvider 3");
-        return null;
-    }
-
-    @Override
-    public ConnectorMergeSink createMergeSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorMergeTableHandle mergeHandle, ConnectorPageSinkId pageSinkId)
-    {
-        System.out.println("SinkProvider 4");
-        return null;
+        return new SpannerSink(session, (JdbcOutputTableHandle) tableHandle, pageSinkId);
     }
 }
