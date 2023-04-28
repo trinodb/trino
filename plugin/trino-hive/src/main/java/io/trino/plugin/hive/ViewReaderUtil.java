de80d2a886843743b2bf8f3fb33731bd445976fa
Grant Nicholas <grant.nicholas@starburstdata.com>
2023-04-28 12:38:20 -0500

    Handle missing MV StorageDescriptor in Glue
    Handle case when `StorageDescriptor` is missing in a materialized view
    table object stored in Glue.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/ViewReaderUtil.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/ViewReaderUtil.java
@@ -150,8 +150,14 @@ public final class ViewReaderUtil
         return tableType.equals(VIRTUAL_VIEW.name());
     }
 
+    public static boolean isTrinoMaterializedView(Table table)
+    {
+        return isTrinoMaterializedView(table.getTableType(), table.getParameters());
+    }
+
     public static boolean isTrinoMaterializedView(String tableType, Map<String, String> tableParameters)
     {
+        // TODO isHiveOrPrestoView should not return true for materialized views
         return isHiveOrPrestoView(tableType) && isPrestoView(tableParameters) && tableParameters.get(TABLE_COMMENT).equalsIgnoreCase(ICEBERG_MATERIALIZED_VIEW_COMMENT);
     }
 
