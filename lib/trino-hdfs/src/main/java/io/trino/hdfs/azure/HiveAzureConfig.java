46e9108dce1a56438bdaa729199acec7ca8597d9
David Phillips <david@acz.org>
2023-05-07 14:10:31 -0700

    Move Hadoop Azure package to HDFS module

--- lib/trino-hdfs/src/main/java/io/trino/hdfs/azure/HiveAzureConfig.java
+++ lib/trino-hdfs/src/main/java/io/trino/hdfs/azure/HiveAzureConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.azure;
+package io.trino.hdfs.azure;
 
 import com.google.common.net.HostAndPort;
 import io.airlift.configuration.Config;
