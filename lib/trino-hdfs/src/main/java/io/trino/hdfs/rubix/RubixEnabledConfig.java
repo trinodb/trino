d740a8f77e43f6903340b2d14d726aacdcdfcdc9
David Phillips <david@acz.org>
2023-05-07 14:05:37 -0700

    Remove RubixEnabledConfig to HDFS module

--- lib/trino-hdfs/src/main/java/io/trino/hdfs/rubix/RubixEnabledConfig.java
+++ lib/trino-hdfs/src/main/java/io/trino/hdfs/rubix/RubixEnabledConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.rubix;
+package io.trino.hdfs.rubix;
 
 import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
