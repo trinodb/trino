392b38ac2ec221fff4e45f0c45dcbdb7c11d2f60
David Phillips <david@acz.org>
2023-05-07 14:07:55 -0700

    Move Hadoop GCS package to HDFS module

--- lib/trino-hdfs/src/main/java/io/trino/hdfs/gcs/HiveGcsConfig.java
+++ lib/trino-hdfs/src/main/java/io/trino/hdfs/gcs/HiveGcsConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.gcs;
+package io.trino.hdfs.gcs;
 
 import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
