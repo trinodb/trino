659c8d7efe22a6d0d9f71485145b18d63cf44d65
Łukasz Osipiuk <lukasz@osipiuk.net>
2023-05-18 20:36:10 +0200

    Only consider direct children when cleaning extra output files

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -2771,6 +2771,10 @@ public class DeltaLakeMetadata
             FileIterator iterator = fileSystem.listFiles(location);
             while (iterator.hasNext()) {
                 Location file = iterator.next().location();
+                if (!file.parentDirectory().equals(location)) {
+                    // we do not want recursive listing
+                    continue;
+                }
                 if (isFileCreatedByQuery(file, queryId) && !filesToKeep.contains(file)) {
                     filesToDelete.add(file);
                 }
