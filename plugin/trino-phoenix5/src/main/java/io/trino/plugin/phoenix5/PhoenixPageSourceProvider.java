ad43661170def6341a496752d97aff96ebf40b79
Vikash Kumar <vikash.kumar@starburstdata.com>
2023-06-15 01:48:17 +0530

    Fix typo

--- plugin/trino-phoenix5/src/main/java/io/trino/plugin/phoenix5/PhoenixPageSourceProvider.java
+++ plugin/trino-phoenix5/src/main/java/io/trino/plugin/phoenix5/PhoenixPageSourceProvider.java
@@ -102,7 +102,7 @@ public class PhoenixPageSourceProvider
                 .map(RowType.Field::getName)
                 .map(Optional::get)
                 .map(fieldName -> indexOf(scanColumns.iterator(), handle -> handle.getColumnName().equals(fieldName)))
-                .peek(fieldIndex -> checkArgument(fieldIndex != -1, "Merge row id filed must exist in scanned columns"))
+                .peek(fieldIndex -> checkArgument(fieldIndex != -1, "Merge row id field must exist in scanned columns"))
                 .collect(toImmutableList());
         return ColumnAdaptation.mergedRowColumns(mergeRowIdSourceChannels);
     }
