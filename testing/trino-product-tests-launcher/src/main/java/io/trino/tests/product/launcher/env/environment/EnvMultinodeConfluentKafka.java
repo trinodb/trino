trinodb/trino

commit 02cc332319a00e8fec5096921f96cb9e0aceacc1

Add Protobuf oneof support for Confluent schemas

lib/trino-record-decoder/pom.xml
@@ -55,6 +55,11 @@
         </dependency>
 
         <dependency>
+            <groupId>com.google.protobuf</groupId>
+            <artifactId>protobuf-java-util</artifactId>
+        </dependency>
+
+        <dependency>
             <groupId>com.squareup.wire</groupId>
             <artifactId>wire-runtime-jvm</artifactId>
         </dependency>
@@ -119,6 +124,18 @@
         <dependency>
             <groupId>io.airlift</groupId>
             <artifactId>testing</artifactId>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>io.confluent</groupId>
+            <artifactId>kafka-protobuf-provider</artifactId>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>org.apache.kafka</groupId>
+            <artifactId>kafka-clients</artifactId>
             <scope>test</scope>
         </dependency>
 

lib/trino-record-decoder/src/main/java/io/trino/decoder/protobuf/ProtobufColumnDecoder.java
@@ -17,7 +17,11 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Descriptors.OneofDescriptor;
 import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.util.JsonFormat;
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 import io.trino.decoder.DecoderColumnHandle;
 import io.trino.decoder.FieldValueProvider;
 import io.trino.spi.TrinoException;
@@ -33,21 +37,31 @@ import io.trino.spi.type.SmallintType;
 import io.trino.spi.type.TimestampType;
 import io.trino.spi.type.TinyintType;
 import io.trino.spi.type.Type;
+import io.trino.spi.type.TypeManager;
+import io.trino.spi.type.TypeSignature;
 import io.trino.spi.type.VarbinaryType;
 import io.trino.spi.type.VarcharType;
 
 import javax.annotation.Nullable;
 
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static io.trino.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static io.trino.spi.StandardErrorCode.GENERIC_USER_ERROR;
+import static io.trino.spi.type.StandardTypes.JSON;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class ProtobufColumnDecoder
 {
+    private static final Slice EMPTY_JSON = Slices.utf8Slice("{}");
+
     private static final Set<Type> SUPPORTED_PRIMITIVE_TYPES = ImmutableSet.of(
             BooleanType.BOOLEAN,
             TinyintType.TINYINT,
@@ -61,11 +75,15 @@ public class ProtobufColumnDecoder
     private final Type columnType;
     private final String columnMapping;
     private final String columnName;
+    private final TypeManager typeManager;
+    private final Type jsonType;
 
-    public ProtobufColumnDecoder(DecoderColumnHandle columnHandle)
+    public ProtobufColumnDecoder(DecoderColumnHandle columnHandle, TypeManager typeManager)
     {
         try {
             requireNonNull(columnHandle, "columnHandle is null");
+            this.typeManager = requireNonNull(typeManager, "typeManager is null");
+            this.jsonType = typeManager.getType(new TypeSignature(JSON));
             this.columnType = columnHandle.getType();
             this.columnMapping = columnHandle.getMapping();
             this.columnName = columnHandle.getName();
@@ -81,7 +99,7 @@ public class ProtobufColumnDecoder
         }
     }
 
-    private static boolean isSupportedType(Type type)
+    private boolean isSupportedType(Type type)
     {
         if (isSupportedPrimitive(type)) {
             return true;
@@ -106,7 +124,8 @@ public class ProtobufColumnDecoder
             }
             return true;
         }
-        return false;
+
+        return type.equals(jsonType);
     }
 
     private static boolean isSupportedPrimitive(Type type)
@@ -118,7 +137,7 @@ public class ProtobufColumnDecoder
 
     public FieldValueProvider decodeField(DynamicMessage dynamicMessage)
     {
-        return new ProtobufValueProvider(locateField(dynamicMessage, columnMapping), columnType, columnName);
+        return new ProtobufValueProvider(locateField(dynamicMessage, columnMapping), columnType, columnName, typeManager);
     }
 
     @Nullable
@@ -128,8 +147,15 @@ public class ProtobufColumnDecoder
         Optional<Descriptor> valueDescriptor = Optional.of(message.getDescriptorForType());
         for (String pathElement : Splitter.on('/').omitEmptyStrings().split(columnMapping)) {
             if (valueDescriptor.filter(descriptor -> descriptor.findFieldByName(pathElement) != null).isEmpty()) {
-                return null;
+                // Search the message to see if this column is oneof type
+                Optional<OneofDescriptor> oneofDescriptor = message.getDescriptorForType().getOneofs().stream()
+                        .filter(descriptor -> descriptor.getName().equals(columnMapping))
+                        .findFirst();
+
+                return oneofDescriptor.map(descriptor -> createOneofJson(message, descriptor))
+                        .orElse(null);
             }
+
             FieldDescriptor fieldDescriptor = valueDescriptor.get().findFieldByName(pathElement);
             value = ((DynamicMessage) value).getField(fieldDescriptor);
             valueDescriptor = getDescriptor(fieldDescriptor);
@@ -143,5 +169,41 @@ public class ProtobufColumnDecoder
             return Optional.of(fieldDescriptor.getMessageType());
         }
         return Optional.empty();
+    }
+
+    private static Object createOneofJson(DynamicMessage message, OneofDescriptor descriptor)
+    {
+        // Collect all oneof field names from the descriptor
+        Set<String> oneofColumns = descriptor.getFields().stream()
+                .map(FieldDescriptor::getName)
+                .collect(toImmutableSet());
+
+        // Find the oneof field in the message; there will be at most one
+        List<Entry<FieldDescriptor, Object>> oneofFields = message.getAllFields().entrySet().stream()
+                .filter(entry -> oneofColumns.contains(entry.getKey().getName()))
+                .collect(toImmutableList());
+
+        if (oneofFields.size() > 1) {
+            throw new TrinoException(GENERIC_INTERNAL_ERROR, format("Expected to find at most one 'oneof' field in message, found fields: %s", oneofFields));
+        }
+
+        // If found, map the field to a JSON string containing a single field:value pair, else return an empty JSON string {}
+        if (!oneofFields.isEmpty()) {
+            try {
+                // Create a new DynamicMessage where the only set field is the oneof field, so we can use the protobuf-java-util to encode the message as JSON
+                // If we encoded the entire input message, it would include all fields
+                Entry<FieldDescriptor, Object> oneofField = oneofFields.get(0);
+                DynamicMessage oneofMessage = DynamicMessage.newBuilder(oneofField.getKey().getContainingType())
+                        .setField(oneofField.getKey(), oneofField.getValue())
+                        .build();
+                return Slices.utf8Slice(JsonFormat.printer()
+                        .omittingInsignificantWhitespace()
+                        .print(oneofMessage));
+            }
+            catch (Exception e) {
+                throw new TrinoException(GENERIC_INTERNAL_ERROR, "Failed to convert oneof message to JSON", e);
+            }
+        }
+        return EMPTY_JSON;
     }
 }

lib/trino-record-decoder/src/main/java/io/trino/decoder/protobuf/ProtobufRowDecoder.java
@@ -17,6 +17,7 @@ import com.google.protobuf.DynamicMessage;
 import io.trino.decoder.DecoderColumnHandle;
 import io.trino.decoder.FieldValueProvider;
 import io.trino.decoder.RowDecoder;
+import io.trino.spi.type.TypeManager;
 
 import java.util.Map;
 import java.util.Optional;
@@ -34,13 +35,13 @@ public class ProtobufRowDecoder
     private final DynamicMessageProvider dynamicMessageProvider;
     private final Map<DecoderColumnHandle, ProtobufColumnDecoder> columnDecoders;
 
-    public ProtobufRowDecoder(DynamicMessageProvider dynamicMessageProvider, Set<DecoderColumnHandle> columns)
+    public ProtobufRowDecoder(DynamicMessageProvider dynamicMessageProvider, Set<DecoderColumnHandle> columns, TypeManager typeManager)
     {
         this.dynamicMessageProvider = requireNonNull(dynamicMessageProvider, "dynamicMessageSupplier is null");
         this.columnDecoders = columns.stream()
                 .collect(toImmutableMap(
                         identity(),
-                        ProtobufColumnDecoder::new));
+                        column -> new ProtobufColumnDecoder(column, typeManager)));
     }
 
     @Override

lib/trino-record-decoder/src/main/java/io/trino/decoder/protobuf/ProtobufRowDecoderFactory.java
@@ -17,6 +17,7 @@ import io.trino.decoder.DecoderColumnHandle;
 import io.trino.decoder.RowDecoder;
 import io.trino.decoder.RowDecoderFactory;
 import io.trino.decoder.protobuf.DynamicMessageProvider.Factory;
+import io.trino.spi.type.TypeManager;
 
 import javax.inject.Inject;
 
@@ -32,11 +33,13 @@ public class ProtobufRowDecoderFactory
     public static final String DEFAULT_MESSAGE = "schema";
 
     private final Factory dynamicMessageProviderFactory;
+    private final TypeManager typeManager;
 
     @Inject
-    public ProtobufRowDecoderFactory(Factory dynamicMessageProviderFactory)
+    public ProtobufRowDecoderFactory(Factory dynamicMessageProviderFactory, TypeManager typeManager)
     {
         this.dynamicMessageProviderFactory = requireNonNull(dynamicMessageProviderFactory, "dynamicMessageProviderFactory is null");
+        this.typeManager = requireNonNull(typeManager, "typeManager is null");
     }
 
     @Override
@@ -44,6 +47,7 @@ public class ProtobufRowDecoderFactory
     {
         return new ProtobufRowDecoder(
                 dynamicMessageProviderFactory.create(Optional.ofNullable(decoderParams.get("dataSchema"))),
-                columns);
+                columns,
+                typeManager);
     }
 }

commit 12eee832d78575d4157889cd2d3007f1af9f9b67

Add product tests for Kafka connector with Confluent licensed libraries
The older approach was to copy the Confluent licensed libraries to the
Kafka connector classpath for all product environments extending from
`Kafka`. This meant that it was not possible to detect changes which
introduce a hard-dependency on the Confluent licensed libraries for
Kafka connector startup.

02cc332319a00e8fec5096921f96cb9e0aceacc1 inadvertently made it so that
the Confluent libraries became required for the Kafka connector to start
up and was not caught by tests because all the product test environments
already had the Confluent libraries present on the Kafka connector
classpath.

This commit instead introduces a new product test environment
`EnvMultinodeKafkaConfluentLicense` which is the only product test
environment that copies the Confluent licensed libraries onto the Kafka
connector classpath. There is also a new test group
`kafka_confluent_license` which now includes all the tests for the
functionality which requires the Confluent licensed libraries.

testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/common/Kafka.java
@@ -23,16 +23,13 @@ import io.trino.tests.product.launcher.testcontainers.PortBinder;
 import org.testcontainers.containers.startupcheck.IsRunningStartupCheckStrategy;
 import org.testcontainers.utility.MountableFile;
 
-import java.io.File;
 import java.time.Duration;
 
-import static io.trino.testing.TestingProperties.getConfluentVersion;
 import static io.trino.tests.product.launcher.docker.ContainerUtil.forSelectedPorts;
 import static io.trino.tests.product.launcher.env.EnvironmentContainers.isTrinoContainer;
 import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_ETC;
 import static java.util.Objects.requireNonNull;
 import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage;
-import static org.testcontainers.utility.MountableFile.forClasspathResource;
 import static org.testcontainers.utility.MountableFile.forHostPath;
 
 public class Kafka
@@ -40,8 +37,6 @@ public class Kafka
 {
     private static final String CONFLUENT_VERSION = "7.3.1";
     private static final int SCHEMA_REGISTRY_PORT = 8081;
-    private static final File KAFKA_PROTOBUF_PROVIDER = new File("testing/trino-product-tests-launcher/target/kafka-protobuf-provider-" + getConfluentVersion() + ".jar");
-    private static final File KAFKA_PROTOBUF_TYPES = new File("testing/trino-product-tests-launcher/target/kafka-protobuf-types-" + getConfluentVersion() + ".jar");
     static final String KAFKA = "kafka";
     static final String SCHEMA_REGISTRY = "schema-registry";
     static final String ZOOKEEPER = "zookeeper";
@@ -69,10 +64,7 @@ public class Kafka
             if (isTrinoContainer(container.getLogicalName())) {
                 MountableFile logConfigFile = forHostPath(configDir.getPath("log.properties"));
                 container
-                        .withCopyFileToContainer(logConfigFile, CONTAINER_TRINO_ETC + "/log.properties")
-                        .withCopyFileToContainer(forHostPath(KAFKA_PROTOBUF_PROVIDER.getAbsolutePath()), "/docker/kafka-protobuf-provider/kafka-protobuf-provider.jar")
-                        .withCopyFileToContainer(forHostPath(KAFKA_PROTOBUF_TYPES.getAbsolutePath()), "/docker/kafka-protobuf-provider/kafka-protobuf-types.jar")
-                        .withCopyFileToContainer(forClasspathResource("install-kafka-protobuf-provider.sh", 0755), "/docker/presto-init.d/install-kafka-protobuf-provider.sh");
+                        .withCopyFileToContainer(logConfigFile, CONTAINER_TRINO_ETC + "/log.properties");
             }
         });
 

testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeConfluentKafka.java
@@ -1,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.trino.tests.product.launcher.env.environment;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+import io.trino.tests.product.launcher.docker.DockerFiles;
+import io.trino.tests.product.launcher.docker.DockerFiles.ResourceProvider;
+import io.trino.tests.product.launcher.env.Environment;
+import io.trino.tests.product.launcher.env.EnvironmentProvider;
+import io.trino.tests.product.launcher.env.common.Kafka;
+import io.trino.tests.product.launcher.env.common.StandardMultinode;
+import io.trino.tests.product.launcher.env.common.TestsEnvironment;
+
+import java.io.File;
+
+import static io.trino.testing.TestingProperties.getConfluentVersion;
+import static io.trino.tests.product.launcher.env.EnvironmentContainers.configureTempto;
+import static io.trino.tests.product.launcher.env.EnvironmentContainers.isTrinoContainer;
+import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_ETC;
+import static java.util.Objects.requireNonNull;
+import static org.testcontainers.utility.MountableFile.forClasspathResource;
+import static org.testcontainers.utility.MountableFile.forHostPath;
+
+/**
+ * {@link EnvMultinodeConfluentKafka} is intended to be the only Kafka product test environment which copies the non-free Confluent License libraries to the Kafka connector
+ * classpath to test functionality which requires those classes.
+ * The other {@link Kafka} environments MUST NOT copy these jars otherwise it's not possible to verify the out of box Trino setup which doesn't ship with the Confluent licensed
+ * libraries.
+ */
+@TestsEnvironment
+public final class EnvMultinodeConfluentKafka
+        extends EnvironmentProvider
+{
+    private static final File KAFKA_PROTOBUF_PROVIDER = new File("testing/trino-product-tests-launcher/target/kafka-protobuf-provider-" + getConfluentVersion() + ".jar");
+    private static final File KAFKA_PROTOBUF_TYPES = new File("testing/trino-product-tests-launcher/target/kafka-protobuf-types-" + getConfluentVersion() + ".jar");
+
+    private final ResourceProvider configDir;
+
+    @Inject
+    public EnvMultinodeConfluentKafka(Kafka kafka, StandardMultinode standardMultinode, DockerFiles dockerFiles)
+    {
+        super(ImmutableList.of(standardMultinode, kafka));
+        requireNonNull(dockerFiles, "dockerFiles is null");
+        configDir = dockerFiles.getDockerFilesHostDirectory("conf/environment/multinode-kafka-confluent-license/");
+    }
+
+    @Override
+    public void extendEnvironment(Environment.Builder builder)
+    {
+        builder.configureContainers(container -> {
+            if (isTrinoContainer(container.getLogicalName())) {
+                builder.addConnector("kafka", forHostPath(configDir.getPath("kafka.properties")), CONTAINER_TRINO_ETC + "/catalog/kafka.properties");
+                builder.addConnector("kafka", forHostPath(configDir.getPath("kafka_schema_registry.properties")), CONTAINER_TRINO_ETC + "/catalog/kafka_schema_registry.properties");
+                container
+                        .withCopyFileToContainer(forHostPath(KAFKA_PROTOBUF_PROVIDER.getAbsolutePath()), "/docker/kafka-protobuf-provider/kafka-protobuf-provider.jar")
+                        .withCopyFileToContainer(forHostPath(KAFKA_PROTOBUF_TYPES.getAbsolutePath()), "/docker/kafka-protobuf-provider/kafka-protobuf-types.jar")
+                        .withCopyFileToContainer(forClasspathResource("install-kafka-protobuf-provider.sh", 0755), "/docker/presto-init.d/install-kafka-protobuf-provider.sh");
+            }
+        });
+
+        configureTempto(builder, configDir);
+    }
+}

testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteKafka.java
@@ -15,6 +15,7 @@ package io.trino.tests.product.launcher.suite.suites;
 
 import com.google.common.collect.ImmutableList;
 import io.trino.tests.product.launcher.env.EnvironmentConfig;
+import io.trino.tests.product.launcher.env.environment.EnvMultinodeConfluentKafka;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafka;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafkaSaslPlaintext;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeKafkaSsl;
@@ -34,6 +35,10 @@ public class SuiteKafka
         return ImmutableList.of(
                 testOnEnvironment(EnvMultinodeKafka.class)
                         .withGroups("configured_features", "kafka")
+                        .build(),
+                testOnEnvironment(EnvMultinodeConfluentKafka.class)
+                        // testing kafka group with this env is slightly redundant but helps verify that copying confluent libraries doesn't break non-confluent functionality
+                        .withGroups("configured_features", "kafka", "kafka_confluent_license")
                         .build(),
                 testOnEnvironment(EnvMultinodeKafkaSsl.class)
                         .withGroups("configured_features", "kafka")

testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/multinode-kafka-confluent-license/kafka.properties
@@ -1,0 +1,23 @@
+connector.name=kafka
+kafka.table-names=product_tests.read_simple_key_and_value,\
+                    product_tests.read_all_datatypes_raw,\
+                    product_tests.read_all_datatypes_csv,\
+                    product_tests.read_all_datatypes_json,\
+                    product_tests.read_all_datatypes_avro,\
+                    product_tests.read_all_null_avro,\
+                    product_tests.read_structural_datatype_avro,\
+                    product_tests.write_simple_key_and_value,\
+                    product_tests.write_all_datatypes_raw,\
+                    product_tests.write_all_datatypes_csv,\
+                    product_tests.write_all_datatypes_json,\
+                    product_tests.write_all_datatypes_avro,\
+                    product_tests.write_structural_datatype_avro,\
+                    product_tests.pushdown_partition,\
+                    product_tests.pushdown_offset,\
+                    product_tests.pushdown_create_time,\
+                    product_tests.all_datatypes_protobuf,\
+                    product_tests.structural_datatype_protobuf,\
+                    product_tests.read_basic_datatypes_protobuf,\
+                    product_tests.read_basic_structural_datatypes_protobuf
+kafka.nodes=kafka:9092
+kafka.table-description-dir=/docker/presto-product-tests/conf/presto/etc/catalog/kafka

testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/multinode-kafka-confluent-license/kafka_schema_registry.properties
@@ -1,0 +1,5 @@
+connector.name=kafka
+kafka.nodes=kafka:9092
+kafka.table-description-supplier=confluent
+kafka.confluent-schema-registry-url=http://schema-registry:8081
+kafka.default-schema=product_tests

testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/multinode-kafka-confluent-license/tempto-configuration.yaml
@@ -1,0 +1,2 @@
+schema-registry:
+  url: http://schema-registry:8081

testing/trino-product-tests/src/main/java/io/trino/tests/product/TestGroups.java
@@ -69,6 +69,7 @@ public final class TestGroups
     public static final String CANCEL_QUERY = "cancel_query";
     public static final String LARGE_QUERY = "large_query";
     public static final String KAFKA = "kafka";
+    public static final String KAFKA_CONFLUENT_LICENSE = "kafka_confluent_license";
     public static final String TWO_HIVES = "two_hives";
     public static final String ICEBERG = "iceberg";
     public static final String ICEBERG_FORMAT_VERSION_COMPATIBILITY = "iceberg_format_version_compatibility";

testing/trino-product-tests/src/main/java/io/trino/tests/product/kafka/TestKafkaProtobufReadsSmokeTest.java
@@ -30,7 +30,6 @@ import io.trino.tempto.fulfillment.table.kafka.KafkaTableDefinition;
 import io.trino.tempto.fulfillment.table.kafka.KafkaTableManager;
 import io.trino.tempto.fulfillment.table.kafka.ListKafkaDataSource;
 import io.trino.tempto.query.QueryResult;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -47,6 +46,7 @@ import static io.trino.tempto.context.ThreadLocalTestContextHolder.testContext;
 import static io.trino.tempto.fulfillment.table.TableHandle.tableHandle;
 import static io.trino.tempto.fulfillment.table.kafka.KafkaMessageContentsBuilder.contentsBuilder;
 import static io.trino.tests.product.TestGroups.KAFKA;
+import static io.trino.tests.product.TestGroups.KAFKA_CONFLUENT_LICENSE;
 import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
 import static io.trino.tests.product.utils.QueryAssertions.assertEventually;
 import static io.trino.tests.product.utils.QueryExecutors.onTrino;
@@ -71,8 +71,24 @@ public class TestKafkaProtobufReadsSmokeTest
     private static final String ALL_DATATYPES_PROTOBUF_TOPIC_SCHEMA_REGISTRY = "all_datatypes_protobuf_schema_registry";
     private static final String ALL_DATATYPES_SCHEMA_PATH = "/docker/presto-product-tests/conf/presto/etc/catalog/kafka/all_datatypes.proto";
 
-    @Test(groups = {KAFKA, PROFILE_SPECIFIC_TESTS}, dataProvider = "catalogs")
-    public void testSelectPrimitiveDataType(KafkaCatalog kafkaCatalog)
+    private static final KafkaCatalog KAFKA_CATALOG = new KafkaCatalog("kafka", "", true, new ProtobufMessageSerializer());
+    private static final KafkaCatalog KAFKA_SCHEMA_REGISTRY_CATALOG = new KafkaCatalog("kafka_schema_registry", "_schema_registry", false, new SchemaRegistryProtobufMessageSerializer());
+
+    @Test(groups = {KAFKA, PROFILE_SPECIFIC_TESTS})
+    public void testSelectPrimitiveDataType()
+            throws Exception
+    {
+        selectPrimitiveDataType(KAFKA_CATALOG);
+    }
+
+    @Test(groups = {KAFKA_CONFLUENT_LICENSE, PROFILE_SPECIFIC_TESTS})
+    public void testSelectPrimitiveDataTypeWithSchemaRegistry()
+            throws Exception
+    {
+        selectPrimitiveDataType(KAFKA_SCHEMA_REGISTRY_CATALOG);
+    }
+
+    private void selectPrimitiveDataType(KafkaCatalog kafkaCatalog)
             throws Exception
     {
         Map<String, Object> record = ImmutableMap.<String, Object>builder()
@@ -100,8 +116,21 @@ public class TestKafkaProtobufReadsSmokeTest
                 });
     }
 
-    @Test(groups = {KAFKA, PROFILE_SPECIFIC_TESTS}, dataProvider = "catalogs")
-    public void testSelectStructuralDataType(KafkaCatalog kafkaCatalog)
+    @Test(groups = {KAFKA, PROFILE_SPECIFIC_TESTS})
+    public void testSelectStructuralDataType()
+            throws Exception
+    {
+        selectStructuralDataType(KAFKA_CATALOG);
+    }
+
+    @Test(groups = {KAFKA_CONFLUENT_LICENSE, PROFILE_SPECIFIC_TESTS})
+    public void testSelectStructuralDataTypeWithSchemaRegistry()
+            throws Exception
+    {
+        selectStructuralDataType(KAFKA_SCHEMA_REGISTRY_CATALOG);
+    }
+
+    private void selectStructuralDataType(KafkaCatalog kafkaCatalog)
             throws Exception
     {
         ImmutableMap<String, Object> record = ImmutableMap.of(
@@ -124,19 +153,6 @@ public class TestKafkaProtobufReadsSmokeTest
                 });
     }
 
-    @DataProvider
-    public static Object[][] catalogs()
-    {
-        return new Object[][] {
-                {
-                        new KafkaCatalog("kafka", "", true, new ProtobufMessageSerializer()),
-                },
-                {
-                        new KafkaCatalog("kafka_schema_registry", "_schema_registry", false, new SchemaRegistryProtobufMessageSerializer()),
-                },
-        };
-    }
-
     private record KafkaCatalog(String catalogName, String topicNameSuffix, boolean columnMappingSupported, MessageSerializer messageSerializer)
     {
         private KafkaCatalog(String catalogName, String topicNameSuffix, boolean columnMappingSupported, MessageSerializer messageSerializer)
@@ -154,7 +170,7 @@ public class TestKafkaProtobufReadsSmokeTest
         }
     }
 
-    @Test(groups = {KAFKA, PROFILE_SPECIFIC_TESTS})
+    @Test(groups = {KAFKA_CONFLUENT_LICENSE, PROFILE_SPECIFIC_TESTS})
     public void testProtobufWithSchemaReferences()
             throws Exception
     {
@@ -198,7 +214,7 @@ public class TestKafkaProtobufReadsSmokeTest
         assertEventually(
                 new Duration(30, SECONDS),
                 () -> {
-                    QueryResult queryResult = onTrino().executeQuery(format("select * from kafka_schema_registry.%s.%s", KAFKA_SCHEMA, ALL_DATATYPES_PROTOBUF_TOPIC_SCHEMA_REGISTRY));
+                    QueryResult queryResult = onTrino().executeQuery(format("select * from %s.%s.%s", KAFKA_SCHEMA_REGISTRY_CATALOG.catalogName(), KAFKA_SCHEMA, ALL_DATATYPES_PROTOBUF_TOPIC_SCHEMA_REGISTRY));
                     assertThat(queryResult).containsOnly(row(
                             "foobar",
                             2,