17b8ddcee45660dd976bfcfa9db188e019bde783
Yuya Ebihara <ebyhry@gmail.com>
2023-06-15 17:35:52 +0900

    Minor refactoring in MongoDB connector

--- plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/TypeUtils.java
+++ plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/TypeUtils.java
@@ -14,9 +14,6 @@
 package io.trino.plugin.mongodb;
 
 import com.google.common.collect.ImmutableSet;
-import io.trino.spi.type.ArrayType;
-import io.trino.spi.type.MapType;
-import io.trino.spi.type.RowType;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.VarcharType;
 
@@ -51,21 +48,6 @@ public final class TypeUtils
     public static boolean isJsonType(Type type)
     {
         return type.getBaseName().equals(JSON);
-    }
-
-    public static boolean isArrayType(Type type)
-    {
-        return type instanceof ArrayType;
-    }
-
-    public static boolean isMapType(Type type)
-    {
-        return type instanceof MapType;
-    }
-
-    public static boolean isRowType(Type type)
-    {
-        return type instanceof RowType;
     }
 
     public static boolean isPushdownSupportedType(Type type)
