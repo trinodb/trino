05ddda8a09df7f1e4b366d47ee25688024f65880
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-06-20 11:11:29 +0200

    Remove unused and defunct TestTable constructor
    Remove constructor which doesn't construct the table.
    Since it was randomizing the name, it couldn't even be used for dropping
    existing table.

--- testing/trino-testing/src/main/java/io/trino/testing/sql/TestTable.java
+++ testing/trino-testing/src/main/java/io/trino/testing/sql/TestTable.java
@@ -44,13 +44,6 @@ public class TestTable
         createAndInsert(rowsToInsert);
     }
 
-    public TestTable(SqlExecutor sqlExecutor, String namePrefix)
-    {
-        this.sqlExecutor = sqlExecutor;
-        this.name = namePrefix + randomNameSuffix();
-        this.tableDefinition = null;
-    }
-
     public void createAndInsert(List<String> rowsToInsert)
     {
         sqlExecutor.execute(format("CREATE TABLE %s %s", name, tableDefinition));
