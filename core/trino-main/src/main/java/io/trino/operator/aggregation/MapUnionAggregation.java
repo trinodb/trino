4cff1775ec58cc0a6b7a93b00b0d024c34097076
Dain Sundstrom <dain@iq80.com>
2023-07-02 23:38:37 -0700

    Rewrite map_agg and map_union functions using flat data

--- core/trino-main/src/main/java/io/trino/operator/aggregation/MapUnionAggregation.java
+++ core/trino-main/src/main/java/io/trino/operator/aggregation/MapUnionAggregation.java
@@ -13,27 +13,18 @@
  */
 package io.trino.operator.aggregation;
 
-import io.trino.operator.aggregation.state.KeyValuePairsState;
 import io.trino.spi.block.Block;
 import io.trino.spi.block.BlockBuilder;
+import io.trino.spi.block.MapBlockBuilder;
 import io.trino.spi.function.AggregationFunction;
 import io.trino.spi.function.AggregationState;
 import io.trino.spi.function.CombineFunction;
-import io.trino.spi.function.Convention;
 import io.trino.spi.function.Description;
 import io.trino.spi.function.InputFunction;
-import io.trino.spi.function.OperatorDependency;
-import io.trino.spi.function.OperatorType;
 import io.trino.spi.function.OutputFunction;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.function.TypeParameter;
 import io.trino.spi.type.Type;
-import io.trino.type.BlockTypeOperators.BlockPositionEqual;
-import io.trino.type.BlockTypeOperators.BlockPositionHashCode;
-
-import static io.trino.spi.function.InvocationConvention.InvocationArgumentConvention.BLOCK_POSITION;
-import static io.trino.spi.function.InvocationConvention.InvocationReturnConvention.FAIL_ON_NULL;
-import static io.trino.spi.function.InvocationConvention.InvocationReturnConvention.NULLABLE_RETURN;
 
 @AggregationFunction("map_union")
 @Description("Aggregate all the maps into a single map")
@@ -45,45 +36,26 @@ public final class MapUnionAggregation
     @TypeParameter("K")
     @TypeParameter("V")
     public static void input(
-            @TypeParameter("K") Type keyType,
-            @OperatorDependency(
-                    operator = OperatorType.EQUAL,
-                    argumentTypes = {"K", "K"},
-                    convention = @Convention(arguments = {BLOCK_POSITION, BLOCK_POSITION}, result = NULLABLE_RETURN))
-                    BlockPositionEqual keyEqual,
-            @OperatorDependency(
-                    operator = OperatorType.HASH_CODE,
-                    argumentTypes = "K",
-                    convention = @Convention(arguments = BLOCK_POSITION, result = FAIL_ON_NULL))
-                    BlockPositionHashCode keyHashCode,
             @TypeParameter("V") Type valueType,
-            @AggregationState({"K", "V"}) KeyValuePairsState state,
+            @AggregationState({"K", "V"}) MapAggregationState state,
             @SqlType("map(K,V)") Block value)
     {
-        KeyValuePairs pairs = state.get();
-        if (pairs == null) {
-            pairs = new KeyValuePairs(keyType, keyEqual, keyHashCode, valueType);
-            state.set(pairs);
-        }
-
-        long startSize = pairs.estimatedInMemorySize();
         for (int i = 0; i < value.getPositionCount(); i += 2) {
-            pairs.add(value, value, i, i + 1);
+            state.add(value, i, value, i + 1);
         }
-        state.addMemoryUsage(pairs.estimatedInMemorySize() - startSize);
     }
 
     @CombineFunction
     public static void combine(
-            @AggregationState({"K", "V"}) KeyValuePairsState state,
-            @AggregationState({"K", "V"}) KeyValuePairsState otherState)
+            @AggregationState({"K", "V"}) MapAggregationState state,
+            @AggregationState({"K", "V"}) MapAggregationState otherState)
     {
-        MapAggregationFunction.combine(state, otherState);
+        state.merge(otherState);
     }
 
     @OutputFunction("map(K, V)")
-    public static void output(@AggregationState({"K", "V"}) KeyValuePairsState state, BlockBuilder out)
+    public static void output(@AggregationState({"K", "V"}) MapAggregationState state, BlockBuilder out)
     {
-        MapAggregationFunction.output(state, out);
+        state.writeAll((MapBlockBuilder) out);
     }
 }
