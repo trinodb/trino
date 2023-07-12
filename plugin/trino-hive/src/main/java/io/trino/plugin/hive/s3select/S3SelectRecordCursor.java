6a17198cca03e957dec0aa04489b0874932bd111
David Phillips <david@acz.org>
2023-07-12 12:34:24 -0700

    Remove unused Hive SERIALIZATION_DDL property

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/s3select/S3SelectRecordCursor.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/s3select/S3SelectRecordCursor.java
@@ -21,29 +21,17 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.RecordReader;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Strings.isNullOrEmpty;
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.trino.plugin.hive.util.SerdeConstants.LIST_COLUMNS;
 import static io.trino.plugin.hive.util.SerdeConstants.LIST_COLUMN_TYPES;
-import static io.trino.plugin.hive.util.SerdeConstants.SERIALIZATION_DDL;
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toList;
 
 class S3SelectRecordCursor<K, V extends Writable>
         extends GenericHiveRecordCursor<K, V>
 {
-    private static final String THRIFT_STRUCT = "struct";
-    private static final String START_STRUCT = "{";
-    private static final String END_STRUCT = "}";
-    private static final String FIELD_SEPARATOR = ",";
-
     public S3SelectRecordCursor(
             Configuration configuration,
             Path path,
@@ -68,9 +56,6 @@ class S3SelectRecordCursor<K, V extends Writable>
         updatedSchema.putAll(splitSchema);
         updatedSchema.setProperty(LIST_COLUMNS, buildColumns(columns));
         updatedSchema.setProperty(LIST_COLUMN_TYPES, buildColumnTypes(columns));
-        ThriftTable thriftTable = parseThriftDdl(splitSchema.getProperty(SERIALIZATION_DDL));
-        updatedSchema.setProperty(SERIALIZATION_DDL,
-                thriftTableToDdl(pruneThriftTable(thriftTable, columns)));
         return updatedSchema;
     }
 
@@ -92,142 +77,5 @@ class S3SelectRecordCursor<K, V extends Writable>
         return columns.stream()
                 .map(column -> column.getHiveType().getTypeInfo().getTypeName())
                 .collect(Collectors.joining(","));
-    }
-
-    /**
-     * Parse Thrift description of a table schema. Examples:
-     * <ul>
-     * <li><code>struct article { varchar article varchar author date date_pub int quantity}</code></li>
-     * <li><code>struct article { varchar article, varchar author, date date_pub, int quantity }</code></li>
-     * <li><code>struct article { varchar article, varchar author, date date_pub, int quantity}</code></li>
-     * </ul>
-     */
-    private static ThriftTable parseThriftDdl(String ddl)
-    {
-        if (isNullOrEmpty(ddl)) {
-            return null;
-        }
-        String[] parts = ddl.trim().split("\\s+");
-        checkArgument(parts.length >= 5, "Invalid Thrift DDL %s", ddl);
-        checkArgument(THRIFT_STRUCT.equals(parts[0]), "Thrift DDL should start with %s", THRIFT_STRUCT);
-        ThriftTable thriftTable = new ThriftTable();
-        thriftTable.setTableName(parts[1]);
-        checkArgument(START_STRUCT.equals(parts[2]), "Invalid Thrift DDL %s", ddl);
-        checkArgument(parts[parts.length - 1].endsWith(END_STRUCT), "Invalid Thrift DDL %s", ddl);
-        String lastColumnNameWithEndStruct = parts[parts.length - 1];
-        parts[parts.length - 1] = lastColumnNameWithEndStruct.substring(0, lastColumnNameWithEndStruct.length() - 1);
-        List<ThriftField> fields = new ArrayList<>();
-        for (int i = 3; i < parts.length - 1; i += 2) {
-            ThriftField thriftField = new ThriftField();
-            thriftField.setType(parts[i]);
-            String columnNameWithFieldSeparator = parts[i + 1];
-            if (columnNameWithFieldSeparator.endsWith(FIELD_SEPARATOR)) {
-                parts[i + 1] = columnNameWithFieldSeparator.substring(0, columnNameWithFieldSeparator.length() - 1);
-            }
-            thriftField.setName(parts[i + 1]);
-            fields.add(thriftField);
-        }
-        thriftTable.setFields(fields);
-
-        return thriftTable;
-    }
-
-    private static ThriftTable pruneThriftTable(ThriftTable thriftTable, List<HiveColumnHandle> columns)
-    {
-        if (thriftTable == null) {
-            return null;
-        }
-        List<ThriftField> fields = thriftTable.getFields();
-        if (fields == null || fields.isEmpty()) {
-            return thriftTable;
-        }
-        Set<String> columnNames = columns.stream()
-                .map(HiveColumnHandle::getName)
-                .collect(toImmutableSet());
-        List<ThriftField> filteredFields = fields.stream()
-                .filter(field -> columnNames.contains(field.getName()))
-                .collect(toList());
-        thriftTable.setFields(filteredFields);
-
-        return thriftTable;
-    }
-
-    private static String thriftTableToDdl(ThriftTable thriftTable)
-    {
-        if (thriftTable == null) {
-            return "";
-        }
-        List<ThriftField> fields = thriftTable.getFields();
-        if (fields == null || fields.isEmpty()) {
-            return "";
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(THRIFT_STRUCT)
-                .append(" ")
-                .append(thriftTable.getTableName())
-                .append(" ")
-                .append(START_STRUCT);
-        stringBuilder.append(fields.stream()
-                .map(field -> " " + field.getType() + " " + field.getName())
-                .collect(Collectors.joining(",")));
-        stringBuilder.append(END_STRUCT);
-
-        return stringBuilder.toString();
-    }
-
-    private static class ThriftField
-    {
-        private String type;
-        private String name;
-
-        private String getType()
-        {
-            return type;
-        }
-
-        private void setType(String type)
-        {
-            checkArgument(!isNullOrEmpty(type), "type is null or empty string");
-            this.type = type;
-        }
-
-        private String getName()
-        {
-            return name;
-        }
-
-        private void setName(String name)
-        {
-            requireNonNull(name, "name is null");
-            this.name = name;
-        }
-    }
-
-    private static class ThriftTable
-    {
-        private String tableName;
-        private List<ThriftField> fields;
-
-        private String getTableName()
-        {
-            return tableName;
-        }
-
-        private void setTableName(String tableName)
-        {
-            checkArgument(!isNullOrEmpty(tableName), "tableName is null or empty string");
-            this.tableName = tableName;
-        }
-
-        private List<ThriftField> getFields()
-        {
-            return fields;
-        }
-
-        private void setFields(List<ThriftField> fields)
-        {
-            requireNonNull(fields, "fields is null");
-            this.fields = fields;
-        }
     }
 }
