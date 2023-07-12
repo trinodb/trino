f138913d8ae9b29f33af40fe24de70d20f8312f2
Martin Traverso <mtraverso@gmail.com>
2023-07-12 13:42:23 -0700

    Move window query tests to JUnit

--- testing/trino-faulttolerant-tests/src/test/java/io/trino/faulttolerant/hive/TestHiveFaultTolerantExecutionWindowQueries.java
+++ testing/trino-faulttolerant-tests/src/test/java/io/trino/faulttolerant/hive/TestHiveFaultTolerantExecutionWindowQueries.java
@@ -18,6 +18,8 @@ import io.trino.plugin.exchange.filesystem.containers.MinioStorage;
 import io.trino.plugin.hive.HiveQueryRunner;
 import io.trino.testing.AbstractTestFaultTolerantExecutionWindowQueries;
 import io.trino.testing.QueryRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInstance;
 import org.testng.annotations.AfterClass;
 
 import java.util.Map;
@@ -25,7 +27,9 @@ import java.util.Map;
 import static io.trino.plugin.exchange.filesystem.containers.MinioStorage.getExchangeManagerProperties;
 import static io.trino.testing.TestingNames.randomNameSuffix;
 import static io.trino.tpch.TpchTable.getTables;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+@TestInstance(PER_CLASS)
 public class TestHiveFaultTolerantExecutionWindowQueries
         extends AbstractTestFaultTolerantExecutionWindowQueries
 {
@@ -48,6 +52,7 @@ public class TestHiveFaultTolerantExecutionWindowQueries
                 .build();
     }
 
+    @AfterAll
     @AfterClass(alwaysRun = true)
     public void destroy()
             throws Exception
