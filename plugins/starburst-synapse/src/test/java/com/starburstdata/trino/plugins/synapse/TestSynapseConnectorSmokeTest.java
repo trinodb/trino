f8bd90e08d453a8bc43e8cd28c9392665407f730
Mayank Vadariya <48036907+mayankvadariya@users.noreply.github.com>
2023-07-12 14:28:44 -0400

    Make TestSynapseConnectorSmokeTest final
    (cherry-picked from commit: 11cb0a41978da9bd706cdca0d1ef9142af6a477e)

--- plugins/starburst-synapse/src/test/java/com/starburstdata/trino/plugins/synapse/TestSynapseConnectorSmokeTest.java
+++ plugins/starburst-synapse/src/test/java/com/starburstdata/trino/plugins/synapse/TestSynapseConnectorSmokeTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 import static com.starburstdata.trino.plugins.synapse.SynapseQueryRunner.createSynapseQueryRunner;
 
-public class TestSynapseConnectorSmokeTest
+public final class TestSynapseConnectorSmokeTest
         extends BaseJdbcConnectorSmokeTest
 {
     @Override
