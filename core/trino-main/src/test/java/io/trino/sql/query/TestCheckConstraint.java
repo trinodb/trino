f9a8d6279d61a01e0a8ae52fbb7baabf7c9f2c56
Slawomir Pajak <slawomir.pajak@starburstdata.com>
2023-07-19 09:56:31 +0200

    Fix testing check constraints not to violate existing data
    Current test data is outside allowed check constraints

--- core/trino-main/src/test/java/io/trino/sql/query/TestCheckConstraint.java
+++ core/trino-main/src/test/java/io/trino/sql/query/TestCheckConstraint.java
@@ -101,7 +101,7 @@ public class TestCheckConstraint
                         return ImmutableList.of("regionkey < 10");
                     }
                     if (schemaTableName.equals(new SchemaTableName("tiny", "nation_multiple_column_constraint"))) {
-                        return ImmutableList.of("nationkey > 100 AND regionkey > 50");
+                        return ImmutableList.of("nationkey < 100 AND regionkey < 50");
                     }
                     if (schemaTableName.equals(new SchemaTableName("tiny", "nation_invalid_function"))) {
                         return ImmutableList.of("invalid_function(nationkey) > 100");
@@ -230,13 +230,13 @@ public class TestCheckConstraint
     @Test
     public void testInsertCheckMultipleColumns()
     {
-        assertThat(assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (101, 'POLAND', 51, 'No comment')"))
+        assertThat(assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (99, 'POLAND', 49, 'No comment')"))
                 .matches("SELECT BIGINT '1'");
 
-        assertThatThrownBy(() -> assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (101, 'POLAND', 50, 'No comment')"))
-                .hasMessage("Check constraint violation: ((nationkey > 100) AND (regionkey > 50))");
-        assertThatThrownBy(() -> assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (100, 'POLAND', 51, 'No comment')"))
-                .hasMessage("Check constraint violation: ((nationkey > 100) AND (regionkey > 50))");
+        assertThatThrownBy(() -> assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (99, 'POLAND', 50, 'No comment')"))
+                .hasMessage("Check constraint violation: ((nationkey < 100) AND (regionkey < 50))");
+        assertThatThrownBy(() -> assertions.query("INSERT INTO mock.tiny.nation_multiple_column_constraint VALUES (100, 'POLAND', 49, 'No comment')"))
+                .hasMessage("Check constraint violation: ((nationkey < 100) AND (regionkey < 50))");
     }
 
     @Test
