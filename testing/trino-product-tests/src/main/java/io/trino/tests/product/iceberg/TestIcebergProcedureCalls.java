c80e1698b73037c789ca187a91733ff64d6223e8
Konrad Dziedzic <konraddziedzic@gmail.com>
2023-07-21 12:05:58 +0200

    Support TINYINT and SMALLINT in migration proceure from hive to iceberg

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/iceberg/TestIcebergProcedureCalls.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/iceberg/TestIcebergProcedureCalls.java
@@ -13,9 +13,13 @@
  */
 package io.trino.tests.product.iceberg;
 
+import com.google.common.collect.ImmutableList;
 import io.trino.tempto.ProductTest;
+import io.trino.tempto.assertions.QueryAssert.Row;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import static io.trino.tempto.assertions.QueryAssert.Row.row;
@@ -53,6 +57,58 @@ public class TestIcebergProcedureCalls
                 .containsOnly(row(1));
 
         onTrino().executeQuery("DROP TABLE IF EXISTS " + icebergTableName);
+    }
+
+    @Test(groups = {ICEBERG, PROFILE_SPECIFIC_TESTS}, dataProvider = "fileFormats")
+    public void testMigrateHiveTableWithTinyintType(String fileFormat)
+    {
+        String tableName = "test_migrate_tinyint" + randomNameSuffix();
+        String hiveTableName = "hive.default." + tableName;
+        String icebergTableName = "iceberg.default." + tableName;
+        String sparkTableName = "iceberg_test.default." + tableName;
+
+        String createTable = "CREATE TABLE " + hiveTableName + "(col TINYINT) WITH (format = '" + fileFormat + "')";
+        if (fileFormat.equals("AVRO")) {
+            assertQueryFailure(() -> onTrino().executeQuery(createTable))
+                    .hasMessageContaining("Column 'col' is tinyint, which is not supported by Avro. Use integer instead.");
+            return;
+        }
+        onTrino().executeQuery(createTable);
+        onTrino().executeQuery("INSERT INTO " + hiveTableName + " VALUES -128, 127");
+
+        onTrino().executeQuery("CALL iceberg.system.migrate('default', '" + tableName + "')");
+
+        List<Row> expected = ImmutableList.of(row(-128), row(127));
+        assertThat(onTrino().executeQuery("SELECT * FROM " + icebergTableName)).containsOnly(expected);
+        assertThat(onSpark().executeQuery("SELECT * FROM " + sparkTableName)).containsOnly(expected);
+
+        onTrino().executeQuery("DROP TABLE " + icebergTableName);
+    }
+
+    @Test(groups = {ICEBERG, PROFILE_SPECIFIC_TESTS}, dataProvider = "fileFormats")
+    public void testMigrateHiveTableWithSmallintType(String fileFormat)
+    {
+        String tableName = "test_migrate_smallint" + randomNameSuffix();
+        String hiveTableName = "hive.default." + tableName;
+        String icebergTableName = "iceberg.default." + tableName;
+        String sparkTableName = "iceberg_test.default." + tableName;
+
+        String createTable = "CREATE TABLE " + hiveTableName + "(col SMALLINT) WITH (format = '" + fileFormat + "')";
+        if (fileFormat.equals("AVRO")) {
+            assertQueryFailure(() -> onTrino().executeQuery(createTable))
+                    .hasMessageContaining("Column 'col' is smallint, which is not supported by Avro. Use integer instead.");
+            return;
+        }
+        onTrino().executeQuery(createTable);
+        onTrino().executeQuery("INSERT INTO " + hiveTableName + " VALUES -32768, 32767");
+
+        onTrino().executeQuery("CALL iceberg.system.migrate('default', '" + tableName + "')");
+
+        List<Row> expected = ImmutableList.of(row(-32768), row(32767));
+        assertThat(onTrino().executeQuery("SELECT * FROM " + icebergTableName)).containsOnly(expected);
+        assertThat(onSpark().executeQuery("SELECT * FROM " + sparkTableName)).containsOnly(expected);
+
+        onTrino().executeQuery("DROP TABLE " + icebergTableName);
     }
 
     @Test(groups = {ICEBERG, PROFILE_SPECIFIC_TESTS})
@@ -227,5 +283,11 @@ public class TestIcebergProcedureCalls
         return (Long) onTrino().executeQuery(
                 format("SELECT snapshot_id FROM iceberg.default.\"%s$snapshots\" WHERE parent_id IS NOT NULL ORDER BY committed_at FETCH FIRST 1 ROW WITH TIES", tableName))
                 .getOnlyValue();
+    }
+
+    @DataProvider
+    public static Object[][] fileFormats()
+    {
+        return new Object[][] {{"ORC"}, {"PARQUET"}, {"AVRO"}};
     }
 }
