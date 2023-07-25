c43020a1ad34fc94597b5deaccdd724274be5052
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2023-07-25 10:35:28 +0200

    Update MSSQL Server JDBC driver to 12.2.0.jre11

--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/BaseSqlServerConnectorTest.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/BaseSqlServerConnectorTest.java
@@ -605,7 +605,7 @@ public abstract class BaseSqlServerConnectorTest
         assertQuery("SELECT orderdate FROM orders WHERE orderdate = DATE '1997-09-14'", "VALUES DATE '1997-09-14'");
         assertQueryFails(
                 "SELECT * FROM orders WHERE orderdate = DATE '-1996-09-14'",
-                "Conversion failed when converting date and/or time from character string\\.");
+                ".*\\QConversion failed when converting date and/or time from character string.\\E");
     }
 
     @Override
