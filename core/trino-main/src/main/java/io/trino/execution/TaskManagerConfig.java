85be651bc39ac93175b2a1dab4d97b5c4b63054b
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-08-02 00:56:27 +0530

    Revert "Set value of maxWriterCount to availableProcessors * 2"
    This reverts commit 6afbc19d477620b4aebec2fe86ea72cdc0da64ac.
    A high default for task.scale-writers.max-writer-count can lead to queries
    running out of memory more often

--- core/trino-main/src/main/java/io/trino/execution/TaskManagerConfig.java
+++ core/trino-main/src/main/java/io/trino/execution/TaskManagerConfig.java
@@ -78,11 +78,11 @@ public class TaskManagerConfig
     private Duration interruptStuckSplitTasksDetectionInterval = new Duration(2, TimeUnit.MINUTES);
 
     private boolean scaleWritersEnabled = true;
-    // Set the value of default max writer count to 2 * max(the number of processors, 32). We can do this
+    // Set the value of default max writer count to the number of processors and cap it to 32. We can do this
     // because preferred write partitioning is always enabled for local exchange thus partitioned inserts will never
     // use this property. Hence, there is no risk in terms of more numbers of physical writers which can cause high
     // resource utilization.
-    private int scaleWritersMaxWriterCount = min(getAvailablePhysicalProcessorCount(), 32) * 2;
+    private int scaleWritersMaxWriterCount = min(getAvailablePhysicalProcessorCount(), 32);
     private int writerCount = 1;
     // Default value of partitioned task writer count should be above 1, otherwise it can create a plan
     // with a single gather exchange node on the coordinator due to a single available processor. Whereas,
