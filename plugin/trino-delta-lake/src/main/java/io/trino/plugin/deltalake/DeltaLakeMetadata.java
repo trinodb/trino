e118fbf97d900abb7bcb0b68cb29da14491ec8a5
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-01 21:49:32 +0200

    Remove duplicate code
    The stream traversal is done already earlier in the method, reuse.

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -2225,9 +2225,7 @@ public class DeltaLakeMetadata
         }
         catch (Exception e) {
             // Can be safely ignored since a VACUUM from DeltaLake will take care of such orphaned files
-            LOG.warn(e, "Failed cleanup of leftover files from failed write, files are: %s", dataFiles.stream()
-                    .map(dataFileInfo -> appendPath(tableLocation, dataFileInfo.getPath()))
-                    .collect(toImmutableList()));
+            LOG.warn(e, "Failed cleanup of leftover files from failed write, files are: %s", filesToDelete);
         }
     }
 
