fc85b30bd846c59ec1fb4d563bbd9da2a7a2ad46
Martin Traverso <mtraverso@gmail.com>
2023-08-03 17:31:09 -0700

    Migrate Accumulo tests to JUnit

--- plugin/trino-accumulo/src/test/java/io/trino/plugin/accumulo/model/TestSerializedRange.java
+++ plugin/trino-accumulo/src/test/java/io/trino/plugin/accumulo/model/TestSerializedRange.java
@@ -14,7 +14,7 @@
 package io.trino.plugin.accumulo.model;
 
 import org.apache.accumulo.core.data.Range;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.testng.Assert.assertEquals;
 
