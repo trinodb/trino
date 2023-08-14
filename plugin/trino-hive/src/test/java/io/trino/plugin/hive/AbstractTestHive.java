f7e2efd11b17d461b509b76910f456cfd77dbe2a
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-14 13:28:37 +0200

    Fix Hive write to bucketed table when native writers disabled
    This reverts commit a794b32a7846a555e1d5da9697cea6e54681b578.

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/AbstractTestHive.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/AbstractTestHive.java
@@ -834,7 +834,6 @@ public abstract class AbstractTestHive
         metadataFactory = new HiveMetadataFactory(
                 new CatalogName("hive"),
                 HiveMetastoreFactory.ofInstance(metastoreClient),
-                getDefaultHiveFileWriterFactories(hiveConfig, hdfsEnvironment),
                 new HdfsFileSystemFactory(hdfsEnvironment, HDFS_FILE_SYSTEM_STATS),
                 hdfsEnvironment,
                 partitionManager,
