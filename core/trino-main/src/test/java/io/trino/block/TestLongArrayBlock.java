886e712186029e00a11756b36f8e2a05ef5caf36
Dain Sundstrom <dain@iq80.com>
2023-08-17 07:53:30 -0700

    Remove Block.getInt(position, offset)

--- core/trino-main/src/test/java/io/trino/block/TestLongArrayBlock.java
+++ core/trino-main/src/test/java/io/trino/block/TestLongArrayBlock.java
@@ -116,12 +116,6 @@ public class TestLongArrayBlock
     }
 
     @Override
-    protected boolean isIntAccessSupported()
-    {
-        return false;
-    }
-
-    @Override
     protected boolean isSliceAccessSupported()
     {
         return false;
