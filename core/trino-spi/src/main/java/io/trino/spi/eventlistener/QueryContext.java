d588abed44e4c9b6940a5503335068b50de03eaf
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2023-09-11 11:40:22 +0200

    Include enabled roles in QueryContext for event listeners
    It's odd that it included the principal and groups, but not enabled
    roles.

--- core/trino-spi/src/main/java/io/trino/spi/eventlistener/QueryContext.java
+++ core/trino-spi/src/main/java/io/trino/spi/eventlistener/QueryContext.java
@@ -34,6 +34,7 @@ public class QueryContext
     private final String user;
     private final String originalUser;
     private final Optional<String> principal;
+    private final Set<String> enabledRoles;
     private final Set<String> groups;
     private final Optional<String> traceToken;
     private final Optional<String> remoteClientAddress;
@@ -65,6 +66,7 @@ public class QueryContext
             String user,
             String originalUser,
             Optional<String> principal,
+            Set<String> enabledRoles,
             Set<String> groups,
             Optional<String> traceToken,
             Optional<String> remoteClientAddress,
@@ -87,6 +89,7 @@ public class QueryContext
         this.user = requireNonNull(user, "user is null");
         this.originalUser = requireNonNull(originalUser, "originalUser is null");
         this.principal = requireNonNull(principal, "principal is null");
+        this.enabledRoles = requireNonNull(enabledRoles, "enabledRoles is null");
         this.groups = requireNonNull(groups, "groups is null");
         this.traceToken = requireNonNull(traceToken, "traceToken is null");
         this.remoteClientAddress = requireNonNull(remoteClientAddress, "remoteClientAddress is null");
@@ -123,6 +126,12 @@ public class QueryContext
     public Optional<String> getPrincipal()
     {
         return principal;
+    }
+
+    @JsonProperty
+    public Set<String> getEnabledRoles()
+    {
+        return enabledRoles;
     }
 
     @JsonProperty
