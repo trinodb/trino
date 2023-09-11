c9d65d52117a527723dd3c30d177673d83046bcb
Bhargavi Sagi <bhsagi@amazon.com>
2023-09-11 16:24:44 -0700

    fix incorrect results when correlated subquery returns zero rows

--- core/trino-main/src/test/java/io/trino/sql/query/TestSubqueries.java
+++ core/trino-main/src/test/java/io/trino/sql/query/TestSubqueries.java
@@ -46,7 +46,7 @@ import static io.trino.sql.planner.assertions.PlanMatchPattern.values;
 import static io.trino.sql.planner.plan.AggregationNode.Step.FINAL;
 import static io.trino.sql.planner.plan.AggregationNode.Step.PARTIAL;
 import static io.trino.sql.planner.plan.AggregationNode.Step.SINGLE;
-import static io.trino.sql.planner.plan.JoinNode.Type.INNER;
+import static io.trino.sql.planner.plan.JoinNode.Type.LEFT;
 import static io.trino.testing.TestingHandles.TEST_CATALOG_NAME;
 import static io.trino.testing.TestingSession.testSessionBuilder;
 import static java.lang.String.format;
@@ -204,7 +204,7 @@ public class TestSubqueries
                 "SELECT (SELECT t.a FROM (VALUES 1, 2, 3) t(a) WHERE t.a = t2.b ORDER BY a LIMIT 1) FROM (VALUES 1.0, 2.0) t2(b)",
                 "VALUES 1, 2",
                 output(
-                        join(INNER, builder -> builder
+                        join(LEFT, builder -> builder
                                 .equiCriteria("cast_b", "cast_a")
                                 .left(
                                         project(
@@ -228,7 +228,7 @@ public class TestSubqueries
                 "SELECT (SELECT t.a FROM (VALUES 1, 2, 3, 4, 5) t(a) WHERE t.a = t2.b * t2.c - 1 ORDER BY a LIMIT 1) FROM (VALUES (1, 2), (2, 3)) t2(b, c)",
                 "VALUES 1, 5",
                 output(
-                        join(INNER, builder -> builder
+                        join(LEFT, builder -> builder
                                 .equiCriteria("expr", "a")
                                 .left(
                                         project(
