9a55d5dd1e45a49ec1da94a4df1802f4be1da16e
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-09-19 13:13:05 +0530

    Make ParquetFileWriter final

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/parquet/ParquetFileWriter.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/parquet/ParquetFileWriter.java
@@ -50,7 +50,7 @@ import static io.trino.plugin.hive.HiveErrorCode.HIVE_WRITER_DATA_ERROR;
 import static io.trino.plugin.hive.HiveErrorCode.HIVE_WRITE_VALIDATION_FAILED;
 import static java.util.Objects.requireNonNull;
 
-public class ParquetFileWriter
+public final class ParquetFileWriter
         implements FileWriter
 {
     private static final int INSTANCE_SIZE = instanceSize(ParquetFileWriter.class);
