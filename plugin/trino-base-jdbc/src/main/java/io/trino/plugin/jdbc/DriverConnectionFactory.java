9f5658cbf6256a79a27ca113623f9e7ada75467a
Yuya Ebihara <ebyhry@gmail.com>
2023-09-19 10:35:38 +0900

    Update OpenTelemetry to 1.30.1
    This requires version up of dep.opentelemetry-instrumentation.version.

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/DriverConnectionFactory.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/DriverConnectionFactory.java
@@ -75,9 +75,7 @@ public class DriverConnectionFactory
             throws SQLException
     {
         Properties properties = getCredentialProperties(session.getIdentity());
-        // TODO: Telemetry is disabled due to NPE being thrown on null connection
-        // Connection connection = dataSource.getConnection(properties);
-        Connection connection = driver.connect(connectionUrl, properties);
+        Connection connection = dataSource.getConnection(properties);
         checkState(connection != null, "Driver returned null connection, make sure the connection URL '%s' is valid for the driver %s", connectionUrl, driver);
         return connection;
     }
