8b2c3be9b91a3a02fb6e67f8c387a28c81a3bd5a
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-09-19 13:01:43 +0530

    Avoid re-reading parquet footer for iceberg stats in writer
    Currently we're reading parquet file footer back just after
    writing it to file system for computing iceberg stats.
    Since Trino parquet writer already has the file footer, there
    is no need to perform an extra IO to get the footer again.

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergParquetFileWriter.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergParquetFileWriter.java
@@ -13,17 +13,18 @@
  */
 package io.trino.plugin.iceberg;
 
-import io.trino.filesystem.TrinoFileSystem;
+import io.trino.filesystem.Location;
 import io.trino.filesystem.TrinoOutputFile;
 import io.trino.parquet.writer.ParquetWriterOptions;
 import io.trino.plugin.hive.parquet.ParquetFileWriter;
-import io.trino.plugin.iceberg.fileio.ForwardingInputFile;
 import io.trino.spi.Page;
+import io.trino.spi.TrinoException;
 import io.trino.spi.type.Type;
 import org.apache.iceberg.Metrics;
 import org.apache.iceberg.MetricsConfig;
-import org.apache.iceberg.io.InputFile;
 import org.apache.parquet.format.CompressionCodec;
+import org.apache.parquet.format.converter.ParquetMetadataConverter;
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
 import org.apache.parquet.schema.MessageType;
 
 import java.io.Closeable;
@@ -31,16 +32,19 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import static io.trino.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
-import static org.apache.iceberg.parquet.ParquetUtil.fileMetrics;
+import static org.apache.iceberg.parquet.ParquetUtil.footerMetrics;
 
 public final class IcebergParquetFileWriter
         implements IcebergFileWriter
 {
     private final MetricsConfig metricsConfig;
-    private final InputFile inputFile;
     private final ParquetFileWriter parquetFileWriter;
+    private final Location location;
 
     public IcebergParquetFileWriter(
             MetricsConfig metricsConfig,
@@ -53,8 +57,7 @@ public final class IcebergParquetFileWriter
             ParquetWriterOptions parquetWriterOptions,
             int[] fileInputColumnIndexes,
             CompressionCodec compressionCodec,
-            String trinoVersion,
-            TrinoFileSystem fileSystem)
+            String trinoVersion)
             throws IOException
     {
         this.parquetFileWriter = new ParquetFileWriter(
@@ -70,14 +73,21 @@ public final class IcebergParquetFileWriter
                 trinoVersion,
                 Optional.empty(),
                 Optional.empty());
+        this.location = outputFile.location();
         this.metricsConfig = requireNonNull(metricsConfig, "metricsConfig is null");
-        this.inputFile = new ForwardingInputFile(fileSystem.newInputFile(outputFile.location()));
     }
 
     @Override
     public Metrics getMetrics()
     {
-        return fileMetrics(inputFile, metricsConfig);
+        ParquetMetadata parquetMetadata;
+        try {
+            parquetMetadata = new ParquetMetadataConverter().fromParquetMetadata(parquetFileWriter.getFileMetadata());
+        }
+        catch (IOException e) {
+            throw new TrinoException(GENERIC_INTERNAL_ERROR, format("Error creating metadata for Parquet file %s", location), e);
+        }
+        return footerMetrics(parquetMetadata, Stream.empty(), metricsConfig);
     }
 
     @Override
