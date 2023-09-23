253dc959395701447cf5f1a5df0ba705f778c6ea
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2023-09-23 23:20:08 +0200

    Remove SyntheticColumnHandleBuilder
    This entity was not adding much value, it had only single and simple
    implementation. However it introduced complexity:
     - changed many constructors APIs
     - protected field DefaultJdbcMetadataFactory
     - allowed to introduce an override of SyntheticColumnHandleBuilder
       which on it is own is is way more complex that single package
       private method

--- plugin/trino-ignite/src/main/java/io/trino/plugin/ignite/IgniteJdbcMetadataFactory.java
+++ plugin/trino-ignite/src/main/java/io/trino/plugin/ignite/IgniteJdbcMetadataFactory.java
@@ -19,7 +19,6 @@ import io.trino.plugin.jdbc.DefaultJdbcMetadataFactory;
 import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcMetadata;
 import io.trino.plugin.jdbc.JdbcQueryEventListener;
-import io.trino.plugin.jdbc.SyntheticColumnHandleBuilder;
 
 import java.util.Set;
 
@@ -31,17 +30,15 @@ public class IgniteJdbcMetadataFactory
     private final Set<JdbcQueryEventListener> jdbcQueryEventListeners;
 
     @Inject
-    public IgniteJdbcMetadataFactory(JdbcClient jdbcClient,
-                Set<JdbcQueryEventListener> jdbcQueryEventListeners,
-                SyntheticColumnHandleBuilder syntheticColumnHandleBuilder)
+    public IgniteJdbcMetadataFactory(JdbcClient jdbcClient, Set<JdbcQueryEventListener> jdbcQueryEventListeners)
     {
-        super(jdbcClient, jdbcQueryEventListeners, syntheticColumnHandleBuilder);
+        super(jdbcClient, jdbcQueryEventListeners);
         this.jdbcQueryEventListeners = ImmutableSet.copyOf(requireNonNull(jdbcQueryEventListeners, "jdbcQueryEventListeners is null"));
     }
 
     @Override
     protected JdbcMetadata create(JdbcClient transactionCachingJdbcClient)
     {
-        return new IgniteMetadata(transactionCachingJdbcClient, jdbcQueryEventListeners, syntheticColumnBuilder);
+        return new IgniteMetadata(transactionCachingJdbcClient, jdbcQueryEventListeners);
     }
 }
