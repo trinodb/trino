d9d8e3c129c725566942abe01585cff18d981c06
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2023-09-27 18:18:55 +0200

    Disable Jackson's read constraints
    Previous testing was conducted against wrong jsonCodec-provided instance

--- client/trino-client/src/test/java/io/trino/client/TestQueryResults.java
+++ client/trino-client/src/test/java/io/trino/client/TestQueryResults.java
@@ -13,53 +13,66 @@
  */
 package io.trino.client;
 
-import io.airlift.json.JsonCodec;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.google.common.base.Strings;
 import org.testng.annotations.Test;
 
-import static io.airlift.json.JsonCodec.jsonCodec;
+import static io.trino.client.JsonCodec.jsonCodec;
+import static java.lang.String.format;
 import static org.testng.Assert.assertEquals;
 
 public class TestQueryResults
 {
     private static final JsonCodec<QueryResults> QUERY_RESULTS_CODEC = jsonCodec(QueryResults.class);
 
+    private static final String GOLDEN_VALUE = "{\n" +
+            "  \"id\" : \"20160128_214710_00012_rk68b\",\n" +
+            "  \"infoUri\" : \"http://localhost:54855/query.html?20160128_214710_00012_rk68b\",\n" +
+            "  \"columns\" : [ {\n" +
+            "    \"name\" : \"_col0\",\n" +
+            "    \"type\" : \"bigint\",\n" +
+            "    \"typeSignature\" : {\n" +
+            "      \"rawType\" : \"varchar\",\n" +
+            "      \"typeArguments\" : [ ],\n" +
+            "      \"literalArguments\" : [ ],\n" +
+            "      \"arguments\" : [ ]\n" +
+            "    }\n" +
+            "  } ],\n" +
+            "  \"data\" : [ [ %s ] ],\n" +
+            "  \"stats\" : {\n" +
+            "    \"state\" : \"FINISHED\",\n" +
+            "    \"queued\" : false,\n" +
+            "    \"scheduled\" : false,\n" +
+            "    \"nodes\" : 0,\n" +
+            "    \"totalSplits\" : 0,\n" +
+            "    \"queuedSplits\" : 0,\n" +
+            "    \"runningSplits\" : 0,\n" +
+            "    \"completedSplits\" : 0,\n" +
+            "    \"cpuTimeMillis\" : 0,\n" +
+            "    \"wallTimeMillis\" : 0,\n" +
+            "    \"queuedTimeMillis\" : 0,\n" +
+            "    \"elapsedTimeMillis\" : 0,\n" +
+            "    \"processedRows\" : 0,\n" +
+            "    \"processedBytes\" : 0,\n" +
+            "    \"peakMemoryBytes\" : 0\n" +
+            "  }\n" +
+            "}";
+
     @Test
     public void testCompatibility()
+            throws JsonProcessingException
     {
-        String goldenValue = "{\n" +
-                "  \"id\" : \"20160128_214710_00012_rk68b\",\n" +
-                "  \"infoUri\" : \"http://localhost:54855/query.html?20160128_214710_00012_rk68b\",\n" +
-                "  \"columns\" : [ {\n" +
-                "    \"name\" : \"_col0\",\n" +
-                "    \"type\" : \"bigint\",\n" +
-                "    \"typeSignature\" : {\n" +
-                "      \"rawType\" : \"bigint\",\n" +
-                "      \"typeArguments\" : [ ],\n" +
-                "      \"literalArguments\" : [ ],\n" +
-                "      \"arguments\" : [ ]\n" +
-                "    }\n" +
-                "  } ],\n" +
-                "  \"data\" : [ [ 123 ] ],\n" +
-                "  \"stats\" : {\n" +
-                "    \"state\" : \"FINISHED\",\n" +
-                "    \"queued\" : false,\n" +
-                "    \"scheduled\" : false,\n" +
-                "    \"nodes\" : 0,\n" +
-                "    \"totalSplits\" : 0,\n" +
-                "    \"queuedSplits\" : 0,\n" +
-                "    \"runningSplits\" : 0,\n" +
-                "    \"completedSplits\" : 0,\n" +
-                "    \"cpuTimeMillis\" : 0,\n" +
-                "    \"wallTimeMillis\" : 0,\n" +
-                "    \"queuedTimeMillis\" : 0,\n" +
-                "    \"elapsedTimeMillis\" : 0,\n" +
-                "    \"processedRows\" : 0,\n" +
-                "    \"processedBytes\" : 0,\n" +
-                "    \"peakMemoryBytes\" : 0\n" +
-                "  }\n" +
-                "}";
+        QueryResults results = QUERY_RESULTS_CODEC.fromJson(format(GOLDEN_VALUE, "\"123\""));
+        assertEquals(results.getId(), "20160128_214710_00012_rk68b");
+    }
 
-        QueryResults results = QUERY_RESULTS_CODEC.fromJson(goldenValue);
+    @Test
+    public void testReadLongColumn()
+            throws JsonProcessingException
+    {
+        String longString = Strings.repeat("a", StreamReadConstraints.DEFAULT_MAX_STRING_LEN + 1);
+        QueryResults results = QUERY_RESULTS_CODEC.fromJson(format(GOLDEN_VALUE, '"' + longString + '"'));
         assertEquals(results.getId(), "20160128_214710_00012_rk68b");
     }
 }
