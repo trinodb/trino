d056382f5e0440a912507baa7a3c6956619079e7
Dariusz Pakulski <61126472+bitweld@users.noreply.github.com>
2023-10-03 17:44:43 +0200

    Don't set s3api create-bucket LocationConstraint config for us-east-1
    Make the LocationConstraint create-bucket-configuration property optional
    depending on which aws region is used to run ci. LocationConstraint
    can't be set if the AWS_REGION used is us-east-1

--- .github/bin/s3/setup-empty-s3-bucket.sh
+++ .github/bin/s3/setup-empty-s3-bucket.sh
@@ -10,10 +10,17 @@ S3_BUCKET_IDENTIFIER=trino-s3fs-ci-$(openssl rand -hex 8)
 S3_BUCKET_TTL=$(date -u -d "+2 hours" +"%Y-%m-%dT%H:%M:%SZ" 2>/dev/null || date -u -v "+2H" +"%Y-%m-%dT%H:%M:%SZ")
 
 echo "Creating an empty AWS S3 bucket ${S3_BUCKET_IDENTIFIER} in the region ${AWS_REGION}"
+
+OPTIONAL_BUCKET_CONFIGURATION=()
+# LocationConstraint configuration property is not allowed for us-east-1 AWS region
+if [ "${AWS_REGION}" != 'us-east-1' ]; then
+    OPTIONAL_BUCKET_CONFIGURATION+=("--create-bucket-configuration" "LocationConstraint=${AWS_REGION}")
+fi
+
 S3_CREATE_BUCKET_OUTPUT=$(aws s3api create-bucket \
   --bucket "${S3_BUCKET_IDENTIFIER}" \
   --region "${AWS_REGION}" \
-  --create-bucket-configuration LocationConstraint="${AWS_REGION}")
+  "${OPTIONAL_BUCKET_CONFIGURATION[@]}")
 
 if [ -z "${S3_CREATE_BUCKET_OUTPUT}" ]; then
     echo "Unexpected error while attempting to create the S3 bucket ${S3_BUCKET_IDENTIFIER} in the region ${AWS_REGION}"
