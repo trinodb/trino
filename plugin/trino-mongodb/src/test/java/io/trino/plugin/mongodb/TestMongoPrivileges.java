0a134ab7f2ad1ba181506c4bb15a5fc52c3632f1
Sasha Sheikin <myminitrue@gmail.com>
2023-10-05 17:23:56 +0200

    Refactor TestMongoPrivileges
    Inline testPrivilege
    Inline createQueryRunner
    Static import

--- plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoPrivileges.java
+++ plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoPrivileges.java
@@ -24,7 +24,6 @@ import io.trino.testing.QueryRunner;
 import org.bson.Document;
 import org.testng.annotations.Test;
 
-import java.util.Locale;
 import java.util.Optional;
 
 import static io.airlift.testing.Closeables.closeAllSuppress;
@@ -34,6 +33,7 @@ import static io.trino.plugin.mongodb.AuthenticatedMongoServer.privilege;
 import static io.trino.plugin.mongodb.AuthenticatedMongoServer.resource;
 import static io.trino.plugin.mongodb.AuthenticatedMongoServer.role;
 import static io.trino.testing.TestingSession.testSessionBuilder;
+import static java.util.Locale.ENGLISH;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestMongoPrivileges
@@ -50,7 +50,23 @@ public class TestMongoPrivileges
             throws Exception
     {
         AuthenticatedMongoServer mongoServer = closeAfterClass(setupMongoServer());
-        return createMongoQueryRunner(mongoServer.testUserConnectionString(TEST_DATABASE, TEST_USER, TEST_PASSWORD).getConnectionString());
+        DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(testSessionBuilder()
+                        .setCatalog(Optional.empty())
+                        .setSchema(Optional.empty())
+                        .build())
+                .build();
+        try {
+            queryRunner.installPlugin(new MongoPlugin());
+            String connectionUrl = mongoServer.testUserConnectionString(TEST_DATABASE, TEST_USER, TEST_PASSWORD).getConnectionString();
+            queryRunner.createCatalog("mongodb", "mongodb", ImmutableMap.of(
+                    "mongodb.case-insensitive-name-matching", "true",
+                    "mongodb.connection-url", connectionUrl));
+            return queryRunner;
+        }
+        catch (Throwable e) {
+            closeAllSuppress(e, queryRunner);
+            throw e;
+        }
     }
 
     @Test
@@ -62,7 +78,7 @@ public class TestMongoPrivileges
     @Test
     public void testTablesVisibility()
     {
-        assertQuery("SHOW TABLES FROM mongodb." + TEST_DATABASE, "VALUES '%s'".formatted(TEST_COLLECTION.toLowerCase(Locale.ENGLISH)));
+        assertQuery("SHOW TABLES FROM mongodb." + TEST_DATABASE, "VALUES '%s'".formatted(TEST_COLLECTION.toLowerCase(ENGLISH)));
     }
 
     private static AuthenticatedMongoServer setupMongoServer()
@@ -87,43 +103,18 @@ public class TestMongoPrivileges
         assertThat(commandStatus).isEqualTo(1.0);
     }
 
-    private static DistributedQueryRunner createMongoQueryRunner(String connectionUrl)
-            throws Exception
-    {
-        DistributedQueryRunner queryRunner = null;
-        try {
-            queryRunner = DistributedQueryRunner.builder(testSessionBuilder()
-                            .setCatalog(Optional.empty())
-                            .setSchema(Optional.empty())
-                            .build())
-                    .build();
-            queryRunner.installPlugin(new MongoPlugin());
-            queryRunner.createCatalog("mongodb", "mongodb", ImmutableMap.of(
-                    "mongodb.case-insensitive-name-matching", "true",
-                    "mongodb.connection-url", connectionUrl));
-            return queryRunner;
-        }
-        catch (Throwable e) {
-            closeAllSuppress(e, queryRunner);
-            throw e;
-        }
-    }
-
     private static Document createTestRole()
     {
         return createRole(
                 TEST_ROLE,
                 ImmutableList.of(
-                        testPrivilege("_schema"),
-                        testPrivilege(TEST_COLLECTION)),
+                        privilege(
+                                resource(TEST_DATABASE, "_schema"),
+                                ImmutableList.of("find")),
+                        privilege(
+                                resource(TEST_DATABASE, TEST_COLLECTION),
+                                ImmutableList.of("find"))),
                 ImmutableList.of());
-    }
-
-    private static Document testPrivilege(String collectionName)
-    {
-        return privilege(
-                resource(TEST_DATABASE, collectionName),
-                ImmutableList.of("find"));
     }
 
     private static Document createTestUser()
