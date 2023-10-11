bae96ad9e2959eb3f9fbe8d68f0c167b2bd52d9b
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-10-11 12:29:58 +0200

    Apply PrefixObjectNameGeneratorModule in tests
    This allows using QueryRunner for manually diagnosing metrics exported
    via `/metrics`. This way the metrics exported in tests will have same
    names as ones exported in real deployment.

--- core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
+++ core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
@@ -72,6 +72,7 @@ import io.trino.security.AccessControlManager;
 import io.trino.security.GroupProviderManager;
 import io.trino.server.GracefulShutdownHandler;
 import io.trino.server.PluginInstaller;
+import io.trino.server.PrefixObjectNameGeneratorModule;
 import io.trino.server.Server;
 import io.trino.server.ServerMainModule;
 import io.trino.server.SessionPropertyDefaults;
@@ -267,6 +268,7 @@ public class TestingTrinoServer
                 .add(new JsonModule())
                 .add(new JaxrsModule())
                 .add(new MBeanModule())
+                .add(new PrefixObjectNameGeneratorModule("io.trino"))
                 .add(new TestingJmxModule())
                 .add(new JmxOpenMetricsModule())
                 .add(new EventModule())
