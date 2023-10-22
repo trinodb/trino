b26c74e46b73cea3a9205d386d15119906a12be9
Dain Sundstrom <dain@iq80.com>
2023-10-22 11:33:31 -0700

    Convert TrinoThriftBlock to ValueBlock

--- plugin/trino-thrift-api/src/main/java/io/trino/plugin/thrift/api/datatypes/TrinoThriftTimestamp.java
+++ plugin/trino-thrift-api/src/main/java/io/trino/plugin/thrift/api/datatypes/TrinoThriftTimestamp.java
@@ -19,6 +19,7 @@ import io.airlift.drift.annotations.ThriftStruct;
 import io.trino.plugin.thrift.api.TrinoThriftBlock;
 import io.trino.spi.block.Block;
 import io.trino.spi.block.LongArrayBlock;
+import io.trino.spi.block.ValueBlock;
 import io.trino.spi.connector.RecordSet;
 import io.trino.spi.type.Type;
 import jakarta.annotation.Nullable;
@@ -77,7 +78,7 @@ public final class TrinoThriftTimestamp
     }
 
     @Override
-    public Block toBlock(Type desiredType)
+    public ValueBlock toBlock(Type desiredType)
     {
         checkArgument(TIMESTAMP_MILLIS.equals(desiredType), "type doesn't match: %s", desiredType);
         int numberOfRecords = numberOfRecords();
