cacd829f9f0fd72ae740645137d8857eee19ef9f
Jan Waś <jan.was@starburstdata.com>
2023-10-30 11:14:14 +0100

    Always use lowercase Hive types

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/metastore/TestMetastoreUtil.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/metastore/TestMetastoreUtil.java
@@ -30,6 +30,7 @@ import io.trino.spi.predicate.ValueSet;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
@@ -183,6 +184,19 @@ public class TestMetastoreUtil
     {
         Properties actual = MetastoreUtil.getHiveSchema(ThriftMetastoreUtil.fromMetastoreApiPartition(TEST_PARTITION_WITH_UNSUPPORTED_FIELDS), ThriftMetastoreUtil.fromMetastoreApiTable(TEST_TABLE_WITH_UNSUPPORTED_FIELDS, TEST_SCHEMA));
         assertEquals(actual, TEST_TABLE_METADATA);
+    }
+
+    @Test
+    public void testHiveSchemaCaseInsensitive()
+    {
+        List<FieldSchema> testSchema = TEST_SCHEMA.stream()
+                .map(fieldSchema -> new FieldSchema(fieldSchema.getName(), fieldSchema.getType().toUpperCase(Locale.ENGLISH), fieldSchema.getComment()))
+                .toList();
+        Properties actualTable = MetastoreUtil.getHiveSchema(ThriftMetastoreUtil.fromMetastoreApiTable(TEST_TABLE_WITH_UNSUPPORTED_FIELDS, testSchema));
+        assertEquals(actualTable, TEST_TABLE_METADATA);
+
+        Properties actualPartition = MetastoreUtil.getHiveSchema(ThriftMetastoreUtil.fromMetastoreApiPartition(TEST_PARTITION_WITH_UNSUPPORTED_FIELDS), ThriftMetastoreUtil.fromMetastoreApiTable(TEST_TABLE_WITH_UNSUPPORTED_FIELDS, testSchema));
+        assertEquals(actualPartition, TEST_TABLE_METADATA);
     }
 
     @Test
