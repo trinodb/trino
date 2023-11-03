be475847d485eaccbe26f3259c5704f239c20a11
Dain Sundstrom <dain@iq80.com>
2023-11-02 21:38:48 -0700

    Add missing TestSecurityConfig
    (cherry picked from commit aefa96bd844a4bb050aa4c9c90c67fb0150a7a32)

---
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/security/TestSecurityConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.hive.security;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
+import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
+import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
+import static io.trino.plugin.hive.security.HiveSecurityModule.LEGACY;
+
+public class TestSecurityConfig
+{
+    @Test
+    public void testDefaults()
+    {
+        assertRecordedDefaults(recordDefaults(SecurityConfig.class)
+                .setSecuritySystem(LEGACY));
+    }
+
+    @Test
+    public void testExplicitPropertyMappings()
+            throws IOException
+    {
+        Map<String, String> properties = ImmutableMap.<String, String>builder()
+                .put("hive.security", "something")
+                .buildOrThrow();
+
+        SecurityConfig expected = new SecurityConfig()
+                .setSecuritySystem("something");
+
+        assertFullMapping(properties, expected);
+    }
+}
