339b86a16ea07fa68192cd5810e6f5a1172c43c9
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-11-09 19:47:59 +0530

    Move getColumnIndexStore to TrinoColumnIndexStore
    This method should be in parquet reader package rather
    than in a connector package
    
    (cherry picked from commit 26b6d6921447e7e2913113b8871a0979ae247826)

--- lib/trino-parquet/src/main/java/io/trino/parquet/reader/TrinoColumnIndexStore.java
+++ lib/trino-parquet/src/main/java/io/trino/parquet/reader/TrinoColumnIndexStore.java
@@ -18,7 +18,11 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ListMultimap;
 import io.trino.parquet.DiskRange;
 import io.trino.parquet.ParquetDataSource;
+import io.trino.parquet.ParquetReaderOptions;
+import io.trino.spi.predicate.Domain;
+import io.trino.spi.predicate.TupleDomain;
 import jakarta.annotation.Nullable;
+import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.format.Util;
 import org.apache.parquet.format.converter.ParquetMetadataConverter;
 import org.apache.parquet.hadoop.metadata.BlockMetaData;
@@ -32,12 +36,15 @@ import org.apache.parquet.schema.PrimitiveType;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.trino.memory.context.AggregatedMemoryContext.newSimpleAggregatedMemoryContext;
 import static java.util.Objects.requireNonNull;
 
@@ -127,6 +134,43 @@ public class TrinoColumnIndexStore
         }
 
         return offsetIndexStore.get(column);
+    }
+
+    public static Optional<ColumnIndexStore> getColumnIndexStore(
+            ParquetDataSource dataSource,
+            BlockMetaData blockMetadata,
+            Map<List<String>, ColumnDescriptor> descriptorsByPath,
+            TupleDomain<ColumnDescriptor> parquetTupleDomain,
+            ParquetReaderOptions options)
+    {
+        if (!options.isUseColumnIndex() || parquetTupleDomain.isAll() || parquetTupleDomain.isNone()) {
+            return Optional.empty();
+        }
+
+        boolean hasColumnIndex = false;
+        for (ColumnChunkMetaData column : blockMetadata.getColumns()) {
+            if (column.getColumnIndexReference() != null && column.getOffsetIndexReference() != null) {
+                hasColumnIndex = true;
+                break;
+            }
+        }
+
+        if (!hasColumnIndex) {
+            return Optional.empty();
+        }
+
+        Set<ColumnPath> columnsReadPaths = new HashSet<>(descriptorsByPath.size());
+        for (List<String> path : descriptorsByPath.keySet()) {
+            columnsReadPaths.add(ColumnPath.get(path.toArray(new String[0])));
+        }
+
+        Map<ColumnDescriptor, Domain> parquetDomains = parquetTupleDomain.getDomains()
+                .orElseThrow(() -> new IllegalStateException("Predicate other than none should have domains"));
+        Set<ColumnPath> columnsFilteredPaths = parquetDomains.keySet().stream()
+                .map(column -> ColumnPath.get(column.getPath()))
+                .collect(toImmutableSet());
+
+        return Optional.of(new TrinoColumnIndexStore(dataSource, blockMetadata, columnsReadPaths, columnsFilteredPaths));
     }
 
     private static <T> Map<ColumnPath, T> loadIndexes(
