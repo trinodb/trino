ddae2ecc015436e8eacd1b430b065ee5fb93d885
James Petty <pettja@amazon.com>
2023-11-15 15:59:34 -0500

    Limit RLE to direct expansion when partitioning
    Flushes PositionsAppenderPageBuilder entries if the cost of converting
    all RLE channels into direct channels would exceed the maximum page size
    by a factor of 8x.
    
    Previously, page builders could buffer a very large number of RLE
    positions without being considered full and then suddenly expand to huge
    sizes when forced to transition to a direct representation as a result
    of the RLE input value changing across one or more columns. In
    particular, this is can easily happen when pages are produced from CROSS
    JOIN UNNEST operations.

--- core/trino-main/src/test/java/io/trino/operator/output/TestPositionsAppenderPageBuilder.java
+++ core/trino-main/src/test/java/io/trino/operator/output/TestPositionsAppenderPageBuilder.java
@@ -34,8 +34,10 @@ public class TestPositionsAppenderPageBuilder
     public void testFullOnPositionCountLimit()
     {
         int maxPageBytes = 1024 * 1024;
+        int maxDirectSize = maxPageBytes * 10;
         PositionsAppenderPageBuilder pageBuilder = PositionsAppenderPageBuilder.withMaxPageSize(
                 maxPageBytes,
+                maxDirectSize,
                 List.of(VARCHAR),
                 new PositionsAppenderFactory(new BlockTypeOperators()));
 
@@ -52,6 +54,52 @@ public class TestPositionsAppenderPageBuilder
         // Append 10 more positions, crossing the threshold on position count
         pageBuilder.appendToOutputPartition(inputPage, positions);
         assertTrue(pageBuilder.isFull(), "pageBuilder should be full");
-        assertEquals(rleBlock.getSizeInBytes(), pageBuilder.getSizeInBytes());
+        PositionsAppenderSizeAccumulator sizeAccumulator = pageBuilder.computeAppenderSizes();
+        assertEquals(rleBlock.getSizeInBytes(), sizeAccumulator.getSizeInBytes());
+        assertTrue(sizeAccumulator.getDirectSizeInBytes() < maxDirectSize, "direct size should still be below threshold");
+        assertEquals(sizeAccumulator.getSizeInBytes(), pageBuilder.getSizeInBytes(), "pageBuilder sizeInBytes must match sizeAccumulator value");
+    }
+
+    @Test
+    public void testFullOnDirectSizeInBytes()
+    {
+        int maxPageBytes = 100;
+        int maxDirectSize = 1000;
+        PositionsAppenderPageBuilder pageBuilder = PositionsAppenderPageBuilder.withMaxPageSize(
+                maxPageBytes,
+                maxDirectSize,
+                List.of(VARCHAR),
+                new PositionsAppenderFactory(new BlockTypeOperators()));
+
+        PositionsAppenderSizeAccumulator sizeAccumulator = pageBuilder.computeAppenderSizes();
+        assertEquals(0L, sizeAccumulator.getSizeInBytes());
+        assertEquals(0L, sizeAccumulator.getDirectSizeInBytes());
+        assertFalse(pageBuilder.isFull());
+
+        Block rleBlock = RunLengthEncodedBlock.create(VARCHAR, Slices.utf8Slice("test"), 10);
+        Page inputPage = new Page(rleBlock);
+
+        IntArrayList positions = IntArrayList.wrap(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        pageBuilder.appendToOutputPartition(inputPage, positions);
+        // 10 positions inserted, size in bytes is still the same since we're in RLE mode but direct size is 10x
+        sizeAccumulator = pageBuilder.computeAppenderSizes();
+        assertEquals(rleBlock.getSizeInBytes(), sizeAccumulator.getSizeInBytes());
+        assertEquals(sizeAccumulator.getSizeInBytes(), pageBuilder.getSizeInBytes(), "pageBuilder sizeInBytes must match sizeAccumulator value");
+        assertEquals(rleBlock.getSizeInBytes() * 10, sizeAccumulator.getDirectSizeInBytes());
+        assertFalse(pageBuilder.isFull());
+
+        // Keep inserting until the direct size limit is reached
+        while (pageBuilder.computeAppenderSizes().getDirectSizeInBytes() < maxDirectSize) {
+            pageBuilder.appendToOutputPartition(inputPage, positions);
+        }
+        // size in bytes is unchanged
+        sizeAccumulator = pageBuilder.computeAppenderSizes();
+        assertEquals(rleBlock.getSizeInBytes(), sizeAccumulator.getSizeInBytes(), "sizeInBytes must still report the RLE block size only");
+        assertEquals(sizeAccumulator.getSizeInBytes(), pageBuilder.getSizeInBytes(), "pageBuilder sizeInBytes must match sizeAccumulator value");
+        // builder reports full due to maximum size in bytes reached
+        assertTrue(pageBuilder.isFull());
+        Page result = pageBuilder.build();
+        assertEquals(120, result.getPositionCount(), "result positions should be below the 8192 maximum");
+        assertTrue(result.getBlock(0) instanceof RunLengthEncodedBlock, "result block is RLE encoded");
     }
 }
