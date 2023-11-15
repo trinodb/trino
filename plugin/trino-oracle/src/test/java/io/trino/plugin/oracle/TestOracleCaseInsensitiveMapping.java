b77f74c7873b58b318873686751763efe925459f
Martin Traverso <mtraverso@gmail.com>
2023-11-15 14:05:39 -0800

    Remove unused methods

--- plugin/trino-oracle/src/test/java/io/trino/plugin/oracle/TestOracleCaseInsensitiveMapping.java
+++ plugin/trino-oracle/src/test/java/io/trino/plugin/oracle/TestOracleCaseInsensitiveMapping.java
@@ -18,7 +18,6 @@ import com.google.common.collect.ImmutableMap;
 import io.trino.plugin.jdbc.BaseCaseInsensitiveMappingTest;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.sql.SqlExecutor;
-import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.util.Optional;
@@ -93,13 +92,5 @@ public class TestOracleCaseInsensitiveMapping
     protected SqlExecutor onRemoteDatabase()
     {
         return oracleServer::execute;
-    }
-
-    @Test
-    public void forceTestNgToRespectSingleThreaded()
-    {
-        // TODO: Remove after updating TestNG to 7.4.0+ (https://github.com/trinodb/trino/issues/8571)
-        // TestNG doesn't enforce @Test(singleThreaded = true) when tests are defined in base class. According to
-        // https://github.com/cbeust/testng/issues/2361#issuecomment-688393166 a workaround it to add a dummy test to the leaf test class.
     }
 }
