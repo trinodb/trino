85f9424bb22748163361f9814adb629232699983
Dain Sundstrom <dain@iq80.com>
2023-11-17 00:04:36 -0800

    Merge ProjectionFactory implementations into Partition implementations

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/aws/athena/DateProjection.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/aws/athena/DateProjection.java
@@ -14,6 +14,8 @@
 package io.trino.plugin.hive.aws.athena;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import io.trino.spi.TrinoException;
 import io.trino.spi.predicate.Domain;
 import io.trino.spi.type.DateType;
 import io.trino.spi.type.TimestampType;
@@ -22,23 +24,52 @@ import io.trino.spi.type.VarcharType;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.slice.Slices.utf8Slice;
-import static io.trino.plugin.hive.aws.athena.DateProjectionFactory.UTC_TIME_ZONE_ID;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.COLUMN_PROJECTION_FORMAT;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.COLUMN_PROJECTION_INTERVAL;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.COLUMN_PROJECTION_INTERVAL_UNIT;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.COLUMN_PROJECTION_RANGE;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.getProjectionPropertyRequiredValue;
+import static io.trino.plugin.hive.aws.athena.PartitionProjectionProperties.getProjectionPropertyValue;
 import static io.trino.spi.predicate.Domain.singleValue;
+import static java.lang.String.format;
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.MONTHS;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
+import static java.util.TimeZone.getTimeZone;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 final class DateProjection
         implements Projection
 {
+    private static final ZoneId UTC_TIME_ZONE_ID = ZoneId.of("UTC");
+    // Limited to only DAYS, HOURS, MINUTES, SECONDS as we are not fully sure how everything above day
+    // is implemented in Athena. So we limit it to a subset of interval units which are explicitly clear how to calculate.
+    // The rest will be implemented if this is required as it would require making compatibility tests
+    // for results received from Athena and verifying if we receive identical with Trino.
+    private static final Set<ChronoUnit> DATE_PROJECTION_INTERVAL_UNITS = ImmutableSet.of(DAYS, HOURS, MINUTES, SECONDS);
+    private static final Pattern DATE_RANGE_BOUND_EXPRESSION_PATTERN = Pattern.compile("^\\s*NOW\\s*(([+-])\\s*([0-9]+)\\s*(DAY|HOUR|MINUTE|SECOND)S?\\s*)?$");
+
     private final String columnName;
     private final DateFormat dateFormat;
     private final Supplier<Instant> leftBound;
@@ -46,14 +77,57 @@ final class DateProjection
     private final int interval;
     private final ChronoUnit intervalUnit;
 
-    public DateProjection(String columnName, DateFormat dateFormat, Supplier<Instant> leftBound, Supplier<Instant> rightBound, int interval, ChronoUnit intervalUnit)
+    public DateProjection(String columnName, Type columnType, Map<String, Object> columnProperties)
     {
+        if (!(columnType instanceof VarcharType) &&
+                !(columnType instanceof DateType) &&
+                !(columnType instanceof TimestampType timestampType && timestampType.isShort())) {
+            throw new InvalidProjectionException(columnName, columnType);
+        }
+
         this.columnName = requireNonNull(columnName, "columnName is null");
+
+        String dateFormatPattern = getProjectionPropertyRequiredValue(
+                columnName,
+                columnProperties,
+                COLUMN_PROJECTION_FORMAT,
+                String::valueOf);
+
+        List<String> range = getProjectionPropertyRequiredValue(
+                columnName,
+                columnProperties,
+                COLUMN_PROJECTION_RANGE,
+                value -> ((List<?>) value).stream()
+                        .map(String.class::cast)
+                        .collect(toImmutableList()));
+        if (range.size() != 2) {
+            throw invalidRangeProperty(columnName, dateFormatPattern, Optional.empty());
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatPattern);
+        dateFormat.setLenient(false);
+        dateFormat.setTimeZone(getTimeZone(UTC_TIME_ZONE_ID));
         this.dateFormat = requireNonNull(dateFormat, "dateFormatPattern is null");
-        this.leftBound = requireNonNull(leftBound, "leftBound is null");
-        this.rightBound = requireNonNull(rightBound, "rightBound is null");
-        this.interval = interval;
-        this.intervalUnit = requireNonNull(intervalUnit, "intervalUnit is null");
+
+        leftBound = parseDateRangerBound(columnName, range.get(0), dateFormat);
+        rightBound = parseDateRangerBound(columnName, range.get(1), dateFormat);
+        if (!leftBound.get().isBefore(rightBound.get())) {
+            throw invalidRangeProperty(columnName, dateFormatPattern, Optional.empty());
+        }
+
+        interval = getProjectionPropertyValue(columnProperties, COLUMN_PROJECTION_INTERVAL, Integer.class::cast).orElse(1);
+        intervalUnit = getProjectionPropertyValue(columnProperties, COLUMN_PROJECTION_INTERVAL_UNIT, ChronoUnit.class::cast)
+                .orElseGet(() -> resolveDefaultChronoUnit(columnName, dateFormatPattern));
+
+        if (!DATE_PROJECTION_INTERVAL_UNITS.contains(intervalUnit)) {
+            throw new InvalidProjectionException(
+                    columnName,
+                    format(
+                            "Property: '%s' value '%s' is invalid. Available options: %s",
+                            COLUMN_PROJECTION_INTERVAL_UNIT,
+                            intervalUnit,
+                            DATE_PROJECTION_INTERVAL_UNITS));
+        }
     }
 
     @Override
@@ -111,5 +185,77 @@ final class DateProjection
             return domain.contains(singleValue(type, MILLISECONDS.toMicros(value.toEpochMilli())));
         }
         throw new InvalidProjectionException(columnName, type);
+    }
+
+    private static ChronoUnit resolveDefaultChronoUnit(String columnName, String dateFormatPattern)
+    {
+        String datePatternWithoutText = dateFormatPattern.replaceAll("'.*?'", "");
+        if (datePatternWithoutText.contains("S") || datePatternWithoutText.contains("s")
+                || datePatternWithoutText.contains("m") || datePatternWithoutText.contains("H")) {
+            // When the provided dates are at single-day or single-month precision.
+            throw new InvalidProjectionException(
+                    columnName,
+                    format(
+                            "Property: '%s' needs to be set when provided '%s' is less that single-day precision. Interval defaults to 1 day or 1 month, respectively. Otherwise, interval is required",
+                            COLUMN_PROJECTION_INTERVAL_UNIT,
+                            COLUMN_PROJECTION_FORMAT));
+        }
+        if (datePatternWithoutText.contains("d")) {
+            return DAYS;
+        }
+        return MONTHS;
+    }
+
+    private static Supplier<Instant> parseDateRangerBound(String columnName, String value, SimpleDateFormat dateFormat)
+    {
+        Matcher matcher = DATE_RANGE_BOUND_EXPRESSION_PATTERN.matcher(value);
+        if (matcher.matches()) {
+            String operator = matcher.group(2);
+            String multiplierString = matcher.group(3);
+            String unitString = matcher.group(4);
+            if (nonNull(operator) && nonNull(multiplierString) && nonNull(unitString)) {
+                unitString = unitString.toUpperCase(Locale.ENGLISH);
+                return new DateExpressionBound(
+                        Integer.parseInt(multiplierString),
+                        ChronoUnit.valueOf(unitString + "S"),
+                        operator.charAt(0) == '+');
+            }
+            if (value.trim().equals("NOW")) {
+                Instant now = Instant.now();
+                return () -> now;
+            }
+            throw invalidRangeProperty(columnName, dateFormat.toPattern(), Optional.of("Invalid expression"));
+        }
+
+        Instant dateBound;
+        try {
+            dateBound = dateFormat.parse(value).toInstant();
+        }
+        catch (ParseException e) {
+            throw invalidRangeProperty(columnName, dateFormat.toPattern(), Optional.of(e.getMessage()));
+        }
+        return () -> dateBound;
+    }
+
+    private static TrinoException invalidRangeProperty(String columnName, String dateFormatPattern, Optional<String> errorDetail)
+    {
+        throw new InvalidProjectionException(
+                columnName,
+                format(
+                        "Property: '%s' needs to be a list of 2 valid dates formatted as '%s' or '%s' that are sequential%s",
+                        COLUMN_PROJECTION_RANGE,
+                        dateFormatPattern,
+                        DATE_RANGE_BOUND_EXPRESSION_PATTERN.pattern(),
+                        errorDetail.map(error -> ": " + error).orElse("")));
+    }
+
+    private record DateExpressionBound(int multiplier, ChronoUnit unit, boolean increment)
+            implements Supplier<Instant>
+    {
+        @Override
+        public Instant get()
+        {
+            return Instant.now().plus(increment ? multiplier : -multiplier, unit);
+        }
     }
 }
