123757c5b0a17ef91ca306dc018b2b77bd06058c
David Phillips <david@acz.org>
2023-11-18 15:15:19 -0800

    Remove unused parameters from QueryRewriteException

--- service/trino-verifier/src/main/java/io/trino/verifier/QueryRewriter.java
+++ service/trino-verifier/src/main/java/io/trino/verifier/QueryRewriter.java
@@ -59,7 +59,6 @@ import static io.trino.sql.tree.LikeClause.PropertiesOption.INCLUDING;
 import static io.trino.sql.tree.SaveMode.IGNORE;
 import static io.trino.verifier.QueryType.READ;
 import static io.trino.verifier.VerifyCommand.statementToQueryType;
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
 
@@ -270,9 +269,9 @@ public class QueryRewriter
     public static class QueryRewriteException
             extends Exception
     {
-        public QueryRewriteException(String messageFormat, Object... args)
+        public QueryRewriteException(String message)
         {
-            super(format(messageFormat, args));
+            super(message);
         }
     }
 
