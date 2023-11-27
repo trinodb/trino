ca15d72408bcd91fed9123afe2132c78b2bb30e7
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-11-27 10:32:12 +0100

    Fix TestEvictableCache.testLoadFailure flakiness
    The test relied on sleep for synchronization.

--- lib/trino-cache/src/test/java/io/trino/cache/TestEmptyCache.java
+++ lib/trino-cache/src/test/java/io/trino/cache/TestEmptyCache.java
@@ -20,12 +20,13 @@ import org.junit.jupiter.api.Timeout;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
 
+import static io.trino.testing.assertions.Assert.assertEventually;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,17 +49,25 @@ public class TestEmptyCache
 
         ExecutorService executor = newFixedThreadPool(2);
         try {
-            AtomicBoolean first = new AtomicBoolean(true);
-            CyclicBarrier barrier = new CyclicBarrier(2);
+            Exchanger<Thread> exchanger = new Exchanger<>();
+            CountDownLatch secondUnblocked = new CountDownLatch(1);
 
             List<Future<String>> futures = new ArrayList<>();
             for (int i = 0; i < 2; i++) {
+                boolean first = i == 0;
                 futures.add(executor.submit(() -> {
-                    barrier.await(10, SECONDS);
+                    if (!first) {
+                        // Wait for the first one to start the call
+                        exchanger.exchange(Thread.currentThread(), 10, SECONDS);
+                        // Prove that we are back in RUNNABLE state.
+                        secondUnblocked.countDown();
+                    }
                     return cache.get(key, () -> {
-                        if (first.compareAndSet(true, false)) {
-                            // first
-                            Thread.sleep(1); // increase chances that second thread calls cache.get before we return
+                        if (first) {
+                            Thread secondThread = exchanger.exchange(null, 10, SECONDS);
+                            assertThat(secondUnblocked.await(10, SECONDS)).isTrue();
+                            // Wait for the second one to hang inside the cache.get call.
+                            assertEventually(() -> assertThat(secondThread.getState()).isNotEqualTo(Thread.State.RUNNABLE));
                             throw new RuntimeException("first attempt is poised to fail");
                         }
                         return "success";
