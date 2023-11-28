17b948c23da43dc1736c4ccea9416a55e58768c0
Raunaq Morarka <raunaqmorarka@gmail.com>
2023-11-28 10:27:11 +0530

    Implement missing overrides in HivePageSource
    (cherry picked from commit 39069fe76e56d7f11fba6463cc85e6ec7e8d2e13)

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HivePageSource.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HivePageSource.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalLong;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -150,6 +151,12 @@ public class HivePageSource
     public boolean isFinished()
     {
         return delegate.isFinished();
+    }
+
+    @Override
+    public CompletableFuture<?> isBlocked()
+    {
+        return delegate.isBlocked();
     }
 
     @Override
