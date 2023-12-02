trinodb/trino

commit 39b04491c05b5abc6545e9113508b9c57dc29d26

Close taskSource when all task partitions complete

core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -1989,6 +1989,7 @@ public class EventDrivenFaultTolerantQueryScheduler
                 finalSinkOutputSelector = sinkOutputSelectorBuilder.build();
                 sinkOutputSelectorBuilder = null;
                 stage.finish();
+                taskSource.close();
             }
         }
 

commit f89f4df8879ca083ea797032e556f503f9316696

Do not fail stage after already done
Ignore failure within stage execution if it already done.
This is to work around the problem which was introduced with
https://github.com/trinodb/trino/commit/39b04491c05.
With that change we close EventDrivenTaskSource when stage completes,
and as a result of that it may emit an failure event due to internall
processes being cancelled.
Handling of event in `StageExecution.fail()` did close a number of
objects needed for query completion (see createStageExecutionCloser()).
As a result the query could hang.

core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -2415,6 +2415,10 @@ public class EventDrivenFaultTolerantQueryScheduler
 
         public void fail(Throwable t)
         {
+            if (stage.getState().isDone()) {
+                // stage already done; ignore
+                return;
+            }
             Closer closer = createStageExecutionCloser();
             closer.register(() -> stage.fail(t));
             try {