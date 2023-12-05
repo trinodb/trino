20f82ee70d829f550c43ee948230d8cff7f8c0d5
Guowei Zhang <zgwgiser@gmail.com>
2023-12-06 01:33:16 +0800

    Fix hudi connector gets stuck #19506

--- plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/split/HudiBackgroundSplitLoader.java
+++ plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/split/HudiBackgroundSplitLoader.java
@@ -13,12 +13,13 @@
  */
 package io.trino.plugin.hudi.split;
 
+import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.trino.plugin.hive.util.AsyncQueue;
 import io.trino.plugin.hudi.HudiTableHandle;
 import io.trino.plugin.hudi.partition.HudiPartitionInfoLoader;
 import io.trino.plugin.hudi.query.HudiDirectoryLister;
-import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorSplit;
 
@@ -26,11 +27,10 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Future;
+import java.util.function.Consumer;
 
-import static io.trino.plugin.hudi.HudiErrorCode.HUDI_CANNOT_OPEN_SPLIT;
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static io.trino.plugin.hudi.HudiSessionProperties.getSplitGeneratorParallelism;
 import static java.util.Objects.requireNonNull;
 
@@ -43,6 +43,7 @@ public class HudiBackgroundSplitLoader
     private final int splitGeneratorNumThreads;
     private final HudiSplitFactory hudiSplitFactory;
     private final List<String> partitions;
+    private final Consumer<Throwable> errorListener;
 
     public HudiBackgroundSplitLoader(
             ConnectorSession session,
@@ -51,7 +52,8 @@ public class HudiBackgroundSplitLoader
             AsyncQueue<ConnectorSplit> asyncQueue,
             Executor splitGeneratorExecutor,
             HudiSplitWeightProvider hudiSplitWeightProvider,
-            List<String> partitions)
+            List<String> partitions,
+            Consumer<Throwable> errorListener)
     {
         this.hudiDirectoryLister = requireNonNull(hudiDirectoryLister, "hudiDirectoryLister is null");
         this.asyncQueue = requireNonNull(asyncQueue, "asyncQueue is null");
@@ -59,6 +61,7 @@ public class HudiBackgroundSplitLoader
         this.splitGeneratorNumThreads = getSplitGeneratorParallelism(session);
         this.hudiSplitFactory = new HudiSplitFactory(tableHandle, hudiSplitWeightProvider);
         this.partitions = requireNonNull(partitions, "partitions is null");
+        this.errorListener = requireNonNull(errorListener, "errorListener is null");
     }
 
     @Override
@@ -66,13 +69,15 @@ public class HudiBackgroundSplitLoader
     {
         Deque<String> partitionQueue = new ConcurrentLinkedDeque<>(partitions);
         List<HudiPartitionInfoLoader> splitGeneratorList = new ArrayList<>();
-        List<Future> splitGeneratorFutures = new ArrayList<>();
+        List<ListenableFuture<Void>> splitGeneratorFutures = new ArrayList<>();
 
         // Start a number of partition split generators to generate the splits in parallel
         for (int i = 0; i < splitGeneratorNumThreads; i++) {
             HudiPartitionInfoLoader generator = new HudiPartitionInfoLoader(hudiDirectoryLister, hudiSplitFactory, asyncQueue, partitionQueue);
             splitGeneratorList.add(generator);
-            splitGeneratorFutures.add(Futures.submit(generator, splitGeneratorExecutor));
+            ListenableFuture<Void> future = Futures.submit(generator, splitGeneratorExecutor);
+            hookErrorListener(future);
+            splitGeneratorFutures.add(future);
         }
 
         for (HudiPartitionInfoLoader generator : splitGeneratorList) {
@@ -81,14 +86,21 @@ public class HudiBackgroundSplitLoader
         }
 
         // Wait for all split generators to finish
-        for (Future future : splitGeneratorFutures) {
-            try {
-                future.get();
+        Futures.whenAllComplete(splitGeneratorFutures).run(asyncQueue::finish, directExecutor());
+    }
+
+    private <T> void hookErrorListener(ListenableFuture<T> future)
+    {
+        Futures.addCallback(future, new FutureCallback<T>()
+        {
+            @Override
+            public void onSuccess(T result) {}
+
+            @Override
+            public void onFailure(@SuppressWarnings("NullableProblems") Throwable t)
+            {
+                errorListener.accept(t);
             }
-            catch (InterruptedException | ExecutionException e) {
-                throw new TrinoException(HUDI_CANNOT_OPEN_SPLIT, "Error generating Hudi split", e);
-            }
-        }
-        asyncQueue.finish();
+        }, directExecutor());
     }
 }
