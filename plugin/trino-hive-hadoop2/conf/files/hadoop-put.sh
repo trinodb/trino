6e7a84706e68ad47d713824fec7564f85e57f25f
David Phillips <david@acz.org>
2023-12-06 14:05:03 +0800

    Remove legacy Hive S3 tests

--- plugin/trino-hive-hadoop2/conf/files/hadoop-put.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xeuo pipefail
-
-# Hadoop 3 without -d (don't create _COPYING_ temporary file) requires additional S3 permissions
-# Hadoop 2 doesn't have '-d' switch
-hadoop fs -put -f -d "$@" ||
-hadoop fs -put -f "$@"
