c3f4262c27b74e35885f87721159a201b89cad75
David Phillips <david@acz.org>
2023-12-07 10:53:05 +0800

    Move Rubix initialization tests to HDFS module

--- plugin/trino-hive-hadoop2/src/test/java/io/trino/plugin/hive/TestHivePlugin.java
+++ plugin/trino-hive-hadoop2/src/test/java/io/trino/plugin/hive/TestHivePlugin.java
@@ -14,66 +14,26 @@
 package io.trino.plugin.hive;
 
 import com.google.common.collect.ImmutableMap;
-import com.qubole.rubix.core.CachingFileSystem;
 import io.trino.spi.Plugin;
 import io.trino.spi.connector.Connector;
 import io.trino.spi.connector.ConnectorFactory;
 import io.trino.testing.TestingConnectorContext;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.parallel.Execution;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static com.google.common.collect.MoreCollectors.onlyElement;
 import static com.google.common.collect.MoreCollectors.toOptional;
 import static com.google.common.collect.Streams.stream;
-import static com.google.common.io.MoreFiles.deleteRecursively;
-import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
 import static io.trino.plugin.hive.HiveSessionProperties.InsertExistingPartitionsBehavior.APPEND;
 import static io.trino.plugin.hive.HiveSessionProperties.InsertExistingPartitionsBehavior.ERROR;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.Files.createTempDirectory;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
-import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
 
-@TestInstance(PER_CLASS)
-@Execution(SAME_THREAD) // see @BeforeEach
 public class TestHivePlugin
 {
-    private Path tempDirectory;
-
-    @BeforeAll
-    public void setup()
-            throws IOException
-    {
-        tempDirectory = createTempDirectory(getClass().getSimpleName());
-    }
-
-    @AfterAll
-    public void tearDown()
-            throws IOException
-    {
-        deleteRecursively(tempDirectory, ALLOW_INSECURE);
-    }
-
-    @AfterEach
-    @BeforeEach
-    public void deinitializeRubix()
-    {
-        // revert static rubix initialization done by other tests
-        CachingFileSystem.deinitialize();
-    }
-
     @Test
     public void testCreateConnector()
     {
@@ -142,44 +102,6 @@ public class TestHivePlugin
     }
 
     @Test
-    public void testS3SecurityMappingAndHiveCachingMutuallyExclusive()
-            throws IOException
-    {
-        Path mappingConfig = Files.createTempFile(null, null);
-        ConnectorFactory connectorFactory = getHiveConnectorFactory();
-
-        assertThatThrownBy(() -> connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.s3.security-mapping.config-file", mappingConfig.toString())
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("hive.cache.location", tempDirectory.toString())
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext()))
-                .hasMessageContaining("S3 security mapping is not compatible with Hive caching");
-    }
-
-    @Test
-    public void testGcsAccessTokenAndHiveCachingMutuallyExclusive()
-    {
-        ConnectorFactory connectorFactory = getHiveConnectorFactory();
-
-        assertThatThrownBy(() -> connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.gcs.use-access-token", "true")
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("hive.cache.location", tempDirectory.toString())
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext()))
-                .hasMessageContaining("Use of GCS access token is not compatible with Hive caching");
-    }
-
-    @Test
     public void testImmutablePartitionsAndInsertOverwriteMutuallyExclusive()
     {
         ConnectorFactory connectorFactory = getHiveConnectorFactory();
@@ -234,81 +156,6 @@ public class TestHivePlugin
                 .filter(propertyMetadata -> "insert_existing_partitions_behavior".equals(propertyMetadata.getName()))
                 .collect(onlyElement())
                 .getDefaultValue();
-    }
-
-    @Test
-    public void testHdfsImpersonationAndHiveCachingMutuallyExclusive()
-    {
-        ConnectorFactory connectorFactory = getHiveConnectorFactory();
-
-        assertThatThrownBy(() -> connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.hdfs.impersonation.enabled", "true")
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("hive.cache.location", tempDirectory.toString())
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext()))
-                .hasMessageContaining("HDFS impersonation is not compatible with Hive caching");
-    }
-
-    @Test
-    public void testRubixCache()
-    {
-        ConnectorFactory connectorFactory = getHiveConnectorFactory();
-
-        connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("hive.cache.location", tempDirectory.toString())
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext())
-                .shutdown();
-    }
-
-    @Test
-    public void testRubixCacheWithNonExistingCacheDirectory()
-    {
-        ConnectorFactory connectorFactory = getHiveConnectorFactory();
-
-        assertThatThrownBy(() -> connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.cache.start-server-on-coordinator", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("hive.cache.location", "/tmp/non/existing/directory")
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext()))
-                .hasMessageContaining("None of the cache parent directories exists");
-
-        assertThatThrownBy(() -> connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.cache.start-server-on-coordinator", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext()))
-                .hasMessageContaining("caching directories were not provided");
-
-        // cache directories should not be required when cache is not explicitly started on coordinator
-        connectorFactory.create(
-                "test",
-                ImmutableMap.<String, String>builder()
-                        .put("hive.cache.enabled", "true")
-                        .put("hive.metastore.uri", "thrift://foo:1234")
-                        .put("bootstrap.quiet", "true")
-                        .buildOrThrow(),
-                new TestingConnectorContext())
-                .shutdown();
     }
 
     @Test
