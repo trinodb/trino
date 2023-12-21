ed572ab8d44fd78208b6a4d2f172d2905deeaf39
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-12-21 11:31:41 +0100

    Add tracing for SplitManager.getSplits
    Sometimes, `ConnectorSplitManager.getSplits` can take long to construct
    `ConnectorSplitSource`. For example, in Delta, there is IO work being
    done before `ConnectorSplitSource` is returned. This work would better
    be delayed until `ConnectorSplitSource.getNextBatch` is invoked, but
    currently this is not the case. Let's add tracing so that time spent in
    `ConnectorSplitManager.getSplits` is attributable.
    
    (cherry picked from commit de79cdc405acd95fa04cc6c86e5776337141416a)

--- core/trino-main/src/main/java/io/trino/split/SplitManager.java
+++ core/trino-main/src/main/java/io/trino/split/SplitManager.java
@@ -38,6 +38,7 @@ import java.util.concurrent.ExecutorService;
 
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.trino.SystemSessionProperties.isAllowPushdownIntoConnectors;
+import static io.trino.tracing.ScopedSpan.scopedSpan;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 
@@ -91,13 +92,19 @@ public class SplitManager
 
         ConnectorSession connectorSession = session.toConnectorSession(catalogHandle);
 
-        ConnectorSplitSource source = splitManager.getSplits(
-                table.getTransaction(),
-                connectorSession,
-                table.getConnectorHandle(),
-                dynamicFilter,
-                preferDeterministicSplits,
-                constraint);
+        ConnectorSplitSource source;
+        try (var ignore = scopedSpan(tracer.spanBuilder("SplitManager.getSplits")
+                .setParent(Context.current().with(parentSpan))
+                .setAttribute(TrinoAttributes.TABLE, table.getConnectorHandle().toString())
+                .startSpan())) {
+            source = splitManager.getSplits(
+                    table.getTransaction(),
+                    connectorSession,
+                    table.getConnectorHandle(),
+                    dynamicFilter,
+                    preferDeterministicSplits,
+                    constraint);
+        }
 
         SplitSource splitSource = new ConnectorAwareSplitSource(catalogHandle, source);
 
@@ -120,10 +127,16 @@ public class SplitManager
         CatalogHandle catalogHandle = function.getCatalogHandle();
         ConnectorSplitManager splitManager = splitManagerProvider.getService(catalogHandle);
 
-        ConnectorSplitSource source = splitManager.getSplits(
-                function.getTransactionHandle(),
-                session.toConnectorSession(catalogHandle),
-                function.getFunctionHandle());
+        ConnectorSplitSource source;
+        try (var ignore = scopedSpan(tracer.spanBuilder("SplitManager.getSplits")
+                .setParent(Context.current().with(parentSpan))
+                .setAttribute(TrinoAttributes.FUNCTION, function.getFunctionHandle().toString())
+                .startSpan())) {
+            source = splitManager.getSplits(
+                    function.getTransactionHandle(),
+                    session.toConnectorSession(catalogHandle),
+                    function.getFunctionHandle());
+        }
 
         SplitSource splitSource = new ConnectorAwareSplitSource(catalogHandle, source);
 
