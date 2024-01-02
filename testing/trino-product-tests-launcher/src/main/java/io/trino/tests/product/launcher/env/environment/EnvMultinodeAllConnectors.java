48b4496ae97d406559fd50c5161020d667679b7c
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-01-02 15:28:47 +0100

    Enable testing of trino-opensearch module

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeAllConnectors.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeAllConnectors.java
@@ -69,6 +69,7 @@ public final class EnvMultinodeAllConnectors
                         "singlestore",
                         "mongodb",
                         "mysql",
+                        "opensearch",
                         "oracle",
                         "phoenix5",
                         "pinot",
