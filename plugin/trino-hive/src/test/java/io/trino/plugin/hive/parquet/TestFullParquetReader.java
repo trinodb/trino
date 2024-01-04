bf093a85df94c6ebabbd9cc1fe26ee56b037fd2f
Raunaq Morarka <raunaqmorarka@gmail.com>
2024-01-04 14:22:15 +0530

    Remove unnecessary forceTestNgToRespectSingleThreaded

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/parquet/TestFullParquetReader.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/parquet/TestFullParquetReader.java
@@ -13,7 +13,6 @@
  */
 package io.trino.plugin.hive.parquet;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 
 import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
@@ -28,13 +27,5 @@ public class TestFullParquetReader
     public TestFullParquetReader()
     {
         super(ParquetTester.fullParquetTester());
-    }
-
-    @Test
-    public void forceTestNgToRespectSingleThreaded()
-    {
-        // TODO: Remove after updating TestNG to 7.4.0+ (https://github.com/trinodb/trino/issues/8571)
-        // TestNG doesn't enforce @Test(singleThreaded = true) when tests are defined in base class. According to
-        // https://github.com/cbeust/testng/issues/2361#issuecomment-688393166 a workaround it to add a dummy test to the leaf test class.
     }
 }
