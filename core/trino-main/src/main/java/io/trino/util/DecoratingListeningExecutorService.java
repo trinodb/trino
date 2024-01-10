c77e6122b80e0350f394be83acfe089285a1c162
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-01-10 10:02:54 +0100

    Extract version embedding to a helper

--- core/trino-main/src/main/java/io/trino/util/DecoratingListeningExecutorService.java
+++ core/trino-main/src/main/java/io/trino/util/DecoratingListeningExecutorService.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.dispatcher;
+package io.trino.util;
 
 import com.google.common.util.concurrent.ForwardingListeningExecutorService;
 import com.google.common.util.concurrent.ListenableFuture;
