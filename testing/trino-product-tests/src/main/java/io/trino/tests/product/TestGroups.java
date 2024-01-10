03a79b4e05dfd385ec936c855d5d23d477ac1cea
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-01-10 12:20:06 +0100

    Give Hive ACID PT suite a name
    `suite-8-non-generic` runs only Hive ACID tests. Since these tests are
    very long and more flaky than others, no other tests will be added to
    this suite. Rename it.
    
    The `hdp3_only` test group verifies environment setup, and as such is
    redundant, removed.
    
    (cherry picked from commit 5bc6d2e88d66252e39ea35a3baa5ebc9dbe6fb14)

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/TestGroups.java
@@ -64,7 +64,6 @@ public final class TestGroups
     public static final String LDAP_CLI = "ldap_cli";
     public static final String LDAP_AND_FILE_CLI = "ldap_and_file_cli";
     public static final String LDAP_MULTIPLE_BINDS = "ldap_multiple_binds";
-    public static final String HDP3_ONLY = "hdp3_only";
     public static final String TLS = "tls";
     public static final String ROLES = "roles";
     public static final String CANCEL_QUERY = "cancel_query";
