d194091e577b0c11c1cd9eef2b1c51290a33856c
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-01-10 10:02:54 +0100

    Extract version embedding to a helper
    (cherry picked from commit 7493acc449d0a97ed2adf6b8adf3c86847bb8514)

--- core/trino-main/src/test/java/io/trino/util/TestDecoratingListeningExecutorService.java
+++ core/trino-main/src/test/java/io/trino/util/TestDecoratingListeningExecutorService.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.dispatcher;
+package io.trino.util;
 
 import com.google.common.util.concurrent.ListeningExecutorService;
 import org.junit.jupiter.api.Test;
