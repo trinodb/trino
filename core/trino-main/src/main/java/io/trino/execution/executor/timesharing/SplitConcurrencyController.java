0ef50c542ee9084d1c6977c3c1908d41746711f1
Martin Traverso <mtraverso@gmail.com>
2024-01-19 18:20:30 -0800

    Move SplitConcurrencyController to timesharing executor package

--- core/trino-main/src/main/java/io/trino/execution/executor/timesharing/SplitConcurrencyController.java
+++ core/trino-main/src/main/java/io/trino/execution/executor/timesharing/SplitConcurrencyController.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.execution;
+package io.trino.execution.executor.timesharing;
 
 import io.airlift.units.Duration;
 import io.trino.annotation.NotThreadSafe;
