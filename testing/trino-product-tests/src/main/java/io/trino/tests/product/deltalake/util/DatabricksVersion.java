25690eaffb514082bcc708eef3ae527a51a20205
Yuya Ebihara <ebyhry@gmail.com>
2024-01-28 18:00:26 +0900

    Run testTimestampInsertCompatibility only in OSS Delta

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/util/DatabricksVersion.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/util/DatabricksVersion.java
@@ -21,7 +21,6 @@ import java.util.regex.Pattern;
 public record DatabricksVersion(int majorVersion, int minorVersion)
         implements Comparable<DatabricksVersion>
 {
-    public static final DatabricksVersion DATABRICKS_133_RUNTIME_VERSION = new DatabricksVersion(13, 3);
     public static final DatabricksVersion DATABRICKS_122_RUNTIME_VERSION = new DatabricksVersion(12, 2);
     public static final DatabricksVersion DATABRICKS_113_RUNTIME_VERSION = new DatabricksVersion(11, 3);
     public static final DatabricksVersion DATABRICKS_104_RUNTIME_VERSION = new DatabricksVersion(10, 4);
