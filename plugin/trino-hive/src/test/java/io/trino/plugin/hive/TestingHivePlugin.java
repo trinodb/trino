6fb598e90bfe6731c06f9ee99af47c68f51b2f60
David Phillips <david@acz.org>
2024-02-05 17:06:09 -0700

    Remove DirectoryLister testing override

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestingHivePlugin.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestingHivePlugin.java
@@ -15,7 +15,6 @@ package io.trino.plugin.hive;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Module;
-import io.trino.plugin.hive.fs.DirectoryLister;
 import io.trino.plugin.hive.metastore.HiveMetastore;
 import io.trino.spi.Plugin;
 import io.trino.spi.connector.ConnectorFactory;
@@ -32,31 +31,29 @@ public class TestingHivePlugin
     private final Path localFileSystemRootPath;
     private final Optional<HiveMetastore> metastore;
     private final Module module;
-    private final Optional<DirectoryLister> directoryLister;
 
     public TestingHivePlugin(Path localFileSystemRootPath)
     {
-        this(localFileSystemRootPath, Optional.empty(), EMPTY_MODULE, Optional.empty());
+        this(localFileSystemRootPath, Optional.empty(), EMPTY_MODULE);
     }
 
     @Deprecated
     public TestingHivePlugin(Path localFileSystemRootPath, HiveMetastore metastore)
     {
-        this(localFileSystemRootPath, Optional.of(metastore), EMPTY_MODULE, Optional.empty());
+        this(localFileSystemRootPath, Optional.of(metastore), EMPTY_MODULE);
     }
 
     @Deprecated
-    public TestingHivePlugin(Path localFileSystemRootPath, Optional<HiveMetastore> metastore, Module module, Optional<DirectoryLister> directoryLister)
+    public TestingHivePlugin(Path localFileSystemRootPath, Optional<HiveMetastore> metastore, Module module)
     {
         this.localFileSystemRootPath = requireNonNull(localFileSystemRootPath, "localFileSystemRootPath is null");
         this.metastore = requireNonNull(metastore, "metastore is null");
         this.module = requireNonNull(module, "module is null");
-        this.directoryLister = requireNonNull(directoryLister, "directoryLister is null");
     }
 
     @Override
     public Iterable<ConnectorFactory> getConnectorFactories()
     {
-        return ImmutableList.of(new TestingHiveConnectorFactory(localFileSystemRootPath, metastore, module, directoryLister));
+        return ImmutableList.of(new TestingHiveConnectorFactory(localFileSystemRootPath, metastore, module));
     }
 }
