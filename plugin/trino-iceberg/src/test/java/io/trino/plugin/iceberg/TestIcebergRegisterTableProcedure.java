e0533b8bbc3c50256a2163342c631e62ce2234cc
David Phillips <david@acz.org>
2024-02-08 13:51:46 -0600

    Remove unused file system factory from Iceberg tests

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergRegisterTableProcedure.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergRegisterTableProcedure.java
@@ -92,10 +92,7 @@ public class TestIcebergRegisterTableProcedure
         queryRunner.createCatalog("tpch", "tpch");
 
         Path dataDir = queryRunner.getCoordinator().getBaseDataDir().resolve("iceberg_data");
-        queryRunner.installPlugin(new TestingIcebergPlugin(
-                dataDir,
-                Optional.of(new TestingIcebergFileMetastoreCatalogModule(metastore)),
-                Optional.empty()));
+        queryRunner.installPlugin(new TestingIcebergPlugin(dataDir, Optional.of(new TestingIcebergFileMetastoreCatalogModule(metastore))));
         queryRunner.createCatalog(ICEBERG_CATALOG, "iceberg", ImmutableMap.of("iceberg.register-table-procedure.enabled", "true"));
         queryRunner.execute("CREATE SCHEMA iceberg.tpch");
         return queryRunner;
