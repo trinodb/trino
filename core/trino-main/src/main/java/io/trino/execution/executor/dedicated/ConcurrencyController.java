9fad5f8b9f153a2a5cb8142240e5d1388165edc5
Martin Traverso <mtraverso@gmail.com>
2024-02-12 12:07:25 -0800

    Limit the max number of drivers per task in thread-per-driver scheduler

--- core/trino-main/src/main/java/io/trino/execution/executor/dedicated/ConcurrencyController.java
+++ core/trino-main/src/main/java/io/trino/execution/executor/dedicated/ConcurrencyController.java
@@ -19,12 +19,15 @@ class ConcurrencyController
 {
     private static final double TARGET_UTILIZATION = 0.5;
 
+    private final int maxConcurrency;
     private int targetConcurrency;
 
-    public ConcurrencyController(int initialConcurrency)
+    public ConcurrencyController(int initialConcurrency, int maxConcurrency)
     {
         checkArgument(initialConcurrency > 0, "initial concurrency must be positive");
+        checkArgument(initialConcurrency <= maxConcurrency, "initial concurrency must be <= maxConcurrency>");
         this.targetConcurrency = initialConcurrency;
+        this.maxConcurrency = maxConcurrency;
     }
 
     public void update(double utilization, int currentConcurrency)
@@ -35,6 +38,8 @@ class ConcurrencyController
         else if (utilization < TARGET_UTILIZATION && currentConcurrency >= targetConcurrency) {
             targetConcurrency++;
         }
+
+        targetConcurrency = Math.min(maxConcurrency, targetConcurrency);
     }
 
     public int targetConcurrency()
