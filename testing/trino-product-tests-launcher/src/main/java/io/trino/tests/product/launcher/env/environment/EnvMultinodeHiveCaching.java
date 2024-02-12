c736b20a4f0cd5cb2bc375ee367d9060385eae58
Raunaq Morarka <raunaqmorarka@gmail.com>
2024-02-12 12:22:34 +0530

    Support embedded alluxio cache in hive

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeHiveCaching.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeHiveCaching.java
@@ -17,25 +17,14 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import io.trino.tests.product.launcher.docker.DockerFiles;
-import io.trino.tests.product.launcher.env.Debug;
 import io.trino.tests.product.launcher.env.Environment;
-import io.trino.tests.product.launcher.env.EnvironmentConfig;
 import io.trino.tests.product.launcher.env.EnvironmentProvider;
-import io.trino.tests.product.launcher.env.ServerPackage;
 import io.trino.tests.product.launcher.env.common.Hadoop;
-import io.trino.tests.product.launcher.env.common.Standard;
+import io.trino.tests.product.launcher.env.common.StandardMultinode;
 import io.trino.tests.product.launcher.env.common.TestsEnvironment;
-import io.trino.tests.product.launcher.env.jdk.JdkProvider;
 
-import java.io.File;
-
-import static io.trino.tests.product.launcher.env.EnvironmentContainers.COORDINATOR;
-import static io.trino.tests.product.launcher.env.EnvironmentContainers.worker;
 import static io.trino.tests.product.launcher.env.common.Hadoop.CONTAINER_TRINO_HIVE_PROPERTIES;
-import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_CONFIG_PROPERTIES;
 import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_ETC;
-import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_JVM_CONFIG;
-import static io.trino.tests.product.launcher.env.common.Standard.createTrinoContainer;
 import static java.util.Objects.requireNonNull;
 import static org.testcontainers.utility.MountableFile.forHostPath;
 
@@ -48,52 +37,22 @@ public final class EnvMultinodeHiveCaching
     private final DockerFiles dockerFiles;
     private final DockerFiles.ResourceProvider configDir;
 
-    private final String imagesVersion;
-    private final JdkProvider jdkProvider;
-    private final File serverPackage;
-    private final boolean debug;
-
     @Inject
     public EnvMultinodeHiveCaching(
             DockerFiles dockerFiles,
-            Standard standard,
-            Hadoop hadoop,
-            EnvironmentConfig environmentConfig,
-            @ServerPackage File serverPackage,
-            JdkProvider jdkProvider,
-            @Debug boolean debug)
+            StandardMultinode standardMultinode,
+            Hadoop hadoop)
     {
-        super(ImmutableList.of(standard, hadoop));
+        super(ImmutableList.of(standardMultinode, hadoop));
         this.dockerFiles = requireNonNull(dockerFiles, "dockerFiles is null");
         this.configDir = dockerFiles.getDockerFilesHostDirectory("conf/environment");
-        this.imagesVersion = environmentConfig.getImagesVersion();
-        this.jdkProvider = requireNonNull(jdkProvider, "jdkProvider is null");
-        this.serverPackage = requireNonNull(serverPackage, "serverPackage is null");
-        this.debug = debug;
     }
 
     @Override
     public void extendEnvironment(Environment.Builder builder)
     {
-        builder.configureContainer(COORDINATOR, container -> container
-                .withCopyFileToContainer(forHostPath(configDir.getPath("multinode/multinode-master-jvm.config")), CONTAINER_TRINO_JVM_CONFIG)
-                .withCopyFileToContainer(forHostPath(dockerFiles.getDockerFilesHostPath("common/standard-multinode/multinode-master-config.properties")), CONTAINER_TRINO_CONFIG_PROPERTIES)
-                .withTmpFs(ImmutableMap.of("/tmp/cache", "rw")));
+        builder.configureContainers(container -> container.withTmpFs(ImmutableMap.of("/tmp/cache", "rw")));
         builder.addConnector("hive", forHostPath(dockerFiles.getDockerFilesHostPath("common/hadoop/hive.properties")), CONTAINER_TRINO_HIVE_NON_CACHED_PROPERTIES);
-        builder.addConnector("hive", forHostPath(configDir.getPath("multinode-cached/hive-coordinator.properties")), CONTAINER_TRINO_HIVE_PROPERTIES);
-
-        createTrinoWorker(builder, 0);
-        createTrinoWorker(builder, 1);
-    }
-
-    @SuppressWarnings("resource")
-    private void createTrinoWorker(Environment.Builder builder, int workerNumber)
-    {
-        builder.addContainer(createTrinoContainer(dockerFiles, serverPackage, jdkProvider, debug, "ghcr.io/trinodb/testing/centos7-oj17:" + imagesVersion, worker(workerNumber))
-                .withCopyFileToContainer(forHostPath(configDir.getPath("multinode/multinode-worker-jvm.config")), CONTAINER_TRINO_JVM_CONFIG)
-                .withCopyFileToContainer(forHostPath(dockerFiles.getDockerFilesHostPath("common/standard-multinode/multinode-worker-config.properties")), CONTAINER_TRINO_CONFIG_PROPERTIES)
-                .withCopyFileToContainer(forHostPath(dockerFiles.getDockerFilesHostPath("common/hadoop/hive.properties")), CONTAINER_TRINO_HIVE_NON_CACHED_PROPERTIES)
-                .withCopyFileToContainer(forHostPath(configDir.getPath("multinode-cached/hive-worker.properties")), CONTAINER_TRINO_HIVE_PROPERTIES)
-                .withTmpFs(ImmutableMap.of("/tmp/cache", "rw")));
+        builder.addConnector("hive", forHostPath(configDir.getPath("multinode-hive-cached/hive.properties")), CONTAINER_TRINO_HIVE_PROPERTIES);
     }
 }
