402384f9fb16e0075d3fceff9f94832e425bdc5d
radek <radoslaw.kondziolka@starburstdata.com>
2024-02-13 16:02:04 +0100

    Create dedicated class ResultQueryInfo for query protocol
    When a client (e.g. cli) fetches query result then Query#getNextResult
    is called. This call constructs QueryInfo every time that is a heavy
    operation. This commit introduces lighter query info representation
    ResultQueryInfo and uses it in query protocol.

--- core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -46,6 +46,7 @@ import io.trino.cost.RuntimeInfoProvider;
 import io.trino.cost.StaticRuntimeInfoProvider;
 import io.trino.exchange.ExchangeContextInstance;
 import io.trino.exchange.SpoolingExchangeInput;
+import io.trino.execution.BasicStageInfo;
 import io.trino.execution.BasicStageStats;
 import io.trino.execution.ExecutionFailureInfo;
 import io.trino.execution.NodeTaskMap;
@@ -393,6 +394,12 @@ public class EventDrivenFaultTolerantQueryScheduler
     public BasicStageStats getBasicStageStats()
     {
         return stageRegistry.getBasicStageStats();
+    }
+
+    @Override
+    public BasicStageInfo getBasicStageInfo()
+    {
+        return new BasicStageInfo(stageRegistry.getStageInfo());
     }
 
     @Override
