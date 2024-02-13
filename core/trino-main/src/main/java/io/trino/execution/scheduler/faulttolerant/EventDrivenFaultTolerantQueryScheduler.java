ed60fb039a96c6daa1b563c05596ba49747e8db9
Dejan Mircevski <dejan.mircevski@starburstdata.com>
2024-02-13 11:15:41 -0500

    Remove dead code in isReadyForExecution()
    Refs #249.

--- core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -1317,11 +1317,6 @@ public class EventDrivenFaultTolerantQueryScheduler
                 return IsReadyForExecutionResult.notReady();
             }
 
-            StageExecution stageExecution = stageExecutions.get(getStageId(subPlan.getFragment().getId()));
-            if (stageExecution != null) {
-                stageExecution.setSpeculative(speculative);
-            }
-
             if (speculative) {
                 log.debug("scheduling speculative %s/%s; sources: finished=%s; kinds=%s",
                         queryStateMachine.getQueryId(),
