8c401f2f4e1c599abe815bf69e1a03e0767c135f
praveenkrishna.d <praveenkrishna@tutanota.com>
2024-02-15 16:05:29 +0530

    Add varchar to float coercion for hive tables

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveCoercionOnPartitionedTable.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveCoercionOnPartitionedTable.java
@@ -153,6 +153,10 @@ public class TestHiveCoercionOnPartitionedTable
                         "    varchar_to_smaller_varchar         VARCHAR(3)," +
                         "    varchar_to_date                    VARCHAR(10)," +
                         "    varchar_to_distant_date            VARCHAR(12)," +
+                        "    varchar_to_float                   VARCHAR(40)," +
+                        "    string_to_float                    STRING," +
+                        "    varchar_to_float_infinity          VARCHAR(40)," +
+                        "    varchar_to_special_float           VARCHAR(40)," +
                         "    varchar_to_double                  VARCHAR(40)," +
                         "    string_to_double                   STRING," +
                         "    varchar_to_double_infinity         VARCHAR(40)," +
