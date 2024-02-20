f5387eb2f5c6ea65afcf92cc091e4fe982edbb08
Anu Sudarsan <anu.at.infy@gmail.com>
2024-02-20 11:38:02 -0500

    [FROM GALAXY-TRINO] Add Snowflake product tests

---
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeSnowflake.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.trino.tests.product.launcher.env.environment;
+
+import com.google.inject.Inject;
+import io.trino.tests.product.launcher.docker.DockerFiles;
+import io.trino.tests.product.launcher.env.DockerContainer;
+import io.trino.tests.product.launcher.env.Environment;
+import io.trino.tests.product.launcher.env.EnvironmentProvider;
+import io.trino.tests.product.launcher.env.common.StandardMultinode;
+import io.trino.tests.product.launcher.env.common.TestsEnvironment;
+
+import static io.trino.tests.product.launcher.env.EnvironmentContainers.COORDINATOR;
+import static io.trino.tests.product.launcher.env.EnvironmentContainers.WORKER;
+import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_ETC;
+import static io.trino.tests.product.launcher.env.common.Standard.CONTAINER_TRINO_JVM_CONFIG;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+import static org.testcontainers.utility.MountableFile.forHostPath;
+
+@TestsEnvironment
+public class EnvMultinodeSnowflake
+        extends EnvironmentProvider
+{
+    private final DockerFiles.ResourceProvider configDir;
+
+    @Inject
+    public EnvMultinodeSnowflake(StandardMultinode standard, DockerFiles dockerFiles)
+    {
+        super(standard);
+        this.configDir = requireNonNull(dockerFiles, "dockerFiles is null")
+                .getDockerFilesHostDirectory("conf/environment/singlenode-snowflake");
+    }
+
+    @Override
+    public void extendEnvironment(Environment.Builder builder)
+    {
+        builder.addConnector("snowflake")
+                .configureContainer(COORDINATOR, this::configureTrinoContainer)
+                .configureContainer(WORKER, this::configureTrinoContainer);
+    }
+
+    private DockerContainer configureTrinoContainer(DockerContainer container)
+    {
+        return container
+                .withCopyFileToContainer(
+                        forHostPath(configDir.getPath("snowflake_jdbc.properties")),
+                        CONTAINER_TRINO_ETC + "/catalog/snowflake_jdbc.properties")
+                .withCopyFileToContainer(
+                        forHostPath(configDir.getPath("jvm.config")),
+                        CONTAINER_TRINO_JVM_CONFIG)
+                .withEnv("SNOWFLAKE_JDBC_URL", getSnowflakeEndpoint())
+                .withEnv("SNOWFLAKE_PASSWORD", getSnowflakePassword());
+    }
+
+    private String getSnowflakeEndpoint()
+    {
+        String accountName = requireNonNull(System.getenv("SNOWFLAKE_ACCOUNT_NAME"), "Expected SNOWFLAKE_ACCOUNT_NAME environment variable to be set");
+        return format("jdbc:snowflake://%s.snowflakecomputing.com/", accountName);
+    }
+
+    private String getSnowflakePassword()
+    {
+        return requireNonNull(System.getenv("SNOWFLAKE_PASSWORD"), "Expected SNOWFLAKE_PASSWORD environment variable to be set");
+    }
+}
---
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/TestSnowflake.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.trino.tests.product;
+
+import io.trino.tempto.ProductTest;
+import org.testng.annotations.Test;
+
+import static io.trino.tempto.assertions.QueryAssert.Row.row;
+import static io.trino.tempto.assertions.QueryAssert.assertThat;
+import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
+import static io.trino.tests.product.TestGroups.SNOWFLAKE;
+import static io.trino.tests.product.utils.QueryExecutors.onTrino;
+import static java.lang.String.format;
+import static java.util.UUID.randomUUID;
+
+/**
+ * Adapted from SEP's {@code TestSnowflake}, but doesn't test the distributed
+ * connector.
+ */
+public class TestSnowflake
+        extends ProductTest
+{
+    @Test(groups = {SNOWFLAKE, PROFILE_SPECIFIC_TESTS})
+    public void testCreateTableAsSelect()
+    {
+        String catalog = "snowflake_jdbc";
+        final String suffix = randomUUID().toString().replace("-", "_");
+
+        onTrino().executeQuery(format("DROP TABLE IF EXISTS %s.test_schema.nation_%s", catalog, suffix));
+
+        assertThat(onTrino().executeQuery(
+                format("""
+                        CREATE TABLE %s.test_schema.nation_%s
+                        AS SELECT * FROM tpch.tiny.nation
+                        """, catalog, suffix)))
+                .containsOnly(row(25));
+
+        try {
+            assertThat(onTrino().executeQuery(
+                    format("SELECT * FROM %s.test_schema.nation_%s", catalog, suffix)))
+                    .hasRowsCount(25);
+        }
+        finally {
+            onTrino().executeQuery(format("DROP TABLE %s.test_schema.nation_%s", catalog, suffix));
+        }
+    }
+}
