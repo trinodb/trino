55fa8cac2a0983da8511064e4442a100f079efed
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-02-20 16:35:02 +0100

    Utilize Metadata.isView in more places

--- core/trino-main/src/main/java/io/trino/metadata/MetadataManager.java
+++ core/trino-main/src/main/java/io/trino/metadata/MetadataManager.java
@@ -1340,9 +1340,9 @@ public final class MetadataManager
 
         Optional<QualifiedObjectName> objectName = prefix.asQualifiedObjectName();
         if (objectName.isPresent()) {
-            return getView(session, objectName.get())
-                    .map(handle -> ImmutableList.of(objectName.get()))
-                    .orElseGet(ImmutableList::of);
+            return isView(session, objectName.get())
+                    ? ImmutableList.of(objectName.get())
+                    : ImmutableList.of();
         }
 
         Optional<CatalogMetadata> catalog = getOptionalCatalogMetadata(session, prefix.getCatalogName());
