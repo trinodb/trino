fba3819cb3d03e0d7f96922e24fbe1ab8e2068ee
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-02-21 13:15:49 +0100

    Support multipart UI token cookies
    When cookie name+value length exceeds 4096 bytes, it is silently rejected by most of the browsers
    per https://datatracker.ietf.org/doc/html/rfc6265#section-6.1.
    
    Since we don't control access & refresh token lengths and encryption scheme, we need to split
    value and set/remove multiple cookies in such cases.

--- core/trino-main/src/main/java/io/trino/server/ui/OAuthWebUiCookie.java
+++ core/trino-main/src/main/java/io/trino/server/ui/OAuthWebUiCookie.java
@@ -17,49 +17,31 @@ import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.core.NewCookie;
 
 import java.time.Instant;
-import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 
 import static io.trino.server.ui.FormWebUiAuthenticationFilter.UI_LOCATION;
-import static java.util.function.Predicate.not;
 
 public final class OAuthWebUiCookie
 {
     // prefix according to: https://tools.ietf.org/html/draft-ietf-httpbis-rfc6265bis-05#section-4.1.3.1
     public static final String OAUTH2_COOKIE = "__Secure-Trino-OAuth2-Token";
+    private static final MultipartUiCookie MULTIPART_COOKIE = new MultipartUiCookie(OAUTH2_COOKIE, UI_LOCATION);
 
     private OAuthWebUiCookie() {}
 
     public static NewCookie[] create(String token, Instant tokenExpiration)
     {
-        return new NewCookie[] {new NewCookie.Builder(OAUTH2_COOKIE)
-                .path(UI_LOCATION)
-                .domain(null)
-                .value(token)
-                .expiry(Date.from(tokenExpiration))
-                .secure(true)
-                .httpOnly(true)
-                .build()};
+        return MULTIPART_COOKIE.create(token, tokenExpiration, true);
     }
 
     public static Optional<String> read(Map<String, Cookie> availableCookies)
     {
-        return Optional.ofNullable(availableCookies.get(OAUTH2_COOKIE))
-                .map(Cookie::getValue)
-                .filter(not(String::isBlank));
+        return MULTIPART_COOKIE.read(availableCookies);
     }
 
     public static NewCookie[] delete(Map<String, Cookie> availableCookies)
     {
-        return new NewCookie[] {new NewCookie.Builder(OAUTH2_COOKIE)
-                .value("delete")
-                .path(UI_LOCATION)
-                .domain(null)
-                .maxAge(0)
-                .expiry(null)
-                .secure(true)
-                .httpOnly(true)
-                .build()};
+        return MULTIPART_COOKIE.delete(availableCookies, true);
     }
 }
