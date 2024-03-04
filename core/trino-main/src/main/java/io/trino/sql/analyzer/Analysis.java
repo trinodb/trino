29b60f46c6173bfb8a3e5dce60fe0de1f8fc8c8a
Matt Deady <matt.deady@starburstdata.com>
2024-03-04 14:24:21 -0500

    Fix temporal predicates in MVs
    (cherry picked from commit d9ead29a36d0b22dbf432a9d8d4b79e91465dc49)

--- core/trino-main/src/main/java/io/trino/sql/analyzer/Analysis.java
+++ core/trino-main/src/main/java/io/trino/sql/analyzer/Analysis.java
@@ -1344,16 +1344,6 @@ public class Analysis
                 .orElse(FALSE);
     }
 
-    public List<Field> getMaterializedViewStorageTableFields(Table node)
-    {
-        return materializedViewStorageTableFields.get(NodeRef.of(node));
-    }
-
-    public void setMaterializedViewStorageTableFields(Table node, List<Field> fields)
-    {
-        materializedViewStorageTableFields.put(NodeRef.of(node), fields);
-    }
-
     public List<PatternInputAnalysis> getPatternInputsAnalysis(Expression expression)
     {
         return patternInputsAnalysis.get(NodeRef.of(expression));
@@ -1392,6 +1382,16 @@ public class Analysis
     public void addResolvedLabels(Map<NodeRef<Identifier>, String> labels)
     {
         resolvedLabels.putAll(labels);
+    }
+
+    public List<Field> getMaterializedViewStorageTableFields(Table node)
+    {
+        return materializedViewStorageTableFields.get(NodeRef.of(node));
+    }
+
+    public void setMaterializedViewStorageTableFields(Table node, List<Field> fields)
+    {
+        materializedViewStorageTableFields.put(NodeRef.of(node), fields);
     }
 
     @Immutable
