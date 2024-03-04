c1975135c934d16e19c042c66bf90b3140b6ecf4
David Phillips <david@acz.org>
2024-03-03 18:45:37 -0800

    Implement createExclusive for Azure

--- lib/trino-filesystem-azure/src/test/java/io/trino/filesystem/azure/AbstractTestAzureFileSystem.java
+++ lib/trino-filesystem-azure/src/test/java/io/trino/filesystem/azure/AbstractTestAzureFileSystem.java
@@ -174,6 +174,12 @@ public abstract class AbstractTestAzureFileSystem
         assertThat(blobContainerClient.listBlobs()).isEmpty();
     }
 
+    @Override
+    protected boolean supportsCreateExclusive()
+    {
+        return true;
+    }
+
     @Test
     @Override
     public void testPaths()
