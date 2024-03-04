cde19422b4d04233182029df67b6a0b24e4dba7d
Li Xiangqun <yingguangtianhuo@gmail.com>
2024-03-04 18:33:08 +0800

    Fix 'Multiple entries with same key' error with dynamic filter
    (cherry picked from commit 80529693789245054162de4a0ae765b571aa5598)

--- core/trino-main/src/main/java/io/trino/server/DynamicFilterService.java
+++ core/trino-main/src/main/java/io/trino/server/DynamicFilterService.java
@@ -511,7 +511,8 @@ public class DynamicFilterService
                                 return applySaturatedCasts(metadata, functionManager, typeOperators, session, updatedSummary, targetType);
                             }
                             return updatedSummary;
-                        })));
+                        },
+                        Domain::intersect)));
     }
 
     private static Set<DynamicFilterId> getLazyDynamicFilters(PlanFragment plan)
