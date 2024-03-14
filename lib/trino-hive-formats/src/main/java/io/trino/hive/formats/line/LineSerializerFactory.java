9396726ae9d36b82784df75c539584f68397bbdf
David Phillips <david@acz.org>
2024-03-14 13:08:56 -0700

    Use set instead of list for serde names
    (cherry picked from commit 695c73b402eee6417862483d0cfba2fc66df3cf3)

--- lib/trino-hive-formats/src/main/java/io/trino/hive/formats/line/LineSerializerFactory.java
+++ lib/trino-hive-formats/src/main/java/io/trino/hive/formats/line/LineSerializerFactory.java
@@ -15,10 +15,11 @@ package io.trino.hive.formats.line;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public interface LineSerializerFactory
 {
-    List<String> getHiveSerDeClassNames();
+    Set<String> getHiveSerDeClassNames();
 
     LineSerializer create(List<Column> columns, Map<String, String> serdeProperties);
 }
