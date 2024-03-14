ff0dc982525bfd28347c5868f597696f52244a1d
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-03-14 16:49:29 +0100

    Fix long decimal partial aggregation below join
    Co-authored-by: Dain Sundstrom <dain@iq80.com>
    Co-authored-by: Lukasz Stec <lukasz.stec@starburstdata.com>
    (cherry picked from commit 22765b3b9be702e9305bf8d2f671db8fe4db291d)

--- core/trino-main/src/test/java/io/trino/operator/aggregation/state/TestLongDecimalWithOverflowStateSerializer.java
+++ core/trino-main/src/test/java/io/trino/operator/aggregation/state/TestLongDecimalWithOverflowStateSerializer.java
@@ -13,9 +13,14 @@
  */
 package io.trino.operator.aggregation.state;
 
+import io.trino.spi.block.Block;
+import io.trino.spi.block.DictionaryBlock;
+import io.trino.spi.block.RunLengthEncodedBlock;
 import io.trino.spi.block.VariableWidthBlock;
 import io.trino.spi.block.VariableWidthBlockBuilder;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,13 +43,18 @@ public class TestLongDecimalWithOverflowStateSerializer
 
     private void testSerde(long low, long high, long overflow, int expectedLength)
     {
+        testSerde(low, high, overflow, expectedLength, Function.identity());
+    }
+
+    private void testSerde(long low, long high, long overflow, int expectedLength, Function<Block, Block> serializedModification)
+    {
         LongDecimalWithOverflowState state = STATE_FACTORY.createSingleState();
         state.getDecimalArray()[0] = high;
         state.getDecimalArray()[1] = low;
         state.setOverflow(overflow);
         state.setNotNull();
 
-        LongDecimalWithOverflowState outState = roundTrip(state, expectedLength);
+        LongDecimalWithOverflowState outState = roundTrip(state, expectedLength, serializedModification);
 
         assertThat(outState.isNotNull()).isTrue();
         assertThat(outState.getDecimalArray()[0]).isEqualTo(high);
@@ -63,7 +73,24 @@ public class TestLongDecimalWithOverflowStateSerializer
         assertThat(outState.isNotNull()).isFalse();
     }
 
+    @Test
+    public void testDictionaryDeserialization()
+    {
+        testSerde(3, 0, 0, 1, block -> DictionaryBlock.create(2, block, new int[] {0, 0}));
+    }
+
+    @Test
+    public void testRleDeserialization()
+    {
+        testSerde(3, 0, 0, 1, block -> RunLengthEncodedBlock.create(block, 2));
+    }
+
     private LongDecimalWithOverflowState roundTrip(LongDecimalWithOverflowState state, int expectedLength)
+    {
+        return roundTrip(state, expectedLength, Function.identity());
+    }
+
+    private LongDecimalWithOverflowState roundTrip(LongDecimalWithOverflowState state, int expectedLength, Function<Block, Block> serializedModification)
     {
         LongDecimalWithOverflowStateSerializer serializer = new LongDecimalWithOverflowStateSerializer();
         VariableWidthBlockBuilder out = new VariableWidthBlockBuilder(null, 1, 0);
@@ -73,7 +100,7 @@ public class TestLongDecimalWithOverflowStateSerializer
         VariableWidthBlock serialized = out.buildValueBlock();
         assertThat(serialized.getSliceLength(0)).isEqualTo(expectedLength * Long.BYTES);
         LongDecimalWithOverflowState outState = STATE_FACTORY.createSingleState();
-        serializer.deserialize(serialized, 0, outState);
+        serializer.deserialize(serializedModification.apply(serialized), 0, outState);
         return outState;
     }
 }
