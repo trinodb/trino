a3491a2e81831fa3f51a0db69cf41a57e5de4875
Martin Traverso <mtraverso@gmail.com>
2024-03-15 10:53:05 -0700

    Remove IntervalLiteral from IR

--- core/trino-main/src/main/java/io/trino/sql/ir/Literal.java
+++ core/trino-main/src/main/java/io/trino/sql/ir/Literal.java
@@ -15,7 +15,7 @@ package io.trino.sql.ir;
 
 public abstract sealed class Literal
         extends Expression
-        permits GenericLiteral, IntervalLiteral, NullLiteral
+        permits GenericLiteral, NullLiteral
 {
     @Override
     public <R, C> R accept(IrVisitor<R, C> visitor, C context)
