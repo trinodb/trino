41b33136973d31b0431978cde6da8711648f288f
Dain Sundstrom <dain@iq80.com>
2024-03-16 16:27:41 -0700

    Fix getTables for Unity Metastore
    Unity does not implement getTableMeta yet, so work around this by
    fetching the tables and views separately. This workaround can not
    distinguish between Hive and Trino views, and is slightly less efficient
    than getTablesMeta.  Once getTableMeta is supported, this workaround
    should be removed.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/HttpThriftMetastoreClientFactory.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/HttpThriftMetastoreClientFactory.java
@@ -56,6 +56,7 @@ public class HttpThriftMetastoreClientFactory
     private final Map<String, String> additionalHeaders;
     private final OpenTelemetry openTelemetry;
 
+    private final AtomicInteger chosenGetTableMetaAlternative = new AtomicInteger(Integer.MAX_VALUE);
     private final AtomicInteger chosenGetTableAlternative = new AtomicInteger(Integer.MAX_VALUE);
     private final AtomicInteger chosenAlterTransactionalTableAlternative = new AtomicInteger(Integer.MAX_VALUE);
     private final AtomicInteger chosenAlterPartitionsAlternative = new AtomicInteger(Integer.MAX_VALUE);
@@ -82,6 +83,7 @@ public class HttpThriftMetastoreClientFactory
                 () -> createHttpTransport(uri),
                 hostname,
                 new MetastoreSupportsDateStatistics(),
+                chosenGetTableMetaAlternative,
                 chosenGetTableAlternative,
                 chosenAlterTransactionalTableAlternative,
                 chosenAlterPartitionsAlternative);
