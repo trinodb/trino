0dc2c079b50a352512672745c29b4b8bd0f998f0
Dain Sundstrom <dain@iq80.com>
2024-03-16 13:34:00 -0700

    Remove unnecessary calls to listMaterializedViews
    HiveMetastore does not implement listMaterializedViews, so there is not
    need to call it.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
@@ -800,8 +800,6 @@ public class HiveMetadata
                 tableNames.add(tableInfo.tableName());
             }
         }
-
-        tableNames.addAll(listMaterializedViews(session, optionalSchemaName));
         return tableNames.build().asList();
     }
 
@@ -809,15 +807,11 @@ public class HiveMetadata
     public Map<SchemaTableName, RelationType> getRelationTypes(ConnectorSession session, Optional<String> optionalSchemaName)
     {
         ImmutableMap.Builder<SchemaTableName, RelationType> result = ImmutableMap.builder();
-
         for (String schemaName : listSchemas(session, optionalSchemaName)) {
             for (TableInfo tableInfo : metastore.getTables(schemaName)) {
                 result.put(tableInfo.tableName(), tableInfo.extendedRelationType().toRelationType());
             }
         }
-
-        listMaterializedViews(session, optionalSchemaName)
-                .forEach(name -> result.put(name, RelationType.MATERIALIZED_VIEW));
         return result.buildKeepingLast();
     }
 
