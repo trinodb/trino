7c3c01cfcc0b5fa84766792a8be519245c71fa31
Yuya Ebihara <ebyhry@gmail.com>
2024-03-21 13:22:44 +0900

    Throw TrinoException when varchar size not present in Redshift

--- plugin/trino-redshift/src/main/java/io/trino/plugin/redshift/RedshiftClient.java
+++ plugin/trino-redshift/src/main/java/io/trino/plugin/redshift/RedshiftClient.java
@@ -143,6 +143,7 @@ import static io.trino.plugin.jdbc.StandardColumnMappings.varcharColumnMapping;
 import static io.trino.plugin.jdbc.StandardColumnMappings.varcharWriteFunction;
 import static io.trino.plugin.jdbc.TypeHandlingJdbcSessionProperties.getUnsupportedTypeHandling;
 import static io.trino.plugin.jdbc.UnsupportedTypeHandling.CONVERT_TO_VARCHAR;
+import static io.trino.plugin.redshift.RedshiftErrorCode.REDSHIFT_INVALID_TYPE;
 import static io.trino.spi.StandardErrorCode.INVALID_ARGUMENTS;
 import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
 import static io.trino.spi.type.BigintType.BIGINT;
@@ -635,6 +636,9 @@ public class RedshiftClient
                         RedshiftClient::writeChar));
 
             case Types.VARCHAR: {
+                if (type.getColumnSize().isEmpty()) {
+                    throw new TrinoException(REDSHIFT_INVALID_TYPE, "column size not present");
+                }
                 int length = type.getRequiredColumnSize();
                 return Optional.of(varcharColumnMapping(
                         length < VarcharType.MAX_LENGTH
