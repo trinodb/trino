1a2a40e08fe3a7bd460fc9d729d94d1f5bd3c636
David Phillips <david@acz.org>
2024-03-21 18:57:57 -0700

    Convert TransactionEntry to record
    (cherry picked from commit 7bc1cabd719b195797fede3c1305a02678b7c272)

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/transactionlog/TransactionEntry.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/transactionlog/TransactionEntry.java
@@ -13,72 +13,12 @@
  */
 package io.trino.plugin.deltalake.transactionlog;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import static java.util.Objects.requireNonNull;
 
-import java.util.Objects;
-
-import static java.lang.String.format;
-
-public class TransactionEntry
+public record TransactionEntry(String appId, long version, long lastUpdated)
 {
-    private final String appId;
-    private final long version;
-    private final long lastUpdated;
-
-    @JsonCreator
-    public TransactionEntry(
-            @JsonProperty("appId") String appId,
-            @JsonProperty("version") long version,
-            @JsonProperty("lastUpdated") long lastUpdated)
+    public TransactionEntry
     {
-        this.appId = appId;
-        this.version = version;
-        this.lastUpdated = lastUpdated;
-    }
-
-    @JsonProperty
-    public String getAppId()
-    {
-        return appId;
-    }
-
-    @JsonProperty
-    public long getVersion()
-    {
-        return version;
-    }
-
-    @JsonProperty
-    public long getLastUpdated()
-    {
-        return lastUpdated;
-    }
-
-    @Override
-    public boolean equals(Object o)
-    {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        TransactionEntry that = (TransactionEntry) o;
-        return version == that.version &&
-                lastUpdated == that.lastUpdated &&
-                Objects.equals(appId, that.appId);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(appId, version, lastUpdated);
-    }
-
-    @Override
-    public String toString()
-    {
-        return format("TransactionEntry{appId=%s, version=%d, lastUpdated=%d}", appId, version, lastUpdated);
+        requireNonNull(appId, "appId is null");
     }
 }
