00ed358500fa4869d049750f936a6b051c9b9842
Martin Traverso <mtraverso@gmail.com>
2024-03-25 19:29:52 -0700

    Revamp IrExpressionInterpreter
    - Operate in IR expression space instead of mixing expressions and
      carrier objects
    - Simplify the implementation of various IR processors
    - Fix evaluation semantics for CASE conditions
    
    (cherry picked from commit f992602dd43a81b72dc6044d37af2fb006e38d28)

--- core/trino-main/src/test/java/io/trino/sql/TestExpressionInterpreter.java
+++ core/trino-main/src/test/java/io/trino/sql/TestExpressionInterpreter.java
@@ -346,6 +346,10 @@ public class TestExpressionInterpreter
         assertOptimizedEquals(
                 new Between(new Reference(INTEGER, "bound_value"), new Constant(INTEGER, 3L), new Constant(INTEGER, 4L)),
                 FALSE);
+
+        assertOptimizedEquals(
+                new Between(new Reference(INTEGER, "unbound_value"), new Constant(INTEGER, 3L), new Constant(INTEGER, 0L)),
+                ifExpression(new Not(new IsNull(new Reference(INTEGER, "unbound_value"))), FALSE));
     }
 
     @Test
@@ -535,7 +539,7 @@ public class TestExpressionInterpreter
                 new Constant(INTEGER, 1L));
         assertEvaluatedEquals(
                 new Case(ImmutableList.of(
-                        new WhenClause(TRUE, new Constant(INTEGER, 1L)), new WhenClause(new Comparison(EQUAL, new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L))), new Constant(INTEGER, 0L)), new Constant(INTEGER, 2L))),
+                        new WhenClause(TRUE, new Constant(INTEGER, 1L))),
                         new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L)))),
                 new Constant(INTEGER, 1L));
     }
@@ -699,7 +703,7 @@ public class TestExpressionInterpreter
                 new Switch(
                         new Constant(INTEGER, null),
                         ImmutableList.of(
-                                new WhenClause(new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L))), new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L))))),
+                                new WhenClause(new Constant(INTEGER, 1L), new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L))))),
                         new Constant(INTEGER, 1L)),
                 new Constant(INTEGER, 1L));
         assertEvaluatedEquals(
@@ -886,6 +890,10 @@ public class TestExpressionInterpreter
                 .hasErrorCode(DIVISION_BY_ZERO);
         assertTrinoExceptionThrownBy(() -> evaluate(new FieldReference(new Row(ImmutableList.of(new Call(DIVIDE_INTEGER, ImmutableList.of(new Constant(INTEGER, 0L), new Constant(INTEGER, 0L))), new Constant(INTEGER, 1L))), 1)))
                 .hasErrorCode(DIVISION_BY_ZERO);
+
+        assertOptimizedEquals(
+                new FieldReference(new Row(ImmutableList.of(new Reference(INTEGER, "unbound_value"), new Constant(INTEGER, 5L))), 0),
+                new Reference(INTEGER, "unbound_value"));
     }
 
     private static void assertOptimizedEquals(Expression actual, Expression expected)
