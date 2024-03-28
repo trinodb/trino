9d8517471fea64959813f02ee38b6724d34d0afa
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-03-28 11:53:43 +0100

    Separate used Temurin release name from .java-version

--- core/docker/build.sh
+++ core/docker/build.sh
@@ -22,10 +22,11 @@ SOURCE_DIR="${SCRIPT_DIR}/../.."
 
 ARCHITECTURES=(amd64 arm64 ppc64le)
 TRINO_VERSION=
-JDK_VERSION=$(cat "${SOURCE_DIR}/.java-version")
-JDK_RELEASE=ga
 
-while getopts ":a:h:r:j:" o; do
+# Must match https://api.adoptium.net/q/swagger-ui/#/Release%20Info/getReleaseNames
+TEMURIN_RELEASE=$(cat "${SOURCE_DIR}/.temurin-release")
+
+while getopts ":a:h:r:t:" o; do
     case "${o}" in
         a)
             IFS=, read -ra ARCHITECTURES <<< "$OPTARG"
@@ -37,8 +38,8 @@ while getopts ":a:h:r:j:" o; do
             usage
             exit 0
             ;;
-        j)
-            JDK_VERSION="${OPTARG}"
+        t)
+            TEMURIN_RELEASE="${OPTARG}"
             ;;
         *)
             usage
@@ -55,26 +56,9 @@ function check_environment() {
     fi
 }
 
-if [[ "${JDK_VERSION}" == *-ea ]]; then
-  JDK_VERSION=${JDK_VERSION/-ea/}
-  JDK_RELEASE="ea"
-fi
-
-function temurin_jdk_link() {
-  local JDK_VERSION="${1}"
-  local JDK_RELEASE="${2}"
-  local ARCH="${3}"
-
-  versionsUrl="https://api.adoptium.net/v3/info/release_names?heap_size=normal&image_type=jdk&os=linux&page=0&page_size=20&project=jdk&release_type=${JDK_RELEASE}&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%28${JDK_VERSION}%2C%5D"
-  if ! result=$(curl -fLs "$versionsUrl" -H 'accept: application/json'); then
-    echo >&2 "Failed to fetch release names for JDK version [${JDK_VERSION}, ) from Temurin API : $result"
-    exit 1
-  fi
-
-  if ! RELEASE_NAME=$(echo "$result" | jq -er '.releases[]' | grep "${JDK_VERSION}" | head -n 1); then
-    echo >&2 "Failed to determine release name: ${RELEASE_NAME}"
-    exit 1
-  fi
+function temurin_download_link() {
+  local RELEASE_NAME="${1}"
+  local ARCH="${2}"
 
   case "${ARCH}" in
     arm64)
@@ -123,13 +107,13 @@ cp -R default "${WORK_DIR}/"
 TAG_PREFIX="trino:${TRINO_VERSION}"
 
 for arch in "${ARCHITECTURES[@]}"; do
-    echo "🫙  Building the image for $arch with JDK ${JDK_VERSION} ${JDK_RELEASE}"
+    echo "🫙  Building the image for $arch with Temurin Release ${TEMURIN_RELEASE}"
     docker build \
         "${WORK_DIR}" \
         --progress=plain \
         --pull \
-        --build-arg JDK_VERSION="${JDK_VERSION}" \
-        --build-arg JDK_DOWNLOAD_LINK="$(temurin_jdk_link "${JDK_VERSION}" "${JDK_RELEASE}" "${arch}")" \
+        --build-arg JDK_VERSION="${TEMURIN_RELEASE}" \
+        --build-arg JDK_DOWNLOAD_LINK="$(temurin_download_link "${TEMURIN_RELEASE}" "${arch}")" \
         --platform "linux/$arch" \
         -f Dockerfile \
         -t "${TAG_PREFIX}-$arch" \
