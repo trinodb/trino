ed136c4e4d19674c18781eb062ffdaf41bfe7a4c
Martin Traverso <mtraverso@gmail.com>
2024-04-04 20:48:22 -0700

    Determine if an expression may fail during evaluation

--- core/trino-main/src/main/java/io/trino/sql/ir/IrExpressions.java
+++ core/trino-main/src/main/java/io/trino/sql/ir/IrExpressions.java
@@ -14,6 +14,15 @@
 package io.trino.sql.ir;
 
 import com.google.common.collect.ImmutableList;
+import io.trino.metadata.ResolvedFunction;
+import io.trino.spi.function.CatalogSchemaFunctionName;
+import io.trino.sql.PlannerContext;
+import io.trino.type.TypeCoercion;
+
+import static io.trino.metadata.GlobalFunctionCatalog.builtinFunctionName;
+import static io.trino.spi.type.VarcharType.VARCHAR;
+import static io.trino.sql.DynamicFilters.isDynamicFilterFunction;
+import static io.trino.type.LikeFunctions.LIKE_FUNCTION_NAME;
 
 public class IrExpressions
 {
@@ -27,5 +36,60 @@ public class IrExpressions
     public static Expression ifExpression(Expression condition, Expression trueCase, Expression falseCase)
     {
         return new Case(ImmutableList.of(new WhenClause(condition, trueCase)), falseCase);
+    }
+
+    public static boolean mayFail(PlannerContext plannerContext, Expression expression)
+    {
+        return switch (expression) {
+            case Between e -> mayFail(plannerContext, e.value()) || mayFail(plannerContext, e.min()) || mayFail(plannerContext, e.max());
+            case Bind e -> false;
+            case Call e -> mayFail(e.function()) || e.arguments().stream().anyMatch(argument -> mayFail(plannerContext, argument)); // TODO: allow functions to be marked as non-failing
+            case Case e -> e.whenClauses().stream().anyMatch(clause -> mayFail(plannerContext, clause.getOperand()) || mayFail(plannerContext, clause.getResult())) ||
+                    mayFail(plannerContext, e.defaultValue());
+            case Cast e -> mayFail(plannerContext, e);
+            case Coalesce e -> e.operands().stream().anyMatch(argument -> mayFail(plannerContext, argument));
+            case Comparison e -> mayFail(plannerContext, e.left()) || mayFail(plannerContext, e.right());
+            case Constant e -> false;
+            case FieldReference e -> false;
+            case In e -> mayFail(plannerContext, e.value()) || e.valueList().stream().anyMatch(argument -> mayFail(plannerContext, argument));
+            case IsNull e -> mayFail(plannerContext, e.value());
+            case Lambda e -> false;
+            case Logical e -> e.terms().stream().anyMatch(argument -> mayFail(plannerContext, argument));
+            case Not e -> mayFail(plannerContext, e.value());
+            case NullIf e -> mayFail(plannerContext, e.first()) || mayFail(plannerContext, e.second());
+            case Reference e -> false;
+            case Row e -> e.items().stream().anyMatch(argument -> mayFail(plannerContext, argument));
+            case Switch e -> mayFail(plannerContext, e.operand()) || e.whenClauses().stream().anyMatch(clause -> mayFail(plannerContext, clause.getOperand()) || mayFail(plannerContext, clause.getResult())) ||
+                    mayFail(plannerContext, e.defaultValue());
+        };
+    }
+
+    // TODO: record "safety" (can the cast fail at runtime) in Cast node (separate from "may return null" that's currently implied by try_cast)
+    private static boolean mayFail(PlannerContext plannerContext, Cast cast)
+    {
+        if (cast.safe()) {
+            return false;
+        }
+
+        TypeCoercion coercions = new TypeCoercion(plannerContext.getTypeManager()::getType);
+        if (coercions.canCoerce(cast.expression().type(), cast.type())) {
+            return false;
+        }
+
+        if (cast.type().equals(VARCHAR)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean mayFail(ResolvedFunction function)
+    {
+        // TODO: these should be attributes of the function
+        CatalogSchemaFunctionName name = function.getName();
+        return !name.equals(builtinFunctionName("length")) &&
+                !name.equals(builtinFunctionName("substring")) &&
+                !name.equals(builtinFunctionName(LIKE_FUNCTION_NAME)) &&
+                !isDynamicFilterFunction(function.getName());
     }
 }
