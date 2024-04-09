3e6d0eb8c6697bf88d2744e66e3ec89f5fac2a8a
Zhang JianAo <zhangjianao@gmail.com>
2024-04-09 12:10:57 +0800

    Add mongodb dynamic filter support
    (cherry picked from commit bfadbb23f2f7c64c1a49d8c9a8bb356bea91eec6)

--- plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoSplitManager.java
+++ plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoSplitManager.java
@@ -13,6 +13,7 @@
  */
 package io.trino.plugin.mongodb;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorSplitManager;
@@ -23,11 +24,17 @@ import io.trino.spi.connector.Constraint;
 import io.trino.spi.connector.DynamicFilter;
 import io.trino.spi.connector.FixedSplitSource;
 
+import java.util.concurrent.CompletableFuture;
+
+import static io.trino.plugin.mongodb.MongoSessionProperties.getDynamicFilteringWaitTimeout;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 public class MongoSplitManager
         implements ConnectorSplitManager
 {
+    private static final ConnectorSplitSource.ConnectorSplitBatch EMPTY_BATCH = new ConnectorSplitSource.ConnectorSplitBatch(ImmutableList.of(), false);
+
     private final MongoServerDetailsProvider serverDetailsProvider;
 
     @Inject
@@ -45,7 +52,59 @@ public class MongoSplitManager
             Constraint constraint)
     {
         MongoSplit split = new MongoSplit(serverDetailsProvider.getServerAddress());
+        return new MongoSplitSource(session, dynamicFilter, new FixedSplitSource(split));
+    }
 
-        return new FixedSplitSource(split);
+    private static class MongoSplitSource
+            implements ConnectorSplitSource
+    {
+        private final DynamicFilter dynamicFilter;
+        private final long startNanos;
+        private final long dynamicFilteringTimeoutNanos;
+
+        private final ConnectorSplitSource delegateSplitSource;
+
+        public MongoSplitSource(ConnectorSession session, DynamicFilter dynamicFilter, ConnectorSplitSource delegateSplitSource)
+        {
+            this.dynamicFilter = requireNonNull(dynamicFilter, "dynamicFilter is null");
+            this.dynamicFilteringTimeoutNanos = (long) getDynamicFilteringWaitTimeout(session).getValue(NANOSECONDS);
+            this.startNanos = System.nanoTime();
+            this.delegateSplitSource = requireNonNull(delegateSplitSource, "delegateSplitSource is null");
+        }
+
+        @Override
+        public CompletableFuture<ConnectorSplitBatch> getNextBatch(int maxSize)
+        {
+            long remainingTimeoutNanos = getRemainingTimeoutNanos();
+            if (remainingTimeoutNanos > 0 && dynamicFilter.isAwaitable()) {
+                // wait for dynamic filter and yield
+                return dynamicFilter.isBlocked()
+                        .thenApply(ignored -> EMPTY_BATCH)
+                        .completeOnTimeout(EMPTY_BATCH, remainingTimeoutNanos, NANOSECONDS);
+            }
+
+            return delegateSplitSource.getNextBatch(maxSize);
+        }
+
+        @Override
+        public void close()
+        {
+            delegateSplitSource.close();
+        }
+
+        @Override
+        public boolean isFinished()
+        {
+            if (getRemainingTimeoutNanos() > 0 && dynamicFilter.isAwaitable()) {
+                return false;
+            }
+
+            return delegateSplitSource.isFinished();
+        }
+
+        private long getRemainingTimeoutNanos()
+        {
+            return dynamicFilteringTimeoutNanos - (System.nanoTime() - startNanos);
+        }
     }
 }
--- plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoClientConfig.java
+++ plugin/trino-mongodb/src/test/java/io/trino/plugin/mongodb/TestMongoClientConfig.java
@@ -14,6 +14,7 @@
 package io.trino.plugin.mongodb;
 
 import com.google.common.collect.ImmutableMap;
+import io.airlift.units.Duration;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -21,6 +22,8 @@ import java.util.Map;
 import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
 import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
 import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TestMongoClientConfig
 {
@@ -44,7 +47,8 @@ public class TestMongoClientConfig
                 .setRequiredReplicaSetName(null)
                 .setImplicitRowFieldPrefix("_pos")
                 .setProjectionPushdownEnabled(true)
-                .setAllowLocalScheduling(false));
+                .setAllowLocalScheduling(false)
+                .setDynamicFilteringWaitTimeout(new Duration(5, SECONDS)));
     }
 
     @Test
@@ -69,6 +73,7 @@ public class TestMongoClientConfig
                 .put("mongodb.implicit-row-field-prefix", "_prefix")
                 .put("mongodb.projection-pushdown-enabled", "false")
                 .put("mongodb.allow-local-scheduling", "true")
+                .put("mongodb.dynamic-filtering.wait-timeout", "2ms")
                 .buildOrThrow();
 
         MongoClientConfig expected = new MongoClientConfig()
@@ -88,7 +93,8 @@ public class TestMongoClientConfig
                 .setRequiredReplicaSetName("replica_set")
                 .setImplicitRowFieldPrefix("_prefix")
                 .setProjectionPushdownEnabled(false)
-                .setAllowLocalScheduling(true);
+                .setAllowLocalScheduling(true)
+                .setDynamicFilteringWaitTimeout(new Duration(2, MILLISECONDS));
 
         assertFullMapping(properties, expected);
     }
