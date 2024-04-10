6c5e537055f1932b3d800159b51c5a1700a4e143
Vikash Kumar <vikash.kumar@starburstdata.com>
2024-04-10 11:08:13 +0530

    Add support for timestamp to varchar coercer in hive parquet tables

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/parquet/ParquetTypeTranslator.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/parquet/ParquetTypeTranslator.java
@@ -17,16 +17,20 @@ import io.trino.plugin.hive.coercions.IntegerNumberToDoubleCoercer;
 import io.trino.plugin.hive.coercions.TypeCoercer;
 import io.trino.spi.type.DoubleType;
 import io.trino.spi.type.Type;
+import io.trino.spi.type.VarcharType;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 
 import java.util.Optional;
 
 import static io.trino.parquet.reader.ColumnReaderFactory.isIntegerAnnotationAndPrimitive;
+import static io.trino.plugin.hive.coercions.TimestampCoercer.LongTimestampToVarcharCoercer;
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.IntegerType.INTEGER;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_NANOS;
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName;
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT32;
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT64;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT96;
 
 public final class ParquetTypeTranslator
 {
@@ -42,6 +46,11 @@ public final class ParquetTypeTranslator
                 if (fromParquetType == INT64) {
                     return Optional.of(new IntegerNumberToDoubleCoercer<>(BIGINT));
                 }
+            }
+        }
+        if (toTrinoType instanceof VarcharType varcharType) {
+            if (fromParquetType == INT96) {
+                return Optional.of(new LongTimestampToVarcharCoercer(TIMESTAMP_NANOS, varcharType));
             }
         }
         return Optional.empty();
