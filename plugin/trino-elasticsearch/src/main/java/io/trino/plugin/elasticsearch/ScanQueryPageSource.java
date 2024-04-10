f1045680a0ad4c627c6764f9c240238f835a77ba
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-04-11 00:08:30 +0200

    Convert ElasticsearchSplit to record

--- plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
+++ plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
@@ -109,8 +109,8 @@ public class ScanQueryPageSource
 
         long start = System.nanoTime();
         SearchResponse searchResponse = client.beginSearch(
-                split.getIndex(),
-                split.getShard(),
+                split.index(),
+                split.shard(),
                 buildSearchQuery(table.constraint().transformKeys(ElasticsearchColumnHandle.class::cast), table.query(), table.regexes()),
                 needAllFields ? Optional.empty() : Optional.of(requiredFields),
                 documentFields,
