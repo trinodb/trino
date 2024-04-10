171a311de5333a2d7c0aa07348200b8f151c6a4b
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-04-10 13:45:58 +0200

    Convert JdbcTransactionHandle to record

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcTransactionHandle.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcTransactionHandle.java
@@ -13,62 +13,22 @@
  */
 package io.trino.plugin.jdbc;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.trino.spi.connector.ConnectorTransactionHandle;
 
-import java.util.Objects;
 import java.util.UUID;
 
-import static com.google.common.base.MoreObjects.toStringHelper;
 import static java.util.Objects.requireNonNull;
 
-public class JdbcTransactionHandle
+public record JdbcTransactionHandle(UUID uuid)
         implements ConnectorTransactionHandle
 {
-    private final UUID uuid;
-
     public JdbcTransactionHandle()
     {
         this(UUID.randomUUID());
     }
 
-    @JsonCreator
-    public JdbcTransactionHandle(@JsonProperty("uuid") UUID uuid)
+    public JdbcTransactionHandle
     {
-        this.uuid = requireNonNull(uuid, "uuid is null");
-    }
-
-    @JsonProperty
-    public UUID getUuid()
-    {
-        return uuid;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj) {
-            return true;
-        }
-        if ((obj == null) || (getClass() != obj.getClass())) {
-            return false;
-        }
-        JdbcTransactionHandle other = (JdbcTransactionHandle) obj;
-        return Objects.equals(uuid, other.uuid);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(uuid);
-    }
-
-    @Override
-    public String toString()
-    {
-        return toStringHelper(this)
-                .add("uuid", uuid)
-                .toString();
+        requireNonNull(uuid, "uuid is null");
     }
 }
