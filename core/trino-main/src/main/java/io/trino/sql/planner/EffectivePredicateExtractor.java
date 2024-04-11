32495efe0363529495230996c1fdc8f118dc760e
Martin Traverso <mtraverso@gmail.com>
2024-04-11 09:13:20 -0700

    Fix incorrect effective predicate derivation for non-equatable type
    EffectivePredicateExtractor was incorrectly deriving an equality
    predicate for a type that's no comparable or orderable.
    
    (cherry picked from commit 43b968f200942c5ec4b27de7c334d2473f4d13c3)

--- core/trino-main/src/main/java/io/trino/sql/planner/EffectivePredicateExtractor.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/EffectivePredicateExtractor.java
@@ -217,6 +217,7 @@ public class EffectivePredicateExtractor
                     .collect(toImmutableList());
 
             List<Expression> projectionEqualities = nonIdentityAssignments.stream()
+                    .filter(assignment -> assignment.getKey().getType().isComparable() || assignment.getKey().getType().isOrderable())
                     .filter(assignment -> Sets.intersection(SymbolsExtractor.extractUnique(assignment.getValue()), newlyAssignedSymbols).isEmpty())
                     .map(ENTRY_TO_EQUALITY)
                     .collect(toImmutableList());
