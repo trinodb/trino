4a83a83030351a70591f9196b3b96b71f03eb224
Jinyang Li <jinyangli34@gmail.com>
2024-04-11 01:30:50 -0700

    Fix last openRecordGroup not processed in FlatArrayBuilder
    When record size is a multiple of RECORDS_PER_GROUP, the last group is skipped

--- core/trino-main/src/main/java/io/trino/operator/aggregation/arrayagg/FlatArrayBuilder.java
+++ core/trino-main/src/main/java/io/trino/operator/aggregation/arrayagg/FlatArrayBuilder.java
@@ -33,6 +33,7 @@ import static io.airlift.slice.SizeOf.sizeOfObjectArray;
 import static io.trino.operator.VariableWidthData.EMPTY_CHUNK;
 import static io.trino.operator.VariableWidthData.POINTER_SIZE;
 import static io.trino.operator.VariableWidthData.getChunkOffset;
+import static java.lang.Math.toIntExact;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 import static java.util.Objects.checkIndex;
 import static java.util.Objects.requireNonNull;
@@ -208,7 +209,8 @@ public class FlatArrayBuilder
                 recordOffset += recordSize;
             }
         }
-        int recordsInOpenGroup = ((int) size) & RECORDS_PER_GROUP_MASK;
+
+        int recordsInOpenGroup = toIntExact(size - ((long) closedRecordGroups.size() * RECORDS_PER_GROUP));
         int recordOffset = 0;
         for (int recordIndex = 0; recordIndex < recordsInOpenGroup; recordIndex++) {
             write(openRecordGroup, recordOffset, blockBuilder);
