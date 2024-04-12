ad4c07db7328fa3804fe920a56afe5cb3f39efb2
Yuya Ebihara <ebyhry@gmail.com>
2024-04-12 21:31:01 +0900

    Use method reference in testMaxRowsExceedsLimit

--- client/trino-jdbc/src/test/java/io/trino/jdbc/BaseTestJdbcResultSet.java
+++ client/trino-jdbc/src/test/java/io/trino/jdbc/BaseTestJdbcResultSet.java
@@ -1155,7 +1155,7 @@ public abstract class BaseTestJdbcResultSet
     {
         try (ConnectedStatement connectedStatement = newStatement()) {
             connectedStatement.getStatement().setLargeMaxRows(Integer.MAX_VALUE * 10L);
-            assertThatThrownBy(() -> connectedStatement.getStatement().getMaxRows())
+            assertThatThrownBy(connectedStatement.getStatement()::getMaxRows)
                     .isInstanceOf(SQLException.class)
                     .hasMessage("Max rows exceeds limit of 2147483647");
         }
