531f77435948c42116421c4e9974c703d25daf28
Yuya Ebihara <ebyhry@gmail.com>
2024-04-13 08:45:33 +0900

    Convert RedisTableDescription to record

--- plugin/trino-redis/src/main/java/io/trino/plugin/redis/RedisTableDescription.java
+++ plugin/trino-redis/src/main/java/io/trino/plugin/redis/RedisTableDescription.java
@@ -14,68 +14,22 @@
 package io.trino.plugin.redis;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * Json description to parse a Redis key/value pairs. See the documentation for the exact JSON syntax.
  */
-public class RedisTableDescription
+public record RedisTableDescription(
+        String tableName,
+        String schemaName,
+        RedisTableFieldGroup key,
+        RedisTableFieldGroup value)
 {
-    private final String tableName;
-    private final String schemaName;
-    private final RedisTableFieldGroup key;
-    private final RedisTableFieldGroup value;
-
     @JsonCreator
-    public RedisTableDescription(
-            @JsonProperty("tableName") String tableName,
-            @JsonProperty("schemaName") String schemaName,
-            @JsonProperty("key") RedisTableFieldGroup key,
-            @JsonProperty("value") RedisTableFieldGroup value)
+    public RedisTableDescription
     {
         checkArgument(!isNullOrEmpty(tableName), "tableName is null or is empty");
-        this.tableName = tableName;
-        this.schemaName = schemaName;
-        this.key = key;
-        this.value = value;
-    }
-
-    @JsonProperty
-    public String getTableName()
-    {
-        return tableName;
-    }
-
-    @JsonProperty
-    public String getSchemaName()
-    {
-        return schemaName;
-    }
-
-    @JsonProperty
-    public RedisTableFieldGroup getKey()
-    {
-        return key;
-    }
-
-    @JsonProperty
-    public RedisTableFieldGroup getValue()
-    {
-        return value;
-    }
-
-    @Override
-    public String toString()
-    {
-        return toStringHelper(this)
-                .add("tableName", tableName)
-                .add("schemaName", schemaName)
-                .add("key", key)
-                .add("value", value)
-                .toString();
     }
 }
