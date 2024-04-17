4291c1b6735b9df3b126280af66489f300b54b31
Yuya Ebihara <ebyhry@gmail.com>
2024-04-17 10:15:55 +0900

    Improve speed when listing table comments in BigQuery

--- plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/DefaultBigQueryMetadataFactory.java
+++ plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/DefaultBigQueryMetadataFactory.java
@@ -25,18 +25,24 @@ public class DefaultBigQueryMetadataFactory
     private final BigQueryClientFactory bigQueryClient;
     private final ListeningExecutorService executorService;
     private final BigQueryTypeManager typeManager;
+    private final boolean isLegacyMetadataListing;
 
     @Inject
-    public DefaultBigQueryMetadataFactory(BigQueryClientFactory bigQueryClient, BigQueryTypeManager typeManager, @ForBigQuery ListeningExecutorService executorService)
+    public DefaultBigQueryMetadataFactory(
+            BigQueryClientFactory bigQueryClient,
+            BigQueryTypeManager typeManager,
+            @ForBigQuery ListeningExecutorService executorService,
+            BigQueryConfig config)
     {
         this.bigQueryClient = requireNonNull(bigQueryClient, "bigQueryClient is null");
         this.typeManager = requireNonNull(typeManager, "typeManager is null");
         this.executorService = requireNonNull(executorService, "executorService is null");
+        this.isLegacyMetadataListing = config.isLegacyMetadataListing();
     }
 
     @Override
     public BigQueryMetadata create(BigQueryTransactionHandle transaction)
     {
-        return new BigQueryMetadata(bigQueryClient, typeManager, executorService);
+        return new BigQueryMetadata(bigQueryClient, typeManager, executorService, isLegacyMetadataListing);
     }
 }
