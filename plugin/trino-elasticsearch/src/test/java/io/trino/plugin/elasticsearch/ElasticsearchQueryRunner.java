cd015857183ea81efc674666dcc363c028a68690
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-04-19 10:05:32 +0200

    Add builder for Elasticsearch query runner

--- plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/ElasticsearchQueryRunner.java
+++ plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/ElasticsearchQueryRunner.java
@@ -13,7 +13,9 @@
  */
 package io.trino.plugin.elasticsearch;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.airlift.log.Level;
 import io.airlift.log.Logger;
 import io.airlift.log.Logging;
@@ -28,6 +30,8 @@ import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.google.common.io.Resources.getResource;
@@ -38,112 +42,115 @@ import static io.trino.plugin.tpch.TpchMetadata.TINY_SCHEMA_NAME;
 import static io.trino.testing.TestingSession.testSessionBuilder;
 import static java.lang.String.format;
 import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public final class ElasticsearchQueryRunner
 {
-    public static final String USER = "elastic_user";
-    public static final String PASSWORD = "123456";
+    private ElasticsearchQueryRunner() {}
 
     static {
         Logging logging = Logging.initialize();
         logging.setLevel("org.elasticsearch.client.RestClient", Level.OFF);
     }
 
-    private ElasticsearchQueryRunner() {}
-
     private static final Logger LOG = Logger.get(ElasticsearchQueryRunner.class);
+
+    public static final String USER = "elastic_user";
+    public static final String PASSWORD = "123456";
     private static final String TPCH_SCHEMA = "tpch";
 
-    public static QueryRunner createElasticsearchQueryRunner(
-            ElasticsearchServer server,
-            Iterable<TpchTable<?>> tables,
-            Map<String, String> extraProperties,
-            Map<String, String> extraConnectorProperties)
-            throws Exception
+    public static Builder builder(ElasticsearchServer server)
     {
-        return createElasticsearchQueryRunner(server, tables, extraProperties, extraConnectorProperties, "elasticsearch");
-    }
-
-    public static QueryRunner createElasticsearchQueryRunner(
-            ElasticsearchServer server,
-            Iterable<TpchTable<?>> tables,
-            Map<String, String> extraProperties,
-            Map<String, String> extraConnectorProperties,
-            String catalogName)
-            throws Exception
-    {
-        RestHighLevelClient client = null;
-        DistributedQueryRunner queryRunner = null;
         try {
-            queryRunner = DistributedQueryRunner.builder(testSessionBuilder()
-                            .setCatalog(catalogName)
-                            .setSchema(TPCH_SCHEMA)
-                            .build())
-                    .setExtraProperties(extraProperties)
-                    .build();
-
-            queryRunner.installPlugin(new JmxPlugin());
-            queryRunner.createCatalog("jmx", "jmx");
-
-            queryRunner.installPlugin(new TpchPlugin());
-            queryRunner.createCatalog("tpch", "tpch");
-
-            ElasticsearchConnectorFactory testFactory = new ElasticsearchConnectorFactory();
-
-            installElasticsearchPlugin(server, queryRunner, catalogName, testFactory, extraConnectorProperties);
-
-            TestingTrinoClient trinoClient = queryRunner.getClient();
-
-            LOG.info("Loading data...");
-
-            client = server.getClient();
-
-            long startTime = System.nanoTime();
-            for (TpchTable<?> table : tables) {
-                loadTpchTopic(client, trinoClient, table);
-            }
-            LOG.info("Loading complete in %s", nanosSince(startTime).toString(SECONDS));
-
-            return queryRunner;
+            return new Builder(server)
+                    .addConnectorProperties(ImmutableMap.<String, String>builder()
+                            .put("elasticsearch.host", server.getAddress().getHost())
+                            .put("elasticsearch.port", Integer.toString(server.getAddress().getPort()))
+                            // Node discovery relies on the publish_address exposed via the Elasticseach API
+                            // This doesn't work well within a docker environment that maps ES's port to a random public port
+                            .put("elasticsearch.ignore-publish-address", "true")
+                            .put("elasticsearch.default-schema-name", TPCH_SCHEMA)
+                            .put("elasticsearch.scroll-size", "1000")
+                            .put("elasticsearch.scroll-timeout", "1m")
+                            .put("elasticsearch.request-timeout", "2m")
+                            .put("elasticsearch.tls.enabled", "true")
+                            .put("elasticsearch.tls.truststore-path", new File(getResource("truststore.jks").toURI()).getPath())
+                            .put("elasticsearch.tls.truststore-password", "123456")
+                            .put("elasticsearch.tls.verify-hostnames", "false")
+                            .put("elasticsearch.security", "PASSWORD")
+                            .put("elasticsearch.auth.user", USER)
+                            .put("elasticsearch.auth.password", PASSWORD)
+                            .buildOrThrow());
         }
-        catch (Exception e) {
-            closeAllSuppress(e, queryRunner, client);
-            throw e;
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static void installElasticsearchPlugin(
-            ElasticsearchServer server,
-            QueryRunner queryRunner,
-            String catalogName,
-            ElasticsearchConnectorFactory factory,
-            Map<String, String> extraConnectorProperties)
-            throws URISyntaxException
+    public static final class Builder
+            extends DistributedQueryRunner.Builder<Builder>
     {
-        queryRunner.installPlugin(new ElasticsearchPlugin(factory));
-        ImmutableMap.Builder<String, String> builder = ImmutableMap.<String, String>builder()
-                .put("elasticsearch.host", server.getAddress().getHost())
-                .put("elasticsearch.port", Integer.toString(server.getAddress().getPort()))
-                // Node discovery relies on the publish_address exposed via the Elasticseach API
-                // This doesn't work well within a docker environment that maps ES's port to a random public port
-                .put("elasticsearch.ignore-publish-address", "true")
-                .put("elasticsearch.default-schema-name", TPCH_SCHEMA)
-                .put("elasticsearch.scroll-size", "1000")
-                .put("elasticsearch.scroll-timeout", "1m")
-                .put("elasticsearch.request-timeout", "2m")
-                .put("elasticsearch.tls.enabled", "true")
-                .put("elasticsearch.tls.truststore-path", new File(getResource("truststore.jks").toURI()).getPath())
-                .put("elasticsearch.tls.truststore-password", "123456")
-                .put("elasticsearch.tls.verify-hostnames", "false")
-                .put("elasticsearch.security", "PASSWORD")
-                .put("elasticsearch.auth.user", USER)
-                .put("elasticsearch.auth.password", PASSWORD);
+        private final ElasticsearchServer server;
+        private final Map<String, String> connectorProperties = new HashMap<>();
+        private List<TpchTable<?>> initialTables = ImmutableList.of();
 
-        Map<String, String> config = builder.putAll(extraConnectorProperties)
-                .buildOrThrow();
+        private Builder(ElasticsearchServer server)
+        {
+            super(testSessionBuilder()
+                    .setCatalog("elasticsearch")
+                    .setSchema(TPCH_SCHEMA)
+                    .build());
+            this.server = requireNonNull(server, "server is null");
+        }
 
-        queryRunner.createCatalog(catalogName, "elasticsearch", config);
+        @CanIgnoreReturnValue
+        public Builder addConnectorProperties(Map<String, String> connectorProperties)
+        {
+            this.connectorProperties.putAll(requireNonNull(connectorProperties, "connectorProperties is null"));
+            return this;
+        }
+
+        @CanIgnoreReturnValue
+        public Builder setInitialTables(Iterable<TpchTable<?>> initialTables)
+        {
+            this.initialTables = ImmutableList.copyOf(requireNonNull(initialTables, "initialTables is null"));
+            return this;
+        }
+
+        @Override
+        public DistributedQueryRunner build()
+                throws Exception
+        {
+            DistributedQueryRunner queryRunner = super.build();
+            try {
+                queryRunner.installPlugin(new JmxPlugin());
+                queryRunner.createCatalog("jmx", "jmx");
+
+                queryRunner.installPlugin(new TpchPlugin());
+                queryRunner.createCatalog("tpch", "tpch");
+
+                queryRunner.installPlugin(new ElasticsearchPlugin(new ElasticsearchConnectorFactory()));
+                queryRunner.createCatalog("elasticsearch", "elasticsearch", connectorProperties);
+
+                TestingTrinoClient trinoClient = queryRunner.getClient();
+
+                LOG.info("Loading data...");
+                long startTime = System.nanoTime();
+                try (RestHighLevelClient client = server.getClient()) {
+                    for (TpchTable<?> table : initialTables) {
+                        loadTpchTopic(client, trinoClient, table);
+                    }
+                }
+                LOG.info("Loading complete in %s", nanosSince(startTime).toString(SECONDS));
+
+                return queryRunner;
+            }
+            catch (Throwable e) {
+                closeAllSuppress(e, queryRunner);
+                throw e;
+            }
+        }
     }
 
     private static void loadTpchTopic(RestHighLevelClient client, TestingTrinoClient trinoClient, TpchTable<?> table)
@@ -158,11 +165,10 @@ public final class ElasticsearchQueryRunner
     public static void main(String[] args)
             throws Exception
     {
-        QueryRunner queryRunner = createElasticsearchQueryRunner(
-                new ElasticsearchServer(ELASTICSEARCH_7_IMAGE),
-                TpchTable.getTables(),
-                ImmutableMap.of("http-server.http.port", "8080"),
-                ImmutableMap.of());
+        QueryRunner queryRunner = builder(new ElasticsearchServer(ELASTICSEARCH_7_IMAGE))
+                .addExtraProperty("http-server.http.port", "8080")
+                .setInitialTables(TpchTable.getTables())
+                .build();
 
         Logger log = Logger.get(ElasticsearchQueryRunner.class);
         log.info("======== SERVER STARTED ========");
