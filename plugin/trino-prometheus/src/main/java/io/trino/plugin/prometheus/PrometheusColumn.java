a35737edd054c0f1094a8e0156ac0b0f59c1983a
Yuya Ebihara <ebyhry@gmail.com>
2024-04-22 20:29:50 +0900

    Convert PrometheusColumn to record

--- plugin/trino-prometheus/src/main/java/io/trino/plugin/prometheus/PrometheusColumn.java
+++ plugin/trino-prometheus/src/main/java/io/trino/plugin/prometheus/PrometheusColumn.java
@@ -13,67 +13,17 @@
  */
 package io.trino.plugin.prometheus;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.trino.spi.type.Type;
-
-import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.util.Objects.requireNonNull;
 
-public final class PrometheusColumn
+public record PrometheusColumn(String name, Type type)
 {
-    private final String name;
-    private final Type type;
-
-    @JsonCreator
-    public PrometheusColumn(
-            @JsonProperty("name") String name,
-            @JsonProperty("type") Type type)
+    public PrometheusColumn
     {
         checkArgument(!isNullOrEmpty(name), "name is null or is empty");
-        this.name = name;
-        this.type = requireNonNull(type, "type is null");
-    }
-
-    @JsonProperty
-    public String getName()
-    {
-        return name;
-    }
-
-    @JsonProperty
-    public Type getType()
-    {
-        return type;
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(name, type);
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-
-        PrometheusColumn other = (PrometheusColumn) obj;
-        return Objects.equals(this.name, other.name) &&
-                Objects.equals(this.type, other.type);
-    }
-
-    @Override
-    public String toString()
-    {
-        return name + ":" + type;
+        requireNonNull(type, "type is null");
     }
 }
