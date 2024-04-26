f354d1e9a56800c4b544db4ce8a11912caab2020
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-04-26 21:14:08 +0200

    Fix thread leak in Kinesis

--- plugin/trino-kinesis/src/main/java/io/trino/plugin/kinesis/s3config/S3TableConfigClient.java
+++ plugin/trino-kinesis/src/main/java/io/trino/plugin/kinesis/s3config/S3TableConfigClient.java
@@ -32,6 +32,7 @@ import io.trino.plugin.kinesis.KinesisConfig;
 import io.trino.plugin.kinesis.KinesisStreamDescription;
 import io.trino.spi.connector.SchemaTableName;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,13 +44,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 /**
  * Utility class to retrieve table definitions from a common place on Amazon S3.
@@ -72,6 +73,8 @@ public class S3TableConfigClient
     private volatile ScheduledFuture<?> updateTaskHandle;
 
     private final Map<String, KinesisStreamDescription> descriptors = Collections.synchronizedMap(new HashMap<>());
+
+    private ScheduledExecutorService scheduler;
 
     @Inject
     public S3TableConfigClient(
@@ -96,8 +99,16 @@ public class S3TableConfigClient
     protected void startS3Updates()
     {
         if (this.bucketUrl.isPresent()) {
-            ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+            scheduler = newSingleThreadScheduledExecutor();
             this.updateTaskHandle = scheduler.scheduleAtFixedRate(this::updateTablesFromS3, 5_000, tableDescriptionRefreshInterval.toMillis(), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    @PreDestroy
+    public void destroy()
+    {
+        if (scheduler != null) {
+            scheduler.shutdownNow();
         }
     }
 
