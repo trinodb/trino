96489b1a3f62caf180eacd84dbdf11a69626c68e
Yuya Ebihara <ebyhry@gmail.com>
2024-04-30 22:41:32 +0900

    Convert KinesisStreamFieldGroup to record

--- plugin/trino-kinesis/src/main/java/io/trino/plugin/kinesis/KinesisStreamFieldGroup.java
+++ plugin/trino-kinesis/src/main/java/io/trino/plugin/kinesis/KinesisStreamFieldGroup.java
@@ -13,58 +13,22 @@
  */
 package io.trino.plugin.kinesis;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
 import java.util.Optional;
 
-import static com.google.common.base.MoreObjects.toStringHelper;
-import static io.trino.plugin.kinesis.KinesisCompressionCodec.UNCOMPRESSED;
 import static java.util.Objects.requireNonNull;
 
-public class KinesisStreamFieldGroup
+public record KinesisStreamFieldGroup(
+        String dataFormat,
+        Optional<KinesisCompressionCodec> compressionCodec,
+        List<KinesisStreamFieldDescription> fields)
 {
-    private final String dataFormat;
-    private final Optional<KinesisCompressionCodec> compressionCodec;
-    private final List<KinesisStreamFieldDescription> fields;
-
-    @JsonCreator
-    public KinesisStreamFieldGroup(
-            @JsonProperty("dataFormat") String dataFormat,
-            @JsonProperty("compressionCodec") Optional<KinesisCompressionCodec> compressionCodec,
-            @JsonProperty("fields") List<KinesisStreamFieldDescription> fields)
+    public KinesisStreamFieldGroup
     {
-        this.dataFormat = requireNonNull(dataFormat, "dataFormat is null");
-        this.compressionCodec = requireNonNull(compressionCodec, "compressionCodec is null");
-        this.fields = ImmutableList.copyOf(requireNonNull(fields, "fields is null"));
-    }
-
-    @JsonProperty
-    public String getDataFormat()
-    {
-        return dataFormat;
-    }
-
-    public KinesisCompressionCodec getCompressionCodec()
-    {
-        return compressionCodec.orElse(UNCOMPRESSED);
-    }
-
-    @JsonProperty
-    public List<KinesisStreamFieldDescription> getFields()
-    {
-        return fields;
-    }
-
-    @Override
-    public String toString()
-    {
-        return toStringHelper(this)
-                .add("dataFormat", dataFormat)
-                .add("compressionCodec", compressionCodec)
-                .add("fields", fields)
-                .toString();
+        requireNonNull(dataFormat, "dataFormat is null");
+        requireNonNull(compressionCodec, "compressionCodec is null");
+        fields = ImmutableList.copyOf(requireNonNull(fields, "fields is null"));
     }
 }
