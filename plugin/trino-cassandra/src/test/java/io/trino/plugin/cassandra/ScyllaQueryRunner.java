031bf2f2d9af8ca49e2e034bb793e9f47237a538
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-05-07 15:03:31 +0200

    Remove unused parameter form query runner creation

--- plugin/trino-cassandra/src/test/java/io/trino/plugin/cassandra/ScyllaQueryRunner.java
+++ plugin/trino-cassandra/src/test/java/io/trino/plugin/cassandra/ScyllaQueryRunner.java
@@ -38,7 +38,6 @@ public final class ScyllaQueryRunner
     public static QueryRunner createScyllaQueryRunner(
             TestingScyllaServer server,
             Map<String, String> extraProperties,
-            Map<String, String> connectorProperties,
             Iterable<TpchTable<?>> tables)
             throws Exception
     {
@@ -51,12 +50,12 @@ public final class ScyllaQueryRunner
             queryRunner.createCatalog("tpch", "tpch");
 
             // note: additional copy via ImmutableList so that if fails on nulls
-            connectorProperties = new HashMap<>(ImmutableMap.copyOf(connectorProperties));
-            connectorProperties.putIfAbsent("cassandra.contact-points", server.getHost());
-            connectorProperties.putIfAbsent("cassandra.native-protocol-port", Integer.toString(server.getPort()));
-            connectorProperties.putIfAbsent("cassandra.allow-drop-table", "true");
-            connectorProperties.putIfAbsent("cassandra.load-policy.use-dc-aware", "true");
-            connectorProperties.putIfAbsent("cassandra.load-policy.dc-aware.local-dc", "datacenter1");
+            Map<String, String> connectorProperties = new HashMap<>();
+            connectorProperties.put("cassandra.contact-points", server.getHost());
+            connectorProperties.put("cassandra.native-protocol-port", Integer.toString(server.getPort()));
+            connectorProperties.put("cassandra.allow-drop-table", "true");
+            connectorProperties.put("cassandra.load-policy.use-dc-aware", "true");
+            connectorProperties.put("cassandra.load-policy.dc-aware.local-dc", "datacenter1");
 
             queryRunner.installPlugin(new CassandraPlugin());
             queryRunner.createCatalog("cassandra", "cassandra", connectorProperties);
@@ -90,7 +89,6 @@ public final class ScyllaQueryRunner
         QueryRunner queryRunner = createScyllaQueryRunner(
                 new TestingScyllaServer(),
                 ImmutableMap.of("http-server.http.port", "8080"),
-                ImmutableMap.of(),
                 TpchTable.getTables());
 
         Logger log = Logger.get(ScyllaQueryRunner.class);
