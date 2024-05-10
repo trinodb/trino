bb0c7bd450587282134d8b88d56b782d36d24e51
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-05-10 14:16:07 +0200

    Migrate Glue/S3 tests to AWS SDK v2

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/metastore/glue/TestDeltaS3AndGlueMetastoreTest.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/metastore/glue/TestDeltaS3AndGlueMetastoreTest.java
@@ -45,6 +45,8 @@ public class TestDeltaS3AndGlueMetastoreTest
                 .setDeltaProperties(ImmutableMap.<String, String>builder()
                         .put("hive.metastore", "glue")
                         .put("hive.metastore.glue.default-warehouse-dir", schemaPath())
+                        .put("fs.hadoop.enabled", "false")
+                        .put("fs.native-s3.enabled", "true")
                         .put("delta.enable-non-concurrent-writes", "true")
                         .buildOrThrow())
                 .setSchemaLocation(schemaPath())
