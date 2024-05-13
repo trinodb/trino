d52ff44efbe9248a6b9e87019923dcf5eacaf466
Yuya Ebihara <ebyhry@gmail.com>
2024-05-13 12:04:35 +0900

    Convert DeltaLakeMergeResult to record

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMergeResult.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMergeResult.java
@@ -13,9 +13,6 @@
  */
 package io.trino.plugin.deltalake;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,38 +21,18 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Objects.requireNonNull;
 
-public class DeltaLakeMergeResult
+public record DeltaLakeMergeResult(
+        List<String> partitionValues,
+        Optional<String> oldFile,
+        Optional<DataFileInfo> newFile)
 {
-    private final List<String> partitionValues;
-    private final Optional<String> oldFile;
-    private final Optional<DataFileInfo> newFile;
-
-    @JsonCreator
-    public DeltaLakeMergeResult(List<String> partitionValues, Optional<String> oldFile, Optional<DataFileInfo> newFile)
+    public DeltaLakeMergeResult
     {
         // Immutable list does not allow nulls
         // noinspection Java9CollectionFactory
-        this.partitionValues = unmodifiableList(new ArrayList<>(requireNonNull(partitionValues, "partitionValues is null")));
-        this.oldFile = requireNonNull(oldFile, "oldFile is null");
-        this.newFile = requireNonNull(newFile, "newFile is null");
+        partitionValues = unmodifiableList(new ArrayList<>(requireNonNull(partitionValues, "partitionValues is null")));
+        requireNonNull(oldFile, "oldFile is null");
+        requireNonNull(newFile, "newFile is null");
         checkArgument(oldFile.isPresent() || newFile.isPresent(), "old or new must be present");
-    }
-
-    @JsonProperty
-    public List<String> getPartitionValues()
-    {
-        return partitionValues;
-    }
-
-    @JsonProperty
-    public Optional<String> getOldFile()
-    {
-        return oldFile;
-    }
-
-    @JsonProperty
-    public Optional<DataFileInfo> getNewFile()
-    {
-        return newFile;
     }
 }
