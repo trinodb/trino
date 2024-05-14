5854632de9f7df3e382154523749999f0746c5e4
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-05-14 09:34:00 +0200

    Set thread context class loader in Glue threads
    It might not be necessary, but it is also hard to verify it is not
    necessary, so it's better to set the TCCL. The TCCL is set in pretty much
    all other thread pools by virtue of default behavior of thread factories
    created with `io.airlift.concurrent.Threads`.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/GlueHiveMetastore.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/GlueHiveMetastore.java
@@ -117,6 +117,7 @@ import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.opentelemetry.context.Context.taskWrapping;
 import static io.trino.plugin.hive.HiveErrorCode.HIVE_FILESYSTEM_ERROR;
 import static io.trino.plugin.hive.HiveErrorCode.HIVE_METASTORE_ERROR;
@@ -208,7 +209,7 @@ public class GlueHiveMetastore
                 config.getPartitionSegments(),
                 config.isAssumeCanonicalPartitionKeys(),
                 visibleTableKinds,
-                newFixedThreadPool(config.getThreads(), Thread.ofPlatform().name("glue-%d-".formatted(poolCounter.getAndIncrement()), 0L).factory()));
+                newFixedThreadPool(config.getThreads(), daemonThreadsNamed("glue-%s-%%s".formatted(poolCounter.getAndIncrement()))));
     }
 
     private GlueHiveMetastore(
