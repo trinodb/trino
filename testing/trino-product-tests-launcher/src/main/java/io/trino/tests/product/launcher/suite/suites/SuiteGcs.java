9a4ed631df27e63863fcf589d8eec9feb98eff16
Anu Sudarsan <anu.at.infy@gmail.com>
2024-05-14 19:13:41 -0400

    Add hive product test for gcs filesystem

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteGcs.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteGcs.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import static io.trino.tests.product.TestGroups.CONFIGURED_FEATURES;
 import static io.trino.tests.product.TestGroups.DELTA_LAKE_GCS;
+import static io.trino.tests.product.TestGroups.HIVE_GCS;
 import static io.trino.tests.product.TestGroups.ICEBERG_GCS;
 import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
 
@@ -38,6 +39,9 @@ public class SuiteGcs
                         .build(),
                 testOnEnvironment(EnvMultinodeGcs.class)
                         .withGroups(ICEBERG_GCS, CONFIGURED_FEATURES)
+                        .build(),
+                testOnEnvironment(EnvMultinodeGcs.class)
+                        .withGroups(HIVE_GCS, CONFIGURED_FEATURES)
                         .build());
     }
 }
