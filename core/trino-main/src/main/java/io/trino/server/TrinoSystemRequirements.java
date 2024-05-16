f99d87f48b2208677ab0b9bf260c73ad30b2c433
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-05-16 18:52:27 +0200

    Verify required JDK is at least 22.0.1

--- core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
+++ core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
@@ -99,7 +99,7 @@ final class TrinoSystemRequirements
 
     private static void verifyJavaVersion()
     {
-        Version required = Version.parse("21.0.1");
+        Version required = Version.parse("22.0.1");
 
         if (Runtime.version().compareTo(required) < 0) {
             failRequirement("Trino requires Java %s at minimum (found %s)", required, Runtime.version());
