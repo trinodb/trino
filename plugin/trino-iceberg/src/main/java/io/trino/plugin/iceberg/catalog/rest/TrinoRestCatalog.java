33a838e3a2503aed54ebe7e0eaecd76bad4d5e42
takezoe <takezoe@gmail.com>
2024-05-19 17:50:27 +0900

    Avoid redundant tail slash check in TrinoRestCatalog

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoRestCatalog.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoRestCatalog.java
@@ -349,9 +349,6 @@ public class TrinoRestCatalog
         String databaseLocation = (String) properties.get(IcebergSchemaProperties.LOCATION_PROPERTY);
         checkArgument(databaseLocation != null, "location must be set for %s", schemaTableName.getSchemaName());
 
-        if (databaseLocation.endsWith("/")) {
-            return databaseLocation + tableName;
-        }
         return appendPath(databaseLocation, tableName);
     }
 
