b6752c9d666c94b28f08c341dfe293549674cc94
kasiafi <30203062+kasiafi@users.noreply.github.com>
2024-05-23 13:15:45 +0200

    Optimize first_value and last_value functions
    Record recently seen non null or null positions,
    and use them when processing new frames.
    
    Should speed up queries with regular framing like
    ROWS BETWEEN 100 PRECEDING AND 100 FOLLOWING,
    ROWS CURRENT ROW,
    or RANGE UNBOUNDED PRECEDING UNBOUNDED FOLLOWING,
    and with non-null values being sparse or none.

--- core/trino-main/src/main/java/io/trino/operator/window/LastValueFunction.java
+++ core/trino-main/src/main/java/io/trino/operator/window/LastValueFunction.java
@@ -28,6 +28,14 @@ public class LastValueFunction
     private final int argumentChannel;
     private final boolean ignoreNulls;
 
+    // Record a frame end with corresponding first non-null position
+    // or last seen null position (going backwards).
+    // The non-null position is valid for all frames starting from the recorded non-null position or before
+    // and ending at the recorded frame end or before.
+    private int recordedFrameEnd = -1;
+    private int recordedValuePosition = -1;
+    private int recordedNullPosition = -1;
+
     public LastValueFunction(List<Integer> argumentChannels, boolean ignoreNulls)
     {
         this.argumentChannel = getOnlyElement(argumentChannels);
@@ -35,30 +43,79 @@ public class LastValueFunction
     }
 
     @Override
+    public void reset()
+    {
+        recordedFrameEnd = -1;
+        recordedValuePosition = -1;
+        recordedNullPosition = -1;
+    }
+
+    @Override
     public void processRow(BlockBuilder output, int frameStart, int frameEnd, int currentPosition)
     {
+        // empty frame
         if (frameStart < 0) {
             output.appendNull();
             return;
         }
 
-        int valuePosition = frameEnd;
+        if (!ignoreNulls) {
+            windowIndex.appendTo(argumentChannel, frameEnd, output);
+            return;
+        }
 
-        if (ignoreNulls) {
-            while (valuePosition >= frameStart) {
-                if (!windowIndex.isNull(argumentChannel, valuePosition)) {
-                    break;
+        if (recordedFrameEnd >= 0 && frameEnd <= recordedFrameEnd) {
+            // try to use the recorded non-null position
+            if (recordedValuePosition >= 0 && frameEnd >= recordedValuePosition) {
+                if (frameStart > recordedValuePosition) {
+                    // only nulls in the frame
+                    output.appendNull();
+                    return;
                 }
-
-                valuePosition--;
-            }
-
-            if (valuePosition < frameStart) {
-                output.appendNull();
+                // use the recorded non-null position
+                windowIndex.appendTo(argumentChannel, recordedValuePosition, output);
                 return;
+            }
+            // try to use the recorded null position
+            if (recordedNullPosition >= 0 && frameEnd >= recordedNullPosition) {
+                if (frameStart >= recordedNullPosition) {
+                    // only nulls in the frame
+                    output.appendNull();
+                    return;
+                }
             }
         }
 
+        // there is no recorded position or the current frame is before / after the recorded frame
+        // find the first non-null or last null position for current the frame and record it for future reference
+        // try to optimize by using the recorded null position
+        int valuePosition;
+        if (recordedFrameEnd >= 0 && recordedNullPosition >= 0 && frameEnd <= recordedFrameEnd && frameEnd >= recordedNullPosition) {
+            valuePosition = recordedNullPosition;
+        }
+        else {
+            valuePosition = frameEnd;
+        }
+
+        recordedFrameEnd = frameEnd;
+        recordedValuePosition = -1;
+        recordedNullPosition = -1;
+
+        while (valuePosition >= frameStart) {
+            if (!windowIndex.isNull(argumentChannel, valuePosition)) {
+                break;
+            }
+
+            valuePosition--;
+        }
+
+        if (valuePosition < frameStart) {
+            recordedNullPosition = frameStart;
+            output.appendNull();
+            return;
+        }
+
+        recordedValuePosition = valuePosition;
         windowIndex.appendTo(argumentChannel, valuePosition, output);
     }
 }
