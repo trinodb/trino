a70e099f9d30f3acf3b16fa8568f4a23df319bff
Anu Sudarsan <anu.at.infy@gmail.com>
2024-05-30 13:24:39 -0400

    Add more http proxy configs for native S3 filesystem implementation
    To make the http proxy config options in parity with legacy implementation

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemConfig.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemConfig.java
@@ -13,6 +13,8 @@
  */
 package io.trino.filesystem.s3;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.net.HostAndPort;
 import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
@@ -27,7 +29,9 @@ import software.amazon.awssdk.retries.api.RetryStrategy;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 
 import java.util.Optional;
+import java.util.Set;
 
+import static com.google.common.base.Strings.nullToEmpty;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static software.amazon.awssdk.awscore.retry.AwsRetryStrategy.adaptiveRetryStrategy;
 import static software.amazon.awssdk.awscore.retry.AwsRetryStrategy.legacyRetryStrategy;
@@ -103,6 +107,10 @@ public class S3FileSystemConfig
     private boolean tcpKeepAlive;
     private HostAndPort httpProxy;
     private boolean httpProxySecure;
+    private String httpProxyUsername;
+    private String httpProxyPassword;
+    private boolean preemptiveBasicProxyAuth;
+    private Set<String> nonProxyHosts = ImmutableSet.of();
     private ObjectCannedAcl objectCannedAcl = ObjectCannedAcl.NONE;
     private RetryMode retryMode = RetryMode.LEGACY;
     private int maxErrorRetries = 10;
@@ -438,6 +446,55 @@ public class S3FileSystemConfig
     public S3FileSystemConfig setHttpProxySecure(boolean httpProxySecure)
     {
         this.httpProxySecure = httpProxySecure;
+        return this;
+    }
+
+    public String getHttpProxyUsername()
+    {
+        return httpProxyUsername;
+    }
+
+    @Config("s3.http-proxy.username")
+    public S3FileSystemConfig setHttpProxyUsername(String httpProxyUsername)
+    {
+        this.httpProxyUsername = httpProxyUsername;
+        return this;
+    }
+
+    public String getHttpProxyPassword()
+    {
+        return httpProxyPassword;
+    }
+
+    @Config("s3.http-proxy.password")
+    @ConfigSecuritySensitive
+    public S3FileSystemConfig setHttpProxyPassword(String httpProxyPassword)
+    {
+        this.httpProxyPassword = httpProxyPassword;
+        return this;
+    }
+
+    public boolean getHttpProxyPreemptiveBasicProxyAuth()
+    {
+        return preemptiveBasicProxyAuth;
+    }
+
+    @Config("s3.http-proxy.preemptive-basic-auth")
+    public S3FileSystemConfig setHttpProxyPreemptiveBasicProxyAuth(boolean preemptiveBasicProxyAuth)
+    {
+        this.preemptiveBasicProxyAuth = preemptiveBasicProxyAuth;
+        return this;
+    }
+
+    public Set<String> getNonProxyHosts()
+    {
+        return nonProxyHosts;
+    }
+
+    @Config("s3.http-proxy.non-proxy-hosts")
+    public S3FileSystemConfig setNonProxyHosts(String nonProxyHosts)
+    {
+        this.nonProxyHosts = ImmutableSet.copyOf(Splitter.on(',').omitEmptyStrings().trimResults().split(nullToEmpty(nonProxyHosts)));
         return this;
     }
 }
--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemFactory.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemFactory.java
@@ -109,6 +109,10 @@ public final class S3FileSystemFactory
                     config.getHttpProxy()));
             httpClient.proxyConfiguration(ProxyConfiguration.builder()
                     .endpoint(endpoint)
+                    .username(config.getHttpProxyUsername())
+                    .password(config.getHttpProxyPassword())
+                    .nonProxyHosts(config.getNonProxyHosts())
+                    .preemptiveBasicAuthenticationEnabled(config.getHttpProxyPreemptiveBasicProxyAuth())
                     .build());
         }
 
