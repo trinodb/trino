6ce4e83aaf545e959fdcde7e9f81f12591805eed
chenjian2664 <chenjian2664@gmail.com>
2024-06-04 18:28:01 +0800

    Add non-null check in MongoPageSink

--- plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoPageSink.java
+++ plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoPageSink.java
@@ -98,9 +98,9 @@ public class MongoPageSink
             Optional<String> pageSinkIdColumnName,
             ConnectorPageSinkId pageSinkId)
     {
-        this.mongoSession = mongoSession;
-        this.remoteTableName = remoteTableName;
-        this.columns = columns;
+        this.mongoSession = requireNonNull(mongoSession, "mongoSession is null");
+        this.remoteTableName = requireNonNull(remoteTableName, "remoteTableName is null");
+        this.columns = ImmutableList.copyOf(requireNonNull(columns, "columns is null"));
         this.implicitPrefix = requireNonNull(implicitPrefix, "implicitPrefix is null");
         this.pageSinkIdColumnName = requireNonNull(pageSinkIdColumnName, "pageSinkIdColumnName is null");
         this.pageSinkId = requireNonNull(pageSinkId, "pageSinkId is null");
