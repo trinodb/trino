beb78358ad7c03921dacd1d8a75bc8b3d5bf122a
Yuya Ebihara <ebyhry@gmail.com>
2024-06-25 13:29:48 +0900

    Extend BaseSystemTable in Iceberg history table

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/HistoryTable.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/HistoryTable.java
@@ -14,36 +14,26 @@
 package io.trino.plugin.iceberg;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
+import io.trino.plugin.iceberg.util.PageListBuilder;
 import io.trino.spi.connector.ColumnMetadata;
-import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorTableMetadata;
-import io.trino.spi.connector.ConnectorTransactionHandle;
-import io.trino.spi.connector.InMemoryRecordSet;
-import io.trino.spi.connector.RecordCursor;
 import io.trino.spi.connector.SchemaTableName;
-import io.trino.spi.connector.SystemTable;
-import io.trino.spi.predicate.TupleDomain;
 import io.trino.spi.type.TimeZoneKey;
-import org.apache.iceberg.Snapshot;
 import org.apache.iceberg.Table;
-import org.apache.iceberg.util.SnapshotUtil;
 
 import java.util.List;
-import java.util.Set;
+import java.util.concurrent.ExecutorService;
 
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.DateTimeEncoding.packDateTimeWithZone;
 import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MILLIS;
+import static io.trino.spi.type.Timestamps.MICROSECONDS_PER_MILLISECOND;
 import static java.util.Objects.requireNonNull;
+import static org.apache.iceberg.MetadataTableType.HISTORY;
 
 public class HistoryTable
-        implements SystemTable
+        extends BaseSystemTable
 {
-    private final ConnectorTableMetadata tableMetadata;
-    private final Table icebergTable;
-
     private static final List<ColumnMetadata> COLUMNS = ImmutableList.<ColumnMetadata>builder()
             .add(new ColumnMetadata("made_current_at", TIMESTAMP_TZ_MILLIS))
             .add(new ColumnMetadata("snapshot_id", BIGINT))
@@ -51,41 +41,23 @@ public class HistoryTable
             .add(new ColumnMetadata("is_current_ancestor", BOOLEAN))
             .build();
 
-    public HistoryTable(SchemaTableName tableName, Table icebergTable)
+    public HistoryTable(SchemaTableName tableName, Table icebergTable, ExecutorService executor)
     {
-        tableMetadata = new ConnectorTableMetadata(requireNonNull(tableName, "tableName is null"), COLUMNS);
-        this.icebergTable = requireNonNull(icebergTable, "icebergTable is null");
+        super(
+                requireNonNull(icebergTable, "icebergTable is null"),
+                new ConnectorTableMetadata(requireNonNull(tableName, "tableName is null"), COLUMNS),
+                HISTORY,
+                executor);
     }
 
     @Override
-    public Distribution getDistribution()
+    protected void addRow(PageListBuilder pagesBuilder, Row row, TimeZoneKey timeZoneKey)
     {
-        return Distribution.SINGLE_COORDINATOR;
-    }
-
-    @Override
-    public ConnectorTableMetadata getTableMetadata()
-    {
-        return tableMetadata;
-    }
-
-    @Override
-    public RecordCursor cursor(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
-    {
-        InMemoryRecordSet.Builder table = InMemoryRecordSet.builder(COLUMNS);
-
-        Set<Long> ancestorIds = ImmutableSet.copyOf(SnapshotUtil.currentAncestorIds(icebergTable));
-        TimeZoneKey timeZoneKey = session.getTimeZoneKey();
-        for (Snapshot snapshot : icebergTable.snapshots()) {
-            long snapshotId = snapshot.snapshotId();
-
-            table.addRow(
-                    packDateTimeWithZone(snapshot.timestampMillis(), timeZoneKey),
-                    snapshotId,
-                    snapshot.parentId(),
-                    ancestorIds.contains(snapshotId));
-        }
-
-        return table.build().cursor();
+        pagesBuilder.beginRow();
+        pagesBuilder.appendTimestampTzMillis(row.get("made_current_at", Long.class) / MICROSECONDS_PER_MILLISECOND, timeZoneKey);
+        pagesBuilder.appendBigint(row.get("snapshot_id", Long.class));
+        pagesBuilder.appendBigint(row.get("parent_id", Long.class));
+        pagesBuilder.appendBoolean(row.get("is_current_ancestor", Boolean.class));
+        pagesBuilder.endRow();
     }
 }
