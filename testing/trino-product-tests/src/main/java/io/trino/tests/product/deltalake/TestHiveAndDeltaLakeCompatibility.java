38a4f894bbd1d7a8019dc19d239b9e6cd0fda5a3
Yuya Ebihara <ebyhry@gmail.com>
2024-06-25 13:59:49 +0900

    Migrate Hive view in Delta to integration tests

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestHiveAndDeltaLakeCompatibility.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestHiveAndDeltaLakeCompatibility.java
@@ -19,14 +19,12 @@ import io.trino.tests.product.hudi.TestHudiHiveViewsCompatibility;
 import io.trino.tests.product.iceberg.TestIcebergHiveViewsCompatibility;
 import org.testng.annotations.Test;
 
-import static io.trino.tempto.assertions.QueryAssert.Row.row;
 import static io.trino.testing.TestingNames.randomNameSuffix;
 import static io.trino.tests.product.TestGroups.DELTA_LAKE_OSS;
 import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
 import static io.trino.tests.product.utils.QueryExecutors.onTrino;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
@@ -42,28 +40,6 @@ public class TestHiveAndDeltaLakeCompatibility
     public void setUp()
     {
         bucketName = requireNonNull(System.getenv("S3_BUCKET"), "Environment variable not set: S3_BUCKET");
-    }
-
-    @Test(groups = {DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
-    public void testInformationSchemaColumnsOnPresenceOfHiveView()
-    {
-        // use dedicated schema so we control the number and shape of tables
-        String schemaName = "test_redirect_to_delta_information_schema_columns_schema_" + randomNameSuffix();
-        onTrino().executeQuery("CREATE SCHEMA IF NOT EXISTS hive." + schemaName + " WITH (location = 's3://" + bucketName + "/test_redirect_to_delta')");
-
-        String hiveViewName = "delta_schema_columns_hive_view_" + randomNameSuffix();
-        String hiveViewQualifiedName = format("hive.%s.%s", schemaName, hiveViewName);
-
-        onTrino().executeQuery("CREATE VIEW " + hiveViewQualifiedName + " AS SELECT 1 AS col_one");
-
-        try {
-            assertThat(onTrino().executeQuery(format("SELECT table_name FROM delta.information_schema.columns WHERE table_schema = '%s'", schemaName)))
-                    .containsOnly(row(hiveViewName));
-        }
-        finally {
-            onTrino().executeQuery("DROP VIEW IF EXISTS " + hiveViewQualifiedName);
-            onTrino().executeQuery("DROP SCHEMA IF EXISTS " + schemaName);
-        }
     }
 
     @Test(groups = {DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
