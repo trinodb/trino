534143e54b2c5cdf4d8c2b0e58853f72f8d9b670
Belén Pruvost <belen@dune.com>
2024-07-17 10:17:49 +0200

    Implement Query Info Endpoint Pruned Parameter

---
+++ core/trino-main/src/main/resources/webapp/src/references.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {PageTitle} from "./components/PageTitle";
+import {ReferenceDetail} from "./components/ReferenceDetail";
+
+ReactDOM.render(
+    <PageTitle title="References" />,
+    document.getElementById('title')
+);
+
+ReactDOM.render(
+    <ReferenceDetail />,
+    document.getElementById('references')
+);
