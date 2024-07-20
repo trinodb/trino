e4a8f062999e47f154c48a0c73a16fc07fea17a1
David Phillips <david@acz.org>
2024-07-20 09:29:06 -0700

    Move metastore procedure to Hive procedure package

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/procedure/FlushMetadataCacheProcedure.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/procedure/FlushMetadataCacheProcedure.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.metastore.procedure;
+package io.trino.plugin.hive.procedure;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
