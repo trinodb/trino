a5d4f481ef39d40a84bafb1f7e3ad5c4a3bc4ccd
David Phillips <david@acz.org>
2024-07-20 09:28:19 -0700

    Move utility class to Glue v1 package

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/v1/converter/Memoizers.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/glue/v1/converter/Memoizers.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive.metastore.util;
+package io.trino.plugin.hive.metastore.glue.v1.converter;
 
 import java.util.Objects;
 import java.util.function.BiFunction;
