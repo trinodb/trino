c3ee39a64df1923df41ac2c95e8ea3e10b45fe4b
Yuya Ebihara <ebyhry@gmail.com>
2024-07-22 20:48:21 +0900

    Fix BaseConnectorTest.testMultipleRangesPredicate

--- testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
@@ -538,7 +538,7 @@ public abstract class BaseConnectorTest
         assertQuery("" +
                 "SELECT orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment " +
                 "FROM orders " +
-                "WHERE orderkey BETWEEN 10 AND 50");
+                "WHERE orderkey BETWEEN 10 AND 50 OR orderkey BETWEEN 100 AND 150");
     }
 
     @Test
