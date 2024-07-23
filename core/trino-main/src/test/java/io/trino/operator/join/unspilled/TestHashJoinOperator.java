b80be861d3c62877f02f204222740dafc5c360a3
Karol Sobczak <napewnotrafi@gmail.com>
2024-07-23 15:59:08 +0200

    Remove BasicWorkProcessorOperatorAdapter
    This commit simplifies hierarchy of WorkProcessorOperator
    adapters. It also simplifies join implementation.

--- core/trino-main/src/test/java/io/trino/operator/join/unspilled/TestHashJoinOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/join/unspilled/TestHashJoinOperator.java
@@ -36,6 +36,7 @@ import io.trino.operator.ProcessorContext;
 import io.trino.operator.TaskContext;
 import io.trino.operator.WorkProcessor;
 import io.trino.operator.WorkProcessorOperator;
+import io.trino.operator.WorkProcessorOperatorAdapter;
 import io.trino.operator.WorkProcessorOperatorFactory;
 import io.trino.operator.join.InternalJoinFilterFunction;
 import io.trino.operator.join.JoinBridgeManager;
@@ -1553,7 +1554,8 @@ public class TestHashJoinOperator
         List<Type> probeTypes = ImmutableList.of(VARCHAR, INTEGER, INTEGER);
         RowPagesBuilder probePages = rowPagesBuilder(false, Ints.asList(0), probeTypes);
         probePages.row("a", 1L, 2L);
-        WorkProcessorOperatorFactory joinOperatorFactory = (WorkProcessorOperatorFactory) innerJoinOperatorFactory(lookupSourceFactory, probePages, false);
+        WorkProcessorOperatorFactory joinOperatorFactory = ((WorkProcessorOperatorAdapter.Factory) innerJoinOperatorFactory(lookupSourceFactory, probePages, false))
+                .getWorkProcessorOperatorFactory();
 
         // build drivers and operators
         instantiateBuildDrivers(buildSideSetup, taskContext);
