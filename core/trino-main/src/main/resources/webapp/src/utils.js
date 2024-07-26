0ce6c291773a9eb38057c517287f678c95329e78
Anil Gupta Somisetty <anilsomisetty118@gmail.com>
2024-07-26 20:44:39 +0530

    Render NaN on UI as n/a

--- core/trino-main/src/main/resources/webapp/src/utils.js
+++ core/trino-main/src/main/resources/webapp/src/utils.js
@@ -340,6 +340,9 @@ export function computeRate(count: number, ms: number): number {
 }
 
 export function precisionRound(n: number): string {
+    if (n === undefined){
+        return "n/a";
+    }
     if (n < 10) {
         return n.toFixed(2);
     }
