09d706203988f8c03a4add5ec3d4a10fdbdc6254
David Phillips <david@acz.org>
2024-07-31 10:12:44 -0700

    Move ForHiveMetastore annotation to thrift package

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/ForHiveMetastore.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/metastore/thrift/ForHiveMetastore.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.hive;
+package io.trino.plugin.hive.metastore.thrift;
 
 import com.google.inject.BindingAnnotation;
 
