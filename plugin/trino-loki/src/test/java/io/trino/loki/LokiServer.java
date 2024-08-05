c59912136657347ccbe913e0990e63c53218d29e
Janos <janos.gub@grafana.com>
2024-08-05 12:15:59 +0200

    Migrating to docker compose

--- plugin/trino-loki/src/test/java/io/trino/loki/LokiServer.java
+++ plugin/trino-loki/src/test/java/io/trino/loki/LokiServer.java
@@ -14,10 +14,11 @@
 package io.trino.loki;
 
 import io.trino.testing.ResourcePresence;
-import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.Closeable;
+import java.io.File;
 import java.net.URI;
 import java.time.Duration;
 
@@ -33,7 +34,7 @@ public class LokiServer
     public static final String PASSWORD = "password";
     public static final String LOKI_QUERY_API = "/loki/api/v1/query";
 
-    private final GenericContainer<?> dockerContainer;
+    private final DockerComposeContainer dockerCompose;
 
     public LokiServer()
     {
@@ -42,35 +43,28 @@ public class LokiServer
 
     public LokiServer(String version, boolean enableBasicAuth)
     {
-        this.dockerContainer = new GenericContainer<>("grafana/loki:" + version)
-                .withExposedPorts(LOKI_PORT);
+        this.dockerCompose = new DockerComposeContainer(new File("plugin/trino-loki/src/test/resources/compose.yaml"));
+
                 //.waitingFor(Wait.forHttp(LOKI_QUERY_API).forResponsePredicate(response -> response.contains("\"status\"")))
                 //.withStartupTimeout(Duration.ofSeconds(360));
-        // Basic authentication was introduced in v2.24.0
-        if (enableBasicAuth) {
-            this.dockerContainer
-                    .withCommand("--config.file=/etc/prometheus/prometheus.yml", "--web.config.file=/etc/prometheus/web.yml")
-                    .withCopyFileToContainer(forClasspathResource("web.yml"), "/etc/prometheus/web.yml")
-                    .waitingFor(Wait.forHttp(LOKI_QUERY_API).forResponsePredicate(response -> response.contains("\"values\"")).withBasicCredentials(USER, PASSWORD))
-                    .withStartupTimeout(Duration.ofSeconds(360));
-        }
-        this.dockerContainer.start();
+
+        this.dockerCompose.start();
     }
 
     public URI getUri()
     {
-        return URI.create("http://" + dockerContainer.getHost() + ":" + dockerContainer.getMappedPort(LOKI_PORT) + "/");
+        return URI.create("http://" + dockerCompose.getServiceHost("loki", LOKI_PORT) + ":" + LOKI_PORT + "/");
     }
 
     @Override
     public void close()
     {
-        dockerContainer.close();
+        dockerCompose.close();
     }
 
     @ResourcePresence
     public boolean isRunning()
     {
-        return dockerContainer.getContainerId() != null;
+        return dockerCompose.getContainerByServiceName("loki").isPresent();
     }
 }
