1f7bb5f58c7ecbcba07e52bd058d38435ef959c8
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-08-05 14:11:33 +0200

    Enable HTTP/2 for internal communication by default
    - Wrap TimeoutHandler with a check
    
    - Improve error handling
    
    - Tune internal HTTP2 settings
    
    - Tweak some HTTP2 settings and test new client

--- core/trino-main/src/main/java/io/trino/server/DisconnectionAwareAsyncResponse.java
+++ core/trino-main/src/main/java/io/trino/server/DisconnectionAwareAsyncResponse.java
@@ -30,9 +30,8 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
-import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Verify.verify;
 import static io.airlift.jaxrs.AsyncResponseHandler.bindAsyncResponse;
 import static java.util.Objects.requireNonNull;
@@ -41,11 +40,10 @@ public class DisconnectionAwareAsyncResponse
         implements AsyncResponse
 {
     // Guards against calling AsyncResponse methods when client is no longer interested in consuming a response
-    private final AtomicBoolean clientDisconnected = new AtomicBoolean();
+    private final AtomicBoolean terminated = new AtomicBoolean();
 
     private final AsyncContext asyncContext;
     private final AsyncResponse delegate;
-    private final AtomicReference<ListenableFuture<?>> cancellableFuture = new AtomicReference<>(null);
 
     public DisconnectionAwareAsyncResponse(@Context HttpServletRequest request, AsyncResponse delegate)
     {
@@ -62,19 +60,16 @@ public class DisconnectionAwareAsyncResponse
             public void onComplete(AsyncEvent event) {}
 
             @Override
-            public void onTimeout(AsyncEvent event) {}
+            public void onTimeout(AsyncEvent event)
+            {
+                terminate();
+            }
 
             @Override
             public void onError(AsyncEvent event)
             {
                 if (wasRequestTerminated(event.getThrowable())) {
-                    if (clientDisconnected.compareAndSet(false, true)) {
-                        asyncContext.complete();
-                        ListenableFuture<?> future = cancellableFuture.getAndSet(null);
-                        if (future != null) {
-                            future.cancel(true);
-                        }
-                    }
+                    terminate();
                 }
             }
 
@@ -83,55 +78,81 @@ public class DisconnectionAwareAsyncResponse
         });
     }
 
-    public DisconnectionAwareAsyncResponse withCancellableFuture(ListenableFuture<?> future)
+    private void terminate()
     {
-        checkState(cancellableFuture.compareAndSet(null, future), "Cancellable future already set");
-        return this;
+        if (terminated.compareAndSet(false, true)) {
+            asyncContext.complete();
+        }
     }
 
     @Override
     public boolean resume(Object response)
     {
-        if (clientDisconnected.get()) {
+        try {
+            return delegate.resume(response);
+        }
+        catch (RuntimeException e) {
+            if (!terminated.get()) {
+                throw e;
+            }
             return true;
         }
-        return delegate.resume(response);
     }
 
     @Override
     public boolean resume(Throwable response)
     {
-        if (clientDisconnected.get()) {
+        try {
+            return delegate.resume(response);
+        }
+        catch (RuntimeException e) {
+            if (!terminated.get()) {
+                throw e;
+            }
             return true;
         }
-        return delegate.resume(response);
     }
 
     @Override
     public boolean cancel()
     {
-        if (clientDisconnected.get()) {
+        try {
+            return delegate.cancel();
+        }
+        catch (RuntimeException e) {
+            if (!terminated.get()) {
+                throw e;
+            }
             return true;
         }
-        return delegate.cancel();
     }
 
     @Override
     public boolean cancel(int retryAfter)
     {
-        if (clientDisconnected.get()) {
+        try {
+            return delegate.cancel(retryAfter);
+        }
+        catch (RuntimeException e) {
+            if (!terminated.get()) {
+                throw e;
+            }
             return true;
         }
-        return delegate.cancel(retryAfter);
     }
 
     @Override
     public boolean cancel(Date retryAfter)
     {
-        if (clientDisconnected.get()) {
+        try {
+            return delegate.cancel(retryAfter);
+        }
+        catch (RuntimeException e) {
+            if (!terminated.get()) {
+                throw e;
+            }
             return true;
         }
-        return delegate.cancel(retryAfter);
     }
 
     @Override
@@ -149,7 +170,7 @@ public class DisconnectionAwareAsyncResponse
     @Override
     public boolean isDone()
     {
-        if (clientDisconnected.get()) {
+        if (wasTerminated()) {
             return true;
         }
         return delegate.isDone();
@@ -191,14 +212,19 @@ public class DisconnectionAwareAsyncResponse
         return delegate.register(callback, callbacks);
     }
 
+    private boolean wasTerminated()
+    {
+        return terminated.get();
+    }
+
     private static boolean wasRequestTerminated(Throwable throwable)
     {
         // Jetty's detected that client disconnected
-        return throwable instanceof IOException ioException && ioException.getMessage().contains("cancel_stream_error");
+        return throwable instanceof IOException ioException && firstNonNull(ioException.getMessage(), "").contains("cancel_stream_error");
     }
 
     public static AsyncResponseHandler bindDisconnectionAwareAsyncResponse(DisconnectionAwareAsyncResponse asyncResponse, ListenableFuture<?> futureResponse, Executor httpResponseExecutor)
     {
-        return bindAsyncResponse(asyncResponse.withCancellableFuture(futureResponse), futureResponse, httpResponseExecutor);
+        return bindAsyncResponse(asyncResponse, futureResponse, httpResponseExecutor);
     }
 }
