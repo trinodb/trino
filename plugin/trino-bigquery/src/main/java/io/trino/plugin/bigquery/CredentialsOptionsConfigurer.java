a076b11b6c40b7df3e4156f62a0e02516768f558
Slawomir Pajak <slawomir.pajak@starburstdata.com>
2024-08-14 13:26:36 +0200

    Fix parent project id handling

--- plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/CredentialsOptionsConfigurer.java
+++ plugin/trino-bigquery/src/main/java/io/trino/plugin/bigquery/CredentialsOptionsConfigurer.java
@@ -31,11 +31,13 @@ public class CredentialsOptionsConfigurer
         implements BigQueryOptionsConfigurer
 {
     private final BigQueryCredentialsSupplier credentialsSupplier;
+    private final Optional<String> configProjectId;
     private final Optional<String> configParentProjectId;
 
     @Inject
     public CredentialsOptionsConfigurer(BigQueryConfig bigQueryConfig, BigQueryCredentialsSupplier credentialsSupplier)
     {
+        this.configProjectId = bigQueryConfig.getProjectId();
         this.configParentProjectId = bigQueryConfig.getParentProjectId();
         this.credentialsSupplier = requireNonNull(credentialsSupplier, "credentialsSupplier is null");
     }
@@ -44,9 +46,11 @@ public class CredentialsOptionsConfigurer
     public BigQueryOptions.Builder configure(BigQueryOptions.Builder builder, ConnectorSession session)
     {
         Optional<Credentials> credentials = credentialsSupplier.getCredentials(session);
-        String parentProjectId = resolveProjectId(configParentProjectId, credentials);
+        String projectId = resolveProjectId(configProjectId, credentials);
         credentials.ifPresent(builder::setCredentials);
-        builder.setProjectId(parentProjectId);
+        builder.setProjectId(projectId);
+        // Quota project id is different name for parent project id, both indicates project used for quota and billing purposes.
+        configParentProjectId.ifPresent(builder::setQuotaProjectId);
         return builder;
     }
 
