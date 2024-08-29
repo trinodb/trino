1fcb194f67c4525cedfa2b6dbf52572e61030c70
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-08-29 13:17:42 +0200

    Update localstack to 3.7.0
    It added conditional write support

--- lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/TestS3FileSystemLocalStack.java
+++ lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/TestS3FileSystemLocalStack.java
@@ -32,7 +32,7 @@ public class TestS3FileSystemLocalStack
     private static final String BUCKET = "test-bucket";
 
     @Container
-    private static final LocalStackContainer LOCALSTACK = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.3.0"))
+    private static final LocalStackContainer LOCALSTACK = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.7.0"))
             .withServices(Service.S3);
 
     @Override
@@ -41,18 +41,6 @@ public class TestS3FileSystemLocalStack
         try (S3Client s3Client = createS3Client()) {
             s3Client.createBucket(builder -> builder.bucket(BUCKET).build());
         }
-    }
-
-    @Override
-    protected boolean isCreateExclusive()
-    {
-        return false; // not supported by localstack
-    }
-
-    @Override
-    protected boolean supportsCreateExclusive()
-    {
-        return false; // not supported by localstack
     }
 
     @Override
@@ -80,7 +68,6 @@ public class TestS3FileSystemLocalStack
                 .setAwsSecretKey(LOCALSTACK.getSecretKey())
                 .setEndpoint(LOCALSTACK.getEndpointOverride(Service.S3).toString())
                 .setRegion(LOCALSTACK.getRegion())
-                .setStreamingPartSize(DataSize.valueOf("5.5MB"))
-                .setSupportsExclusiveCreate(false));
+                .setStreamingPartSize(DataSize.valueOf("5.5MB")));
     }
 }
