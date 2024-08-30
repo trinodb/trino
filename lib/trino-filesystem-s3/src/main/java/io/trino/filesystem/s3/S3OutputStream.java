d9cb7465ac7ca2adb355b27c2256f686d42d7fda
Marcin Rusek <marcin.rusek@starburstdata.com>
2024-08-30 15:42:04 +0200

    Extract S3 sse request modifications to separate class

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3OutputStream.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3OutputStream.java
@@ -14,7 +14,6 @@
 package io.trino.filesystem.s3;
 
 import io.trino.filesystem.encryption.EncryptionKey;
-import io.trino.filesystem.s3.S3FileSystemConfig.S3SseType;
 import io.trino.memory.context.AggregatedMemoryContext;
 import io.trino.memory.context.LocalMemoryContext;
 import software.amazon.awssdk.core.exception.SdkException;
@@ -49,6 +48,7 @@ import static io.trino.filesystem.s3.S3FileSystemConfig.ObjectCannedAcl.getCanne
 import static io.trino.filesystem.s3.S3FileSystemConfig.S3SseType.NONE;
 import static io.trino.filesystem.s3.S3SseCUtils.encoded;
 import static io.trino.filesystem.s3.S3SseCUtils.md5Checksum;
+import static io.trino.filesystem.s3.S3SseRequestConfigurator.addEncryptionSettings;
 import static java.lang.Math.clamp;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
@@ -56,8 +56,6 @@ import static java.lang.System.arraycopy;
 import static java.net.HttpURLConnection.HTTP_PRECON_FAILED;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
-import static software.amazon.awssdk.services.s3.model.ServerSideEncryption.AES256;
-import static software.amazon.awssdk.services.s3.model.ServerSideEncryption.AWS_KMS;
 
 final class S3OutputStream
         extends OutputStream
@@ -70,8 +68,6 @@ final class S3OutputStream
     private final S3Context context;
     private final int partSize;
     private final RequestPayer requestPayer;
-    private final S3SseType sseType;
-    private final Optional<String> sseKmsKeyId;
     private final ObjectCannedACL cannedAcl;
     private final boolean exclusiveCreate;
     private final Optional<EncryptionKey> key;
@@ -101,12 +97,10 @@ final class S3OutputStream
         this.context = requireNonNull(context, "context is null");
         this.partSize = context.partSize();
         this.requestPayer = context.requestPayer();
-        this.sseType = context.s3SseContext().sseType();
-        this.sseKmsKeyId = context.s3SseContext().sseKmsKeyId();
         this.cannedAcl = getCannedAcl(context.cannedAcl());
         this.key = requireNonNull(key, "key is null");
 
-        verify(key.isEmpty() || sseType == NONE, "Encryption key cannot be used with SSE configuration");
+        verify(key.isEmpty() || context.s3SseContext().sseType() == NONE, "Encryption key cannot be used with SSE configuration");
     }
 
     @SuppressWarnings("NumericCastThatLosesPrecision")
@@ -230,11 +224,7 @@ final class S3OutputStream
                             builder.sseCustomerAlgorithm(encryption.algorithm());
                             builder.sseCustomerKeyMD5(md5Checksum(encryption));
                         });
-                        switch (sseType) {
-                            case NONE -> { /* ignored */ }
-                            case S3 -> builder.serverSideEncryption(AES256);
-                            case KMS -> sseKmsKeyId.ifPresent(builder.serverSideEncryption(AWS_KMS)::ssekmsKeyId);
-                        }
+                        addEncryptionSettings(builder, context.s3SseContext());
                     })
                     .build();
 
@@ -320,11 +310,7 @@ final class S3OutputStream
                             builder.sseCustomerAlgorithm(encryption.algorithm());
                             builder.sseCustomerKeyMD5(md5Checksum(encryption));
                         });
-                        switch (sseType) {
-                            case NONE -> { /* ignored */ }
-                            case S3 -> builder.serverSideEncryption(AES256);
-                            case KMS -> sseKmsKeyId.ifPresent(builder.serverSideEncryption(AWS_KMS)::ssekmsKeyId);
-                        }
+                        addEncryptionSettings(builder, context.s3SseContext());
                     })
                     .build();
 
