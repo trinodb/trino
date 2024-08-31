d2837212b2aa21c1b77f5392b12202f436fc9e46
Yuya Ebihara <ebyhry@gmail.com>
2024-08-31 22:49:14 +0900

    Add a new query runner main with spark3-delta in Delta Lake

--- plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/DeltaLakeQueryRunner.java
+++ plugin/trino-delta-lake/src/test/java/io/trino/plugin/deltalake/DeltaLakeQueryRunner.java
@@ -223,6 +223,29 @@ public final class DeltaLakeQueryRunner
         }
     }
 
+    public static final class DeltaLakeSparkQueryRunnerMain
+    {
+        private DeltaLakeSparkQueryRunnerMain() {}
+
+        public static void main(String[] args)
+                throws Exception
+        {
+            String bucketName = "test-bucket";
+            SparkDeltaLake sparkDeltaLake = new SparkDeltaLake(bucketName);
+
+            QueryRunner queryRunner = builder()
+                    .addCoordinatorProperty("http-server.http.port", "8080")
+                    .addMetastoreProperties(sparkDeltaLake.hiveHadoop())
+                    .addS3Properties(sparkDeltaLake.minio(), bucketName)
+                    .addDeltaProperty("delta.enable-non-concurrent-writes", "true")
+                    .build();
+
+            Logger log = Logger.get(DeltaLakeSparkQueryRunnerMain.class);
+            log.info("======== SERVER STARTED ========");
+            log.info("\n====\n%s\n====", queryRunner.getCoordinator().getBaseUrl());
+        }
+    }
+
     public static final class S3DeltaLakeQueryRunnerMain
     {
         private S3DeltaLakeQueryRunnerMain() {}
