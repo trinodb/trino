e25deb6d468a7864f71f11819e837ca37d297be2
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-09-04 18:23:34 +0200

    Format and check js/jsx pages at root of trino-web-ui

--- core/trino-web-ui/src/main/resources/webapp/src/plan.jsx
+++ core/trino-web-ui/src/main/resources/webapp/src/plan.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {LivePlan} from "./components/LivePlan";
-import {PageTitle} from "./components/PageTitle";
-import {getFirstParameter} from "./utils";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { LivePlan } from './components/LivePlan'
+import { PageTitle } from './components/PageTitle'
+import { getFirstParameter } from './utils'
+
+ReactDOM.render(<PageTitle title="Query Details" />, document.getElementById('title'))
 
 ReactDOM.render(
-    <PageTitle title="Query Details" />,
-    document.getElementById('title')
-);
-
-ReactDOM.render(
-    <LivePlan queryId={getFirstParameter(window.location.search)} isEmbedded={false}/>,
+    <LivePlan queryId={getFirstParameter(window.location.search)} isEmbedded={false} />,
     document.getElementById('live-plan-container')
-);
+)
--- core/trino-web-ui/src/main/resources/webapp/src/timeline.jsx
+++ core/trino-web-ui/src/main/resources/webapp/src/timeline.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {PageTitle} from "./components/PageTitle";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { PageTitle } from './components/PageTitle'
 
-ReactDOM.render(
-    <PageTitle title="Query Details" />,
-    document.getElementById('title')
-);
+ReactDOM.render(<PageTitle title="Query Details" />, document.getElementById('title'))
