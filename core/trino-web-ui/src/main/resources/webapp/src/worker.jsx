022427143696576a06efa5af68ab5ca280e4d81c
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-09-04 18:23:34 +0200

    Format and check js/jsx pages at root of trino-web-ui

--- core/trino-web-ui/src/main/resources/webapp/src/worker.jsx
+++ core/trino-web-ui/src/main/resources/webapp/src/worker.jsx
@@ -1,20 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {WorkerStatus} from "./components/WorkerStatus";
-import {WorkerThreadList} from "./components/WorkerThreadList";
-import {PageTitle} from "./components/PageTitle";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { WorkerStatus } from './components/WorkerStatus'
+import { WorkerThreadList } from './components/WorkerThreadList'
+import { PageTitle } from './components/PageTitle'
 
-ReactDOM.render(
-    <PageTitle title="Worker Status" />,
-    document.getElementById('title')
-);
+ReactDOM.render(<PageTitle title="Worker Status" />, document.getElementById('title'))
 
-ReactDOM.render(
-    <WorkerStatus />,
-    document.getElementById('worker-status')
-);
+ReactDOM.render(<WorkerStatus />, document.getElementById('worker-status'))
 
-ReactDOM.render(
-    <WorkerThreadList />,
-    document.getElementById('worker-threads')
-);
+ReactDOM.render(<WorkerThreadList />, document.getElementById('worker-threads'))
