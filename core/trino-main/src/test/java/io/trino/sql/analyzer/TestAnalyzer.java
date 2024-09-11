3885f0d9bac99b7a1871e1c6836623e3d95a7dac
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-09-12 00:23:26 +0200

    Fix error message on row dereference

--- core/trino-main/src/test/java/io/trino/sql/analyzer/TestAnalyzer.java
+++ core/trino-main/src/test/java/io/trino/sql/analyzer/TestAnalyzer.java
@@ -385,6 +385,14 @@ public class TestAnalyzer
     }
 
     @Test
+    public void testRowReferencesUnknownField()
+    {
+        assertFails("SELECT row('a', 'b', 'c').field")
+                .hasErrorCode(INVALID_COLUMN_REFERENCE)
+                .hasMessage("line 1:8: Column reference 'field' is invalid");
+    }
+
+    @Test
     public void testSelectAllColumns()
     {
         // wildcard without FROM
