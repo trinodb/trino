6bfce8416ea25f32cbd4f621aeb7f091fbc2060d
Yuya Ebihara <ebyhry@gmail.com>
2024-09-14 18:38:45 +0900

    Add a space in testExplain

--- core/trino-parser/src/test/java/io/trino/sql/parser/TestSqlParser.java
+++ core/trino-parser/src/test/java/io/trino/sql/parser/TestSqlParser.java
@@ -3955,7 +3955,7 @@ public class TestSqlParser
                 new Explain(
                         simpleQuery(selectList(new AllColumns()), table(QualifiedName.of("t"))),
                         ImmutableList.of(
-                                new ExplainType(location(1, 1),ExplainType.Type.LOGICAL),
+                                new ExplainType(location(1, 1), ExplainType.Type.LOGICAL),
                                 new ExplainFormat(location(1, 1), ExplainFormat.Type.TEXT))));
 
         assertStatementIsInvalid("EXPLAIN VERBOSE SELECT * FROM t")
