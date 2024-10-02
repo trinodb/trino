ee0822cbcebf465363522c3c24baabddca70f3c1
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-10-02 23:01:58 +0200

    Migrate segment pruner tests to memory file system

--- plugin/trino-spooling-filesystem/src/test/java/io/trino/spooling/filesystem/TestFileSystemSegmentPruner.java
+++ plugin/trino-spooling-filesystem/src/test/java/io/trino/spooling/filesystem/TestFileSystemSegmentPruner.java
@@ -14,21 +14,13 @@
 package io.trino.spooling.filesystem;
 
 import com.google.common.collect.ImmutableList;
-import io.airlift.units.DataSize;
 import io.trino.filesystem.FileEntry;
 import io.trino.filesystem.FileIterator;
 import io.trino.filesystem.Location;
 import io.trino.filesystem.TrinoFileSystem;
-import io.trino.filesystem.TrinoFileSystemFactory;
-import io.trino.filesystem.s3.S3FileSystemConfig;
-import io.trino.filesystem.s3.S3FileSystemFactory;
-import io.trino.filesystem.s3.S3FileSystemStats;
+import io.trino.filesystem.memory.MemoryFileSystem;
 import io.trino.spi.QueryId;
 import io.trino.spi.protocol.SpoolingContext;
-import io.trino.spi.security.ConnectorIdentity;
-import io.trino.testing.containers.Minio;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -37,13 +29,10 @@ import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static io.opentelemetry.api.OpenTelemetry.noop;
-import static io.trino.testing.containers.Minio.MINIO_REGION;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.time.temporal.ChronoUnit.MILLIS;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -52,40 +41,27 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @TestInstance(PER_CLASS)
 class TestFileSystemSegmentPruner
 {
-    private Minio minio;
-    private static final String BUCKET_NAME = "segments" + UUID.randomUUID().toString().replace("-", "");
-    private static final Location LOCATION = Location.of("s3://" + BUCKET_NAME + "/");
+    private static final String TEST_LOCATION = "memory://";
 
-    @BeforeAll
-    public void setup()
-    {
-        minio = Minio.builder().build();
-        minio.start();
-        minio.createBucket(BUCKET_NAME);
-    }
-
-    @AfterAll
-    public void teardown()
-    {
-        minio.stop();
-    }
+    private static final FileSystemSpoolingConfig SPOOLING_CONFIG = new FileSystemSpoolingConfig()
+            .setLocation(TEST_LOCATION);
 
     @Test
     public void shouldPruneExpiredSegments()
     {
+        MemoryFileSystem fileSystem = new MemoryFileSystem();
         try (ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor()) {
-            TrinoFileSystemFactory fileSystemFactory = getFileSystemFactory();
-            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(getSpoolingConfig(), fileSystemFactory, executorService);
+            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(SPOOLING_CONFIG, _ -> fileSystem, executorService);
 
             Instant now = Instant.now();
             QueryId queryId = QueryId.valueOf("prune_expired");
 
-            Location _ = writeNewDummySegment(fileSystemFactory, queryId, now.minusSeconds(1));
-            Location nonExpiredSegment = writeNewDummySegment(fileSystemFactory, queryId, now.plusSeconds(1));
+            Location _ = createNewDummySegment(fileSystem, queryId, now.minusSeconds(1));
+            Location nonExpiredSegment = createNewDummySegment(fileSystem, queryId, now.plusSeconds(1));
 
             pruner.pruneExpiredBefore(now.truncatedTo(MILLIS));
 
-            List<Location> files = listFiles(fileSystemFactory, queryId);
+            List<Location> files = listFiles(fileSystem, queryId);
             assertThat(files)
                     .hasSize(1)
                     .containsOnly(nonExpiredSegment);
@@ -95,20 +71,20 @@ class TestFileSystemSegmentPruner
     @Test
     public void shouldNotPruneLiveSegments()
     {
+        MemoryFileSystem fileSystem = new MemoryFileSystem();
         try (ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor()) {
-            TrinoFileSystemFactory fileSystemFactory = getFileSystemFactory();
-            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(getSpoolingConfig(), fileSystemFactory, executorService);
+            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(SPOOLING_CONFIG, _ -> fileSystem, executorService);
 
             Instant now = Instant.now();
 
             QueryId queryId = QueryId.valueOf("prune_live");
 
-            Location _ = writeNewDummySegment(fileSystemFactory, queryId, now.plusSeconds(1));
-            Location _ = writeNewDummySegment(fileSystemFactory, queryId, now.plusSeconds(2));
+            Location _ = createNewDummySegment(fileSystem, queryId, now.plusSeconds(1));
+            Location _ = createNewDummySegment(fileSystem, queryId, now.plusSeconds(2));
 
             pruner.pruneExpiredBefore(now.truncatedTo(MILLIS));
 
-            List<Location> files = listFiles(fileSystemFactory, queryId);
+            List<Location> files = listFiles(fileSystem, queryId);
             assertThat(files)
                     .hasSize(2);
         }
@@ -117,57 +93,46 @@ class TestFileSystemSegmentPruner
     @Test
     public void shouldNotPruneSegmentsIfNotStrictlyBeforeExpiration()
     {
+        TrinoFileSystem memoryFileSystem = new MemoryFileSystem();
         try (ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor()) {
-            TrinoFileSystemFactory fileSystemFactory = getFileSystemFactory();
-            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(getSpoolingConfig(), fileSystemFactory, executorService);
+            FileSystemSegmentPruner pruner = new FileSystemSegmentPruner(SPOOLING_CONFIG, _ -> memoryFileSystem, executorService);
 
             Instant now = Instant.now();
 
             QueryId queryId = QueryId.valueOf("prune_now");
 
-            Location firstSegment = writeNewDummySegment(fileSystemFactory, queryId, now);
-            Location secondSegment = writeNewDummySegment(fileSystemFactory, queryId, now);
+            Location firstSegment = createNewDummySegment(memoryFileSystem, queryId, now);
+            Location secondSegment = createNewDummySegment(memoryFileSystem, queryId, now);
 
             pruner.pruneExpiredBefore(now.truncatedTo(MILLIS));
 
-            List<Location> files = listFiles(fileSystemFactory, queryId);
+            List<Location> files = listFiles(memoryFileSystem, queryId);
             assertThat(files)
                     .hasSize(2)
                     .containsOnly(firstSegment, secondSegment);
         }
     }
 
-    private TrinoFileSystemFactory getFileSystemFactory()
-    {
-        S3FileSystemConfig filesystemConfig = new S3FileSystemConfig()
-                .setEndpoint(minio.getMinioAddress())
-                .setRegion(MINIO_REGION)
-                .setPathStyleAccess(true)
-                .setAwsAccessKey(Minio.MINIO_ACCESS_KEY)
-                .setAwsSecretKey(Minio.MINIO_SECRET_KEY)
-                .setStreamingPartSize(DataSize.valueOf("5.5MB"));
-        return new S3FileSystemFactory(noop(), filesystemConfig, new S3FileSystemStats());
-    }
-
-    private Location writeNewDummySegment(TrinoFileSystemFactory fileSystemFactory, QueryId queryId, Instant ttl)
+    private Location createNewDummySegment(TrinoFileSystem fileSystem, QueryId queryId, Instant ttl)
     {
         SpoolingContext context = new SpoolingContext("encoding", queryId, 100, 1000);
         FileSystemSpooledSegmentHandle handle = FileSystemSpooledSegmentHandle.random(ThreadLocalRandom.current(), context, ttl);
-        try (OutputStream segment = createFileSystem(fileSystemFactory).newOutputFile(LOCATION.appendPath(handle.storageObjectName())).create()) {
-            segment.write("dummy".getBytes(UTF_8));
-            return LOCATION.appendPath(handle.storageObjectName());
+        Location location = Location.of(TEST_LOCATION).appendPath(handle.storageObjectName());
+        try (OutputStream stream = fileSystem.newOutputFile(location).create()) {
+            stream.write("dummy".getBytes(UTF_8));
+            return location;
         }
         catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
 
-    private List<Location> listFiles(TrinoFileSystemFactory fileSystemFactory, QueryId queryId)
+    private List<Location> listFiles(TrinoFileSystem fileSystem, QueryId queryId)
     {
         ImmutableList.Builder<Location> files = ImmutableList.builder();
 
         try {
-            FileIterator iterator = createFileSystem(fileSystemFactory).listFiles(LOCATION);
+            FileIterator iterator = fileSystem.listFiles(Location.of(TEST_LOCATION));
             while (iterator.hasNext()) {
                 FileEntry entry = iterator.next();
                 if (entry.location().fileName().endsWith(queryId.toString())) {
@@ -179,17 +144,5 @@ class TestFileSystemSegmentPruner
         catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-    }
-
-    private TrinoFileSystem createFileSystem(TrinoFileSystemFactory fileSystemFactory)
-    {
-        return fileSystemFactory.create(ConnectorIdentity.ofUser("ignored"));
-    }
-
-    private FileSystemSpoolingConfig getSpoolingConfig()
-    {
-        return new FileSystemSpoolingConfig()
-                .setS3Enabled(true)
-                .setLocation(LOCATION.toString());
     }
 }
