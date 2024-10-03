bf95171e64e594cbe5e132c70db7045d448fac15
Yuya Ebihara <ebyhry@gmail.com>
2024-10-03 20:22:04 +0900

    Migrate optimize product test to integration test in Delta Lake

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeIdentityColumnCompatibility.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeIdentityColumnCompatibility.java
@@ -186,35 +186,6 @@ public class TestDeltaLakeIdentityColumnCompatibility
 
     @Test(groups = {DELTA_LAKE_DATABRICKS_104, PROFILE_SPECIFIC_TESTS})
     @Flaky(issue = DATABRICKS_COMMUNICATION_FAILURE_ISSUE, match = DATABRICKS_COMMUNICATION_FAILURE_MATCH)
-    public void testOptimizeProcedureWithIdentityColumn()
-    {
-        String tableName = "test_optimize_identity_column_" + randomNameSuffix();
-
-        onDelta().executeQuery("CREATE TABLE default." + tableName +
-                "(data INT, col_identity BIGINT GENERATED ALWAYS AS IDENTITY)" +
-                "USING DELTA " +
-                "LOCATION 's3://" + bucketName + "/" + "databricks-compatibility-test-" + tableName + "'");
-        try {
-            onDelta().executeQuery("INSERT INTO default." + tableName + " (data) VALUES 10");
-            onDelta().executeQuery("INSERT INTO default." + tableName + " (data) VALUES 20");
-
-            onTrino().executeQuery("ALTER TABLE delta.default." + tableName + " EXECUTE OPTIMIZE");
-
-            assertThat((String) onDelta().executeQuery("SHOW CREATE TABLE default." + tableName).getOnlyValue())
-                    .contains("col_identity BIGINT GENERATED ALWAYS AS IDENTITY");
-
-            assertThat(onTrino().executeQuery("SELECT * FROM delta.default." + tableName))
-                    .containsOnly(row(10, 1), row(20, 2));
-            assertThat(onDelta().executeQuery("SELECT * FROM default." + tableName))
-                    .containsOnly(row(10, 1), row(20, 2));
-        }
-        finally {
-            dropDeltaTableWithRetry("default." + tableName);
-        }
-    }
-
-    @Test(groups = {DELTA_LAKE_DATABRICKS_104, PROFILE_SPECIFIC_TESTS})
-    @Flaky(issue = DATABRICKS_COMMUNICATION_FAILURE_ISSUE, match = DATABRICKS_COMMUNICATION_FAILURE_MATCH)
     public void testIdentityColumnCheckpointInterval()
     {
         String tableName = "test_identity_column_checkpoint_interval_" + randomNameSuffix();
