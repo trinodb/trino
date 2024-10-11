b543d7f7d43135b4117e71ec11691e293401fbce
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-10-11 16:38:34 +0200

    Simplify returned value

--- client/trino-client/src/main/java/io/trino/client/JsonResultRows.java
+++ client/trino-client/src/main/java/io/trino/client/JsonResultRows.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import static com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE;
@@ -138,32 +137,24 @@ public final class JsonResultRows
 
     public static ResultRows forJsonParser(JsonParser parser, List<Column> columns)
     {
-        return new ResultRows() {
-            @Override
-            public Iterator<List<Object>> iterator()
-            {
-                try {
-                    return new RowWiseIterator(parser, createTypeDecoders(columns));
-                }
-                catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
+        return () -> {
+            try {
+                return new RowWiseIterator(parser, createTypeDecoders(columns));
+            }
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
         };
     }
 
     public static ResultRows forInputStream(InputStream stream, TypeDecoder[] decoders)
     {
-        return new ResultRows() {
-            @Override
-            public Iterator<List<Object>> iterator()
-            {
-                try {
-                    return new RowWiseIterator(stream, decoders);
-                }
-                catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
+        return () -> {
+            try {
+                return new RowWiseIterator(stream, decoders);
+            }
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
         };
     }
