ea71d30530b9ab022618de5cf76f2628bc9d6914
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-10-24 16:50:18 +0200

    Correct duplicated rnn guard

--- core/trino-main/src/main/java/io/trino/operator/OutputSpoolingOperatorFactory.java
+++ core/trino-main/src/main/java/io/trino/operator/OutputSpoolingOperatorFactory.java
@@ -399,7 +399,7 @@ public class OutputSpoolingOperatorFactory
         public OutputSpoolingInfo
         {
             requireNonNull(spoolingWallTime, "spoolingWallTime is null");
-            requireNonNull(spoolingWallTime, "spoolingWallTime is null");
+            requireNonNull(spoolingCpuTime, "spoolingCpuTime is null");
         }
 
         @Override
