66b7a1e1d048354520cdd0cf75763f28ce43cbb6
zhaner08 <zhaner08@hotmail.com>
2024-10-25 17:09:37 -0400

    Pass in requestPayer for ListObjectsV2Request in listObjects method

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystem.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystem.java
@@ -239,6 +239,7 @@ final class S3FileSystem
 
         ListObjectsV2Request request = ListObjectsV2Request.builder()
                 .overrideConfiguration(context::applyCredentialProviderOverride)
+                .requestPayer(requestPayer)
                 .bucket(s3Location.bucket())
                 .prefix(key)
                 .build();
