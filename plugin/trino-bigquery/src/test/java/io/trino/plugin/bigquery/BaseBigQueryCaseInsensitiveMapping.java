2bff3dfa2cb7773f033b780d55e331ca4793fd33
Yuya Ebihara <ebyhry@gmail.com>
2024-11-08 12:38:23 +0900

    Replace unused variable with _

--- plugin/trino-bigquery/src/test/java/io/trino/plugin/bigquery/BaseBigQueryCaseInsensitiveMapping.java
+++ plugin/trino-bigquery/src/test/java/io/trino/plugin/bigquery/BaseBigQueryCaseInsensitiveMapping.java
@@ -109,7 +109,7 @@ public abstract class BaseBigQueryCaseInsensitiveMapping
         String trinoSchema = bigQuerySchema.toLowerCase(ENGLISH);
         String namePrefix = format("%s.Test_Case", bigQuerySchema);
 
-        try (AutoCloseable schema = withSchema(bigQuerySchema);
+        try (AutoCloseable _ = withSchema(bigQuerySchema);
                 TestView view = new TestView(bigQuerySqlExecutor, namePrefix, "SELECT 'a' AS lower_case_name, 'b' AS Mixed_Case_Name, 'c' AS UPPER_CASE_NAME")) {
             String viewName = view.getName().substring(bigQuerySchema.length() + 1).toLowerCase(ENGLISH);
             assertThat(computeActual("SHOW TABLES FROM " + trinoSchema).getOnlyColumn()).contains(viewName);
@@ -216,7 +216,7 @@ public abstract class BaseBigQueryCaseInsensitiveMapping
             throws Exception
     {
         String schemaName = "Test_Create_Case_Sensitive_Clash_" + randomNameSuffix();
-        try (AutoCloseable schema = withSchema(schemaName)) {
+        try (AutoCloseable _ = withSchema(schemaName)) {
             assertQueryFails("CREATE SCHEMA " + schemaName.toLowerCase(ENGLISH), ".*Schema 'bigquery\\.\\Q" + schemaName.toLowerCase(ENGLISH) + "\\E' already exists");
         }
     }
@@ -226,7 +226,7 @@ public abstract class BaseBigQueryCaseInsensitiveMapping
             throws Exception
     {
         String schemaName = "Test_Drop_Case_Sensitive_" + randomNameSuffix();
-        try (AutoCloseable schema = withSchema(schemaName)) {
+        try (AutoCloseable _ = withSchema(schemaName)) {
             assertUpdate("DROP SCHEMA " + schemaName.toLowerCase(ENGLISH));
         }
     }
@@ -236,8 +236,8 @@ public abstract class BaseBigQueryCaseInsensitiveMapping
             throws Exception
     {
         String schemaName = "Test_Drop_Case_Sensitive_Clash_" + randomNameSuffix();
-        try (AutoCloseable schema = withSchema(schemaName);
-                AutoCloseable secondSchema = withSchema(schemaName.toLowerCase(ENGLISH))) {
+        try (AutoCloseable _ = withSchema(schemaName);
+                AutoCloseable _ = withSchema(schemaName.toLowerCase(ENGLISH))) {
             assertQueryFails("DROP SCHEMA " + schemaName.toLowerCase(ENGLISH), "Found ambiguous names in BigQuery.*");
         }
     }
