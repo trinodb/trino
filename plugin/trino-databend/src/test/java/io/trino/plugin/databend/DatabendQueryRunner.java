865362f90c708f9d8e8fa28904c7fc2c09c2f899
hantmac <hantmac@outlook.com>
2024-11-25 17:40:08 +0800

    add DatabendQueryRunner class

---
+++ plugin/trino-databend/src/test/java/io/trino/plugin/databend/DatabendQueryRunner.java
@@ -0,0 +1,103 @@
+package io.trino.plugin.databend;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import io.airlift.log.Level;
+import io.airlift.log.Logger;
+import io.airlift.log.Logging;
+import io.trino.plugin.tpch.TpchPlugin;
+import io.trino.testing.DistributedQueryRunner;
+import io.trino.testing.QueryRunner;
+import io.trino.tpch.TpchTable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.airlift.testing.Closeables.closeAllSuppress;
+import static io.trino.plugin.tpch.TpchMetadata.TINY_SCHEMA_NAME;
+import static io.trino.testing.QueryAssertions.copyTpchTables;
+import static io.trino.testing.TestingSession.testSessionBuilder;
+import static java.util.Objects.requireNonNull;
+
+public final class DatabendQueryRunner
+{
+    static {
+        Logging logging = Logging.initialize();
+    }
+
+    public static final String TPCH_SCHEMA = "tpch";
+
+    private DatabendQueryRunner() {}
+
+    public static Builder builder(TestingDatabendServer server)
+    {
+        return new Builder()
+                .addConnectorProperty("connection-url", server.getJdbcUrl());
+    }
+
+    public static final class Builder
+            extends DistributedQueryRunner.Builder<Builder>
+    {
+        private final Map<String, String> connectorProperties = new HashMap<>();
+        private List<TpchTable<?>> initialTables = ImmutableList.of();
+
+        private Builder()
+        {
+            super(testSessionBuilder()
+                    .setCatalog("databend")
+                    .setSchema(TPCH_SCHEMA)
+                    .build());
+        }
+
+        @CanIgnoreReturnValue
+        public Builder addConnectorProperty(String key, String value)
+        {
+            this.connectorProperties.put(key, value);
+            return this;
+        }
+
+        @CanIgnoreReturnValue
+        public Builder setInitialTables(Iterable<TpchTable<?>> initialTables)
+        {
+            this.initialTables = ImmutableList.copyOf(requireNonNull(initialTables, "initialTables is null"));
+            return this;
+        }
+
+        @Override
+        public DistributedQueryRunner build()
+                throws Exception
+        {
+            DistributedQueryRunner queryRunner = super.build();
+            try {
+                queryRunner.installPlugin(new TpchPlugin());
+                queryRunner.createCatalog("tpch", "tpch");
+
+                queryRunner.installPlugin(new DatabendPlugin());
+                queryRunner.createCatalog("databend", "databend", connectorProperties);
+                queryRunner.execute("CREATE SCHEMA " + TPCH_SCHEMA);
+                copyTpchTables(queryRunner, "tpch", TINY_SCHEMA_NAME, initialTables);
+
+                return queryRunner;
+            }
+            catch (Throwable e) {
+                closeAllSuppress(e, queryRunner);
+                throw e;
+            }
+        }
+    }
+
+    public static void main(String[] args)
+            throws Exception
+    {
+        QueryRunner queryRunner = builder(new TestingDatabendServer())
+                .addCoordinatorProperty("http-server.http.port", "8000")
+                .setInitialTables(TpchTable.getTables())
+                .build();
+
+        Logger log = Logger.get(DatabendQueryRunner.class);
+        log.info("======== SERVER STARTED ========");
+        log.info("\n====\n%s\n====", queryRunner.getCoordinator().getBaseUrl());
+    }
+}
+
