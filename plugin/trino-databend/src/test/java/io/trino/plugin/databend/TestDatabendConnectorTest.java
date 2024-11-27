7f72b1ccbc5b0bce2d71acc1ceb8f13b6786d980
hantmac <hantmac@outlook.com>
2024-11-27 19:15:27 +0800

    fix

--- plugin/trino-databend/src/test/java/io/trino/plugin/databend/TestDatabendConnectorTest.java
+++ plugin/trino-databend/src/test/java/io/trino/plugin/databend/TestDatabendConnectorTest.java
@@ -24,7 +24,7 @@ public class TestDatabendConnectorTest
     {
         databendServer = closeAfterClass(new TestingDatabendServer(TestingDatabendServer.DATABEND_DEFAULT_IMAGE));
         return DatabendQueryRunner.builder(databendServer)
-                .addConnectorProperty("databend.connection-timeout", "60")
+                .addConnectorProperty("databend.connection-timeout", "60s")
                 .setInitialTables(REQUIRED_TPCH_TABLES)
                 .build();
     }
