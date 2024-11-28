568c9c8796c942a4ad4c6acbfd5f4f734ffc6eba
hantmac <hantmac@outlook.com>
2024-11-28 17:07:52 +0800

    fix TestDatabendConfig

--- plugin/trino-databend/src/test/java/io/trino/plugin/databend/TestDatabendConfig.java
+++ plugin/trino-databend/src/test/java/io/trino/plugin/databend/TestDatabendConfig.java
@@ -14,29 +14,49 @@
 package io.trino.plugin.databend;
 
 import com.google.common.collect.ImmutableMap;
+import io.airlift.units.Duration;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Map;
 
-import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
 import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
 import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestDatabendConfig
 {
     @Test
     public void testDefaults()
     {
-        assertRecordedDefaults(recordDefaults(DatabendConfig.class).setConnectionTimeout("10"));
+        assertRecordedDefaults(recordDefaults(DatabendConfig.class)
+                .setConnectionTimeout(Duration.valueOf("60s"))
+                .setPresignedUrlDisabled(false)
+                .setCacheMaximumSize(10000)
+                .setCacheMissing(false)
+                .setConnectionUrl(null)
+                .setJdbcTypesMappedToVarchar(new HashSet<>())
+                .setMetadataCacheTtl(Duration.ZERO)
+                .setSchemaNamesCacheTtl(Duration.ZERO)
+                .setStatisticsCacheTtl(Duration.ZERO)
+                .setTableNamesCacheTtl(Duration.ZERO));
     }
 
     @Test
     public void testExplicitPropertyMappings()
     {
-        Map<String, String> properties = ImmutableMap.<String, String>builder().put("databend.connection-timeout", "60").buildOrThrow();
+        Map<String, String> properties = ImmutableMap.<String, String>builder()
+                .put("databend.connection-timeout", Duration.valueOf("60s").toString())
+                .put("databend.presigned-url-disabled", "true")
+                .buildOrThrow();
 
-        DatabendConfig expected = new DatabendConfig().setConnectionTimeout("60");
+        DatabendConfig expected = new DatabendConfig()
+                .setConnectionTimeout(Duration.valueOf("60s"))
+                .setPresignedUrlDisabled(true);
 
-        assertFullMapping(properties, expected);
+        assertThat(expected.getConnectionTimeout().toString())
+                .isEqualTo(properties.get("databend.connection-timeout"));
+        assertThat(expected.getPresignedUrlDisabled().toString())
+                .isEqualTo(properties.get("databend.presigned-url-disabled"));
     }
 }
