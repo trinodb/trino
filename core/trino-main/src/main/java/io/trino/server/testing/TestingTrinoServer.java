80fee59913ffcd69b7954db16887a57b2a583ba9
Bartosz Rybacki <bartosz.rybacki@starburstdata.com>
2024-11-29 15:27:25 +0100

    Reactivation of worker nodes
    Adds new node states to enable full control over shutdown and reactivation of workers.
    - state: DRAINING - a reversible shutdown,
    - state: DRAINED - all tasks are finished, server can be safely and quickly stopped. Can still go back to ACTIVE.

--- core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
+++ core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
@@ -666,7 +666,7 @@ public class TestingTrinoServer
         return mBeanServer;
     }
 
-    public NodeStateManager getGracefulShutdownHandler()
+    public NodeStateManager getNodeStateManager()
     {
         return nodeStateManager;
     }
