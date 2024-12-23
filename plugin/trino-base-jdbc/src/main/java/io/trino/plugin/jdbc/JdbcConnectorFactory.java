8018b456a501fe61260feae4dd114c03da9120b2
Piotr Rzysko <piotr.rzysko@gmail.com>
2024-12-23 10:21:04 +0100

    Expose redactable properties for PostgreSQL connector
    Exposed properties fall into one of the following categories: they are
    either explicitly marked as security-sensitive or are unknown. The
    connector assumes that unknown properties might be misspelled
    security-sensitive properties.
    
    The purpose of the included test is to identify security-sensitive
    properties that may be used by the connector. It uses the output
    generated by the maven-dependency-plugin, configured in the connector's
    pom.xml file. This output contains the connector's runtime classpath,
    which is then scanned to identify all property names annotated with
    @Config. Scanning the classpath ensures that all configuration classes
    are included, even those used conditionally.

--- plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcConnectorFactory.java
+++ plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcConnectorFactory.java
@@ -13,10 +13,21 @@
  */
 package io.trino.plugin.jdbc;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.BootstrapConfig;
 import io.opentelemetry.api.OpenTelemetry;
+import io.trino.plugin.base.config.ConfigPropertyMetadata;
+import io.trino.plugin.base.mapping.MappingConfig;
+import io.trino.plugin.jdbc.credential.CredentialConfig;
+import io.trino.plugin.jdbc.credential.CredentialProviderTypeConfig;
+import io.trino.plugin.jdbc.credential.ExtraCredentialConfig;
+import io.trino.plugin.jdbc.credential.file.ConfigFileBasedCredentialProviderConfig;
+import io.trino.plugin.jdbc.credential.keystore.KeyStoreBasedCredentialProviderConfig;
+import io.trino.plugin.jdbc.logging.FormatBasedRemoteQueryModifierConfig;
 import io.trino.spi.NodeManager;
 import io.trino.spi.VersionEmbedder;
 import io.trino.spi.catalog.CatalogName;
@@ -26,11 +37,15 @@ import io.trino.spi.connector.ConnectorFactory;
 import io.trino.spi.type.TypeManager;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.trino.plugin.base.Versions.checkStrictSpiVersionMatch;
+import static io.trino.plugin.base.config.ConfigPropertyMetadata.getConfigProperties;
 import static java.util.Objects.requireNonNull;
 
 public class JdbcConnectorFactory
@@ -38,12 +53,39 @@ public class JdbcConnectorFactory
 {
     private final String name;
     private final Supplier<Module> module;
+    private final Set<String> nonRedactablePropertyNames;
 
-    public JdbcConnectorFactory(String name, Supplier<Module> module)
+    public JdbcConnectorFactory(String name, Supplier<Module> module, Set<ConfigPropertyMetadata> additionalProperties)
     {
         checkArgument(!isNullOrEmpty(name), "name is null or empty");
         this.name = name;
         this.module = requireNonNull(module, "module is null");
+        Set<Class<?>> configClasses = ImmutableSet.<Class<?>>builder()
+                .add(BaseJdbcConfig.class)
+                .add(CredentialConfig.class)
+                .add(JdbcStatisticsConfig.class)
+                .add(JdbcWriteConfig.class)
+                .add(QueryConfig.class)
+                .add(RemoteQueryCancellationConfig.class)
+                .add(TypeHandlingJdbcConfig.class)
+                .add(JdbcMetadataConfig.class)
+                .add(JdbcJoinPushdownConfig.class)
+                .add(DecimalConfig.class)
+                .add(JdbcDynamicFilteringConfig.class)
+                .add(KeyStoreBasedCredentialProviderConfig.class)
+                .add(FormatBasedRemoteQueryModifierConfig.class)
+                .add(ConfigFileBasedCredentialProviderConfig.class)
+                .add(CredentialProviderTypeConfig.class)
+                .add(ExtraCredentialConfig.class)
+                .add(MappingConfig.class)
+                .add(BootstrapConfig.class)
+                .build();
+        this.nonRedactablePropertyNames = Stream.concat(
+                        configClasses.stream().flatMap(clazz -> getConfigProperties(clazz).stream()),
+                        requireNonNull(additionalProperties, "additionalProperties is null").stream())
+                .filter(property -> !property.sensitive())
+                .map(ConfigPropertyMetadata::name)
+                .collect(toImmutableSet());
     }
 
     @Override
@@ -73,5 +115,11 @@ public class JdbcConnectorFactory
                 .initialize();
 
         return injector.getInstance(JdbcConnector.class);
+    }
+
+    @Override
+    public Set<String> getRedactablePropertyNames(Set<String> propertyNames)
+    {
+        return Sets.difference(propertyNames, nonRedactablePropertyNames);
     }
 }
