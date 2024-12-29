4def327122ecb22011b8e55fbf3ef426bfab1386
David Phillips <david@acz.org>
2024-12-29 15:48:35 -0800

    Add lakehouse connector

---
+++ plugin/trino-lakehouse/src/test/java/io/trino/plugin/lakehouse/TestLakehouseIcebergConnectorSmokeTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.lakehouse;
+
+import org.junit.jupiter.api.Test;
+
+import static io.trino.plugin.lakehouse.TableType.ICEBERG;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestLakehouseIcebergConnectorSmokeTest
+        extends BaseLakehouseConnectorSmokeTest
+{
+    protected TestLakehouseIcebergConnectorSmokeTest()
+    {
+        super(ICEBERG);
+    }
+
+    @Test
+    @Override
+    public void testShowCreateTable()
+    {
+        assertThat((String) computeScalar("SHOW CREATE TABLE region")).matches(
+                """
+                \\QCREATE TABLE lakehouse.tpch.region (
+                   regionkey bigint,
+                   name varchar,
+                   comment varchar
+                )
+                WITH (
+                   format = 'PARQUET',
+                   format_version = 2,
+                   location = \\E's3://test-bucket-.*/tpch/region-.*'\\Q,
+                   max_commit_retry = 4,
+                   type = 'ICEBERG'
+                )\\E""");
+    }
+}
