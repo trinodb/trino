acf6ccc325d73bf16ce53a6ddff38537900be6ca
David Phillips <david@acz.org>
2025-01-01 17:38:17 -0800

    Remove shadowed field from MockConnectorMetadata

--- core/trino-main/src/test/java/io/trino/connector/MockConnector.java
+++ core/trino-main/src/test/java/io/trino/connector/MockConnector.java
@@ -317,7 +317,7 @@ public class MockConnector
     @Override
     public ConnectorMetadata getMetadata(ConnectorSession session, ConnectorTransactionHandle transaction)
     {
-        return metadataWrapper.apply(new MockConnectorMetadata(allowSplittingReadIntoMultipleSubQueries));
+        return metadataWrapper.apply(new MockConnectorMetadata());
     }
 
     @Override
@@ -438,13 +438,6 @@ public class MockConnector
     private class MockConnectorMetadata
             implements ConnectorMetadata
     {
-        private final boolean allowSplittingReadIntoMultipleSubQueries;
-
-        public MockConnectorMetadata(boolean allowSplittingReadIntoMultipleSubQueries)
-        {
-            this.allowSplittingReadIntoMultipleSubQueries = allowSplittingReadIntoMultipleSubQueries;
-        }
-
         @Override
         public boolean schemaExists(ConnectorSession session, String schemaName)
         {
