063dc04e12c4ec98476e93cf2f73d66bf8e691b0
David Phillips <david@acz.org>
2025-01-01 17:16:45 -0800

    Inline partition projection name in HiveConfig

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveConfig.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveConfig.java
@@ -66,8 +66,6 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 })
 public class HiveConfig
 {
-    public static final String CONFIGURATION_HIVE_PARTITION_PROJECTION_ENABLED = "hive.partition-projection-enabled";
-
     private boolean singleStatementWritesOnly;
 
     private DataSize maxSplitSize = DataSize.of(64, MEGABYTE);
@@ -1249,7 +1247,7 @@ public class HiveConfig
         return partitionProjectionEnabled;
     }
 
-    @Config(CONFIGURATION_HIVE_PARTITION_PROJECTION_ENABLED)
+    @Config("hive.partition-projection-enabled")
     @ConfigDescription("Enables AWS Athena partition projection")
     public HiveConfig setPartitionProjectionEnabled(boolean enabledAthenaPartitionProjection)
     {
