e63e7df46233cb77eb90ef701643249c4e9e0ab3
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-01-02 14:20:13 +0100

    Use StandardTypes.KDB_TREE const

--- plugin/trino-geospatial/src/main/java/io/trino/plugin/geospatial/KdbTreeCasts.java
+++ plugin/trino-geospatial/src/main/java/io/trino/plugin/geospatial/KdbTreeCasts.java
@@ -19,6 +19,7 @@ import io.trino.spi.TrinoException;
 import io.trino.spi.function.LiteralParameters;
 import io.trino.spi.function.ScalarOperator;
 import io.trino.spi.function.SqlType;
+import io.trino.spi.type.StandardTypes;
 
 import static io.trino.spi.StandardErrorCode.INVALID_CAST_ARGUMENT;
 import static io.trino.spi.function.OperatorType.CAST;
@@ -29,7 +30,7 @@ public final class KdbTreeCasts
 
     @LiteralParameters("x")
     @ScalarOperator(CAST)
-    @SqlType(KdbTreeType.NAME)
+    @SqlType(StandardTypes.KDB_TREE)
     public static Object castVarcharToKdbTree(@SqlType("varchar(x)") Slice json)
     {
         try {
