b0f7e05123272ca6087e7f26c4bbfb01c4b839c4
仟弋 <yejunhao.yjh@alibaba-inc.com>
2025-01-09 15:37:29 +0800

    Remove hadoop support

--- plugin/trino-paimon/src/main/java/io/trino/plugin/paimon/PaimonConnectorFactory.java
+++ plugin/trino-paimon/src/main/java/io/trino/plugin/paimon/PaimonConnectorFactory.java
@@ -31,17 +31,7 @@ import io.trino.spi.connector.Connector;
 import io.trino.spi.connector.ConnectorContext;
 import io.trino.spi.connector.ConnectorFactory;
 import io.trino.spi.type.TypeManager;
-import org.apache.paimon.utils.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
@@ -52,41 +42,6 @@ import static java.util.Objects.requireNonNull;
 public class PaimonConnectorFactory
         implements ConnectorFactory
 {
-    private static final Logger LOG = LoggerFactory.getLogger(PaimonConnectorFactory.class);
-
-    // see https://trino.io/docs/current/connector/hive.html#hive-general-configuration-properties
-    private static final String HADOOP_CONF_FILES_KEY = "hive.config.resources";
-    // see org.apache.paimon.utils.HadoopUtils
-    private static final String HADOOP_CONF_PREFIX = "hadoop.";
-
-    private static void readHadoopXml(String path, Map<String, String> config)
-            throws Exception
-    {
-        path = path.trim();
-        if (path.isEmpty()) {
-            return;
-        }
-
-        File xmlFile = new File(path);
-        NodeList propertyNodes =
-                DocumentBuilderFactory.newInstance()
-                        .newDocumentBuilder()
-                        .parse(xmlFile)
-                        .getElementsByTagName("property");
-        for (int i = 0; i < propertyNodes.getLength(); i++) {
-            Node propertyNode = propertyNodes.item(i);
-            if (propertyNode.getNodeType() == 1) {
-                Element propertyElement = (Element) propertyNode;
-                String key = propertyElement.getElementsByTagName("name").item(0).getTextContent();
-                String value =
-                        propertyElement.getElementsByTagName("value").item(0).getTextContent();
-                if (!StringUtils.isNullOrWhitespaceOnly(value)) {
-                    config.putIfAbsent(HADOOP_CONF_PREFIX + key, value);
-                }
-            }
-        }
-    }
-
     @Override
     public String getName()
     {
@@ -106,20 +61,6 @@ public class PaimonConnectorFactory
             ConnectorContext context,
             Module module)
     {
-        config = new HashMap<>(config);
-        if (config.containsKey(HADOOP_CONF_FILES_KEY)) {
-            for (String hadoopXml : config.get(HADOOP_CONF_FILES_KEY).split(",")) {
-                try {
-                    readHadoopXml(hadoopXml, config);
-                }
-                catch (Exception e) {
-                    LOG.warn(
-                            "Failed to read hadoop xml file " + hadoopXml + ", skipping this file.",
-                            e);
-                }
-            }
-        }
-
         ClassLoader classLoader = PaimonConnectorFactory.class.getClassLoader();
         try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(classLoader)) {
             Bootstrap app =
