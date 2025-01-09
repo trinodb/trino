a7cb1f054f1f50b624ae071fd6a2e7a4a0ec1ab8
Piotr Rzysko <piotr.rzysko@gmail.com>
2025-01-09 09:55:37 +0100

    Expose security-sensitive properties for Delta Lake connector

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeConnectorFactory.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeConnectorFactory.java
@@ -13,12 +13,14 @@
  */
 package io.trino.plugin.deltalake;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import io.airlift.bootstrap.Bootstrap;
 import io.airlift.bootstrap.LifeCycleManager;
+import io.airlift.configuration.ConfigPropertyMetadata;
 import io.airlift.json.JsonModule;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
@@ -29,6 +31,7 @@ import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorPageSinkProvider
 import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorPageSourceProvider;
 import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorSplitManager;
 import io.trino.plugin.base.classloader.ClassLoaderSafeNodePartitioningProvider;
+import io.trino.plugin.base.config.ConfigUtils;
 import io.trino.plugin.base.jmx.ConnectorObjectNameGeneratorModule;
 import io.trino.plugin.base.jmx.MBeanServerModule;
 import io.trino.plugin.base.session.SessionPropertiesProvider;
@@ -67,6 +70,9 @@ public class DeltaLakeConnectorFactory
 {
     public static final String CONNECTOR_NAME = "delta_lake";
 
+    private static final Module DEFAULT_ADDITIONAL_MODULE = EMPTY_MODULE;
+    private static final Optional<Module> DEFAULT_METASTORE_MODULE = Optional.empty();
+
     @Override
     public String getName()
     {
@@ -77,9 +83,22 @@ public class DeltaLakeConnectorFactory
     public Connector create(String catalogName, Map<String, String> config, ConnectorContext context)
     {
         checkStrictSpiVersionMatch(context, this);
-        return createConnector(catalogName, config, context, Optional.empty(), EMPTY_MODULE);
+        return createConnector(catalogName, config, context, DEFAULT_METASTORE_MODULE, DEFAULT_ADDITIONAL_MODULE);
     }
 
+    @Override
+    public Set<String> getSecuritySensitivePropertyNames(String catalogName, Map<String, String> config, ConnectorContext context)
+    {
+        try (ThreadContextClassLoader _ = new ThreadContextClassLoader(DeltaLakeConnectorFactory.class.getClassLoader())) {
+            Bootstrap app = createBootstrap(catalogName, config, context, DEFAULT_METASTORE_MODULE, DEFAULT_ADDITIONAL_MODULE, true);
+
+            Set<ConfigPropertyMetadata> usedProperties = app.configure();
+
+            return ConfigUtils.getSecuritySensitivePropertyNames(config, usedProperties);
+        }
+    }
+
+    @VisibleForTesting
     public static Connector createConnector(
             String catalogName,
             Map<String, String> config,
@@ -89,32 +108,9 @@ public class DeltaLakeConnectorFactory
     {
         ClassLoader classLoader = DeltaLakeConnectorFactory.class.getClassLoader();
         try (ThreadContextClassLoader _ = new ThreadContextClassLoader(classLoader)) {
-            Bootstrap app = new Bootstrap(
-                    new MBeanModule(),
-                    new ConnectorObjectNameGeneratorModule("io.trino.plugin.deltalake", "trino.plugin.deltalake"),
-                    new JsonModule(),
-                    new MBeanServerModule(),
-                    new CatalogNameModule(catalogName),
-                    metastoreModule.orElse(new DeltaLakeMetastoreModule()),
-                    new DeltaLakeModule(),
-                    new DeltaLakeSecurityModule(),
-                    new DeltaLakeSynchronizerModule(),
-                    new FileSystemModule(catalogName, context.getNodeManager(), context.getOpenTelemetry(), false, false),
-                    binder -> {
-                        binder.bind(OpenTelemetry.class).toInstance(context.getOpenTelemetry());
-                        binder.bind(Tracer.class).toInstance(context.getTracer());
-                        binder.bind(NodeVersion.class).toInstance(new NodeVersion(context.getNodeManager().getCurrentNode().getVersion()));
-                        binder.bind(NodeManager.class).toInstance(context.getNodeManager());
-                        binder.bind(TypeManager.class).toInstance(context.getTypeManager());
-                        binder.bind(PageIndexerFactory.class).toInstance(context.getPageIndexerFactory());
-                        binder.bind(CatalogName.class).toInstance(new CatalogName(catalogName));
-                    },
-                    module);
+            Bootstrap app = createBootstrap(catalogName, config, context, metastoreModule, module, false);
 
-            Injector injector = app
-                    .doNotInitializeLogging()
-                    .setRequiredConfigurationProperties(config)
-                    .initialize();
+            Injector injector = app.initialize();
 
             LifeCycleManager lifeCycleManager = injector.getInstance(LifeCycleManager.class);
             ConnectorSplitManager splitManager = injector.getInstance(ConnectorSplitManager.class);
@@ -158,5 +154,42 @@ public class DeltaLakeConnectorFactory
                     connectorTableFunctions,
                     functionProvider);
         }
+    }
+
+    private static Bootstrap createBootstrap(
+            String catalogName,
+            Map<String, String> config,
+            ConnectorContext context,
+            Optional<Module> metastoreModule,
+            Module module,
+            boolean quietBootstrap)
+    {
+        Bootstrap app = new Bootstrap(
+                new MBeanModule(),
+                new ConnectorObjectNameGeneratorModule("io.trino.plugin.deltalake", "trino.plugin.deltalake"),
+                new JsonModule(),
+                new MBeanServerModule(),
+                new CatalogNameModule(catalogName),
+                metastoreModule.orElse(new DeltaLakeMetastoreModule()),
+                new DeltaLakeModule(),
+                new DeltaLakeSecurityModule(),
+                new DeltaLakeSynchronizerModule(),
+                new FileSystemModule(catalogName, context.getNodeManager(), context.getOpenTelemetry(), false, quietBootstrap),
+                binder -> {
+                    binder.bind(OpenTelemetry.class).toInstance(context.getOpenTelemetry());
+                    binder.bind(Tracer.class).toInstance(context.getTracer());
+                    binder.bind(NodeVersion.class).toInstance(new NodeVersion(context.getNodeManager().getCurrentNode().getVersion()));
+                    binder.bind(NodeManager.class).toInstance(context.getNodeManager());
+                    binder.bind(TypeManager.class).toInstance(context.getTypeManager());
+                    binder.bind(PageIndexerFactory.class).toInstance(context.getPageIndexerFactory());
+                    binder.bind(CatalogName.class).toInstance(new CatalogName(catalogName));
+                },
+                module);
+
+        return app
+                .withQuiet(quietBootstrap)
+                .withSkipErrorReporting(quietBootstrap)
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperties(config);
     }
 }
