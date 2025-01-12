7bd1f075f6434387290b22bd238a5d528289bc4a
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2025-01-12 09:36:34 +0000

    Fix code formatting in Preview Web UI

--- core/trino-web-ui/src/main/resources/webapp-preview/src/components/Auth.tsx
+++ core/trino-web-ui/src/main/resources/webapp-preview/src/components/Auth.tsx
@@ -11,33 +11,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Box, CircularProgress, Alert, Typography } from "@mui/material";
+import { Box, CircularProgress, Alert, Typography } from '@mui/material'
 import { useAuth } from './AuthContext'
-import { Texts } from "../constant";
-import { Login } from './Login';
+import { Texts } from '../constant'
+import { Login } from './Login'
 
 export const Auth = () => {
-  const { authInfo, error, loading } = useAuth();
+    const { authInfo, error, loading } = useAuth()
 
-  if (!authInfo) {
-      return (
-          <Box sx={{ p: 10 }} display="flex" flexDirection="column"  alignItems="center" justifyContent="center">
-            {loading ?
-              <>
-                <CircularProgress/>
-                <Typography mt={2}>{Texts.Auth.Authenticating}</Typography>
-              </> : error &&
-              <Alert severity="error">
-                {error}
-              </Alert>
-            }
-          </Box>
-      )
-  }
+    if (!authInfo) {
+        return (
+            <Box sx={{ p: 10 }} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                {loading ? (
+                    <>
+                        <CircularProgress />
+                        <Typography mt={2}>{Texts.Auth.Authenticating}</Typography>
+                    </>
+                ) : (
+                    error && <Alert severity="error">{error}</Alert>
+                )}
+            </Box>
+        )
+    }
 
-  if (authInfo.authType === 'form' && !authInfo?.authenticated) {
-    return <Login passwordAllowed={authInfo.passwordAllowed} />
-  }
+    if (authInfo.authType === 'form' && !authInfo?.authenticated) {
+        return <Login passwordAllowed={authInfo.passwordAllowed} />
+    }
 
-  return <div>{Texts.Auth.NotImplementedAuthType}</div>
+    return <div>{Texts.Auth.NotImplementedAuthType}</div>
 }
--- core/trino-web-ui/src/main/resources/webapp-preview/src/components/Dashboard.tsx
+++ core/trino-web-ui/src/main/resources/webapp-preview/src/components/Dashboard.tsx
@@ -11,183 +11,193 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { useEffect, useState } from 'react';
-import Typography from '@mui/material/Typography';
-import {
-  Box,
-  Grid2 as Grid,
-} from "@mui/material";
+import { useEffect, useState } from 'react'
+import Typography from '@mui/material/Typography'
+import { Box, Grid2 as Grid } from '@mui/material'
 import { MetricCard } from './MetricCard.tsx'
-import { useSnackbar } from "./SnackbarContext.ts";
-import { ApiResponse } from "../api/base.ts";
-import { statsApi, Stats } from "../api/webapp/api.ts";
-import { Texts } from "../constant.ts";
+import { useSnackbar } from './SnackbarContext.ts'
+import { ApiResponse } from '../api/base.ts'
+import { statsApi, Stats } from '../api/webapp/api.ts'
+import { Texts } from '../constant.ts'
 import {
-  MAX_HISTORY,
-  addExponentiallyWeightedToHistory,
-  addToHistory,
-  formatCount,
-  formatDataSizeBytes,
-  precisionRound,
+    MAX_HISTORY,
+    addExponentiallyWeightedToHistory,
+    addToHistory,
+    formatCount,
+    formatDataSizeBytes,
+    precisionRound,
 } from '../utils/utils.ts'
 
-interface ClusterStats
-{
-  runningQueries: number[];
-  queuedQueries: number[];
-  blockedQueries: number[];
-  activeWorkers: number[];
-  runningDrivers: number[];
-  reservedMemory: number[];
-  rowInputRate: number[];
-  byteInputRate: number[];
-  perWorkerCpuTimeRate: number[];
+interface ClusterStats {
+    runningQueries: number[]
+    queuedQueries: number[]
+    blockedQueries: number[]
+    activeWorkers: number[]
+    runningDrivers: number[]
+    reservedMemory: number[]
+    rowInputRate: number[]
+    byteInputRate: number[]
+    perWorkerCpuTimeRate: number[]
 
-  lastRefresh: number | null;
+    lastRefresh: number | null
 
-  lastInputRows: number
-  lastInputBytes: number;
-  lastCpuTime: number;
+    lastInputRows: number
+    lastInputBytes: number
+    lastCpuTime: number
 }
 
 export const Dashboard = () => {
-  const { showSnackbar } = useSnackbar();
-  const initialFilledHistory = Array(MAX_HISTORY).fill(0);
-  const [clusterStats, setClusterStats] = useState<ClusterStats>({
-    runningQueries: initialFilledHistory,
-    queuedQueries: initialFilledHistory,
-    blockedQueries: initialFilledHistory,
-    activeWorkers: initialFilledHistory,
-    runningDrivers: initialFilledHistory,
-    reservedMemory: initialFilledHistory,
-    rowInputRate: initialFilledHistory,
-    byteInputRate: initialFilledHistory,
-    perWorkerCpuTimeRate: initialFilledHistory,
+    const { showSnackbar } = useSnackbar()
+    const initialFilledHistory = Array(MAX_HISTORY).fill(0)
+    const [clusterStats, setClusterStats] = useState<ClusterStats>({
+        runningQueries: initialFilledHistory,
+        queuedQueries: initialFilledHistory,
+        blockedQueries: initialFilledHistory,
+        activeWorkers: initialFilledHistory,
+        runningDrivers: initialFilledHistory,
+        reservedMemory: initialFilledHistory,
+        rowInputRate: initialFilledHistory,
+        byteInputRate: initialFilledHistory,
+        perWorkerCpuTimeRate: initialFilledHistory,
 
-    lastRefresh: null,
+        lastRefresh: null,
 
-    lastInputRows: 0,
-    lastInputBytes: 0,
-    lastCpuTime: 0,
-  });
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    getClusterStats();
-    const intervalId = setInterval(getClusterStats, 1000);
-    return () => clearInterval(intervalId);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (error) {
-      showSnackbar(error, 'error');
-    }
-  }, [error, showSnackbar])
-
-  const getClusterStats = () => {
-    setError(null);
-    statsApi().then((apiResponse: ApiResponse<Stats>) => {
-      if (apiResponse.status === 200 && apiResponse.data) {
-        const newClusterStats: Stats = apiResponse.data;
-        setClusterStats(prevClusterStats => {
-          let newRowInputRate: number[] = initialFilledHistory;
-          let newByteInputRate: number[] = initialFilledHistory;
-          let newPerWorkerCpuTimeRate: number[] = []
-          if (prevClusterStats.lastRefresh !== null) {
-            const rowsInputSinceRefresh = newClusterStats.totalInputRows - prevClusterStats.lastInputRows
-            const bytesInputSinceRefresh = newClusterStats.totalInputBytes - prevClusterStats.lastInputBytes
-            const cpuTimeSinceRefresh = newClusterStats.totalCpuTimeSecs - prevClusterStats.lastCpuTime
-            const secsSinceRefresh = (Date.now() - prevClusterStats.lastRefresh) / 1000.0
-
-            newRowInputRate = addExponentiallyWeightedToHistory(
-                rowsInputSinceRefresh / secsSinceRefresh,
-                prevClusterStats.rowInputRate
-            )
-            newByteInputRate = addExponentiallyWeightedToHistory(
-                bytesInputSinceRefresh / secsSinceRefresh,
-                prevClusterStats.byteInputRate
-            )
-            newPerWorkerCpuTimeRate = addExponentiallyWeightedToHistory(
-                cpuTimeSinceRefresh / newClusterStats.activeWorkers / secsSinceRefresh,
-                prevClusterStats.perWorkerCpuTimeRate
-            )
-          }
-
-          return {
-            // instantaneous stats
-            runningQueries: addToHistory(newClusterStats.runningQueries, prevClusterStats.runningQueries),
-            queuedQueries: addToHistory(newClusterStats.queuedQueries, prevClusterStats.queuedQueries),
-            blockedQueries: addToHistory(newClusterStats.blockedQueries, prevClusterStats.blockedQueries),
-            activeWorkers: addToHistory(newClusterStats.activeWorkers, prevClusterStats.activeWorkers),
-
-            // moving averages
-            runningDrivers: addExponentiallyWeightedToHistory(
-                newClusterStats.runningDrivers,
-                prevClusterStats.runningDrivers
-            ),
-            reservedMemory: addExponentiallyWeightedToHistory(
-                newClusterStats.reservedMemory,
-                prevClusterStats.reservedMemory
-            ),
-
-            // moving averages for diffs
-            rowInputRate: newRowInputRate,
-            byteInputRate: newByteInputRate,
-            perWorkerCpuTimeRate: newPerWorkerCpuTimeRate,
-
-            lastInputRows: newClusterStats.totalInputRows,
-            lastInputBytes: newClusterStats.totalInputBytes,
-            lastCpuTime: newClusterStats.totalCpuTimeSecs,
-
-            lastRefresh: Date.now(),
-          }
-        });
-        setError(null);
-      } else {
-        setError(`${Texts.Error.Communication} ${apiResponse.status}: ${apiResponse.message}`);
-      }
+        lastInputRows: 0,
+        lastInputBytes: 0,
+        lastCpuTime: 0,
     })
-  };
+    const [error, setError] = useState<string | null>(null)
 
-  return (
-    <>
-      <Box sx={{ pb: 2 }}>
-        <Typography variant="h4">
-          Cluster Overview
-        </Typography>
-      </Box>
-      <Box>
-        <Grid container spacing={3}>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Running Queries" values={clusterStats.runningQueries} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Active Workers" values={clusterStats.activeWorkers} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Rows/sec" values={clusterStats.rowInputRate} numberFormatter={formatCount} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Queued Queries" values={clusterStats.queuedQueries} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Runnable Drivers" values={clusterStats.runningDrivers} numberFormatter={precisionRound} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Bytes/sec" values={clusterStats.byteInputRate} numberFormatter={formatDataSizeBytes} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Blocked Queries" values={clusterStats.blockedQueries} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Reserved Memory (B)" values={clusterStats.reservedMemory} numberFormatter={formatDataSizeBytes} />
-          </Grid>
-          <Grid size={{ xs:12, sm: 12, md: 6, lg:4 }}>
-            <MetricCard title="Worker Parallelism" values={clusterStats.perWorkerCpuTimeRate} numberFormatter={precisionRound} />
-          </Grid>
-        </Grid>
-      </Box>
-    </>
-  );
+    useEffect(() => {
+        getClusterStats()
+        const intervalId = setInterval(getClusterStats, 1000)
+        return () => clearInterval(intervalId)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        if (error) {
+            showSnackbar(error, 'error')
+        }
+    }, [error, showSnackbar])
+
+    const getClusterStats = () => {
+        setError(null)
+        statsApi().then((apiResponse: ApiResponse<Stats>) => {
+            if (apiResponse.status === 200 && apiResponse.data) {
+                const newClusterStats: Stats = apiResponse.data
+                setClusterStats((prevClusterStats) => {
+                    let newRowInputRate: number[] = initialFilledHistory
+                    let newByteInputRate: number[] = initialFilledHistory
+                    let newPerWorkerCpuTimeRate: number[] = []
+                    if (prevClusterStats.lastRefresh !== null) {
+                        const rowsInputSinceRefresh = newClusterStats.totalInputRows - prevClusterStats.lastInputRows
+                        const bytesInputSinceRefresh = newClusterStats.totalInputBytes - prevClusterStats.lastInputBytes
+                        const cpuTimeSinceRefresh = newClusterStats.totalCpuTimeSecs - prevClusterStats.lastCpuTime
+                        const secsSinceRefresh = (Date.now() - prevClusterStats.lastRefresh) / 1000.0
+
+                        newRowInputRate = addExponentiallyWeightedToHistory(
+                            rowsInputSinceRefresh / secsSinceRefresh,
+                            prevClusterStats.rowInputRate
+                        )
+                        newByteInputRate = addExponentiallyWeightedToHistory(
+                            bytesInputSinceRefresh / secsSinceRefresh,
+                            prevClusterStats.byteInputRate
+                        )
+                        newPerWorkerCpuTimeRate = addExponentiallyWeightedToHistory(
+                            cpuTimeSinceRefresh / newClusterStats.activeWorkers / secsSinceRefresh,
+                            prevClusterStats.perWorkerCpuTimeRate
+                        )
+                    }
+
+                    return {
+                        // instantaneous stats
+                        runningQueries: addToHistory(newClusterStats.runningQueries, prevClusterStats.runningQueries),
+                        queuedQueries: addToHistory(newClusterStats.queuedQueries, prevClusterStats.queuedQueries),
+                        blockedQueries: addToHistory(newClusterStats.blockedQueries, prevClusterStats.blockedQueries),
+                        activeWorkers: addToHistory(newClusterStats.activeWorkers, prevClusterStats.activeWorkers),
+
+                        // moving averages
+                        runningDrivers: addExponentiallyWeightedToHistory(
+                            newClusterStats.runningDrivers,
+                            prevClusterStats.runningDrivers
+                        ),
+                        reservedMemory: addExponentiallyWeightedToHistory(
+                            newClusterStats.reservedMemory,
+                            prevClusterStats.reservedMemory
+                        ),
+
+                        // moving averages for diffs
+                        rowInputRate: newRowInputRate,
+                        byteInputRate: newByteInputRate,
+                        perWorkerCpuTimeRate: newPerWorkerCpuTimeRate,
+
+                        lastInputRows: newClusterStats.totalInputRows,
+                        lastInputBytes: newClusterStats.totalInputBytes,
+                        lastCpuTime: newClusterStats.totalCpuTimeSecs,
+
+                        lastRefresh: Date.now(),
+                    }
+                })
+                setError(null)
+            } else {
+                setError(`${Texts.Error.Communication} ${apiResponse.status}: ${apiResponse.message}`)
+            }
+        })
+    }
+
+    return (
+        <>
+            <Box sx={{ pb: 2 }}>
+                <Typography variant="h4">Cluster Overview</Typography>
+            </Box>
+            <Box>
+                <Grid container spacing={3}>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard title="Running Queries" values={clusterStats.runningQueries} />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard title="Active Workers" values={clusterStats.activeWorkers} />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard title="Rows/sec" values={clusterStats.rowInputRate} numberFormatter={formatCount} />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard title="Queued Queries" values={clusterStats.queuedQueries} />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard
+                            title="Runnable Drivers"
+                            values={clusterStats.runningDrivers}
+                            numberFormatter={precisionRound}
+                        />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard
+                            title="Bytes/sec"
+                            values={clusterStats.byteInputRate}
+                            numberFormatter={formatDataSizeBytes}
+                        />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard title="Blocked Queries" values={clusterStats.blockedQueries} />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard
+                            title="Reserved Memory (B)"
+                            values={clusterStats.reservedMemory}
+                            numberFormatter={formatDataSizeBytes}
+                        />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4 }}>
+                        <MetricCard
+                            title="Worker Parallelism"
+                            values={clusterStats.perWorkerCpuTimeRate}
+                            numberFormatter={precisionRound}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    )
 }
