41ba97d395611b5bf9b6095440bc44c4ba31d03f
Yuya Ebihara <ebyhry@gmail.com>
2025-01-16 09:39:37 +0900

    Update Exasol image version to 8.32.0
    Co-Authored-By: YotillaAntoni <92581297+yotillaantoni@users.noreply.github.com>

--- plugin/trino-exasol/src/test/java/io/trino/plugin/exasol/TestingExasolServer.java
+++ plugin/trino-exasol/src/test/java/io/trino/plugin/exasol/TestingExasolServer.java
@@ -47,7 +47,7 @@ public class TestingExasolServer
 
     public TestingExasolServer()
     {
-        container = new ExasolContainer<>("8.27.0")
+        container = new ExasolContainer<>("8.32.0")
                 .withExposedPorts(8563)
                 .withRequiredServices(ExasolService.JDBC);
         cleanup = startOrReuse(container);
