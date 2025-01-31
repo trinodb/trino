4a5e5a19e513f83f7946440d43d724481ce40f66
Łukasz Osipiuk <lukasz@osipiuk.net>
2025-01-31 15:58:33 +0100

    Add faker FTE query runner

--- plugin/trino-faker/src/test/java/io/trino/plugin/faker/FakerQueryRunner.java
+++ plugin/trino-faker/src/test/java/io/trino/plugin/faker/FakerQueryRunner.java
@@ -17,14 +17,17 @@ import com.google.common.collect.ImmutableMap;
 import io.airlift.log.Level;
 import io.airlift.log.Logger;
 import io.airlift.log.Logging;
+import io.trino.plugin.exchange.filesystem.FileSystemExchangePlugin;
 import io.trino.plugin.tpch.TpchPlugin;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.QueryRunner;
 
+import java.io.File;
 import java.util.Map;
 
 import static io.airlift.testing.Closeables.closeAllSuppress;
 import static io.trino.testing.TestingSession.testSessionBuilder;
+import static java.nio.file.Files.createTempDirectory;
 import static java.util.Objects.requireNonNullElse;
 
 public class FakerQueryRunner
@@ -94,5 +97,38 @@ public class FakerQueryRunner
         Logger log = Logger.get(FakerQueryRunner.class);
         log.info("======== SERVER STARTED ========");
         log.info("\n====\n%s\n====", queryRunner.getCoordinator().getBaseUrl());
+    }
+
+    public static final class FakerQueryRunnerWithTaskRetries
+    {
+        private FakerQueryRunnerWithTaskRetries() {}
+
+        public static void main(String[] args)
+                throws Exception
+        {
+            Logger log = Logger.get(FakerQueryRunnerWithTaskRetries.class);
+
+            File exchangeManagerDirectory = createTempDirectory("exchange_manager").toFile();
+            Map<String, String> exchangeManagerProperties = ImmutableMap.<String, String>builder()
+                    .put("exchange.base-directories", exchangeManagerDirectory.getAbsolutePath())
+                    .buildOrThrow();
+            exchangeManagerDirectory.deleteOnExit();
+
+            @SuppressWarnings("resource")
+            QueryRunner queryRunner = builder()
+                    .setExtraProperties(ImmutableMap.<String, String>builder()
+                            .put("http-server.http.port", requireNonNullElse(System.getenv("TRINO_PORT"), "8080"))
+                            .put("retry-policy", "TASK")
+                            .put("fault-tolerant-execution-task-memory", "1GB")
+                            .buildOrThrow())
+                    .setAdditionalSetup(runner -> {
+                        runner.installPlugin(new FileSystemExchangePlugin());
+                        runner.loadExchangeManager("filesystem", exchangeManagerProperties);
+                    })
+                    .build();
+
+            log.info("======== SERVER STARTED ========");
+            log.info("\n====\n%s\n====", queryRunner.getCoordinator().getBaseUrl());
+        }
     }
 }
