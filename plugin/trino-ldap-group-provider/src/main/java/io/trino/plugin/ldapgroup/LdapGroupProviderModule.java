boyee-zhang/SEP_66

Add caching group provider and enable for LDAP group provider
This Guice Module can be used to enable caching in the group provider,
by adding it to the list of modules in a Guice context in a group
provider factory, or to any other Guice context as needed.

Features:

* Configurable configuration prefix
* Ability to bind the final `GroupProvider` with a custom binding
  annotation
  * useful especially when the Guice context is not entirely
    isolated and there are other `GroupProvider` bindings in it
* An `@Inject`-able hook for cache invalidation

Author:    Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
Date:      Tue Jul 11 16:48:28 2023 +0200

plugin/trino-ldap-group-provider/src/main/java/io/trino/plugin/ldapgroup/LdapGroupProviderModule.java
@@ -16,6 +16,7 @@ package io.trino.plugin.ldapgroup;
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.trino.plugin.base.group.CachingGroupProviderModule;
 import io.trino.spi.security.GroupProvider;
 
 import static io.airlift.configuration.ConditionalModule.conditionalModule;
@@ -28,7 +29,7 @@ public class LdapGroupProviderModule
     protected void setup(Binder binder)
     {
         configBinder(binder).bindConfig(LdapGroupProviderConfig.class);
-        binder.bind(GroupProvider.class).to(LdapGroupProvider.class).in(Scopes.SINGLETON);
+        binder.bind(GroupProvider.class).annotatedWith(CachingGroupProviderModule.ForCachingGroupProvider.class).to(LdapGroupProvider.class).in(Scopes.SINGLETON);
         install(conditionalModule(
                 LdapGroupProviderConfig.class,
                 config -> config.getLdapUserMemberOfAttribute() != null,

trinodb/trino

plugin/trino-ldap-group-provider/src/main/java/io/trino/plugin/ldapgroup/LdapGroupProviderModule.java
@@ -16,6 +16,7 @@ package io.trino.plugin.ldapgroup;
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.trino.plugin.base.group.CachingGroupProviderModule;
 import io.trino.spi.security.GroupProvider;
 
 import static io.airlift.configuration.ConditionalModule.conditionalModule;
@@ -33,11 +34,11 @@ public class LdapGroupProviderModule
                 LdapGroupProviderConfig::getLdapUseGroupFilter,
                 innerBinder -> {
                     configBinder(innerBinder).bindConfig(LdapFilteringGroupProviderConfig.class);
-                    innerBinder.bind(GroupProvider.class).to(LdapFilteringGroupProvider.class).in(Scopes.SINGLETON);
+                    innerBinder.bind(GroupProvider.class).annotatedWith(CachingGroupProviderModule.ForCachingGroupProvider.class).to(LdapFilteringGroupProvider.class).in(Scopes.SINGLETON);
                 },
                 innerBinder -> {
                     configBinder(binder).bindConfig(LdapSingleQueryGroupProviderConfig.class);
-                    innerBinder.bind(GroupProvider.class).to(LdapSingleQueryGroupProvider.class).in(Scopes.SINGLETON);
+                    innerBinder.bind(GroupProvider.class).annotatedWith(CachingGroupProviderModule.ForCachingGroupProvider.class).to(LdapSingleQueryGroupProvider.class).in(Scopes.SINGLETON);
                 }));
     }
 }