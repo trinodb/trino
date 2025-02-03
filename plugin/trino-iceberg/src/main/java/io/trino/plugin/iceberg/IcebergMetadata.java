8f37aff5951c853ce1372873ecb24b6796d2be76
Yuya Ebihara <ebyhry@gmail.com>
2025-02-03 11:23:27 +0900

    Add support for deletion vector in Iceberg

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -132,6 +132,7 @@ import org.apache.datasketches.theta.CompactSketch;
 import org.apache.iceberg.AppendFiles;
 import org.apache.iceberg.BaseTable;
 import org.apache.iceberg.ContentFile;
+import org.apache.iceberg.ContentFileParsers;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.DataFiles;
 import org.apache.iceberg.DeleteFile;
@@ -162,6 +163,7 @@ import org.apache.iceberg.StatisticsFile;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.TableScan;
+import org.apache.iceberg.TableUtil;
 import org.apache.iceberg.Transaction;
 import org.apache.iceberg.UpdatePartitionSpec;
 import org.apache.iceberg.UpdateProperties;
@@ -1365,6 +1367,7 @@ public class IcebergMetadata
     {
         return new IcebergWritableTableHandle(
                 name,
+                TableUtil.formatVersion(table),
                 SchemaParser.toJson(table.schema()),
                 transformValues(table.specs(), PartitionSpecParser::toJson),
                 table.spec().specId(),
@@ -2972,7 +2975,11 @@ public class IcebergMetadata
     @Override
     public Optional<ConnectorPartitioningHandle> getUpdateLayout(ConnectorSession session, ConnectorTableHandle tableHandle)
     {
-        return getInsertLayout(session, tableHandle)
+        Optional<ConnectorTableLayout> insertLayout = getInsertLayout(session, tableHandle);
+        if (insertLayout.isEmpty() || insertLayout.get().getPartitioning().isEmpty()) {
+            return Optional.of(new IcebergPartitioningHandle(true, List.of()));
+        }
+        return insertLayout
                 .flatMap(ConnectorTableLayout::getPartitioning)
                 .map(IcebergPartitioningHandle.class::cast)
                 .map(IcebergPartitioningHandle::forUpdate);
@@ -3066,10 +3073,22 @@ public class IcebergMetadata
                 case POSITION_DELETES -> {
                     FileMetadata.Builder deleteBuilder = FileMetadata.deleteFileBuilder(partitionSpec)
                             .withPath(task.path())
-                            .withFormat(task.fileFormat().toIceberg())
+                            .withFormat(task.fileFormat())
                             .ofPositionDeletes()
                             .withFileSizeInBytes(task.fileSizeInBytes())
                             .withMetrics(task.metrics().metrics());
+
+                    if (task.fileFormat() == FileFormat.PUFFIN) {
+                        deleteBuilder.withRecordCount(task.metrics().recordCount());
+                        deleteBuilder.withContentOffset(task.contentOffset().orElseThrow(() -> new IllegalArgumentException("Content offset is required for deletion vector")));
+                        deleteBuilder.withContentSizeInBytes(task.contentSize().orElseThrow(() -> new IllegalArgumentException("Content size is required for deletion vector")));
+                        deleteBuilder.withReferencedDataFile(task.referencedDataFile().orElseThrow());
+                        for (String rewrittenDeleteFile : task.rewrittenDeleteFiles()) {
+                            DeleteFile deleteFile = (DeleteFile) ContentFileParsers.fromJson(rewrittenDeleteFile, partitionSpec);
+                            rowDelta.removeDeletes(deleteFile);
+                        }
+                    }
+
                     task.fileSplitOffsets().ifPresent(deleteBuilder::withSplitOffsets);
                     if (!partitionSpec.fields().isEmpty()) {
                         String partitionDataJson = task.partitionDataJson()
@@ -3083,7 +3102,7 @@ public class IcebergMetadata
                 case DATA -> {
                     DataFiles.Builder builder = DataFiles.builder(partitionSpec)
                             .withPath(task.path())
-                            .withFormat(task.fileFormat().toIceberg())
+                            .withFormat(task.fileFormat())
                             .withFileSizeInBytes(task.fileSizeInBytes())
                             .withMetrics(task.metrics().metrics());
                     if (!icebergTable.spec().fields().isEmpty()) {
