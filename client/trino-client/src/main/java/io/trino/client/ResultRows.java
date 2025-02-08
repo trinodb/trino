c095134d7a621b9348f2416ce9ad7b1a299c318e
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-02-08 15:36:28 +0100

    Clarify ResultRows usage

--- client/trino-client/src/main/java/io/trino/client/ResultRows.java
+++ client/trino-client/src/main/java/io/trino/client/ResultRows.java
@@ -23,6 +23,8 @@ import static java.util.Collections.emptyIterator;
 
 /**
  * Allows iterating over decoded result data in row-wise manner.
+ *
+ * Iterator can be acquired only once, and it should be closed after use.
  */
 public interface ResultRows
         extends Iterable<List<Object>>, Closeable
