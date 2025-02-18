f1a1a50f5325869a623225016bbf7dc4c97b8728
Dain Sundstrom <dain@iq80.com>
2025-02-17 17:04:47 -0800

    Convert variance aggregations to declared decomposition

--- core/trino-main/src/main/java/io/trino/operator/aggregation/VarianceAggregation.java
+++ core/trino-main/src/main/java/io/trino/operator/aggregation/VarianceAggregation.java
@@ -15,15 +15,21 @@ package io.trino.operator.aggregation;
 
 import io.trino.operator.aggregation.state.VarianceState;
 import io.trino.spi.block.BlockBuilder;
+import io.trino.spi.block.RowBlock;
+import io.trino.spi.block.RowBlockBuilder;
+import io.trino.spi.block.ValueBlock;
 import io.trino.spi.function.AggregationFunction;
 import io.trino.spi.function.AggregationState;
-import io.trino.spi.function.CombineFunction;
+import io.trino.spi.function.BlockIndex;
+import io.trino.spi.function.BlockPosition;
+import io.trino.spi.function.Decomposition;
 import io.trino.spi.function.Description;
 import io.trino.spi.function.InputFunction;
 import io.trino.spi.function.OutputFunction;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.type.StandardTypes;
 
+import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 
 @AggregationFunction
@@ -43,15 +49,33 @@ public final class VarianceAggregation
         state.update((double) value);
     }
 
-    @CombineFunction
-    public static void combine(@AggregationState VarianceState state, @AggregationState VarianceState otherState)
+    @InputFunction(hidden = true)
+    public static void intermediateInput(
+            @AggregationState VarianceState state,
+            @BlockPosition @SqlType("row(count bigint, mean double, m2 double)") ValueBlock block,
+            @BlockIndex int position)
     {
-        state.merge(otherState);
+        RowBlock rowBlock = (RowBlock) block;
+        state.merge(
+                BIGINT.getLong(rowBlock.getFieldBlock(0), position),
+                DOUBLE.getDouble(rowBlock.getFieldBlock(1), position),
+                DOUBLE.getDouble(rowBlock.getFieldBlock(2), position));
+    }
+
+    @AggregationFunction(value = "variance$intermediate", hidden = true)
+    @OutputFunction(value = "row(count bigint, mean double, m2 double)", decomposition = @Decomposition(partial = "variance$intermediate", output = "variance$intermediate"))
+    public static void intermediateOutput(@AggregationState VarianceState state, BlockBuilder out)
+    {
+        ((RowBlockBuilder) out).buildEntry(fieldBuilders -> {
+            BIGINT.writeLong(fieldBuilders.get(0), state.getCount());
+            DOUBLE.writeDouble(fieldBuilders.get(1), state.getMean());
+            DOUBLE.writeDouble(fieldBuilders.get(2), state.getM2());
+        });
     }
 
     @AggregationFunction(value = "variance", alias = "var_samp")
     @Description("Returns the sample variance of the argument")
-    @OutputFunction(StandardTypes.DOUBLE)
+    @OutputFunction(value = StandardTypes.DOUBLE, decomposition = @Decomposition(partial = "variance$intermediate", output = "variance"))
     public static void variance(@AggregationState VarianceState state, BlockBuilder out)
     {
         long count = state.getCount();
@@ -67,7 +91,7 @@ public final class VarianceAggregation
 
     @AggregationFunction("var_pop")
     @Description("Returns the population variance of the argument")
-    @OutputFunction(StandardTypes.DOUBLE)
+    @OutputFunction(value = StandardTypes.DOUBLE, decomposition = @Decomposition(partial = "variance$intermediate", output = "var_pop"))
     public static void variancePop(@AggregationState VarianceState state, BlockBuilder out)
     {
         long count = state.getCount();
@@ -83,7 +107,7 @@ public final class VarianceAggregation
 
     @AggregationFunction(value = "stddev", alias = "stddev_samp")
     @Description("Returns the sample standard deviation of the argument")
-    @OutputFunction(StandardTypes.DOUBLE)
+    @OutputFunction(value = StandardTypes.DOUBLE, decomposition = @Decomposition(partial = "variance$intermediate", output = "stddev"))
     public static void stddev(@AggregationState VarianceState state, BlockBuilder out)
     {
         long count = state.getCount();
@@ -100,7 +124,7 @@ public final class VarianceAggregation
 
     @AggregationFunction("stddev_pop")
     @Description("Returns the population standard deviation of the argument")
-    @OutputFunction(StandardTypes.DOUBLE)
+    @OutputFunction(value = StandardTypes.DOUBLE, decomposition = @Decomposition(partial = "variance$intermediate", output = "stddev_pop"))
     public static void stddevPop(@AggregationState VarianceState state, BlockBuilder out)
     {
         long count = state.getCount();
