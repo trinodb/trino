aa21912988dd2a55feaa3280e9c73c635665185a
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-02-25 12:59:27 +0100

    Extract QueryDataProducer creation to a factory

--- core/trino-main/src/main/java/io/trino/server/protocol/spooling/SpoolingServerModule.java
+++ core/trino-main/src/main/java/io/trino/server/protocol/spooling/SpoolingServerModule.java
@@ -24,6 +24,7 @@ import com.google.inject.multibindings.ProvidesIntoSet;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.trino.SystemSessionPropertiesProvider;
 import io.trino.server.ServerConfig;
+import io.trino.server.protocol.QueryDataProducerFactory;
 import io.trino.server.protocol.spooling.SpoolingConfig.SegmentRetrievalMode;
 import io.trino.spi.spool.SpoolingManager;
 
@@ -43,6 +44,7 @@ public class SpoolingServerModule
         install(new QueryDataEncodingModule());
 
         binder.bind(SpoolingManagerRegistry.class).in(Scopes.SINGLETON);
+        binder.bind(QueryDataProducerFactory.class).in(Scopes.SINGLETON);
         OptionalBinder<SpoolingManager> spoolingManagerBinder = newOptionalBinder(binder, new TypeLiteral<>() {});
         newOptionalBinder(binder, SpoolingConfig.class);
         SpoolingEnabledConfig spoolingEnabledConfig = buildConfigObject(SpoolingEnabledConfig.class);
