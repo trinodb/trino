359fb5c004c976afb5a8e8d5ef8b7bea6cca42c1
Maxim Korolyov <korolyov.maxim@gmail.com>
2025-03-02 15:35:22 +0100

    try without catalog

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/scylladb/TestScyllaDB.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/scylladb/TestScyllaDB.java
@@ -29,15 +29,15 @@ public class TestScyllaDB
     @Test(groups = {SCYLLADB, PROFILE_SPECIFIC_TESTS})
     public void testCreateTableAsSelect()
     {
-        onTrino().executeQuery("CALL cassandra.system.execute('CREATE KEYSPACE test WITH REPLICATION = {''class'':''SimpleStrategy'', ''replication_factor'': 1}')");
-        QueryResult result = onTrino().executeQuery("CREATE TABLE cassandra.test.nation AS SELECT * FROM tpch.tiny.nation");
+        onTrino().executeQuery("CALL system.execute('CREATE KEYSPACE test WITH REPLICATION = {''class'':''SimpleStrategy'', ''replication_factor'': 1}')");
+        QueryResult result = onTrino().executeQuery("CREATE TABLE test.nation AS SELECT * FROM tpch.tiny.nation");
         try {
             assertThat(result).updatedRowsCountIsEqualTo(25);
-            assertThat(onTrino().executeQuery("SELECT COUNT(*) FROM cassandra.test.nation"))
+            assertThat(onTrino().executeQuery("SELECT COUNT(*) FROM test.nation"))
                     .containsOnly(row(25));
         }
         finally {
-            onTrino().executeQuery("DROP TABLE cassandra.test.nation");
+            onTrino().executeQuery("DROP TABLE test.nation");
         }
     }
 }
