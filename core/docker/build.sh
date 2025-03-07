0f2f2053aec160ab522646a850b9adc2e316be13
Jan Waś <jan.was@starburstdata.com>
2025-03-07 16:02:03 +0100

    Allow building the core Docker image

--- core/docker/build.sh
+++ core/docker/build.sh
@@ -9,6 +9,8 @@ Builds the Trino Docker image
 
 -h       Display help
 -a       Build the specified comma-separated architectures, defaults to amd64,arm64,ppc64le
+-p       Use the specified server package (artifact id), for example: trino-server (default), trino-server-core
+-t       Image tag name, defaults to trino
 -r       Build the specified Trino release version, downloads all required artifacts
 -j       Build the Trino release with specified JDK distribution
 -x       Skip image tests
@@ -23,13 +25,15 @@ SOURCE_DIR="${SCRIPT_DIR}/../.."
 
 ARCHITECTURES=(amd64 arm64 ppc64le)
 TRINO_VERSION=
+TAG_PREFIX=trino
+SERVER_ARTIFACT=trino-server
 
 JDK_RELEASE=$(cat "${SOURCE_DIR}/core/jdk/current")
 JDKS_PATH="${SOURCE_DIR}/core/jdk"
 
 SKIP_TESTS=false
 
-while getopts ":a:h:r:j:x" o; do
+while getopts ":a:h:r:p:t:j:x" o; do
     case "${o}" in
         a)
             IFS=, read -ra ARCH_ARG <<< "$OPTARG"
@@ -43,6 +47,12 @@ while getopts ":a:h:r:j:x" o; do
             ;;
         r)
             TRINO_VERSION=${OPTARG}
+            ;;
+        p)
+            SERVER_ARTIFACT=${OPTARG}
+            ;;
+        t)
+            TAG_PREFIX=${OPTARG}
             ;;
         h)
             usage
@@ -89,17 +99,17 @@ check_environment
 
 if [ -n "$TRINO_VERSION" ]; then
     echo "🎣 Downloading server and client artifacts for release version ${TRINO_VERSION}"
-    for artifactId in io.trino:trino-server:"${TRINO_VERSION}":tar.gz io.trino:trino-cli:"${TRINO_VERSION}":jar:executable; do
+    for artifactId in "io.trino:${SERVER_ARTIFACT}:${TRINO_VERSION}:tar.gz" io.trino:trino-cli:"${TRINO_VERSION}":jar:executable; do
         "${SOURCE_DIR}/mvnw" -C dependency:get -Dtransitive=false -Dartifact="$artifactId"
     done
     local_repo=$("${SOURCE_DIR}/mvnw" -B help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
-    trino_server="$local_repo/io/trino/trino-server/${TRINO_VERSION}/trino-server-${TRINO_VERSION}.tar.gz"
+    trino_server="$local_repo/io/trino/${SERVER_ARTIFACT}/${TRINO_VERSION}/${SERVER_ARTIFACT}-${TRINO_VERSION}.tar.gz"
     trino_client="$local_repo/io/trino/trino-cli/${TRINO_VERSION}/trino-cli-${TRINO_VERSION}-executable.jar"
     chmod +x "$trino_client"
 else
     TRINO_VERSION=$("${SOURCE_DIR}/mvnw" -f "${SOURCE_DIR}/pom.xml" --quiet help:evaluate -Dexpression=project.version -DforceStdout)
-    echo "🎯 Using currently built artifacts from the core/trino-server and client/trino-cli modules and version ${TRINO_VERSION}"
-    trino_server="${SOURCE_DIR}/core/trino-server/target/trino-server-${TRINO_VERSION}.tar.gz"
+    echo "🎯 Using currently built artifacts from the core/${SERVER_ARTIFACT} and client/trino-cli modules and version ${TRINO_VERSION}"
+    trino_server="${SOURCE_DIR}/core/${SERVER_ARTIFACT}/target/${SERVER_ARTIFACT}-${TRINO_VERSION}.tar.gz"
     trino_client="${SOURCE_DIR}/client/trino-cli/target/trino-cli-${TRINO_VERSION}-executable.jar"
 fi
 
@@ -107,13 +117,16 @@ echo "🧱 Preparing the image build context directory"
 WORK_DIR="$(mktemp -d)"
 cp "$trino_server" "${WORK_DIR}/"
 cp "$trino_client" "${WORK_DIR}/trino-cli.jar"
-tar -C "${WORK_DIR}" -xzf "${WORK_DIR}/trino-server-${TRINO_VERSION}.tar.gz"
-rm "${WORK_DIR}/trino-server-${TRINO_VERSION}.tar.gz"
-mv "${WORK_DIR}/trino-server-${TRINO_VERSION}" "${WORK_DIR}/trino-server"
+tar -C "${WORK_DIR}" -xzf "${WORK_DIR}/${SERVER_ARTIFACT}-${TRINO_VERSION}.tar.gz"
+rm "${WORK_DIR}/${SERVER_ARTIFACT}-${TRINO_VERSION}.tar.gz"
+mv "${WORK_DIR}/${SERVER_ARTIFACT}-${TRINO_VERSION}" "${WORK_DIR}/trino-server"
 cp -R bin "${WORK_DIR}/trino-server"
 cp -R default "${WORK_DIR}/"
+if [ "${SERVER_ARTIFACT}" != "trino-server" ]; then
+    rm -rf "${WORK_DIR}"/default/etc/catalog/*.properties
+fi
 
-TAG_PREFIX="trino:${TRINO_VERSION}"
+TAG="${TAG_PREFIX}:${TRINO_VERSION}"
 
 for arch in "${ARCHITECTURES[@]}"; do
     echo "🫙  Building the image for $arch with JDK ${JDK_RELEASE}"
@@ -126,7 +139,7 @@ for arch in "${ARCHITECTURES[@]}"; do
         --build-arg JDK_DOWNLOAD_LINK="$(jdk_download_link "${JDKS_PATH}/${JDK_RELEASE}" "${arch}")" \
         --platform "linux/$arch" \
         -f Dockerfile \
-        -t "${TAG_PREFIX}-$arch"
+        -t "${TAG}-$arch"
 done
 
 echo "🧹 Cleaning up the build context directory"
@@ -139,8 +152,8 @@ else
   echo
   source container-test.sh
   for arch in "${ARCHITECTURES[@]}"; do
-      test_container "${TAG_PREFIX}-$arch" "linux/$arch"
-      docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' "${TAG_PREFIX}-$arch"
+      test_container "${TAG}-$arch" "linux/$arch"
+      docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' "${TAG}-$arch"
   done
 fi
 
