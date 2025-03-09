26a4cfd22b6b91b032a916739c158372b48fd21d
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2025-03-09 11:15:40 +0000

    Add query details main component with tabs

--- core/trino-web-ui/src/main/resources/webapp-preview/src/api/webapp/api.ts
+++ core/trino-web-ui/src/main/resources/webapp-preview/src/api/webapp/api.ts
@@ -94,17 +94,37 @@ export interface QueryStats {
     finishingTime: string
     fullyBlocked: boolean
     internalNetworkInputDataSize: string
+    failedInternalNetworkInputDataSize: string
     peakTotalMemoryReservation: string
     peakUserMemoryReservation: string
+    peakRevocableMemoryReservation: string
+    physicalInputPositions: number
+    failedPhysicalInputPositions: number
     physicalInputDataSize: string
+    failedPhysicalInputDataSize: string
     physicalInputReadTime: string
+    failedPhysicalInputReadTime: string
     physicalWrittenDataSize: string
+    failedPhysicalWrittenDataSize: string
+    internalNetworkInputPositions: number
+    failedInternalNetworkInputPositions: number
     planningTime: string
+    planningCpuTime: string
     progressPercentage: number
     queuedDrivers: number
     queuedTime: string
     rawInputDataSize: string
     rawInputPositions: number
+    processedInputPositions: number
+    failedProcessedInputPositions: number
+    processedInputDataSize: string
+    failedProcessedInputDataSize: string
+    outputPositions: number
+    failedOutputPositions: number
+    outputDataSize: string
+    failedOutputDataSize: string
+    writtenPositions: number
+    logicalWrittenDataSize: string
     runningDrivers: number
     runningPercentage: number
     spilledDataSize: string
@@ -116,26 +136,84 @@ export interface QueryStats {
     blockedReasons: string[]
 }
 
-export interface QueryInfo {
-    clientTags: string[]
+export interface Warning {
+    message: string
+    warningCode: {
+        name: string
+    }
+}
+
+export interface StackInfo {
+    message: string
+    type: string
+    suppressed: StackInfo[]
+    stack: string[]
+    cause: StackInfo
+}
+
+export interface QueryInfoBase {
     queryId: string
     queryStats: QueryStats
-    queryTextPreview: string
     queryType: string
+    state: string
+    scheduled: boolean
+    memoryPool: string
+    errorType: string
+    errorCode: {
+        code: string
+        name: string
+    }
+    warnings: Warning[]
+    failureInfo: StackInfo
+}
+
+export interface QueryInfo extends QueryInfoBase {
+    clientTags: string[]
+    queryTextPreview: string
     resourceGroupId: string[]
     retryPolicy: string
-    scheduled: boolean
     self: string
     sessionPrincipal: string
     sessionSource: string
     sessionUser: string
-    state: string
-    memoryPool: string
     queryDataEncoding: string
-    errorType: string
-    errorCode: {
-        name: string
-    }
+}
+
+export interface Session {
+    queryId: string
+    transactionId: string
+    clientTransactionSupport: boolean
+    user: string
+    originalUser: string
+    groups: string[]
+    originalUserGroups: string[]
+    principal: string
+    enabledRoles: string[]
+    source: string
+    catalog: string
+    schema: string
+    timeZoneKey: number
+    locale: string
+    remoteUserAddress: string
+    userAgent: string
+    clientTags: string[]
+    clientCapabilities: string[]
+    start: string
+    protocolName: string
+    timeZone: string
+    queryDataEncoding: string
+    systemProperties: { [key: string]: string | number | boolean }
+    catalogProperties: { [key: string]: string | number | boolean }
+}
+
+export interface QueryStatusInfo extends QueryInfoBase {
+    session: Session
+    query: string
+    preparedQuery: string
+    resourceGroupId: string[]
+    retryPolicy: string
+    pruned: boolean
+    finalQueryInfo: boolean
 }
 
 export async function statsApi(): Promise<ApiResponse<Stats>> {
@@ -152,4 +230,8 @@ export async function workerStatusApi(nodeId: string): Promise<ApiResponse<Worke
 
 export async function queryApi(): Promise<ApiResponse<QueryInfo[]>> {
     return await api.get<QueryInfo[]>('/ui/api/query')
+}
+
+export async function queryStatusApi(queryId: string): Promise<ApiResponse<QueryStatusInfo>> {
+    return await api.get<QueryStatusInfo>(`/ui/api/query/${queryId}`)
 }
