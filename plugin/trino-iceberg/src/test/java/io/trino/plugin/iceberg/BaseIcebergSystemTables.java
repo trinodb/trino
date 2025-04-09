4b425a315c4c7631903e2b4283dd9f58562cbc29
Yuya Ebihara <ebyhry@gmail.com>
2025-04-09 09:21:58 +0900

    Rename tests in BaseIcebergSystemTables

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/BaseIcebergSystemTables.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/BaseIcebergSystemTables.java
@@ -134,7 +134,7 @@ public abstract class BaseIcebergSystemTables
     }
 
     @Test
-    public void testPartitionTable()
+    public void testPartitionsTable()
     {
         assertQuery("SELECT count(*) FROM test_schema.test_table", "VALUES 6");
         assertQuery("SHOW COLUMNS FROM test_schema.\"test_table$partitions\"",
@@ -162,7 +162,7 @@ public abstract class BaseIcebergSystemTables
     }
 
     @Test
-    public void testPartitionTableWithNan()
+    public void testPartitionsTableWithNan()
     {
         assertQuery("SELECT count(*) FROM test_schema.test_table_nan", "VALUES 6");
 
@@ -198,7 +198,7 @@ public abstract class BaseIcebergSystemTables
     }
 
     @Test
-    public void testPartitionTableOnDropColumn()
+    public void testPartitionsTableOnDropColumn()
     {
         MaterializedResult resultAfterDrop = computeActual("SELECT * from test_schema.\"test_table_drop_column$partitions\"");
         assertThat(resultAfterDrop.getRowCount()).isEqualTo(3);
@@ -768,7 +768,7 @@ public abstract class BaseIcebergSystemTables
     }
 
     @Test
-    public void testPartitionColumns()
+    public void testPartitionsColumns()
     {
         try (TestTable testTable = newTrinoTable("test_partition_columns", """
                 WITH (partitioning = ARRAY[
