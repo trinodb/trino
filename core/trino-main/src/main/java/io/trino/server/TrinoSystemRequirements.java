bfa589b27bf38da04980f2bd29556140f0614bdf
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-05-17 18:57:12 +0200

    Require Java 24 to build and run Trino

--- core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
+++ core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
@@ -100,7 +100,7 @@ final class TrinoSystemRequirements
 
     private static void verifyJavaVersion()
     {
-        Version required = Version.parse("23");
+        Version required = Version.parse("24.0.1");
         if (Runtime.version().compareTo(required) < 0) {
             failRequirement("Trino requires Java %s at minimum (found %s)", required, Runtime.version());
         }
