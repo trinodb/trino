4444b507df935ffd5887bf8aaae90317d95e440b
Huw Ayling-Miller <mail@huw.org.uk>
2025-05-18 16:25:06 +0100

    Add unit test for createViewSession when called from a connector

--- core/trino-main/src/test/java/io/trino/TestSession.java
+++ core/trino-main/src/test/java/io/trino/TestSession.java
@@ -13,9 +13,17 @@
  */
 package io.trino;
 
+import io.trino.connector.MockConnectorFactory;
 import io.trino.spi.QueryId;
+import io.trino.spi.connector.ConnectorSession;
+import io.trino.spi.connector.ConnectorViewDefinition;
+import io.trino.spi.connector.SchemaTableName;
+import io.trino.spi.connector.SchemaTablePrefix;
 import io.trino.spi.security.Identity;
 import io.trino.spi.type.TimeZoneKey;
+import io.trino.spi.type.TypeId;
+import io.trino.sql.planner.TestingConnectorTransactionHandle;
+import io.trino.testing.TestingConnectorContext;
 import io.trino.transaction.TransactionId;
 import org.junit.jupiter.api.Test;
 
@@ -24,8 +32,12 @@ import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 import static io.trino.testing.TestingSession.testSessionBuilder;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestSession
@@ -117,5 +129,59 @@ public class TestSession
         assertThat(viewSession.getClientInfo()).isEqualTo(clientInfo);
         assertThat(viewSession.getTraceToken()).isEqualTo(traceToken);
         assertThat(viewSession.getStart()).isEqualTo(start);
+    }
+
+    @Test
+    public void testCreateViewSessionWithinView()
+    {
+        final var catalog = Optional.of("test_catalog");
+        final var queryId = "test_query_id";
+        final var originalIdentity = new Identity.Builder("test_original_user").build();
+        final var source = Optional.of("test_source");
+        final var timeZoneKey = TimeZoneKey.UTC_KEY;
+        final var locale = Locale.ENGLISH;
+        final var traceToken = Optional.of("test_trace_token");
+        final var start = Instant.ofEpochMilli(2L);
+        final var schemaTableName = new SchemaTableName("test_schema", "test_table");
+        final var viewDefinition = new ConnectorViewDefinition(
+                "", Optional.empty(), Optional.empty(),
+                singletonList(new ConnectorViewDefinition.ViewColumn("test_column", TypeId.of("test_type"), Optional.empty())),
+                Optional.empty(), Optional.empty(), true, emptyList());
+
+        Session originalSession = Session.builder(testSessionBuilder().build())
+                .setQueryId(new QueryId(queryId))
+                .setOriginalIdentity(originalIdentity)
+                .setSource(source)
+                .setTimeZoneKey(timeZoneKey)
+                .setLocale(locale)
+                .setTraceToken(traceToken)
+                .setStart(start)
+                .build();
+
+        ConnectorSession connectorSession = originalSession.toConnectorSession();
+
+        final BiFunction<ConnectorSession, SchemaTablePrefix, Map<SchemaTableName, ConnectorViewDefinition>>
+                viewAssertion = (viewSession, schemaTablePrefix) -> {
+                    assertThat(viewSession).isNotNull();
+                    assertThat(viewSession.getQueryId()).isEqualTo(queryId);
+                    assertThat(viewSession.getSource()).isEqualTo(source);
+                    assertThat(viewSession.getTimeZoneKey()).isEqualTo(timeZoneKey);
+                    assertThat(viewSession.getLocale()).isEqualTo(locale);
+                    assertThat(viewSession.getTraceToken()).isEqualTo(traceToken);
+                    assertThat(viewSession.getStart()).isEqualTo(start);
+
+                    return singletonMap(schemaTableName, viewDefinition);
+                };
+
+        final var connector = MockConnectorFactory.builder()
+                .withGetViews(viewAssertion)
+                .build()
+                .create(catalog.get(), Collections.emptyMap(), new TestingConnectorContext());
+
+        final var view = connector.getMetadata(connectorSession, TestingConnectorTransactionHandle.INSTANCE)
+                .getView(connectorSession, schemaTableName);
+
+        // Ensure that the custom getViews has been called
+        assertThat(view).contains(viewDefinition);
     }
 }
