b4b2d9a3d285efca71298acea8f181d775b964bd
chenjian2664 <chenjian2664@gmail.com>
2025-06-11 14:21:36 +0800

    Add support for Databricks 16.4 LTS environment to product tests

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteDeltaLakeDatabricks164.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteDeltaLakeDatabricks164.java
@@ -15,25 +15,27 @@ package io.trino.tests.product.launcher.suite.suites;
 
 import com.google.common.collect.ImmutableList;
 import io.trino.tests.product.launcher.env.EnvironmentConfig;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeDeltaLakeDatabricks154;
+import io.trino.tests.product.launcher.env.environment.EnvSinglenodeDeltaLakeDatabricks164;
 import io.trino.tests.product.launcher.suite.SuiteDeltaLakeDatabricks;
 import io.trino.tests.product.launcher.suite.SuiteTestRun;
 
 import java.util.List;
 
 import static io.trino.tests.product.TestGroups.CONFIGURED_FEATURES;
-import static io.trino.tests.product.TestGroups.DELTA_LAKE_DATABRICKS_154;
+import static io.trino.tests.product.TestGroups.DELTA_LAKE_DATABRICKS;
+import static io.trino.tests.product.TestGroups.DELTA_LAKE_EXCLUDE_164;
 import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
 
-public class SuiteDeltaLakeDatabricks154
+public class SuiteDeltaLakeDatabricks164
         extends SuiteDeltaLakeDatabricks
 {
     @Override
     public List<SuiteTestRun> getTestRuns(EnvironmentConfig config)
     {
         return ImmutableList.of(
-                testOnEnvironment(EnvSinglenodeDeltaLakeDatabricks154.class)
-                        .withGroups(CONFIGURED_FEATURES, DELTA_LAKE_DATABRICKS_154)
+                testOnEnvironment(EnvSinglenodeDeltaLakeDatabricks164.class)
+                        .withGroups(CONFIGURED_FEATURES, DELTA_LAKE_DATABRICKS)
+                        .withExcludedGroups(DELTA_LAKE_EXCLUDE_164)
                         .withExcludedTests(getExcludedTests())
                         .build());
     }
