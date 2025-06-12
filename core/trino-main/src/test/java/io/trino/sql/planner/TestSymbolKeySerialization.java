0079a6f907c8c3ad9a7eb38709920f8ce9e1abb6
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-06-12 10:56:45 +0200

    Simplify symbol key encoding to avoid splitting and base64-encoding
    New format is: type length (int) : symbol.type() (varchar) : symbol.name() (varchar)

--- core/trino-main/src/test/java/io/trino/sql/planner/TestSymbolKeySerialization.java
+++ core/trino-main/src/test/java/io/trino/sql/planner/TestSymbolKeySerialization.java
@@ -26,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TestSymbolKeySerialization
 {
@@ -39,10 +40,38 @@ class TestSymbolKeySerialization
     {
         Map<Symbol, String> symbols = Map.of(
                 new Symbol(TYPE_MANAGER.getType(TypeId.of("integer")), "a"), "value",
-                new Symbol(TYPE_MANAGER.getType(TypeId.of("varchar")), "b"), "value2");
+                new Symbol(TYPE_MANAGER.getType(TypeId.of("varchar")), "b"), "value",
+                new Symbol(TYPE_MANAGER.getType(TypeId.of("integer")), "abcd"), "value",
+                new Symbol(TYPE_MANAGER.getType(TypeId.of("integer")), "1abcd"), "value",
+                new Symbol(TYPE_MANAGER.getType(TypeId.of("varchar")), "b".repeat(256)), "value",
+                new Symbol(TYPE_MANAGER.getType(TypeId.of("id")), "a"), "value");
 
         assertThat(SYMBOL_KEY_CODEC.fromJson(SYMBOL_KEY_CODEC.toJson(symbols)))
                 .isEqualTo(symbols);
+
+        assertThat(SYMBOL_KEY_CODEC.toJson(symbols))
+                .contains("7|integer|a")
+                .contains("7|varchar|b")
+                .contains("7|varchar|%s".formatted("b".repeat(256)))
+                .contains("7|integer|1abcd")
+                .contains("7|integer|abcd")
+                .contains("2|id|a");
+    }
+
+    @Test
+    void testMalformedSymbolKey()
+    {
+        assertThatThrownBy(() -> SYMBOL_KEY_CODEC.fromJson("{\"1|a|\":\"value\"}"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Symbol key is malformed: 1|a|");
+
+        assertThatThrownBy(() -> SYMBOL_KEY_CODEC.fromJson("{\"256|a|\":\"value\"}"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Symbol key is malformed: 256|a|");
+
+        assertThatThrownBy(() -> SYMBOL_KEY_CODEC.fromJson("{\"1|a\":\"value\"}"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Symbol key is malformed: 1|a");
     }
 
     private static ObjectMapperProvider createObjectMapperProvider(TestingTypeManager typeManager)
