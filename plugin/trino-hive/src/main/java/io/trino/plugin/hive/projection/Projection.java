e8232e789f713ac27768fb5ead0a901f7c464d3c
chenjian2664 <chenjian2664@gmail.com>
2025-06-20 09:45:55 +0800

    Fix reading specifying format for date partition projection

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/projection/Projection.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/projection/Projection.java
@@ -22,4 +22,9 @@ sealed interface Projection
         permits DateProjection, EnumProjection, InjectedProjection, IntegerProjection
 {
     List<String> getProjectedValues(Optional<Domain> partitionValueFilter);
+
+    default String transformValue(String value)
+    {
+        return value;
+    }
 }
