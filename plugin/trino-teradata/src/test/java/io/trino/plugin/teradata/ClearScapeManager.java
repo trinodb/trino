c47bc5a4339dbd266cff912f0de0da719a796271
Dabbiru Vasudha <Dabbiru.vasudha@teradata.com>
2025-07-28 16:16:03 +0530

    commit

--- plugin/trino-teradata/src/test/java/io/trino/plugin/teradata/ClearScapeManager.java
+++ plugin/trino-teradata/src/test/java/io/trino/plugin/teradata/ClearScapeManager.java
@@ -1,6 +1,7 @@
 package io.trino.plugin.teradata;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
