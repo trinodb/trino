0ae9327d84b4d9518163f9ce9ccd4da53b8270bb
Sasha Sheikin <myminitrue@gmail.com>
2025-08-29 14:39:26 +0200

    Repackage SingletonIdentityCacheMapping

--- lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/cache/identity/SingletonIdentityCacheMapping.java
+++ lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/cache/identity/SingletonIdentityCacheMapping.java
@@ -11,9 +11,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.jdbc;
+package io.trino.plugin.base.cache.identity;
 
-import io.trino.plugin.base.cache.identity.IdentityCacheMapping;
 import io.trino.spi.connector.ConnectorSession;
 
 public final class SingletonIdentityCacheMapping
