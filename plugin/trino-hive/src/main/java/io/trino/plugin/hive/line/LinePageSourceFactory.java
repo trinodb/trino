f82e822ffb6e4ab0c223a891ea917b2a17f100ef
Raunaq Morarka <raunaqmorarka@gmail.com>
2025-09-12 01:12:28 +0530

    Pre-populate file length and modifiedTime for TextLineReader
    Allows S3InputStream#closeStream to avoid aborting connections
    Allows filsystem cache to avoid extra FS operations

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/line/LinePageSourceFactory.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/line/LinePageSourceFactory.java
@@ -96,7 +96,7 @@ public abstract class LinePageSourceFactory
 
         checkArgument(acidInfo.isEmpty(), "Acid is not supported");
 
-        return Optional.of(projectColumnDereferences(columns, baseColumns -> createPageSource(session, path, start, length, estimatedFileSize, schema, baseColumns)));
+        return Optional.of(projectColumnDereferences(columns, baseColumns -> createPageSource(session, path, start, length, estimatedFileSize, fileModifiedTime, schema, baseColumns)));
     }
 
     private ConnectorPageSource createPageSource(
@@ -105,6 +105,7 @@ public abstract class LinePageSourceFactory
             long start,
             long length,
             long estimatedFileSize,
+            long fileModifiedTime,
             Schema schema,
             List<HiveColumnHandle> columns)
     {
@@ -129,7 +130,7 @@ public abstract class LinePageSourceFactory
         }
 
         TrinoFileSystem trinoFileSystem = fileSystemFactory.create(session);
-        TrinoInputFile inputFile = trinoFileSystem.newInputFile(path);
+        TrinoInputFile inputFile = lineReaderFactory.newInputFile(trinoFileSystem, path, estimatedFileSize, fileModifiedTime);
         try {
             // buffer file if small
             long smallFileReadTimeNanos = 0;
