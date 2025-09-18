871177122478e4c7a22786e0ca43ffaec7165993
Marcin Rusek <marcin.rusek@starburstdata.com>
2025-09-18 12:42:15 +0200

    Set Calendar type used for temporal types in ORC footer

--- testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
@@ -50,6 +50,7 @@ public final class TestGroups
     public static final String HDFS_IMPERSONATION = "hdfs_impersonation";
     public static final String HDFS_NO_IMPERSONATION = "hdfs_no_impersonation";
     public static final String HIVE_GCS = "hive_gcs";
+    public static final String HIVE4 = "hive4";
     public static final String HIVE_SPARK = "hive_spark";
     public static final String HIVE_SPARK_NO_STATS_FALLBACK = "hive_spark_no_stats_fallback";
     public static final String HIVE_COMPRESSION = "hive_compression";
