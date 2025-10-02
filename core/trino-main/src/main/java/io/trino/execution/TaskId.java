6e739acb6380e72099f6832c181244353c119c2d
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-02 19:42:10 +0200

    Inline fullId to toString method

--- core/trino-main/src/main/java/io/trino/execution/TaskId.java
+++ core/trino-main/src/main/java/io/trino/execution/TaskId.java
@@ -18,16 +18,14 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import io.trino.spi.QueryId;
 
 import java.util.List;
-import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static io.airlift.slice.SizeOf.estimatedSizeOf;
 import static io.airlift.slice.SizeOf.instanceSize;
 import static io.trino.spi.QueryId.parseDottedId;
 import static java.lang.Integer.parseInt;
 import static java.util.Objects.requireNonNull;
 
-public record TaskId(String fullId, StageId stageId, int partitionId, int attemptId)
+public record TaskId(StageId stageId, int partitionId, int attemptId)
 {
     private static final int INSTANCE_SIZE = instanceSize(TaskId.class);
 
@@ -35,20 +33,14 @@ public record TaskId(String fullId, StageId stageId, int partitionId, int attemp
     public static TaskId valueOf(String fullId)
     {
         List<String> parts = parseDottedId(fullId, 4, "taskId");
-        return new TaskId(fullId, new StageId(new QueryId(parts.get(0)), parseInt(parts.get(1))), parseInt(parts.get(2)), parseInt(parts.get(3)));
+        return new TaskId(new StageId(new QueryId(parts.get(0)), parseInt(parts.get(1))), parseInt(parts.get(2)), parseInt(parts.get(3)));
     }
 
     public TaskId
     {
-        requireNonNull(fullId, "fullId is null");
         requireNonNull(stageId, "stageId is null");
         checkArgument(partitionId >= 0, "partitionId is negative: %s", partitionId);
         checkArgument(attemptId >= 0, "attemptId is negative: %s", attemptId);
-    }
-
-    public TaskId(StageId stageId, int partitionId, int attemptId)
-    {
-        this("%s.%s.%s".formatted(stageId.toString(), String.valueOf(partitionId), String.valueOf(attemptId)), stageId, partitionId, attemptId);
     }
 
     public QueryId queryId()
@@ -60,30 +52,11 @@ public record TaskId(String fullId, StageId stageId, int partitionId, int attemp
     @JsonValue
     public String toString()
     {
-        return fullId;
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(fullId);
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        TaskId other = (TaskId) obj;
-        return Objects.equals(this.fullId, other.fullId);
+        return stageId.queryId().id() + '.' + stageId.id() + '.' + partitionId + '.' + attemptId;
     }
 
     public long getRetainedSizeInBytes()
     {
-        return INSTANCE_SIZE + estimatedSizeOf(fullId);
+        return INSTANCE_SIZE;
     }
 }
