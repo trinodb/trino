265ff2c5d570fa6b3b6f810dc6497303642b345d
codluca <lcodrutlucian@gmail.com>
2025-10-10 23:05:08 +0300

    Add procedures support for Lakehouse
    LakehouseModule module declares the procedures from Hive, DeltaLake and Iceberg.
    If the same procedure appears in two modules, a wrapper procedure is declared in the subpackage 'procedures'.
    The new procedure will have the extra first parameter named 'tableType" (possible values 'HIVE', 'DELTA" and 'ICEBERG').
    Based on the 'tableType', the wrapper procedure delegates to the actual procedure from the concrete module.

--- plugin/trino-lakehouse/src/test/java/io/trino/plugin/lakehouse/TestLakehouseDeltaConnectorSmokeTest.java
+++ plugin/trino-lakehouse/src/test/java/io/trino/plugin/lakehouse/TestLakehouseDeltaConnectorSmokeTest.java
@@ -21,6 +21,8 @@ import static io.trino.plugin.deltalake.DeltaLakeTableType.PARTITIONS;
 import static io.trino.plugin.deltalake.DeltaLakeTableType.PROPERTIES;
 import static io.trino.plugin.deltalake.DeltaLakeTableType.TRANSACTIONS;
 import static io.trino.plugin.lakehouse.TableType.DELTA;
+import static io.trino.testing.TestingNames.randomNameSuffix;
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -86,5 +88,28 @@ public class TestLakehouseDeltaConnectorSmokeTest
                 .failure().hasMessageMatching(".* Table .* does not exist");
         assertThat(query("SELECT count(*) FROM lakehouse.tpch.\"region$timeline\""))
                 .failure().hasMessageMatching(".* Table .* does not exist");
+    }
+
+    @Test
+    void testProcedures()
+    {
+        String tableName = "table_for_procedures_" + randomNameSuffix();
+
+        assertUpdate(format("CREATE TABLE %s AS SELECT 2 AS age", tableName), 1);
+
+        assertThat(query(format("CALL lakehouse.system.vacuum(CURRENT_SCHEMA, '%s', '8.00d')", tableName)))
+                .succeeds().returnsEmptyResult();
+
+        assertThat(query(format("CALL lakehouse.system.drop_stats('DELTA', CURRENT_SCHEMA, '%s')", tableName)))
+                .succeeds().returnsEmptyResult();
+
+        assertThat(query(format("CALL lakehouse.system.register_table('DELTA', CURRENT_SCHEMA, '%s', 's3://bucket/table')", tableName)))
+                .failure().hasMessage("Failed checking table location s3://bucket/table");
+
+        assertThat(query(format("CALL lakehouse.system.unregister_table('DELTA', CURRENT_SCHEMA, '%s')", tableName)))
+                .succeeds().returnsEmptyResult();
+
+        assertThat(query(format("CALL lakehouse.system.flush_metadata_cache('DELTA', CURRENT_SCHEMA, '%s')", tableName)))
+                .succeeds().returnsEmptyResult();
     }
 }
