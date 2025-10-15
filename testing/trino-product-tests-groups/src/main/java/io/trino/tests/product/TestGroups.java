ee6c140d474a023f99a0439498e219280344b10c
Yuya Ebihara <ebyhry@gmail.com>
2025-10-15 09:09:55 +0900

    Stop running product tests with Databricks 11.3 LTS

--- testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
@@ -95,11 +95,9 @@ public final class TestGroups
     public static final String DELTA_LAKE_AZURE = "delta-lake-azure";
     public static final String DELTA_LAKE_GCS = "delta-lake-gcs";
     public static final String DELTA_LAKE_DATABRICKS = "delta-lake-databricks";
-    public static final String DELTA_LAKE_DATABRICKS_122 = "delta-lake-databricks-122";
     public static final String DELTA_LAKE_DATABRICKS_133 = "delta-lake-databricks-133";
     public static final String DELTA_LAKE_DATABRICKS_143 = "delta-lake-databricks-143";
     public static final String DELTA_LAKE_DATABRICKS_154 = "delta-lake-databricks-154";
-    public static final String DELTA_LAKE_EXCLUDE_113 = "delta-lake-exclude-113";
     // TODO: Remove it once we support generatedColumns, particularly for writes in Delta Lake
     public static final String DELTA_LAKE_EXCLUDE_164 = "delta-lake-exclude-164";
     public static final String DELTA_LAKE_ALLUXIO_CACHING = "delta-lake-alluxio-caching";
