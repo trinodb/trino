4258ddf2228a1f239741a0a9a24f6386f7e3900c
Konrad Dziedzic <konraddziedzic@gmail.com>
2025-10-16 18:38:30 +0200

    Load initial catalogs in TestingTrinoServer

--- testing/trino-tests/src/test/java/io/trino/connector/system/TestSystemMetadataCatalogTable.java
+++ testing/trino-tests/src/test/java/io/trino/connector/system/TestSystemMetadataCatalogTable.java
@@ -14,15 +14,9 @@
 package io.trino.connector.system;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.inject.Key;
 import io.trino.Session;
-import io.trino.connector.ConnectorServicesProvider;
-import io.trino.metadata.CatalogManager;
+import io.trino.connector.TestDynamicCatalogs;
 import io.trino.plugin.memory.MemoryPlugin;
-import io.trino.spi.catalog.CatalogName;
-import io.trino.spi.catalog.CatalogProperties;
-import io.trino.spi.catalog.CatalogStore;
-import io.trino.spi.connector.ConnectorName;
 import io.trino.testing.AbstractTestQueryFramework;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.QueryRunner;
@@ -36,23 +30,18 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
 public class TestSystemMetadataCatalogTable
         extends AbstractTestQueryFramework
 {
-    private CatalogStore catalogStore;
-    private ConnectorServicesProvider connectorServicesProvider;
-    private CatalogManager catalogManager;
-
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
         Session session = testSessionBuilder().build();
         QueryRunner queryRunner = DistributedQueryRunner.builder(session)
+                .setAdditionalModule(new TestDynamicCatalogs.TestCatalogStoreModule())
+                .setCoordinatorProperties(ImmutableMap.of("catalog.store", "prepopulated_memory"))
                 .setWorkerCount(0)
                 .build();
         queryRunner.installPlugin(new MemoryPlugin());
         queryRunner.createCatalog("healthy_catalog", "memory", ImmutableMap.of("memory.max-data-per-node", "128MB"));
-        catalogStore = queryRunner.getCoordinator().getInstance(new Key<>() {});
-        connectorServicesProvider = queryRunner.getCoordinator().getInstance(new Key<>() {});
-        catalogManager = queryRunner.getCoordinator().getInstance(new Key<>() {});
         return queryRunner;
     }
 
@@ -61,11 +50,13 @@ public class TestSystemMetadataCatalogTable
     {
         assertQuery("SELECT * FROM system.metadata.catalogs", "VALUES" +
                 "('healthy_catalog', 'healthy_catalog', 'memory', 'OPERATIONAL'), " +
+                "('broken_catalog', 'broken_catalog', 'memory', 'FAILING'), " +
                 "('system', 'system', 'system', 'OPERATIONAL')");
 
         assertUpdate("CREATE CATALOG brain USING memory WITH (\"memory.max-data-per-node\" = '128MB')");
         assertQuery("SELECT * FROM system.metadata.catalogs", "VALUES" +
                 "('healthy_catalog', 'healthy_catalog', 'memory', 'OPERATIONAL'), " +
+                "('broken_catalog', 'broken_catalog', 'memory', 'FAILING'), " +
                 "('brain', 'brain', 'memory', 'OPERATIONAL'), " +
                 "('system', 'system', 'system', 'OPERATIONAL')");
 
@@ -75,14 +66,9 @@ public class TestSystemMetadataCatalogTable
     @Test
     public void testCatalogNotLoadedCorrectly()
     {
-        CatalogProperties catalogProperties = catalogStore.createCatalogProperties(new CatalogName("broken"), new ConnectorName("memory"), ImmutableMap.of("memory.max-data-per-n", "128MB"));
-
-        catalogStore.addOrReplaceCatalog(catalogProperties);
-        connectorServicesProvider.loadInitialCatalogs();
         assertQuery("SELECT * FROM system.metadata.catalogs", "VALUES" +
                 "('healthy_catalog', 'healthy_catalog', 'memory', 'OPERATIONAL'), " +
-                "('broken', 'broken', 'memory', 'FAILING'), " +
+                "('broken_catalog', 'broken_catalog', 'memory', 'FAILING'), " +
                 "('system', 'system', 'system', 'OPERATIONAL')");
-        catalogManager.dropCatalog(new CatalogName("broken"), false);
     }
 }
