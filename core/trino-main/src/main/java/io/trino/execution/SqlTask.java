a0c208a814a896b1a17b3455cc7e43024a86afaf
James Petty <pettja@amazon.com>
2025-10-22 10:29:07 -0400

    Make PipelineStatus a record class

--- core/trino-main/src/main/java/io/trino/execution/SqlTask.java
+++ core/trino-main/src/main/java/io/trino/execution/SqlTask.java
@@ -363,10 +363,10 @@ public class SqlTask
             TaskContext taskContext = taskHolder.getTaskExecution().getTaskContext();
             for (PipelineContext pipelineContext : taskContext.getPipelineContexts()) {
                 PipelineStatus pipelineStatus = pipelineContext.getPipelineStatus();
-                queuedPartitionedDrivers += pipelineStatus.getQueuedPartitionedDrivers();
-                queuedPartitionedSplitsWeight += pipelineStatus.getQueuedPartitionedSplitsWeight();
-                runningPartitionedDrivers += pipelineStatus.getRunningPartitionedDrivers();
-                runningPartitionedSplitsWeight += pipelineStatus.getRunningPartitionedSplitsWeight();
+                queuedPartitionedDrivers += pipelineStatus.queuedPartitionedDrivers();
+                queuedPartitionedSplitsWeight += pipelineStatus.queuedPartitionedSplitsWeight();
+                runningPartitionedDrivers += pipelineStatus.runningPartitionedDrivers();
+                runningPartitionedSplitsWeight += pipelineStatus.runningPartitionedSplitsWeight();
                 physicalWrittenBytes += pipelineContext.getPhysicalWrittenDataSize();
             }
             writerInputDataSize = succinctBytes(taskContext.getWriterInputDataSize());
