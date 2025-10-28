6b4525ee13ae22f849fec4662c4de208dca48487
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-28 12:11:59 +0100

    Use ThreadPerTaskExecutor for loading plugins and creating connectors
    This work is mostly I/O bound (reading JARs) so it's a good fit for virtual threads

--- core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
+++ core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
@@ -162,6 +162,7 @@ import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
+import static io.airlift.concurrent.Threads.virtualThreadsNamed;
 import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
 import static io.airlift.json.JsonBinder.jsonBinder;
@@ -173,6 +174,7 @@ import static io.trino.server.InternalCommunicationHttpClientModule.internalHttp
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static java.util.concurrent.Executors.newThreadPerTaskExecutor;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.weakref.jmx.guice.ExportBinder.newExporter;
 
@@ -543,9 +545,7 @@ public class ServerMainModule
         if (!config.isConcurrentStartup()) {
             return directExecutor();
         }
-        return new BoundedExecutor(
-                newCachedThreadPool(daemonThreadsNamed("startup-%s")),
-                Runtime.getRuntime().availableProcessors());
+        return newThreadPerTaskExecutor(virtualThreadsNamed("startup#v%s"));
     }
 
     @Provides
