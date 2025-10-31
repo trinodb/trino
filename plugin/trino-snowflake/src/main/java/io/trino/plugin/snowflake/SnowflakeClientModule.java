f8843b8ca1f2f0c25ecdb45dabdf317502d76f0d
Parth Shyara <parth.shyara.x@gmail.com>
2025-10-31 17:03:34 +0530

    add `privateKey` as a JVM property for Snowflake JDBC Connector

--- plugin/trino-snowflake/src/main/java/io/trino/plugin/snowflake/SnowflakeClientModule.java
+++ plugin/trino-snowflake/src/main/java/io/trino/plugin/snowflake/SnowflakeClientModule.java
@@ -35,6 +35,10 @@ import net.snowflake.client.jdbc.SnowflakeDriver;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.Properties;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
@@ -72,6 +76,7 @@ public class SnowflakeClientModule
         snowflakeConfig.getDatabase().ifPresent(database -> properties.setProperty("db", database));
         snowflakeConfig.getRole().ifPresent(role -> properties.setProperty("role", role));
         snowflakeConfig.getWarehouse().ifPresent(warehouse -> properties.setProperty("warehouse", warehouse));
+        snowflakeConfig.getPrivateKey().ifPresent(privateKey -> properties.put("privateKey", parsePrivateKey(privateKey)));
 
         setOutputProperties(properties);
 
@@ -115,5 +120,18 @@ public class SnowflakeClientModule
         properties.setProperty("TIME_OUTPUT_FORMAT", "HH24:MI:SS.FF9");
         // Don't treat decimals as bigints as they may overflow
         properties.setProperty("JDBC_TREAT_DECIMAL_AS_INT", "FALSE");
+    }
+
+    protected static PrivateKey parsePrivateKey(String privateKey)
+    {
+        try {
+            byte[] keyBytes = Base64.getDecoder().decode(privateKey);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePrivate(keySpec);
+        }
+        catch (Exception e) {
+            throw new TrinoException(NOT_SUPPORTED, "Unable to parse provided private key", e);
+        }
     }
 }
