dc0a54594222f1f9739846dc12b2c2129a58e71b
Chinthanippu Satish <satish.chinthanippu@teradata.com>
2025-11-04 08:46:42 -0800

    Add Teradata connector

---
+++ plugin/trino-teradata/src/main/java/io/trino/plugin/teradata/TeradataClientModule.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.teradata;
+
+import com.google.inject.Binder;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.opentelemetry.api.OpenTelemetry;
+import io.trino.plugin.jdbc.BaseJdbcConfig;
+import io.trino.plugin.jdbc.ConnectionFactory;
+import io.trino.plugin.jdbc.DriverConnectionFactory;
+import io.trino.plugin.jdbc.ForBaseJdbc;
+import io.trino.plugin.jdbc.JdbcClient;
+import io.trino.plugin.jdbc.JdbcJoinPushdownSupportModule;
+import io.trino.plugin.jdbc.JdbcStatisticsConfig;
+import io.trino.plugin.jdbc.credential.CredentialProvider;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static io.airlift.configuration.ConfigBinder.configBinder;
+
+public class TeradataClientModule
+        extends AbstractConfigurationAwareModule
+{
+    @Provides
+    @Singleton
+    @ForBaseJdbc
+    public static ConnectionFactory getConnectionFactory(BaseJdbcConfig config, TeradataConfig teradataConfig, CredentialProvider credentialProvider, OpenTelemetry openTelemetry)
+            throws SQLException
+    {
+        Properties connectionProperties = new Properties();
+        Driver driver = DriverManager.getDriver(config.getConnectionUrl());
+        String logonMechanism = LogonMechanism.fromString(teradataConfig.getLogMech()).getMechanism();
+        connectionProperties.put("LOGMECH", logonMechanism);
+        if (!logonMechanism.equals("TD2")) {
+            throw new IllegalArgumentException("Unsupported logon mechanism: " + logonMechanism);
+        }
+        return DriverConnectionFactory.builder(driver, config.getConnectionUrl(), credentialProvider).setConnectionProperties(connectionProperties).setOpenTelemetry(openTelemetry).build();
+    }
+
+    @Override
+    public void setup(Binder binder)
+    {
+        configBinder(binder).bindConfig(TeradataConfig.class);
+        binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(TeradataClient.class).in(Scopes.SINGLETON);
+        configBinder(binder).bindConfig(JdbcStatisticsConfig.class);
+        install(new JdbcJoinPushdownSupportModule());
+    }
+}
---
+++ plugin/trino-teradata/src/test/java/io/trino/plugin/unit/TestTeradataConfig.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.unit;
+
+import io.trino.plugin.teradata.TeradataConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestTeradataConfig
+{
+    @Test
+    public void testDefaults()
+    {
+        TeradataConfig config = new TeradataConfig();
+        assertThat(config.getLogMech()).isEqualTo("TD2");
+        assertThat(config.getTeradataCaseSensitivity()).isEqualTo(TeradataConfig.TeradataCaseSensitivity.CASE_SENSITIVE);
+    }
+
+    @Test
+    public void testSetters()
+    {
+        TeradataConfig config = new TeradataConfig()
+                .setLogMech("TD2")
+                .setTeradataCaseSensitivity(TeradataConfig.TeradataCaseSensitivity.CASE_INSENSITIVE);
+        assertThat(config.getLogMech()).isEqualTo("TD2");
+        assertThat(config.getTeradataCaseSensitivity()).isEqualTo(TeradataConfig.TeradataCaseSensitivity.CASE_INSENSITIVE);
+    }
+
+    @Test
+    public void testTeradataCaseSensitivityEnum()
+    {
+        assertThat(TeradataConfig.TeradataCaseSensitivity.valueOf("CASE_INSENSITIVE"))
+                .isEqualTo(TeradataConfig.TeradataCaseSensitivity.CASE_INSENSITIVE);
+        assertThat(TeradataConfig.TeradataCaseSensitivity.valueOf("CASE_SENSITIVE"))
+                .isEqualTo(TeradataConfig.TeradataCaseSensitivity.CASE_SENSITIVE);
+        assertThat(TeradataConfig.TeradataCaseSensitivity.valueOf("AS_DEFINED"))
+                .isEqualTo(TeradataConfig.TeradataCaseSensitivity.AS_DEFINED);
+    }
+}
