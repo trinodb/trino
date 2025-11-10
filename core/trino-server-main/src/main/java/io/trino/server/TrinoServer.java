5fa18ec3f7a3f1edfb8f6fbc0b386d80da6364f2
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-11-10 14:49:12 +0100

    Compile trino-server-main targeting Java 11
    By targeting Java 11 we can eliminate manual Java version string
    parsing. The downside is that someone trying to run Trino server with
    JDK 8, 9 or 10 won't get a nice error message anymore. The situation is
    unlikely given how ancient these versions are.

--- core/trino-server-main/src/main/java/io/trino/server/TrinoServer.java
+++ core/trino-server-main/src/main/java/io/trino/server/TrinoServer.java
@@ -13,11 +13,7 @@
  */
 package io.trino.server;
 
-import com.google.common.base.StandardSystemProperty;
-import com.google.common.primitives.Ints;
-
 import static com.google.common.base.MoreObjects.firstNonNull;
-import static com.google.common.base.Strings.nullToEmpty;
 
 public final class TrinoServer
 {
@@ -25,15 +21,13 @@ public final class TrinoServer
 
     public static void main(String[] args)
     {
-        String javaVersion = nullToEmpty(StandardSystemProperty.JAVA_VERSION.value());
-        String majorVersion = javaVersion.split("\\D", 2)[0];
-        Integer major = Ints.tryParse(majorVersion);
-        if (major == null || major < 22) {
+        Runtime.Version javaVersion = Runtime.version();
+        if (javaVersion.feature() < 22) {
             System.err.printf("ERROR: Trino requires Java 22+ (found %s)%n", javaVersion);
             System.exit(100);
         }
 
-        String version = TrinoServer.class.getPackage().getImplementationVersion();
-        new Server().start(firstNonNull(version, "unknown"));
+        String trinoVersion = TrinoServer.class.getPackage().getImplementationVersion();
+        new Server().start(firstNonNull(trinoVersion, "unknown"));
     }
 }
