724c093baa49bcfd03ee6fb2e1185d8345993433
Pieterjan Spoelders <pj.spoelders@gmail.com>
2025-12-05 03:59:09 -0500

    Update Exasol docker container used in integration tests - Updated to 2025.1.8

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeExasol.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeExasol.java
@@ -56,7 +56,7 @@ public class EnvMultinodeExasol
 
     private DockerContainer createExasol()
     {
-        DockerContainer container = new DockerContainer("exasol/docker-db:2025.1.3", "exasol")
+        DockerContainer container = new DockerContainer("exasol/docker-db:2025.1.8", "exasol")
                 .withStartupCheckStrategy(new IsRunningStartupCheckStrategy())
                 .waitingFor(forSelectedPorts(EXASOL_PORT))
                 .withEnv("COSLWD_ENABLED", "1"); //Disables rsyslogd, cleans up log clutter and speeds up database startup
