4fc06bb4dbc7628611cb0764d5164421a4f021ac
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-12-19 14:12:58 +0100

    Recover Delta S3 transaction log atomic write after retries
    This solves the problem described in detail in
    28ad62d6048e5355c453e09e90f325a0e61745f0 commit. Previous solution (that
    commit) was on S3 filesystem level and required additional permissions
    to run. The new solution is on Delta Lake connector level and requires
    no permissions. Delta Lake connector can guarantee the transaction log
    content to be unique and can use this for determining provenance of the
    object suspected of being a conflict with another writer.

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/transactionlog/writer/S3ConditionalWriteLogSynchronizer.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/transactionlog/writer/S3ConditionalWriteLogSynchronizer.java
@@ -14,8 +14,12 @@
 package io.trino.plugin.deltalake.transactionlog.writer;
 
 import com.google.inject.Inject;
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import io.trino.filesystem.FileMayHaveAlreadyExistedException;
 import io.trino.filesystem.Location;
 import io.trino.filesystem.TrinoFileSystem;
+import io.trino.filesystem.TrinoInput;
 import io.trino.plugin.deltalake.DeltaLakeFileSystemFactory;
 import io.trino.plugin.deltalake.metastore.VendedCredentialsHandle;
 import io.trino.spi.connector.ConnectorSession;
@@ -24,6 +28,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.FileAlreadyExistsException;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
 
 public class S3ConditionalWriteLogSynchronizer
@@ -40,12 +46,38 @@ public class S3ConditionalWriteLogSynchronizer
     @Override
     public void write(ConnectorSession session, VendedCredentialsHandle credentialsHandle, String clusterId, Location newLogEntryPath, byte[] entryContents)
     {
+        // This is key to ensuring that two different queries/clusters can never write the same transaction log entry contents.
+        // Contents-based checks are used to recover when S3 write succeeds on the server, but is retried on the client and the fails.
+        String contentsAsString = new String(entryContents, UTF_8);
+        checkArgument(contentsAsString.contains(session.getQueryId()), "Committed transaction log contents must contain the query ID: [%s]", contentsAsString);
+
         TrinoFileSystem fileSystem = fileSystemFactory.create(session, credentialsHandle);
         try {
             fileSystem.newOutputFile(newLogEntryPath).createExclusive(entryContents);
         }
         catch (FileAlreadyExistsException e) {
-            throw new TransactionConflictException("Conflict detected while writing Transaction Log entry " + newLogEntryPath + " to S3", e);
+            throw new TransactionConflictException("Conflict detected while writing Transaction Log entry %s to S3".formatted(newLogEntryPath), e);
+        }
+        catch (FileMayHaveAlreadyExistedException mayExistException) {
+            // Check if the object was actually created by a previous attempt of AWS SDK's implicit retries
+            // This is workaround for AWS SDK limitation: https://github.com/aws/aws-sdk-java-v2/issues/6580
+            try {
+                try (TrinoInput trinoInput = fileSystem.newInputFile(newLogEntryPath).newInput()) {
+                    Slice readBack = trinoInput.readFully(0, entryContents.length + 1);
+                    // The contents are unique per query, so if they match, we know we created it
+                    boolean createdByUs = readBack.equals(Slices.wrappedBuffer(entryContents));
+                    if (!createdByUs) {
+                        throw new TransactionConflictException("Conflict detected while writing Transaction Log entry %s to S3".formatted(newLogEntryPath), mayExistException);
+                    }
+                }
+            }
+            catch (IOException | RuntimeException verificationException) {
+                TransactionConflictException conflictException = new TransactionConflictException(
+                        "Potential conflict suspected while writing Transaction Log entry %s to S3. Conflicting object provenance could not be verified".formatted(newLogEntryPath),
+                        verificationException);
+                conflictException.addSuppressed(mayExistException);
+                throw conflictException;
+            }
         }
         catch (IOException e) {
             throw new UncheckedIOException(e);
