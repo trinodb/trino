243600f4c93bbb9b2c74803a324c167e76492951
Vikash Kumar <vikash.kumar@starburstdata.com>
2025-12-26 01:31:27 +0530

    Fix variable name in BaseConnectorTest

--- testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
@@ -4139,8 +4139,8 @@ public abstract class BaseConnectorTest
         }
 
         assertUpdate("CREATE TABLE " + tableName + " AS SELECT 123 x", 1);
-        String invalidTargetTableName = validTargetColumnName + "z";
-        assertThatThrownBy(() -> assertUpdate("ALTER TABLE " + tableName + " RENAME COLUMN x TO " + invalidTargetTableName))
+        String invalidTargetColumnName = validTargetColumnName + "z";
+        assertThatThrownBy(() -> assertUpdate("ALTER TABLE " + tableName + " RENAME COLUMN x TO " + invalidTargetColumnName))
                 .satisfies(this::verifyColumnNameLengthFailurePermissible);
         assertQuery("SELECT x FROM " + tableName, "VALUES 123");
 
