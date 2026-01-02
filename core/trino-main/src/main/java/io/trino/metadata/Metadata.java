d727ccf1024e0e7e89ea813cb573946415826598
Dain Sundstrom <dain@iq80.com>
2026-01-01 16:17:48 -0800

    Allow table procedures to declare which columns to read

--- core/trino-main/src/main/java/io/trino/metadata/Metadata.java
+++ core/trino-main/src/main/java/io/trino/metadata/Metadata.java
@@ -121,6 +121,8 @@ public interface Metadata
             String procedureName,
             Map<String, Object> executeProperties);
 
+    Set<ColumnHandle> getColumnHandlesForTableExecute(Session session, TableExecuteHandle tableExecuteHandle);
+
     Optional<TableLayout> getLayoutForTableExecute(Session session, TableExecuteHandle tableExecuteHandle);
 
     BeginTableExecuteResult<TableExecuteHandle, TableHandle> beginTableExecute(Session session, TableExecuteHandle handle, TableHandle updatedSourceTableHandle);
