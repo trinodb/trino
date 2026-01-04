998ba29d6e7058108bffe274ef551451dde8198a
Dain Sundstrom <dain@iq80.com>
2026-01-03 17:20:00 -0800

    Convert geometry accessor functions from ESRI to JTS
    Migrate ST_NumPoints and related accessor functions to JTS.
    
    Test updates for behavior differences:
    - ST_NumPoints now counts closing vertices in polygons per OGC standard
    - Ring vertex ordering may differ cosmetically (same geometry)

--- plugin/trino-geospatial/src/test/java/io/trino/plugin/geospatial/TestGeoFunctions.java
+++ plugin/trino-geospatial/src/test/java/io/trino/plugin/geospatial/TestGeoFunctions.java
@@ -898,8 +898,8 @@ public class TestGeoFunctions
         assertNumPoints("MULTIPOINT (1 2, 2 4, 3 6, 4 8)", 4);
         assertNumPoints("LINESTRING (8 4, 5 7)", 2);
         assertNumPoints("MULTILINESTRING ((1 1, 5 1), (2 4, 4 4))", 4);
-        assertNumPoints("POLYGON ((0 0, 8 0, 0 8, 0 0), (1 1, 1 5, 5 1, 1 1))", 6);
-        assertNumPoints("MULTIPOLYGON (((1 1, 1 3, 3 3, 3 1, 1 1)), ((2 4, 2 6, 6 6, 6 4, 2 4)))", 8);
+        assertNumPoints("POLYGON ((0 0, 8 0, 0 8, 0 0), (1 1, 1 5, 5 1, 1 1))", 8);
+        assertNumPoints("MULTIPOLYGON (((1 1, 1 3, 3 3, 3 1, 1 1)), ((2 4, 2 6, 6 6, 6 4, 2 4)))", 10);
         assertNumPoints("GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (8 4, 5 7), POLYGON EMPTY)", 3);
     }
 
