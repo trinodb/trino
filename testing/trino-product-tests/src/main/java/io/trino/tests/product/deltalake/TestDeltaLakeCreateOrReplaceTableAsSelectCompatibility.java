d24cf0ffc2d5d4ca0bd28049633a57d52aec451b
chenjian2664 <chenjian2664@gmail.com>
2026-01-17 22:49:33 +0800

    Preserve table features on `CREATE OR REPLACE TABLE`
    According to
    https://github.com/delta-io/delta/blob/master/PROTOCOL.md#table-features-for-new-and-existing-tables,
    retain existing read and write features when executing
     `CREATE OR REPLACE TABLE`, as previous table data may
     still depend on them.

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeCreateOrReplaceTableAsSelectCompatibility.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeCreateOrReplaceTableAsSelectCompatibility.java
@@ -140,6 +140,24 @@ public class TestDeltaLakeCreateOrReplaceTableAsSelectCompatibility
         }
     }
 
+    @Test(groups = {DELTA_LAKE_OSS, PROFILE_SPECIFIC_TESTS})
+    public void testCreateOrReplaceTableOnlyDiscardFeaturesOnTrino()
+    {
+        String tableName = "test_create_or_replace_table_discard_features_" + randomNameSuffix();
+
+        onTrino().executeQuery("CREATE TABLE delta.default." + tableName + " (ts VARCHAR) " +
+                               "with (location = 's3://" + bucketName + "/databricks-compatibility-test-" + tableName + "', checkpoint_interval = 10, deletion_vectors_enabled = true)");
+        try {
+            List<Row> expectedRows = performInsert(onTrino(), tableName, 12);
+
+            onTrino().executeQuery("CREATE OR REPLACE TABLE delta.default." + tableName + " AS SELECT CAST(ts AS TIMESTAMP(6)) as ts FROM " + tableName);
+            assertThat(onDelta().executeQuery("SELECT date_format(ts, \"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\") FROM default." + tableName)).containsOnly(expectedRows);
+        }
+        finally {
+            onTrino().executeQuery("DROP TABLE delta.default." + tableName);
+        }
+    }
+
     private static List<Row> performInsert(QueryExecutor queryExecutor, String tableName, int numberOfRows)
     {
         ImmutableList.Builder<Row> expectedRowBuilder = ImmutableList.builder();
