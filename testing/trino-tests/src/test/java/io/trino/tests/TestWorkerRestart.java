c01c69804ad677c91ddaa6f33fe855e032b580a5
Piotr Rzysko <piotr.rzysko@gmail.com>
2026-01-21 16:54:09 +0100

    Extend testRestartDuringQuery expected error pattern
    The added error may occur when the response includes a Content-Type
    header but the body is corrupted (for example, truncated).

--- testing/trino-tests/src/test/java/io/trino/tests/TestWorkerRestart.java
+++ testing/trino-tests/src/test/java/io/trino/tests/TestWorkerRestart.java
@@ -102,7 +102,8 @@ public class TestWorkerRestart
                                                         "|Expected response from \\S+ is empty" +
                                                         "|Error fetching \\S+: Expected response code to be 200, but was 5\\d\\d" +
                                                         "|Could not communicate with the remote task. The node may have crashed or be under too much load" +
-                                                        "|Error fetching \\S+: Content-Type header is not set");
+                                                        "|Error fetching \\S+: Content-Type header is not set" +
+                                                        "|Unexpected response from \\S+");
 
                 // Ensure that the restarted worker is able to serve queries.
                 assertThat((long) queryRunner.execute("SELECT count(*) FROM tpch.tiny.lineitem").getOnlyValue())
