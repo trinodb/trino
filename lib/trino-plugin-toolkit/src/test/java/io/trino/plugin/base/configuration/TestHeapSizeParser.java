f1eb6a7419b1b3d34b6b9e73c44ff3a0631d9479
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-01-29 13:54:07 +0100

    Move ThreadCount and HeapSize parsers to plugin toolkit

--- lib/trino-plugin-toolkit/src/test/java/io/trino/plugin/base/configuration/TestHeapSizeParser.java
+++ lib/trino-plugin-toolkit/src/test/java/io/trino/plugin/base/configuration/TestHeapSizeParser.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.execution;
+package io.trino.plugin.base.configuration;
 
 import io.airlift.units.DataSize;
 import org.junit.jupiter.api.Test;
