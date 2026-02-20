287271f46e883eb82a62865e78e5955c8ee97082
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:22:11 -0800

    Migrate TestParquetFileWithReorderedColumns to JUnit

--- testing/trino-product-tests/src/test/java/io/trino/tests/product/hive/TestParquetFileWithReorderedColumns.java
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/hive/TestParquetFileWithReorderedColumns.java
@@ -14,60 +14,48 @@
 package io.trino.tests.product.hive;
 
 import com.google.common.io.Resources;
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
-import io.trino.tempto.AfterMethodWithContext;
-import io.trino.tempto.BeforeMethodWithContext;
-import io.trino.tempto.ProductTest;
-import io.trino.tempto.hadoop.hdfs.HdfsClient;
-import org.testng.annotations.Test;
+import io.trino.testing.containers.HdfsClient;
+import io.trino.testing.containers.environment.ProductTest;
+import io.trino.testing.containers.environment.RequiresEnvironment;
+import io.trino.tests.product.TestGroup;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 
-import static io.trino.tempto.assertions.QueryAssert.Row.row;
 import static io.trino.testing.TestingNames.randomNameSuffix;
-import static io.trino.tests.product.TestGroups.HIVE_SPARK;
-import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
-import static io.trino.tests.product.utils.QueryExecutors.onHive;
-import static io.trino.tests.product.utils.QueryExecutors.onSpark;
-import static io.trino.tests.product.utils.QueryExecutors.onTrino;
+import static io.trino.testing.containers.environment.QueryResultAssert.assertThat;
+import static io.trino.testing.containers.environment.Row.row;
 import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestParquetFileWithReorderedColumns
-        extends ProductTest
+/**
+ * Tests for Parquet files with reordered columns.
+ * <p>
+ * Ported from the Tempto-based TestParquetFileWithReorderedColumns.
+ */
+@ProductTest
+@RequiresEnvironment(HiveSparkEnvironment.class)
+@TestGroup.HiveSpark
+class TestParquetFileWithReorderedColumns
 {
-    @Inject
-    private HdfsClient hdfsClient;
-
-    @Inject
-    @Named("databases.hive.warehouse_directory_path")
-    private String warehouseDirectory;
-
-    @BeforeMethodWithContext
-    public void setup()
-            throws Exception
+    @Test
+    void testReadParquetFileWithReorderedColumns(HiveSparkEnvironment env)
+            throws IOException
     {
+        String warehouseDirectory = env.getWarehouseDirectory();
+        HdfsClient hdfsClient = env.createHdfsClient();
+
+        // Create directory and upload parquet file
         hdfsClient.createDirectory(warehouseDirectory + "/TestParquetFileWithReorderedColumns");
         try (InputStream inputStream = Resources.asByteSource(Resources.getResource("parquet/reordered_columns.parquet")).openStream()) {
-            hdfsClient.saveFile(warehouseDirectory + "/TestParquetFileWithReorderedColumns/reordered_columns.parquet", inputStream);
+            hdfsClient.saveFile(warehouseDirectory + "/TestParquetFileWithReorderedColumns/reordered_columns.parquet", inputStream.readAllBytes());
         }
-    }
 
-    @AfterMethodWithContext
-    public void cleanup()
-    {
-        hdfsClient.delete(warehouseDirectory + "/TestParquetFileWithReorderedColumns");
-    }
-
-    @Test(groups = {HIVE_SPARK, PROFILE_SPECIFIC_TESTS})
-    public void testReadParquetFileWithReorderedColumns()
-    {
         String sourceTableName = "test_reordered_columns_table_" + randomNameSuffix();
         String tableName = "test_read_reordered_columns_table_" + randomNameSuffix();
         try {
-            onTrino().executeQuery(format(
-                    "CREATE TABLE %s" +
+            env.executeTrinoUpdate(format(
+                    "CREATE TABLE hive.default.%s" +
                             " (id bigint, buyplan_style_detail_id bigint, last_modified_on bigint) " +
                             "WITH ( " +
                             "   format = 'PARQUET', " +
@@ -75,21 +63,24 @@ public class TestParquetFileWithReorderedColumns
                             ")",
                     sourceTableName,
                     warehouseDirectory));
+
             // Write parquet file with Trino parquet writer using an existing file as the source which reproduces the problem of Apache Spark not reading the file
-            onTrino().executeQuery(format("CREATE TABLE %s WITH (format = 'PARQUET') AS SELECT * FROM %s", tableName, sourceTableName));
-            String sql = "SELECT COUNT(*), SUM(id), SUM(buyplan_style_detail_id), SUM(last_modified_on) FROM " + tableName;
-            assertThat(onTrino().executeQuery(sql))
+            env.executeTrinoUpdate(format("CREATE TABLE hive.default.%s WITH (format = 'PARQUET') AS SELECT * FROM hive.default.%s", tableName, sourceTableName));
+
+            String sql = "SELECT COUNT(*), SUM(id), SUM(buyplan_style_detail_id), SUM(last_modified_on) FROM hive.default." + tableName;
+            assertThat(env.executeTrino(sql))
                     .containsExactlyInOrder(row(50438L, 323043905052L, 67694121262L, 83905381446283000L));
 
-            assertThat(onHive().executeQuery(sql))
+            assertThat(env.executeHive("SELECT COUNT(*), SUM(id), SUM(buyplan_style_detail_id), SUM(last_modified_on) FROM default." + tableName))
                     .containsExactlyInOrder(row(50438L, 323043905052L, 67694121262L, 83905381446283000L));
 
-            assertThat(onSpark().executeQuery(sql))
+            assertThat(env.executeSpark("SELECT COUNT(*), SUM(id), SUM(buyplan_style_detail_id), SUM(last_modified_on) FROM default." + tableName))
                     .containsExactlyInOrder(row(50438L, 323043905052L, 67694121262L, 83905381446283000L));
         }
         finally {
-            onTrino().executeQuery("DROP TABLE " + tableName);
-            onTrino().executeQuery("DROP TABLE " + sourceTableName);
+            env.executeTrinoUpdate("DROP TABLE IF EXISTS hive.default." + tableName);
+            env.executeTrinoUpdate("DROP TABLE IF EXISTS hive.default." + sourceTableName);
+            hdfsClient.delete(warehouseDirectory + "/TestParquetFileWithReorderedColumns");
         }
     }
 }
