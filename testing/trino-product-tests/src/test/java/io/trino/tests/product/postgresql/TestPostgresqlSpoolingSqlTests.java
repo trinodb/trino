47dbc886df6ace45d4c7b120a9efef273a459cd2
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:33:15 -0800

    Add TestPostgresqlSpoolingSqlTests JUnit coverage

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/postgresql/TestPostgresqlSpoolingSqlTests.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.postgresql;
+
+import io.trino.testing.containers.environment.ProductTest;
+import io.trino.testing.containers.environment.RequiresEnvironment;
+import io.trino.tests.product.TestGroup;
+
+/**
+ * PostgreSQL SQL tests running against the PostgreSQL + spooling environment.
+ * <p>
+ * This class inherits all test methods from {@link BasePostgresqlSqlTests}
+ * and runs them against {@link PostgresqlSpoolingEnvironment}, which includes
+ * MinIO for result spooling.
+ */
+@ProductTest
+@RequiresEnvironment(PostgresqlSpoolingEnvironment.class)
+@TestGroup.Postgresql
+@TestGroup.PostgresqlSpooling
+@TestGroup.ProfileSpecificTests
+class TestPostgresqlSpoolingSqlTests
+        extends BasePostgresqlSqlTests
+{
+}
