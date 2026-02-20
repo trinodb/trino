8b68e6efcf7648bd130783db8c4d647e54810fa0
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:33:15 -0800

    Migrate TestPostgresqlSqlTests to JUnit

--- testing/trino-product-tests/src/main/resources/sql-tests/testcases/connectors/postgresql/select_real.sql
+++
@@ -1,8 +0,0 @@
--- database: trino; groups: postgresql,profile_specific_tests; tables: postgres.public.real_table_psql
---!
-select * from postgresql.public.real_table_psql
---!
--- delimiter: |; trimValues: true; ignoreOrder: true;
-null|4000.10889|100.59889|11.456789|96.42|
-2|100.97|0.94|0.8895|73.658|
-null|null|null|null|null|
--- testing/trino-product-tests/src/main/resources/sql-tests/testcases/connectors/postgresql/show_schemas.sql
+++
@@ -1,8 +0,0 @@
--- database: trino; groups: postgresql,profile_specific_tests; queryType: SELECT;
---!
-show schemas from postgresql
---!
--- delimiter: |; trimValues: true; ignoreOrder: true;
-information_schema|
-pg_catalog|
-public|
