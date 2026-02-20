855cfb6483658eb5f9e5e0acc8cb3d69708f879c
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:22:52 -0800

    Migrate TestIcebergOptimize to JUnit

--- testing/trino-product-tests/src/test/java/io/trino/tests/product/iceberg/TestIcebergOptimize.java
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/iceberg/TestIcebergOptimize.java
@@ -13,42 +13,46 @@
  */
 package io.trino.tests.product.iceberg;
 
-import io.trino.tempto.ProductTest;
-import org.testng.annotations.Test;
+import io.trino.testing.containers.environment.ProductTest;
+import io.trino.testing.containers.environment.RequiresEnvironment;
+import io.trino.tests.product.TestGroup;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
-import static io.trino.tempto.assertions.QueryAssert.Row.row;
 import static io.trino.testing.TestingNames.randomNameSuffix;
-import static io.trino.tests.product.TestGroups.ICEBERG;
-import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
-import static io.trino.tests.product.utils.QueryExecutors.onSpark;
-import static io.trino.tests.product.utils.QueryExecutors.onTrino;
+import static io.trino.testing.containers.environment.QueryResultAssert.assertThat;
+import static io.trino.testing.containers.environment.Row.row;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests compatibility between Iceberg connector and Spark Iceberg.
+ * Tests Iceberg table optimization operations with Spark interoperability.
+ * <p>
+ * Ported from the Tempto-based TestIcebergOptimize.
  */
-public class TestIcebergOptimize
-        extends ProductTest
+@ProductTest
+@RequiresEnvironment(SparkIcebergEnvironment.class)
+@TestGroup.Iceberg
+class TestIcebergOptimize
 {
     // see spark-defaults.conf
     private static final String SPARK_CATALOG = "iceberg_test";
     private static final String TRINO_CATALOG = "iceberg";
     private static final String TEST_SCHEMA_NAME = "default";
 
-    @Test(groups = {ICEBERG, PROFILE_SPECIFIC_TESTS})
-    public void testOptimizeTableAfterDelete()
+    @Test
+    void testOptimizeTableAfterDelete(SparkIcebergEnvironment env)
+            throws Exception
     {
         String baseTableName = "test_optimize_with_small_split_size_" + randomNameSuffix();
         String trinoTableName = trinoTableName(baseTableName);
         String sparkTableName = sparkTableName(baseTableName);
-        onTrino().executeQuery("DROP TABLE IF EXISTS " + trinoTableName);
-        onTrino().executeQuery("CREATE TABLE " + trinoTableName + "(regionkey integer, country varchar, description varchar) ");
+        env.executeTrinoUpdate("DROP TABLE IF EXISTS " + trinoTableName);
+        env.executeTrinoUpdate("CREATE TABLE " + trinoTableName + "(regionkey integer, country varchar, description varchar) ");
 
-        onTrino().executeQuery("INSERT INTO " + trinoTableName + " VALUES " +
+        env.executeTrinoUpdate("INSERT INTO " + trinoTableName + " VALUES " +
                 "(1, 'Poland', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum erat faucibus tempus ullamcorper. Duis at auctor est, a accumsan magna.'), " +
                 "(1, 'Austria', ' Aliquam rhoncus tortor eu sagittis vulputate. Donec non erat nec dui tempor mattis pellentesque quis sapien. Proin dolor elit, porttitor aliquam erat et, aliquam eleifend elit. '), " +
                 "(1, 'France', 'Cras et elit sed nisl faucibus volutpat sed ac sapien. Sed ut metus vulputate, feugiat massa sed, lacinia ipsum. Phasellus ultrices ligula non ultricies gravida'), " +
@@ -59,51 +63,54 @@ public class TestIcebergOptimize
                 "(3, 'China', 'Fusce sit amet eleifend nunc. Maecenas bibendum felis felis, eu cursus neque viverra sed. Sed faucibus augue eu placerat elementum. Nam vitae hendrerit odio.')," +
                 "(3, 'Laos', 'Nulla vel placerat nibh. Pellentesque a cursus nunc. In tristique sollicitudin vestibulum. Sed imperdiet justo eget rhoncus condimentum. In commodo, purus sit amet malesuada rutrum, neque magna euismod elit')");
 
-        List<String> initialFiles = getActiveFiles(TRINO_CATALOG, TEST_SCHEMA_NAME, baseTableName);
+        List<String> initialFiles = getActiveFiles(env, baseTableName);
 
-        onTrino().executeQuery("DELETE FROM " + trinoTableName + " WHERE regionkey = 1");
-        onTrino().executeQuery("DELETE FROM " + trinoTableName + " WHERE regionkey = 2");
+        env.executeTrinoUpdate("DELETE FROM " + trinoTableName + " WHERE regionkey = 1");
+        env.executeTrinoUpdate("DELETE FROM " + trinoTableName + " WHERE regionkey = 2");
 
         // Verify that delete files exists
         assertThat(
-                onTrino().executeQuery(
+                env.executeTrino(
                         format("SELECT summary['total-delete-files'] FROM %s.%s.\"%s$snapshots\" ", TRINO_CATALOG, TEST_SCHEMA_NAME, baseTableName) +
-                                "WHERE snapshot_id = " + getCurrentSnapshotId(TRINO_CATALOG, TEST_SCHEMA_NAME, baseTableName)))
+                                "WHERE snapshot_id = " + getCurrentSnapshotId(env, baseTableName)))
                 .containsOnly(row("2"));
 
         // Set the split size to a small number of bytes so each ORC stripe gets its own split.
         // TODO Drop Spark dependency once that the setting 'read.split.target-size' can be set through Trino
-        onSpark().executeQuery("ALTER TABLE " + sparkTableName + " SET TBLPROPERTIES ('read.split.target-size'='100')");
+        env.executeSparkUpdate("ALTER TABLE " + sparkTableName + " SET TBLPROPERTIES ('read.split.target-size'='100')");
 
         // For optimize we need to set task_min_writer_count to 1, otherwise it will create more than one file.
-        onTrino().executeQuery("SET SESSION task_min_writer_count = 1");
-        onTrino().executeQuery("ALTER TABLE " + trinoTableName + " EXECUTE OPTIMIZE");
+        // Session setting must persist for the optimize call
+        env.executeTrinoInSession(session -> {
+            session.executeUpdate("SET SESSION task_min_writer_count = 1");
+            session.executeUpdate("ALTER TABLE " + trinoTableName + " EXECUTE OPTIMIZE");
+        });
 
-        List<String> updatedFiles = getActiveFiles(TRINO_CATALOG, TEST_SCHEMA_NAME, baseTableName);
+        List<String> updatedFiles = getActiveFiles(env, baseTableName);
         assertThat(updatedFiles)
                 .hasSize(1)
                 .isNotEqualTo(initialFiles);
 
-        assertThat(onTrino().executeQuery("SELECT * FROM " + trinoTableName))
+        assertThat(env.executeTrino("SELECT * FROM " + trinoTableName))
                 .containsOnly(
                         row(3, "Japan", "Sed vitae dignissim mi, eu mattis ante. Nam vulputate augue magna, vel viverra diam interdum at. Phasellus vehicula ante sit amet cursus venenatis."),
                         row(3, "China", "Fusce sit amet eleifend nunc. Maecenas bibendum felis felis, eu cursus neque viverra sed. Sed faucibus augue eu placerat elementum. Nam vitae hendrerit odio."),
                         row(3, "Laos", "Nulla vel placerat nibh. Pellentesque a cursus nunc. In tristique sollicitudin vestibulum. Sed imperdiet justo eget rhoncus condimentum. In commodo, purus sit amet malesuada rutrum, neque magna euismod elit"));
 
-        onTrino().executeQuery("DROP TABLE " + trinoTableName);
+        env.executeTrinoUpdate("DROP TABLE " + trinoTableName);
     }
 
-    private List<String> getActiveFiles(String catalog, String schema, String tableName)
+    private List<String> getActiveFiles(SparkIcebergEnvironment env, String tableName)
     {
-        return onTrino().executeQuery(format("SELECT file_path FROM %s.%s.\"%s$files\"", catalog, schema, tableName))
+        return env.executeTrino(format("SELECT file_path FROM %s.%s.\"%s$files\"", TRINO_CATALOG, TEST_SCHEMA_NAME, tableName))
                 .rows().stream()
                 .map(row -> (String) row.get(0))
                 .collect(toImmutableList());
     }
 
-    private long getCurrentSnapshotId(String catalog, String schema, String tableName)
+    private long getCurrentSnapshotId(SparkIcebergEnvironment env, String tableName)
     {
-        return (long) onTrino().executeQuery(format("SELECT snapshot_id FROM %s.%s.\"%s$snapshots\" ORDER BY committed_at DESC FETCH FIRST 1 ROW WITH TIES", catalog, schema, tableName))
+        return (long) env.executeTrino(format("SELECT snapshot_id FROM %s.%s.\"%s$snapshots\" ORDER BY committed_at DESC FETCH FIRST 1 ROW WITH TIES", TRINO_CATALOG, TEST_SCHEMA_NAME, tableName))
                 .getOnlyValue();
     }
 
