9f20a9fb11f180be260661c556fba86abcb60c4e
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:33:32 -0800

    Add ClickHouse environment

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/clickhouse/ClickHouseEnvironment.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.clickhouse;
+
+import io.trino.testing.containers.TrinoProductTestContainer;
+import io.trino.testing.containers.environment.ProductTestEnvironment;
+import org.testcontainers.clickhouse.ClickHouseContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.trino.TrinoContainer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * ClickHouse product test environment.
+ */
+public class ClickHouseEnvironment
+        extends ProductTestEnvironment
+{
+    private Network network;
+    private ClickHouseContainer clickhouse;
+    private TrinoContainer trino;
+
+    @Override
+    public void start()
+    {
+        if (trino != null && trino.isRunning()) {
+            return; // Already started
+        }
+
+        network = Network.newNetwork();
+
+        clickhouse = new ClickHouseContainer("clickhouse/clickhouse-server:24.3")
+                .withNetwork(network)
+                .withNetworkAliases("clickhouse");
+        clickhouse.start();
+
+        trino = TrinoProductTestContainer.builder()
+                .withNetwork(network)
+                .withCatalog("clickhouse", Map.of(
+                        "connector.name", "clickhouse",
+                        "connection-url", "jdbc:clickhouse://clickhouse:8123/default",
+                        "connection-user", clickhouse.getUsername(),
+                        "connection-password", clickhouse.getPassword()))
+                .build();
+        trino.start();
+
+        try {
+            TrinoProductTestContainer.waitForClusterReady(trino);
+        }
+        catch (SQLException | InterruptedException e) {
+            throw new RuntimeException("Failed to wait for Trino cluster", e);
+        }
+    }
+
+    @Override
+    public Connection createTrinoConnection()
+            throws SQLException
+    {
+        return TrinoProductTestContainer.createConnection(trino);
+    }
+
+    @Override
+    public Connection createTrinoConnection(String user)
+            throws SQLException
+    {
+        return TrinoProductTestContainer.createConnection(trino, user);
+    }
+
+    @Override
+    public String getTrinoJdbcUrl()
+    {
+        return trino.getJdbcUrl();
+    }
+
+    @Override
+    public boolean isRunning()
+    {
+        return trino != null && trino.isRunning();
+    }
+
+    @Override
+    protected void doClose()
+    {
+        if (trino != null) {
+            trino.close();
+            trino = null;
+        }
+        if (clickhouse != null) {
+            clickhouse.close();
+            clickhouse = null;
+        }
+        if (network != null) {
+            network.close();
+            network = null;
+        }
+    }
+}
