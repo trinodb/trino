5aaf7a3556952d8d91037b3b8a29ab339b766c67
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:22:06 -0800

    Migrate TestHiveOnOrcLegacyDateCompatibility to JUnit

--- testing/trino-product-tests/src/test/java/io/trino/tests/product/hive/TestHiveOnOrcLegacyDateCompatibility.java
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/hive/TestHiveOnOrcLegacyDateCompatibility.java
@@ -13,38 +13,42 @@
  */
 package io.trino.tests.product.hive;
 
-import io.trino.tempto.ProductTest;
-import io.trino.tempto.assertions.QueryAssert;
-import org.testng.annotations.Test;
+import io.trino.testing.containers.environment.ProductTest;
+import io.trino.testing.containers.environment.RequiresEnvironment;
+import io.trino.testing.containers.environment.Row;
+import io.trino.tests.product.TestGroup;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
 import java.sql.Timestamp;
 
-import static io.trino.tempto.assertions.QueryAssert.Row.row;
 import static io.trino.testing.TestingNames.randomNameSuffix;
-import static io.trino.tests.product.TestGroups.HIVE4;
-import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
-import static io.trino.tests.product.utils.QueryExecutors.onHive;
-import static io.trino.tests.product.utils.QueryExecutors.onTrino;
+import static io.trino.testing.containers.environment.QueryResultAssert.assertThat;
+import static io.trino.testing.containers.environment.Row.row;
 import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestHiveOnOrcLegacyDateCompatibility
-        extends ProductTest
+/**
+ * Tests for Hive ORC legacy date compatibility.
+ * <p>
+ * Ported from the Tempto-based TestHiveOnOrcLegacyDateCompatibility.
+ */
+@ProductTest
+@RequiresEnvironment(MultinodeHive4Environment.class)
+@TestGroup.Hive4
+class TestHiveOnOrcLegacyDateCompatibility
 {
     private static final String TRINO_CATALOG = "hive";
     private static final String SCHEMA = "default";
 
-    @Test(groups = {HIVE4, PROFILE_SPECIFIC_TESTS})
-    public void testReadLegacyDateFromOrcWrittenByTrino()
+    @Test
+    void testReadLegacyDateFromOrcWrittenByTrino(MultinodeHive4Environment env)
     {
         String hiveTableName = "test_hive_orc_legacy_date_compatibility_%s".formatted(randomNameSuffix());
         String trinoTableName = format("%s.%s.%s", TRINO_CATALOG, SCHEMA, hiveTableName);
 
         try {
-            onTrino().executeQuery("CREATE TABLE %s (date_col date, t timestamp) WITH (format = 'ORC')".formatted(trinoTableName));
-            onTrino().executeQuery(
-                    """
+            env.executeTrinoUpdate("CREATE TABLE %s (date_col date, t timestamp) WITH (format = 'ORC')".formatted(trinoTableName));
+            env.executeTrinoUpdate("""
                     INSERT INTO %s VALUES
                     (DATE '0002-01-01', TIMESTAMP '0002-01-01 00:00:00.123'),
                     (DATE '1500-01-01', TIMESTAMP '1500-01-01 00:00:00.123'),
@@ -52,31 +56,30 @@ public class TestHiveOnOrcLegacyDateCompatibility
                     (DATE '1582-11-04', TIMESTAMP '1582-11-04 00:00:00.123'),
                     (DATE '2000-02-29', TIMESTAMP '2000-02-29 00:00:00.123')""".formatted(trinoTableName));
 
-            QueryAssert.Row[] expectedRows = {
+            Row[] expectedRows = {
                     row(Date.valueOf("0002-01-01"), Timestamp.valueOf("0002-01-01 00:00:00.123")),
                     row(Date.valueOf("1500-01-01"), Timestamp.valueOf("1500-01-01 00:00:00.123")),
                     row(Date.valueOf("1582-10-04"), Timestamp.valueOf("1582-10-04 00:00:00.123")),
                     row(Date.valueOf("1582-11-04"), Timestamp.valueOf("1582-11-04 00:00:00.123")),
                     row(Date.valueOf("2000-02-29"), Timestamp.valueOf("2000-02-29 00:00:00.123"))};
 
-            assertThat(onTrino().executeQuery("SELECT date_col, t FROM " + trinoTableName)).containsOnly(expectedRows);
-            assertThat(onHive().executeQuery("SELECT date_col, t  FROM " + hiveTableName)).containsOnly(expectedRows);
+            assertThat(env.executeTrino("SELECT date_col, t FROM " + trinoTableName)).containsOnly(expectedRows);
+            assertThat(env.executeHive("SELECT date_col, t FROM " + hiveTableName)).containsOnly(expectedRows);
         }
         finally {
-            onTrino().executeQuery("DROP TABLE IF EXISTS " + trinoTableName);
+            env.executeTrinoUpdate("DROP TABLE IF EXISTS " + trinoTableName);
         }
     }
 
-    @Test(groups = {HIVE4, PROFILE_SPECIFIC_TESTS})
-    public void testReadLegacyDateFromOrcWrittenByHive()
+    @Test
+    void testReadLegacyDateFromOrcWrittenByHive(MultinodeHive4Environment env)
     {
         String hiveTableName = "test_hive_orc_legacy_date_compatibility_%s".formatted(randomNameSuffix());
         String trinoTableName = format("%s.%s.%s", TRINO_CATALOG, SCHEMA, hiveTableName);
 
         try {
-            onHive().executeQuery("CREATE TABLE %s (date_col date, t timestamp) STORED AS ORC".formatted(hiveTableName));
-            onHive().executeQuery(
-                    """
+            env.executeHiveUpdate("CREATE TABLE %s (date_col date, t timestamp) STORED AS ORC".formatted(hiveTableName));
+            env.executeHiveUpdate("""
                     INSERT INTO %s VALUES
                     ('0002-01-01', '0002-01-01 00:00:00.123'),
                     ('1500-01-01', '1500-01-01 00:00:00.123'),
@@ -84,25 +87,25 @@ public class TestHiveOnOrcLegacyDateCompatibility
                     ('1582-11-04', '1582-11-04 00:00:00.123'),
                     ('2000-02-29', '2000-02-29 00:00:00.123')""".formatted(hiveTableName));
 
-            QueryAssert.Row[] expectedRowsHive = {
+            Row[] expectedRowsHive = {
                     row(Date.valueOf("0002-01-01"), Timestamp.valueOf("0002-01-01 00:00:00.123")),
                     row(Date.valueOf("1500-01-01"), Timestamp.valueOf("1500-01-01 00:00:00.123")),
                     row(Date.valueOf("1582-10-04"), Timestamp.valueOf("1582-10-04 00:00:00.123")),
                     row(Date.valueOf("1582-11-04"), Timestamp.valueOf("1582-11-04 00:00:00.123")),
                     row(Date.valueOf("2000-02-29"), Timestamp.valueOf("2000-02-29 00:00:00.123"))};
-            assertThat(onHive().executeQuery("SELECT date_col, t FROM " + hiveTableName)).containsOnly(expectedRowsHive);
+            assertThat(env.executeHive("SELECT date_col, t FROM " + hiveTableName)).containsOnly(expectedRowsHive);
 
             // https://github.com/trinodb/trino/issues/26865
-            QueryAssert.Row[] expectedRowsTrino = {
+            Row[] expectedRowsTrino = {
                     row(Date.valueOf("0001-12-30"), Timestamp.valueOf("0001-12-30 00:00:00.123")),
                     row(Date.valueOf("1500-01-10"), Timestamp.valueOf("1500-01-10 00:00:00.123")),
                     row(Date.valueOf("1582-10-24"), Timestamp.valueOf("1582-10-24 00:00:00.123")),
                     row(Date.valueOf("1582-11-04"), Timestamp.valueOf("1582-11-04 00:00:00.123")),
                     row(Date.valueOf("2000-02-29"), Timestamp.valueOf("2000-02-29 00:00:00.123"))};
-            assertThat(onTrino().executeQuery("SELECT date_col, t FROM " + trinoTableName)).containsOnly(expectedRowsTrino);
+            assertThat(env.executeTrino("SELECT date_col, t FROM " + trinoTableName)).containsOnly(expectedRowsTrino);
         }
         finally {
-            onHive().executeQuery("DROP TABLE IF EXISTS " + trinoTableName);
+            env.executeHiveUpdate("DROP TABLE IF EXISTS " + hiveTableName);
         }
     }
 }
