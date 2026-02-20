0f8eb474e5e8922e3a702d035faaf69cc0a67728
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:35:44 -0800

    Add Hive suites

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/suite/SuiteHdfsImpersonation.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.suite;
+
+import io.trino.tests.product.TestGroup;
+import io.trino.tests.product.hive.HiveImpersonationEnvironment;
+import io.trino.tests.product.hive.HiveKerberosImpersonationCredentialCacheEnvironment;
+import io.trino.tests.product.hive.HiveKerberosImpersonationEnvironment;
+import io.trino.tests.product.suite.SuiteRunner.TestRunResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SuiteHdfsImpersonation
+{
+    private SuiteHdfsImpersonation() {}
+
+    public static void main(String[] args)
+    {
+        System.setProperty("trino.product-test.environment-mode", "STRICT");
+
+        List<TestRunResult> results = new ArrayList<>();
+
+        results.add(SuiteRunner.forEnvironment(HiveImpersonationEnvironment.class)
+                .includeTag(TestGroup.HdfsImpersonation.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(HiveKerberosImpersonationEnvironment.class)
+                .includeTag(TestGroup.HdfsImpersonation.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(HiveKerberosImpersonationCredentialCacheEnvironment.class)
+                .includeTag(TestGroup.HdfsImpersonation.class)
+                .run());
+
+        SuiteRunner.printSummary(results);
+        System.exit(SuiteRunner.hasFailures(results) ? 1 : 0);
+    }
+}
