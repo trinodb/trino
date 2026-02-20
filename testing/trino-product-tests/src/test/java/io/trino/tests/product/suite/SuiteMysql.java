28cf5e6fc65e7fc681889164e5f262f78c644c08
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:32:56 -0800

    Add MySQL suite

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/suite/SuiteMysql.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.suite;
+
+import io.trino.tests.product.TestGroup;
+import io.trino.tests.product.mysql.MySqlEnvironment;
+import io.trino.tests.product.suite.SuiteRunner.TestRunResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JUnit 5 test suite for MySQL connector tests.
+ * <p>
+ * This suite runs MySQL tests tagged with {@code @TestGroup.Mysql} using {@link MySqlEnvironment}.
+ * <p>
+ * Additional database runs can be appended as more environments are introduced in later commits.
+ * <p>
+ * To run this suite:
+ * <pre>
+ * java -cp ... io.trino.tests.product.suite.SuiteMysql
+ * </pre>
+ * <p>
+ * Or via Maven:
+ * <pre>
+ * mvn exec:java -Dexec.mainClass="io.trino.tests.product.suite.SuiteMysql"
+ * </pre>
+ */
+public final class SuiteMysql
+{
+    private SuiteMysql() {}
+
+    public static void main(String[] args)
+    {
+        // Set strict mode to ensure tests are properly isolated
+        System.setProperty("trino.product-test.environment-mode", "STRICT");
+
+        List<TestRunResult> results = new ArrayList<>();
+
+        // Run 1: MySQL tests
+        results.add(SuiteRunner.forEnvironment(MySqlEnvironment.class)
+                .includeTag(TestGroup.Mysql.class)
+                .run());
+
+        // Print combined summary
+        SuiteRunner.printSummary(results);
+
+        // Exit with appropriate code
+        System.exit(SuiteRunner.hasFailures(results) ? 1 : 0);
+    }
+}
