c2ceddb6a4b608d63a19b9c74b2edb0f73369e43
Dain Sundstrom <dain@iq80.com>
2026-02-20 13:34:20 -0800

    Add LDAP suite

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/suite/SuiteLdap.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.suite;
+
+import io.trino.tests.product.TestGroup;
+import io.trino.tests.product.ldap.LdapAndFileEnvironment;
+import io.trino.tests.product.ldap.LdapAndFileRerunEnvironment;
+import io.trino.tests.product.ldap.LdapBasicEnvironment;
+import io.trino.tests.product.ldap.LdapBindDnEnvironment;
+import io.trino.tests.product.ldap.LdapInsecureEnvironment;
+import io.trino.tests.product.ldap.LdapReferralsEnvironment;
+import io.trino.tests.product.suite.SuiteRunner.TestRunResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SuiteLdap
+{
+    private SuiteLdap() {}
+
+    public static void main(String[] args)
+    {
+        System.setProperty("trino.product-test.environment-mode", "STRICT");
+
+        List<TestRunResult> results = new ArrayList<>();
+
+        results.add(SuiteRunner.forEnvironment(LdapBasicEnvironment.class)
+                .includeTag(TestGroup.Ldap.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(LdapAndFileEnvironment.class)
+                .includeTag(TestGroup.LdapAndFile.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(LdapAndFileRerunEnvironment.class)
+                .includeTag(TestGroup.Ldap.class)
+                .excludeTag(TestGroup.LdapMultipleBinds.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(LdapInsecureEnvironment.class)
+                .includeTag(TestGroup.Ldap.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(LdapReferralsEnvironment.class)
+                .includeTag(TestGroup.Ldap.class)
+                .run());
+
+        results.add(SuiteRunner.forEnvironment(LdapBindDnEnvironment.class)
+                .includeTag(TestGroup.Ldap.class)
+                .excludeTag(TestGroup.LdapMultipleBinds.class)
+                .run());
+
+        SuiteRunner.printSummary(results);
+        System.exit(SuiteRunner.hasFailures(results) ? 1 : 0);
+    }
+}
