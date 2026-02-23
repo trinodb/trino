ae5efb6a7a31e12fb6f1881899a454698e577757
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-02-23 15:39:00 -0500

    Suggest environment name

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/EnvironmentFactory.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/EnvironmentFactory.java
@@ -13,19 +13,29 @@
  */
 package io.trino.tests.product.launcher.env;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
+import com.google.inject.ConfigurationException;
 import com.google.inject.Inject;
+import com.google.inject.spi.Message;
+import info.debatty.java.stringsimilarity.JaroWinkler;
+import info.debatty.java.stringsimilarity.interfaces.StringSimilarity;
 
 import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Verify.verify;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.trino.tests.product.launcher.Configurations.canonicalEnvironmentName;
+import static java.util.Comparator.comparingDouble;
 import static java.util.Objects.requireNonNull;
 
 public final class EnvironmentFactory
 {
+    private static final StringSimilarity SIMILARITY = new JaroWinkler();
+
     private final Map<String, EnvironmentProvider> environmentProviders;
 
     @Inject
@@ -37,7 +47,9 @@ public final class EnvironmentFactory
     public Environment.Builder get(String environmentName, PrintStream printStream, EnvironmentConfig config, Map<String, String> extraOptions)
     {
         environmentName = canonicalEnvironmentName(environmentName);
-        checkArgument(environmentProviders.containsKey(environmentName), "No environment with name '%s'. Those do exist, however: %s", environmentName, list());
+        if (!environmentProviders.containsKey(environmentName)) {
+            throw new ConfigurationException(ImmutableList.of(new Message("No environment with name '%s'%s".formatted(environmentName, suggest(environmentName, environmentProviders.keySet())))));
+        }
         return environmentProviders.get(environmentName)
                 .createEnvironment(environmentName, printStream, config, extraOptions);
     }
@@ -45,5 +57,39 @@ public final class EnvironmentFactory
     public List<String> list()
     {
         return Ordering.natural().sortedCopy(environmentProviders.keySet());
+    }
+
+    private static String suggest(String environmentName, Set<String> allEnvironments)
+    {
+        List<String> suggestions = findSimilar(environmentName, allEnvironments, 3);
+        if (suggestions.isEmpty()) {
+            return "";
+        }
+
+        return ". Did you mean to use " + switch (suggestions.size()) {
+            case 3 -> "'" + suggestions.get(0) + "', '" + suggestions.get(1) + "' or '" + suggestions.get(2) + "'?";
+            case 2 -> "'" + suggestions.get(0) + "' or '" + suggestions.get(1) + "'?";
+            default -> "'" + suggestions.get(0) + "'?";
+        };
+    }
+
+    private static List<String> findSimilar(String environmentName, Set<String> allEnvironments, int count)
+    {
+        return allEnvironments.stream()
+                .map(candidate -> new Match(candidate, SIMILARITY.similarity(environmentName, candidate)))
+                .filter(match -> match.ratio() > 0.5)
+                .sorted(comparingDouble(Match::ratio).reversed())
+                .limit(count)
+                .map(Match::environment)
+                .collect(toImmutableList());
+    }
+
+    private record Match(String environment, double ratio)
+    {
+        public Match
+        {
+            requireNonNull(environment, "environment is null");
+            verify(ratio >= 0.0 && ratio <= 1.0, "ratio must be in the [0, 1.0] range");
+        }
     }
 }
