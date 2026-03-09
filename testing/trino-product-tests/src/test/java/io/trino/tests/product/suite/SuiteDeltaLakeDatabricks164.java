68c3b7e893a1e1a65d4ff39a3145611a0237625c
Dain Sundstrom <dain@iq80.com>
2026-03-09 10:47:59 -0700

    Add Databricks 164 suite

---
+++ testing/trino-product-tests/src/test/java/io/trino/tests/product/suite/SuiteDeltaLakeDatabricks164.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.suite;
+
+import io.trino.tests.product.TestGroup;
+import io.trino.tests.product.deltalake.DeltaLakeDatabricks164Environment;
+import io.trino.tests.product.suite.SuiteRunner.TestRunResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SuiteDeltaLakeDatabricks164
+{
+    // Parity note: legacy launcher keeps a dedicated 164 lane filtered by DELTA_LAKE_DATABRICKS_164.
+    // There are currently no tests tagged for that group, so this lane intentionally discovers 0 tests.
+    private SuiteDeltaLakeDatabricks164() {}
+
+    public static void main(String[] args)
+    {
+        System.setProperty("trino.product-test.environment-mode", "STRICT");
+
+        List<TestRunResult> results = new ArrayList<>();
+        results.add(SuiteRunner.forEnvironment(DeltaLakeDatabricks164Environment.class)
+                .includeTag(TestGroup.ConfiguredFeatures.class)
+                .includeTag(TestGroup.DeltaLakeDatabricks164.class)
+                .run());
+
+        SuiteRunner.printSummary(results);
+        System.exit(SuiteRunner.hasFailures(results) ? 1 : 0);
+    }
+}
