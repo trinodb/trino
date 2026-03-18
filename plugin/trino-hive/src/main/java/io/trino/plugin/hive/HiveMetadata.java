9cdf97a9a1d4fc7ad7874df27c9efb84d775eda5
Yuya Ebihara <ebyhry@gmail.com>
2026-03-19 07:45:26 +0900

    Revert "Fix updating column statistics when inserting with overwrite mode"
    This reverts commit 571fd670d290529d981169ea8e2f4e17b901f69a.

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
@@ -181,7 +181,6 @@ import static io.trino.metastore.Partitions.toPartitionValues;
 import static io.trino.metastore.PrincipalPrivileges.NO_PRIVILEGES;
 import static io.trino.metastore.PrincipalPrivileges.fromHivePrivilegeInfos;
 import static io.trino.metastore.StatisticsUpdateMode.MERGE_INCREMENTAL;
-import static io.trino.metastore.StatisticsUpdateMode.OVERWRITE_ALL;
 import static io.trino.metastore.StorageFormat.VIEW_STORAGE_FORMAT;
 import static io.trino.metastore.type.Category.PRIMITIVE;
 import static io.trino.parquet.writer.ParquetWriter.SUPPORTED_BLOOM_FILTER_TYPES;
@@ -2362,8 +2361,7 @@ public class HiveMetadata
                                 partitionValues,
                                 partitionUpdate.getWritePath(),
                                 partitionUpdate.getFileNames(),
-                                partitionStatistics,
-                                MERGE_INCREMENTAL));
+                                partitionStatistics));
             }
             else if (partitionUpdate.getUpdateMode() == NEW || partitionUpdate.getUpdateMode() == OVERWRITE) {
                 // insert into new partition or overwrite existing partition
@@ -2388,13 +2386,6 @@ public class HiveMetadata
                             TrinoFileSystem fileSystem = fileSystemFactory.create(session);
                             cleanExtraOutputFiles(fileSystem, session.getQueryId(), partitionUpdate.getTargetPath(), ImmutableSet.copyOf(partitionUpdate.getFileNames()));
                         }
-                        partitionUpdateInfosBuilder.add(
-                                new PartitionUpdateInfo(
-                                        partitionValues,
-                                        partitionUpdate.getWritePath(),
-                                        partitionUpdate.getFileNames(),
-                                        partitionStatistics,
-                                        OVERWRITE_ALL));
                     }
                     else {
                         metastore.dropPartition(session, handle.getSchemaName(), handle.getTableName(), partition.getValues(), true);
@@ -2700,8 +2691,7 @@ public class HiveMetadata
                                 partitionValues,
                                 partitionUpdate.getWritePath(),
                                 partitionUpdate.getFileNames(),
-                                PartitionStatistics.empty(),
-                                MERGE_INCREMENTAL));
+                                PartitionStatistics.empty()));
             }
         }
 
