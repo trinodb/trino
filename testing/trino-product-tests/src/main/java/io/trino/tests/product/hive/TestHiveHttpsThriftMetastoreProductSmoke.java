7be4843094c8e09a0e9d783882865674534eeb7b
niteshy <nitesh.yadav.cse@gmail.com>
2026-04-21 21:23:27 -0700

    Add product tests for Hive Thrift metastore over HTTP and HTTPS
    Add launcher environments and a small suite so CI exercises the
    HttpThriftMetastoreClient path against Hive4 HMS with Thrift over Http &
    HTTPS.
    
    - HTTPS: nginx TLS termination + coordinator truststore/JAVA_TOOL_OPTIONS
    - Catalogs: http(s) metastore URIs, bearer rules, x-actor-username header
    - Fix HMS HTTP/HTTPS PT smoke tests running under unrelated suites

---
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveHttpsThriftMetastoreProductSmoke.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.tests.product.hive;
+
+import io.trino.tempto.ProductTest;
+import org.testng.annotations.Test;
+
+import static io.trino.tempto.assertions.QueryAssert.Row.row;
+import static io.trino.tempto.assertions.QueryAssert.assertThat;
+import static io.trino.tests.product.TestGroups.HIVE4_HTTPS_THRIFT_METASTORE;
+import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
+import static io.trino.tests.product.utils.QueryExecutors.onTrino;
+
+public class TestHiveHttpsThriftMetastoreProductSmoke
+        extends ProductTest
+{
+    @Test(groups = {HIVE4_HTTPS_THRIFT_METASTORE, PROFILE_SPECIFIC_TESTS})
+    public void testSelectFromInformationSchema()
+    {
+        assertThat(onTrino().executeQuery("SELECT count(*) FROM hive.information_schema.schemata WHERE schema_name = 'default'"))
+                .containsOnly(row(1));
+    }
+}
