eba05afe4b97e952da566bda48fbc0bc759f224a
Dain Sundstrom <dain@iq80.com>
2026-05-04 16:21:25 -0700

    Write short variant timestamps as micros

--- core/trino-main/src/test/java/io/trino/util/variant/TestVariantWriter.java
+++ core/trino-main/src/test/java/io/trino/util/variant/TestVariantWriter.java
@@ -14,6 +14,8 @@
 package io.trino.util.variant;
 
 import io.airlift.slice.Slice;
+import io.trino.spi.type.LongTimestamp;
+import io.trino.spi.type.LongTimestampWithTimeZone;
 import io.trino.spi.type.Type;
 import io.trino.spi.variant.Variant;
 import org.junit.jupiter.api.Test;
@@ -26,9 +28,18 @@ import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.RealType.REAL;
 import static io.trino.spi.type.SmallintType.SMALLINT;
+import static io.trino.spi.type.TimeZoneKey.UTC_KEY;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_MICROS;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_NANOS;
+import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
+import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_NANOS;
 import static io.trino.spi.type.TinyintType.TINYINT;
 import static io.trino.spi.type.VarbinaryType.VARBINARY;
 import static io.trino.spi.type.VarcharType.VARCHAR;
+import static io.trino.spi.variant.Header.PrimitiveType.TIMESTAMP_NTZ_MICROS;
+import static io.trino.spi.variant.Header.PrimitiveType.TIMESTAMP_NTZ_NANOS;
+import static io.trino.spi.variant.Header.PrimitiveType.TIMESTAMP_UTC_MICROS;
+import static io.trino.spi.variant.Header.PrimitiveType.TIMESTAMP_UTC_NANOS;
 import static java.lang.Float.floatToRawIntBits;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,6 +67,34 @@ class TestVariantWriter
     {
         assertPrimitiveWrite(BOOLEAN, null, Variant.NULL_VALUE);
         assertPrimitiveWrite(VARCHAR, null, Variant.NULL_VALUE);
+    }
+
+    @Test
+    void testTimestampPrecision()
+    {
+        long epochMicros = Long.MIN_VALUE;
+        Variant microsTimestamp = VariantWriter.create(TIMESTAMP_MICROS).write(epochMicros);
+        assertThat(microsTimestamp.primitiveType()).isEqualTo(TIMESTAMP_NTZ_MICROS);
+        assertThat(microsTimestamp.getTimestampMicros()).isEqualTo(epochMicros);
+
+        LongTimestamp timestamp = new LongTimestamp(123_456L, 789_000);
+        Variant nanosTimestamp = VariantWriter.create(TIMESTAMP_NANOS).write(timestamp);
+        assertThat(nanosTimestamp.primitiveType()).isEqualTo(TIMESTAMP_NTZ_NANOS);
+        assertThat(nanosTimestamp.getTimestampNanos()).isEqualTo(123_456_789L);
+    }
+
+    @Test
+    void testTimestampWithTimeZonePrecision()
+    {
+        LongTimestampWithTimeZone microsTimestampWithTimeZone = LongTimestampWithTimeZone.fromEpochMillisAndFraction(123L, 456_000_000, UTC_KEY);
+        Variant microsTimestamp = VariantWriter.create(TIMESTAMP_TZ_MICROS).write(microsTimestampWithTimeZone);
+        assertThat(microsTimestamp.primitiveType()).isEqualTo(TIMESTAMP_UTC_MICROS);
+        assertThat(microsTimestamp.getTimestampMicros()).isEqualTo(123_456L);
+
+        LongTimestampWithTimeZone nanosTimestampWithTimeZone = LongTimestampWithTimeZone.fromEpochMillisAndFraction(123L, 456_789_000, UTC_KEY);
+        Variant nanosTimestamp = VariantWriter.create(TIMESTAMP_TZ_NANOS).write(nanosTimestampWithTimeZone);
+        assertThat(nanosTimestamp.primitiveType()).isEqualTo(TIMESTAMP_UTC_NANOS);
+        assertThat(nanosTimestamp.getTimestampNanos()).isEqualTo(123_456_789L);
     }
 
     private static void assertPrimitiveWrite(Type type, Object value, Variant expected)
