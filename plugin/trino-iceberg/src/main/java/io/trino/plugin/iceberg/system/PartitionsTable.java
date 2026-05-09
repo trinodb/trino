1c6f74ea49cf46b13afc4c4f04f7a288d5c4c726
Jalpan Randeri <jalpaan.randeri@gmail.com>
2026-05-08 22:21:53 -0700

    basic scafoldding without configurable guard
    working test
    
    reset jdk to 25
    
    importing airlift style
    
    style

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/system/PartitionsTable.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/system/PartitionsTable.java
@@ -16,6 +16,7 @@ package io.trino.plugin.iceberg.system;
 import com.google.common.collect.ImmutableList;
 import io.trino.plugin.iceberg.IcebergStatistics;
 import io.trino.plugin.iceberg.StructLikeWrapperWithFieldIdToIndex;
+import io.trino.spi.TrinoException;
 import io.trino.spi.block.SqlRow;
 import io.trino.spi.connector.ColumnMetadata;
 import io.trino.spi.connector.ConnectorSession;
@@ -25,14 +26,19 @@ import io.trino.spi.connector.InMemoryRecordSet;
 import io.trino.spi.connector.RecordCursor;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.connector.SystemTable;
+import io.trino.spi.predicate.Domain;
+import io.trino.spi.predicate.Range;
 import io.trino.spi.predicate.TupleDomain;
 import io.trino.spi.type.RowType;
 import io.trino.spi.type.TypeManager;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.PartitionField;
+import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableScan;
+import org.apache.iceberg.expressions.Expression;
+import org.apache.iceberg.expressions.Expressions;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types.NestedField;
@@ -53,6 +59,7 @@ import java.util.stream.Stream;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static io.trino.plugin.iceberg.IcebergTypes.convertIcebergValueToTrino;
+import static io.trino.plugin.iceberg.IcebergTypes.convertTrinoValueToIceberg;
 import static io.trino.plugin.iceberg.IcebergUtil.getIdentityPartitions;
 import static io.trino.plugin.iceberg.IcebergUtil.primitiveFieldTypes;
 import static io.trino.plugin.iceberg.StructLikeWrapperWithFieldIdToIndex.createStructLikeWrapper;
@@ -60,15 +67,26 @@ import static io.trino.plugin.iceberg.TypeConverter.toTrinoType;
 import static io.trino.plugin.iceberg.util.SystemTableUtil.getAllPartitionFields;
 import static io.trino.plugin.iceberg.util.SystemTableUtil.getPartitionColumnType;
 import static io.trino.plugin.iceberg.util.SystemTableUtil.partitionTypes;
+import static io.trino.spi.StandardErrorCode.QUERY_REJECTED;
 import static io.trino.spi.block.RowValueBuilder.buildRowValue;
 import static io.trino.spi.type.BigintType.BIGINT;
 import static io.trino.spi.type.TypeUtils.writeNativeValue;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toSet;
 
 public class PartitionsTable
         implements SystemTable
 {
+    // TODO: compute() upon feedback
+    //  current logic is
+    //  knob-1: estimated-data-file-object-size = 5 kb
+    //  knob-2: allowed-budget = 5% of -xmX (e.g. 500 gb = 25 gb )
+    //  Formula:
+    //                       ( allowed budget ) x 1024 x 1024
+    //    - allowed-files = ----------------------------------
+    //                           estimated-file-size-in-kb
+    private static final long MAX_FILES_PER_PARTITIONS_SCAN = 100L;
     private final TypeManager typeManager;
     private final Table icebergTable;
     private final OptionalLong snapshotId;
@@ -97,6 +115,15 @@ public class PartitionsTable
         this.partitionColumnType = getPartitionColumnType(typeManager, partitionFields, icebergTable.schema());
         partitionColumnType.ifPresent(icebergPartitionColumn ->
                 columnMetadataBuilder.add(new ColumnMetadata("partition", icebergPartitionColumn.rowType())));
+        // expose each identity-transform partitoin field as a top-level column so predicate on those value can be
+        // pushed to iceberg scan api
+        Schema schema = icebergTable.schema();
+        for (PartitionField field : partitionFields) {
+            if (field.transform().isIdentity()) {
+                Type sourceType = schema.findType(field.sourceId());
+                columnMetadataBuilder.add(new ColumnMetadata(field.name(), toTrinoType(sourceType, typeManager)));
+            }
+        }
 
         Stream.of("record_count", "file_count", "total_size")
                 .forEach(metric -> columnMetadataBuilder.add(new ColumnMetadata(metric, BIGINT)));
@@ -161,29 +188,143 @@ public class PartitionsTable
     @Override
     public RecordCursor cursor(ConnectorTransactionHandle transactionHandle, ConnectorSession session, TupleDomain<Integer> constraint)
     {
-        if (snapshotId.isEmpty()) {
+        var icebergFilter = convertToIcebergFilter(constraint);
+        if (snapshotId.isEmpty() || icebergFilter == Expressions.alwaysFalse()) {
             return new InMemoryRecordSet(resultTypes, ImmutableList.of()).cursor();
         }
+
+        // Pre-flight guard:
+        if (icebergFilter == Expressions.alwaysTrue()) {
+            var totalDatafiles = Long.parseLong(
+                    icebergTable.snapshot(snapshotId.getAsLong())
+                            .summary()
+                            .getOrDefault("total-data-files", "0"));
+            if (totalDatafiles > MAX_FILES_PER_PARTITIONS_SCAN) {
+                throw new TrinoException(QUERY_REJECTED,
+                        format("Scan on %s would visit %d of data files unfiltered, exceeding threshold(%d). Add a predicate " +
+                                        "on top-level partition column to prune the manifest",
+                                icebergTable.name(), totalDatafiles, MAX_FILES_PER_PARTITIONS_SCAN));
+            }
+        }
+
         TableScan tableScan = icebergTable.newScan()
                 .useSnapshot(snapshotId.getAsLong())
+                .filter(icebergFilter)
                 .includeColumnStats()
                 .planWith(executor);
         // TODO make the cursor lazy
         return buildRecordCursor(getStatisticsByPartition(tableScan));
     }
 
+    private Expression convertToIcebergFilter(TupleDomain<Integer> constraint)
+    {
+        if (constraint.isAll()) {
+            return Expressions.alwaysTrue();
+        }
+        else if (constraint.isNone()) {
+            return Expressions.alwaysFalse();
+        }
+
+        Map<Integer, Domain> domains = constraint.getDomains().orElseThrow();
+        Schema schema = icebergTable.schema();
+        int startIdx = partitionColumnType.isPresent() ? 1 : 0;
+
+        Expression result = Expressions.alwaysTrue();
+        for (int i = 0; i < partitionFields.size(); i++) {
+            var colDomain = domains.get(startIdx + i);
+            if (colDomain == null || colDomain.isAll()) {
+                continue;
+            }
+            PartitionField field = partitionFields.get(i);
+            var sourceName = schema.findColumnName(field.sourceId());
+            var trinoType = toTrinoType(schema.findType(field.sourceId()), typeManager);
+            var exp = domainToExpression(sourceName, trinoType, colDomain);
+            if (exp.isPresent()) {
+                result = Expressions.and(result, exp.get());
+            }
+        }
+        return result;
+    }
+
+    private static Optional<Expression> domainToExpression(String fieldName,
+            io.trino.spi.type.Type trinoType,
+            Domain domain)
+    {
+        if (domain.isAll()) {
+            return Optional.empty();
+        }
+        else if (domain.isNone()) {
+            return Optional.of(Expressions.alwaysFalse());
+        }
+        else if (domain.isOnlyNull()) {
+            return Optional.of(Expressions.isNull(fieldName));
+        }
+        else if (domain.getValues().isAll() && !domain.isNullAllowed()) {
+            return Optional.of(Expressions.notNull(fieldName));
+        }
+        else {
+            var rangeExp = domain.getValues()
+                    .getRanges()
+                    .getOrderedRanges()
+                    .stream()
+                    .map(range -> rangeToExpression(fieldName, trinoType, range))
+                    .toList();
+            var valExp = rangeExp.stream()
+                    .reduce(Expressions::or)
+                    .orElse(Expressions.alwaysFalse());
+
+            return Optional.of(domain.isNullAllowed()
+                    ? Expressions.or(valExp, Expressions.isNull(fieldName))
+                    : valExp);
+        }
+    }
+
+    private static Expression rangeToExpression(String fieldName,
+            io.trino.spi.type.Type trinoType,
+            Range range)
+    {
+        if (range.isSingleValue()) {
+            var icebergValue = convertTrinoValueToIceberg(trinoType, range.getSingleValue());
+            return Expressions.equal(fieldName, icebergValue);
+        }
+
+        Expression exp = Expressions.alwaysTrue();
+        if (!range.isLowUnbounded()) {
+            var low = convertTrinoValueToIceberg(trinoType, range.getLowBoundedValue());
+            exp = Expressions.and(exp, range.isLowInclusive()
+                    ? Expressions.greaterThanOrEqual(fieldName, low)
+                    : Expressions.greaterThan(fieldName, low));
+        }
+
+        if (!range.isHighUnbounded()) {
+            var high = convertTrinoValueToIceberg(trinoType, range.getHighBoundedValue());
+            exp = Expressions.and(exp, range.isHighInclusive()
+                    ? Expressions.greaterThanOrEqual(fieldName, high)
+                    : Expressions.greaterThan(fieldName, high));
+        }
+        return exp;
+    }
+
     private Map<StructLikeWrapperWithFieldIdToIndex, IcebergStatistics> getStatisticsByPartition(TableScan tableScan)
     {
         try (CloseableIterable<FileScanTask> fileScanTasks = tableScan.planFiles()) {
             Map<StructLikeWrapperWithFieldIdToIndex, IcebergStatistics.Builder> partitions = new HashMap<>();
+            long fileCount = 0;
             for (FileScanTask fileScanTask : fileScanTasks) {
+                if (fileCount > MAX_FILES_PER_PARTITIONS_SCAN) {
+                    throw new TrinoException(QUERY_REJECTED,
+                            format("Scan on %s exceeded threshold %d. Add a more selective predicate " +
+                                            "on top-level partition column to prune the manifest",
+                                    icebergTable.name(), MAX_FILES_PER_PARTITIONS_SCAN));
+                }
                 DataFile dataFile = fileScanTask.file();
                 StructLikeWrapperWithFieldIdToIndex structLikeWrapperWithFieldIdToIndex = createStructLikeWrapper(fileScanTask);
 
                 partitions.computeIfAbsent(
-                        structLikeWrapperWithFieldIdToIndex,
-                        _ -> new IcebergStatistics.Builder(icebergTable.schema().columns(), typeManager))
+                                structLikeWrapperWithFieldIdToIndex,
+                                _ -> new IcebergStatistics.Builder(icebergTable.schema().columns(), typeManager))
                         .acceptDataFile(dataFile, fileScanTask.spec());
+                fileCount++;
             }
 
             return partitions.entrySet().stream()
@@ -227,6 +368,25 @@ public class PartitionsTable
                     }
                 }));
             });
+
+            // add top level partition columns
+            for (PartitionField field : partitionFields) {
+                if (field.transform().isIdentity()) {
+                    Type sourceType = icebergTable.schema().findType(field.sourceId());
+                    Integer fieldId = field.fieldId();
+                    if (partitionStruct.getFieldIdToIndex().containsKey(fieldId)) {
+                        row.add(convertIcebergValueToTrino(
+                                sourceType,
+                                partitionStruct.getStructLikeWrapper()
+                                        .get()
+                                        .get(partitionStruct.getFieldIdToIndex().get(fieldId),
+                                                sourceType.typeId().javaClass())));
+                    }
+                    else {
+                        row.add(null);
+                    }
+                }
+            }
 
             // add the top level metrics.
             row.add(icebergStatistics.recordCount());
