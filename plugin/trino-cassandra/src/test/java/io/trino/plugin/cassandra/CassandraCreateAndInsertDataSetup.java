655f3892edfeb58f68731c5cc96480199429f130
Dain Sundstrom <dain@iq80.com>
2026-05-14 11:38:31 -0700

    Harden Cassandra test table cleanup

--- plugin/trino-cassandra/src/test/java/io/trino/plugin/cassandra/CassandraCreateAndInsertDataSetup.java
+++ plugin/trino-cassandra/src/test/java/io/trino/plugin/cassandra/CassandraCreateAndInsertDataSetup.java
@@ -15,6 +15,7 @@ package io.trino.plugin.cassandra;
 
 import com.google.common.collect.ImmutableList;
 import io.airlift.json.JsonCodec;
+import io.airlift.units.Duration;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.datatype.ColumnSetup;
 import io.trino.testing.datatype.DataSetup;
@@ -35,6 +36,7 @@ import static io.trino.plugin.cassandra.util.CassandraCqlUtils.quoteStringLitera
 import static io.trino.testing.assertions.Assert.assertEventually;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -108,13 +110,13 @@ public class CassandraCreateAndInsertDataSetup
 
     private void waitForTableVisibility(String keyspaceName, String tableName)
     {
-        assertEventually(() -> assertThat(queryRunner.execute(format("SELECT * FROM %s.%s", keyspaceName, tableName)).getRowCount())
+        assertEventually(new Duration(1, MINUTES), () -> assertThat(queryRunner.execute(format("SELECT * FROM %s.%s", keyspaceName, tableName)).getRowCount())
                 .isEqualTo(1));
     }
 
     private TestTable createTestTable(List<ColumnSetup> inputs)
     {
-        return new TestTable(sqlExecutor, tableNamePrefix, tableDefinition(inputs));
+        return new CassandraTestTable(sqlExecutor, tableNamePrefix, tableDefinition(inputs));
     }
 
     private String tableDefinition(List<ColumnSetup> inputs)
@@ -130,5 +132,20 @@ public class CassandraCreateAndInsertDataSetup
         return IntStream.range(0, inputs.size())
                 .mapToObj(column -> format("col_%d %s", column, inputs.get(column).getDeclaredType().orElseThrow()))
                 .collect(joining(",", "(" + ID_COLUMN_NAME + " uuid PRIMARY KEY,", ") WITH comment=" + quoteStringLiteral(PRESTO_COMMENT_METADATA + " " + columnMetadata)));
+    }
+
+    private static class CassandraTestTable
+            extends TestTable
+    {
+        public CassandraTestTable(SqlExecutor sqlExecutor, String namePrefix, String tableDefinition)
+        {
+            super(sqlExecutor, namePrefix, tableDefinition);
+        }
+
+        @Override
+        public void close()
+        {
+            TestCassandraTable.dropTableWithRetry(sqlExecutor, name);
+        }
     }
 }
