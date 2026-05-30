727fe7b4f830fe84bf9b1f74ec79379312340251
Martin Traverso <mtraverso@gmail.com>
2026-05-30 12:47:10 +0000

    Support CARDINALITY and UNNEST on multisets
    CARDINALITY returns a multiset's element count and UNNEST expands a multiset into
    rows, the two most basic ways to consume a multiset value. Both already work for
    arrays over the same block representation, so extend them to accept the multiset
    collection kind as well.

---
+++ core/trino-main/src/main/java/io/trino/operator/scalar/MultisetCardinalityFunction.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.operator.scalar;
+
+import io.trino.spi.block.SqlMultiset;
+import io.trino.spi.function.Description;
+import io.trino.spi.function.ScalarFunction;
+import io.trino.spi.function.SqlType;
+import io.trino.spi.function.TypeParameter;
+import io.trino.spi.type.StandardTypes;
+
+@Description("Returns the cardinality (number of elements) of the multiset")
+@ScalarFunction(value = "cardinality", neverFails = true)
+public final class MultisetCardinalityFunction
+{
+    private MultisetCardinalityFunction() {}
+
+    @TypeParameter("E")
+    @SqlType(StandardTypes.BIGINT)
+    public static long multisetCardinality(@SqlType("multiset(E)") SqlMultiset multiset)
+    {
+        return multiset.getSize();
+    }
+}
