8801f9a47a0b37f5f028e1888991b4f1b41be413
Yuya Ebihara <ebyhry@gmail.com>
2026-06-03 17:47:28 +0900

    Fix typo in RegisterTableProcedure

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/procedure/RegisterTableProcedure.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/procedure/RegisterTableProcedure.java
@@ -167,7 +167,7 @@ public class RegisterTableProcedure
             throw new TrinoException(
                     ICEBERG_INVALID_METADATA,
                     """
-                    Table metadata file [%s] declares table location as [%s] which is differs from location provided [%s]. \
+                    Table metadata file [%s] declares table location as [%s] which differs from location provided [%s]. \
                     Iceberg table can only be registered with the same location it was created with.""".formatted(metadataLocation, tableMetadata.location(), tableLocation));
         }
 
