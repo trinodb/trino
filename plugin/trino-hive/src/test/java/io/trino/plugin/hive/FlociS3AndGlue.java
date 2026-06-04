bcd43ca7f6f7543a906ec76f11cbc8c42b94fbd3
David Phillips <david@acz.org>
2026-06-04 14:59:47 -0700

    Add Floci S3 and Glue tests

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/FlociS3AndGlue.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/FlociS3AndGlue.java
@@ -14,10 +14,20 @@
 package io.trino.plugin.hive;
 
 import com.google.common.collect.ImmutableMap;
+import io.opentelemetry.api.OpenTelemetry;
+import io.trino.filesystem.s3.S3FileSystemConfig;
+import io.trino.filesystem.s3.S3FileSystemFactory;
+import io.trino.filesystem.s3.S3FileSystemStats;
+import io.trino.plugin.hive.metastore.glue.GlueHiveMetastoreConfig;
 import io.trino.testing.containers.Floci;
 import software.amazon.awssdk.services.glue.GlueClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.Map;
+
+import static io.trino.testing.containers.Floci.FLOCI_ACCESS_KEY;
+import static io.trino.testing.containers.Floci.FLOCI_REGION;
+import static io.trino.testing.containers.Floci.FLOCI_SECRET_KEY;
 
 public final class FlociS3AndGlue
         implements AutoCloseable
@@ -34,6 +44,26 @@ public final class FlociS3AndGlue
         floci.createBucket(bucketName);
     }
 
+    public S3Client createS3Client()
+    {
+        return S3Client.builder()
+                .applyMutation(floci::updateClient)
+                .build();
+    }
+
+    public S3FileSystemFactory createFileSystemFactory()
+    {
+        return new S3FileSystemFactory(
+                OpenTelemetry.noop(),
+                new S3FileSystemConfig()
+                        .setAwsAccessKey(FLOCI_ACCESS_KEY)
+                        .setAwsSecretKey(FLOCI_SECRET_KEY)
+                        .setEndpoint(floci.endpoint().toString())
+                        .setRegion(FLOCI_REGION)
+                        .setPathStyleAccess(true),
+                new S3FileSystemStats());
+    }
+
     public GlueClient createGlueClient()
     {
         return GlueClient.builder()
@@ -45,9 +75,9 @@ public final class FlociS3AndGlue
     {
         return ImmutableMap.<String, String>builder()
                 .put("hive.metastore.glue.endpoint-url", floci.endpoint().toString())
-                .put("hive.metastore.glue.region", Floci.FLOCI_REGION)
-                .put("hive.metastore.glue.aws-access-key", Floci.FLOCI_ACCESS_KEY)
-                .put("hive.metastore.glue.aws-secret-key", Floci.FLOCI_SECRET_KEY)
+                .put("hive.metastore.glue.region", FLOCI_REGION)
+                .put("hive.metastore.glue.aws-access-key", FLOCI_ACCESS_KEY)
+                .put("hive.metastore.glue.aws-secret-key", FLOCI_SECRET_KEY)
                 .buildOrThrow();
     }
 
@@ -55,12 +85,20 @@ public final class FlociS3AndGlue
     {
         return ImmutableMap.<String, String>builder()
                 .putAll(glueProperties())
-                .put("s3.region", Floci.FLOCI_REGION)
                 .put("s3.endpoint", floci.endpoint().toString())
-                .put("s3.aws-access-key", Floci.FLOCI_ACCESS_KEY)
-                .put("s3.aws-secret-key", Floci.FLOCI_SECRET_KEY)
+                .put("s3.region", FLOCI_REGION)
+                .put("s3.aws-access-key", FLOCI_ACCESS_KEY)
+                .put("s3.aws-secret-key", FLOCI_SECRET_KEY)
                 .put("s3.path-style-access", "true")
                 .buildOrThrow();
+    }
+
+    public void configureGlueHiveMetastore(GlueHiveMetastoreConfig config)
+    {
+        config.setGlueEndpointUrl(floci.endpoint())
+                .setGlueRegion(FLOCI_REGION)
+                .setAwsAccessKey(FLOCI_ACCESS_KEY)
+                .setAwsSecretKey(FLOCI_SECRET_KEY);
     }
 
     @Override
