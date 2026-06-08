ef750d12ac9bd61f31561b6ee95c167e8921f57d
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-06-08 16:56:00 +0200

    Update airbase to 387

--- lib/trino-plugin-toolkit/src/test/java/io/trino/plugin/base/util/TestJsonUtils.java
+++ lib/trino-plugin-toolkit/src/test/java/io/trino/plugin/base/util/TestJsonUtils.java
@@ -54,12 +54,11 @@ public class TestJsonUtils
 
     @Test
     public void testTrailingContent()
-            throws IOException
     {
         // parseJson(String)
         assertThatThrownBy(() -> parseJson("{} {}}", JsonNode.class))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Found characters after the expected end of input");
+                .isInstanceOf(UncheckedIOException.class)
+                .hasMessage("Could not parse JSON");
         assertThatThrownBy(() -> parseJson("{} not even a JSON here", JsonNode.class))
                 .isInstanceOf(UncheckedIOException.class)
                 .hasMessage("Could not parse JSON")
@@ -67,8 +66,8 @@ public class TestJsonUtils
 
         // parseJson(byte[], Class)
         assertThatThrownBy(() -> parseJson("{} {}}".getBytes(US_ASCII), TestObject.class))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Found characters after the expected end of input");
+                .isInstanceOf(UncheckedIOException.class)
+                .hasMessage("Could not parse JSON");
         assertThatThrownBy(() -> parseJson("{} not even a JSON here".getBytes(US_ASCII), TestObject.class))
                 .isInstanceOf(UncheckedIOException.class)
                 .hasMessage("Could not parse JSON")
