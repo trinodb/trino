3ca7ba24ccc1fff40e2a989cb65cec333995fc03
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-12 12:08:55 +0200

    Annotate VARCHAR cast operators fallibility

--- core/trino-main/src/main/java/io/trino/type/VarcharOperators.java
+++ core/trino-main/src/main/java/io/trino/type/VarcharOperators.java
@@ -32,6 +32,7 @@ public final class VarcharOperators
 {
     private VarcharOperators() {}
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.BOOLEAN)
@@ -74,6 +75,7 @@ public final class VarcharOperators
         return (b >= 'a') && (b <= 'z');
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.DOUBLE)
@@ -87,6 +89,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.REAL)
@@ -100,6 +103,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.BIGINT)
@@ -113,6 +117,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.INTEGER)
@@ -126,6 +131,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.SMALLINT)
@@ -139,6 +145,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.TINYINT)
@@ -152,6 +159,7 @@ public final class VarcharOperators
         }
     }
 
+    // fallible
     @LiteralParameters("x")
     @ScalarOperator(CAST)
     @SqlType(StandardTypes.NUMBER)
@@ -173,7 +181,7 @@ public final class VarcharOperators
     }
 
     @LiteralParameters("x")
-    @ScalarOperator(CAST)
+    @ScalarOperator(value = CAST, neverFails = true)
     @SqlType(StandardTypes.VARBINARY)
     public static Slice castToBinary(@SqlType("varchar(x)") Slice slice)
     {
