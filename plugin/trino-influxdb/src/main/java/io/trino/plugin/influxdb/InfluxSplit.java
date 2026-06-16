36ecb73e49b23ec7ebddc672e474827fd153ce91
Daniel Erwan <contact@erwandaniel.fr>
2026-06-17 00:20:02 +0200

    Add InfluxDB connector

---
+++ plugin/trino-influxdb/src/main/java/io/trino/plugin/influxdb/InfluxSplit.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.trino.plugin.influxdb;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.slice.SizeOf;
+import io.trino.spi.HostAddress;
+import io.trino.spi.connector.ConnectorSplit;
+
+import java.util.List;
+
+import static io.airlift.slice.SizeOf.estimatedSizeOf;
+
+public record InfluxSplit(List<HostAddress> addresses)
+        implements ConnectorSplit
+{
+    private static final long INSTANCE_SIZE = SizeOf.instanceSize(InfluxSplit.class);
+
+    public InfluxSplit()
+    {
+        this(ImmutableList.of());
+    }
+
+    @Override
+    public List<HostAddress> getAddresses()
+    {
+        return addresses;
+    }
+
+    @Override
+    public long getRetainedSizeInBytes()
+    {
+        return INSTANCE_SIZE
+                + estimatedSizeOf(addresses, HostAddress::getRetainedSizeInBytes);
+    }
+}
