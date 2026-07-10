c7699c0eb5a6ad161c328965203d5cbae1ab7c22
Yuya Ebihara <ebyhry@gmail.com>
2026-07-11 05:06:21 +0900

    Remove MinioNotificationsAssertions

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeDatabricksMinioReads.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/deltalake/TestDeltaLakeDatabricksMinioReads.java
@@ -13,26 +13,11 @@
  */
 package io.trino.tests.product.deltalake;
 
-import org.testng.annotations.Test;
-
-import static io.minio.messages.EventType.OBJECT_ACCESSED_GET;
-import static io.trino.tests.product.TestGroups.DELTA_LAKE_MINIO;
-import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
-import static io.trino.tests.product.utils.MinioNotificationsAssertions.assertNotificationsCount;
-
 public class TestDeltaLakeDatabricksMinioReads
         extends BaseTestDeltaLakeMinioReads
 {
     public TestDeltaLakeDatabricksMinioReads()
     {
         super("region_databricks", "io/trino/plugin/deltalake/testing/resources/databricks73/region");
-    }
-
-    @Override
-    @Test(groups = {DELTA_LAKE_MINIO, PROFILE_SPECIFIC_TESTS})
-    public void testReadRegionTable()
-    {
-        super.testReadRegionTable();
-        assertNotificationsCount(NOTIFICATIONS_TABLE, OBJECT_ACCESSED_GET, tableName + "/part-00000-98195ace-e492-4cd7-97d1-9b955202874b-c000.snappy.parquet", 1);
     }
 }
