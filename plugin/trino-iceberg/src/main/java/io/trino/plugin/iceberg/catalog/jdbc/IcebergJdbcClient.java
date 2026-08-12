13457747fecd148be702603b14957b55a9087a24
Tony Baeg <tony.baeg@gmail.com>
2026-08-11 20:42:01 -0400

    Use text blocks for SQL in IcebergJdbcClient

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/jdbc/IcebergJdbcClient.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/jdbc/IcebergJdbcClient.java
@@ -51,10 +51,11 @@ public class IcebergJdbcClient
     private void createTableV0(String schemaName, String tableName, String metadataLocation)
     {
         try (Handle handle = Jdbi.open(connectionFactory)) {
-            handle.createUpdate("" +
-                            "INSERT INTO iceberg_tables " +
-                            "(catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location) " +
-                            "VALUES (:catalog, :schema, :table, :metadata_location, null)")
+            handle.createUpdate(
+                            """
+                            INSERT INTO iceberg_tables
+                            (catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location)
+                            VALUES (:catalog, :schema, :table, :metadata_location, null)""")
                     .bind("catalog", catalogName)
                     .bind("schema", schemaName)
                     .bind("table", tableName)
@@ -66,10 +67,11 @@ public class IcebergJdbcClient
     private void createTableV1(String schemaName, String tableName, String metadataLocation)
     {
         try (Handle handle = Jdbi.open(connectionFactory)) {
-            handle.createUpdate("" +
-                            "INSERT INTO iceberg_tables " +
-                            "(catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location, iceberg_type) " +
-                            "VALUES (:catalog, :schema, :table, :metadata_location, null, 'TABLE')")
+            handle.createUpdate(
+                            """
+                            INSERT INTO iceberg_tables
+                            (catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location, iceberg_type)
+                            VALUES (:catalog, :schema, :table, :metadata_location, null, 'TABLE')""")
                     .bind("catalog", catalogName)
                     .bind("schema", schemaName)
                     .bind("table", tableName)
@@ -81,10 +83,11 @@ public class IcebergJdbcClient
     public void alterTable(String schemaName, String tableName, String newMetadataLocation, String previousMetadataLocation)
     {
         try (Handle handle = Jdbi.open(connectionFactory)) {
-            int updatedRecords = handle.createUpdate("" +
-                            "UPDATE iceberg_tables " +
-                            "SET metadata_location = :metadata_location, previous_metadata_location = :previous_metadata_location " +
-                            "WHERE catalog_name = :catalog AND table_namespace = :schema AND table_name = :table AND metadata_location = :previous_metadata_location")
+            int updatedRecords = handle.createUpdate(
+                            """
+                            UPDATE iceberg_tables
+                            SET metadata_location = :metadata_location, previous_metadata_location = :previous_metadata_location
+                            WHERE catalog_name = :catalog AND table_namespace = :schema AND table_name = :table AND metadata_location = :previous_metadata_location""")
                     .bind("metadata_location", newMetadataLocation)
                     .bind("previous_metadata_location", previousMetadataLocation)
                     .bind("catalog", catalogName)
@@ -100,10 +103,11 @@ public class IcebergJdbcClient
     public Optional<String> getMetadataLocation(String schemaName, String tableName)
     {
         try (Handle handle = Jdbi.open(connectionFactory)) {
-            return handle.createQuery("" +
-                            "SELECT metadata_location " +
-                            "FROM iceberg_tables " +
-                            "WHERE catalog_name = :catalog AND table_namespace = :schema AND table_name = :table")
+            return handle.createQuery(
+                            """
+                            SELECT metadata_location
+                            FROM iceberg_tables
+                            WHERE catalog_name = :catalog AND table_namespace = :schema AND table_name = :table""")
                     .bind("catalog", catalogName)
                     .bind("schema", schemaName)
                     .bind("table", tableName)
