e7f066ebeb8157e7f504656b56d0e938e92819b4
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-08-13 15:26:57 +0200

    Add legacy_varchar_to_char_coercion session property
    Introduce the legacy_varchar_to_char_coercion to accompany the
    deprecated.legacy-varchar-to-char-coercion config. Usages of the
    property will be added as separate commit, as they are mechanical.

--- core/trino-main/src/main/java/io/trino/SystemSessionProperties.java
+++ core/trino-main/src/main/java/io/trino/SystemSessionProperties.java
@@ -33,6 +33,7 @@ import io.trino.sql.planner.OptimizerConfig.DistinctAggregationsStrategy;
 import io.trino.sql.planner.OptimizerConfig.JoinDistributionType;
 import io.trino.sql.planner.OptimizerConfig.JoinReorderingStrategy;
 import io.trino.sql.planner.OptimizerConfig.MarkDistinctStrategy;
+import io.trino.type.CharVarcharCoercion;
 
 import java.util.List;
 import java.util.Optional;
@@ -54,6 +55,8 @@ import static io.trino.spi.session.PropertyMetadata.stringProperty;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.TimeZoneKey.getTimeZoneKey;
+import static io.trino.type.CharVarcharCoercion.LEGACY;
+import static io.trino.type.CharVarcharCoercion.SQL_STANDARD;
 import static java.lang.Math.min;
 import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -154,6 +157,7 @@ public final class SystemSessionProperties
     public static final String MERGE_PROJECT_WITH_VALUES = "merge_project_with_values";
     public static final String TIME_ZONE_ID = "time_zone_id";
     public static final String LEGACY_CATALOG_ROLES = "legacy_catalog_roles";
+    public static final String LEGACY_VARCHAR_TO_CHAR_COERCION = "legacy_varchar_to_char_coercion";
     public static final String INCREMENTAL_HASH_ARRAY_LOAD_FACTOR_ENABLED = "incremental_hash_array_load_factor_enabled";
     public static final String MAX_PARTIAL_TOP_N_MEMORY = "max_partial_top_n_memory";
     public static final String RETRY_POLICY = "retry_policy";
@@ -770,6 +774,11 @@ public final class SystemSessionProperties
                         LEGACY_CATALOG_ROLES,
                         "Enable legacy role management syntax that assumed all roles are catalog scoped",
                         featuresConfig.isLegacyCatalogRoles(),
+                        true),
+                booleanProperty(
+                        LEGACY_VARCHAR_TO_CHAR_COERCION,
+                        "Implicitly coerce varchar to char, instead of char to varchar",
+                        featuresConfig.isLegacyVarcharToCharCoercion(),
                         true),
                 booleanProperty(
                         INCREMENTAL_HASH_ARRAY_LOAD_FACTOR_ENABLED,
@@ -1720,6 +1729,11 @@ public final class SystemSessionProperties
     public static boolean isLegacyCatalogRoles(Session session)
     {
         return session.getSystemProperty(LEGACY_CATALOG_ROLES, Boolean.class);
+    }
+
+    public static CharVarcharCoercion getCharVarcharCoercion(Session session)
+    {
+        return session.getSystemProperty(LEGACY_VARCHAR_TO_CHAR_COERCION, Boolean.class) ? LEGACY : SQL_STANDARD;
     }
 
     public static boolean isIncrementalHashArrayLoadFactorEnabled(Session session)
