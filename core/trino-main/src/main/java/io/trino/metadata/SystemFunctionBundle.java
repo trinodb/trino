637321e3494c90d3dedb54f3531d612a93449a35
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-08-17 13:05:59 +0200

    Select CAST(char AS varchar) behavior from the session
    Respect the legacy_varchar_to_char_coercion session property when
    casting from CHAR to VARCHAR. Previously, the cast implementation was
    chosen at system start based on the
    deprecated.legacy-varchar-to-char-coercion configuration property.

--- core/trino-main/src/main/java/io/trino/metadata/SystemFunctionBundle.java
+++ core/trino-main/src/main/java/io/trino/metadata/SystemFunctionBundle.java
@@ -153,7 +153,6 @@ import io.trino.operator.scalar.JoniRegexpFunctions;
 import io.trino.operator.scalar.JoniRegexpReplaceLambdaFunction;
 import io.trino.operator.scalar.JsonFunctions;
 import io.trino.operator.scalar.JsonOperators;
-import io.trino.operator.scalar.LegacyCharToVarcharCast;
 import io.trino.operator.scalar.LegacyVarcharToCharSaturatedFloorCast;
 import io.trino.operator.scalar.LuhnCheckFunction;
 import io.trino.operator.scalar.MapCardinalityFunction;
@@ -536,7 +535,7 @@ public final class SystemFunctionBundle
                 .scalars(FailureFunction.class)
                 .scalars(JoniRegexpCasts.class)
                 .scalars(CharacterStringCasts.class)
-                .scalars(featuresConfig.isLegacyVarcharToCharCoercion() ? LegacyCharToVarcharCast.class : CharToVarcharCast.class)
+                .scalars(CharToVarcharCast.class)
                 .scalars(LuhnCheckFunction.class)
                 .scalar(DecimalOperators.Negation.class)
                 .scalars(DivideRoundToScale.class)
