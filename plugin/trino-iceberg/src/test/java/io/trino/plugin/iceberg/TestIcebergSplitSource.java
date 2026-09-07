61df530a3faadb38b7e61aa3c54177f4655e1246
Marius Grama <findinpath@gmail.com>
2026-09-07 23:03:33 +0200

    Add branch field to IcebergTableHandle
    Resolve FOR VERSION AS OF '<ref>' to a branch name when it names an
    existing Iceberg ref.

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergSplitSource.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergSplitSource.java
@@ -668,6 +668,7 @@ public class TestIcebergSplitSource
                 nationTable.location(),
                 nationTable.properties(),
                 Optional.empty(),
+                Optional.empty(),
                 false,
                 Optional.empty(),
                 ImmutableSet.of(),
