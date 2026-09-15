9c9ed677b6929b363a037bb9ab2d792a9bd6d182
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-09-15 16:29:35 +0200

    Add null checks to Coercion

--- core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/UnwrapRowSubscript.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/UnwrapRowSubscript.java
@@ -35,6 +35,7 @@ import static io.trino.SystemSessionProperties.getCharVarcharCoercion;
 import static io.trino.metadata.GlobalFunctionCatalog.builtinFunctionName;
 import static io.trino.operator.scalar.TryCastFunction.TRY_CAST_FUNCTION_NAME;
 import static io.trino.sql.ir.IrExpressions.cast;
+import static java.util.Objects.requireNonNull;
 
 /**
  * Transforms expressions of the form
@@ -119,5 +120,11 @@ public class UnwrapRowSubscript
         }
     }
 
-    private record Coercion(Type type, boolean safe) {}
+    private record Coercion(Type type, boolean safe)
+    {
+        private Coercion
+        {
+            requireNonNull(type, "type is null");
+        }
+    }
 }
