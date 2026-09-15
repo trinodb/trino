cefcacc356d91ad0e90f5cf5aa126ed4cc01a8d6
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-09-15 15:41:50 +0200

    Convert TranslationResult to a record

--- core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/ImplementExceptDistinctAsUnion.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/iterative/rule/ImplementExceptDistinctAsUnion.java
@@ -91,14 +91,14 @@ public class ImplementExceptDistinctAsUnion
 
         // except predicate: the row must be present in the first source and absent in all the other sources
         ImmutableList.Builder<Expression> predicatesBuilder = ImmutableList.builder();
-        predicatesBuilder.add(comparison(metadata, getCharVarcharCoercion(context.getSession()), GREATER_THAN_OR_EQUAL, result.getCountSymbols().get(0).toSymbolReference(), new Constant(BIGINT, 1L)));
+        predicatesBuilder.add(comparison(metadata, getCharVarcharCoercion(context.getSession()), GREATER_THAN_OR_EQUAL, result.countSymbols().get(0).toSymbolReference(), new Constant(BIGINT, 1L)));
         for (int i = 1; i < node.getSources().size(); i++) {
-            predicatesBuilder.add(comparison(metadata, getCharVarcharCoercion(context.getSession()), EQUAL, result.getCountSymbols().get(i).toSymbolReference(), new Constant(BIGINT, 0L)));
+            predicatesBuilder.add(comparison(metadata, getCharVarcharCoercion(context.getSession()), EQUAL, result.countSymbols().get(i).toSymbolReference(), new Constant(BIGINT, 0L)));
         }
         return Result.ofPlanNode(
                 new ProjectNode(
                         context.getIdAllocator().getNextId(),
-                        new FilterNode(context.getIdAllocator().getNextId(), result.getPlanNode(), and(predicatesBuilder.build())),
+                        new FilterNode(context.getIdAllocator().getNextId(), result.planNode(), and(predicatesBuilder.build())),
                         Assignments.identity(node.getOutputSymbols())));
     }
 }
